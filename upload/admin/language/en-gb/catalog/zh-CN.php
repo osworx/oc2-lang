9f44cf0455b871f976a4f98344bc41c1e0315de5
OSWorX <info@osworx.net>
2025-03-16 11:05:18 +0100

    New translations attribute.php (Chinese Simplified)

---
+++ upload/admin/language/en-gb/catalog/zh-CN.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = '属性';
+
+// Text
+$_['text_success']           = '修改属性成功!';
+$_['text_list']              = '属性列表';
+$_['text_add']               = '添加属性';
+$_['text_edit']              = '编辑属性';
+
+// Column
+$_['column_name']            = '属性名称';
+$_['column_attribute_group'] = '属性分组';
+$_['column_sort_order']      = '排列顺序';
+$_['column_action']          = '操作';
+
+// Entry
+$_['entry_name']             = '属性名称';
+$_['entry_attribute_group']  = '属性分组';
+$_['entry_sort_order']       = '排列顺序';
+
+// Error
+$_['error_warning']          = '警告:请仔细检查错误格式!';
+$_['error_permission']       = '警告: 您没有修改属性的权限！';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = '属性名称必须介于 3 至 64 个字符之间 ！';
+$_['error_product']          = '警告： 无法删除此属性，因其已经分配给 %s 产品 ！';
