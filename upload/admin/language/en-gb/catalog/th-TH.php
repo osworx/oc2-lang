e9f1d603b51acbf3ad500fe1fd75b9665b9cd266
OSWorX <info@osworx.net>
2025-03-16 14:18:48 +0100

    New translations download.php (Thai)

--- upload/admin/language/en-gb/catalog/th-TH.php
+++ upload/admin/language/en-gb/catalog/th-TH.php
@@ -1,46 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'หมวดหมู่สินค้า';
+$_['heading_title']     = 'ดาวน์โหลด';
 
 // Text
-$_['text_success']           = 'คุณได้ปรับเปลี่ยนหมวดหมู่สินค้าเรียบร้อยแล้ว';
-$_['text_list']              = 'รายชื่อหมวดหมู่สินค้า';
-$_['text_add']               = 'เพิ่มหมวดหมู่';
-$_['text_edit']              = 'แก้ไขหมวดหมู่สินค้า';
-$_['text_default']           = 'ค่าเริ่มต้น';
+$_['text_success']      = 'สำเร็จ: คุณได้ปรับเปลี่ยนการดาวน์โหลด';
+$_['text_list']         = 'รายการดาวน์โหลด';
+$_['text_add']          = 'เพิ่มดาวน์โหลด';
+$_['text_edit']         = 'แก้ไขดาวน์โหลด';
+$_['text_upload']       = 'อัปโหลดไฟล์เสร็จเรียบร้อยแล้ว!';
 
 // Column
-$_['column_name']            = 'ชื่อหมวดหมู่';
-$_['column_sort_order']      = 'ลำดับการจัดเรียง';
-$_['column_action']          = 'การดำเนินการ';
+$_['column_name']       = 'ชื่อ';
+$_['column_date_added'] = 'วันที่เพิ่ม';
+$_['column_action']     = 'การดำเนินการ';
 
 // Entry
-$_['entry_name']             = 'ชื่อหมวดหมู่';
-$_['entry_description']      = 'คำอธิบาย';
-$_['entry_meta_title'] 	     = 'ชื่อแท็ก Meta';
-$_['entry_meta_keyword']     = 'คำเรียก Tag Meta';
-$_['entry_meta_description'] = 'คำอธิบาย Tag Meta';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = 'หมวดแม่';
-$_['entry_filter']           = 'ตัวกรอง';
-$_['entry_store']            = 'ร้านค้า';
-$_['entry_image']            = 'รูปภาพ';
-$_['entry_top']              = 'ด้านบน';
-$_['entry_column']           = 'คอลัมน์';
-$_['entry_sort_order']       = 'ลำดับการจัดเรียง';
-$_['entry_status']           = 'สถานะ';
-$_['entry_layout']           = 'Layout Override';
+$_['entry_name']        = 'ชื่อ';
+$_['entry_filename']    = 'ชื่อแฟ้ม';
+$_['entry_mask']        = 'Mask';
 
 // Help
-$_['help_filter']            = '(อัตโนมัติ)';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = 'แสดงในแถบเมนูด้านบน เฉพาะประเภทหลักด้านบน';
-$_['help_column']            = 'จำนวนคอลัมน์ที่จะใช้สำหรับประเภทด้านล่าง 3 เฉพาะประเภทหลักด้านบน';
+$_['help_filename']     = 'คุณสามารถอัปโหลดผ่านปุ่มอัปโหลด หรือใช้ FTP เพื่ออัปโหลดไปยังไดเรกทอรีดาวน์โหลด และป้อนรายละเอียดด้านล่าง';
+$_['help_mask']         = 'ขอแนะนำว่า ชื่อแฟ้มและ Mask ใช้เพื่อหยุดคนที่พยายามใช้ไฟล์ของคุณโดยตรง';
 
 // Error
-$_['error_warning']          = 'คำเตือน: กรุณาตรวจสอบแบบฟอร์มอย่างรอบคอบสำหรับข้อผิดพลาด!';
-$_['error_permission']       = 'คำเตือน: คุณไม่มีสิทธิ์ในการแก้ไขหมวดหมู่';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = 'ชื่อ Meta ต้องมากกว่า 3 และน้อยกว่า 255 อักษร!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = 'คำเตือน: กรุณาตรวจสอบแบบฟอร์มอย่างรอบคอบสำหรับข้อผิดพลาด!';
+$_['error_permission']  = 'คำเตือน: คุณไม่มีสิทธิ์แก้ไขดาวน์โหลด';
+$_['error_name']        = 'ดาวน์โหลดชื่อต้องอยู่ระหว่าง 3 ถึง 64 อักขระ';
+$_['error_upload']      = 'จำเป็นต้องอัพโหลดไฟล์!';
+$_['error_filename']    = 'ชื่อไฟล์ ต้องอยู่ระหว่าง 3 ถึง 64 ตัวอักษร!';
+$_['error_exists']      = 'ไม่มีไฟล์อยู่!';
+$_['error_mask']        = 'Mask ต้องอยู่ระหว่าง 2 ถึง 128 ตัวอักษร!';
+$_['error_filetype']    = 'ชนิดแฟ้มไม่ถูกต้อง';
+$_['error_product']     = 'คำเตือน: การดาวน์โหลดนี้ไม่สามารถลบเนื่องปัจจุบันมีการใช้กับผลิตภัณฑ์ %s อยู่';
