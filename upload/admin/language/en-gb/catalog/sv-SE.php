62b4c8bd437f28eac01a7abd736e9a0c44873c26
OSWorX <info@osworx.net>
2025-03-16 11:13:06 +0100

    New translations recurring.php (Swedish)

--- upload/admin/language/en-gb/catalog/sv-SE.php
+++ upload/admin/language/en-gb/catalog/sv-SE.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'Produkter';
+$_['heading_title']			= 'Avbetalningsprofil';
 
 // Text
-$_['text_success']           = 'Klart: Du har ändrat i produkter!';
-$_['text_list']              = 'Produktlista';
-$_['text_add']               = 'Lägg till produkt';
-$_['text_edit']              = 'Redigera produkt';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'Standard';
-$_['text_option']            = 'Alternativ';
-$_['text_option_value']      = 'Alternativt värde';
-$_['text_percent']           = 'Procentandel';
-$_['text_amount']            = 'Fast belopp';
-
-// Column
-$_['column_name']            = 'Produktnamn';
-$_['column_model']           = 'Artikelnummer';
-$_['column_image']           = 'Bild';
-$_['column_price']           = 'Pris';
-$_['column_quantity']        = 'Antal';
-$_['column_status']          = 'Status';
-$_['column_action']          = 'Åtgärd';
+$_['text_success']          = 'Klart: Du har ändrat avbetalningsplan!';
+$_['text_list']             = 'Avbetalningsplanslista';
+$_['text_add']              = 'Lägg till avbetalningsplan';
+$_['text_edit']             = 'Redigera avbetalningsplan';
+$_['text_day']				= 'Dag';
+$_['text_week']				= 'Vecka';
+$_['text_semi_month']		= '14 dagars';
+$_['text_month']			= 'Månad';
+$_['text_year']				= 'År';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Återkommande belopp beräknas på period och antal.</p><p>Till exempel om du använder en period med "vecka" och antal "2", då debiteras användaren var 14: e dag.</p><p>Avbetalningslängden är antalet gånger som användaren kommer att göra en betalning, ange detta till 0 om du vill ha betalningar tills de annulleras.</p>';
+$_['text_profile']			= 'Återkommande profiler';
+$_['text_trial']			= 'Avbetalningsprofil';
 
 // Entry
-$_['entry_name']             = 'Produktnamn';
-$_['entry_description']      = 'Beskrivning';
-$_['entry_meta_title'] 	     = 'Metatagtitel';
-$_['entry_meta_keyword'] 	 = 'Metataggsökord';
-$_['entry_meta_description'] = 'Metataggbeskrivning';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Artikelnummer';
-$_['entry_sku']              = 'SKU';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'Lagerplats:';
-$_['entry_shipping']         = 'Kräver frakt:';
-$_['entry_manufacturer']     = 'Tillverkare:';
-$_['entry_store']            = 'Butiker';
-$_['entry_date_available']   = 'Tillgänglig (datum):';
-$_['entry_quantity']         = 'Antal';
-$_['entry_minimum']          = 'Minsta antal';
-$_['entry_stock_status']     = 'Ej i lager:<br/><span class="help">Status som visas när en produkt är slut i lagers.</span>';
-$_['entry_price']            = 'Pris';
-$_['entry_tax_class']        = 'Momsklass';
-$_['entry_points']           = 'Poäng:<br/><span class="help">Antal poäng som krävs för att köpa denna vara. Om du inte vill att varan ska kunna köpas med poäng ange "0".</span>';
-$_['entry_option_points']    = 'Poäng:<br/><span class="help">Antal poäng som krävs för att köpa denna vara. Om du inte vill att varan ska kunna köpas med poäng ange "0".</span>';
-$_['entry_subtract']         = 'Dra ifrån lager:';
-$_['entry_weight_class']     = 'Viktklass';
-$_['entry_weight']           = 'Vikt:';
-$_['entry_dimension']        = 'Dimensioner (L x B x H):';
-$_['entry_length_class']     = 'Längdklass';
-$_['entry_length']           = 'Längdklass';
-$_['entry_width']            = 'Bredd';
-$_['entry_height']           = 'Höjd';
-$_['entry_image']            = 'Bild';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Kundgrupp';
-$_['entry_date_start']       = 'Startdatum';
-$_['entry_date_end']         = 'Slutdatum';
-$_['entry_priority']         = 'Prioriteringsordning';
-$_['entry_attribute']        = 'Attribut';
-$_['entry_attribute_group']  = 'Attributgrupp';
-$_['entry_text']             = 'Text';
-$_['entry_option']           = 'Alternativ';
-$_['entry_option_value']     = 'Alternativt värde';
-$_['entry_required']         = 'Krävs';
-$_['entry_status']           = 'Status';
-$_['entry_sort_order']       = 'Sorteringsordning';
-$_['entry_category']         = 'Kategorier';
-$_['entry_filter']           = 'Filter';
-$_['entry_download']         = 'Nedladdningar';
-$_['entry_related']          = 'Relaterade produkter';
-$_['entry_tag']          	 = 'Produkttaggar';
-$_['entry_reward']           = 'Lojalitetspoäng produkten ger vid köp';
-$_['entry_layout']           = 'Layoutomställning';
-$_['entry_recurring']        = 'Återkommande profiler';
+$_['entry_name']		    = 'Namn';
+$_['entry_price']			= 'Pris';
+$_['entry_duration']		= 'Varaktighet';
+$_['entry_cycle']			= 'Cykel';
+$_['entry_frequency']		= 'Frekvens';
+$_['entry_trial_price']		= 'Prov pris';
+$_['entry_trial_duration']	= 'Avbetalningens längd';
+$_['entry_trial_status']	= 'Avbetalningens status';
+$_['entry_trial_cycle']	    = 'Avbetalningens antal';
+$_['entry_trial_frequency'] = 'Avbetalningsperiod';
+$_['entry_status']			= 'Status';
+$_['entry_sort_order']		= 'Sorteringsordning';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'Lagerhållningsenhet';
-$_['help_upc']               = 'Allmän produktkod';
-$_['help_ean']               = 'Europeiska artikelnummer';
-$_['help_jan']               = 'Japanska artikelnummer';
-$_['help_isbn']              = 'Internationellt standardboknummer';
-$_['help_mpn']               = 'Tillverkarens artikelnummer';
-$_['help_manufacturer']      = '(Komplettera automatiskt)';
-$_['help_minimum']           = 'Tvingad till ett minsta ordervärde';
-$_['help_stock_status']      = 'Status visas när en produkt är slutsåld';
-$_['help_points']            = 'Antal poäng som behövs för att köpa denna punkt. Om du inte vill lämna denna produkt köpas med poäng som 0.';
-$_['help_category']          = '(Komplettera automatiskt)';
-$_['help_filter']            = '(Komplettera automatiskt)';
-$_['help_download']          = '(Komplettera automatiskt)';
-$_['help_related']           = '(Komplettera automatiskt)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'Namn';
+$_['column_sort_order']	    = 'Sorteringsordning';
+$_['column_action']         = 'Åtgärd';
 
 // Error
-$_['error_warning']          = 'Varning: Kolla gärna formuläret noga vad det gäller fel!';
-$_['error_permission']       = 'Varning: Du har inte behörighet att ändra i produkter!';
-$_['error_name']             = 'Produktnamnet måste innheålla mer än 3 och mindre än 255 tecken!';
-$_['error_meta_title']       = 'Metatitel måste vara större än 3 och mindre än 255 tecken!';
-$_['error_model']            = 'Produktens artikelnummer måste innehålla mer än 3 och mindre än 64 tecken!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Varning: Kolla gärna formuläret noga vad det gäller fel!';
+$_['error_permission']		= 'Varning: Du har inte behörighet att ändra avbetalningsplan!';
+$_['error_name']			= 'Profilnamn måste vara större än 3 och mindre än 255 tecken!';
+$_['error_product']			= 'Varning: Denna avbetalningsplan kan inte raderas eftersom det för närvarande är tilldelad %s produkter!';
