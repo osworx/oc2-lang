a3cd054a88fe01a2051bbcd937d62bdbe3ffd70c
OSWorX <info@osworx.net>
2025-03-16 14:19:51 +0100

    New translations information.php (Belarusian)

--- upload/admin/language/en-gb/catalog/be-BY.php
+++ upload/admin/language/en-gb/catalog/be-BY.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'Фільтры';
+$_['heading_title']          = 'Інфармацыя';
 
 // Text
-$_['text_success']      = 'Поспех: Фільтры змененыя!';
-$_['text_list']         = 'Спіс фільтраў';
-$_['text_add']          = 'Дадаць фільтр';
-$_['text_edit']         = 'Рэдагаваць фільтр';
+$_['text_success']           = 'Вы паспяхова змянілі інфармацыю!';
+$_['text_list']              = 'Спіс інфармацыі';
+$_['text_add']               = 'Дадаць інфармацыю';
+$_['text_edit']              = 'Рэдагаваць інфармацыю';
+$_['text_default']           = 'Па-змаўчанню';
 
 // Column
-$_['column_group']      = 'Група фільтраў';
-$_['column_sort_order'] = 'Парадак сартавання';
-$_['column_action']     = 'Дзеянне';
+$_['column_title']           = 'Тытул інфармацыі';
+$_['column_sort_order']	     = 'Парадак сартавання';
+$_['column_action']          = 'Дзеянне';
 
 // Entry
-$_['entry_group']       = 'Назва групы фільтраў';
-$_['entry_name']        = 'Назва фільтра';
-$_['entry_sort_order']  = 'Парадак сартавання';
+$_['entry_title']            = 'Тытул інфармацыі';
+$_['entry_description']      = 'Апісанне';
+$_['entry_store']            = 'Крамы';
+$_['entry_meta_title'] 	     = 'Мета-тэг Title';
+$_['entry_meta_keyword'] 	 = 'Мета-тэг Keywords';
+$_['entry_meta_description'] = 'Мета-тэг Description';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Уніз';
+$_['entry_status']           = 'Статус';
+$_['entry_sort_order']       = 'Парадак сартавання';
+$_['entry_layout']           = 'Перавызначэнне разметкі';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Паказаць у ніжняй частцы.';
 
 // Error
-$_['error_warning']     = 'Увага: Калі ласка, ўважліва праверце ўведзеные дадзеныя!';
-$_['error_permission']  = 'Увага: Вы не можаце змяняць фільтры!';
-$_['error_group']       = 'Даўжыня назвы групы фільтраў павінна быць ад 1 да 64 сімвалаў!';
-$_['error_name']        = 'Назва фільтра павінна быць ад 1 да 64 сімвалаў!';
+$_['error_warning']          = 'Увага: Калі ласка, ўважліва праверце ўведзеные дадзеныя!';
+$_['error_permission']       = 'Увага: у Вас няма права змяняць інфармацыю!';
+$_['error_title']            = 'Тытул інфармацыі павінен мець ад 3 да 64 сімвалаў!';
+$_['error_description']      = 'Апісанне павінна складацца больш чым з 64 сімвалаў!';
+$_['error_meta_title']       = 'Даўжыня meta title павінна быць ад 3 да 255 сімвалаў!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'Увага: Гэтая інфармацыйная старонка не можа быць выдалена, бо звязана з правіламі для профіляў карыстальнікаў!';
+$_['error_checkout']         = 'Увага: Гэтая інфармацыйная старонка не можа быць выдалена, бо звязана з правіламі разлікаў у краме!';
+$_['error_affiliate']        = 'Увага: Гэтая інфармацыйная старонка не можа быць выдалена, бо звязана з правіламі для партнёраў!';
+$_['error_return']           = 'Увага: Гэтая інфармацыйная старонка не можа быць выдалена бо выкарыстоўваецца для правілаў вяртання пакупак!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
