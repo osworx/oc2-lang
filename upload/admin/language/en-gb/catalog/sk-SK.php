12a4d37a12fed09ab0ce64e36c2ce74af4098239
OSWorX <info@osworx.net>
2025-03-16 14:18:34 +0100

    New translations download.php (Slovak)

--- upload/admin/language/en-gb/catalog/sk-SK.php
+++ upload/admin/language/en-gb/catalog/sk-SK.php
@@ -1,46 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Kategórie';
+$_['heading_title']     = 'Stiahnutia';
 
 // Text
-$_['text_success']           = 'Hotovo: Upravili ste kategórie!';
-$_['text_list']              = 'Zoznam kategórií';
-$_['text_add']               = 'Pridať kategóriu';
-$_['text_edit']              = 'Upraviť kategóriu';
-$_['text_default']           = 'Pôvodné';
+$_['text_success']      = 'Úspech: Zmenili ste stiahnutia!';
+$_['text_list']         = 'Zoznam stiahnutí';
+$_['text_add']          = 'Pridaj stiahnutie';
+$_['text_edit']         = 'Upraviť stiahnutie';
+$_['text_upload']       = 'Váš súbor bol úspešne nahraný!';
 
 // Column
-$_['column_name']            = 'Názov kategórie';
-$_['column_sort_order']      = 'Poradie';
-$_['column_action']          = 'Akcia';
+$_['column_name']       = 'Meno stiahnutia';
+$_['column_date_added'] = 'Dátum pridaný';
+$_['column_action']     = 'Akcia';
 
 // Entry
-$_['entry_name']             = 'Názov kategórie';
-$_['entry_description']      = 'Popis';
-$_['entry_meta_title'] 	     = 'Meta Tag Title (nadpis)';
-$_['entry_meta_keyword']     = 'Meta Tag Kľúčové slová';
-$_['entry_meta_description'] = 'Meta Tag Popis';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = 'Nadradená kategória';
-$_['entry_filter']           = 'Filtre';
-$_['entry_store']            = 'Obchody';
-$_['entry_image']            = 'Obrázok';
-$_['entry_top']              = 'Zobraziť v menu';
-$_['entry_column']           = 'Stĺpce';
-$_['entry_sort_order']       = 'Poradie';
-$_['entry_status']           = 'Stav';
-$_['entry_layout']           = 'Šablóna';
+$_['entry_name']        = 'Meno stiahnutia';
+$_['entry_filename']    = 'Meno súboru';
+$_['entry_mask']        = 'Maska';
 
 // Help
-$_['help_filter']            = '(Automatické dokončovanie)';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = 'Zobraziť v hornom menu. Funguje len pre najvyššie kategórie.';
-$_['help_column']            = 'Počet stĺpcov pre 3 podkategórie. Funguje len pre najvyššie nadradené kategórie.';
+$_['help_filename']     = 'Možete nahrať pomocou tlačítka nahrať alebo použiť FTP k nahraniu stiahnutého rozcestníka a vložte následovné údaje.';
+$_['help_mask']         = 'Odporúča sa, aby názov súboru a maska boli odlišné, aby zastavilo ľudí pokúšajúcich sa priamo spojiť s vašimi stianutiami.';
 
 // Error
-$_['error_warning']          = 'Varovanie: Skontrolujte formulár starostlivo kvôli chybám!';
-$_['error_permission']       = 'Varovanie: Nemáte potrebné oprávnenie na upravenie kategórie!';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = 'Meta Tag Title (nadpis) musí mať aspoň 3 a menej ako 255 znakov!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = 'Varovanie: Skontrolujte formulár starostlivo kvôli chybám!';
+$_['error_permission']  = 'Upozornenie: Nemáte oprávnenie k zmene sťahovania!';
+$_['error_name']        = 'Názov sťahovania musí mať medzi 3 a 64 znakov!';
+$_['error_upload']      = 'Nahranie nutné!';
+$_['error_filename']    = 'Meno súboru musí mať medzi 3 a 128 znakov!';
+$_['error_exists']      = 'Súbor neexistuje!';
+$_['error_mask']        = 'Maska musí mať medzi 3 a 128 znakov!';
+$_['error_filetype']    = 'Neplatný typ súboru!';
+$_['error_product']     = 'Upozornenie: Toto sťahovanie nemôže byť vymazané, pretože je teraz priradené k %s produktom!';
