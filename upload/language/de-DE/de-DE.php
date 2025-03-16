8979dc6e29365ea015c4a6af18532e623f333c80
OSWorX <info@osworx.net>
2025-03-16 09:08:30 +0100

    New translations recurring.php (German)

--- upload/language/de-DE/de-DE.php
+++ upload/language/de-DE/de-DE.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'Produkte';
+$_['heading_title']			= 'Abozahlungen';
 
 // Text
-$_['text_success']           = 'Produkt erfolgreich bearbeitet';
-$_['text_list']              = 'Übersicht';
-$_['text_add']               = 'Neu';
-$_['text_edit']              = 'Bearbeiten';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'Standardshop';
-$_['text_option']            = 'Option';
-$_['text_option_value']      = 'Optionswert';
-$_['text_percent']           = 'Prozente';
-$_['text_amount']            = 'Fixbetrag';
-
-// Column
-$_['column_name']            = 'Name';
-$_['column_model']           = 'Art.Nr.';
-$_['column_image']           = 'Bild';
-$_['column_price']           = 'Preis';
-$_['column_quantity']        = 'Menge';
-$_['column_status']          = 'Status';
-$_['column_action']          = 'Aktion';
+$_['text_success']          = 'Abozahlung wurde erfolgreich bearbeitet';
+$_['text_list']             = 'Übersicht';
+$_['text_add']              = 'Neu';
+$_['text_edit']             = 'Bearbeiten';
+$_['text_day']				= 'Tag';
+$_['text_week']				= 'Woche';
+$_['text_semi_month']		= '14 Tage';
+$_['text_month']			= 'Monat';
+$_['text_year']				= 'Jahr';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Wiederholende Zahlungen werden aus Frequenz und Wiederholung kalkuliert.</p><p>Beispiel: Frequenz : Woche, Wiederholung : 2 = alle 2 Wochen erfolgt eine Verrechnung.</p><p>Dauer ist die Anzahl wieoft Zahlungen erfolgen, wird der Wert 0 (Null) angewendet, erfolgen Verrechnung solange der Kunde von sich aus storniert/beendet.</p>';
+$_['text_profile']			= 'Abozahlung';
+$_['text_trial']			= 'Testperiode';
 
 // Entry
-$_['entry_name']             = 'Name';
-$_['entry_description']      = 'Beschreibung';
-$_['entry_meta_title'] 	     = 'Metatag Titel';
-$_['entry_meta_keyword'] 	 = 'Metatag Schlüsselwörter';
-$_['entry_meta_description'] = 'Metatag Beschreibung';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Art.Nr.';
-$_['entry_sku']              = 'Bestandseinheit (SKU)';
-$_['entry_upc']              = 'Strichcode UPC';
-$_['entry_ean']              = 'Strichcode EAN';
-$_['entry_jan']              = 'Strichcode JAN';
-$_['entry_isbn']             = 'Strichcode ISBN';
-$_['entry_mpn']              = 'Strichcode MPN';
-$_['entry_location']         = 'Lagerort';
-$_['entry_shipping']         = 'Erfordert Versand';
-$_['entry_manufacturer']     = 'Hersteller';
-$_['entry_store']            = 'Shops';
-$_['entry_date_available']   = 'Verfügbar ab';
-$_['entry_quantity']         = 'Menge';
-$_['entry_minimum']          = 'Mindestbestellmenge';
-$_['entry_stock_status']     = 'Kein Lagerstand';
-$_['entry_price']            = 'Preis';
-$_['entry_tax_class']        = 'Steuerklasse';
-$_['entry_points']           = 'Punkte';
-$_['entry_option_points']    = 'Punkte';
-$_['entry_subtract']         = 'Von Lagerstand abziehen';
-$_['entry_weight_class']     = 'Gewichtseinheit';
-$_['entry_weight']           = 'Gewicht';
-$_['entry_dimension']        = 'Abmessungen (L x B x H)';
-$_['entry_length_class']     = 'Längeneinheit';
-$_['entry_length']           = 'Länge';
-$_['entry_width']            = 'Breite';
-$_['entry_height']           = 'Höhe';
-$_['entry_image']            = 'Bild';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Kundengruppe';
-$_['entry_date_start']       = 'Ab';
-$_['entry_date_end']         = 'Bis';
-$_['entry_priority']         = 'Reihenfolge';
-$_['entry_attribute']        = 'Eigenschaft';
-$_['entry_attribute_group']  = 'Gruppe';
-$_['entry_text']             = 'Text (einzeilig)';
-$_['entry_option']           = 'Option';
-$_['entry_option_value']     = 'Optionswert';
-$_['entry_required']         = 'Benötigt';
-$_['entry_status']           = 'Status';
-$_['entry_sort_order']       = 'Reihenfolge';
-$_['entry_category']         = 'Kategorien';
-$_['entry_filter']           = 'Filter';
-$_['entry_download']         = 'Downloads';
-$_['entry_related']          = 'Ähnliche Produkte';
-$_['entry_tag']          	 = 'Suchbegriffe';
-$_['entry_reward']           = 'Bonuspunkte';
-$_['entry_layout']           = 'Layout überschreiben';
-$_['entry_recurring']        = 'Abozahlung';
+$_['entry_name']		    = 'Name';
+$_['entry_price']			= 'Preis';
+$_['entry_duration']		= 'Dauer';
+$_['entry_cycle']			= 'Wiederholung';
+$_['entry_frequency']		= 'Frequenz';
+$_['entry_trial_price']		= 'Testpreis';
+$_['entry_trial_duration']	= 'Testdauer';
+$_['entry_trial_status']	= 'Teststatus';
+$_['entry_trial_cycle']	    = 'Testwiederholung';
+$_['entry_trial_frequency'] = 'Testanzahl';
+$_['entry_status']			= 'Status';
+$_['entry_sort_order']		= 'Reihenfolge';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'Siehe <a href=&quot;http://de.wikipedia.org/wiki/Stock_Keeping_Unit&quot; title=&quot;SKU bei Wikipedia&quot; target=&quot;_blank&quot;>SKU auf Wikipedia</a>';
-$_['help_upc']               = 'Universale Artikelnummer';
-$_['help_ean']               = 'Europäische Artikelnummer';
-$_['help_jan']               = 'Japanische Artikelnummer';
-$_['help_isbn']              = 'Internationale Standard Buchnummer';
-$_['help_mpn']               = 'Hersteller Artikelnummer';
-$_['help_manufacturer']      = 'Begriff eintippen, System zeigt passende an';
-$_['help_minimum']           = 'Mindestmenge welche bestellt werden muss';
-$_['help_stock_status']      = 'Angezeigter Status wenn Produkt nicht (mehr) lagernd ist';
-$_['help_points']            = 'Anzahl Punkte die nötig sind um dieses Produkt zu kaufen. 0 (Null) bedeutet, dass das Produkt nicht mit Punkten gekauft werden kann';
-$_['help_category']          = 'Begriff eintippen, System zeigt passende an';
-$_['help_filter']            = 'Begriff eintippen, System zeigt passende an';
-$_['help_download']          = 'Begriff eintippen, System zeigt passende an';
-$_['help_related']           = 'Begriff eintippen, System zeigt passende an';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'Name';
+$_['column_sort_order']	    = 'Reihenfolge';
+$_['column_action']         = 'Aktion';
 
 // Error
-$_['error_warning']          = 'Erforderliche Daten fehlen, bitte Felder überprüfen.';
-$_['error_permission']       = 'Hinweis: keine Rechte für diese Aktion';
-$_['error_name']             = 'Produktname muss zwischen 3 und 255 Buchstaben lang sein';
-$_['error_meta_title']       = 'Metatag Titel muss zwischen 3 und 255 Zeichen lang sein';
-$_['error_model']            = 'Artikelnr. muss zwischen 3 und 64 Zeichen lang sein!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Erforderliche Daten fehlen, bitte Felder überprüfen.';
+$_['error_permission']		= 'Heinweis: keine Rechte für diese Aktion';
+$_['error_name']			= 'Name muss zwischen 3 und 255 Zeichen lang sein';
+$_['error_product']			= 'Diese Abozahlung kann nicht gelöscht werden da sie %s Produkt(en) verbunden ist';
