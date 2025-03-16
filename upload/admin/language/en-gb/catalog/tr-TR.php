bdc6c4551790e677d1489f5d97493c8ac88114e6
OSWorX <info@osworx.net>
2025-03-16 11:13:56 +0100

    New translations review.php (Turkish)

--- upload/admin/language/en-gb/catalog/tr-TR.php
+++ upload/admin/language/en-gb/catalog/tr-TR.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'Yinelenen Profiller';
+$_['heading_title']     = 'Yorumlar';
 
 // Text
-$_['text_success']          = 'Başarı:Yinelenen Profilleri düzenlediniz!';
-$_['text_list']             = 'Yinelenen Profil Listesi';
-$_['text_add']              = 'Yinelenen Profil ekle';
-$_['text_edit']             = 'Yinelenen Profilleri düzenle';
-$_['text_day']				= 'Gün';
-$_['text_week']				= 'Hafta';
-$_['text_semi_month']		= 'ayda iki kez olan';
-$_['text_month']			= 'Ay';
-$_['text_year']				= 'Yıl';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Yinelenen Miktarlar karşılaşma sıklığı ve dönüşüm miktarına göre hesaplanmaktadır. </p><p>Örneğin Karşılaşma Sıklığı olarak "Hafta" ve Dönüşüm Miktarı olarak "2" değerlerini kullanırsanız kullanıcı faturası 2 haftada bir defa düzenlenecektir. </p><p>Süre kullanıcının ödeme yapacağı Zaman Birimi Miktarı kadardır, ödemeleri iptal edileceği zamana kadar bekletmek isterseniz bu değeri "0" olarak belirleyin. </p>';
-$_['text_profile']			= 'Yinelenen profil';
-$_['text_trial']			= 'Deneme Profili';
-
-// Entry
-$_['entry_name']		    = 'İsim';
-$_['entry_price']			= 'Fiyat';
-$_['entry_duration']		= 'Süre';
-$_['entry_cycle']			= 'Döngü';
-$_['entry_frequency']		= 'Sıklık';
-$_['entry_trial_price']		= 'Deneme Fiyatı';
-$_['entry_trial_duration']	= 'Deneme Süresi';
-$_['entry_trial_status']	= 'Deneme Durumu';
-$_['entry_trial_cycle']	    = 'Deneme Döngüsü';
-$_['entry_trial_frequency'] = 'Deneme Sıklığı';
-$_['entry_status']			= 'Durum';
-$_['entry_sort_order']		= 'Sıralama Düzeni';
+$_['text_success']      = 'Başarılı:Yorumları düzenlediniz!';
+$_['text_list']         = 'Yorum Listesi';
+$_['text_add']          = 'Yorum Ekle';
+$_['text_edit']         = 'Yorumu düzenle';
 
 // Column
-$_['column_name']			= 'İsim';
-$_['column_sort_order']	    = 'Sıralama Düzeni';
-$_['column_action']         = 'Eylem';
+$_['column_product']    = 'Ürün';
+$_['column_author']     = 'Yazar';
+$_['column_rating']     = 'Derecelendirme';
+$_['column_status']     = 'Durum';
+$_['column_date_added'] = 'Eklenme Tarihi';
+$_['column_action']     = 'Eylem';
+
+// Entry
+$_['entry_product']     = 'Ürün';
+$_['entry_author']      = 'Yazar';
+$_['entry_rating']      = 'Derecelendirme';
+$_['entry_status']      = 'Durum';
+$_['entry_text']        = 'Yazı';
+$_['entry_date_added']  = 'Eklenme Tarihi';
+
+// Help
+$_['help_product']      = '(Otomatik Tamamla)';
 
 // Error
-$_['error_warning']         = 'Uyarı: Lütfen formu gösterilen hatalar nedeniyle dikkatlice kontrol edin!';
-$_['error_permission']		= 'Uyarı: Yinelenen profilleri düzenleme iznine sahip değilsiniz!';
-$_['error_name']			= 'Profil Adı 3 karakterden büyük ve 255 karakterden küçük olmalıdır!';
-$_['error_product']			= 'Uyarı:Yinelenen Profil ürünlerin %s kısmına atanmış olduğu için silinemez!';
+$_['error_warning']     = 'Uyarı: Lütfen formu gösterilen hatalar nedeniyle dikkatlice kontrol edin!';
+$_['error_permission']  = 'Uyarı: Yorumları değiştirme yetkiniz yok!';
+$_['error_product']     = 'Zorunlu Ürün!';
+$_['error_author']      = 'Yazar Adı 3 ile 64 karakter arasında olmalıdır!';
+$_['error_text']        = 'Yorum Metni en az 1 karakter olmalıdır!';
+$_['error_rating']      = 'Yorum derecelendirme zorunludur!';
