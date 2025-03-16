5315681b58d34e95e6bb3d0c62d1ceb672166c2c
OSWorX <info@osworx.net>
2025-03-16 09:08:51 +0100

    New translations footer.php (Greek)

--- upload/language/el-GR/el-GR.php
+++ upload/language/el-GR/el-GR.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Διαχειριστής Εικόνων';
-
 // Text
-$_['text_uploaded']    = 'Επιτυχία: Το αρχείο έχει φορτωθεί!';
-$_['text_directory']   = 'Επιτυχία: Ο κατάλογος δημιουργήθηκε!';
-$_['text_delete']      = 'Επιτυχία: Το αρχείο ή ο κατάλογος έχει διαγραφεί!';
-
-// Entry
-$_['entry_search']     = 'Αναζήτηση..';
-$_['entry_folder']     = 'Όνομα φακέλου';
-
-// Error
-$_['error_permission'] = 'Προειδοποίηση: Δεν επιτρέπεται η πρόσβαση!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Προειδοποίηση: Ένα αρχείου ή ένας κατάλογος με το ίδιο όνομα υπάρχει ήδη!';
-$_['error_directory']  = 'Προειδοποίηση: Ο κατάλογος δεν υπάρχει!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Προειδοποίηση: Λάθος τύπος αρχείου!';
-$_['error_upload']     = 'Προειδοποίηση: Το αρχείο δεν μπόρεσε να φορτωθεί για άγνωστο λόγο!';
-$_['error_delete']     = 'Προειδοποίηση: Δεν μπορείτε να διαγράψετε αυτόν τον κατάλογο!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Έκδοση %s';
