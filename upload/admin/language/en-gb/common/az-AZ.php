d9222da1253159766fda3502a7005bc79c49ff1f
OSWorX <info@osworx.net>
2025-03-16 11:19:39 +0100

    New translations header.php (Azerbaijani)

--- upload/admin/language/en-gb/common/az-AZ.php
+++ upload/admin/language/en-gb/common/az-AZ.php
@@ -1,19 +1,23 @@
 <?php
-// header
-$_['heading_title']   = 'Şifrənizi unutmusunuz?';
+// Heading
+$_['heading_title']          = 'OpenCart';
 
 // Text
-$_['text_forgotten']  = 'Unudulmuş Keçidsözü';
-$_['text_your_email'] = 'E-mail Ünvanınız';
-$_['text_email']      = 'Enter the e-mail address associated with your account. Click submit to have a password reset link e-mailed to you.';
-$_['text_success']    = 'An email with a confirmation link has been sent your admin email address.';
-
-// Entry
-$_['entry_email']     = 'E-mail Ünvanı';
-$_['entry_password']  = 'Yeni Keçidsözü';
-$_['entry_confirm']   = 'Doğrula';
-
-// Error
-$_['error_email']     = 'Warning: The E-Mail Address was not found in our records, please try again!';
-$_['error_password']  = 'Password must be between 4 and 20 characters!';
-$_['error_confirm']   = 'Password and password confirmation do not match!';
+$_['text_order']             = 'Sifarişlər';
+$_['text_processing_status'] = 'Processing';
+$_['text_complete_status']   = 'Completed';
+$_['text_customer']          = 'Müştərilər';
+$_['text_online']            = 'Customers Online';
+$_['text_approval']          = 'Pending approval';
+$_['text_product']           = 'Məhsullar';
+$_['text_stock']             = 'Out of stock';
+$_['text_review']            = 'Rəylər';
+$_['text_return']            = 'Returns';
+$_['text_affiliate']         = 'Affiliates';
+$_['text_store']             = 'Dükanlar';
+$_['text_front']             = 'Store Front';
+$_['text_help']              = 'Help';
+$_['text_homepage']          = 'OpenCart Homepage';
+$_['text_support']           = 'Support Forum';
+$_['text_documentation']     = 'Documentation';
+$_['text_logout']            = 'Logout';
