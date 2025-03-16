4be7e3b43c9c68973d2fb67c76088ea477d4c573
OSWorX <info@osworx.net>
2025-03-16 09:09:43 +0100

    New translations login.php (Hungarian)

--- upload/language/hu-HU/hu-HU.php
+++ upload/language/hu-HU/hu-HU.php
@@ -1,23 +1,19 @@
 <?php
-// Heading
-$_['heading_title']          = 'OpenCart';
+// header
+$_['heading_title']  = 'Adminisztráció';
 
 // Text
-$_['text_order']             = 'Megrendelések';
-$_['text_processing_status'] = 'Processing';
-$_['text_complete_status']   = 'Teljesített';
-$_['text_customer']          = 'Vásárlók';
-$_['text_online']            = 'Online ügyfelek';
-$_['text_approval']          = 'Jóváhagyás függőben';
-$_['text_product']           = 'Termékek';
-$_['text_stock']             = 'Nincs raktáron';
-$_['text_review']            = 'Vélemények';
-$_['text_return']            = 'Visszárúk';
-$_['text_affiliate']         = 'Partnerprogram';
-$_['text_store']             = 'Áruházak';
-$_['text_front']             = 'Áruház';
-$_['text_help']              = 'Segítség';
-$_['text_homepage']          = 'OpenCart Homepage';
-$_['text_support']           = 'Támogatói fórum';
-$_['text_documentation']     = 'Dokumentáció';
-$_['text_logout']            = 'Kijelentkezés';
+$_['text_heading']   = 'Adminisztráció';
+$_['text_login']     = 'Kérjük, adja meg a bejelentkezési adatait.';
+$_['text_forgotten'] = 'Elfelejtett jelszó';
+
+// Entry
+$_['entry_username'] = 'Felhasználónév';
+$_['entry_password'] = 'Jelszó';
+
+// Button
+$_['button_login']   = 'Belépés';
+
+// Error
+$_['error_login']    = 'Nincs egyezõ felhasználónév és/vagy jelszó.';
+$_['error_token']    = 'Érvénytelen. Jelentkezzen be újra.';
