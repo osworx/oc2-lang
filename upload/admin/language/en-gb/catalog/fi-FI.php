43d8153f18461617f3c011584eddb0e4210727cf
OSWorX <info@osworx.net>
2025-03-16 11:06:38 +0100

    New translations category.php (Finnish)

--- upload/admin/language/en-gb/catalog/fi-FI.php
+++ upload/admin/language/en-gb/catalog/fi-FI.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Ominaisuusryhmät';
+$_['heading_title']          = 'Kategoriat';
 
 // Text
-$_['text_success']      = 'Onnistui: ominaisuusryhmiä on muutettu!';
-$_['text_list']         = 'Ominaisuusryhmät';
-$_['text_add']          = 'Lisää ominaisuusryhmä';
-$_['text_edit']         = 'Muokkaa ominaisuusryhmää';
+$_['text_success']           = 'Olet muokannut kategorioita onnistuneesti!';
+$_['text_list']              = 'Kategorialista';
+$_['text_add']               = 'Lisää kategoria';
+$_['text_edit']              = 'Muokkaa kategoriaa';
+$_['text_default']           = 'Oletus';
 
 // Column
-$_['column_name']       = 'Ominaisuusryhmän nimi';
-$_['column_sort_order'] = 'Lajittelujärjestys';
-$_['column_action']     = 'Toiminto';
+$_['column_name']            = 'Kategorian nimi';
+$_['column_sort_order']      = 'Lajittelujärjestys';
+$_['column_action']          = 'Toiminto';
 
 // Entry
-$_['entry_name']        = 'Ominaisuusryhmän nimi';
-$_['entry_sort_order']  = 'Lajittelujärjestys';
+$_['entry_name']             = 'Kategorian nimi';
+$_['entry_description']      = 'Kuvaus';
+$_['entry_meta_title'] 	     = 'Metatagien otsikko';
+$_['entry_meta_keyword']     = 'Metatagien avainsanat';
+$_['entry_meta_description'] = 'Metatagin kuvaus';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Ylempi kategoria';
+$_['entry_filter']           = 'Suodattimet';
+$_['entry_store']            = 'Kaupat';
+$_['entry_image']            = 'Kuva';
+$_['entry_top']              = 'Ylös';
+$_['entry_column']           = 'Sarakkeita';
+$_['entry_sort_order']       = 'Lajittelujärjestys';
+$_['entry_status']           = 'Tila';
+$_['entry_layout']           = 'Ohita asettelu';
+
+// Help
+$_['help_filter']            = '(Automaattinen täydennys)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Näyttää kategorian päävalikossa. Toimii vain ylimmän tason kategorioille.';
+$_['help_column']            = 'Kolmen alimman kategorian käyttämä pystyrivien määrä. Vaikuttaa vain ylimmän tason kategorioissa.';
 
 // Error
-$_['error_warning']     = 'Varoitus: tarkista lomake virheiden varalta!';
-$_['error_permission']  = 'Varoitus: sinulla ei ole oikeuksia muokata ominaisuusryhmiä!';
-$_['error_name']        = 'Ominaisuusryhmän nimen tulee olla 3 - 64 merkkiä pitkä!';
-$_['error_attribute']   = 'Varoitus: tätä ominaisuusryhmää ei voi poistaa, koska %s ominaisuutta on määritetty siihen!';
-$_['error_product']     = 'Varoitus: tätä ominaisuusryhmää ei voi poistaa, koska %s tuotetta käyttää sitä!';
+$_['error_warning']          = 'Varoitus: tarkista lomake virheiden varalta!';
+$_['error_permission']       = 'Varoitus: sinulla ei ole oikeuksia muokata kategorioita!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Sivun otsikon  tulee olla 3 - 255 merkkiä pitkä!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
