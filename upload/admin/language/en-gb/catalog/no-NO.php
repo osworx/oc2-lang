0e899fbacd95e7e763a31c4da5f938fbc26ac344
OSWorX <info@osworx.net>
2025-03-16 11:09:34 +0100

    New translations information.php (Norwegian)

--- upload/admin/language/en-gb/catalog/no-NO.php
+++ upload/admin/language/en-gb/catalog/no-NO.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'Filtre';
+$_['heading_title']          = 'Informasjonssider';
 
 // Text
-$_['text_success']      = 'Endringer ble vellykket lagret.';
-$_['text_list']         = 'Liste';
-$_['text_add']          = 'Legg til';
-$_['text_edit']         = 'Endre';
+$_['text_success']           = 'Endringer ble vellykket lagret.';
+$_['text_list']              = 'Liste';
+$_['text_add']               = 'Legg til';
+$_['text_edit']              = 'Endre';
+$_['text_default']           = 'Standard';
 
 // Column
-$_['column_group']      = 'Gruppe';
-$_['column_sort_order'] = 'Sortering';
-$_['column_action']     = 'Valg';
+$_['column_title']           = 'Tittel';
+$_['column_sort_order']	     = 'Sortering';
+$_['column_action']          = 'Valg';
 
 // Entry
-$_['entry_group']       = 'Gruppenavn';
-$_['entry_name']        = 'Filternavn';
-$_['entry_sort_order']  = 'Sortering';
+$_['entry_title']            = 'Tittel';
+$_['entry_description']      = 'Beskrivelse';
+$_['entry_store']            = 'Butikker';
+$_['entry_meta_title'] 	     = 'Meta-tittel';
+$_['entry_meta_keyword'] 	 = 'Meta-nøkkelord';
+$_['entry_meta_description'] = 'Meta-beskrivelse';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Bunnmeny';
+$_['entry_status']           = 'Status';
+$_['entry_sort_order']       = 'Sortering';
+$_['entry_layout']           = 'Overstyring av visning';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Vis i bunnfeltet.';
 
 // Error
-$_['error_warning']     = 'Påse at alle påkrevde felter er fylt inn.';
-$_['error_permission']  = 'Du har ikke rettigheter til å utføre valgte handling.';
-$_['error_group']       = 'Navn må inneholde mellom 3 og 64 tegn.';
-$_['error_name']        = 'Filternavn må inneholde mellom 3 og 64 tegn.';
+$_['error_warning']          = 'Påse at alle påkrevde felter er fylt inn.';
+$_['error_permission']       = 'Du har ikke rettigheter til å utføre valgte handling.';
+$_['error_title']            = 'Tittel må inneholde mellom 3 og 64 tegn.';
+$_['error_description']      = 'Beskrivelse må inneholde mer enn 3 tegn.';
+$_['error_meta_title']       = 'Meta-tittel må inneholde mellom 3 og 255 tegn.';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'Denne siden kan ikke slettes ettersom den er satt som butikkens kontobetingelser.';
+$_['error_checkout']         = 'Denne siden kan ikke slettes ettersom den er satt som butikkens salgsbetingelser.';
+$_['error_affiliate']        = 'Denne siden kan ikke slettes ettersom den er satt som butikkens affiliatebetingelser.';
+$_['error_return']           = 'Denne siden kan ikke slettes ettersom den er satt som butikkens returbetingelser.';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
