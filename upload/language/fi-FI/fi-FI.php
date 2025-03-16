797e45a51890d4d53606252058ef2e8d6ecf90ab
OSWorX <info@osworx.net>
2025-03-16 09:09:01 +0100

    New translations manufacturer.php (Finnish)

--- upload/language/fi-FI/fi-FI.php
+++ upload/language/fi-FI/fi-FI.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Tiedotteet';
+$_['heading_title']     = 'Valmistajat';
 
 // Text
-$_['text_success']           = 'Olet muokannut tietoa onnistuneesti!';
-$_['text_list']              = 'Tiedotteet';
-$_['text_add']               = 'Lisää tiedote';
-$_['text_edit']              = 'Muokkaa tiedotetta';
-$_['text_default']           = 'Oletus';
+$_['text_success']      = 'Onnistui: Olet muokannut valmistajia!';
+$_['text_list']         = 'Valmistajat';
+$_['text_add']          = 'Lisää valmistaja';
+$_['text_edit']         = 'Muokkaa valmistajaa';
+$_['text_default']      = 'Oletus';
+$_['text_percent']      = 'Prosenttiosuus';
+$_['text_amount']       = 'Kiinteä summa';
 
 // Column
-$_['column_title']           = 'Tiedotteen otsikko';
-$_['column_sort_order']	     = 'Lajittelujärjestys';
-$_['column_action']          = 'Toiminto';
+$_['column_name']       = 'Valmistajan nimi';
+$_['column_sort_order'] = 'Lajittelujärjestys';
+$_['column_action']     = 'Toiminto';
 
 // Entry
-$_['entry_title']            = 'Tiedotteen otsikko';
-$_['entry_description']      = 'Kuvaus';
-$_['entry_store']            = 'Kaupat';
-$_['entry_meta_title'] 	     = 'Metatagien otsikko';
-$_['entry_meta_keyword'] 	 = 'Metatagien avainsanat';
-$_['entry_meta_description'] = 'Metatagin kuvaus';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'Alaosa';
-$_['entry_status']           = 'Tila';
-$_['entry_sort_order']       = 'Lajittelujärjestys';
-$_['entry_layout']           = 'Ohita asettelu';
+$_['entry_name']        = 'Valmistajan nimi';
+$_['entry_store']       = 'Kaupat';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'Kuva';
+$_['entry_sort_order']  = 'Lajittelujärjestys';
+$_['entry_type']        = 'Tyyppi';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'Näytä alatunnisteessa.';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'Varoitus: tarkista lomake virheiden varalta!';
-$_['error_permission']       = 'Varoitus: sinulla ei ole oikeuksia muokata informaatio-sivuja!';
-$_['error_title']            = 'Tiedotteen otsikon tulee olla 3 - 64 merkkiä pitkä!';
-$_['error_description']      = 'Kuvauksen tulee olla vähintään 3 merkkiä pitkä!';
-$_['error_meta_title']       = 'Sivun otsikon  tulee olla 3 - 255 merkkiä pitkä!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'Varoitus: tätä tietosivua ei voi poistaa, koska se on määritetty verkkokaupan rekisteriselosteeksi!';
-$_['error_checkout']         = 'Varoitus: tätä tietosivua ei voi poistaa, koska se on määritetty verkkokaupan toimitusehdoiksi!';
-$_['error_affiliate']        = 'Varoitus: tätä tietosivua ei voi poistaa, koska se on määritetty verkkokaupan kumppanuusehdoiksi!';
-$_['error_return']           = 'Tiedotesivua ei voi poistaa, koska se on määritelty palautusehdoiksi.';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'Varoitus: tarkista lomake virheiden varalta!';
+$_['error_permission']  = 'Varoitus: sinulla ei ole oikeuksia muokata valmistajia!';
+$_['error_name']        = 'Valmistajan nimen tulee olla 3 - 64 merkkiä pitkä!';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'Varoitus: tätä valmistajaa ei voi poistaa, koska %s tuotetta käyttää sitä!';
