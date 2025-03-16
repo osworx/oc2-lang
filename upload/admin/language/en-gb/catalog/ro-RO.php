1bfb728f52f9863e32066ee6d035bcf820490e0a
OSWorX <info@osworx.net>
2025-03-16 11:04:46 +0100

    New translations attribute.php (Romanian)

---
+++ upload/admin/language/en-gb/catalog/ro-RO.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Atribute';
+
+// Text
+$_['text_success']           = 'Succes: Ai modificat atributele!';
+$_['text_list']              = 'Lista de atribute';
+$_['text_add']               = 'Adaugă atribut';
+$_['text_edit']              = 'Modifică atribut';
+
+// Column
+$_['column_name']            = 'Denumire atribut';
+$_['column_attribute_group'] = 'Grup atribut';
+$_['column_sort_order']      = 'Ordinea de sortare';
+$_['column_action']          = 'Acțiune';
+
+// Entry
+$_['entry_name']             = 'Denumire atribut';
+$_['entry_attribute_group']  = 'Grup atribut';
+$_['entry_sort_order']       = 'Ordinea de sortare';
+
+// Error
+$_['error_warning']          = 'Avertisment: Vă rugăm să verificaţi formularul pentru erori!';
+$_['error_permission']       = 'Avertisment: Nu aveţi permisiunile necesare pentru a modifica atributele!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Denumirea atributului trebuie să fie aibă între 3 şi 64 de caractere!';
+$_['error_product']          = 'Avertisment: Acest atribut nu poate fi șters deoarece este asociat la %s produse!';
