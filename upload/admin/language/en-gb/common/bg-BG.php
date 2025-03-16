166e1de628bc7d6a8f93d4b6b2d7493094700e8a
OSWorX <info@osworx.net>
2025-03-16 14:27:36 +0100

    New translations footer.php (Bulgarian)

--- upload/admin/language/en-gb/common/bg-BG.php
+++ upload/admin/language/en-gb/common/bg-BG.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Мениджър на изображения';
-
 // Text
-$_['text_uploaded']    = 'Успех: Вашият файл е качен!';
-$_['text_directory']   = 'Успех: Директория, създадена!';
-$_['text_delete']      = 'Успех: Вашия файл или директория е изтрит!';
-
-// Entry
-$_['entry_search']     = 'Търсене..';
-$_['entry_folder']     = 'Име на папка';
-
-// Error
-$_['error_permission'] = 'Предупреждение: Разрешението е отказано!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Внимание: Файл или директория със същото име вече съществува!';
-$_['error_directory']  = 'Предупреждение: Директория не съществува!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Внимание: Неправилен тип на файла!';
-$_['error_upload']     = 'Предупреждение: Файлът не може да бъде качен поради неизвестна причина!';
-$_['error_delete']     = 'Внимание: Не може да изтриете тази директория!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Версия %s';
