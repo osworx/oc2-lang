1197c88250d77519043d7bca373dbfc0cdb0d913
OSWorX <info@osworx.net>
2025-03-16 14:19:29 +0100

    New translations filter.php (Chinese Traditional)

--- upload/admin/language/en-gb/catalog/zh-TW.php
+++ upload/admin/language/en-gb/catalog/zh-TW.php
@@ -1,35 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = '下載';
+$_['heading_title']     = '篩選條件';
 
 // Text
-$_['text_success']      = '成功：您已經修改下載！';
-$_['text_list']         = '下載清單';
-$_['text_add']          = '新增下載';
-$_['text_edit']         = '編輯下載';
-$_['text_upload']       = '您的檔案已成功上傳！';
+$_['text_success']      = '成功：您已經修改篩選條件！';
+$_['text_list']         = '篩選清單';
+$_['text_add']          = '新增篩選';
+$_['text_edit']         = '編輯篩選';
 
 // Column
-$_['column_name']       = '下載名稱';
-$_['column_date_added'] = '新增日期';
+$_['column_group']      = '篩選群組';
+$_['column_sort_order'] = '排序次序';
 $_['column_action']     = '動作';
 
 // Entry
-$_['entry_name']        = '下載名稱';
-$_['entry_filename']    = '檔案名稱';
-$_['entry_mask']        = '遮罩';
-
-// Help
-$_['help_filename']     = '您可以透過上傳按鈕直接上傳或使用 FTP工具上傳資料到下載目錄並輸入下面的詳細資訊。';
-$_['help_mask']         = '建議把檔案與遮罩設為不同的名稱以防止有人試圖直接連結到您的下載。';
+$_['entry_group']       = '篩選群組名稱';
+$_['entry_name']        = '篩選名稱';
+$_['entry_sort_order']  = '排序次序';
 
 // Error
 $_['error_warning']     = '警告：請仔細檢查表格的錯誤！';
-$_['error_permission']  = '警告：您沒有修改下載的權限！';
-$_['error_name']        = '下載名稱必須介於 3 到 64 個字元之間！';
-$_['error_upload']      = '需要上傳！';
-$_['error_filename']    = '檔案名稱必須介於 3 到 128 個字元之間！';
-$_['error_exists']      = '檔案不存在！';
-$_['error_mask']        = '遮罩必須介於 3 到 128 個字元之間！';
-$_['error_filetype']    = '無效的檔案類型！';
-$_['error_product']     = '警告：無法刪除這個下載，因為目前已指派給 %s 個商品！';
+$_['error_permission']  = '警告：您沒有修改篩選條件的權限！';
+$_['error_group']       = '篩選群組名稱必須介於 1 到 64 個字元之間！';
+$_['error_name']        = '篩選名稱必須介於 1 到 64 個字元之間！';
