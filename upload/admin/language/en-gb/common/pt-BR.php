b7c4df33506e612f9c3411867d77c35b352d9d9d
OSWorX <info@osworx.net>
2025-03-16 14:28:50 +0100

    New translations forgotten.php (Portuguese, Brazilian)

--- upload/admin/language/en-gb/common/pt-BR.php
+++ upload/admin/language/en-gb/common/pt-BR.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Esqueceu-se da sua palavra-passe?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Versão %s';
+$_['text_forgotten']  = 'Senha esquecida';
+$_['text_your_email'] = 'Seu endereço de E-Mail';
+$_['text_email']      = 'Digite o endereço de e-mail associado à sua conta. Clique em enviar para redefinir sua senha.';
+$_['text_success']    = 'Um e-mail com um link de confirmação foi enviado para seu endereço de e-mail do administrador.';
+
+// Entry
+$_['entry_email']     = 'Endereço de email';
+$_['entry_password']  = 'Nova Senha';
+$_['entry_confirm']   = 'Confirmar';
+
+// Error
+$_['error_email']     = 'Atenção: O endereço de email não foi encontrado em nossos registros, por favor, tente novamente!';
+$_['error_password']  = 'Senha deve ter entre 3 e 20 caracteres!';
+$_['error_confirm']   = 'Senha e confirmação de senha não combinam!';
