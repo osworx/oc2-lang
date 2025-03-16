5708e51c96223032fb4f3fe1263b42706d7d9230
OSWorX <info@osworx.net>
2025-03-16 14:23:59 +0100

    New translations review.php (Italian)

--- upload/admin/language/en-gb/catalog/it-IT.php
+++ upload/admin/language/en-gb/catalog/it-IT.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'Profili ricorrenti';
+$_['heading_title']     = 'Recensioni';
 
 // Text
-$_['text_success']          = 'Hai modificato i profili ricorrenti con successo!';
-$_['text_list']             = 'Lista profili ricorrenti';
-$_['text_add']              = 'Aggiungi profilo ricorrente';
-$_['text_edit']             = 'Modifica profilo ricorrente';
-$_['text_day']				= 'Giorno';
-$_['text_week']				= 'Settimana';
-$_['text_semi_month']		= 'Metà mese';
-$_['text_month']			= 'Mese';
-$_['text_year']				= 'Anno';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Gli importi ricorrenti sono calcolati da frequenza e cicli.</p><p>Ad esempio se si utilizza una frequenza di una "settimana" e un ciclo di "2", l\'utente sarà addebitato ogni 2 settimane.</p><p>La durata è il numero di volte che all\'utente sarà richiesto un pagamento, impostare questo a 0 se si desiderano pagamenti ciclici fino a quando il ciclo non verrà eliminato manualmente.</p>';
-$_['text_profile']			= 'Profilo ricorrente';
-$_['text_trial']			= 'Profilo di prova';
-
-// Entry
-$_['entry_name']		    = 'Nome';
-$_['entry_price']			= 'Prezzo';
-$_['entry_duration']		= 'Durata';
-$_['entry_cycle']			= 'Ciclo';
-$_['entry_frequency']		= 'Frequenza';
-$_['entry_trial_price']		= 'Prezzo di prova';
-$_['entry_trial_duration']	= 'Durata della prova';
-$_['entry_trial_status']	= 'Stato della prova';
-$_['entry_trial_cycle']	    = 'Ciclo di prova';
-$_['entry_trial_frequency'] = 'Frequenza di prova';
-$_['entry_status']			= 'Stato';
-$_['entry_sort_order']		= 'Ordinamento';
+$_['text_success']      = 'Hai modificato le recensioni con successo!';
+$_['text_list']         = 'Elenco recensioni';
+$_['text_add']          = 'Aggiungi una recensione';
+$_['text_edit']         = 'Modifica la recensione';
 
 // Column
-$_['column_name']			= 'Nome';
-$_['column_sort_order']	    = 'Ordinamento';
-$_['column_action']         = 'Azione';
+$_['column_product']    = 'Prodotto';
+$_['column_author']     = 'Autore';
+$_['column_rating']     = 'Voto';
+$_['column_status']     = 'Stato';
+$_['column_date_added'] = 'Data inserimento';
+$_['column_action']     = 'Azione';
+
+// Entry
+$_['entry_product']     = 'Prodotto';
+$_['entry_author']      = 'Autore';
+$_['entry_rating']      = 'Voto';
+$_['entry_status']      = 'Stato';
+$_['entry_text']        = 'Testo';
+$_['entry_date_added']  = 'Data inserimento';
+
+// Help
+$_['help_product']      = '(Completamento automatico)';
 
 // Error
-$_['error_warning']         = 'Attenzione: Si prega di controllare attentamente gli errori del modulo!';
-$_['error_permission']		= 'Attenzione: Non hai il permesso per modificare i profili ricorrenti!';
-$_['error_name']			= 'll nome del profilo deve essere maggiore di 3 e minore di 255 caratteri!';
-$_['error_product']			= 'Attenzione: Questo profilo ricorrente non può essere eliminato perché attualmente assegnato a %s prodotti!';
+$_['error_warning']     = 'Attenzione: Si prega di controllare attentamente gli errori del modulo!';
+$_['error_permission']  = 'Attenzione: Non hai il permesso peri modificare le recensioni!';
+$_['error_product']     = 'Prodotto richiesto!';
+$_['error_author']      = 'L\'Autore deve essere compreso tra 3 e 64 caratteri!';
+$_['error_text']        = 'Il Testo della recensione deve avere almeno 1 carattere!';
+$_['error_rating']      = 'Punteggio della recensione richiesto!';
