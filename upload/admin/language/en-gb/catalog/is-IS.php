06a0825f5098b0915301360c7ba44966fc9ab650
OSWorX <info@osworx.net>
2025-03-16 11:06:11 +0100

    New translations attribute_group.php (Icelandic)

--- upload/admin/language/en-gb/catalog/is-IS.php
+++ upload/admin/language/en-gb/catalog/is-IS.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Eigindir';
+$_['heading_title']     = 'Eigindaflokkar';
 
 // Text
-$_['text_success']           = 'Fínt: Þú hefur breytt eigindum.';
-$_['text_list']              = 'Eigindalisti';
-$_['text_add']               = 'Bæta við eigind';
-$_['text_edit']              = 'Breyta eigind';
+$_['text_success']      = 'Fínt: Þú hefur breytt eigindaflokkum!';
+$_['text_list']         = 'Listi Eigindaflokka';
+$_['text_add']          = 'Bæta við eigindaflokki';
+$_['text_edit']         = 'Breyta eigindaflokki';
 
 // Column
-$_['column_name']            = 'Heiti eigindar';
-$_['column_attribute_group'] = 'Eigindaflokkur';
-$_['column_sort_order']      = 'Breytiröð';
-$_['column_action']          = 'Aðgerð';
+$_['column_name']       = 'Heiti eigindaflokks';
+$_['column_sort_order'] = 'Breytiröð';
+$_['column_action']     = 'Aðgerð';
 
 // Entry
-$_['entry_name']             = 'Heiti eigindar';
-$_['entry_attribute_group']  = 'Eigindaflokkur';
-$_['entry_sort_order']       = 'Breytiröð';
+$_['entry_name']        = 'Heiti eigindaflokks';
+$_['entry_sort_order']  = 'Breytiröð';
 
 // Error
-$_['error_warning']          = 'Aðvörun: Farðu vel yfir formið til að forðast villur!';
-$_['error_permission']       = 'Aðvörun: Þú hefur ekki heimild til að breyta eigindum!';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'Eigindarheiti verður að vera 3 til 64 stafir!';
-$_['error_product']          = 'Aðvörun: Þessari eigind verður ekki eytt því hún er tengd %s vörum!';
+$_['error_warning']     = 'Aðvörun: Farðu vel yfir formið til að forðast villur!';
+$_['error_permission']  = 'Aðvörun: Þú hefur ekki heimild til að breyta eigindaflokki!';
+$_['error_name']        = 'Heiti eigindaflokks verður að vera 3 til 64 stafir!';
+$_['error_attribute']   = 'Aðvörun: Þessum eigindaflokki verður ekki breytt því hann er tengdur %s eigindum!';
+$_['error_product']     = 'Aðvörun: Þessum eigindaflokki verður ekki eytt því hann er tengdur %s vörum!';
