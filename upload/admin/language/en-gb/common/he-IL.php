89614ced68e157eed801f6d74aa8f8778e842d56
OSWorX <info@osworx.net>
2025-03-16 11:19:58 +0100

    New translations login.php (Hebrew)

--- upload/admin/language/en-gb/common/he-IL.php
+++ upload/admin/language/en-gb/common/he-IL.php
@@ -1,23 +1,19 @@
 <?php
-// Heading
-$_['heading_title']          = 'OpenCart';
+// header
+$_['heading_title']  = 'ניהול';
 
 // Text
-$_['text_order']             = 'הזמנות';
-$_['text_processing_status'] = 'Processing';
-$_['text_complete_status']   = 'הושלם';
-$_['text_customer']          = 'לקוחות';
-$_['text_online']            = 'לקוחות מקוונים';
-$_['text_approval']          = 'מחכה לאישור';
-$_['text_product']           = 'מוצרים';
-$_['text_stock']             = 'אזל מהמלאי';
-$_['text_review']            = 'ביקורות';
-$_['text_return']            = 'החזרות';
-$_['text_affiliate']         = 'שותפים';
-$_['text_store']             = 'חנויות';
-$_['text_front']             = 'חזית החנות';
-$_['text_help']              = 'עזרה';
-$_['text_homepage']          = 'OpenCart Homepage';
-$_['text_support']           = 'פורום תמיכה';
-$_['text_documentation']     = 'תיעוד';
-$_['text_logout']            = 'התנתק';
+$_['text_heading']   = 'ניהול';
+$_['text_login']     = 'הזן את פרטי ההתחברות שלך.';
+$_['text_forgotten'] = 'סיסמה נשכחה';
+
+// Entry
+$_['entry_username'] = 'שם משתמש';
+$_['entry_password'] = 'סיסמא';
+
+// Button
+$_['button_login']   = 'התחברות';
+
+// Error
+$_['error_login']    = 'שם משתמש או הסיסמא לא תואמים.';
+$_['error_token']    = 'פעולה לא תקינה. אנא התחבר שוב.';
