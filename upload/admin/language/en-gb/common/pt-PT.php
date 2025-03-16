ac88a1f30c443f977e8d09c894397cf947186529
OSWorX <info@osworx.net>
2025-03-16 11:16:48 +0100

    New translations filemanager.php (Portuguese)

--- upload/admin/language/en-gb/common/pt-PT.php
+++ upload/admin/language/en-gb/common/pt-PT.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Painel Administração';
+$_['heading_title']    = 'Gestor de Imagens';
+
+// Text
+$_['text_uploaded']    = 'Sucesso: Seu Ficheiro foi carregado!';
+$_['text_directory']   = 'Sucesso: Diretório criado!';
+$_['text_delete']      = 'Sucesso: O seu ficheiro ou directório foi eliminado!';
+
+// Entry
+$_['entry_search']     = 'Procurar..';
+$_['entry_folder']     = 'Nome da Pasta';
 
 // Error
-$_['error_install'] = 'Atenção: A pasta de instalação "Install" ainda não foi Eliminada. Esta deve ser eliminada por motivos de segurança!';
+$_['error_permission'] = 'Aviso: Não tem Permissões!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Aviso: Já existe um Ficheiro ou Diretório com o mesmo nome!';
+$_['error_directory']  = 'Atenção: O Diretório não existe!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Aviso: Tipo de Ficheiro incorreto!';
+$_['error_upload']     = 'Aviso: O Ficheiro não pode ser carregado por um motivo desconhecido!';
+$_['error_delete']     = 'Aviso: Não pode apagar este Directório!';
