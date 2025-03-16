329a3a98a1ba04ae680c9b624636af271a85def7
OSWorX <info@osworx.net>
2025-03-16 11:17:31 +0100

    New translations footer.php (Finnish)

--- upload/admin/language/en-gb/common/fi-FI.php
+++ upload/admin/language/en-gb/common/fi-FI.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Kuvienhallinta';
-
 // Text
-$_['text_uploaded']    = 'Tiedoston lähettäminen onnistui!';
-$_['text_directory']   = 'Hakemiston luominen onnistui!';
-$_['text_delete']      = 'Tiedoston tai hakemiston poisto onnistui!';
-
-// Entry
-$_['entry_search']     = 'Hae..';
-$_['entry_folder']     = 'Kansion nimi';
-
-// Error
-$_['error_permission'] = 'Varoitus: Ei käyttöikeuksia!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Varoitus:  samanniminen tiedosto tai hakemisto on jo olemassa!';
-$_['error_directory']  = 'Varoitus: Hakemistoa ei ole olemassa!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Varoitus: Virheellinen tiedostomuoto!';
-$_['error_upload']     = 'Varoitus: Tiedoston lataaminen ei onnistunut tuntemattomasta syystä!';
-$_['error_delete']     = 'Varoitus: Et voi poistaa tätä hakemistoa!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Versio %s';
