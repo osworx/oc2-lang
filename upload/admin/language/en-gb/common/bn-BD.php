60d0afa7901b54bb45eae1e5965d39f0fc6da21a
OSWorX <info@osworx.net>
2025-03-16 14:27:20 +0100

    New translations filemanager.php (Bengali)

--- upload/admin/language/en-gb/common/bn-BD.php
+++ upload/admin/language/en-gb/common/bn-BD.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'ড্যাশবোর্ড';
+$_['heading_title']    = 'ইমেজ ম্যানেজার';
+
+// Text
+$_['text_uploaded']    = 'Success: Your file has been uploaded!';
+$_['text_directory']   = 'সাফল্য: নির্দেশিকা তৈরি করা হয়েছে!';
+$_['text_delete']      = 'সাফল্য: আপনার ফাইল অথবা প্রক্রিয়াস্থান মুছে ফেলা হয়েছে!';
+
+// Entry
+$_['entry_search']     = 'অনুসন্ধান..';
+$_['entry_folder']     = 'Folder Name';
 
 // Error
-$_['error_install'] = 'সতর্কতা: ইন্সটল ফোল্ডার এখনো বিদ্যমান এবং নিরাপত্তার খাতিরে এটি মুছে ফেলা উচিত!';
+$_['error_permission'] = 'Warning: Permission Denied!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Warning: A file or directory with the same name already exists!';
+$_['error_directory']  = 'Warning: Directory does not exist!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Warning: Incorrect file type!';
+$_['error_upload']     = 'Warning: File could not be uploaded for an unknown reason!';
+$_['error_delete']     = 'Warning: You can not delete this directory!';
