3c48c70d6ff83f06eb3770aa84decd10edbbb1d2
OSWorX <info@osworx.net>
2025-03-16 11:08:11 +0100

    New translations download.php (French, Canada)

--- upload/admin/language/en-gb/catalog/fr-CA.php
+++ upload/admin/language/en-gb/catalog/fr-CA.php
@@ -1,46 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Catégories';
+$_['heading_title']     = 'Téléchargements';
 
 // Text
-$_['text_success']           = 'Succès: Vous avez modifié les catégories !';
-$_['text_list']              = 'Liste des catégories';
-$_['text_add']               = 'Ajouter une catégorie';
-$_['text_edit']              = 'Modifier la catégorie';
-$_['text_default']           = 'Par défaut';
+$_['text_success']      = 'Succès : Vous avez modifié les téléchargements !';
+$_['text_list']         = 'Liste de téléchargements';
+$_['text_add']          = 'Ajouter téléchargement';
+$_['text_edit']         = 'Modifier téléchargement';
+$_['text_upload']       = 'Envoi du fichier réussi!';
 
 // Column
-$_['column_name']            = 'Nom de la catégorie';
-$_['column_sort_order']      = 'Ordre de présentation';
-$_['column_action']          = 'Action';
+$_['column_name']       = 'Nom du téléchargement';
+$_['column_date_added'] = 'Date d\'ajout';
+$_['column_action']     = 'Action';
 
 // Entry
-$_['entry_name']             = 'Nom de la catégorie';
-$_['entry_description']      = 'Description';
-$_['entry_meta_title'] 	     = 'Titre de métaélément';
-$_['entry_meta_keyword']     = 'Mots-clef de métaélément';
-$_['entry_meta_description'] = 'Description de métaélément';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = 'Parent';
-$_['entry_filter']           = 'Filtres';
-$_['entry_store']            = 'Magasins';
-$_['entry_image']            = 'Image';
-$_['entry_top']              = 'Haut';
-$_['entry_column']           = 'Colonnes';
-$_['entry_sort_order']       = 'Ordre de présentation';
-$_['entry_status']           = 'État';
-$_['entry_layout']           = 'Outrepasser la mise en page';
+$_['entry_name']        = 'Nom du téléchargement';
+$_['entry_filename']    = 'Nom du fichier';
+$_['entry_mask']        = 'Masquer';
 
 // Help
-$_['help_filter']            = '(Autocomplétion)';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = 'Afficher dans la barre de menu du haut. Applicable seulement pour les catégories de premier niveau.';
-$_['help_column']            = 'Nombre de colonnes à utiliser pour les 3 catégories du bas. Applicable seulement aux catégories de premier niveau.';
+$_['help_filename']     = 'Vous pouvez envoyer via le bouton d\'envoi ou utiliser le protocol FTP pour envoyer au répertoire de téléchargement et entrer les détails ci-dessous.';
+$_['help_mask']         = 'Il est recommandé de différencier le nom de fichier et sa masque pour empêcher la création de liens directs vers vos téléchargements.';
 
 // Error
-$_['error_warning']          = 'Attention: Veuillez vérifier que le formulaire ne contient pas d\'erreur !';
-$_['error_permission']       = 'Attention: vous n’êtes pas autorisé à modifier les catégories !';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = 'Le titre méta doit contenir plus de 3 et moins de 255 caractères !';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = 'Attention: Veuillez vérifier que le formulaire ne contient pas d\'erreur !';
+$_['error_permission']  = 'Avertissement: vous n\'êtes pas autorisé à modifier les téléchargements!';
+$_['error_name']        = 'Le nom du téléchargement doit compter entre 3 et 64 caractères!';
+$_['error_upload']      = 'Envoi nécessaire!';
+$_['error_filename']    = 'Le nom de fichier doit compter entre 3 et 128 caractères!';
+$_['error_exists']      = 'Fichier non existant!';
+$_['error_mask']        = 'Le masque doit compter entre 3 et 128 caractères!';
+$_['error_filetype']    = 'Type de fichier non valide!';
+$_['error_product']     = 'Avertissement: il est impossible d\'effacer ce téléchargement puisqu\'il est alloué à %s !';
