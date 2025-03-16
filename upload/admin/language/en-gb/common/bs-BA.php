ae66d846f27cc6cb91397a01965282ce01f79ba0
OSWorX <info@osworx.net>
2025-03-16 14:29:02 +0100

    New translations forgotten.php (Bosnian)

--- upload/admin/language/en-gb/common/bs-BA.php
+++ upload/admin/language/en-gb/common/bs-BA.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Zaboravili ste šifru?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Verzija %s';
+$_['text_forgotten']  = 'Zaboravljena šifra';
+$_['text_your_email'] = 'Vaša e-mail adresa';
+$_['text_email']      = 'Unesite e-mail adresu povezanu sa vašim računom. Pritisnite na pošalji da bi ste dobili link za resetiranje šifre na vaš e-mail.';
+$_['text_success']    = 'E-mail sa linkom za potrvrdu je poslan na vašu administratorsku e-mail adresu.';
+
+// Entry
+$_['entry_email']     = 'E-mail adresa';
+$_['entry_password']  = 'Nova Šifra';
+$_['entry_confirm']   = 'Potvrdi';
+
+// Error
+$_['error_email']     = 'Upozorenje: E-mail adresa nije pronađena u našoj bazi podataka, molimo pokušajte ponovo!';
+$_['error_password']  = 'Password must be between 4 and 20 characters!';
+$_['error_confirm']   = 'Šifra i potvrda šifre se ne slažu!';
