64b9fbf9c755686ad855ced6de4483ea61c42427
OSWorX <info@osworx.net>
2025-03-16 14:17:50 +0100

    New translations category.php (Ukrainian)

--- upload/admin/language/en-gb/catalog/uk-UA.php
+++ upload/admin/language/en-gb/catalog/uk-UA.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Групи атрибутів';
+$_['heading_title']          = 'Категорії';
 
 // Text
-$_['text_success']      = 'Вітаємо: Ви успішно змінили групу атрибутів!';
-$_['text_list']         = 'Список груп атрибутів';
-$_['text_add']          = 'Додати групу атрибутів';
-$_['text_edit']         = 'Редагувати групу атрибутів';
+$_['text_success']           = 'Вітаємо: Ви успішно змінили категорію!';
+$_['text_list']              = 'Список категорій';
+$_['text_add']               = 'Додати категорію';
+$_['text_edit']              = 'Редагувати категорію';
+$_['text_default']           = 'За замовчуванням';
 
 // Column
-$_['column_name']       = 'Назва групи атрибутів';
-$_['column_sort_order'] = 'Порядок сортування';
-$_['column_action']     = 'Дія';
+$_['column_name']            = 'Назва категорії';
+$_['column_sort_order']      = 'Порядок сортування';
+$_['column_action']          = 'Дія';
 
 // Entry
-$_['entry_name']        = 'Назва групи атрибутів';
-$_['entry_sort_order']  = 'Порядок сортування';
+$_['entry_name']             = 'Назва категорії';
+$_['entry_description']      = 'Опис';
+$_['entry_meta_title'] 	     = 'Заголовок сторінки';
+$_['entry_meta_keyword']     = 'Ключові слова';
+$_['entry_meta_description'] = 'Мета-тег опис';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Батьківська категорія';
+$_['entry_filter']           = 'Фільтри';
+$_['entry_store']            = 'Магазини';
+$_['entry_image']            = 'Зображення';
+$_['entry_top']              = 'Згори:<br/><span class="help">Розміщення у верхньому полі меню. Працює тільки з батьківськими категоріями.</span>';
+$_['entry_column']           = 'Стовпці:<br/><span class="help">Кількість стовпців для трьох категорій, що розташовані знизу. Працює тільки з батьківськими категоріями.</span>';
+$_['entry_sort_order']       = 'Порядок сортування';
+$_['entry_status']           = 'Стан';
+$_['entry_layout']           = 'Вигляд інтерфейсу';
+
+// Help
+$_['help_filter']            = '(Автозаповнення)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Показати в верхньому меню. Працює тільки для батьківських категорій.';
+$_['help_column']            = 'Число стовпців, які використовуються для нижньої 3 категорії. Працює тільки з батьківськими категоріями.';
 
 // Error
-$_['error_warning']     = 'Будь ласка, перевірте форму на наявність помилок!';
-$_['error_permission']  = 'Увага: У Вас недостатньо повноважень для редагування атрибутів!';
-$_['error_name']        = 'Назва групи атрибутів повинна містити від 3 до 64 символів!';
-$_['error_attribute']   = 'Неможливо видалити групу атрибутів, оскільки вона використовується в атрибутах %s!';
-$_['error_product']     = 'Неможливо видалити групу атрибутів, оскільки вона використовується в товарах %s!';
+$_['error_warning']          = 'Будь ласка, перевірте форму на наявність помилок!';
+$_['error_permission']       = 'Увага: У Вас недостатньо повноважень для редагування категорії!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Заголовок сторінки повинен містити від 3 до 255 символів!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
