ce3ddce778fd5930a7e03d07aa67acb25bd47689
OSWorX <info@osworx.net>
2025-03-16 11:12:57 +0100

    New translations recurring.php (Lithuanian)

--- upload/admin/language/en-gb/catalog/lt-LT.php
+++ upload/admin/language/en-gb/catalog/lt-LT.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'Prekės';
+$_['heading_title']			= 'Pasikartojantys profiliai';
 
 // Text
-$_['text_success']           = 'Jūs sėkmingai modifikavote prekes!';
-$_['text_list']              = 'Prekių sąrašas';
-$_['text_add']               = 'Pridėti prekę';
-$_['text_edit']              = 'Redaguoti prekę';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'Numatytasis';
-$_['text_option']            = 'Pasirinkimas';
-$_['text_option_value']      = 'Pasirinkimo reikšmė';
-$_['text_percent']           = 'Procentas';
-$_['text_amount']            = 'Nustatyta suma';
-
-// Column
-$_['column_name']            = 'Produkto pavadinimas';
-$_['column_model']           = 'Modelis';
-$_['column_image']           = 'Paveikslėlis';
-$_['column_price']           = 'Kaina';
-$_['column_quantity']        = 'Kiekis';
-$_['column_status']          = 'Statusas';
-$_['column_action']          = 'Veiksmas';
+$_['text_success']          = 'Atlikta: pasikartojantys profiliai sėkmingai pakeisti!';
+$_['text_list']             = 'Pasikartojančių profilių sąrašas';
+$_['text_add']              = 'Pridėti pasikartojantį profilį';
+$_['text_edit']             = 'Redaguoti pasikartojantį profilį';
+$_['text_day']				= 'Diena';
+$_['text_week']				= 'Savaitė';
+$_['text_semi_month']		= 'Pusę mėnesio';
+$_['text_month']			= 'Mėnuo';
+$_['text_year']				= 'Metai';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Pasikartojančios sumos yra suskaičiuojamos pagal dažnį ir ciklus.</p><p>Pavyzdžiui, jei naudojamas dažnumas "savaitė" ir ciklas "2", tuomet vartotojas bus apmokęstintas kas dvi savaites.</p><p>Trukmė yra skaičius kartų, kiek vartotojas mokės. Nustatykite 0, jei norite tęsti mokėjimus iki kols jie bus nutraukti.</p>';
+$_['text_profile']			= 'Periodinis profilis';
+$_['text_trial']			= 'Bandomasis profilis';
 
 // Entry
-$_['entry_name']             = 'Produkto pavadinimas';
-$_['entry_description']      = 'Aprašymas';
-$_['entry_meta_title'] 	     = 'Meta Tago pavadinimas';
-$_['entry_meta_keyword'] 	 = 'Meta Tag Raktažodžiai';
-$_['entry_meta_description'] = 'Meta Tag Aprašymas';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Modelis';
-$_['entry_sku']              = 'SKU';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'Vietovė';
-$_['entry_shipping']         = 'Reikalingas siuntimas';
-$_['entry_manufacturer']     = 'Gamintojas';
-$_['entry_store']            = 'Parduotuvės';
-$_['entry_date_available']   = 'Pasirodymo data';
-$_['entry_quantity']         = 'Kiekis';
-$_['entry_minimum']          = 'Mažiausias kiekis';
-$_['entry_stock_status']     = 'Nėra sandėlyje';
-$_['entry_price']            = 'Kaina';
-$_['entry_tax_class']        = 'Mokesčių klasė';
-$_['entry_points']           = 'Taškai';
-$_['entry_option_points']    = 'Taškai';
-$_['entry_subtract']         = 'Sumažinti iš sandėlio';
-$_['entry_weight_class']     = 'Svorio klasė';
-$_['entry_weight']           = 'Svoris';
-$_['entry_dimension']        = 'Išmatavimai (I x P x A)';
-$_['entry_length_class']     = 'Ilgio klasė';
-$_['entry_length']           = 'Ilgis';
-$_['entry_width']            = 'Plotis';
-$_['entry_height']           = 'Aukštis';
-$_['entry_image']            = 'Paveikslėlis';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Pirkėjų grupė';
-$_['entry_date_start']       = 'Pradžia';
-$_['entry_date_end']         = 'Pabaiga';
-$_['entry_priority']         = 'Prioritetas';
-$_['entry_attribute']        = 'Atributai';
-$_['entry_attribute_group']  = 'Savybės grupė';
-$_['entry_text']             = 'Tekstas';
-$_['entry_option']           = 'Pasirinkimas';
-$_['entry_option_value']     = 'Pasirinkimo reikšmė';
-$_['entry_required']         = 'Privalomas';
-$_['entry_status']           = 'Statusas';
-$_['entry_sort_order']       = 'Rikiavimo tvarka';
-$_['entry_category']         = 'Kategorijos';
-$_['entry_filter']           = 'Filtrai';
-$_['entry_download']         = 'Atsisiuntimai';
-$_['entry_related']          = 'Susijusios prekės';
-$_['entry_tag']          	 = 'Prekės žymos';
-$_['entry_reward']           = 'Premijos taškai';
-$_['entry_layout']           = 'Šablono valdymas';
-$_['entry_recurring']        = 'Periodinis profilis';
+$_['entry_name']		    = 'Vardas';
+$_['entry_price']			= 'Kaina';
+$_['entry_duration']		= 'Trukmė';
+$_['entry_cycle']			= 'Ciklas';
+$_['entry_frequency']		= 'Dažnis';
+$_['entry_trial_price']		= 'Bandymo kaina';
+$_['entry_trial_duration']	= 'Bandymo trukmė';
+$_['entry_trial_status']	= 'Bandymo būsena';
+$_['entry_trial_cycle']	    = 'Bandymo ciklas';
+$_['entry_trial_frequency'] = 'Bandymo dažnis';
+$_['entry_status']			= 'Statusas';
+$_['entry_sort_order']		= 'Rikiavimo tvarka';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'Sandėlio likutis';
-$_['help_upc']               = 'Universalus produkto kodas';
-$_['help_ean']               = 'Europietiškas numeris';
-$_['help_jan']               = 'Japoniškas numeris';
-$_['help_isbn']              = 'Tarptautinis standartinis užsakymo numeris';
-$_['help_mpn']               = 'Gamintojo Part numeris';
-$_['help_manufacturer']      = '(Užbaigti automatiškai)';
-$_['help_minimum']           = 'Priversti užsakyti minimalų kiekį';
-$_['help_stock_status']      = 'Rodomas pranešimas, kai produkto nebėra sandėlyje';
-$_['help_points']            = 'Skaičius taškų, kurie yra reikalingi nusipirkti šį pirkinį. Jei nenorite leisti nusipirkti produkto taškais, palikite 0.';
-$_['help_category']          = '(Užbaigti automatiškai)';
-$_['help_filter']            = '(Užbaigti automatiškai)';
-$_['help_download']          = '(Užbaigti automatiškai)';
-$_['help_related']           = '(Užbaigti automatiškai)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'Vardas';
+$_['column_sort_order']	    = 'Rikiavimo tvarka';
+$_['column_action']         = 'Veiksmas';
 
 // Error
-$_['error_warning']          = 'Įspėjimas: Patikrinkite klaidas formoje!';
-$_['error_permission']       = 'Jūs neturite teisių modifikuoti produktus!';
-$_['error_name']             = 'Produkto pavadinimas turi būti nuo 3 iki 255 simbolių!';
-$_['error_meta_title']       = 'Meta pavadinimas turi būti ilgesnis kaip 3 ir trumpesnis kaip 255 simboliai!';
-$_['error_model']            = 'Produkto modelis turi būti nuo 3 iki 64 simbolių!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Įspėjimas: Patikrinkite klaidas formoje!';
+$_['error_permission']		= 'Įspėjimas: neturite teisių keisti pasikartojančius profilius!';
+$_['error_name']			= 'Profilio pavadinimas turi būti didesnis nei 3 ir mažesnis nei 255 simboliai!';
+$_['error_product']			= 'Įspėjimas: šis pasikartojantis profilis negali būti ištrintas, kadangi yra priskirtas prie %s produktų!';
