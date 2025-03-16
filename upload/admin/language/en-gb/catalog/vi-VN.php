81ec522420a77da1cbc7cfaf5f1a2261e8a9f050
OSWorX <info@osworx.net>
2025-03-16 14:21:05 +0100

    New translations manufacturer.php (Vietnamese)

--- upload/admin/language/en-gb/catalog/vi-VN.php
+++ upload/admin/language/en-gb/catalog/vi-VN.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Thông tin';
+$_['heading_title']     = 'Các nhà sản xuất';
 
 // Text
-$_['text_success']           = 'Kết quả: Bạn đã sửa xong thông tin!';
-$_['text_list']              = 'Danh sách thông tin';
-$_['text_add']               = 'Thêm thông tin';
-$_['text_edit']              = 'Sửa thông tin';
-$_['text_default']           = 'Mặc định';
+$_['text_success']      = 'Kết quả: Bạn đã sửa xong các nhà sản xuất!';
+$_['text_list']         = 'Danh sách nhà sản xuất';
+$_['text_add']          = 'Thêm nhà sản xuất';
+$_['text_edit']         = 'Sửa nhà sản xuất';
+$_['text_default']      = 'Mặc định';
+$_['text_percent']      = 'Phần trăm';
+$_['text_amount']       = 'Số tiền cố định';
 
 // Column
-$_['column_title']           = 'Tên loại thông tin';
-$_['column_sort_order']	     = 'Thứ tự sắp xếp';
-$_['column_action']          = 'Tác vụ';
+$_['column_name']       = 'Tên nhà sản xuất';
+$_['column_sort_order'] = 'Thứ tự sắp xếp';
+$_['column_action']     = 'Tác vụ';
 
 // Entry
-$_['entry_title']            = 'Tên loại thông tin';
-$_['entry_description']      = 'Mô tả';
-$_['entry_store']            = 'Gian hàng';
-$_['entry_meta_title'] 	     = 'Tên thẻ Meta';
-$_['entry_meta_keyword'] 	 = 'Từ khóa cho thẻ Meta';
-$_['entry_meta_description'] = 'Mô tả cho thẻ Meta';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'Dưới cùng';
-$_['entry_status']           = 'Trạng thái';
-$_['entry_sort_order']       = 'Thứ tự sắp xếp';
-$_['entry_layout']           = 'Ghi đè layout';
+$_['entry_name']        = 'Tên nhà sản xuất';
+$_['entry_store']       = 'Gian hàng';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'Hình ảnh';
+$_['entry_sort_order']  = 'Thứ tự sắp xếp';
+$_['entry_type']        = 'Kiểu';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'Hiện dưới cuối trang.';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'Chú ý: Vui lòng kiểm tra lỗi cẩn thận trong biểu mẫu!';
-$_['error_permission']       = 'Chú ý: Bạn không có quyền để sửa đổi thông tin!';
-$_['error_title']            = 'Tên của loại thông tin phải từ 3 tới 64 ký tự!';
-$_['error_description']      = 'Mô tả phải nhiều hơn 3 ký tự!';
-$_['error_meta_title']       = 'Tên thẻ Meta phải nhiều hơn 3 và ít hơn 255 ký tự!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'Chú ý: Trang thông tin này không thể bị xóa vì nó đang được gán cho các mục tài khoản lưu trữ!';
-$_['error_checkout']         = 'Chú ý: Trang thông tin này không thể bị xóa vì nó đang được gán cho các mục kiểm tra lưu trữ!';
-$_['error_affiliate']        = 'Chú ý: Trang thông tin này không thể bị xóa vì nó đang được gán cho các mục nhánh lưu trữ!';
-$_['error_return']           = 'Chú ý: Trang thông tin này không thể bị xóa vì nó đang được gán cho các mục thống kê lưu trữ!';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'Chú ý: Vui lòng kiểm tra lỗi cẩn thận trong biểu mẫu!';
+$_['error_permission']  = 'Chú ý: Bạn không có quyền sửa nhà sản xuất!';
+$_['error_name']        = 'Tên nhà sản xuất phải từ 2 đến 64 ký tự!';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'Chú ý: Nhà sản xuất này không thể bị xoá vì nó đang được gán cho các sản phẩm %s !';
