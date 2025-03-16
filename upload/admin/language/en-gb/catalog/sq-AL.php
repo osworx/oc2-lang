cf803d1f1756e813e4bc09ea04ea2d98b90a8e66
OSWorX <info@osworx.net>
2025-03-16 14:20:59 +0100

    New translations manufacturer.php (Albanian)

--- upload/admin/language/en-gb/catalog/sq-AL.php
+++ upload/admin/language/en-gb/catalog/sq-AL.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Information';
+$_['heading_title']     = 'Manufacturers';
 
 // Text
-$_['text_success']           = 'Success: You have modified information!';
-$_['text_list']              = 'Information List';
-$_['text_add']               = 'Add Information';
-$_['text_edit']              = 'Edit Information';
-$_['text_default']           = 'Parapërzgjedhur';
+$_['text_success']      = 'Success: You have modified manufacturers!';
+$_['text_list']         = 'Manufacturer List';
+$_['text_add']          = 'Add Manufacturer';
+$_['text_edit']         = 'Edit Manufacturer';
+$_['text_default']      = 'Parapërzgjedhur';
+$_['text_percent']      = 'Percentage';
+$_['text_amount']       = 'Fixed Amount';
 
 // Column
-$_['column_title']           = 'Information Title';
-$_['column_sort_order']	     = 'Radhitja';
-$_['column_action']          = 'Veprimi';
+$_['column_name']       = 'Manufacturer Name';
+$_['column_sort_order'] = 'Radhitja';
+$_['column_action']     = 'Veprimi';
 
 // Entry
-$_['entry_title']            = 'Information Title';
-$_['entry_description']      = 'Përshkrimi';
-$_['entry_store']            = 'Dyqanet';
-$_['entry_meta_title'] 	     = 'Meta titulli';
-$_['entry_meta_keyword'] 	 = 'Fjalë kyçe meta.';
-$_['entry_meta_description'] = 'Përshkrimi Meta Tag';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'Bottom';
-$_['entry_status']           = 'Statusi';
-$_['entry_sort_order']       = 'Radhitja';
-$_['entry_layout']           = 'Faqosja e shtypit';
+$_['entry_name']        = 'Manufacturer Name';
+$_['entry_store']       = 'Dyqanet';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'Fotografia';
+$_['entry_sort_order']  = 'Radhitja';
+$_['entry_type']        = 'Type';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'Display in the bottom footer.';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'Vërejtje: Ju lutem kontrolloni formën me kujdes për ndonjë gabim!';
-$_['error_permission']       = 'Warning: You do not have permission to modify information!';
-$_['error_title']            = 'Information Title must be between 3 and 64 characters!';
-$_['error_description']      = 'Description must be more than 3 characters!';
-$_['error_meta_title']       = 'Produkti Model duhet të jetë më i madh se 3 dhe më pak se 255 karaktere!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'Warning: This information page cannot be deleted as it is currently assigned as the store account terms!';
-$_['error_checkout']         = 'Warning: This information page cannot be deleted as it is currently assigned as the store checkout terms!';
-$_['error_affiliate']        = 'Warning: This information page cannot be deleted as it is currently assigned as the store affiliate terms!';
-$_['error_return']           = 'Warning: This information page cannot be deleted as it is currently assigned as the store return terms!';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'Vërejtje: Ju lutem kontrolloni formën me kujdes për ndonjë gabim!';
+$_['error_permission']  = 'Warning: You do not have permission to modify manufacturers!';
+$_['error_name']        = 'Manufacturer Name must be between 2 and 64 characters!';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'Warning: This manufacturer cannot be deleted as it is currently assigned to %s products!';
