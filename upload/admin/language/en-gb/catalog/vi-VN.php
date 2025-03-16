8d70ac155a28df06f63143974b6b4ec5ea25d864
OSWorX <info@osworx.net>
2025-03-16 11:07:57 +0100

    New translations download.php (Vietnamese)

--- upload/admin/language/en-gb/catalog/vi-VN.php
+++ upload/admin/language/en-gb/catalog/vi-VN.php
@@ -1,46 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Các hạng mục';
+$_['heading_title']     = 'Các mục tải về';
 
 // Text
-$_['text_success']           = 'Kết quả: Bạn đã sửa đổi xong các hạng mục!';
-$_['text_list']              = 'Danh sách hạng mục';
-$_['text_add']               = 'Thêm hạng mục';
-$_['text_edit']              = 'Chỉnh sửa hạng mục';
-$_['text_default']           = 'Mặc định';
+$_['text_success']      = 'Kết quả: Bạn đã thay đổi các mục tải về!';
+$_['text_list']         = 'Danh sách tải về';
+$_['text_add']          = 'Thêm 1 mục tải về';
+$_['text_edit']         = 'Sửa mục tải về';
+$_['text_upload']       = 'File của bạn tải lên thành công!';
 
 // Column
-$_['column_name']            = 'Tên hạng mục';
-$_['column_sort_order']      = 'Thứ tự sắp xếp';
-$_['column_action']          = 'Tác vụ';
+$_['column_name']       = 'Tên mục tải về';
+$_['column_date_added'] = 'Ngày thêm vào';
+$_['column_action']     = 'Tác vụ';
 
 // Entry
-$_['entry_name']             = 'Tên hạng mục';
-$_['entry_description']      = 'Mô tả';
-$_['entry_meta_title'] 	     = 'Tên thẻ Meta';
-$_['entry_meta_keyword']     = 'Từ khóa cho thẻ Meta';
-$_['entry_meta_description'] = 'Mô tả cho thẻ Meta';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = 'Mục cha';
-$_['entry_filter']           = 'Bộ lọc';
-$_['entry_store']            = 'Gian hàng';
-$_['entry_image']            = 'Hình ảnh';
-$_['entry_top']              = 'Đầu trang';
-$_['entry_column']           = 'Cột';
-$_['entry_sort_order']       = 'Thứ tự sắp xếp';
-$_['entry_status']           = 'Trạng thái';
-$_['entry_layout']           = 'Ghi đè layout';
+$_['entry_name']        = 'Tên mục tải về';
+$_['entry_filename']    = 'Tên file';
+$_['entry_mask']        = 'Tên giả';
 
 // Help
-$_['help_filter']            = '(Tự động gợi ý)';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = 'Hiện trong thanh trình đơn trên cùng. Chỉ làm việc đối với các hạng mục gốc.';
-$_['help_column']            = 'Số cột dùng cho 3 hạng mục cuối. Chỉ làm việc đối với các hạng mục gốc.';
+$_['help_filename']     = 'Bạn có thể tải file lên theo nút "Tải lên" hoặc sử dụng FTP để tải tới "Thư mục tải về"  và nhập các chi tiết dưới đây.';
+$_['help_mask']         = 'Tên file và tên giả của file nên khác nhau để ngăn người khác cố gắng liên kết trực tiếp tới các file tải về của bạn.';
 
 // Error
-$_['error_warning']          = 'Chú ý: Vui lòng kiểm tra lỗi cẩn thận trong biểu mẫu!';
-$_['error_permission']       = 'Chú ý: Bạn không có quyền sửa đổi các hạng mục!';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = 'Tên thẻ Meta phải nhiều hơn 3 và ít hơn 255 ký tự!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = 'Chú ý: Vui lòng kiểm tra lỗi cẩn thận trong biểu mẫu!';
+$_['error_permission']  = 'Chú ý: Bạn không có quyền sửa các mục tải về!';
+$_['error_name']        = 'Tên tải về phải từ 3 tới 64 ký tự!';
+$_['error_upload']      = 'Đã yêu cầu tải lên!';
+$_['error_filename']    = 'Tên file phải từ 3 tới 128 ký tự!';
+$_['error_exists']      = 'File không tồn tại!';
+$_['error_mask']        = 'Tên giả của file phải từ 3 đến 128 ký tự!';
+$_['error_filetype']    = 'Loại file không hợp lệ!';
+$_['error_product']     = 'Chú ý: Mục tải về này không thể bị xoá vì nó hiện đang được gán cho các sản phẩm %s !';
