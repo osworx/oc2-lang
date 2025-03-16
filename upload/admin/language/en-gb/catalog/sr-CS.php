788ff30d272a1b5547592e96904294573c474af1
OSWorX <info@osworx.net>
2025-03-16 11:06:25 +0100

    New translations attribute_group.php (Serbian (Latin))

--- upload/admin/language/en-gb/catalog/sr-CS.php
+++ upload/admin/language/en-gb/catalog/sr-CS.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Atributi';
+$_['heading_title']     = 'Grupa Atributa';
 
 // Text
-$_['text_success']           = 'Uspešno ste promenili atribute!';
-$_['text_list']              = 'Lista atributa';
-$_['text_add']               = 'Dodaj atribut';
-$_['text_edit']              = 'Izmeni atribut';
+$_['text_success']      = 'Uspeh: Vi ste uspešno izmenili grupu atributa!';
+$_['text_list']         = 'Lista Grupe Atributa';
+$_['text_add']          = 'Dodaj Grupu Atributa';
+$_['text_edit']         = 'Izmeni Grupu Atributa';
 
 // Column
-$_['column_name']            = 'Ime atributa';
-$_['column_attribute_group'] = 'Grupa atributa';
-$_['column_sort_order']      = 'Sortiraj po';
-$_['column_action']          = 'Akcija';
+$_['column_name']       = 'Ime Grupe Atributa';
+$_['column_sort_order'] = 'Sortiraj po';
+$_['column_action']     = 'Akcija';
 
 // Entry
-$_['entry_name']             = 'Ime atributa';
-$_['entry_attribute_group']  = 'Grupa atributa';
-$_['entry_sort_order']       = 'Sortiraj po';
+$_['entry_name']        = 'Ime Grupe Atributa';
+$_['entry_sort_order']  = 'Sortiraj po';
 
 // Error
-$_['error_warning']          = 'Upozorenje: Molimo proverite formu zbog grešaka!';
-$_['error_permission']       = 'Upozorenje: nemate ovlašćenje za izmenu atributa!';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'Ime atributa mora da ima između 3 i 64 karaktera!';
-$_['error_product']          = 'Upozorenje: Ovaj atribut ne može biti obrisan jer je trenutno povezan sa %s proizvoda!';
+$_['error_warning']     = 'Upozorenje: Molimo proverite formu zbog grešaka!';
+$_['error_permission']  = 'Upozorenje: Nemate dozvolu za izmenu atributa!';
+$_['error_name']        = 'Ime Grupe Atributa morabiti izmedju 3 i 64 karaktera!';
+$_['error_attribute']   = 'Upozorenje: Ova grupa atributa ne može biti izbrisana jer je trenutno dodeljena %s atributu!';
+$_['error_product']     = 'Upozorenje: Ova grupa atributa ne može biti izbrisana jer je trenutno dodeljena %s proizvodima!';
