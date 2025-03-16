239c418186632e55c7326cfc41d4422438075142
OSWorX <info@osworx.net>
2025-03-16 14:29:57 +0100

    New translations login.php (Bulgarian)

--- upload/admin/language/en-gb/common/bg-BG.php
+++ upload/admin/language/en-gb/common/bg-BG.php
@@ -1,23 +1,19 @@
 <?php
-// Heading
-$_['heading_title']          = 'OpenCart';
+// header
+$_['heading_title']  = 'Администрация';
 
 // Text
-$_['text_order']             = 'Поръчки';
-$_['text_processing_status'] = 'Processing';
-$_['text_complete_status']   = 'Завършено';
-$_['text_customer']          = 'Клиенти';
-$_['text_online']            = 'Потребители онлайн';
-$_['text_approval']          = 'Чакащо одобрение';
-$_['text_product']           = 'Продукти';
-$_['text_stock']             = 'Изчерпано количество';
-$_['text_review']            = 'Отзиви';
-$_['text_return']            = 'Връщане';
-$_['text_affiliate']         = 'Съдружници';
-$_['text_store']             = 'Магазини';
-$_['text_front']             = 'Витрина';
-$_['text_help']              = 'Помощ';
-$_['text_homepage']          = 'OpenCart Homepage';
-$_['text_support']           = 'Форум за поддръжка';
-$_['text_documentation']     = 'Документация';
-$_['text_logout']            = 'Изход';
+$_['text_heading']   = 'Администрация';
+$_['text_login']     = 'Моля, въведете данните си за вход.';
+$_['text_forgotten'] = 'Забравена парола';
+
+// Entry
+$_['entry_username'] = 'Потребителско име';
+$_['entry_password'] = 'Парола';
+
+// Button
+$_['button_login']   = 'Вход';
+
+// Error
+$_['error_login']    = 'Няма съвпадение за потребителско име и / или парола.';
+$_['error_token']    = 'Невалидна сесия. Моля влезете отново.';
