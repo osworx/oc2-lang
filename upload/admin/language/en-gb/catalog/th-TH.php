a2cb247b71047e0ebf7a3c9b0b9df86661f09576
OSWorX <info@osworx.net>
2025-03-16 14:21:11 +0100

    New translations manufacturer.php (Thai)

--- upload/admin/language/en-gb/catalog/th-TH.php
+++ upload/admin/language/en-gb/catalog/th-TH.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'ข้อมูล';
+$_['heading_title']     = 'ผู้ผลิต';
 
 // Text
-$_['text_success']           = 'ความสำเร็จ: คุณได้ปรับเปลี่ยนข้อมูล';
-$_['text_list']              = 'รายการข้อมูล';
-$_['text_add']               = 'เพิ่มข้อมูล';
-$_['text_edit']              = 'แก้ไขข้อมูล';
-$_['text_default']           = 'ค่าเริ่มต้น';
+$_['text_success']      = 'ความสำเร็จ: คุณได้ปรับเปลี่ยนผู้ผลิต';
+$_['text_list']         = 'รายการผู้ผลิต';
+$_['text_add']          = 'เพิ่มผู้ผลิต';
+$_['text_edit']         = 'แก้ไขผู้ผลิต';
+$_['text_default']      = 'ค่าเริ่มต้น';
+$_['text_percent']      = 'เปอร์เซ็นต์';
+$_['text_amount']       = 'คงที่';
 
 // Column
-$_['column_title']           = 'ชื่อข้อมูล';
-$_['column_sort_order']	     = 'ลำดับการจัดเรียง';
-$_['column_action']          = 'การดำเนินการ';
+$_['column_name']       = 'ชื่อผู้ผลิต';
+$_['column_sort_order'] = 'ลำดับการจัดเรียง';
+$_['column_action']     = 'การดำเนินการ';
 
 // Entry
-$_['entry_title']            = 'ชื่อข้อมูล';
-$_['entry_description']      = 'คำอธิบาย';
-$_['entry_store']            = 'ร้านค้า';
-$_['entry_meta_title'] 	     = 'ชื่อแท็ก Meta';
-$_['entry_meta_keyword'] 	 = 'คำเรียก Tag Meta';
-$_['entry_meta_description'] = 'คำอธิบาย Tag Meta';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'ด้านล่าง';
-$_['entry_status']           = 'สถานะ';
-$_['entry_sort_order']       = 'ลำดับการจัดเรียง';
-$_['entry_layout']           = 'Layout Override';
+$_['entry_name']        = 'ชื่อผู้ผลิต';
+$_['entry_store']       = 'ร้านค้า';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'รูปภาพ';
+$_['entry_sort_order']  = 'ลำดับการจัดเรียง';
+$_['entry_type']        = 'ประเภท';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'แสดงในส่วนท้ายด้านล่าง (footer)';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'คำเตือน: กรุณาตรวจสอบแบบฟอร์มอย่างรอบคอบสำหรับข้อผิดพลาด!';
-$_['error_permission']       = 'คำเตือน: คุณไม่มีสิทธิ์ในการแก้ไขข้อมูล';
-$_['error_title']            = 'เรื่องข้อมูลต้องอยู่ระหว่าง 3 ถึง 64 อักขระ';
-$_['error_description']      = 'คำอธิบายต้องมากกว่า 3 ตัวอักษร';
-$_['error_meta_title']       = 'ชื่อ Meta ต้องมากกว่า 3 และน้อยกว่า 255 อักษร!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'คำเตือน: หน้านี้ไม่สามารถลบเนื่องปัจจุบันมีการกำหนดให้เป็นเงื่อนไขบัญชีร้านค้า';
-$_['error_checkout']         = 'คำเตือน: หน้านี้ไม่สามารถลบเนื่องปัจจุบันมีการกำหนดให้เป็นเงื่อนไขเช็คเอาท์';
-$_['error_affiliate']        = 'คำเตือน: หน้านี้ไม่สามารถลบเนื่องปัจจุบันมีการกำหนดให้เป็นเงื่อนไขพันธมิตรร้านค้า';
-$_['error_return']           = 'คำเตือน: หน้านี้ไม่สามารถลบเนื่องปัจจุบันมีการกำหนดให้เป็นเงื่อนไขการส่งคืนร้านค้า';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'คำเตือน: กรุณาตรวจสอบแบบฟอร์มอย่างรอบคอบสำหรับข้อผิดพลาด!';
+$_['error_permission']  = 'คำเตือน: คุณไม่มีสิทธิ์ในการปรับเปลี่ยนผู้ผลิต';
+$_['error_name']        = 'ชื่อผู้ผลิตต้องอยู่ระหว่าง 2 ถึง 64 อักขระ';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'คำเตือน: ผู้ผลิตนี้ไม่สามารถลบเนื่องจากใช้อยู่กับผลิตภัณฑ์ %s';
