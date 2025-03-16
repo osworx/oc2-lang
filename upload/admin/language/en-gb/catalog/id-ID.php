8b48372070aead36bcf2a709337dc771159a9673
OSWorX <info@osworx.net>
2025-03-16 11:07:06 +0100

    New translations category.php (Indonesian)

--- upload/admin/language/en-gb/catalog/id-ID.php
+++ upload/admin/language/en-gb/catalog/id-ID.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Grup Atribut';
+$_['heading_title']          = 'Kategori';
 
 // Text
-$_['text_success']      = 'Sukses: Anda telah memodifikasi grup atribut!';
-$_['text_list']         = 'Daftar kelompok atribut';
-$_['text_add']          = 'Menambahkan atribut kelompok';
-$_['text_edit']         = 'Edit Atribut Kelompok';
+$_['text_success']           = 'Sukses: Anda telah mengubah kategori!';
+$_['text_list']              = 'Daftar Kategori';
+$_['text_add']               = 'Tambahkan Kategori';
+$_['text_edit']              = 'Ubah Kategori';
+$_['text_default']           = 'Standar';
 
 // Column
-$_['column_name']       = 'Nama Grup Atribut';
-$_['column_sort_order'] = 'Urutan';
-$_['column_action']     = 'Tindakan';
+$_['column_name']            = 'Nama Kategori';
+$_['column_sort_order']      = 'Urutan';
+$_['column_action']          = 'Tindakan';
 
 // Entry
-$_['entry_name']        = 'Nama Grup Atribut';
-$_['entry_sort_order']  = 'Urutan';
+$_['entry_name']             = 'Nama Kategori';
+$_['entry_description']      = 'Deskripsi';
+$_['entry_meta_title'] 	     = 'Judul Tag Meta';
+$_['entry_meta_keyword']     = 'Kata Kunci Tag Meta';
+$_['entry_meta_description'] = 'Deskripsi Tag Meta';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Induk';
+$_['entry_filter']           = 'Filter';
+$_['entry_store']            = 'Toko';
+$_['entry_image']            = 'Gambar';
+$_['entry_top']              = 'Atas';
+$_['entry_column']           = 'Kolom';
+$_['entry_sort_order']       = 'Urutan';
+$_['entry_status']           = 'Status';
+$_['entry_layout']           = 'Penimpaan Tata Letak';
+
+// Help
+$_['help_filter']            = '(Autocomplete)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Tampilan di menu bar atas. Hanya bekerja untuk induk kategori.';
+$_['help_column']            = 'Jumlah kolom untuk menggunakan 3 kategori dibawah. Hanya bekerja untuk induk kategori.';
 
 // Error
-$_['error_warning']     = 'Peringatan: Mohon periksa formulir secara teliti untuk kesalahan!';
-$_['error_permission']  = 'Peringatan: Anda tidak memiliki izin untuk mengubah grup atribut!';
-$_['error_name']        = 'Nama grup atribut harus antara 3 dan 64 karakter!';
-$_['error_attribute']   = 'Peringatan: Grup atribut ini tidak bisa dihapus karena saat ini ditetapkan untuk atribut %s!';
-$_['error_product']     = 'Peringatan: Atribut ini tidak bisa dihapus karena saat ini ditetapkan untuk produk %s!';
+$_['error_warning']          = 'Peringatan: Mohon periksa formulir secara teliti untuk kesalahan!';
+$_['error_permission']       = 'Peringatan: Anda tidak memiliki izin untuk mengubah kategori!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Judul meta harus lebih besar dari 3 dan kurang dari 255 karakter!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
