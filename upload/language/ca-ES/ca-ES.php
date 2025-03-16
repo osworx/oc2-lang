a7da388ec181c7f1ba6129815530b6441993f119
OSWorX <info@osworx.net>
2025-03-16 09:07:32 +0100

    New translations attribute_group.php (Catalan)

--- upload/language/ca-ES/ca-ES.php
+++ upload/language/ca-ES/ca-ES.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Atributs';
+$_['heading_title']     = 'Atributs';
 
 // Text
-$_['text_success']           = 'Èxit: Heu modificat els atributs!';
-$_['text_list']              = 'Llista d\'atributs';
-$_['text_add']               = 'Afegeix un atribut';
-$_['text_edit']              = 'Edita els atributs';
+$_['text_success']      = 'Èxit: Heu modificat els atributs!';
+$_['text_list']         = 'Llista d\'atributs';
+$_['text_add']          = 'Afegir atribut';
+$_['text_edit']         = 'Editar atribut';
 
 // Column
-$_['column_name']            = 'Nom de l\'atribut';
-$_['column_attribute_group'] = 'Grup d\'atributs';
-$_['column_sort_order']      = 'Ordre de classificació';
-$_['column_action']          = 'Acció';
+$_['column_name']       = 'Nom de l\'Atribut';
+$_['column_sort_order'] = 'Ordre de classificació';
+$_['column_action']     = 'Acció';
 
 // Entry
-$_['entry_name']             = 'Nom de l\'atribut';
-$_['entry_attribute_group']  = 'Grup d\'atributs';
-$_['entry_sort_order']       = 'Ordre de classificació';
+$_['entry_name']        = 'Nom de l\'Atribut';
+$_['entry_sort_order']  = 'Ordre de classificació';
 
 // Error
-$_['error_warning']          = 'Advertència: Si us plau reviseu acuradament el formulari pels errors!';
-$_['error_permission']       = 'Advertència: No teniu permís per modificar els atributs!';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'El nom de l\'atribut ha de ser entre 3 i 64 caràcters!';
-$_['error_product']          = 'Advertència: Aquests atributs no es poden suprimir com actualment està assignat a %s productes!';
+$_['error_warning']     = 'Advertència: Si us plau reviseu acuradament el formulari pels errors!';
+$_['error_permission']  = 'Advertència: No teniu permís per modificar els atributs!';
+$_['error_name']        = 'Advertència: El Nom de l\'Atribut deu de tenir entre 3 i 64 caràcters!';
+$_['error_attribute']   = 'Advertència: Aquest Grup d\'Atributs no pot ser eliminat ja que està assignat actualment a %s atributs!';
+$_['error_product']     = 'Advertència: Aquest atribut no pot ser eliminat ja està assignat actualment a %s productes!';
