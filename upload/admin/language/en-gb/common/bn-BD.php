1dc8a4c4bc84f9e213a976707c50449e1c4db3b4
OSWorX <info@osworx.net>
2025-03-16 11:19:35 +0100

    New translations header.php (Bengali)

--- upload/admin/language/en-gb/common/bn-BD.php
+++ upload/admin/language/en-gb/common/bn-BD.php
@@ -1,19 +1,23 @@
 <?php
-// header
-$_['heading_title']   = 'আপনার পাসওয়ার্ড ভুলে গেছেন?';
+// Heading
+$_['heading_title']          = 'OpenCart';
 
 // Text
-$_['text_forgotten']  = 'পাসওয়ার্ড ভুলে গেছেন';
-$_['text_your_email'] = 'আপনার ই-মেইল ঠিকানা';
-$_['text_email']      = 'আপনার হিসাবের সাথে সংশ্লিষ্ট ই-মেইল ঠিকানা প্রবেশ করান। ক্লিক জমা করুন আপনার গুপ্ত-সংকেত আপনাকে ই-মেইল যোগসূত্র পুনঃবিন্যাস করুন।.';
-$_['text_success']    = 'Admin আপনার ই-মেইল ঠিকানা নিশ্চিতকরণ লিংক দিয়ে ই-মেইল প্রেরণ করা হয়েছে।.';
-
-// Entry
-$_['entry_email']     = 'ই-মেইল ঠিকানা';
-$_['entry_password']  = 'নতুন পাসওয়ার্ড';
-$_['entry_confirm']   = 'নিশ্চিত করুন';
-
-// Error
-$_['error_email']     = 'Warning: The E-Mail Address was not found in our records, please try again!';
-$_['error_password']  = 'Password must be between 4 and 20 characters!';
-$_['error_confirm']   = 'Password and password confirmation do not match!';
+$_['text_order']             = 'অর্ডার';
+$_['text_processing_status'] = 'Processing';
+$_['text_complete_status']   = 'সম্পূর্ণ হয়েছে';
+$_['text_customer']          = 'ক্রেতা';
+$_['text_online']            = 'Customers Online';
+$_['text_approval']          = 'Pending approval';
+$_['text_product']           = 'Products';
+$_['text_stock']             = 'Out of stock';
+$_['text_review']            = 'Reviews';
+$_['text_return']            = 'Returns';
+$_['text_affiliate']         = 'Affiliates';
+$_['text_store']             = 'ভাণ্ডার';
+$_['text_front']             = 'Store Front';
+$_['text_help']              = 'Help';
+$_['text_homepage']          = 'OpenCart Homepage';
+$_['text_support']           = 'Support Forum';
+$_['text_documentation']     = 'Documentation';
+$_['text_logout']            = 'Logout';
