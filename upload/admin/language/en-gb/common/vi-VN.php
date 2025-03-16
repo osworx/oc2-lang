6c6406ea1d6718818e1037cceca6ac0d8ada308a
OSWorX <info@osworx.net>
2025-03-16 11:18:40 +0100

    New translations forgotten.php (Vietnamese)

--- upload/admin/language/en-gb/common/vi-VN.php
+++ upload/admin/language/en-gb/common/vi-VN.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Bạn quên mật mã?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Phiên bản %s';
+$_['text_forgotten']  = 'Quên mật khẩu';
+$_['text_your_email'] = 'Địa chỉ E-Mail của bạn';
+$_['text_email']      = 'Nhập địa chỉ email được liên kết với tài khoản của bạn. Nhấp vào gửi để có liên kết thiết lập lại mật mã gửi tới e-mail của bạn.';
+$_['text_success']    = 'Một email chứa liên kết xác nhận đã gửi tới địa chỉ email quản trị của bạn.';
+
+// Entry
+$_['entry_email']     = 'Địa chỉ e-Mail';
+$_['entry_password']  = 'Mật mã mới';
+$_['entry_confirm']   = 'Xác nhận';
+
+// Error
+$_['error_email']     = 'Chú ý: Địa chỉ e-Mail không tìm thấy trong hồ sơ của chúng tôi, xin vui lòng thử lại!';
+$_['error_password']  = 'Password must be between 4 and 20 characters!';
+$_['error_confirm']   = 'Mật mã và sự xác nhận mật mã không trùng khớp!';
