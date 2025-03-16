0ae46d35409677cf0cdc4faaadad361fb3576a44
OSWorX <info@osworx.net>
2025-03-16 11:06:55 +0100

    New translations category.php (Swedish)

--- upload/admin/language/en-gb/catalog/sv-SE.php
+++ upload/admin/language/en-gb/catalog/sv-SE.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Attributgrupper';
+$_['heading_title']          = 'Kategorier';
 
 // Text
-$_['text_success']      = 'Klart: Du har modifierat attributgrupper!';
-$_['text_list']         = 'Attributgruppslista';
-$_['text_add']          = 'Lägg till attributgrupp';
-$_['text_edit']         = 'Redigera attributgrupp';
+$_['text_success']           = 'Klart: Du har ändrat kategorier!';
+$_['text_list']              = 'Kategorilista';
+$_['text_add']               = 'Lägg till kategori';
+$_['text_edit']              = 'Redigera kategori';
+$_['text_default']           = 'Standard';
 
 // Column
-$_['column_name']       = 'Attributgruppens namn';
-$_['column_sort_order'] = 'Sorteringsordning';
-$_['column_action']     = 'Åtgärd';
+$_['column_name']            = 'Kategorinamn';
+$_['column_sort_order']      = 'Sorteringsordning';
+$_['column_action']          = 'Åtgärd';
 
 // Entry
-$_['entry_name']        = 'Attributgruppens namn';
-$_['entry_sort_order']  = 'Sorteringsordning';
+$_['entry_name']             = 'Kategorinamn';
+$_['entry_description']      = 'Beskrivning';
+$_['entry_meta_title'] 	     = 'Metatagtitel';
+$_['entry_meta_keyword']     = 'Metataggsökord';
+$_['entry_meta_description'] = 'Metataggbeskrivning';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Överordnad';
+$_['entry_filter']           = 'Filter';
+$_['entry_store']            = 'Butiker';
+$_['entry_image']            = 'Bild';
+$_['entry_top']              = 'Topp';
+$_['entry_column']           = 'Kolumner';
+$_['entry_sort_order']       = 'Sorteringsordning';
+$_['entry_status']           = 'Status';
+$_['entry_layout']           = 'Layoutomställning';
+
+// Help
+$_['help_filter']            = '(Komplettera automatiskt)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Visas i den övre menyraden. Fungerar bara för de översta överordnade kategorierna.';
+$_['help_column']            = 'Antalet kolumner för de nedre 3 kategorierna. Fungerar bara för de översta överordnade kategorierna.';
 
 // Error
-$_['error_warning']     = 'Varning: Kolla gärna formuläret noga vad det gäller fel!';
-$_['error_permission']  = 'Varning: Du har inte behörighet att redigera attributgrupper!';
-$_['error_name']        = 'Attributgruppens namn, måste vara mellan 3 och 64 tecken långt!';
-$_['error_attribute']   = 'Varning: Denna attributgrupp kan inte raderas då den används av %s attribut!';
-$_['error_product']     = 'Varning: Denna attributgrupp kan inte raderas då den används av %s produkter!';
+$_['error_warning']          = 'Varning: Kolla gärna formuläret noga vad det gäller fel!';
+$_['error_permission']       = 'Varning: Du har inte behörighet att ändra kategorier!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Metatitel måste vara större än 3 och mindre än 255 tecken!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
