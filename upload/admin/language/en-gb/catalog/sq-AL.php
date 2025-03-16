8747832e204d689f45856fe9fa772b4337c9e7b3
OSWorX <info@osworx.net>
2025-03-16 11:05:13 +0100

    New translations attribute.php (Albanian)

---
+++ upload/admin/language/en-gb/catalog/sq-AL.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Atributet';
+
+// Text
+$_['text_success']           = 'Suksess: Ju keni mdifikuar atributet!';
+$_['text_list']              = 'Lista e atributeve';
+$_['text_add']               = 'Shto Atribut';
+$_['text_edit']              = 'Modifiko Atribut';
+
+// Column
+$_['column_name']            = 'Emri i Atributit';
+$_['column_attribute_group'] = 'Grupi i Atributit';
+$_['column_sort_order']      = 'Radhitja';
+$_['column_action']          = 'Veprimi';
+
+// Entry
+$_['entry_name']             = 'Emri i Atributit';
+$_['entry_attribute_group']  = 'Grupi i Atributit';
+$_['entry_sort_order']       = 'Radhitja';
+
+// Error
+$_['error_warning']          = 'Vërejtje: Ju lutem kontrolloni formën me kujdes për ndonjë gabim!';
+$_['error_permission']       = 'Kujdes: Ju nuk keni leje të modifikoni atributet!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Emri i Atributit duhet të jete nga 3 deri në 64 shkronja!';
+$_['error_product']          = 'Nuk mund te fshihet pasi eshte percaktuar produktit %s!';
