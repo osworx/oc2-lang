9f365f2608630d571601a84f06ebfe66731d8664
OSWorX <info@osworx.net>
2025-03-16 11:16:49 +0100

    New translations filemanager.php (Slovak)

--- upload/admin/language/en-gb/common/sk-SK.php
+++ upload/admin/language/en-gb/common/sk-SK.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Nástenka';
+$_['heading_title']    = 'Obrázkový manažér';
+
+// Text
+$_['text_uploaded']    = 'Úspech: Váš súbor bol nahraný!';
+$_['text_directory']   = 'Úspech: Rozcestník bol vytvorený!';
+$_['text_delete']      = 'Úspech: Váš súbor alebo rozcestník bol odstránený!';
+
+// Entry
+$_['entry_search']     = 'Hľadať..';
+$_['entry_folder']     = 'Názov priečinku';
 
 // Error
-$_['error_install'] = 'Upozornenie: Inštalačný priečinok existuje a mal by byť odstránený z bezpečnostných dôvodov!';
+$_['error_permission'] = 'Upozornenie: Prístup zamietnutý!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Upozornenie: Súbor alebo rozcestník s rovnakým menom už existuje!';
+$_['error_directory']  = 'Upozornenie: Rozcestník neexistuje!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Upozornenie: Nesprávny typ súboru!';
+$_['error_upload']     = 'Upozornenie: Súbor nemohol byť nahraný z neznámych príčin!';
+$_['error_delete']     = 'Upozornenie: Nemožte zmazať tento rozcestník!';
