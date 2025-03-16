565ebee2ae2f200a33a0de66a4df990a5dba3f47
OSWorX <info@osworx.net>
2025-03-16 14:24:04 +0100

    New translations review.php (Norwegian)

--- upload/admin/language/en-gb/catalog/no-NO.php
+++ upload/admin/language/en-gb/catalog/no-NO.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'Gjentakende profiler';
+$_['heading_title']     = 'Produktomtaler';
 
 // Text
-$_['text_success']          = 'Endringer ble vellykket lagret.';
-$_['text_list']             = 'Liste';
-$_['text_add']              = 'Legg til';
-$_['text_edit']             = 'Endre';
-$_['text_day']				= 'Dag';
-$_['text_week']				= 'Uke';
-$_['text_semi_month']		= 'Halvmåned';
-$_['text_month']			= 'Måned';
-$_['text_year']				= 'År';
-$_['text_recurring']	    = '<p><i class=fa fa-info-circle"></i>Gjentakende beløp kalkuleres ut fra frekvens og syklus.</p><p>F.eks. dersom du bruker frekvens \'uke\' og syklus \'2\', vil kunden bli fakturert hver 2. uke.</p><p>Varighet er antall ganger som kunden vil måtte betale, sett til 0 dersom det skal løpe til oppsigelse.</p>';
-$_['text_profile']			= 'Gjentakende profil';
-$_['text_trial']			= 'Prøveperiode';
-
-// Entry
-$_['entry_name']		    = 'Navn';
-$_['entry_price']			= 'Pris';
-$_['entry_duration']		= 'Varighet';
-$_['entry_cycle']			= 'Syklus';
-$_['entry_frequency']		= 'Frekvens';
-$_['entry_trial_price']		= 'Pris for prøvetid';
-$_['entry_trial_duration']	= 'Varighet for prøvetid';
-$_['entry_trial_status']	= 'Status for prøvetid';
-$_['entry_trial_cycle']	    = 'Syklus for prøvetid';
-$_['entry_trial_frequency'] = 'Frekvens for prøvetid';
-$_['entry_status']			= 'Status';
-$_['entry_sort_order']		= 'Sortering';
+$_['text_success']      = 'Endringer ble vellykket lagret.';
+$_['text_list']         = 'Liste';
+$_['text_add']          = 'Legg til';
+$_['text_edit']         = 'Endre';
 
 // Column
-$_['column_name']			= 'Navn';
-$_['column_sort_order']	    = 'Sortering';
-$_['column_action']         = 'Valg';
+$_['column_product']    = 'Produkt';
+$_['column_author']     = 'Skribent';
+$_['column_rating']     = 'Vurdering';
+$_['column_status']     = 'Status';
+$_['column_date_added'] = 'Lagt til';
+$_['column_action']     = 'Valg';
+
+// Entry
+$_['entry_product']     = 'Produkt';
+$_['entry_author']      = 'Skribent';
+$_['entry_rating']      = 'Vurdering';
+$_['entry_status']      = 'Status';
+$_['entry_text']        = 'Tekstfelt';
+$_['entry_date_added']  = 'Lagt til';
+
+// Help
+$_['help_product']      = '(Autofullfør)';
 
 // Error
-$_['error_warning']         = 'Påse at alle påkrevde felter er fylt inn.';
-$_['error_permission']		= 'Du har ikke rettigheter til å utføre valgte handling.';
-$_['error_name']			= 'Navn må inneholde mellom 3 og 255 tegn.';
-$_['error_product']			= 'Denne profilen kan ikke slettes ettersom den er tilknyttet %s produkter.';
+$_['error_warning']     = 'Påse at alle påkrevde felter er fylt inn.';
+$_['error_permission']  = 'Du har ikke rettigheter til å utføre valgte handling.';
+$_['error_product']     = 'Produkt må velges.';
+$_['error_author']      = 'Skribent må inneholde mellom 3 og 64 tegn.';
+$_['error_text']        = 'Tekst må inneholde minst 1 tegn.';
+$_['error_rating']      = 'Vurdering må angis.';
