a649a5a53be330a9432a508fd67c98aef2f09c2a
OSWorX <info@osworx.net>
2025-03-16 14:20:24 +0100

    New translations information.php (Thai)

--- upload/admin/language/en-gb/catalog/th-TH.php
+++ upload/admin/language/en-gb/catalog/th-TH.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'ตัวกรอง';
+$_['heading_title']          = 'ข้อมูล';
 
 // Text
-$_['text_success']      = 'สำเร็จ: คุณได้ปรับเปลี่ยนตัวกรอง';
-$_['text_list']         = 'รายละเอียดการกรองข้อมูล';
-$_['text_add']          = 'เพิ่มตัวกรอง';
-$_['text_edit']         = 'แก้ไขตัวกรอง';
+$_['text_success']           = 'ความสำเร็จ: คุณได้ปรับเปลี่ยนข้อมูล';
+$_['text_list']              = 'รายการข้อมูล';
+$_['text_add']               = 'เพิ่มข้อมูล';
+$_['text_edit']              = 'แก้ไขข้อมูล';
+$_['text_default']           = 'ค่าเริ่มต้น';
 
 // Column
-$_['column_group']      = 'กลุ่มตัวกรอง';
-$_['column_sort_order'] = 'ลำดับการจัดเรียง';
-$_['column_action']     = 'การดำเนินการ';
+$_['column_title']           = 'ชื่อข้อมูล';
+$_['column_sort_order']	     = 'ลำดับการจัดเรียง';
+$_['column_action']          = 'การดำเนินการ';
 
 // Entry
-$_['entry_group']       = 'ชื่อกลุ่มของตัวกรอง';
-$_['entry_name']        = 'ชื่อตัวกรอง';
-$_['entry_sort_order']  = 'ลำดับการจัดเรียง';
+$_['entry_title']            = 'ชื่อข้อมูล';
+$_['entry_description']      = 'คำอธิบาย';
+$_['entry_store']            = 'ร้านค้า';
+$_['entry_meta_title'] 	     = 'ชื่อแท็ก Meta';
+$_['entry_meta_keyword'] 	 = 'คำเรียก Tag Meta';
+$_['entry_meta_description'] = 'คำอธิบาย Tag Meta';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'ด้านล่าง';
+$_['entry_status']           = 'สถานะ';
+$_['entry_sort_order']       = 'ลำดับการจัดเรียง';
+$_['entry_layout']           = 'Layout Override';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'แสดงในส่วนท้ายด้านล่าง (footer)';
 
 // Error
-$_['error_warning']     = 'คำเตือน: กรุณาตรวจสอบแบบฟอร์มอย่างรอบคอบสำหรับข้อผิดพลาด!';
-$_['error_permission']  = 'คำเตือน: คุณไม่มีสิทธิ์ในการปรับเปลี่ยนตัวกรอง';
-$_['error_group']       = 'ชื่อกลุ่มกรองต้องอยู่ระหว่าง 1 ถึง 64 อักขระ';
-$_['error_name']        = 'ชื่อตัวกรองต้องอยู่ระหว่าง 1 ถึง 64 อักขระ';
+$_['error_warning']          = 'คำเตือน: กรุณาตรวจสอบแบบฟอร์มอย่างรอบคอบสำหรับข้อผิดพลาด!';
+$_['error_permission']       = 'คำเตือน: คุณไม่มีสิทธิ์ในการแก้ไขข้อมูล';
+$_['error_title']            = 'เรื่องข้อมูลต้องอยู่ระหว่าง 3 ถึง 64 อักขระ';
+$_['error_description']      = 'คำอธิบายต้องมากกว่า 3 ตัวอักษร';
+$_['error_meta_title']       = 'ชื่อ Meta ต้องมากกว่า 3 และน้อยกว่า 255 อักษร!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'คำเตือน: หน้านี้ไม่สามารถลบเนื่องปัจจุบันมีการกำหนดให้เป็นเงื่อนไขบัญชีร้านค้า';
+$_['error_checkout']         = 'คำเตือน: หน้านี้ไม่สามารถลบเนื่องปัจจุบันมีการกำหนดให้เป็นเงื่อนไขเช็คเอาท์';
+$_['error_affiliate']        = 'คำเตือน: หน้านี้ไม่สามารถลบเนื่องปัจจุบันมีการกำหนดให้เป็นเงื่อนไขพันธมิตรร้านค้า';
+$_['error_return']           = 'คำเตือน: หน้านี้ไม่สามารถลบเนื่องปัจจุบันมีการกำหนดให้เป็นเงื่อนไขการส่งคืนร้านค้า';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
