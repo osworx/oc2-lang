ddf2418221234098d1c6d0611604a68c3c4eb698
OSWorX <info@osworx.net>
2025-03-16 09:05:59 +0100

    New translations footer.php (Romanian)

--- upload/language/ro-RO/ro-RO.php
+++ upload/language/ro-RO/ro-RO.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Manager de imagini';
-
 // Text
-$_['text_uploaded']    = 'Succes: Fișierul dvs. a fost încarcat!';
-$_['text_directory']   = 'Succes: Director creat!';
-$_['text_delete']      = 'Succes: Fișierul sau directorul dvs. a fost șters!';
-
-// Entry
-$_['entry_search']     = 'Caută..';
-$_['entry_folder']     = 'Nume folder';
-
-// Error
-$_['error_permission'] = 'Avertisment: Acces refuzat!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Avertisment: Un fișier sau un director cu același nume există deja!';
-$_['error_directory']  = 'Avertisment: Directorul nu există!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Avertisment: Tip fișier incorect!';
-$_['error_upload']     = 'Avertisment: Fișierul nu a putut fi încărcat din motive necunoscute!';
-$_['error_delete']     = 'Avertisment: Nu puteți șterge acest director!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Versiunea %s';
