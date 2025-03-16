63ffa45bd689d648fcb33d320aa6fac268db7b72
OSWorX <info@osworx.net>
2025-03-16 11:18:12 +0100

    New translations forgotten.php (Spanish)

--- upload/admin/language/en-gb/common/es-ES.php
+++ upload/admin/language/en-gb/common/es-ES.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = '¿Olvidó su contraseña?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Versión %s';
+$_['text_forgotten']  = 'Contraseña olvidada';
+$_['text_your_email'] = 'Su dirección de correo electrónico';
+$_['text_email']      = 'Introduzca la dirección de correo electrónico asociada a su cuenta. Haga clic en enviar para recibir un enlace por correo electrónico para restaurar la contraseña.';
+$_['text_success']    = 'Un correo electrónico con un enlace de confirmación ha sido enviado a su dirección de correo electrónico de administrador.';
+
+// Entry
+$_['entry_email']     = 'Dirección de correo electrónico';
+$_['entry_password']  = 'Nueva Contraseña';
+$_['entry_confirm']   = 'Confirmar';
+
+// Error
+$_['error_email']     = 'ADVERTENCIA: La dirección de correo electrónico no fue encontrada en nuestros registros, por favor intentelo de nuevo!';
+$_['error_password']  = 'La Contraseña debe poseer entre 4 y 20 caracteres!';
+$_['error_confirm']   = 'La contraseña y su confirmación no coinciden!';
