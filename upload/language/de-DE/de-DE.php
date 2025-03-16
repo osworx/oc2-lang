a26169c141ee88fd2c8b1f6bf260436a151601ed
OSWorX <info@osworx.net>
2025-03-16 09:08:31 +0100

    New translations review.php (German)

--- upload/language/de-DE/de-DE.php
+++ upload/language/de-DE/de-DE.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'Abozahlungen';
+$_['heading_title']     = 'Bewertungen';
 
 // Text
-$_['text_success']          = 'Abozahlung wurde erfolgreich bearbeitet';
-$_['text_list']             = 'Übersicht';
-$_['text_add']              = 'Neu';
-$_['text_edit']             = 'Bearbeiten';
-$_['text_day']				= 'Tag';
-$_['text_week']				= 'Woche';
-$_['text_semi_month']		= '14 Tage';
-$_['text_month']			= 'Monat';
-$_['text_year']				= 'Jahr';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Wiederholende Zahlungen werden aus Frequenz und Wiederholung kalkuliert.</p><p>Beispiel: Frequenz : Woche, Wiederholung : 2 = alle 2 Wochen erfolgt eine Verrechnung.</p><p>Dauer ist die Anzahl wieoft Zahlungen erfolgen, wird der Wert 0 (Null) angewendet, erfolgen Verrechnung solange der Kunde von sich aus storniert/beendet.</p>';
-$_['text_profile']			= 'Abozahlung';
-$_['text_trial']			= 'Testperiode';
-
-// Entry
-$_['entry_name']		    = 'Name';
-$_['entry_price']			= 'Preis';
-$_['entry_duration']		= 'Dauer';
-$_['entry_cycle']			= 'Wiederholung';
-$_['entry_frequency']		= 'Frequenz';
-$_['entry_trial_price']		= 'Testpreis';
-$_['entry_trial_duration']	= 'Testdauer';
-$_['entry_trial_status']	= 'Teststatus';
-$_['entry_trial_cycle']	    = 'Testwiederholung';
-$_['entry_trial_frequency'] = 'Testanzahl';
-$_['entry_status']			= 'Status';
-$_['entry_sort_order']		= 'Reihenfolge';
+$_['text_success']      = 'Datensatz erfolgreich bearbeitet';
+$_['text_list']         = 'Übersicht';
+$_['text_add']          = 'Neu';
+$_['text_edit']         = 'Bearbeiten';
 
 // Column
-$_['column_name']			= 'Name';
-$_['column_sort_order']	    = 'Reihenfolge';
-$_['column_action']         = 'Aktion';
+$_['column_product']    = 'Produkt';
+$_['column_author']     = 'Autor';
+$_['column_rating']     = 'Bewertung';
+$_['column_status']     = 'Status';
+$_['column_date_added'] = 'Erstellt';
+$_['column_action']     = 'Aktion';
+
+// Entry
+$_['entry_product']     = 'Produkt';
+$_['entry_author']      = 'Autor';
+$_['entry_rating']      = 'Bewertung';
+$_['entry_status']      = 'Status';
+$_['entry_text']        = 'Text (einzeilig)';
+$_['entry_date_added']  = 'Erstellt';
+
+// Help
+$_['help_product']      = 'Begriff eintippen, System zeigt passende an';
 
 // Error
-$_['error_warning']         = 'Erforderliche Daten fehlen, bitte Felder überprüfen.';
-$_['error_permission']		= 'Heinweis: keine Rechte für diese Aktion';
-$_['error_name']			= 'Name muss zwischen 3 und 255 Zeichen lang sein';
-$_['error_product']			= 'Diese Abozahlung kann nicht gelöscht werden da sie %s Produkt(en) verbunden ist';
+$_['error_warning']     = 'Erforderliche Daten fehlen, bitte Felder überprüfen.';
+$_['error_permission']  = 'Keine Rechte für diese Aktion';
+$_['error_product']     = 'Produkt erforderlich!';
+$_['error_author']      = 'Autorenname muss zwischen 3 und max. 64 Zeichen lang sein!';
+$_['error_text']        = 'Text muss mindestens 1 Zeichen lang sein';
+$_['error_rating']      = 'Eine Bewertung ist zwingend erforderlich';
