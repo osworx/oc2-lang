b561b1dbd8cef5a5ee0671c9080cbc75874c8d5c
OSWorX <info@osworx.net>
2025-03-16 14:16:16 +0100

    New translations attribute.php (Vietnamese)

--- upload/admin/language/en-gb/catalog/vi-VN.php
+++ upload/admin/language/en-gb/catalog/vi-VN.php
@@ -1,36 +1,27 @@
 <?php
 // Heading
-$_['heading_title']     = 'Đánh giá';
+$_['heading_title']          = 'Các thuộc tính';
 
 // Text
-$_['text_success']      = 'Thành công: Bạn đã sửa đổi đánh giá!';
-$_['text_list']         = 'Danh sach đánh giá';
-$_['text_add']          = 'Thêm đánh giá';
-$_['text_edit']         = 'Sửa đánh giá';
+$_['text_success']           = 'Thành công: Bạn đã sửa các thuộc tính!';
+$_['text_list']              = 'Danh sách thuộc tính';
+$_['text_add']               = 'Thêm thuộc tính';
+$_['text_edit']              = 'Sửa thuộc tính';
 
 // Column
-$_['column_product']    = 'Sản phẩm';
-$_['column_author']     = 'Tác giả';
-$_['column_rating']     = 'Xếp hạng';
-$_['column_status']     = 'Trạng thái';
-$_['column_date_added'] = 'Ngày thêm vào';
-$_['column_action']     = 'Tác vụ';
+$_['column_name']            = 'Tên thuộc tính';
+$_['column_attribute_group'] = 'Nhóm thuộc tính';
+$_['column_sort_order']      = 'Thứ tự sắp xếp';
+$_['column_action']          = 'Tác vụ';
 
 // Entry
-$_['entry_product']     = 'Sản phẩm';
-$_['entry_author']      = 'Tác giả';
-$_['entry_rating']      = 'Xếp hạng';
-$_['entry_status']      = 'Trạng thái';
-$_['entry_text']        = 'Mô tả';
-$_['entry_date_added']  = 'Ngày thêm vào';
-
-// Help
-$_['help_product']      = '(Tự động gợi ý)';
+$_['entry_name']             = 'Tên thuộc tính';
+$_['entry_attribute_group']  = 'Nhóm thuộc tính';
+$_['entry_sort_order']       = 'Thứ tự sắp xếp';
 
 // Error
-$_['error_warning']     = 'Chú ý: Vui lòng kiểm tra lỗi cẩn thận trong biểu mẫu!';
-$_['error_permission']  = 'Chú ý: Bạn không có quyền sửa đổi đánh giá!';
-$_['error_product']     = 'Buộc phải nhập sản phẩm!';
-$_['error_author']      = 'Tác giả phải từ 3 đến 64 ký tự!';
-$_['error_text']        = 'Nội dung đánh giá phải có ít nhất 1 ký tự!';
-$_['error_rating']      = 'Buộc phải bầu chọn!';
+$_['error_warning']          = 'Chú ý: Vui lòng kiểm tra lỗi cẩn thận trong biểu mẫu!';
+$_['error_permission']       = 'Chú ý: Bạn không có quyền sửa đổi các thuộc tính!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Tên thuộc tính phải từ 3 đến 64 ký tự!';
+$_['error_product']          = 'Cảnh báo: Thuộc tính này không thể bị xoá vì nó hiện đang được gán cho %s sản phẩm!';
