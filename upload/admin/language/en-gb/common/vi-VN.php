f05b5643730faa90ece1f731b080cef8d2475fac
OSWorX <info@osworx.net>
2025-03-16 14:29:35 +0100

    New translations header.php (Vietnamese)

--- upload/admin/language/en-gb/common/vi-VN.php
+++ upload/admin/language/en-gb/common/vi-VN.php
@@ -1,19 +1,23 @@
 <?php
-// header
-$_['heading_title']   = 'Bạn quên mật mã?';
+// Heading
+$_['heading_title']          = 'OpenCart';
 
 // Text
-$_['text_forgotten']  = 'Quên mật khẩu';
-$_['text_your_email'] = 'Địa chỉ E-Mail của bạn';
-$_['text_email']      = 'Nhập địa chỉ email được liên kết với tài khoản của bạn. Nhấp vào gửi để có liên kết thiết lập lại mật mã gửi tới e-mail của bạn.';
-$_['text_success']    = 'Một email chứa liên kết xác nhận đã gửi tới địa chỉ email quản trị của bạn.';
-
-// Entry
-$_['entry_email']     = 'Địa chỉ e-Mail';
-$_['entry_password']  = 'Mật mã mới';
-$_['entry_confirm']   = 'Xác nhận';
-
-// Error
-$_['error_email']     = 'Chú ý: Địa chỉ e-Mail không tìm thấy trong hồ sơ của chúng tôi, xin vui lòng thử lại!';
-$_['error_password']  = 'Password must be between 4 and 20 characters!';
-$_['error_confirm']   = 'Mật mã và sự xác nhận mật mã không trùng khớp!';
+$_['text_order']             = 'Các đơn hàng';
+$_['text_processing_status'] = 'Processing';
+$_['text_complete_status']   = 'Đã xong';
+$_['text_customer']          = 'Các khách hàng';
+$_['text_online']            = 'Khách đang online';
+$_['text_approval']          = 'Đang chờ phê duyệt';
+$_['text_product']           = 'Các sản phẩm';
+$_['text_stock']             = 'Hết hàng';
+$_['text_review']            = 'Đánh giá';
+$_['text_return']            = 'Trả lại';
+$_['text_affiliate']         = 'Chi nhánh';
+$_['text_store']             = 'Gian hàng';
+$_['text_front']             = 'Cửa hàng ở phía trước';
+$_['text_help']              = 'Hỗ trợ';
+$_['text_homepage']          = 'OpenCart Homepage';
+$_['text_support']           = 'Diễn đàn hỗ trợ';
+$_['text_documentation']     = 'Tài liệu';
+$_['text_logout']            = 'Thoát';
