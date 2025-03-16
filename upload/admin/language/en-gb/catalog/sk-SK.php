afc694120ef1346a17332b011c9e117cd7315e98
OSWorX <info@osworx.net>
2025-03-16 11:05:11 +0100

    New translations attribute.php (Slovak)

---
+++ upload/admin/language/en-gb/catalog/sk-SK.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Vlastnosti';
+
+// Text
+$_['text_success']           = 'Hotovo: Atribút bol zmenený!';
+$_['text_list']              = 'Zoznam vlastností';
+$_['text_add']               = 'Pridať vlastnosť';
+$_['text_edit']              = 'Upraviť vlastnosť';
+
+// Column
+$_['column_name']            = 'Názov vlastnosti';
+$_['column_attribute_group'] = 'Skupina vlastností';
+$_['column_sort_order']      = 'Poradie';
+$_['column_action']          = 'Akcia';
+
+// Entry
+$_['entry_name']             = 'Názov vlastnosti';
+$_['entry_attribute_group']  = 'Skupina vlastností';
+$_['entry_sort_order']       = 'Poradie';
+
+// Error
+$_['error_warning']          = 'Varovanie: Skontrolujte formulár starostlivo kvôli chybám!';
+$_['error_permission']       = 'Varovanie: Nemáte oprávnenie k úprave vlastností!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Názov vlastnosti musí byť medzi v rozmedzí 3 až 64 znakov!';
+$_['error_product']          = 'Varovanie: Táto vlastnosť nemôže byť odstránená, pretože je v súčasnosti priradená k %s produktom!';
