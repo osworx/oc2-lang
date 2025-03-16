0231f823399dae69ef99d52e2e5e425e257e3ddb
OSWorX <info@osworx.net>
2025-03-16 11:13:08 +0100

    New translations recurring.php (Ukrainian)

--- upload/admin/language/en-gb/catalog/uk-UA.php
+++ upload/admin/language/en-gb/catalog/uk-UA.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'Товари';
+$_['heading_title']			= 'Періодичні платежі';
 
 // Text
-$_['text_success']           = 'Вітаємо: Ви успішно змінили товари!';
-$_['text_list']              = 'Список товарів';
-$_['text_add']               = 'Додати товар';
-$_['text_edit']              = 'Редагувати товар';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'За замовчуванням';
-$_['text_option']            = 'Параметр';
-$_['text_option_value']      = 'Значення параметру';
-$_['text_percent']           = 'Відсотки';
-$_['text_amount']            = 'Фіксована сума';
-
-// Column
-$_['column_name']            = 'Назва товару';
-$_['column_model']           = 'Модель';
-$_['column_image']           = 'Зображення';
-$_['column_price']           = 'Ціна';
-$_['column_quantity']        = 'Кількість';
-$_['column_status']          = 'Стан';
-$_['column_action']          = 'Дія';
+$_['text_success']          = 'Вітаємо: Ви успішно змінили регулярні платежі!';
+$_['text_list']             = 'Регулярні платежі';
+$_['text_add']              = 'Додати профіль регулярних платежів';
+$_['text_edit']             = 'Редагувати профіль регулярних платежів';
+$_['text_day']				= 'День';
+$_['text_week']				= 'Тиждень';
+$_['text_semi_month']		= 'половина місяця';
+$_['text_month']			= 'Місяц';
+$_['text_year']				= 'Рік';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Періодичні платежі розраховуються за частотою та циклами.</p><p>Наприклад, якщо ви використовуєте частоту "тиждень" і цикл «2», то користувачу буде виставлений рахунок кожні 2 тижні.</p> <p>Тривалість – кількість разів, котрі користувач буде робити платіж, встановіть це у "0", якщо ви хочете платежі, поки вони будуть скасовані.</p>';
+$_['text_profile']			= 'Регулярні платежі';
+$_['text_trial']			= 'Пробний профіль';
 
 // Entry
-$_['entry_name']             = 'Назва товару';
-$_['entry_description']      = 'Опис';
-$_['entry_meta_title'] 	     = 'Заголовок сторінки';
-$_['entry_meta_keyword'] 	 = 'Ключові слова';
-$_['entry_meta_description'] = 'Мета-тег опис';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Модель';
-$_['entry_sku']              = 'Артикул (SKU)';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'Місцезнаходження';
-$_['entry_shipping']         = 'Необхідна доставка';
-$_['entry_manufacturer']     = 'Виробник';
-$_['entry_store']            = 'Магазини';
-$_['entry_date_available']   = 'Дата наявності';
-$_['entry_quantity']         = 'Кількість';
-$_['entry_minimum']          = 'Мінімальна кількість';
-$_['entry_stock_status']     = 'В наявності статус';
-$_['entry_price']            = 'Ціна';
-$_['entry_tax_class']        = 'Податкова категорія';
-$_['entry_points']           = 'Бали';
-$_['entry_option_points']    = 'Бали';
-$_['entry_subtract']         = 'Відняти зі складу:<br/><span class="help">Якщо вказано "Так", загальна кількість товарів автоматично відніматиметься при кожному замовленні</span>';
-$_['entry_weight_class']     = 'Вагова категорія';
-$_['entry_weight']           = 'Вага';
-$_['entry_dimension']        = 'Розміри (Д х Ш х В)';
-$_['entry_length_class']     = 'Категорія довжини';
-$_['entry_length']           = 'Довжина';
-$_['entry_width']            = 'Ширина';
-$_['entry_height']           = 'Висота';
-$_['entry_image']            = 'Зображення';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Група клієнтів';
-$_['entry_date_start']       = 'Дата початку';
-$_['entry_date_end']         = 'Дата завершення';
-$_['entry_priority']         = 'Приорітет';
-$_['entry_attribute']        = 'Атрибут';
-$_['entry_attribute_group']  = 'Група атрибутів';
-$_['entry_text']             = 'Текст';
-$_['entry_option']           = 'Параметр';
-$_['entry_option_value']     = 'Значення параметру';
-$_['entry_required']         = 'Обов\'язково';
-$_['entry_status']           = 'Стан';
-$_['entry_sort_order']       = 'Порядок сортування';
-$_['entry_category']         = 'Категорії';
-$_['entry_filter']           = 'Фільтри';
-$_['entry_download']         = 'Завантаження';
-$_['entry_related']          = 'Супутні товари';
-$_['entry_tag']          	 = 'Мітки товару';
-$_['entry_reward']           = 'Нарахування балів';
-$_['entry_layout']           = 'Вигляд інтерфейсу';
-$_['entry_recurring']        = 'Регулярні платежі';
+$_['entry_name']		    = 'Ім\'я';
+$_['entry_price']			= 'Ціна';
+$_['entry_duration']		= 'Тривалість';
+$_['entry_cycle']			= 'Цикл';
+$_['entry_frequency']		= 'Частота';
+$_['entry_trial_price']		= 'Пробна ціна';
+$_['entry_trial_duration']	= 'Пробний термін';
+$_['entry_trial_status']	= 'Пробний статус';
+$_['entry_trial_cycle']	    = 'Пробний цикл';
+$_['entry_trial_frequency'] = 'Пробна періодичність';
+$_['entry_status']			= 'Стан';
+$_['entry_sort_order']		= 'Порядок сортування';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'Одиниця зберігання на складі';
-$_['help_upc']               = 'Універсальний код продукту';
-$_['help_ean']               = 'Європейський артикул';
-$_['help_jan']               = 'Японський артикул';
-$_['help_isbn']              = 'ISBN- міжнародний стандартний артикул';
-$_['help_mpn']               = 'Номер виробника';
-$_['help_manufacturer']      = '(Автозаповнення)';
-$_['help_minimum']           = 'Мінімальна сума замовлення';
-$_['help_stock_status']      = 'Статус, який буде показано, коли продукт буде в наявності';
-$_['help_points']            = 'Кількість балів, необхідна для купівлі цього товару. Якщо ви не хочете, щоб цей товар був придбаний за бали, поставте 0.';
-$_['help_category']          = '(Автозаповнення)';
-$_['help_filter']            = '(Автозаповнення)';
-$_['help_download']          = '(Автозаповнення)';
-$_['help_related']           = '(Автозаповнення)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'Ім\'я';
+$_['column_sort_order']	    = 'Порядок сортування';
+$_['column_action']         = 'Дія';
 
 // Error
-$_['error_warning']          = 'Будь ласка, перевірте форму на наявність помилок!';
-$_['error_permission']       = 'Увага: У Вас недостатньо повноважень для редагування товарів!';
-$_['error_name']             = 'Назва товару повинна містити від 3 до 255 символів!';
-$_['error_meta_title']       = 'Заголовок сторінки повинен містити від 3 до 255 символів!';
-$_['error_model']            = 'Назва моделі повинна містити від 1 до 64 символів!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Будь ласка, перевірте форму на наявність помилок!';
+$_['error_permission']		= 'Увага: У Вас недостатньо повноважень для редагування регулярних платежів!';
+$_['error_name']			= 'Назва профілю повинна містити від 3 до 255 символів!';
+$_['error_product']			= 'Увага: Цей профіль регулярних платежів не можливо видалити, оскільки він призначений для %s товарів!';
