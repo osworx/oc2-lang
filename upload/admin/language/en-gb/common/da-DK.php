b10f6dcaee50a88832ae6b4e93098994751cf1d7
OSWorX <info@osworx.net>
2025-03-16 14:28:26 +0100

    New translations forgotten.php (Danish)

--- upload/admin/language/en-gb/common/da-DK.php
+++ upload/admin/language/en-gb/common/da-DK.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Glemt adgangskode?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Version %s';
+$_['text_forgotten']  = 'Glemt adgangskode';
+$_['text_your_email'] = 'Din e-mail-adresse';
+$_['text_email']      = 'Angiv den e-mail-adresse, der er knyttet til din konto. Klik på Send for at få et nulstillet din adgangskode og et link til genoprettelse sendt til mail-adressen.';
+$_['text_success']    = 'En e-mail med et bekræftelseslink er blevet sendt til din e-mail adresse.';
+
+// Entry
+$_['entry_email']     = 'E-mail adresse';
+$_['entry_password']  = 'Ny adgangskode';
+$_['entry_confirm']   = 'Bekræft';
+
+// Error
+$_['error_email']     = 'Fejl: Denne e-mail-adresse blev ikke fundet i vor databaseposter. Prøv venligst igen!';
+$_['error_password']  = 'Adgangskoden skal være mellem 4 og 20 karakterer!';
+$_['error_confirm']   = 'Adgangskode og adgangskodebekræftelse stemmer ikke overens!';
