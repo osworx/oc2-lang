ee30c08e795d7110df1750dc045ea1cf29ce0157
OSWorX <info@osworx.net>
2025-03-16 14:15:48 +0100

    New translations attribute.php (Spanish)

--- upload/admin/language/en-gb/catalog/es-ES.php
+++ upload/admin/language/en-gb/catalog/es-ES.php
@@ -1,36 +1,27 @@
 <?php
 // Heading
-$_['heading_title']     = 'Comentarios';
+$_['heading_title']          = 'Atributos';
 
 // Text
-$_['text_success']      = '¡Ha modificado exitosamente los comentarios!';
-$_['text_list']         = 'Lista de Comentarios';
-$_['text_add']          = 'Agregar Comentarios';
-$_['text_edit']         = 'Editar Comentarios';
+$_['text_success']           = 'Has modificado los atributos correctamente!';
+$_['text_list']              = 'Lista de atributos';
+$_['text_add']               = 'Agregar atributo';
+$_['text_edit']              = 'Editar atributo';
 
 // Column
-$_['column_product']    = 'Producto';
-$_['column_author']     = 'Autor';
-$_['column_rating']     = 'Calificación';
-$_['column_status']     = 'Estado';
-$_['column_date_added'] = 'Agregar Fecha';
-$_['column_action']     = 'Acción';
+$_['column_name']            = 'Nombre de atributo';
+$_['column_attribute_group'] = 'Grupo de atributos';
+$_['column_sort_order']      = 'Ordenar por';
+$_['column_action']          = 'Acción';
 
 // Entry
-$_['entry_product']     = 'Producto';
-$_['entry_author']      = 'Autor';
-$_['entry_rating']      = 'Calificación';
-$_['entry_status']      = 'Estado';
-$_['entry_text']        = 'Texto';
-$_['entry_date_added']  = 'Agregar Fecha';
-
-// Help
-$_['help_product']      = '(Autocompletar)';
+$_['entry_name']             = 'Nombre de atributo';
+$_['entry_attribute_group']  = 'Grupo de atributos';
+$_['entry_sort_order']       = 'Ordenar por';
 
 // Error
-$_['error_warning']     = '¡Advertencia: Por favor revise cuidadosamente los errores en el formulario!';
-$_['error_permission']  = '¡Advertencia: No tienes permiso para modificar los comentarios!';
-$_['error_product']     = '¡Producto requerido!';
-$_['error_author']      = '¡El autor debe tener entre 3 y 64 carácteres!';
-$_['error_text']        = '¡El Texto del Comentario debe tener al menos 1 carácter!';
-$_['error_rating']      = '¡Calificación del comentario requerida!';
+$_['error_warning']          = '¡Advertencia: Por favor revise cuidadosamente los errores en el formulario!';
+$_['error_permission']       = 'ADVERTENCIA: No tienes permiso para modificar atributos!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'El nombre del atributo debe tener entre 3 y 64 caracteres.';
+$_['error_product']          = 'ADVERTENCIA: Este atributo no se puede eliminar porque actualmente está asignado a los productos %s!';
