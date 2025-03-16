53d68d8f6cb22280efb956c9cf47a18e6a40e812
OSWorX <info@osworx.net>
2025-03-16 11:20:21 +0100

    New translations login.php (Persian)

--- upload/admin/language/en-gb/common/fa-IR.php
+++ upload/admin/language/en-gb/common/fa-IR.php
@@ -1,23 +1,19 @@
 <?php
-// Heading
-$_['heading_title']          = 'اپن‌کارت';
+// header
+$_['heading_title']  = 'مدیریت';
 
 // Text
-$_['text_order']             = 'سفارش‌‌‌‌‌‌ها';
-$_['text_processing_status'] = 'Processing';
-$_['text_complete_status']   = 'تکمیل';
-$_['text_customer']          = 'مشتری‌ها';
-$_['text_online']            = 'مشتری‌های برخط';
-$_['text_approval']          = 'در انتظار تایید';
-$_['text_product']           = 'کالاها';
-$_['text_stock']             = 'موجود نیست';
-$_['text_review']            = 'بررسی‌ها';
-$_['text_return']            = 'برگشتی‌ها';
-$_['text_affiliate']         = 'بازاریاب‌ها';
-$_['text_store']             = 'فروشگاه‌ها';
-$_['text_front']             = 'نمای فروشگاه';
-$_['text_help']              = 'راهنمایی';
-$_['text_homepage']          = 'OpenCart Homepage';
-$_['text_support']           = 'انجمن پشتیبانی';
-$_['text_documentation']     = 'مستندات';
-$_['text_logout']            = 'خروج';
+$_['text_heading']   = 'مدیریت';
+$_['text_login']     = 'لطفا اطلاعات ورود خود را وارد کنید.';
+$_['text_forgotten'] = 'فراموشی گذرواژه';
+
+// Entry
+$_['entry_username'] = 'نام کاربری';
+$_['entry_password'] = 'گذرواژه';
+
+// Button
+$_['button_login']   = 'ورود';
+
+// Error
+$_['error_login']    = 'نام کاربری و/یا گذرواژه تطبیق ندارند.';
+$_['error_token']    = 'نشانه جلسه نامعتبر است. لطفا دوباره وارد شوید.';
