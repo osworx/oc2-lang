27c929b9a183a8e646ffa6b86f859caa134896bc
OSWorX <info@osworx.net>
2025-03-16 09:10:18 +0100

    New translations login.php (Italian)

--- upload/language/it-IT/it-IT.php
+++ upload/language/it-IT/it-IT.php
@@ -1,23 +1,19 @@
 <?php
-// Heading
-$_['heading_title']          = 'OpenCart';
+// header
+$_['heading_title']  = 'Amministrazione';
 
 // Text
-$_['text_order']             = 'Ordini';
-$_['text_processing_status'] = 'In elaborazione';
-$_['text_complete_status']   = 'Completato';
-$_['text_customer']          = 'Clienti';
-$_['text_online']            = 'Clienti online';
-$_['text_approval']          = 'In attesa di approvazione';
-$_['text_product']           = 'Prodotti';
-$_['text_stock']             = 'Prodotto Esaurito';
-$_['text_review']            = 'Recensioni';
-$_['text_return']            = 'Ritorni';
-$_['text_affiliate']         = 'Affiliati';
-$_['text_store']             = 'Negozi';
-$_['text_front']             = 'Negozio Online';
-$_['text_help']              = 'Supporto';
-$_['text_homepage']          = 'OpenCart Homepage';
-$_['text_support']           = 'Forum di supporto';
-$_['text_documentation']     = 'Documentazione';
-$_['text_logout']            = 'Disconnettiti';
+$_['text_heading']   = 'Amministrazione';
+$_['text_login']     = 'Inserisci i tuoi dati di accesso.';
+$_['text_forgotten'] = 'Password dimenticata';
+
+// Entry
+$_['entry_username'] = 'Nome utente';
+$_['entry_password'] = 'Password';
+
+// Button
+$_['button_login']   = 'Effettuate L\'Accesso';
+
+// Error
+$_['error_login']    = 'Nessuna corrispondenza per Username e/o Password.';
+$_['error_token']    = 'Sessione token non valido. Effettua nuovamente il login.';
