4b4feb7cc5b2b03c5788b7f04fe6b9fbf23c210f
OSWorX <info@osworx.net>
2025-03-16 14:27:27 +0100

    New translations filemanager.php (French, Canada)

--- upload/admin/language/en-gb/common/fr-CA.php
+++ upload/admin/language/en-gb/common/fr-CA.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Tableau de bord';
+$_['heading_title']    = 'Gestionnaire d\'images';
+
+// Text
+$_['text_uploaded']    = 'Félicitations, votre fichier a correctement été transféré!';
+$_['text_directory']   = 'Le dossier vient d’être créé !';
+$_['text_delete']      = 'Votre fichier ou dossier a correctement été supprimé !';
+
+// Entry
+$_['entry_search']     = 'Recherche...';
+$_['entry_folder']     = 'Nom du dossier';
 
 // Error
-$_['error_install'] = 'Attention : le dossier <install> existe encore et doit être supprimé pour des raisons de sécurité !';
+$_['error_permission'] = 'Attention : accès refusé !';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Attention : un fichier ou dossier portant le même nom existe déjà !';
+$_['error_directory']  = 'Attention : le dossier n’existe pas !';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Attention : type de fichier incorrect !';
+$_['error_upload']     = 'Attention : le fichier n’a pas pu être transféré pour une raison inconnue !';
+$_['error_delete']     = 'Attention : vous ne pouvez pas supprimer ce dossier !';
