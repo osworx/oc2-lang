b22397e35bfab712f8e65da26d5bb3606681434f
OSWorX <info@osworx.net>
2025-03-16 09:10:48 +0100

    New translations footer.php (Georgian)

--- upload/language/ka-GE/ka-GE.php
+++ upload/language/ka-GE/ka-GE.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'გამოსახულების მენეჯერი';
-
 // Text
-$_['text_uploaded']    = 'წარმატება: თქვენი ფაილი აიტვირთა!';
-$_['text_directory']   = 'წარმატება: დირექტორია შექმნილია!';
-$_['text_delete']      = 'წარმატება: თქვენი ფაილი ან დირექტორია წაშლილია!';
-
-// Entry
-$_['entry_search']     = 'ძებნა..';
-$_['entry_folder']     = 'ფოლდერის სახელი';
-
-// Error
-$_['error_permission'] = 'გაფრთხილება: ნებართვა აკრძალულია!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'გაფრთხილება: ფაილი ან დირექტორია იგივე სახელით უკვე არსებობს!';
-$_['error_directory']  = 'გაფრთხილება: დირექტორია არ არსებობს!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'გაფრთხილება: არასწორი ფაილის ტიპი!';
-$_['error_upload']     = 'გაფრთხილება: ფაილი არ აიტვირთვა უცნობი მიზეზით!';
-$_['error_delete']     = 'გაფრთხილება: თქვენ არ შეგიძლიათ წაშალოთ ეს დირექტორია!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'ვერსია %s';
