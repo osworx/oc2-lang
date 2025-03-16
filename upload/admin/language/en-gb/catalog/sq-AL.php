4b5179af9dd46188485313978ac4239c6a491af0
OSWorX <info@osworx.net>
2025-03-16 11:08:45 +0100

    New translations filter.php (Albanian)

--- upload/admin/language/en-gb/catalog/sq-AL.php
+++ upload/admin/language/en-gb/catalog/sq-AL.php
@@ -1,35 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = 'Shkarkimet';
+$_['heading_title']     = 'Filtrat';
 
 // Text
-$_['text_success']      = 'Ju keni modifikuar me sukses shkarkimet!';
-$_['text_list']         = 'Lista e shkarkimeve';
-$_['text_add']          = 'Shto shkarkim';
-$_['text_edit']         = 'Redaktor shkarkim';
-$_['text_upload']       = 'Fajlli juaj është ngarkuar me sukses!';
+$_['text_success']      = 'Success: You have modified filters!';
+$_['text_list']         = 'Filter List';
+$_['text_add']          = 'Add Filter';
+$_['text_edit']         = 'Edit Filter';
 
 // Column
-$_['column_name']       = 'Emri i shkarkimit';
-$_['column_date_added'] = 'Data e shtuar';
+$_['column_group']      = 'Filter Group';
+$_['column_sort_order'] = 'Radhitja';
 $_['column_action']     = 'Veprimi';
 
 // Entry
-$_['entry_name']        = 'Emri i shkarkimit';
-$_['entry_filename']    = 'Emri i fajllit';
-$_['entry_mask']        = 'Maska';
-
-// Help
-$_['help_filename']     = 'Ju mund të ngarkoni përmes butonit të ngarkimit ose të përdorni FTP për të ngarkuar në drejtorinë e shkarkimit dhe të futni detajet më poshtë.';
-$_['help_mask']         = 'Rekomandohet që emri i skedarit dhe maska të jenë të ndryshme për të ndaluar njerëzit që përpiqen të lidhen drejtpërdrejt me shkarkimet tuaja.';
+$_['entry_group']       = 'Filter Group Name';
+$_['entry_name']        = 'Filter Name';
+$_['entry_sort_order']  = 'Radhitja';
 
 // Error
 $_['error_warning']     = 'Vërejtje: Ju lutem kontrolloni formën me kujdes për ndonjë gabim!';
-$_['error_permission']  = 'Vërejtje: Ju nuk keni leje për të modifikuar shkarkimet!';
-$_['error_name']        = 'Emri i shkarkimit duhet të jetë në mes 3 dhe 64 karaktereve!';
-$_['error_upload']      = 'Kërkohet ngarkimi!';
-$_['error_filename']    = 'Emri i fajllit duhet të jetë në mes 3 dhe 128 karaktereve!';
-$_['error_exists']      = 'Fajlli nuk ekziston!';
-$_['error_mask']        = 'Maska duhet të jetë në mes 3 dhe 128 karaktereve!';
-$_['error_filetype']    = 'Lloji invalid i fajllit!';
-$_['error_product']     = 'Vërejtje: Ky shkarkim nuk mund të fshihet sepse është i caktuar për produktet %s!';
+$_['error_permission']  = 'Warning: You do not have permission to modify filters!';
+$_['error_group']       = 'Filter Group Name must be between 1 and 64 characters!';
+$_['error_name']        = 'Filter Name must be between 1 and 64 characters!';
