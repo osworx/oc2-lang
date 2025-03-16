07aa252198765c17af6e2c32d7865c554226c634
OSWorX <info@osworx.net>
2025-03-16 09:11:54 +0100

    New translations footer.php (Norwegian)

--- upload/language/no-NO/no-NO.php
+++ upload/language/no-NO/no-NO.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Bildebehandler';
-
 // Text
-$_['text_uploaded']    = 'Filen ble lastet opp.';
-$_['text_directory']   = 'Mappe ble opprettet.';
-$_['text_delete']      = 'Filen eller mappen ble slettet.';
-
-// Entry
-$_['entry_search']     = 'Søk ...';
-$_['entry_folder']     = 'Mappenavn';
-
-// Error
-$_['error_permission'] = 'Tilgang nektet.';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'En fil eller mappe med samme navn finnes allerede.';
-$_['error_directory']  = 'Mappe finnes ikke.';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Ugyldig filtype.';
-$_['error_upload']     = 'Filen kunne ikke lastes opp av ukjent grunn.';
-$_['error_delete']     = 'Du kan ikke slette denne mappen.';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Versjon %s';
