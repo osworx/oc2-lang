33ed6bb9a1a388dac4d7b590ff397209fa38c992
OSWorX <info@osworx.net>
2025-03-16 14:20:18 +0100

    New translations information.php (Vietnamese)

--- upload/admin/language/en-gb/catalog/vi-VN.php
+++ upload/admin/language/en-gb/catalog/vi-VN.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'Bộ lọc';
+$_['heading_title']          = 'Thông tin';
 
 // Text
-$_['text_success']      = 'Kết quả: Bạn đã sửa xong bộ lọc!';
-$_['text_list']         = 'Danh sách bộ lọc';
-$_['text_add']          = 'Thêm bộ lọc';
-$_['text_edit']         = 'Sửa bộ lọc';
+$_['text_success']           = 'Kết quả: Bạn đã sửa xong thông tin!';
+$_['text_list']              = 'Danh sách thông tin';
+$_['text_add']               = 'Thêm thông tin';
+$_['text_edit']              = 'Sửa thông tin';
+$_['text_default']           = 'Mặc định';
 
 // Column
-$_['column_group']      = 'Nhóm bộ lọc';
-$_['column_sort_order'] = 'Thứ tự sắp xếp';
-$_['column_action']     = 'Tác vụ';
+$_['column_title']           = 'Tên loại thông tin';
+$_['column_sort_order']	     = 'Thứ tự sắp xếp';
+$_['column_action']          = 'Tác vụ';
 
 // Entry
-$_['entry_group']       = 'Tên nhóm bộ lọc';
-$_['entry_name']        = 'Tên bộ lọc';
-$_['entry_sort_order']  = 'Thứ tự sắp xếp';
+$_['entry_title']            = 'Tên loại thông tin';
+$_['entry_description']      = 'Mô tả';
+$_['entry_store']            = 'Gian hàng';
+$_['entry_meta_title'] 	     = 'Tên thẻ Meta';
+$_['entry_meta_keyword'] 	 = 'Từ khóa cho thẻ Meta';
+$_['entry_meta_description'] = 'Mô tả cho thẻ Meta';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Dưới cùng';
+$_['entry_status']           = 'Trạng thái';
+$_['entry_sort_order']       = 'Thứ tự sắp xếp';
+$_['entry_layout']           = 'Ghi đè layout';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Hiện dưới cuối trang.';
 
 // Error
-$_['error_warning']     = 'Chú ý: Vui lòng kiểm tra lỗi cẩn thận trong biểu mẫu!';
-$_['error_permission']  = 'Chú ý: Bạn không có quyền sửa các bộ lọc!';
-$_['error_group']       = 'Tên nhóm bộ lọc phải từ 1 tới 64 ký tự!';
-$_['error_name']        = 'Tên bộ lọc phải từ 1 tới 64 ký tự!';
+$_['error_warning']          = 'Chú ý: Vui lòng kiểm tra lỗi cẩn thận trong biểu mẫu!';
+$_['error_permission']       = 'Chú ý: Bạn không có quyền để sửa đổi thông tin!';
+$_['error_title']            = 'Tên của loại thông tin phải từ 3 tới 64 ký tự!';
+$_['error_description']      = 'Mô tả phải nhiều hơn 3 ký tự!';
+$_['error_meta_title']       = 'Tên thẻ Meta phải nhiều hơn 3 và ít hơn 255 ký tự!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'Chú ý: Trang thông tin này không thể bị xóa vì nó đang được gán cho các mục tài khoản lưu trữ!';
+$_['error_checkout']         = 'Chú ý: Trang thông tin này không thể bị xóa vì nó đang được gán cho các mục kiểm tra lưu trữ!';
+$_['error_affiliate']        = 'Chú ý: Trang thông tin này không thể bị xóa vì nó đang được gán cho các mục nhánh lưu trữ!';
+$_['error_return']           = 'Chú ý: Trang thông tin này không thể bị xóa vì nó đang được gán cho các mục thống kê lưu trữ!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
