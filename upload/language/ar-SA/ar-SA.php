f8ffcf6bb83ab634516924a0a007ac432fbae24e
OSWorX <info@osworx.net>
2025-03-16 09:06:47 +0100

    New translations recurring.php (Arabic)

--- upload/language/ar-SA/ar-SA.php
+++ upload/language/ar-SA/ar-SA.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'المنتجات';
+$_['heading_title']			= 'ملف الاستقطاع';
 
 // Text
-$_['text_success']           = 'مبروك: لقد قمت بتعديل المنتجات بنجاح!';
-$_['text_list']              = 'قائمة المنتجات';
-$_['text_add']               = 'إضافة منتج';
-$_['text_edit']              = 'تحرير المنتج';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'الافتراضي';
-$_['text_option']            = 'خيار';
-$_['text_option_value']      = 'قيمة الخيار';
-$_['text_percent']           = 'النسبة المئوية';
-$_['text_amount']            = 'المبلغ الثابت';
-
-// Column
-$_['column_name']            = 'اسم المنتج';
-$_['column_model']           = 'النوع';
-$_['column_image']           = 'صورة';
-$_['column_price']           = 'السعر';
-$_['column_quantity']        = 'الكمية';
-$_['column_status']          = 'الحالة';
-$_['column_action']          = 'عمل';
+$_['text_success']          = 'تم بنجاح: لقد قمت بتعديل ملفات الاستقطاع!';
+$_['text_list']             = 'قائمة ملفات الاستقطاع';
+$_['text_add']              = 'أضف ملف إستقطاع';
+$_['text_edit']             = 'تحرير ملف إستقطاع';
+$_['text_day']				= 'يوم';
+$_['text_week']				= 'أسبوع';
+$_['text_semi_month']		= 'نصف شهر';
+$_['text_month']			= 'شهر';
+$_['text_year']				= 'سنة';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> تحسب مبالغ الإستقطاع من تكرار الدورات.</p><p>على سبيل المثال، إذا كنت تستخدم التكرار "أسبوع" و الدورة "2"، اذاً سيتم السحب كل اسبوعين.</p><p>الفترة هي عدد المرات التي سوف تجعل المستخدم يدفع، و إذا كنت ترغب في الدفع حتى يتم الإلغاء يتم تعيين اقيمتها إلى صفر.</p>';
+$_['text_profile']			= 'ملف الاستقطاع';
+$_['text_trial']			= 'ملف التجربة';
 
 // Entry
-$_['entry_name']             = 'اسم المنتج';
-$_['entry_description']      = 'الوصف';
-$_['entry_meta_title'] 	     = 'عنوان Meta-Tag';
-$_['entry_meta_keyword'] 	 = 'الكلمات الوصفية "Meta Tag Keywords"';
-$_['entry_meta_description'] = 'وصف العلامة الوصفية "Meta Tag Description"';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'النوع';
-$_['entry_sku']              = 'عدد الوحدات بالمخزون (SKU)';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'الرقم الدولي للكتاب (ISBN)';
-$_['entry_mpn']              = 'رقم القطعة في المصنع (MPN)';
-$_['entry_location']         = 'الموقع';
-$_['entry_shipping']         = 'يتطلب الشحن';
-$_['entry_manufacturer']     = 'الشركة المصنعة';
-$_['entry_store']            = 'المتاجر';
-$_['entry_date_available']   = 'تاريخ التوفر';
-$_['entry_quantity']         = 'الكمية';
-$_['entry_minimum']          = 'الحد الأدنى للكمية';
-$_['entry_stock_status']     = 'حالة نفاذ المخزون';
-$_['entry_price']            = 'السعر';
-$_['entry_tax_class']        = 'فئة ضريبة';
-$_['entry_points']           = 'نقاط';
-$_['entry_option_points']    = 'نقاط';
-$_['entry_subtract']         = 'الخصم من المخزون';
-$_['entry_weight_class']     = 'فئة الوزن';
-$_['entry_weight']           = 'الوزن';
-$_['entry_dimension']        = 'الأبعاد (الطول × العرض × الارتفاع)';
-$_['entry_length_class']     = 'فئة الطول';
-$_['entry_length']           = 'الطول';
-$_['entry_width']            = 'عرض';
-$_['entry_height']           = 'الارتفاع';
-$_['entry_image']            = 'صورة';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'مجموعة العميل';
-$_['entry_date_start']       = 'تاريخ البدء';
-$_['entry_date_end']         = 'تاريخ الانتهاء';
-$_['entry_priority']         = 'الأولوية';
-$_['entry_attribute']        = 'الخصائص';
-$_['entry_attribute_group']  = 'مجموعة الخصائص';
-$_['entry_text']             = 'نص';
-$_['entry_option']           = 'خيار';
-$_['entry_option_value']     = 'قيمة الخيار';
-$_['entry_required']         = 'مطلوب';
-$_['entry_status']           = 'الحالة';
-$_['entry_sort_order']       = 'الترتيب';
-$_['entry_category']         = 'الأقسام';
-$_['entry_filter']           = 'ترشيحات/تصفيات';
-$_['entry_download']         = 'التنزيلات';
-$_['entry_related']          = 'منتجات ذات الصلة';
-$_['entry_tag']          	 = 'الكلمات الدليلة للمنتج';
-$_['entry_reward']           = 'نقاط المكافأة';
-$_['entry_layout']           = 'تجاوز التخطيط';
-$_['entry_recurring']        = 'ملف الاستقطاع';
+$_['entry_name']		    = 'الاسم';
+$_['entry_price']			= 'السعر';
+$_['entry_duration']		= 'الفترة';
+$_['entry_cycle']			= 'الدورة';
+$_['entry_frequency']		= 'التكرار';
+$_['entry_trial_price']		= 'سعر التجربة';
+$_['entry_trial_duration']	= 'فترت التجربة';
+$_['entry_trial_status']	= 'حالة التجربة';
+$_['entry_trial_cycle']	    = 'دورة التجربة';
+$_['entry_trial_frequency'] = 'تكرار التجربة';
+$_['entry_status']			= 'الحالة';
+$_['entry_sort_order']		= 'الترتيب';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'وحدة حفظ المخزون';
-$_['help_upc']               = 'رمز المنتج العالمي';
-$_['help_ean']               = 'رقم المقالة الأوروبية';
-$_['help_jan']               = 'رقم المقالة اليابانية';
-$_['help_isbn']              = 'الرقم الدولي للكتاب';
-$_['help_mpn']               = 'رقم القطعة في المصنع';
-$_['help_manufacturer']      = '(إكمال تلقائي)';
-$_['help_minimum']           = 'فرض حد أدنى لكمية الطلب';
-$_['help_stock_status']      = 'الحالة التي تظهر عندما يكون المنتج نافذ من المخزون';
-$_['help_points']            = 'عدد نقاط المكافئة لشراء هذا العنصر. إذا كنت لا تريد تمكين شراء هذا المنتج عن طريق النقاط يجب تركه على 0.';
-$_['help_category']          = '(إكمال تلقائي)';
-$_['help_filter']            = '(إكمال تلقائي)';
-$_['help_download']          = '(إكمال تلقائي)';
-$_['help_related']           = '(إكمال تلقائي)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'الاسم';
+$_['column_sort_order']	    = 'الترتيب';
+$_['column_action']         = 'عمل';
 
 // Error
-$_['error_warning']          = 'تحذير: الرجاء التحقق من النموذج واصلاح الأخطاء!';
-$_['error_permission']       = 'تحذير: ليس لديك الصلاحية للتعديل على المنتجات!';
-$_['error_name']             = 'اسم المنتج يجب أن يكون أطول من 3 وأقل من 255 حرفاً!';
-$_['error_meta_title']       = 'يجب أن لا يقل عدد حروف العنوان عن 3حروف ولا يزيد عن 255 حرف!';
-$_['error_model']            = 'نوع المنتج يجب أن يكون أطول من 3 وأقل من 64 حرفاً!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'تحذير: الرجاء التحقق من النموذج واصلاح الأخطاء!';
+$_['error_permission']		= 'تحذير: ليس لديك الصلاحية للتعديل على ملف الاستقطاع!';
+$_['error_name']			= 'اسم الملف يجب أن يكون أطول من 3 وأقل من 255 حرفاً!';
+$_['error_product']			= 'تحذير: لا يمكن حذف ملف الاستقطاع لتعيينة على %s منتجات!';
