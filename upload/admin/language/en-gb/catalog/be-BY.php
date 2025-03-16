48cfd771215ee6db7ac76049e381d7cf80f4bd2e
OSWorX <info@osworx.net>
2025-03-16 11:13:33 +0100

    New translations review.php (Belarusian)

--- upload/admin/language/en-gb/catalog/be-BY.php
+++ upload/admin/language/en-gb/catalog/be-BY.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'Перыядычныя плацяжы';
+$_['heading_title']     = 'Агляды';
 
 // Text
-$_['text_success']          = 'Success: You have modified recurring profiles!';
-$_['text_list']             = 'Recurring Profile List';
-$_['text_add']              = 'Add Recurring Profile';
-$_['text_edit']             = 'Edit Recurring Profile';
-$_['text_day']				= 'Дзень';
-$_['text_week']				= 'Тыдзень';
-$_['text_semi_month']		= '15 дзен';
-$_['text_month']			= 'Месяц';
-$_['text_year']				= 'Год';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Recurring amounts are calculated by the frequency and cycles.</p><p>For example if you use a frequency of "week" and a cycle of "2", then the user will be billed every 2 weeks.</p><p>The duration is the number of times the user will make a payment, set this to 0 if you want payments until they are cancelled.</p>';
-$_['text_profile']			= 'Перыядычныя плацяжы';
-$_['text_trial']			= 'Trial Profile';
-
-// Entry
-$_['entry_name']		    = 'Назва';
-$_['entry_price']			= 'Цана';
-$_['entry_duration']		= 'Працягласць';
-$_['entry_cycle']			= 'Цыкл';
-$_['entry_frequency']		= 'Частата';
-$_['entry_trial_price']		= 'Trial price';
-$_['entry_trial_duration']	= 'Trial duration';
-$_['entry_trial_status']	= 'Trial status';
-$_['entry_trial_cycle']	    = 'Trial cycle';
-$_['entry_trial_frequency'] = 'Trial frequency';
-$_['entry_status']			= 'Статус';
-$_['entry_sort_order']		= 'Парадак сартавання';
+$_['text_success']      = 'Поспех: Агляды змененыя!';
+$_['text_list']         = 'Спіс аглядаў';
+$_['text_add']          = 'Дадаць агляд';
+$_['text_edit']         = 'Рэдагаваць агляд';
 
 // Column
-$_['column_name']			= 'Назва';
-$_['column_sort_order']	    = 'Парадак сартавання';
-$_['column_action']         = 'Дзеянне';
+$_['column_product']    = 'Тавар';
+$_['column_author']     = 'Аўтар';
+$_['column_rating']     = 'Рэйтынг';
+$_['column_status']     = 'Статус';
+$_['column_date_added'] = 'Дададзена';
+$_['column_action']     = 'Дзеянне';
+
+// Entry
+$_['entry_product']     = 'Тавар';
+$_['entry_author']      = 'Аўтар';
+$_['entry_rating']      = 'Рэйтынг';
+$_['entry_status']      = 'Статус';
+$_['entry_text']        = 'Text';
+$_['entry_date_added']  = 'Дададзена';
+
+// Help
+$_['help_product']      = '(Аўтазавяршэнне)';
 
 // Error
-$_['error_warning']         = 'Увага: Калі ласка, ўважліва праверце ўведзеные дадзеныя!';
-$_['error_permission']		= 'Warning: You do not have permission to modify recurring profiles!';
-$_['error_name']			= 'Назва профілю павінна быць ад 3 да 255 сімвалаў!';
-$_['error_product']			= 'Warning: This recurring profile cannot be deleted as it is currently assigned to %s products!';
+$_['error_warning']     = 'Увага: Калі ласка, ўважліва праверце ўведзеные дадзеныя!';
+$_['error_permission']  = 'Увага: Вы не можаце змяняць агляды!';
+$_['error_product']     = 'Патрэбна ўказаць прадукт!';
+$_['error_author']      = 'Аўтар павінен мець ад 3 да 64 сімвалаў!';
+$_['error_text']        = 'Тэкст агляду павінен быць не меньш за 1 сімвал!';
+$_['error_rating']      = 'Патрэбна ўказаць рэйтынг агляду!';
