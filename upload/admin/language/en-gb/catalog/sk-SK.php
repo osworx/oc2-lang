b113c61840b5b8ccb4d4edf6ae5a7aef3c9c1878
OSWorX <info@osworx.net>
2025-03-16 14:17:45 +0100

    New translations category.php (Slovak)

--- upload/admin/language/en-gb/catalog/sk-SK.php
+++ upload/admin/language/en-gb/catalog/sk-SK.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Skupiny vlastností';
+$_['heading_title']          = 'Kategórie';
 
 // Text
-$_['text_success']      = 'Hotovo: Upravili ste skupinu vlastností!';
-$_['text_list']         = 'Zoznam skupín vlastností';
-$_['text_add']          = 'Pridať skupinu vlastností';
-$_['text_edit']         = 'Upraviť skupinu vlastností';
+$_['text_success']           = 'Hotovo: Upravili ste kategórie!';
+$_['text_list']              = 'Zoznam kategórií';
+$_['text_add']               = 'Pridať kategóriu';
+$_['text_edit']              = 'Upraviť kategóriu';
+$_['text_default']           = 'Pôvodné';
 
 // Column
-$_['column_name']       = 'Názov skupiny vlastností';
-$_['column_sort_order'] = 'Poradie';
-$_['column_action']     = 'Akcia';
+$_['column_name']            = 'Názov kategórie';
+$_['column_sort_order']      = 'Poradie';
+$_['column_action']          = 'Akcia';
 
 // Entry
-$_['entry_name']        = 'Názov skupiny vlastností';
-$_['entry_sort_order']  = 'Poradie';
+$_['entry_name']             = 'Názov kategórie';
+$_['entry_description']      = 'Popis';
+$_['entry_meta_title'] 	     = 'Meta Tag Title (nadpis)';
+$_['entry_meta_keyword']     = 'Meta Tag Kľúčové slová';
+$_['entry_meta_description'] = 'Meta Tag Popis';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Nadradená kategória';
+$_['entry_filter']           = 'Filtre';
+$_['entry_store']            = 'Obchody';
+$_['entry_image']            = 'Obrázok';
+$_['entry_top']              = 'Zobraziť v menu';
+$_['entry_column']           = 'Stĺpce';
+$_['entry_sort_order']       = 'Poradie';
+$_['entry_status']           = 'Stav';
+$_['entry_layout']           = 'Šablóna';
+
+// Help
+$_['help_filter']            = '(Automatické dokončovanie)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Zobraziť v hornom menu. Funguje len pre najvyššie kategórie.';
+$_['help_column']            = 'Počet stĺpcov pre 3 podkategórie. Funguje len pre najvyššie nadradené kategórie.';
 
 // Error
-$_['error_warning']     = 'Varovanie: Skontrolujte formulár starostlivo kvôli chybám!';
-$_['error_permission']  = 'Varovanie: Nemáte potrebné oprávnenia na upravenie skupiny vlastností!';
-$_['error_name']        = 'Názov skupiny vlastností musí byť v rozmedzí 3 až 64 znakov!';
-$_['error_attribute']   = 'Varovanie: Táto skupina vlastností nemôže byť zmazaná! Je priradená k %s vlastnostiam!';
-$_['error_product']     = 'Varovanie: Táto skupina vlastností nemôže byť zmazaná! Je priradená k %s produktom!';
+$_['error_warning']          = 'Varovanie: Skontrolujte formulár starostlivo kvôli chybám!';
+$_['error_permission']       = 'Varovanie: Nemáte potrebné oprávnenie na upravenie kategórie!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Meta Tag Title (nadpis) musí mať aspoň 3 a menej ako 255 znakov!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
