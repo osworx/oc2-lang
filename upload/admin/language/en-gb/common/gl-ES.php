2870c6824103153868db6448f15acde6092c1424
OSWorX <info@osworx.net>
2025-03-16 14:28:02 +0100

    New translations footer.php (Galician)

--- upload/admin/language/en-gb/common/gl-ES.php
+++ upload/admin/language/en-gb/common/gl-ES.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Xestor de imaxes';
-
 // Text
-$_['text_uploaded']    = 'Actualizaronse os ficheiros correctamente!';
-$_['text_directory']   = 'Novo directorio creado correctamente!';
-$_['text_delete']      = 'O ficheiro ou directorio eliminouse correctamente!';
-
-// Entry
-$_['entry_search']     = 'Buscar..';
-$_['entry_folder']     = 'Nome do cartafol';
-
-// Error
-$_['error_permission'] = 'Aviso: Permiso denegado!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Aviso: O ficheiro ou directorio con ese nome xa existe!';
-$_['error_directory']  = 'Aviso: O directorio non existe!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Aviso: Tipo de ficheiro incorrecto!';
-$_['error_upload']     = 'Aviso: O ficheiro non pudo ser actualizado por razóns desconocidas!';
-$_['error_delete']     = 'Aviso: Non pode eliminar este directorio!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Versión %s';
