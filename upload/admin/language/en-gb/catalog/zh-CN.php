5b729745afd9081549c67ce47a6fb6eb525defbc
OSWorX <info@osworx.net>
2025-03-16 14:19:28 +0100

    New translations filter.php (Chinese Simplified)

--- upload/admin/language/en-gb/catalog/zh-CN.php
+++ upload/admin/language/en-gb/catalog/zh-CN.php
@@ -1,35 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = '下载';
+$_['heading_title']     = '筛选器';
 
 // Text
-$_['text_success']      = '成功： 您修改了下载资源 ！';
-$_['text_list']         = '下载列表';
-$_['text_add']          = '添加下载资源';
-$_['text_edit']         = '编辑下载资源';
-$_['text_upload']       = '已成功上传文件 ！';
+$_['text_success']      = '您已经成功修改筛选器!';
+$_['text_list']         = '筛选列表';
+$_['text_add']          = '新增筛选';
+$_['text_edit']         = '编辑筛选';
 
 // Column
-$_['column_name']       = '下载资源名称';
-$_['column_date_added'] = '添加日期';
+$_['column_group']      = '筛选组';
+$_['column_sort_order'] = '排列顺序';
 $_['column_action']     = '操作';
 
 // Entry
-$_['entry_name']        = '下载资源名称';
-$_['entry_filename']    = '文件名';
-$_['entry_mask']        = '虚拟资源名称';
-
-// Help
-$_['help_filename']     = '你可以通过上传按钮上传或使用 FTP 上传到下载目录并在下面输入详细描述信息。';
-$_['help_mask']         = '建议的真实文件名和虚拟文件名不同，阻止人们试图直接链接到您的下载资源。';
+$_['entry_group']       = '筛选组名称';
+$_['entry_name']        = '筛选名称';
+$_['entry_sort_order']  = '排列顺序';
 
 // Error
 $_['error_warning']     = '警告:请仔细检查错误格式!';
-$_['error_permission']  = '警告: 您没有权限修改下载资源 ！';
-$_['error_name']        = '下载名称必须是 3 和 64 个字符之间 ！';
-$_['error_upload']      = '上传资源必填 ！';
-$_['error_filename']    = '文件名必须介于 3 到 128 个字符之间 ！';
-$_['error_exists']      = '文件不存在！';
-$_['error_mask']        = '虚拟名称必须是 3 和 128 个字符之间 ！';
-$_['error_filetype']    = '不允许的文件类型。';
-$_['error_product']     = '警告： 不能删除此下载资源，因为它目前已分配给 %s 的产品 ！';
+$_['error_permission']  = '警告:您没有权限修改此筛选器!';
+$_['error_group']       = '筛选组名称必须介于1-64个字符之间!';
+$_['error_name']        = '筛选名称必须介于1-64个字符之间!';
