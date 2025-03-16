f043dbb2103b9d8a6a956d8d7d70fb6dce383fe9
OSWorX <info@osworx.net>
2025-03-16 14:16:58 +0100

    New translations attribute_group.php (Albanian)

--- upload/admin/language/en-gb/catalog/sq-AL.php
+++ upload/admin/language/en-gb/catalog/sq-AL.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Atributet';
+$_['heading_title']     = 'Grupet e Atributit';
 
 // Text
-$_['text_success']           = 'Suksess: Ju keni mdifikuar atributet!';
-$_['text_list']              = 'Lista e atributeve';
-$_['text_add']               = 'Shto Atribut';
-$_['text_edit']              = 'Modifiko Atribut';
+$_['text_success']      = 'Ju keni modifikuar grupet e atributeve me sukses!';
+$_['text_list']         = 'Lista e grupeve të atributeve';
+$_['text_add']          = 'Shto grupin e atributeve';
+$_['text_edit']         = 'Modifiko grupin e atributeve';
 
 // Column
-$_['column_name']            = 'Emri i Atributit';
-$_['column_attribute_group'] = 'Grupi i Atributit';
-$_['column_sort_order']      = 'Radhitja';
-$_['column_action']          = 'Veprimi';
+$_['column_name']       = 'Emri i grupit të atributeve';
+$_['column_sort_order'] = 'Radhitja';
+$_['column_action']     = 'Veprimi';
 
 // Entry
-$_['entry_name']             = 'Emri i Atributit';
-$_['entry_attribute_group']  = 'Grupi i Atributit';
-$_['entry_sort_order']       = 'Radhitja';
+$_['entry_name']        = 'Emri i grupit të atributeve';
+$_['entry_sort_order']  = 'Radhitja';
 
 // Error
-$_['error_warning']          = 'Vërejtje: Ju lutem kontrolloni formën me kujdes për ndonjë gabim!';
-$_['error_permission']       = 'Kujdes: Ju nuk keni leje të modifikoni atributet!';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'Emri i Atributit duhet të jete nga 3 deri në 64 shkronja!';
-$_['error_product']          = 'Nuk mund te fshihet pasi eshte percaktuar produktit %s!';
+$_['error_warning']     = 'Vërejtje: Ju lutem kontrolloni formën me kujdes për ndonjë gabim!';
+$_['error_permission']  = 'Vërejtje: Ju nuk keni autorizim për të modifikuar grupin e atributeve!';
+$_['error_name']        = 'Emri i grupit të atributeve duhet të jetë nga 3 deri në 64 karaktere!';
+$_['error_attribute']   = 'Vërejtje: Ky grup i atributeve nuk mund të fshihet sepse është i lirdhur aktualisht me atributet e %s!';
+$_['error_product']     = 'Vërejtje: Ky grup i atributeve nuk mund të fshihet sepse është i lirdhur aktualisht me produktet e %s!';
