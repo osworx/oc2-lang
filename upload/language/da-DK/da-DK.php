c13498ef82400508fe69e051e7916d50046cd57a
OSWorX <info@osworx.net>
2025-03-16 09:08:17 +0100

    New translations filemanager.php (Danish)

--- upload/language/da-DK/da-DK.php
+++ upload/language/da-DK/da-DK.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Oversigtspanel';
+$_['heading_title']    = 'Billedhåndtering';
+
+// Text
+$_['text_uploaded']    = 'Succes: Din fil er blevet uploadet!';
+$_['text_directory']   = 'Succes: Mappe oprettet!';
+$_['text_delete']      = 'Succes: Din fil eller mappe er blevet slettet!';
+
+// Entry
+$_['entry_search']     = 'Søg ...';
+$_['entry_folder']     = 'Mappenavn';
 
 // Error
-$_['error_install'] = 'Advarsel: "Install"-mappen stadig findes og bør slettes af sikkerhedsmæssige årsager!';
+$_['error_permission'] = 'Fejl: Adgang nægtet!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Advarsel: En fil eller mappe med samme navn findes i forvejen!';
+$_['error_directory']  = 'Fejl: Mappe findes ikke!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Fejl: Ugyldig filtype!';
+$_['error_upload']     = 'Advarsel: Filen kunne ikke uploades af en ukendt årsag!';
+$_['error_delete']     = 'Fejl: Du kan ikke slette denne mappe!';
