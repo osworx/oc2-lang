6710046b133f254a27e4b55c1d8d2a7359e8af8b
OSWorX <info@osworx.net>
2025-03-16 09:12:10 +0100

    New translations filemanager.php (Polish)

--- upload/language/pl-PL/pl-PL.php
+++ upload/language/pl-PL/pl-PL.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Panel administracyjny';
+$_['heading_title']    = 'Zarządzanie zdjęciami';
+
+// Text
+$_['text_uploaded']    = 'Sukces: Plik został przesłany!';
+$_['text_directory']   = 'Sukces: Katalog utworzony!';
+$_['text_delete']      = 'Sukces: Plik lub katalog został usunięty!';
+
+// Entry
+$_['entry_search']     = 'Wyszukaj..';
+$_['entry_folder']     = 'Nazwa folderu';
 
 // Error
-$_['error_install'] = 'Uwaga: Folder instalacji nadal istnieje i powinien zostać usunięty ze względów bezpieczeństwa!';
+$_['error_permission'] = 'Ostrzeżenie: Odmowa uprawnień!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Uwaga: Plik lub katalog o takiej nazwie już istnieje!';
+$_['error_directory']  = 'Ostrzeżenie: Katalog nie istnieje!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Ostrzeżenie: Nieprawidłowy typ pliku!';
+$_['error_upload']     = 'Ostrzeżenie: Nie można przesłać pliku z nieznanego powodu!';
+$_['error_delete']     = 'Ostrzeżenie: Nie można usunąć tego katalogu!';
