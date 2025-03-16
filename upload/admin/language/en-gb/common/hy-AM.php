9c4a8b1a9a92378a7d0b460d32fa6a9ce97a28f4
OSWorX <info@osworx.net>
2025-03-16 14:27:43 +0100

    New translations footer.php (Armenian)

--- upload/admin/language/en-gb/common/hy-AM.php
+++ upload/admin/language/en-gb/common/hy-AM.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Պատկերի Մենեջեր';
-
 // Text
-$_['text_uploaded']    = 'Ձեր ֆայլը վերբեռնված է!';
-$_['text_directory']   = 'Դիրեկտորիան ստեղծված է!';
-$_['text_delete']      = 'Ֆայլը կամ դիրեկտորիան ջնջված է!';
-
-// Entry
-$_['entry_search']     = 'Որոնել..';
-$_['entry_folder']     = 'Թղթապանակի անուն';
-
-// Error
-$_['error_permission'] = 'Թույլտվությունը մերժված է!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Նույն անունով ֆայլ կամ դիրեկտորիա արդեն գոյություն ունի!';
-$_['error_directory']  = 'Զգուշացում։Դիրեկտորիան գոյություն չունի!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Զգուշացում։Սխալ ֆայլի տեսակ!';
-$_['error_upload']     = 'Զգուշացում։Ֆայլը չի կարող վերբեռնվել անհայտ պատճառներով!';
-$_['error_delete']     = 'Զգուշացում։Չեք կարող ջնջել այս դիրեկտորիան!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Տարբերակ՝ %s';
