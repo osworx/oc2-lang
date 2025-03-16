2fce0a3250bc97f04f9243920927e5313c86cbee
OSWorX <info@osworx.net>
2025-03-16 14:23:27 +0100

    New translations recurring.php (Chinese Traditional)

--- upload/admin/language/en-gb/catalog/zh-TW.php
+++ upload/admin/language/en-gb/catalog/zh-TW.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = '商品';
+$_['heading_title']			= '定期設定檔';
 
 // Text
-$_['text_success']           = '成功：您已經修改商品！';
-$_['text_list']              = '商品清單';
-$_['text_add']               = '新增商品';
-$_['text_edit']              = '編輯商品';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = '預設';
-$_['text_option']            = '選項';
-$_['text_option_value']      = '選項值';
-$_['text_percent']           = '百分比';
-$_['text_amount']            = '固定金額';
-
-// Column
-$_['column_name']            = '商品名稱';
-$_['column_model']           = '型號';
-$_['column_image']           = '圖像';
-$_['column_price']           = '價格';
-$_['column_quantity']        = '數量';
-$_['column_status']          = '狀態';
-$_['column_action']          = '動作';
+$_['text_success']          = '成功：您已經修改分期設定檔！';
+$_['text_list']             = '分期設定檔清單';
+$_['text_add']              = '新增分期設定檔';
+$_['text_edit']             = '編輯分期設定檔';
+$_['text_day']				= '日';
+$_['text_week']				= '週';
+$_['text_semi_month']		= '半個月';
+$_['text_month']			= '月';
+$_['text_year']				= '年';
+$_['text_recurring']	    = '<p> <i class="fa fa-info-circle"> </i> 定期金額由頻率和週期計算. </p><p> 例如, 如果您使用 "周" 的頻率和 "2" 的週期, 則使用者將每2周計費一次. </p><p> 持續時間是使用者進行付款的次數, 如果您希望在取消之前付款, 則將其設置為 0. </p>';
+$_['text_profile']			= '分期設定';
+$_['text_trial']			= '試用簡介';
 
 // Entry
-$_['entry_name']             = '商品名稱';
-$_['entry_description']      = '說明';
-$_['entry_meta_title'] 	     = 'Meta 標籤標題';
-$_['entry_meta_keyword'] 	 = 'Meta 標籤關鍵字';
-$_['entry_meta_description'] = 'Meta 標籤描述';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = '型號';
-$_['entry_sku']              = 'SKU';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = '製造國';
-$_['entry_shipping']         = '需要運送';
-$_['entry_manufacturer']     = '製造商';
-$_['entry_store']            = '商店';
-$_['entry_date_available']   = '可用日期';
-$_['entry_quantity']         = '數量';
-$_['entry_minimum']          = '最低數量';
-$_['entry_stock_status']     = '缺貨狀態';
-$_['entry_price']            = '價格';
-$_['entry_tax_class']        = '稅別';
-$_['entry_points']           = '點數';
-$_['entry_option_points']    = '點數';
-$_['entry_subtract']         = '計算庫存';
-$_['entry_weight_class']     = '重量類別';
-$_['entry_weight']           = '重量';
-$_['entry_dimension']        = '尺寸 (長 x 寬 x 高)';
-$_['entry_length_class']     = '長度類別';
-$_['entry_length']           = '長度';
-$_['entry_width']            = '寬度';
-$_['entry_height']           = '高度';
-$_['entry_image']            = '圖像';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = '客戶群組';
-$_['entry_date_start']       = '開始日期';
-$_['entry_date_end']         = '結束日期';
-$_['entry_priority']         = '優先順序';
-$_['entry_attribute']        = '屬性';
-$_['entry_attribute_group']  = '屬性群組';
-$_['entry_text']             = '文字';
-$_['entry_option']           = '選項';
-$_['entry_option_value']     = '選項值';
-$_['entry_required']         = '必要項目';
-$_['entry_status']           = '狀態';
-$_['entry_sort_order']       = '排序次序';
-$_['entry_category']         = '類別';
-$_['entry_filter']           = '篩選條件';
-$_['entry_download']         = '下載';
-$_['entry_related']          = '相關商品';
-$_['entry_tag']          	 = '商品標籤';
-$_['entry_reward']           = '紅利點數';
-$_['entry_layout']           = '覆蓋版面配置';
-$_['entry_recurring']        = '分期設定';
+$_['entry_name']		    = '名稱';
+$_['entry_price']			= '價格';
+$_['entry_duration']		= '長度';
+$_['entry_cycle']			= '週期';
+$_['entry_frequency']		= '頻率';
+$_['entry_trial_price']		= '試用價格';
+$_['entry_trial_duration']	= '試用時間';
+$_['entry_trial_status']	= '試用狀態';
+$_['entry_trial_cycle']	    = '試驗週期';
+$_['entry_trial_frequency'] = '試用頻率';
+$_['entry_status']			= '狀態';
+$_['entry_sort_order']		= '排序次序';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = '庫存單位';
-$_['help_upc']               = '通用商品碼';
-$_['help_ean']               = '歐洲通用商品碼';
-$_['help_jan']               = '日本通用商品碼';
-$_['help_isbn']              = '國際標準書號';
-$_['help_mpn']               = '製造商零件編號';
-$_['help_manufacturer']      = '(自動完成)';
-$_['help_minimum']           = '強制最低購買數量。';
-$_['help_stock_status']      = '當商品缺貨時，顯示的狀態。';
-$_['help_points']            = 'Number of points needed to buy this item. If you don\'t want this product to be purchased with points leave as 0.';
-$_['help_category']          = '(自動完成)';
-$_['help_filter']            = '(自動完成)';
-$_['help_download']          = '(自動完成)';
-$_['help_related']           = '(自動完成)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= '名稱';
+$_['column_sort_order']	    = '排序次序';
+$_['column_action']         = '動作';
 
 // Error
-$_['error_warning']          = '警告：請仔細檢查表格的錯誤！';
-$_['error_permission']       = '警告：您沒有修改商品的權限！';
-$_['error_name']             = '商品名稱必須大於 3 且小於 255 個字元！';
-$_['error_meta_title']       = 'Meta 標題必須大於 3 且小於 255 個字元之間！';
-$_['error_model']            = '商品型號必須大於 1 且小於 64 個字元！';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = '警告：請仔細檢查表格的錯誤！';
+$_['error_permission']		= '警告：您沒有修改分期設定檔的權限！';
+$_['error_name']			= '設定檔名稱必須大於 3 且小於 255 個字元之間！';
+$_['error_product']			= '警告：無法刪除此分期設定檔，因為目前已指派給 %s 個商品！';
