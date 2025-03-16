5f2693c5633958908f57c439833921a62308ebca
OSWorX <info@osworx.net>
2025-03-16 14:16:15 +0100

    New translations attribute.php (Chinese Simplified)

--- upload/admin/language/en-gb/catalog/zh-CN.php
+++ upload/admin/language/en-gb/catalog/zh-CN.php
@@ -1,36 +1,27 @@
 <?php
 // Heading
-$_['heading_title']     = '商品点评';
+$_['heading_title']          = '属性';
 
 // Text
-$_['text_success']      = '修改成功: 您已经修改了评论.';
-$_['text_list']         = '评论列表';
-$_['text_add']          = '我要点评';
-$_['text_edit']         = '编辑评论';
+$_['text_success']           = '修改属性成功!';
+$_['text_list']              = '属性列表';
+$_['text_add']               = '添加属性';
+$_['text_edit']              = '编辑属性';
 
 // Column
-$_['column_product']    = '产品列表';
-$_['column_author']     = '点评人列表';
-$_['column_rating']     = '等级分';
-$_['column_status']     = '状态';
-$_['column_date_added'] = '添加日期';
-$_['column_action']     = '操作';
+$_['column_name']            = '属性名称';
+$_['column_attribute_group'] = '属性分组';
+$_['column_sort_order']      = '排列顺序';
+$_['column_action']          = '操作';
 
 // Entry
-$_['entry_product']     = '产品列表';
-$_['entry_author']      = '点评人列表';
-$_['entry_rating']      = '等级分';
-$_['entry_status']      = '状态';
-$_['entry_text']        = '文本框';
-$_['entry_date_added']  = '添加日期';
-
-// Help
-$_['help_product']      = '自动完成';
+$_['entry_name']             = '属性名称';
+$_['entry_attribute_group']  = '属性分组';
+$_['entry_sort_order']       = '排列顺序';
 
 // Error
-$_['error_warning']     = '警告:请仔细检查错误格式!';
-$_['error_permission']  = '提示: 您没有权限修改评论!';
-$_['error_product']     = '产品错误! 请选择要评论的产品.';
-$_['error_author']      = '点评人名称长度为3到64个字符长度';
-$_['error_text']        = '请至少输入一个字符!';
-$_['error_rating']      = '请选择一个评分等级!';
+$_['error_warning']          = '警告:请仔细检查错误格式!';
+$_['error_permission']       = '警告: 您没有修改属性的权限！';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = '属性名称必须介于 3 至 64 个字符之间 ！';
+$_['error_product']          = '警告： 无法删除此属性，因其已经分配给 %s 产品 ！';
