da14580e8bf7003f8718950a9792f4ce1286c737
OSWorX <info@osworx.net>
2025-03-16 14:28:28 +0100

    New translations forgotten.php (Finnish)

--- upload/admin/language/en-gb/common/fi-FI.php
+++ upload/admin/language/en-gb/common/fi-FI.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Unohditko salasanasi?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Versio %s';
+$_['text_forgotten']  = 'Unohtunut salasana';
+$_['text_your_email'] = 'Sähköpostiosoitteesi';
+$_['text_email']      = 'Kirjoita tilisi sähköpostiosoite. Paina lähetä vastaanottaaksesi salasanan palautuslinkin sähköpostiisi.';
+$_['text_success']    = 'Vahvistuslinkin sisältävä sähköposti on lähetetty ylläpitäjän sähköpostiosoitteeseen.';
+
+// Entry
+$_['entry_email']     = 'Sähköpostiosoite';
+$_['entry_password']  = 'Uusi salasana';
+$_['entry_confirm']   = 'Vahvista';
+
+// Error
+$_['error_email']     = 'Varoitus: sähköpostiosoitetta ei löydy tiedoistamme, yritä uudelleen!';
+$_['error_password']  = 'Salasanan tulee olla 4 - 20 merkkiä pitkä!';
+$_['error_confirm']   = 'Salasanat eivät täsmää!';
