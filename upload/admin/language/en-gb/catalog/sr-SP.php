1421032af10b1fe1641d78ef4cec86090b1bbdd0
OSWorX <info@osworx.net>
2025-03-16 11:05:14 +0100

    New translations attribute.php (Serbian (Cyrillic))

---
+++ upload/admin/language/en-gb/catalog/sr-SP.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Атрибути';
+
+// Text
+$_['text_success']           = 'Успешно: изменили сте атрибуте!';
+$_['text_list']              = 'Листа атрибута';
+$_['text_add']               = 'Додај атрибут';
+$_['text_edit']              = 'Измени атрибут';
+
+// Column
+$_['column_name']            = 'Назив атрибута';
+$_['column_attribute_group'] = 'Група атрибута';
+$_['column_sort_order']      = 'Редослед';
+$_['column_action']          = 'Акција';
+
+// Entry
+$_['entry_name']             = 'Назив атрибута';
+$_['entry_attribute_group']  = 'Група атрибута';
+$_['entry_sort_order']       = 'Редослед';
+
+// Error
+$_['error_warning']          = 'Упозорење: молимо вас да пажљиво проверите могуће грешке у формулару!';
+$_['error_permission']       = 'Упозорење: ви немате дозволу за измене атрибута!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Назив атрибута мора бити између 3 и 64 карактера!';
+$_['error_product']          = 'Упозорење: Овај атрибут није могуће обрисати јер тренутно има придружених %s производа!';
