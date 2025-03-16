00d79df2cf16407347df07fd5b77f88a8fdf0f20
OSWorX <info@osworx.net>
2025-03-16 09:07:16 +0100

    New translations category.php (Bulgarian)

--- upload/language/bg-BG/bg-BG.php
+++ upload/language/bg-BG/bg-BG.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Групи атрибути';
+$_['heading_title']          = 'Категории';
 
 // Text
-$_['text_success']      = 'Успех: Вие модифицирахте групите на атрибутите!';
-$_['text_list']         = 'Списък на групите на атрибутите';
-$_['text_add']          = 'Добавяне на група атрибути';
-$_['text_edit']         = 'Редактиране на група на атрибут';
+$_['text_success']           = 'Успех: Вие модифицирахте категориите!';
+$_['text_list']              = 'Списък на категориите';
+$_['text_add']               = 'Добави Категория';
+$_['text_edit']              = 'Редактиране на категория';
+$_['text_default']           = 'По подразбиране';
 
 // Column
-$_['column_name']       = 'Име на атрибута група';
-$_['column_sort_order'] = 'Ред на Сортиране';
-$_['column_action']     = 'Действие';
+$_['column_name']            = 'Име на Категорията';
+$_['column_sort_order']      = 'Ред на Сортиране';
+$_['column_action']          = 'Действие';
 
 // Entry
-$_['entry_name']        = 'Име на атрибута група';
-$_['entry_sort_order']  = 'Ред на Сортиране';
+$_['entry_name']             = 'Име на Категорията';
+$_['entry_description']      = 'Описание';
+$_['entry_meta_title'] 	     = 'Мета име';
+$_['entry_meta_keyword']     = 'Мета тагове ключови думи';
+$_['entry_meta_description'] = 'Мета тагове описание';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Източник';
+$_['entry_filter']           = 'Филтри';
+$_['entry_store']            = 'Магазини';
+$_['entry_image']            = 'Изображение';
+$_['entry_top']              = 'Нагоре';
+$_['entry_column']           = 'Колони';
+$_['entry_sort_order']       = 'Ред на Сортиране';
+$_['entry_status']           = 'Статус';
+$_['entry_layout']           = 'Замени оформление';
+
+// Help
+$_['help_filter']            = '(Автодовършване)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Показване в горната лента на менюто. Работи само за Топ категории.';
+$_['help_column']            = 'Брой на колоните, които да използвате за дъното 3 категории. Работи само за Топ  категории.';
 
 // Error
-$_['error_warning']     = 'Внимание: Моля, проверете формуляра внимателно за грешки!';
-$_['error_permission']  = 'Внимание: Нямате разрешение да променяте атрибутите!';
-$_['error_name']        = 'Името на групата на атрибута трябва да бъде между 3 и 64 знака!';
-$_['error_attribute']   = 'Предупреждение: Тази група на атрибута не може да бъде изтрита, тъй като тя е в момента присвоена на %s атрибути!';
-$_['error_product']     = 'Внимание: Този група на атрибута  не може да бъде изтрита, тъй като в момента се ползва от %s продуктa!';
+$_['error_warning']          = 'Внимание: Моля, проверете формуляра внимателно за грешки!';
+$_['error_permission']       = 'Внимание: Нямате права да променяте параметрите на Настройки!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Мета заглавие трябва да бъде по-голямо от 3 и по-малко от 255 знака!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
