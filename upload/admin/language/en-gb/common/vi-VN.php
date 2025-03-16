3b2ff1cd06e8039cf4fa79a38e17d59a46463f69
OSWorX <info@osworx.net>
2025-03-16 11:20:17 +0100

    New translations login.php (Vietnamese)

--- upload/admin/language/en-gb/common/vi-VN.php
+++ upload/admin/language/en-gb/common/vi-VN.php
@@ -1,23 +1,19 @@
 <?php
-// Heading
-$_['heading_title']          = 'OpenCart';
+// header
+$_['heading_title']  = 'Quản trị';
 
 // Text
-$_['text_order']             = 'Các đơn hàng';
-$_['text_processing_status'] = 'Processing';
-$_['text_complete_status']   = 'Đã xong';
-$_['text_customer']          = 'Các khách hàng';
-$_['text_online']            = 'Khách đang online';
-$_['text_approval']          = 'Đang chờ phê duyệt';
-$_['text_product']           = 'Các sản phẩm';
-$_['text_stock']             = 'Hết hàng';
-$_['text_review']            = 'Đánh giá';
-$_['text_return']            = 'Trả lại';
-$_['text_affiliate']         = 'Chi nhánh';
-$_['text_store']             = 'Gian hàng';
-$_['text_front']             = 'Cửa hàng ở phía trước';
-$_['text_help']              = 'Hỗ trợ';
-$_['text_homepage']          = 'OpenCart Homepage';
-$_['text_support']           = 'Diễn đàn hỗ trợ';
-$_['text_documentation']     = 'Tài liệu';
-$_['text_logout']            = 'Thoát';
+$_['text_heading']   = 'Quản trị';
+$_['text_login']     = 'Xin vui lòng nhập các thông tin đăng nhập.';
+$_['text_forgotten'] = 'Quên mật khẩu';
+
+// Entry
+$_['entry_username'] = 'Tên đăng nhập';
+$_['entry_password'] = 'Mật khẩu';
+
+// Button
+$_['button_login']   = 'Đăng nhập';
+
+// Error
+$_['error_login']    = 'Tên người dùng và/hoặc mật khẩu không đúng.';
+$_['error_token']    = 'Phiên đăng nhập không hợp lệ. Xin vui lòng đăng nhập lại.';
