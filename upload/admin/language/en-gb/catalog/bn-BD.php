5bde5f59c43fc00f66a2ad4160890ee1c80c133c
OSWorX <info@osworx.net>
2025-03-16 14:20:23 +0100

    New translations information.php (Bengali)

--- upload/admin/language/en-gb/catalog/bn-BD.php
+++ upload/admin/language/en-gb/catalog/bn-BD.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'ফিলটারসমূহ';
+$_['heading_title']          = 'তথ্য';
 
 // Text
-$_['text_success']      = 'সাফল্য: আপনি ফিল্টারগুলো পরিবর্তন করেছেন!';
-$_['text_list']         = 'ফিল্টারের তালিকা';
-$_['text_add']          = 'ফিল্টার যুক্তকরণ';
-$_['text_edit']         = 'ফিল্টার সম্পাদনা';
+$_['text_success']           = 'সাফল্য: আপনি তথ্য পরিবর্তন করেছেন!';
+$_['text_list']              = 'তথ্য তালিকা';
+$_['text_add']               = 'তথ্য সম্পাদনা করুন';
+$_['text_edit']              = 'তথ্য সম্পাদনা';
+$_['text_default']           = 'ডিফল্ট (পূর্ব-নির্ধারিত)';
 
 // Column
-$_['column_group']      = 'ফিল্টার গোষ্ঠী';
-$_['column_sort_order'] = 'সর্ট অর্ডার';
-$_['column_action']     = 'অ্যাকশনঃ';
+$_['column_title']           = 'তথ্য শিরোনাম';
+$_['column_sort_order']	     = 'সর্ট অর্ডার';
+$_['column_action']          = 'অ্যাকশনঃ';
 
 // Entry
-$_['entry_group']       = 'ফিল্টার গোষ্ঠীর নাম';
-$_['entry_name']        = 'ফিল্টারের নাম';
-$_['entry_sort_order']  = 'সর্ট অর্ডার';
+$_['entry_title']            = 'তথ্য শিরোনাম';
+$_['entry_description']      = 'বিবরণ';
+$_['entry_store']            = 'ভাণ্ডার';
+$_['entry_meta_title'] 	     = 'মেটা ট্যাগ টাইটেল';
+$_['entry_meta_keyword'] 	 = 'মেটা ট্যাগ কীওয়ার্ডসমূহ';
+$_['entry_meta_description'] = 'মেটা ট্যাগ- বিবরণ';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Bottom';
+$_['entry_status']           = 'অবস্থা';
+$_['entry_sort_order']       = 'সর্ট অর্ডার';
+$_['entry_layout']           = 'লেআউট ওভাররাইড';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Display in the bottom footer.';
 
 // Error
-$_['error_warning']     = 'সতর্কীকরণ: অনুগ্রহ করে, ফর্মের ভুলভ্রান্তি, মনোযোগ সহকারে দেখে নিন I!';
-$_['error_permission']  = 'সতর্কীকরণ: ফিল্টারগুলো পরিবর্তন করার অনুমতি আপনার নেই!';
-$_['error_group']       = 'ফিল্টার গোষ্ঠীর নাম ১ ও ৬৪ বর্ণ-চিহ্নসম্বলিত হতে হবে।!';
-$_['error_name']        = 'ফিল্টারের নাম ১ থেকে ৬৪ বর্ণ-চিহ্নসম্বলিত হতে হবে!';
+$_['error_warning']          = 'সতর্কীকরণ: অনুগ্রহ করে, ফর্মের ভুলভ্রান্তি, মনোযোগ সহকারে দেখে নিন I!';
+$_['error_permission']       = 'Warning: You do not have permission to modify information!';
+$_['error_title']            = 'Information Title must be between 3 and 64 characters!';
+$_['error_description']      = 'Description must be more than 3 characters!';
+$_['error_meta_title']       = 'মেটা টাইটেল -এ, অবশ্যই, ৩ -এর বেশি ও ২৫৫ -র কম চিহ্ন থাকবে I!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'Warning: This information page cannot be deleted as it is currently assigned as the store account terms!';
+$_['error_checkout']         = 'Warning: This information page cannot be deleted as it is currently assigned as the store checkout terms!';
+$_['error_affiliate']        = 'Warning: This information page cannot be deleted as it is currently assigned as the store affiliate terms!';
+$_['error_return']           = 'Warning: This information page cannot be deleted as it is currently assigned as the store return terms!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
