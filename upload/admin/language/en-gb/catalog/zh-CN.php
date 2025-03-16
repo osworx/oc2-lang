855451d8d8f01900e6c9061dfbe7bd1257378e9d
OSWorX <info@osworx.net>
2025-03-16 14:21:03 +0100

    New translations manufacturer.php (Chinese Simplified)

--- upload/admin/language/en-gb/catalog/zh-CN.php
+++ upload/admin/language/en-gb/catalog/zh-CN.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = '信息管理';
+$_['heading_title']     = '厂商管理';
 
 // Text
-$_['text_success']           = '您已成功修改信息!';
-$_['text_list']              = '信息列表';
-$_['text_add']               = '添加信息';
-$_['text_edit']              = '编辑信息';
-$_['text_default']           = '默认';
+$_['text_success']      = '您已经成功修改厂商管理!';
+$_['text_list']         = '厂商列表';
+$_['text_add']          = '新增厂商';
+$_['text_edit']         = '编辑厂商';
+$_['text_default']      = '默认';
+$_['text_percent']      = '百分比';
+$_['text_amount']       = '固定金额';
 
 // Column
-$_['column_title']           = '信息标题';
-$_['column_sort_order']	     = '排列顺序';
-$_['column_action']          = '操作';
+$_['column_name']       = '厂商名称';
+$_['column_sort_order'] = '排列顺序';
+$_['column_action']     = '操作';
 
 // Entry
-$_['entry_title']            = '信息标题';
-$_['entry_description']      = '描述';
-$_['entry_store']            = '商店';
-$_['entry_meta_title'] 	     = '标签名';
-$_['entry_meta_keyword'] 	 = '关键词';
-$_['entry_meta_description'] = '标签描述';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = '底部';
-$_['entry_status']           = '状态';
-$_['entry_sort_order']       = '排列顺序';
-$_['entry_layout']           = '布局';
+$_['entry_name']        = '厂商名称';
+$_['entry_store']       = '商店';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = '图像';
+$_['entry_sort_order']  = '排列顺序';
+$_['entry_type']        = '类型';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = '在页面底部显示.';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = '警告:请仔细检查错误格式!';
-$_['error_permission']       = '警告:您没有权限修改信息!';
-$_['error_title']            = '信息标题必须介于3-64个字符之间!';
-$_['error_description']      = '内容必须超过3个字符!';
-$_['error_meta_title']       = '元标题必须3-255个字符之间!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = '警告:该信息页面不能被删除,因为它目前正适用于商店帐户条款!';
-$_['error_checkout']         = '警告:该信息页面不能被删除,因为它目前被适用于商店结算条款!';
-$_['error_affiliate']        = '警告:该信息页面不能被删除,因为它目前被适用于商店加盟条款!';
-$_['error_return']           = '警告:该信息页面不能被删除,因为它目前正被适用于商店退货条款!';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = '警告:请仔细检查错误格式!';
+$_['error_permission']  = '警告: 您没有权限修改厂商管理!';
+$_['error_name']        = '厂商名称必须介于2-64个字符之间!';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = '警告:您不能删除该厂商,因为它目前正关联%s产品!';
