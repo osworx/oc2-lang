2b05abf9ab510e76859d524b5ab1e99d1acc09c8
OSWorX <info@osworx.net>
2025-03-16 11:05:21 +0100

    New translations attribute.php (Galician)

---
+++ upload/admin/language/en-gb/catalog/gl-ES.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Atributos';
+
+// Text
+$_['text_success']           = 'Modificaronse os atributos correctamente!';
+$_['text_list']              = 'Lista de atributos';
+$_['text_add']               = 'Engadir atributo';
+$_['text_edit']              = 'Modifcar atributo';
+
+// Column
+$_['column_name']            = 'Nome do atributo';
+$_['column_attribute_group'] = 'Grupo de atributos';
+$_['column_sort_order']      = 'Orde da clasificación';
+$_['column_action']          = 'Acción';
+
+// Entry
+$_['entry_name']             = 'Nome do atributo';
+$_['entry_attribute_group']  = 'Grupo de atributos';
+$_['entry_sort_order']       = 'Orde da clasificación';
+
+// Error
+$_['error_warning']          = 'Aviso: Por favor, revise o formulario e busque os erros!';
+$_['error_permission']       = 'Aviso: Non ten permisos para modificar os atributos!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'O nome do atributo debe ter entre 3 e 64 caracteres!';
+$_['error_product']          = 'Aviso: Este atributo non pode ser eliminado, actualmente está atribuído a %s produtos!';
