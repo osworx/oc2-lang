7c76e7d52be75233221b368b3110453283b98e63
OSWorX <info@osworx.net>
2025-03-16 14:19:42 +0100

    New translations filter.php (English, United Kingdom)

--- upload/admin/language/en-gb/catalog/en-GB.php
+++ upload/admin/language/en-gb/catalog/en-GB.php
@@ -1,35 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = 'Downloads';
+$_['heading_title']     = 'Filters';
 
 // Text
-$_['text_success']      = 'Success: You have modified downloads!';
-$_['text_list']         = 'Download List';
-$_['text_add']          = 'Add Download';
-$_['text_edit']         = 'Edit Download';
-$_['text_upload']       = 'Your file was successfully uploaded!';
+$_['text_success']      = 'Success: You have modified filters!';
+$_['text_list']         = 'Filter List';
+$_['text_add']          = 'Add Filter';
+$_['text_edit']         = 'Edit Filter';
 
 // Column
-$_['column_name']       = 'Download Name';
-$_['column_date_added'] = 'Date Added';
+$_['column_group']      = 'Filter Group';
+$_['column_sort_order'] = 'Sort Order';
 $_['column_action']     = 'Action';
 
 // Entry
-$_['entry_name']        = 'Download Name';
-$_['entry_filename']    = 'Filename';
-$_['entry_mask']        = 'Mask';
-
-// Help
-$_['help_filename']     = 'You can upload via the upload button or use FTP to upload to the download directory and enter the details below.';
-$_['help_mask']         = 'It is recommended that the filename and the mask are different to stop people trying to directly link to your downloads.';
+$_['entry_group']       = 'Filter Group Name';
+$_['entry_name']        = 'Filter Name';
+$_['entry_sort_order']  = 'Sort Order';
 
 // Error
 $_['error_warning']     = 'Warning: Please check the form carefully for errors!';
-$_['error_permission']  = 'Warning: You do not have permission to modify downloads!';
-$_['error_name']        = 'Download Name must be between 3 and 64 characters!';
-$_['error_upload']      = 'Upload required!';
-$_['error_filename']    = 'Filename must be between 3 and 128 characters!';
-$_['error_exists']      = 'File does not exist!';
-$_['error_mask']        = 'Mask must be between 3 and 128 characters!';
-$_['error_filetype']    = 'Invalid file type!';
-$_['error_product']     = 'Warning: This download cannot be deleted as it is currently assigned to %s products!';
+$_['error_permission']  = 'Warning: You do not have permission to modify filters!';
+$_['error_group']       = 'Filter Group Name must be between 1 and 64 characters!';
+$_['error_name']        = 'Filter Name must be between 1 and 64 characters!';
