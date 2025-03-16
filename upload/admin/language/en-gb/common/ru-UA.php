fc97390b816cd19ff703c37230289271716acad4
OSWorX <info@osworx.net>
2025-03-16 11:17:21 +0100

    New translations filemanager.php (Rսssian, Սkraine)

--- upload/admin/language/en-gb/common/ru-UA.php
+++ upload/admin/language/en-gb/common/ru-UA.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Панель управления';
+$_['heading_title']    = 'Менеджер изображений';
+
+// Text
+$_['text_uploaded']    = 'Поздравляю! : ваш файл был загружен на сервер!';
+$_['text_directory']   = 'Поздравляю! : Папка создана!';
+$_['text_delete']      = 'Поздравляю! : Файл или каталог удален!';
+
+// Entry
+$_['entry_search']     = 'Поиск..';
+$_['entry_folder']     = 'Имя папки';
 
 // Error
-$_['error_install'] = 'Предупреждение: Папка установки по-прежнему существует и должна быть удалена по соображениям безопасности!';
+$_['error_permission'] = 'Внимание: Доступ запрещен!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Внимание: Файл или каталог с таким же именем уже существует!';
+$_['error_directory']  = 'Внимание: Каталог не существует!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Внимание: Неправильный тип (формат) файла!';
+$_['error_upload']     = 'Внимание: Файл невозможно загрузить на сервер по неизвестной причине!';
+$_['error_delete']     = 'Внимание: Вы не можете удалить этот каталог!';
