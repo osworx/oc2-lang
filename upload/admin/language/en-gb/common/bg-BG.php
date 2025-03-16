f7dc24494016de0ff3528cd94c0a1a6b19738468
OSWorX <info@osworx.net>
2025-03-16 14:26:44 +0100

    New translations filemanager.php (Bulgarian)

--- upload/admin/language/en-gb/common/bg-BG.php
+++ upload/admin/language/en-gb/common/bg-BG.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Табло';
+$_['heading_title']    = 'Мениджър на изображения';
+
+// Text
+$_['text_uploaded']    = 'Успех: Вашият файл е качен!';
+$_['text_directory']   = 'Успех: Директория, създадена!';
+$_['text_delete']      = 'Успех: Вашия файл или директория е изтрит!';
+
+// Entry
+$_['entry_search']     = 'Търсене..';
+$_['entry_folder']     = 'Име на папка';
 
 // Error
-$_['error_install'] = 'Внимание: Инсталиране папка все още съществува и следва да бъдат заличени от съображения за сигурност!';
+$_['error_permission'] = 'Предупреждение: Разрешението е отказано!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Внимание: Файл или директория със същото име вече съществува!';
+$_['error_directory']  = 'Предупреждение: Директория не съществува!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Внимание: Неправилен тип на файла!';
+$_['error_upload']     = 'Предупреждение: Файлът не може да бъде качен поради неизвестна причина!';
+$_['error_delete']     = 'Внимание: Не може да изтриете тази директория!';
