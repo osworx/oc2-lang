5d7d33a188f052bfff504d9763847a5b3d10031e
OSWorX <info@osworx.net>
2025-03-16 14:21:56 +0100

    New translations option.php (Indonesian)

--- upload/admin/language/en-gb/catalog/id-ID.php
+++ upload/admin/language/en-gb/catalog/id-ID.php
@@ -1,35 +1,40 @@
 <?php
 // Heading
-$_['heading_title']     = 'Merek';
+$_['heading_title']      = 'Pilihan';
 
 // Text
-$_['text_success']      = 'Berhasil: Anda telah memodifikasi merek!';
-$_['text_list']         = 'Daftar Merek';
-$_['text_add']          = 'Tambahkan Merek';
-$_['text_edit']         = 'Mengedit Merek';
-$_['text_default']      = 'Standar';
-$_['text_percent']      = 'Persentase';
-$_['text_amount']       = 'Jumlah Tetap';
+$_['text_success']       = 'Sukses: Anda telah memodifikasi pilihan!';
+$_['text_list']          = 'Daftar pilihan';
+$_['text_add']           = 'Tambah Pilihan';
+$_['text_edit']          = 'Edit pilihan';
+$_['text_choose']        = 'Pilih';
+$_['text_select']        = 'Pilihan';
+$_['text_radio']         = 'Radio';
+$_['text_checkbox']      = 'Kotak centang';
+$_['text_input']         = 'Masukan';
+$_['text_text']          = 'Teks';
+$_['text_textarea']      = 'Textarea';
+$_['text_file']          = 'Berkas';
+$_['text_date']          = 'Tanggal';
+$_['text_datetime']      = 'Tanggal &amp; waktu';
+$_['text_time']          = 'Waktu';
 
 // Column
-$_['column_name']       = 'Nama Merek';
-$_['column_sort_order'] = 'Urutan';
-$_['column_action']     = 'Tindakan';
+$_['column_name']        = 'Nama pilihan';
+$_['column_sort_order']  = 'Urutan';
+$_['column_action']      = 'Tindakan';
 
 // Entry
-$_['entry_name']        = 'Nama Merek';
-$_['entry_store']       = 'Toko';
-$_['entry_keyword']     = 'SEO URL';
-$_['entry_image']       = 'Gambar';
-$_['entry_sort_order']  = 'Urutan';
-$_['entry_type']        = 'Jenis';
-
-// Help
-$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['entry_name']         = 'Nama pilihan';
+$_['entry_type']         = 'Jenis';
+$_['entry_option_value'] = 'Nama Nilai Opsi';
+$_['entry_image']        = 'Gambar';
+$_['entry_sort_order']   = 'Urutan';
 
 // Error
-$_['error_warning']     = 'Peringatan: Mohon periksa formulir secara teliti untuk kesalahan!';
-$_['error_permission']  = 'Peringatan: Anda tidak memiliki izin untuk memodifikasi merek!';
-$_['error_name']        = 'Nama Merek harus antara 3 dan 64 karakter!';
-$_['error_keyword']     = 'SEO URL already in use!';
-$_['error_product']     = 'Peringatan: merk ini tidak bisa dihapus karena saat ini digunakan untuk  produk %s!';
+$_['error_warning']      = 'Peringatan: Mohon periksa formulir secara teliti untuk kesalahan!';
+$_['error_permission']   = 'Peringatan: Anda tidak memiliki izin untuk memodifikasi opsi!';
+$_['error_name']         = 'Nama pilihan harus antara 1 dan 128 karakter!';
+$_['error_type']         = 'Peringatan: pilihan nilai yang diperlukan!';
+$_['error_option_value'] = 'Nama Nilai Opsi harus antara 1 dan 128 karakter!';
+$_['error_product']      = 'Peringatan: Download ini tidak bisa dihapus karena saat ini ditetapkan untuk produk %s!';
