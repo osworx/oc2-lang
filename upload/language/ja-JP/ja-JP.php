29a0ace24815a10cd0da3c3203dffb82be50a4d3
OSWorX <info@osworx.net>
2025-03-16 09:10:20 +0100

    New translations attribute_group.php (Japanese)

--- upload/language/ja-JP/ja-JP.php
+++ upload/language/ja-JP/ja-JP.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = '属性：';
+$_['heading_title']     = '属性グループ';
 
 // Text
-$_['text_success']           = '成功: 属性を変更した ！';
-$_['text_list']              = '属性リスト';
-$_['text_add']               = '属性を追加';
-$_['text_edit']              = '属性を編集';
+$_['text_success']      = '成功: 属性グループを変更しました！';
+$_['text_list']         = '属性グループリスト';
+$_['text_add']          = '属性グループを追加';
+$_['text_edit']         = '属性グループを編集';
 
 // Column
-$_['column_name']            = '属性名';
-$_['column_attribute_group'] = '属性グループ';
-$_['column_sort_order']      = '並べ替え順序';
-$_['column_action']          = 'アクション';
+$_['column_name']       = '属性グループ名';
+$_['column_sort_order'] = '並べ替え順序';
+$_['column_action']     = 'アクション';
 
 // Entry
-$_['entry_name']             = '属性名';
-$_['entry_attribute_group']  = '属性グループ';
-$_['entry_sort_order']       = '並べ替え順序';
+$_['entry_name']        = '属性グループ名';
+$_['entry_sort_order']  = '並べ替え順序';
 
 // Error
-$_['error_warning']          = 'Warning: Please check the form carefully for errors!';
-$_['error_permission']       = '警告：あなたは属性を変更する権限がありません！';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = '属性名は、3〜64文字の間でなければなりません！';
-$_['error_product']          = '警告: %s に割り当てられているため、この属性を削除できません！';
+$_['error_warning']     = 'Warning: Please check the form carefully for errors!';
+$_['error_permission']  = '警告：あなたは属性を変更する権限がありません！';
+$_['error_name']        = '属性グループの名前は、3 から 64 文字の間でなければなりません!';
+$_['error_attribute']   = '警告: %s 属性に割り当てられているため、この属性グループを削除できません！';
+$_['error_product']     = '警告: %s に割り当てられているため、この属性を削除できません！';
