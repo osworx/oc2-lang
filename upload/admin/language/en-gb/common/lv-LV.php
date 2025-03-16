b372ac839f0823cf6468dfa67c553f2131ce895a
OSWorX <info@osworx.net>
2025-03-16 11:17:10 +0100

    New translations filemanager.php (Latvian)

--- upload/admin/language/en-gb/common/lv-LV.php
+++ upload/admin/language/en-gb/common/lv-LV.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Infopanelis';
+$_['heading_title']    = 'Attēlu pārvaldnieks';
+
+// Text
+$_['text_uploaded']    = 'Veiksmīgi: Jūsu fails ir augšupielādēts!';
+$_['text_directory']   = 'Veiksmīgi: Direktorija izveidota!';
+$_['text_delete']      = 'Veiksmīgi: Fails vai direktorija ir izdzēsti!';
+
+// Entry
+$_['entry_search']     = 'Meklēt..';
+$_['entry_folder']     = 'Mapes nosaukums';
 
 // Error
-$_['error_install'] = 'Brīdinājums: Instalēšanas mape joprojām pastāv. Tā ir jādzēš drošības apsvērumu dēļ!';
+$_['error_permission'] = 'Brīdinājums: Pieeja liegta!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Brīdinājums: Fails vai direktorija ar šādu nosaukumu jau eksistē!';
+$_['error_directory']  = 'Brīdinājums: Direktorija neeksistē!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Brīdinājums: Nepareizs faila tips!';
+$_['error_upload']     = 'Brīdinājums: Failu nevar augšupielādēt nezināmu iemeslu dēļ!';
+$_['error_delete']     = 'Brīdinājums: Nevar izdzēst šo direktoriju!';
