822e0441ea742b8928474e3abbbf1ba40d69f2be
OSWorX <info@osworx.net>
2025-03-16 11:05:23 +0100

    New translations attribute.php (Portuguese, Brazilian)

---
+++ upload/admin/language/en-gb/catalog/pt-BR.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Atributos';
+
+// Text
+$_['text_success']           = 'Sucesso: Você modificou os atributos!';
+$_['text_list']              = 'Lista do atributos';
+$_['text_add']               = 'Adicionar atributo';
+$_['text_edit']              = 'Editar atributo';
+
+// Column
+$_['column_name']            = 'Nome do atributo';
+$_['column_attribute_group'] = 'Grupo do atributo';
+$_['column_sort_order']      = 'Ordem de Classificação';
+$_['column_action']          = 'Ação';
+
+// Entry
+$_['entry_name']             = 'Nome do atributo';
+$_['entry_attribute_group']  = 'Grupo do atributo';
+$_['entry_sort_order']       = 'Ordem de Classificação';
+
+// Error
+$_['error_warning']          = 'Aviso: Por favor, verifique a forma cuidadosamente para evitar erros!';
+$_['error_permission']       = 'Aviso: Você não tem permissão para modificar os atributos!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Nome do atributo deve estar entre 3 e 64 caracteres!';
+$_['error_product']          = 'Aviso: Este atributo não pode ser excluído porque esta atualmente atribuído a %s produtos!';
