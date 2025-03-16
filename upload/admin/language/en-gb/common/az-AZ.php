660795124741f42bf96401617c86ec591720b510
OSWorX <info@osworx.net>
2025-03-16 11:18:49 +0100

    New translations forgotten.php (Azerbaijani)

--- upload/admin/language/en-gb/common/az-AZ.php
+++ upload/admin/language/en-gb/common/az-AZ.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Şifrənizi unutmusunuz?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Versiya %s';
+$_['text_forgotten']  = 'Unudulmuş Keçidsözü';
+$_['text_your_email'] = 'E-mail Ünvanınız';
+$_['text_email']      = 'Enter the e-mail address associated with your account. Click submit to have a password reset link e-mailed to you.';
+$_['text_success']    = 'An email with a confirmation link has been sent your admin email address.';
+
+// Entry
+$_['entry_email']     = 'E-mail Ünvanı';
+$_['entry_password']  = 'Yeni Keçidsözü';
+$_['entry_confirm']   = 'Doğrula';
+
+// Error
+$_['error_email']     = 'Warning: The E-Mail Address was not found in our records, please try again!';
+$_['error_password']  = 'Password must be between 4 and 20 characters!';
+$_['error_confirm']   = 'Password and password confirmation do not match!';
