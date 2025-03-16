48a88b3d26d1ed56d761865f1438ea3a10730eff
OSWorX <info@osworx.net>
2025-03-16 09:08:12 +0100

    New translations option.php (Danish)

--- upload/language/da-DK/da-DK.php
+++ upload/language/da-DK/da-DK.php
@@ -1,35 +1,40 @@
 <?php
 // Heading
-$_['heading_title']     = 'Producenter';
+$_['heading_title']      = 'Valgmuligheder';
 
 // Text
-$_['text_success']      = 'Succes: Du har ændret producenter!';
-$_['text_list']         = 'Producenter';
-$_['text_add']          = 'Tilføj producent';
-$_['text_edit']         = 'Rediger producent';
-$_['text_default']      = 'Standard';
-$_['text_percent']      = 'Procentdel';
-$_['text_amount']       = 'Fast mængde';
+$_['text_success']       = 'Succes: Du har ændret valgmuligheder!';
+$_['text_list']          = 'Valgmuligheder';
+$_['text_add']           = 'Tilføj valgmulighed';
+$_['text_edit']          = 'Rediger valgmulighed';
+$_['text_choose']        = 'Vælg';
+$_['text_select']        = 'Vælg';
+$_['text_radio']         = 'Radioknap';
+$_['text_checkbox']      = 'Afkrydsning';
+$_['text_input']         = 'Inputfelt';
+$_['text_text']          = 'Tekst';
+$_['text_textarea']      = 'Tekstbox';
+$_['text_file']          = 'Fil';
+$_['text_date']          = 'Dato';
+$_['text_datetime']      = 'Dato &amp; tid';
+$_['text_time']          = 'Tid';
 
 // Column
-$_['column_name']       = 'Producentnavn';
-$_['column_sort_order'] = 'Sorteringsrækkefølge';
-$_['column_action']     = 'Handling';
+$_['column_name']        = 'Valgmulighed navn';
+$_['column_sort_order']  = 'Sorteringsrækkefølge';
+$_['column_action']      = 'Handling';
 
 // Entry
-$_['entry_name']        = 'Producentnavn';
-$_['entry_store']       = 'Butikker';
-$_['entry_keyword']     = 'SEO URL';
-$_['entry_image']       = 'Billede';
-$_['entry_sort_order']  = 'Sorteringsrækkefølge';
-$_['entry_type']        = 'Type';
-
-// Help
-$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['entry_name']         = 'Valgmulighed navn';
+$_['entry_type']         = 'Type';
+$_['entry_option_value'] = 'Valgmulighed værdi';
+$_['entry_image']        = 'Billede';
+$_['entry_sort_order']   = 'Sorteringsrækkefølge';
 
 // Error
-$_['error_warning']     = 'Advarsel: Gennemgå venligst formularen for fejl!';
-$_['error_permission']  = 'Fejl: Du har ikke tilladelse til at ændre producenter!';
-$_['error_name']        = 'Producentnavn skal være mellem 2 og 64 karakterer!';
-$_['error_keyword']     = 'SEO URL already in use!';
-$_['error_product']     = 'Fejl: Denne producent kan ikke slettes, da den i øjeblikket er tilknyttet %s producenter!';
+$_['error_warning']      = 'Advarsel: Gennemgå venligst formularen for fejl!';
+$_['error_permission']   = 'Fejl: Du har ikke tilladelse til at ændre valgmuligheder!';
+$_['error_name']         = 'Valgmulighed navn skal være mellem 1 og 128 karakterer!';
+$_['error_type']         = 'Fejl: Værdi for valgmulighed er påkrævet!';
+$_['error_option_value'] = 'Valgmulighed navn skal være mellem 1 og 128 karakterer!';
+$_['error_product']      = 'Fejl: Denne valgmulighed kan ikke slettes, da den i øjeblikket er tilknyttet %s produkter!';
