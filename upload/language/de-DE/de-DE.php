993514e525867e712a87a5d3e79726e854178c37
OSWorX <info@osworx.net>
2025-03-16 09:08:27 +0100

    New translations manufacturer.php (German)

--- upload/language/de-DE/de-DE.php
+++ upload/language/de-DE/de-DE.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Texte';
+$_['heading_title']     = 'Hersteller';
 
 // Text
-$_['text_success']           = 'Text erfolgreich aktualisiert!';
-$_['text_list']              = 'Übersicht';
-$_['text_add']               = 'Neu';
-$_['text_edit']              = 'Bearbeiten';
-$_['text_default']           = 'Standardshop';
+$_['text_success']      = 'Hersteller erfolgreich bearbeitet';
+$_['text_list']         = 'Übersicht';
+$_['text_add']          = 'Neu';
+$_['text_edit']         = 'Bearbeiten';
+$_['text_default']      = 'Standardshop';
+$_['text_percent']      = 'Prozente';
+$_['text_amount']       = 'Fixbetrag';
 
 // Column
-$_['column_title']           = 'Bezeichnung';
-$_['column_sort_order']	     = 'Reihenfolge';
-$_['column_action']          = 'Aktion';
+$_['column_name']       = 'Name';
+$_['column_sort_order'] = 'Reihenfolge';
+$_['column_action']     = 'Aktion';
 
 // Entry
-$_['entry_title']            = 'Bezeichnung';
-$_['entry_description']      = 'Beschreibung';
-$_['entry_store']            = 'Shops';
-$_['entry_meta_title'] 	     = 'Metatag Titel';
-$_['entry_meta_keyword'] 	 = 'Metatag Schlüsselwörter';
-$_['entry_meta_description'] = 'Metatag Beschreibung';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'Fussbereich';
-$_['entry_status']           = 'Status';
-$_['entry_sort_order']       = 'Reihenfolge';
-$_['entry_layout']           = 'Layout überschreiben';
+$_['entry_name']        = 'Name';
+$_['entry_store']       = 'Shops';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'Bild';
+$_['entry_sort_order']  = 'Reihenfolge';
+$_['entry_type']        = 'Art';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'Zeige Link zu Text im Fussbereich';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'Erforderliche Daten fehlen, bitte Felder überprüfen.';
-$_['error_permission']       = 'Keine Rechte für diese Aktion';
-$_['error_title']            = 'Titel muss zwischen 3 und 64 Buchstaben lang sein';
-$_['error_description']      = 'Beschreibung muss zwischen 3 und 255 Zeichen lang sein';
-$_['error_meta_title']       = 'Metatag Titel muss zwischen 3 und 255 Zeichen lang sein';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'Text kann nicht gelöscht werden da er aktuell als Standard für Nutzungsbedingung definiert ist';
-$_['error_checkout']         = 'Text kann nicht gelöscht werden da er aktuell als Standard beim Bezahlvorgang definiert ist';
-$_['error_affiliate']        = 'Text kann nicht gelöscht werden da er den Partnerbedingungen eines Shops zugeordnet ist';
-$_['error_return']           = 'Text kann nicht gelöscht werden da er als Standardtext für Retouren definiert ist';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'Erforderliche Daten fehlen, bitte Felder überprüfen.';
+$_['error_permission']  = 'Keine Rechte für diese Aktion';
+$_['error_name']        = 'Herstellername muss zwischen 2 und 64 Zeichen lang sein!';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'Dieser Hersteller kann nicht gelöscht werden da er aktuell mit %s Produkten verknüpft ist!';
