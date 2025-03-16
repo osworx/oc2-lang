d719fb8e30fe81e844eda5c47895db732a9f8fb5
OSWorX <info@osworx.net>
2025-03-16 11:07:52 +0100

    New translations download.php (Swedish)

--- upload/admin/language/en-gb/catalog/sv-SE.php
+++ upload/admin/language/en-gb/catalog/sv-SE.php
@@ -1,46 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Kategorier';
+$_['heading_title']     = 'Nedladdningar';
 
 // Text
-$_['text_success']           = 'Klart: Du har ändrat kategorier!';
-$_['text_list']              = 'Kategorilista';
-$_['text_add']               = 'Lägg till kategori';
-$_['text_edit']              = 'Redigera kategori';
-$_['text_default']           = 'Standard';
+$_['text_success']      = 'Klart: Du har ändrat nedladdningar!';
+$_['text_list']         = 'Ladda ner lista';
+$_['text_add']          = 'Lägg till hämtning';
+$_['text_edit']         = 'Redigera nedladdning';
+$_['text_upload']       = 'Din fil laddades upp framgångsrikt!';
 
 // Column
-$_['column_name']            = 'Kategorinamn';
-$_['column_sort_order']      = 'Sorteringsordning';
-$_['column_action']          = 'Åtgärd';
+$_['column_name']       = 'Hämta namn';
+$_['column_date_added'] = 'Skapad';
+$_['column_action']     = 'Åtgärd';
 
 // Entry
-$_['entry_name']             = 'Kategorinamn';
-$_['entry_description']      = 'Beskrivning';
-$_['entry_meta_title'] 	     = 'Metatagtitel';
-$_['entry_meta_keyword']     = 'Metataggsökord';
-$_['entry_meta_description'] = 'Metataggbeskrivning';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = 'Överordnad';
-$_['entry_filter']           = 'Filter';
-$_['entry_store']            = 'Butiker';
-$_['entry_image']            = 'Bild';
-$_['entry_top']              = 'Topp';
-$_['entry_column']           = 'Kolumner';
-$_['entry_sort_order']       = 'Sorteringsordning';
-$_['entry_status']           = 'Status';
-$_['entry_layout']           = 'Layoutomställning';
+$_['entry_name']        = 'Hämta namn';
+$_['entry_filename']    = 'Filnamn';
+$_['entry_mask']        = 'Mask';
 
 // Help
-$_['help_filter']            = '(Komplettera automatiskt)';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = 'Visas i den övre menyraden. Fungerar bara för de översta överordnade kategorierna.';
-$_['help_column']            = 'Antalet kolumner för de nedre 3 kategorierna. Fungerar bara för de översta överordnade kategorierna.';
+$_['help_filename']     = 'Du kan överföra via knappen Ladda upp eller använda FTP för att ladda upp till nedladdningskatalogen och ange uppgifterna nedan.';
+$_['help_mask']         = 'Det rekommenderas att filnamnet och masken är olika för förhindra att man försöker direktlänka till dina nedladdningar.';
 
 // Error
-$_['error_warning']          = 'Varning: Kolla gärna formuläret noga vad det gäller fel!';
-$_['error_permission']       = 'Varning: Du har inte behörighet att ändra kategorier!';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = 'Metatitel måste vara större än 3 och mindre än 255 tecken!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = 'Varning: Kolla gärna formuläret noga vad det gäller fel!';
+$_['error_permission']  = 'Varning: Du har inte behörighet att ändra nedladdningar!';
+$_['error_name']        = 'Hämta namn måste vara mellan 3 och 64 tecken långt!';
+$_['error_upload']      = 'Uppladdning krävs!';
+$_['error_filename']    = 'Filnamn måste vara mellan 3 och 128 tecken långt!';
+$_['error_exists']      = 'Filen existerar inte!';
+$_['error_mask']        = 'Mask måste vara mellan 3 och 128 tecken långt!';
+$_['error_filetype']    = 'Ogiltig filtyp!';
+$_['error_product']     = 'Varning: Den här nedladdningen kan inte raderas eftersom den för närvarande är tilldelad %s produkter!';
