7de705d1e3d6504520b61ef817db33135c3290f1
OSWorX <info@osworx.net>
2025-03-16 14:24:08 +0100

    New translations review.php (Slovak)

--- upload/admin/language/en-gb/catalog/sk-SK.php
+++ upload/admin/language/en-gb/catalog/sk-SK.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'Opakujúci sa profil';
+$_['heading_title']     = 'Recenzie';
 
 // Text
-$_['text_success']          = 'Úspech: Zmenili ste opakujúci sa profily!';
-$_['text_list']             = 'Zoznam opakujúcich sa profilov';
-$_['text_add']              = 'Pridaj opakujúci sa profil';
-$_['text_edit']             = 'Uprav opakujúci sa profil';
-$_['text_day']				= 'Deň';
-$_['text_week']				= 'Týždeň';
-$_['text_semi_month']		= 'Polovičný mesiac';
-$_['text_month']			= 'Mesiac';
-$_['text_year']				= 'Rok';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> opakované množstvá sú počítané podľa frekvencie a cyklov. </p><p> Napríklad ak používate frekvenciu "týždeň" a cyklus "2" potom užívateľ bude faktúrovaný každé 2 týždne. </p><p> Trvanie je číslo koľko krát užívateľ zaplatil, nastavte ako 0 ak chcete platby pokial sa nezrušia. </p>';
-$_['text_profile']			= 'Opakujúci sa profil ';
-$_['text_trial']			= 'Skúšobný profil';
-
-// Entry
-$_['entry_name']		    = 'Názov';
-$_['entry_price']			= 'Cena';
-$_['entry_duration']		= 'Trvanie';
-$_['entry_cycle']			= 'Cyklus';
-$_['entry_frequency']		= 'Frekvencia';
-$_['entry_trial_price']		= 'Skúšobná cena';
-$_['entry_trial_duration']	= 'Škúšobná doba';
-$_['entry_trial_status']	= 'Skúšobný stav';
-$_['entry_trial_cycle']	    = 'Skúšobný cyklus';
-$_['entry_trial_frequency'] = 'Skúšobná frekvencia';
-$_['entry_status']			= 'Stav';
-$_['entry_sort_order']		= 'Poradie';
+$_['text_success']      = 'Úspech: Zmenili ste Recenzie!';
+$_['text_list']         = 'Recenzie';
+$_['text_add']          = 'Pridať recenziu';
+$_['text_edit']         = 'Upraviť recenziu';
 
 // Column
-$_['column_name']			= 'Názov';
-$_['column_sort_order']	    = 'Poradie';
-$_['column_action']         = 'Akcia';
+$_['column_product']    = 'Produkt';
+$_['column_author']     = 'Autor';
+$_['column_rating']     = 'Hodnotenie';
+$_['column_status']     = 'Stav';
+$_['column_date_added'] = 'Dátum pridaný';
+$_['column_action']     = 'Akcia';
+
+// Entry
+$_['entry_product']     = 'Produkt';
+$_['entry_author']      = 'Autor';
+$_['entry_rating']      = 'Hodnotenie';
+$_['entry_status']      = 'Stav';
+$_['entry_text']        = 'Text';
+$_['entry_date_added']  = 'Dátum pridaný';
+
+// Help
+$_['help_product']      = '(Automatické dokončovanie)';
 
 // Error
-$_['error_warning']         = 'Varovanie: Skontrolujte formulár starostlivo kvôli chybám!';
-$_['error_permission']		= 'Upozornenie: Nemáte povolenie na zmenu opakujúcich profilov!';
-$_['error_name']			= 'Názov profilu musí byť dlhší než 3 a kratší ako 255 znakov!';
-$_['error_product']			= 'Upozornenie: Tento opakujúci profil sa nedá odstrániť pretože je momentálne použitý u %s výrobkov!';
+$_['error_warning']     = 'Varovanie: Skontrolujte formulár starostlivo kvôli chybám!';
+$_['error_permission']  = 'Upozornenie: Nemáte oprávnenie k zmene recenzí!';
+$_['error_product']     = 'Produkt je nutný!';
+$_['error_author']      = 'Autor musí mať medzi 3 a 64 znakov!';
+$_['error_text']        = 'Text recenzie musí mať aspoň 1 znak!';
+$_['error_rating']      = 'Hodnotenie recenzie nutné!';
