239476580c2892a6a1256ac6771c9c82c0d7ece1
OSWorX <info@osworx.net>
2025-03-16 14:16:33 +0100

    New translations attribute.php (Serbian (Latin))

--- upload/admin/language/en-gb/catalog/sr-CS.php
+++ upload/admin/language/en-gb/catalog/sr-CS.php
@@ -1,36 +1,27 @@
 <?php
 // Heading
-$_['heading_title']     = 'Kritike';
+$_['heading_title']          = 'Atributi';
 
 // Text
-$_['text_success']      = 'Uspeh: Vi ste izmenili kritike!';
-$_['text_list']         = 'Lista Kritika';
-$_['text_add']          = 'Dodaj Kritiku';
-$_['text_edit']         = 'Izmeni Kritiku';
+$_['text_success']           = 'Uspešno ste promenili atribute!';
+$_['text_list']              = 'Lista atributa';
+$_['text_add']               = 'Dodaj atribut';
+$_['text_edit']              = 'Izmeni atribut';
 
 // Column
-$_['column_product']    = 'Proizvod';
-$_['column_author']     = 'Autor';
-$_['column_rating']     = 'Ocena';
-$_['column_status']     = 'Stanje';
-$_['column_date_added'] = 'Datum Dodavanja';
-$_['column_action']     = 'Akcija';
+$_['column_name']            = 'Ime atributa';
+$_['column_attribute_group'] = 'Grupa atributa';
+$_['column_sort_order']      = 'Sortiraj po';
+$_['column_action']          = 'Akcija';
 
 // Entry
-$_['entry_product']     = 'Proizvod';
-$_['entry_author']      = 'Autor';
-$_['entry_rating']      = 'Ocena';
-$_['entry_status']      = 'Stanje';
-$_['entry_text']        = 'Тekst';
-$_['entry_date_added']  = 'Datum Dodavanja';
-
-// Help
-$_['help_product']      = '(Automatsko Dovršavanje)';
+$_['entry_name']             = 'Ime atributa';
+$_['entry_attribute_group']  = 'Grupa atributa';
+$_['entry_sort_order']       = 'Sortiraj po';
 
 // Error
-$_['error_warning']     = 'Upozorenje: Molimo proverite formu zbog grešaka!';
-$_['error_permission']  = 'Upozorenje: Vi nemate dozvolu da biste prilagodili kritike!';
-$_['error_product']     = 'Potreban proizvod!';
-$_['error_author']      = 'Autor morra biti između 3 i 64 karaktera!';
-$_['error_text']        = 'Tekst Kritike mora biti naj manje 1 karakter!';
-$_['error_rating']      = 'Potrebna ocena kritike!';
+$_['error_warning']          = 'Upozorenje: Molimo proverite formu zbog grešaka!';
+$_['error_permission']       = 'Upozorenje: nemate ovlašćenje za izmenu atributa!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Ime atributa mora da ima između 3 i 64 karaktera!';
+$_['error_product']          = 'Upozorenje: Ovaj atribut ne može biti obrisan jer je trenutno povezan sa %s proizvoda!';
