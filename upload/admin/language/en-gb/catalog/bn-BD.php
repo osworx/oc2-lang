55c75d3bc3a751a05ec0cfa3478a0ad3245b65e9
OSWorX <info@osworx.net>
2025-03-16 11:11:36 +0100

    New translations option.php (Bengali)

--- upload/admin/language/en-gb/catalog/bn-BD.php
+++ upload/admin/language/en-gb/catalog/bn-BD.php
@@ -1,35 +1,40 @@
 <?php
 // Heading
-$_['heading_title']     = 'Manufacturers';
+$_['heading_title']      = 'Options';
 
 // Text
-$_['text_success']      = 'Success: You have modified manufacturers!';
-$_['text_list']         = 'উত্পাদক';
-$_['text_add']          = 'নির্মাতার';
-$_['text_edit']         = 'Edit Manufacturer';
-$_['text_default']      = 'ডিফল্ট (পূর্ব-নির্ধারিত)';
-$_['text_percent']      = 'Percentage';
-$_['text_amount']       = 'Fixed Amount';
+$_['text_success']       = 'Success: You have modified options!';
+$_['text_list']          = 'পছন্দ-তালিকা';
+$_['text_add']           = 'অপশন যুক্ত করুন';
+$_['text_edit']          = 'Edit Option';
+$_['text_choose']        = 'Choose';
+$_['text_select']        = 'Select';
+$_['text_radio']         = 'Radio';
+$_['text_checkbox']      = 'Checkbox';
+$_['text_input']         = 'Input';
+$_['text_text']          = 'Text';
+$_['text_textarea']      = 'Textarea';
+$_['text_file']          = 'File';
+$_['text_date']          = 'Date';
+$_['text_datetime']      = 'Date &amp; Time';
+$_['text_time']          = 'Time';
 
 // Column
-$_['column_name']       = 'Manufacturer Name';
-$_['column_sort_order'] = 'সর্ট অর্ডার';
-$_['column_action']     = 'অ্যাকশনঃ';
+$_['column_name']        = 'Option Name';
+$_['column_sort_order']  = 'সর্ট অর্ডার';
+$_['column_action']      = 'অ্যাকশনঃ';
 
 // Entry
-$_['entry_name']        = 'Manufacturer Name';
-$_['entry_store']       = 'ভাণ্ডার';
-$_['entry_keyword']     = 'SEO URL';
-$_['entry_image']       = 'ছবি';
-$_['entry_sort_order']  = 'সর্ট অর্ডার';
-$_['entry_type']        = 'Type';
-
-// Help
-$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['entry_name']         = 'Option Name';
+$_['entry_type']         = 'Type';
+$_['entry_option_value'] = 'Option Value Name';
+$_['entry_image']        = 'ছবি';
+$_['entry_sort_order']   = 'সর্ট অর্ডার';
 
 // Error
-$_['error_warning']     = 'সতর্কীকরণ: অনুগ্রহ করে, ফর্মের ভুলভ্রান্তি, মনোযোগ সহকারে দেখে নিন I!';
-$_['error_permission']  = 'Warning: You do not have permission to modify manufacturers!';
-$_['error_name']        = 'Manufacturer Name must be between 2 and 64 characters!';
-$_['error_keyword']     = 'SEO URL already in use!';
-$_['error_product']     = 'Warning: This manufacturer cannot be deleted as it is currently assigned to %s products!';
+$_['error_warning']      = 'সতর্কীকরণ: অনুগ্রহ করে, ফর্মের ভুলভ্রান্তি, মনোযোগ সহকারে দেখে নিন I!';
+$_['error_permission']   = 'Warning: You do not have permission to modify options!';
+$_['error_name']         = 'Option Name must be between 1 and 128 characters!';
+$_['error_type']         = 'Warning: Option Values required!';
+$_['error_option_value'] = 'Option Value Name must be between 1 and 128 characters!';
+$_['error_product']      = 'Warning: This option cannot be deleted as it is currently assigned to %s products!';
