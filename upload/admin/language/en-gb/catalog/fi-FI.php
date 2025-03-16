fc81e43b164475e451a26f69bc597abe29495309
OSWorX <info@osworx.net>
2025-03-16 14:23:55 +0100

    New translations review.php (Finnish)

--- upload/admin/language/en-gb/catalog/fi-FI.php
+++ upload/admin/language/en-gb/catalog/fi-FI.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'Toistuvat profiilit';
+$_['heading_title']     = 'Arvostelut';
 
 // Text
-$_['text_success']          = 'Onnistui: Olet muuttanut toistuvia profiiliileja!';
-$_['text_list']             = 'Toistuvat profiilit';
-$_['text_add']              = 'Lisää toistuva profiili';
-$_['text_edit']             = 'Toistuvan profiilin muokkaaminen';
-$_['text_day']				= 'Päivä';
-$_['text_week']				= 'Viikko';
-$_['text_semi_month']		= 'Kahdesti kuussa';
-$_['text_month']			= 'Kuukausi';
-$_['text_year']				= 'Vuosi';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Toistuvat summat lasketaan tiheydestä ja syklistä.</p><p>Esimerkiksi jos tiheys on "viikko" ja sykli on "2", käyttäjää laskutetaan 2 viikon välein.</p><p>Kesto on montako maksua käyttäjälle tulee. Aseta kestoksi 0, jos maksut jatkuvat toistaiseksi.</p>';
-$_['text_profile']			= 'Toistuva profiili';
-$_['text_trial']			= 'Koejakson profiili';
-
-// Entry
-$_['entry_name']		    = 'Nimi';
-$_['entry_price']			= 'Hinta';
-$_['entry_duration']		= 'Kesto';
-$_['entry_cycle']			= 'Sykli';
-$_['entry_frequency']		= 'Tiheys';
-$_['entry_trial_price']		= 'Koejakson hinta';
-$_['entry_trial_duration']	= 'Koejakson kesto';
-$_['entry_trial_status']	= 'Koejakson tila';
-$_['entry_trial_cycle']	    = 'Koejakson sykli';
-$_['entry_trial_frequency'] = 'Koejakson tiheys';
-$_['entry_status']			= 'Tila';
-$_['entry_sort_order']		= 'Lajittelujärjestys';
+$_['text_success']      = 'Onnistui: Olet muokannut arvosteluja!';
+$_['text_list']         = 'Arvostelut';
+$_['text_add']          = 'Lisää arvostelu';
+$_['text_edit']         = 'Muokkaa arvostelua';
 
 // Column
-$_['column_name']			= 'Nimi';
-$_['column_sort_order']	    = 'Lajittelujärjestys';
-$_['column_action']         = 'Toiminto';
+$_['column_product']    = 'Tuote';
+$_['column_author']     = 'Tekijä';
+$_['column_rating']     = 'Arvostelu';
+$_['column_status']     = 'Tila';
+$_['column_date_added'] = 'Lisäyspäivämäärä';
+$_['column_action']     = 'Toiminto';
+
+// Entry
+$_['entry_product']     = 'Tuote';
+$_['entry_author']      = 'Tekijä';
+$_['entry_rating']      = 'Arvostelu';
+$_['entry_status']      = 'Tila';
+$_['entry_text']        = 'Teksti';
+$_['entry_date_added']  = 'Lisäyspäivämäärä';
+
+// Help
+$_['help_product']      = '(Automaattinen täydennys)';
 
 // Error
-$_['error_warning']         = 'Varoitus: tarkista lomake virheiden varalta!';
-$_['error_permission']		= 'Varoitus: Sinulla ei ole oikeuksia muokata toistuvia profiileja!';
-$_['error_name']			= 'Profiilin nimen tulee olla pidempi kuin 3 ja lyhyempi kuin 255 merkkiä!';
-$_['error_product']			= 'Varoitus: Tätä toistuvaa profiilia ei voida poistaa, koska se on määritetty %s tuotteelle!';
+$_['error_warning']     = 'Varoitus: tarkista lomake virheiden varalta!';
+$_['error_permission']  = 'Varoitus: sinulla ei ole oikeuksia muokata arvosteluja!';
+$_['error_product']     = 'Tuote on pakollinen!';
+$_['error_author']      = 'Kirjoittajan tulee olla 3 - 64 merkkiä pitkä!';
+$_['error_text']        = 'Arvostelutekstin tulee sisältää ainakin 1 merkki!';
+$_['error_rating']      = 'Arvosana on pakollinen!';
