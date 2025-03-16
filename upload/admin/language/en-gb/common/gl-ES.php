aae7b3e8f7552a29df306f34adeea3226eb5e11f
OSWorX <info@osworx.net>
2025-03-16 14:28:48 +0100

    New translations forgotten.php (Galician)

--- upload/admin/language/en-gb/common/gl-ES.php
+++ upload/admin/language/en-gb/common/gl-ES.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Esqueciches o contrasinal?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Versión %s';
+$_['text_forgotten']  = 'Contrasinal esquecido';
+$_['text_your_email'] = 'A tua direccion de correo electónico';
+$_['text_email']      = 'Engade o correo electrónico para asocialo coa tua conta. Pincha enviar para obter un novo contrasinal que che enviaremos.';
+$_['text_success']    = 'Un correo electrónico con un enlace de confirmación foi enviado o seu enderezo de correo electrónico.';
+
+// Entry
+$_['entry_email']     = 'Enderezo de correo electrónico';
+$_['entry_password']  = 'Novo contrasinal';
+$_['entry_confirm']   = 'Confirmar';
+
+// Error
+$_['error_email']     = 'Aviso: O correo electrónico non foi encontrado nos nosos rexistros, tenteo de novo!';
+$_['error_password']  = 'Password must be between 4 and 20 characters!';
+$_['error_confirm']   = 'O contrasinal e a sua confirmacion non coinciden!';
