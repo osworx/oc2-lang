bbae11a426e7e68267abc81cf5397bdbabd75fc0
OSWorX <info@osworx.net>
2025-03-16 14:20:10 +0100

    New translations information.php (Slovak)

--- upload/admin/language/en-gb/catalog/sk-SK.php
+++ upload/admin/language/en-gb/catalog/sk-SK.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'Filtre';
+$_['heading_title']          = 'Údaje';
 
 // Text
-$_['text_success']      = 'Úspech: Zmenili ste filtre!';
-$_['text_list']         = 'Zoznam filtrov';
-$_['text_add']          = 'Pridaj filter';
-$_['text_edit']         = 'Upraviť filter';
+$_['text_success']           = 'Úspech: Zmenili ste údaje!';
+$_['text_list']              = 'Zoznam údajov';
+$_['text_add']               = 'Pridaj údaj';
+$_['text_edit']              = 'Upraviť údaje';
+$_['text_default']           = 'Pôvodné';
 
 // Column
-$_['column_group']      = 'Skupina filtrov';
-$_['column_sort_order'] = 'Poradie';
-$_['column_action']     = 'Akcia';
+$_['column_title']           = 'Názov informácie';
+$_['column_sort_order']	     = 'Poradie';
+$_['column_action']          = 'Akcia';
 
 // Entry
-$_['entry_group']       = 'Názov skupiny filtrov';
-$_['entry_name']        = 'Názov filtru';
-$_['entry_sort_order']  = 'Poradie';
+$_['entry_title']            = 'Názov informácie';
+$_['entry_description']      = 'Popis';
+$_['entry_store']            = 'Obchody';
+$_['entry_meta_title'] 	     = 'Meta Tag Title (nadpis)';
+$_['entry_meta_keyword'] 	 = 'Meta Tag Kľúčové slová';
+$_['entry_meta_description'] = 'Meta Tag Popis';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Spodok';
+$_['entry_status']           = 'Stav';
+$_['entry_sort_order']       = 'Poradie';
+$_['entry_layout']           = 'Šablóna';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Zobraziť v zápätií.';
 
 // Error
-$_['error_warning']     = 'Varovanie: Skontrolujte formulár starostlivo kvôli chybám!';
-$_['error_permission']  = 'Upozornenie: Nemáte povolenie k zmene filtrov!';
-$_['error_group']       = 'Názov skupiny filtrov musí mať medzi 1 a 64 znakov!';
-$_['error_name']        = 'Názov filtru musí mať medzi 1 a 64 znakov!';
+$_['error_warning']          = 'Varovanie: Skontrolujte formulár starostlivo kvôli chybám!';
+$_['error_permission']       = 'Upozornenie: Nemáte oprávnenie pre správu informácií!';
+$_['error_title']            = 'Názov informácie musí byť dlhší ako 3 znaky a kratší než 64 znakov!';
+$_['error_description']      = 'Popis musí byť dlhší ako 3 znaky!';
+$_['error_meta_title']       = 'Meta Tag Title (nadpis) musí mať aspoň 3 a menej ako 255 znakov!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'Upozornenie: Informačná stránka nemôže byť zrušená, pretože je nastavená ako predvolený účet obchodu!';
+$_['error_checkout']         = 'Upozornenie: Táto strana s údajmi nemôže byť zmazaná pretože sa momentálne používa ako platobné podmienky obchodu!';
+$_['error_affiliate']        = 'Upozornenie: Táto strana s údajmi nemôže byť smazaná pretože sa momentálne používa ako Obchodné affiliálne podmienky!';
+$_['error_return']           = 'Upozornenie: Táto strana s údajmi nemôže byť smazaná pretože sa momentálne používa ako obchodné reklamačné podmienky!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
