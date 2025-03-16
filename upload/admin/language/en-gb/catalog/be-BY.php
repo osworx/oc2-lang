215f5a805037ac706078f5f565e434b0b7e3a22a
OSWorX <info@osworx.net>
2025-03-16 11:04:50 +0100

    New translations attribute.php (Belarusian)

---
+++ upload/admin/language/en-gb/catalog/be-BY.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Атрыбуты';
+
+// Text
+$_['text_success']           = 'Поспех: атрыбуты змененыя!';
+$_['text_list']              = 'Спіс атрыбутаў';
+$_['text_add']               = 'Дадаць атрыбут';
+$_['text_edit']              = 'Рэдагаваць атрыбут';
+
+// Column
+$_['column_name']            = 'Назва атрыбуту';
+$_['column_attribute_group'] = 'Група атрыбутаў';
+$_['column_sort_order']      = 'Парадак сартавання';
+$_['column_action']          = 'Дзеянне';
+
+// Entry
+$_['entry_name']             = 'Назва атрыбуту';
+$_['entry_attribute_group']  = 'Група атрыбутаў';
+$_['entry_sort_order']       = 'Парадак сартавання';
+
+// Error
+$_['error_warning']          = 'Увага: Калі ласка, ўважліва праверце ўведзеные дадзеныя!';
+$_['error_permission']       = 'Увага: вы не маеце права рэдагаваць атрыбут!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Назва атрыбуту павінна мець ад 3 да 64 знакаў!';
+$_['error_product']          = 'Увага: гэты атрыбут не можа быць выдалены, паколькі ён адносіцца да %s тавараў!';
