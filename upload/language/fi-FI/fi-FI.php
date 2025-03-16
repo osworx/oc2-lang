3f4cef1b2d9994adfaf5d712ca44e5a6bd5a2874
OSWorX <info@osworx.net>
2025-03-16 09:09:07 +0100

    New translations filemanager.php (Finnish)

--- upload/language/fi-FI/fi-FI.php
+++ upload/language/fi-FI/fi-FI.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Hallintasivu';
+$_['heading_title']    = 'Kuvienhallinta';
+
+// Text
+$_['text_uploaded']    = 'Tiedoston lähettäminen onnistui!';
+$_['text_directory']   = 'Hakemiston luominen onnistui!';
+$_['text_delete']      = 'Tiedoston tai hakemiston poisto onnistui!';
+
+// Entry
+$_['entry_search']     = 'Hae..';
+$_['entry_folder']     = 'Kansion nimi';
 
 // Error
-$_['error_install'] = 'Varoitus: asennushakemisto on edellen olemassa ja turvallisuuden vuoksi se tulisi poistaa!';
+$_['error_permission'] = 'Varoitus: Ei käyttöikeuksia!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Varoitus:  samanniminen tiedosto tai hakemisto on jo olemassa!';
+$_['error_directory']  = 'Varoitus: Hakemistoa ei ole olemassa!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Varoitus: Virheellinen tiedostomuoto!';
+$_['error_upload']     = 'Varoitus: Tiedoston lataaminen ei onnistunut tuntemattomasta syystä!';
+$_['error_delete']     = 'Varoitus: Et voi poistaa tätä hakemistoa!';
