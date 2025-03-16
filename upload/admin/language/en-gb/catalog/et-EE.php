c58d4d75e41b96e70774f528738b7c9893a76250
OSWorX <info@osworx.net>
2025-03-16 14:18:50 +0100

    New translations download.php (Estonian)

--- upload/admin/language/en-gb/catalog/et-EE.php
+++ upload/admin/language/en-gb/catalog/et-EE.php
@@ -1,46 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Kategooriad';
+$_['heading_title']     = 'Allalaadimised';
 
 // Text
-$_['text_success']           = 'Õnnestus: Te olete muutnud kategooriaid!';
-$_['text_list']              = 'Kategooria nimekiri';
-$_['text_add']               = 'Lisa Kategooria';
-$_['text_edit']              = 'Muuda kategooriat';
-$_['text_default']           = 'Vaikimisi';
+$_['text_success']      = 'Oled edukalt muutnud allalaadimisi!';
+$_['text_list']         = 'Allalaadimiste nimekiri';
+$_['text_add']          = 'Lisa allalaadimine';
+$_['text_edit']         = 'Muuda allalaadimist';
+$_['text_upload']       = 'Sinu fail on edukalt üles laaditud!';
 
 // Column
-$_['column_name']            = 'Kategooria nimi';
-$_['column_sort_order']      = 'Sorteerimis järjekord';
-$_['column_action']          = 'Tegevus';
+$_['column_name']       = 'Allalaadimise nimi';
+$_['column_date_added'] = 'Lisamise kuupäev';
+$_['column_action']     = 'Tegevus';
 
 // Entry
-$_['entry_name']             = 'Kategooria nimi';
-$_['entry_description']      = 'Kirjeldus';
-$_['entry_meta_title'] 	     = 'Meta Tag pealkiri';
-$_['entry_meta_keyword']     = 'Sisukirjelduse märksõnad';
-$_['entry_meta_description'] = 'Sisukirjelduse kirjeldus';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = 'Vanem';
-$_['entry_filter']           = 'Filtrid';
-$_['entry_store']            = 'Poed';
-$_['entry_image']            = 'Pilt';
-$_['entry_top']              = 'Üles';
-$_['entry_column']           = 'Väljad';
-$_['entry_sort_order']       = 'Sorteerimis järjekord';
-$_['entry_status']           = 'Staatus';
-$_['entry_layout']           = 'Asetuse ülekirjutamine';
+$_['entry_name']        = 'Allalaadimise nimi';
+$_['entry_filename']    = 'Failinimi';
+$_['entry_mask']        = 'Maskeering';
 
 // Help
-$_['help_filter']            = '(Automaatselt lõpetatud)';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = 'Ülemise Menüüriba kuvamine. Toimib ainult ülemkategooriate puhul.';
-$_['help_column']            = 'Number of columns to use for the bottom 3 categories. Only works for the top parent categories.';
+$_['help_filename']     = 'Sa võid üles laadida läbi nuppu või kasutada FTP programmi, ning laadida üles vastavasse kausta ja sisestada andmed alla.';
+$_['help_mask']         = 'On soovitatav, et failinimi ja maskeering oleksid erinevad, et takistaks inimestel otsest ligipääsu teie allalaadimistele.';
 
 // Error
-$_['error_warning']          = 'Hoiatus: Palun kontrollige vormi vead hoolikalt üle!';
-$_['error_permission']       = 'Hoiatus! Teil pole luba muuta kategooriaid!';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = 'Meta pealkiri peab olema üle 3 ja alla 255 tähemärgi!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = 'Hoiatus: Palun kontrollige vormi vead hoolikalt üle!';
+$_['error_permission']  = 'Hoiatus: Sul ei ole õigusi, et muuta allalaadimisi!';
+$_['error_name']        = 'Allalaadimise nime pikkus peab olema vahemikus 3 kuni 64 märki!';
+$_['error_upload']      = 'Üleslaadimine vajalik!';
+$_['error_filename']    = 'Failinimi peab olema 3 kuni 128 tähemärki pikk!';
+$_['error_exists']      = 'Faili ei eksiteeri!';
+$_['error_mask']        = 'Maskeering peab jääma 3 ja 128 tähemärgi vahemikku!';
+$_['error_filetype']    = 'Faili tüüp pole toetatud!';
+$_['error_product']     = 'Hoiatus: Seda allalaadimist ei saa kustutada, sest see on hetkel määratud tootele %s!';
