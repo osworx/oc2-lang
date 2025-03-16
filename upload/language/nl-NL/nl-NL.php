10127d06ccd32dcea51297b8856f48150996c4ed
OSWorX <info@osworx.net>
2025-03-16 09:11:27 +0100

    New translations attribute_group.php (Dutch)

--- upload/language/nl-NL/nl-NL.php
+++ upload/language/nl-NL/nl-NL.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Attributen';
+$_['heading_title']     = 'Attribuut groepen';
 
 // Text
-$_['text_success']           = 'Succes: Instellingen gewijzigd!';
-$_['text_list']              = 'Lijst';
-$_['text_add']               = 'Toevoegen';
-$_['text_edit']              = 'Wijzigen';
+$_['text_success']      = 'Succes: Instellingen gewijzigd!';
+$_['text_list']         = 'Lijst';
+$_['text_add']          = 'Toevoegen';
+$_['text_edit']         = 'Wijzigen';
 
 // Column
-$_['column_name']            = 'Attribuutnaam';
-$_['column_attribute_group'] = 'Attribuut groep';
-$_['column_sort_order']      = 'Sorteervolgorde';
-$_['column_action']          = 'Aktie';
+$_['column_name']       = 'Attribuut groepsnaam';
+$_['column_sort_order'] = 'Sorteervolgorde';
+$_['column_action']     = 'Aktie';
 
 // Entry
-$_['entry_name']             = 'Attribuutnaam';
-$_['entry_attribute_group']  = 'Attribuut groep';
-$_['entry_sort_order']       = 'Sorteervolgorde';
+$_['entry_name']        = 'Attribuut groepsnaam';
+$_['entry_sort_order']  = 'Sorteervolgorde';
 
 // Error
-$_['error_warning']          = 'Waarschuwing: Controlleer of alle verplichte velden volledig zijn ingevuld!';
-$_['error_permission']       = 'Waarschuwing: U heeft geen rechten deze instellingen te wijzigen!';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'Waarschuwing: Attribuutnaam dient tussen de 3 en 64 tekens lang te zijn!';
-$_['error_product']          = 'Waarschuwing: Deze attribuut kan niet worden verwijderd want het is gekoppeld aan %s product(en)!';
+$_['error_warning']     = 'Waarschuwing: Controlleer of alle verplichte velden volledig zijn ingevuld!';
+$_['error_permission']  = 'Waarschuwing: U heeft geen rechten deze instellingen te wijzigen!';
+$_['error_name']        = 'Waarschuwing: Attribuut groepsnaam dient tussen de 3 en 64 tekens lang te zijn!';
+$_['error_attribute']   = 'Waarschuwing: Deze groep kan niet worden verwijderd omdat hij gekoppeld is aan %s attributen!';
+$_['error_product']     = 'Waarschuwing: Deze groep kan niet worden verwijderd omdat hij gekoppeld is aan %s product(en)!';
