48df63ac0ef69c000e36881e120bbd135119d000
OSWorX <info@osworx.net>
2025-03-16 11:17:57 +0100

    New translations footer.php (Persian)

--- upload/admin/language/en-gb/common/fa-IR.php
+++ upload/admin/language/en-gb/common/fa-IR.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'مدیریت تصویر';
-
 // Text
-$_['text_uploaded']    = 'موفقیت: پرونده شما بارگذاری شده است!';
-$_['text_directory']   = 'موفقیت: دایرکتوری ایجاد شد!';
-$_['text_delete']      = 'موفقیت: پرونده یا دایرکتوری شما حذف شده است!';
-
-// Entry
-$_['entry_search']     = 'جستجو..';
-$_['entry_folder']     = 'نام پوشه';
-
-// Error
-$_['error_permission'] = 'هشدار: دسترسی ممنوع است!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'هشدار: یک پرونده یا دایرکتوری با نام مشابه در حال حاضر وجود دارد!';
-$_['error_directory']  = 'هشدار: دایرکتوری وجود ندارد!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'هشدار: نوع پرونده نادرست!';
-$_['error_upload']     = 'هشدار: پرونده به دلیل ناشناخته نمی‌تواند بارگذاری شود!';
-$_['error_delete']     = 'هشدار: شما نمی‌توانید این دایرکتوری را حذف کنید!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'نگارش %s';
