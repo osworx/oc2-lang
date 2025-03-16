2f52b75a6a3e46b252bbefc8b7f4862602ccbb8d
OSWorX <info@osworx.net>
2025-03-16 11:16:27 +0100

    New translations filemanager.php (Belarusian)

--- upload/admin/language/en-gb/common/be-BY.php
+++ upload/admin/language/en-gb/common/be-BY.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Панэль кіравання';
+$_['heading_title']    = 'Менеджэр выяў';
+
+// Text
+$_['text_uploaded']    = 'Поспех: Ваш файл загружаны!';
+$_['text_directory']   = 'Поспех: Дырэкторыя створана!';
+$_['text_delete']      = 'Поспех: Ваш файл / тэчка выдалена!';
+
+// Entry
+$_['entry_search']     = 'Пошук..';
+$_['entry_folder']     = 'Назва тэчкі';
 
 // Error
-$_['error_install'] = 'Увага: Дырэкторыя інсталяцыі ўсё яшчэ існуе і для бяспекі павінна быць выдалена!';
+$_['error_permission'] = 'Увага: Вы не маеце дазволу на гэтую аперацыю!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Увага: Файл / тэчка за такой назвай ўжо існуе!';
+$_['error_directory']  = 'Увага: Дырэкторыя не існуе!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Увага: Хібны тып файла!';
+$_['error_upload']     = 'Увага: Файл не можа быць загружаны па нявызначанай прычыне!';
+$_['error_delete']     = 'Увага: Вы не можаце выдаліць гэтую дырэкторыю!';
