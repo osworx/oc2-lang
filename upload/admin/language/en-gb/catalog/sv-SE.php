24b99df2922199c3d8681888e0c0e9f2fe21db68
OSWorX <info@osworx.net>
2025-03-16 14:22:36 +0100

    New translations product.php (Swedish)

--- upload/admin/language/en-gb/catalog/sv-SE.php
+++ upload/admin/language/en-gb/catalog/sv-SE.php
@@ -1,40 +1,108 @@
 <?php
 // Heading
-$_['heading_title']      = 'Alternativ';
+$_['heading_title']          = 'Produkter';
 
 // Text
-$_['text_success']       = 'Klart: Du har ändrat i alternativ!';
-$_['text_list']          = 'Alternativlista';
-$_['text_add']           = 'Lägg till alternativ';
-$_['text_edit']          = 'Redigera alternativ';
-$_['text_choose']        = 'Välja';
-$_['text_select']        = 'Markera';
-$_['text_radio']         = 'Radioknapp';
-$_['text_checkbox']      = 'Kryssruta';
-$_['text_input']         = 'Indata';
-$_['text_text']          = 'Text';
-$_['text_textarea']      = 'Textarea';
-$_['text_file']          = 'Fil';
-$_['text_date']          = 'Datum';
-$_['text_datetime']      = 'Datum &amp; Tid';
-$_['text_time']          = 'Tid';
+$_['text_success']           = 'Klart: Du har ändrat i produkter!';
+$_['text_list']              = 'Produktlista';
+$_['text_add']               = 'Lägg till produkt';
+$_['text_edit']              = 'Redigera produkt';
+$_['text_plus']              = '+';
+$_['text_minus']             = '-';
+$_['text_default']           = 'Standard';
+$_['text_option']            = 'Alternativ';
+$_['text_option_value']      = 'Alternativt värde';
+$_['text_percent']           = 'Procentandel';
+$_['text_amount']            = 'Fast belopp';
 
 // Column
-$_['column_name']        = 'Alternativnamn';
-$_['column_sort_order']  = 'Sorteringsordning';
-$_['column_action']      = 'Åtgärd';
+$_['column_name']            = 'Produktnamn';
+$_['column_model']           = 'Artikelnummer';
+$_['column_image']           = 'Bild';
+$_['column_price']           = 'Pris';
+$_['column_quantity']        = 'Antal';
+$_['column_status']          = 'Status';
+$_['column_action']          = 'Åtgärd';
 
 // Entry
-$_['entry_name']         = 'Alternativnamn';
-$_['entry_type']         = 'Typ';
-$_['entry_option_value'] = 'Alternativvärde';
-$_['entry_image']        = 'Bild';
-$_['entry_sort_order']   = 'Sorteringsordning';
+$_['entry_name']             = 'Produktnamn';
+$_['entry_description']      = 'Beskrivning';
+$_['entry_meta_title'] 	     = 'Metatagtitel';
+$_['entry_meta_keyword'] 	 = 'Metataggsökord';
+$_['entry_meta_description'] = 'Metataggbeskrivning';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_model']            = 'Artikelnummer';
+$_['entry_sku']              = 'SKU';
+$_['entry_upc']              = 'UPC';
+$_['entry_ean']              = 'EAN';
+$_['entry_jan']              = 'JAN';
+$_['entry_isbn']             = 'ISBN';
+$_['entry_mpn']              = 'MPN';
+$_['entry_location']         = 'Lagerplats:';
+$_['entry_shipping']         = 'Kräver frakt:';
+$_['entry_manufacturer']     = 'Tillverkare:';
+$_['entry_store']            = 'Butiker';
+$_['entry_date_available']   = 'Tillgänglig (datum):';
+$_['entry_quantity']         = 'Antal';
+$_['entry_minimum']          = 'Minsta antal';
+$_['entry_stock_status']     = 'Ej i lager:<br/><span class="help">Status som visas när en produkt är slut i lagers.</span>';
+$_['entry_price']            = 'Pris';
+$_['entry_tax_class']        = 'Momsklass';
+$_['entry_points']           = 'Poäng:<br/><span class="help">Antal poäng som krävs för att köpa denna vara. Om du inte vill att varan ska kunna köpas med poäng ange "0".</span>';
+$_['entry_option_points']    = 'Poäng:<br/><span class="help">Antal poäng som krävs för att köpa denna vara. Om du inte vill att varan ska kunna köpas med poäng ange "0".</span>';
+$_['entry_subtract']         = 'Dra ifrån lager:';
+$_['entry_weight_class']     = 'Viktklass';
+$_['entry_weight']           = 'Vikt:';
+$_['entry_dimension']        = 'Dimensioner (L x B x H):';
+$_['entry_length_class']     = 'Längdklass';
+$_['entry_length']           = 'Längdklass';
+$_['entry_width']            = 'Bredd';
+$_['entry_height']           = 'Höjd';
+$_['entry_image']            = 'Bild';
+$_['entry_additional_image'] = 'Additional Images';
+$_['entry_customer_group']   = 'Kundgrupp';
+$_['entry_date_start']       = 'Startdatum';
+$_['entry_date_end']         = 'Slutdatum';
+$_['entry_priority']         = 'Prioriteringsordning';
+$_['entry_attribute']        = 'Attribut';
+$_['entry_attribute_group']  = 'Attributgrupp';
+$_['entry_text']             = 'Text';
+$_['entry_option']           = 'Alternativ';
+$_['entry_option_value']     = 'Alternativt värde';
+$_['entry_required']         = 'Krävs';
+$_['entry_status']           = 'Status';
+$_['entry_sort_order']       = 'Sorteringsordning';
+$_['entry_category']         = 'Kategorier';
+$_['entry_filter']           = 'Filter';
+$_['entry_download']         = 'Nedladdningar';
+$_['entry_related']          = 'Relaterade produkter';
+$_['entry_tag']          	 = 'Produkttaggar';
+$_['entry_reward']           = 'Lojalitetspoäng produkten ger vid köp';
+$_['entry_layout']           = 'Layoutomställning';
+$_['entry_recurring']        = 'Återkommande profiler';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_sku']               = 'Lagerhållningsenhet';
+$_['help_upc']               = 'Allmän produktkod';
+$_['help_ean']               = 'Europeiska artikelnummer';
+$_['help_jan']               = 'Japanska artikelnummer';
+$_['help_isbn']              = 'Internationellt standardboknummer';
+$_['help_mpn']               = 'Tillverkarens artikelnummer';
+$_['help_manufacturer']      = '(Komplettera automatiskt)';
+$_['help_minimum']           = 'Tvingad till ett minsta ordervärde';
+$_['help_stock_status']      = 'Status visas när en produkt är slutsåld';
+$_['help_points']            = 'Antal poäng som behövs för att köpa denna punkt. Om du inte vill lämna denna produkt köpas med poäng som 0.';
+$_['help_category']          = '(Komplettera automatiskt)';
+$_['help_filter']            = '(Komplettera automatiskt)';
+$_['help_download']          = '(Komplettera automatiskt)';
+$_['help_related']           = '(Komplettera automatiskt)';
+$_['help_tag']               = 'Comma separated';
 
 // Error
-$_['error_warning']      = 'Varning: Kolla gärna formuläret noga vad det gäller fel!';
-$_['error_permission']   = 'Varning: Du har inte behörighet att ändra alternativ!';
-$_['error_name']         = 'Alternativnamnet måste innehålla mellan 1 och 128 tecken!';
-$_['error_type']         = 'Varning: Alternativvärde krävs!';
-$_['error_option_value'] = 'Alternativvärde måste innehålla mellan 1 och 128 tecken!';
-$_['error_product']      = 'Varning: Detta alternativ kan inte raderas eftersom det är kopplat till %s produkter!';
+$_['error_warning']          = 'Varning: Kolla gärna formuläret noga vad det gäller fel!';
+$_['error_permission']       = 'Varning: Du har inte behörighet att ändra i produkter!';
+$_['error_name']             = 'Produktnamnet måste innheålla mer än 3 och mindre än 255 tecken!';
+$_['error_meta_title']       = 'Metatitel måste vara större än 3 och mindre än 255 tecken!';
+$_['error_model']            = 'Produktens artikelnummer måste innehålla mer än 3 och mindre än 64 tecken!';
+$_['error_keyword']          = 'SEO URL already in use!';
