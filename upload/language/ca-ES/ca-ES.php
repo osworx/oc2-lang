9c228304e4d512e0b018445bb5d59ccdb85890a1
OSWorX <info@osworx.net>
2025-03-16 09:07:31 +0100

    New translations attribute.php (Catalan)

---
+++ upload/language/ca-ES/ca-ES.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Atributs';
+
+// Text
+$_['text_success']           = 'Èxit: Heu modificat els atributs!';
+$_['text_list']              = 'Llista d\'atributs';
+$_['text_add']               = 'Afegeix un atribut';
+$_['text_edit']              = 'Edita els atributs';
+
+// Column
+$_['column_name']            = 'Nom de l\'atribut';
+$_['column_attribute_group'] = 'Grup d\'atributs';
+$_['column_sort_order']      = 'Ordre de classificació';
+$_['column_action']          = 'Acció';
+
+// Entry
+$_['entry_name']             = 'Nom de l\'atribut';
+$_['entry_attribute_group']  = 'Grup d\'atributs';
+$_['entry_sort_order']       = 'Ordre de classificació';
+
+// Error
+$_['error_warning']          = 'Advertència: Si us plau reviseu acuradament el formulari pels errors!';
+$_['error_permission']       = 'Advertència: No teniu permís per modificar els atributs!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'El nom de l\'atribut ha de ser entre 3 i 64 caràcters!';
+$_['error_product']          = 'Advertència: Aquests atributs no es poden suprimir com actualment està assignat a %s productes!';
