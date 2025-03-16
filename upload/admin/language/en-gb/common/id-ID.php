6f7277fb0aff9b00756328be6aacd2b83751a29c
OSWorX <info@osworx.net>
2025-03-16 14:27:18 +0100

    New translations filemanager.php (Indonesian)

--- upload/admin/language/en-gb/common/id-ID.php
+++ upload/admin/language/en-gb/common/id-ID.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Dasbor';
+$_['heading_title']    = 'Manajer Gambar';
+
+// Text
+$_['text_uploaded']    = 'Berhasil: File anda telah di unggah!';
+$_['text_directory']   = 'Berhasil: Direktori telah dibuat!';
+$_['text_delete']      = 'Berhasil: berkas atau direktori anda telah dihapus!';
+
+// Entry
+$_['entry_search']     = 'Cari..';
+$_['entry_folder']     = 'Nama Direktori';
 
 // Error
-$_['error_install'] = 'Peringatan: Folder install masih ada dan harus dihapus untuk alasan keamanan!';
+$_['error_permission'] = 'Peringatan: Izin ditolak!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Peringatan: File atau direktori dengan nama yang sama sudah ada!';
+$_['error_directory']  = 'Peringatan: Direktori tidak ditemukan!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Peringatan: Jenis berkas salah!';
+$_['error_upload']     = 'Peringatan: Berkas tidak dapat di unggah untuk alasan yang tidak diketahui!';
+$_['error_delete']     = 'Peringatan: Anda tidak dapat menghapus direktori ini!';
