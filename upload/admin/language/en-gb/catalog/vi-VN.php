4b31935e136e5a4f87d79cc5e34ecc49fe5fed5c
OSWorX <info@osworx.net>
2025-03-16 11:13:11 +0100

    New translations recurring.php (Vietnamese)

--- upload/admin/language/en-gb/catalog/vi-VN.php
+++ upload/admin/language/en-gb/catalog/vi-VN.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'Các sản phẩm';
+$_['heading_title']			= 'Các hồ sơ định kỳ';
 
 // Text
-$_['text_success']           = 'Kết quả: Bạn đã sửa xong các sản phẩm!';
-$_['text_list']              = 'Danh sách sản phẩm';
-$_['text_add']               = 'Thêm sản phẩm';
-$_['text_edit']              = 'Sửa sản phẩm';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'Mặc định';
-$_['text_option']            = 'Tuỳ chọn';
-$_['text_option_value']      = 'Giá trị tùy chọn';
-$_['text_percent']           = 'Phần trăm';
-$_['text_amount']            = 'Số tiền cố định';
-
-// Column
-$_['column_name']            = 'Tên sản phẩm';
-$_['column_model']           = 'Số hiệu mẫu';
-$_['column_image']           = 'Hình ảnh';
-$_['column_price']           = 'Giá';
-$_['column_quantity']        = 'Số lượng';
-$_['column_status']          = 'Trạng thái';
-$_['column_action']          = 'Tác vụ';
+$_['text_success']          = 'Kết quả: Bạn không có quyền sửa các hồ sơ định kỳ!';
+$_['text_list']             = 'Danh sách hồ sơ định kỳ';
+$_['text_add']              = 'Thêm hồ sơ định kỳ';
+$_['text_edit']             = 'Sửa hồ sơ định kỳ';
+$_['text_day']				= 'Ngày';
+$_['text_week']				= 'Tuần';
+$_['text_semi_month']		= 'Nửa tháng';
+$_['text_month']			= 'Tháng';
+$_['text_year']				= 'Năm';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Số lượng định kỳ được tính bởi tần suất và chu kỳ.</p><p>Ví dụ nếu bạn dùng tần suất là "tuần" và chu kỳ là "2", thì người dùng sẽ nhận hóa đơn mỗi 2 tuần.</p><p>Trong khoảng thời gian đó thì số lần người dùng trả tiền là một lần, gán cái này bằng 0 nếu bạn muốn thanh toán cho tới khi chúng được hủy.</p>';
+$_['text_profile']			= 'Hồ sơ định kỳ';
+$_['text_trial']			= 'Hồ sơ thử nghiệm';
 
 // Entry
-$_['entry_name']             = 'Tên sản phẩm';
-$_['entry_description']      = 'Mô tả';
-$_['entry_meta_title'] 	     = 'Tên thẻ Meta';
-$_['entry_meta_keyword'] 	 = 'Từ khóa cho thẻ Meta';
-$_['entry_meta_description'] = 'Mô tả cho thẻ Meta';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Số hiệu mẫu';
-$_['entry_sku']              = 'SKU';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'Xuất xứ';
-$_['entry_shipping']         = 'Các yêu cầu chuyển hàng';
-$_['entry_manufacturer']     = 'Nhà sản xuất';
-$_['entry_store']            = 'Gian hàng';
-$_['entry_date_available']   = 'Ngày có';
-$_['entry_quantity']         = 'Số lượng';
-$_['entry_minimum']          = 'Số lượng tối thiểu';
-$_['entry_stock_status']     = 'Tình trạng hết hàng';
-$_['entry_price']            = 'Giá';
-$_['entry_tax_class']        = 'Thuế';
-$_['entry_points']           = 'Điểm';
-$_['entry_option_points']    = 'Điểm';
-$_['entry_subtract']         = 'Trừ số lượng tồn';
-$_['entry_weight_class']     = 'Đơn vị trọng lượng';
-$_['entry_weight']           = 'Cân nặng';
-$_['entry_dimension']        = 'Kích cỡ (L x W x H)';
-$_['entry_length_class']     = 'Đơn vị chiều dài';
-$_['entry_length']           = 'Chiều dài';
-$_['entry_width']            = 'Rộng';
-$_['entry_height']           = 'Cao';
-$_['entry_image']            = 'Hình ảnh';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Nhóm khách hàng';
-$_['entry_date_start']       = 'Ngày bắt đầu';
-$_['entry_date_end']         = 'Ngày kết thúc';
-$_['entry_priority']         = 'Độ ưu tiên';
-$_['entry_attribute']        = 'Thuộc tính';
-$_['entry_attribute_group']  = 'Nhóm thuộc tính';
-$_['entry_text']             = 'Mô tả';
-$_['entry_option']           = 'Tuỳ chọn';
-$_['entry_option_value']     = 'Giá trị tùy chọn';
-$_['entry_required']         = 'Được yêu cầu';
-$_['entry_status']           = 'Trạng thái';
-$_['entry_sort_order']       = 'Thứ tự sắp xếp';
-$_['entry_category']         = 'Các hạng mục';
-$_['entry_filter']           = 'Bộ lọc';
-$_['entry_download']         = 'Các mục tải về';
-$_['entry_related']          = 'Sản phẩm liên quan';
-$_['entry_tag']          	 = 'Các thẻ sản phẩm';
-$_['entry_reward']           = 'Điểm thưởng';
-$_['entry_layout']           = 'Ghi đè layout';
-$_['entry_recurring']        = 'Hồ sơ định kỳ';
+$_['entry_name']		    = 'Tên';
+$_['entry_price']			= 'Giá';
+$_['entry_duration']		= 'Khoảng thời gian';
+$_['entry_cycle']			= 'Chu kỳ';
+$_['entry_frequency']		= 'Tần suất';
+$_['entry_trial_price']		= 'Giá thử nghiệm';
+$_['entry_trial_duration']	= 'Thời gian thử nghiệm';
+$_['entry_trial_status']	= 'Trạng thái thử nghiệm';
+$_['entry_trial_cycle']	    = 'Chu kỳ thử nghiệm';
+$_['entry_trial_frequency'] = 'Tần suất thử nghiệm';
+$_['entry_status']			= 'Trạng thái';
+$_['entry_sort_order']		= 'Thứ tự sắp xếp';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'SKU';
-$_['help_upc']               = 'Mã sản phẩm chung';
-$_['help_ean']               = 'Số bài viết châu Âu';
-$_['help_jan']               = 'Số bài viết Nhật bản';
-$_['help_isbn']              = 'Số sách chuẩn quốc tế';
-$_['help_mpn']               = 'Seri của nhà sản xuất';
-$_['help_manufacturer']      = '(Tự động gợi ý)';
-$_['help_minimum']           = 'Số lượng tối thiểu khi đặt hàng';
-$_['help_stock_status']      = 'Hiện trạng thái khi 1 sản phẩm hết hàng';
-$_['help_points']            = 'Số điểm cần có để mua vật này. Nếu bạn không muốn sản phẩm được mua bằng điểm hãy để nó là 0.';
-$_['help_category']          = '(Tự động gợi ý)';
-$_['help_filter']            = '(Tự động gợi ý)';
-$_['help_download']          = '(Tự động gợi ý)';
-$_['help_related']           = '(Tự động gợi ý)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'Tên';
+$_['column_sort_order']	    = 'Thứ tự sắp xếp';
+$_['column_action']         = 'Tác vụ';
 
 // Error
-$_['error_warning']          = 'Chú ý: Vui lòng kiểm tra lỗi cẩn thận trong biểu mẫu!';
-$_['error_permission']       = 'Chú ý: Bạn không có quyền sửa các sản phẩm!';
-$_['error_name']             = 'Tên sản phẩm phải lớn hơn 3 và ít hơn 255 ký tự!';
-$_['error_meta_title']       = 'Tên thẻ Meta phải nhiều hơn 3 và ít hơn 255 ký tự!';
-$_['error_model']            = 'Số hiệu mẫu sản phẩm phải lớn hơn 1 và ít hơn 64 ký tự!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Chú ý: Vui lòng kiểm tra lỗi cẩn thận trong biểu mẫu!';
+$_['error_permission']		= 'Chú ý: Bạn không có quyền sửa các hồ sơ định kỳ!';
+$_['error_name']			= 'Tên hồ sơ phải lớn hơn 3 và ít hơn 255 ký tự!';
+$_['error_product']			= 'Chú ý: Hồ sơ định kỳ này không thể xóa vì nó đang được gán cho các sản phẩm  %s !';
