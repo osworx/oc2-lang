cf911eeffbf5bd0c129343786f54056befd4c7ad
OSWorX <info@osworx.net>
2025-03-16 09:06:40 +0100

    New translations category.php (Arabic)

--- upload/language/ar-SA/ar-SA.php
+++ upload/language/ar-SA/ar-SA.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'مجموعة الخصائص';
+$_['heading_title']          = 'الأقسام';
 
 // Text
-$_['text_success']      = 'تم بنجاح: لقد قمت بتعديل خصائص المجموعات!';
-$_['text_list']         = 'قائمة مجموعة الخصائص';
-$_['text_add']          = 'إضافة مجموعة خصائص';
-$_['text_edit']         = 'تحرير مجموعة الخصائص';
+$_['text_success']           = 'تم حفظ تعديلاتك على الأقسام بنجاح!';
+$_['text_list']              = 'قائمة الأقسام';
+$_['text_add']               = 'اضافة قسم';
+$_['text_edit']              = 'تحرير القسم';
+$_['text_default']           = 'الافتراضي';
 
 // Column
-$_['column_name']       = 'اسم مجموعة الخصائص';
-$_['column_sort_order'] = 'الترتيب';
-$_['column_action']     = 'عمل';
+$_['column_name']            = 'اسم القسم';
+$_['column_sort_order']      = 'الترتيب';
+$_['column_action']          = 'عمل';
 
 // Entry
-$_['entry_name']        = 'اسم مجموعة الخصائص';
-$_['entry_sort_order']  = 'الترتيب';
+$_['entry_name']             = 'اسم القسم';
+$_['entry_description']      = 'الوصف';
+$_['entry_meta_title'] 	     = 'عنوان Meta-Tag';
+$_['entry_meta_keyword']     = 'الكلمات الوصفية "Meta Tag Keywords"';
+$_['entry_meta_description'] = 'وصف العلامة الوصفية "Meta Tag Description"';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'الرئيسي';
+$_['entry_filter']           = 'ترشيحات/تصفيات';
+$_['entry_store']            = 'المتاجر';
+$_['entry_image']            = 'صورة';
+$_['entry_top']              = 'أعلى';
+$_['entry_column']           = 'الأعمدة';
+$_['entry_sort_order']       = 'الترتيب';
+$_['entry_status']           = 'الحالة';
+$_['entry_layout']           = 'تجاوز التخطيط';
+
+// Help
+$_['help_filter']            = '(إكمال تلقائي)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'عرض في القائمة العلوية . تعمل فقط في الأقسام الرئيسية وليس الفرعيه منها .';
+$_['help_column']            = 'عدد الاعمدة الممكن استخدامها في الأسفل هو 3 أقسام. تعمل فقط في الأقسام الرئيسة.';
 
 // Error
-$_['error_warning']     = 'تحذير: الرجاء التحقق من النموذج واصلاح الأخطاء!';
-$_['error_permission']  = 'تحذير: ليس لديك الإذن بتعديل الخصائص!';
-$_['error_name']        = 'يجب أن يكون اسم مجموعة الخصائص بين 3 و 64 حرفاً!';
-$_['error_attribute']   = 'تحذير: مجموعة الخصائص هذه لا يمكن حذفها, حيث انها مرتبطة بـ %s خصائص!';
-$_['error_product']     = 'تحذير: مجموعة الخصائص هذه لا يمكن حذفها, حيث انها مرتبطة بـ %s منتجات!';
+$_['error_warning']          = 'تحذير: الرجاء التحقق من النموذج واصلاح الأخطاء!';
+$_['error_permission']       = 'تحذير: ليس لديك الصلاحيات لتعديل الأقسام!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'يجب أن لا يقل عدد حروف العنوان عن 3حروف ولا يزيد عن 255 حرف!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
