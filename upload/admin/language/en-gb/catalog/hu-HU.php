abbd232283e76cdab6e22d9b9039851b34270fcf
OSWorX <info@osworx.net>
2025-03-16 11:11:10 +0100

    New translations option.php (Hungarian)

--- upload/admin/language/en-gb/catalog/hu-HU.php
+++ upload/admin/language/en-gb/catalog/hu-HU.php
@@ -1,35 +1,40 @@
 <?php
 // Heading
-$_['heading_title']     = 'Gyártók';
+$_['heading_title']      = 'Opciók';
 
 // Text
-$_['text_success']      = 'Siker: Módosította a gyártókat!';
-$_['text_list']         = 'Gyártó lista';
-$_['text_add']          = 'Gyártó hozzáadása';
-$_['text_edit']         = 'Gyártó szerkesztése';
-$_['text_default']      = 'Alapértelmezés';
-$_['text_percent']      = 'Százalék';
-$_['text_amount']       = 'Fix összeg';
+$_['text_success']       = 'Siker: Módosította az opciókat!';
+$_['text_list']          = 'Opciók listája';
+$_['text_add']           = 'Opció hozzáadása';
+$_['text_edit']          = 'Opció szerkeszetése';
+$_['text_choose']        = 'Kiválasztás';
+$_['text_select']        = 'Választ';
+$_['text_radio']         = 'Rádiógomb';
+$_['text_checkbox']      = 'Jelölőnégyzet';
+$_['text_input']         = 'Bevitel';
+$_['text_text']          = 'Szöveg';
+$_['text_textarea']      = 'Szövegmező';
+$_['text_file']          = 'Fájl';
+$_['text_date']          = 'Dátum';
+$_['text_datetime']      = 'Dátum &amp; Idő';
+$_['text_time']          = 'Idő';
 
 // Column
-$_['column_name']       = 'Gyártó neve';
-$_['column_sort_order'] = 'Rendezés';
-$_['column_action']     = 'Művelet';
+$_['column_name']        = 'Opció neve';
+$_['column_sort_order']  = 'Rendezés';
+$_['column_action']      = 'Művelet';
 
 // Entry
-$_['entry_name']        = 'Gyártó neve';
-$_['entry_store']       = 'Áruházak';
-$_['entry_keyword']     = 'SEO URL';
-$_['entry_image']       = 'Kép';
-$_['entry_sort_order']  = 'Rendezés';
-$_['entry_type']        = 'Típus';
-
-// Help
-$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['entry_name']         = 'Opció neve';
+$_['entry_type']         = 'Típus';
+$_['entry_option_value'] = 'Option Value Name';
+$_['entry_image']        = 'Kép';
+$_['entry_sort_order']   = 'Rendezés';
 
 // Error
-$_['error_warning']     = 'Figyelem: Kérjük, figyelmesen ellenőrizze a hibákat!';
-$_['error_permission']  = 'Figyelmeztetés: Nincs engedélye a gyártók módosításához!';
-$_['error_name']        = 'Gyártó nevének 2 és 64 karakter között kell lennie!';
-$_['error_keyword']     = 'SEO URL already in use!';
-$_['error_product']     = 'Figyelmeztetés: Ez a gyártó nem törölhető, mert jelenleg hozzá van rendelve a(z) %s termék(ek)!';
+$_['error_warning']      = 'Figyelem: Kérjük, figyelmesen ellenőrizze a hibákat!';
+$_['error_permission']   = 'Figyelem: Nincs jogosultsága az opciók módosításához!';
+$_['error_name']         = 'Beállítás nevének 1 és 128 karakter között kell lennie!';
+$_['error_type']         = 'Figyelmeztetés: Beállítási értékekek szükségesek!';
+$_['error_option_value'] = 'Beállítás érték nevének 1 és 128 karakter között kell lennie!';
+$_['error_product']      = 'Figyelmeztetés: Ez a beállítás nem törölhető, mert jelenleg hozzá van rendelve a(z) %s termékekhez!';
