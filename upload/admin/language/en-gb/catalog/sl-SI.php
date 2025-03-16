f8313b30e1b101dd29b49f5d7eeee684a3494928
OSWorX <info@osworx.net>
2025-03-16 14:20:10 +0100

    New translations information.php (Slovenian)

--- upload/admin/language/en-gb/catalog/sl-SI.php
+++ upload/admin/language/en-gb/catalog/sl-SI.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'Filtri';
+$_['heading_title']          = 'Informacije';
 
 // Text
-$_['text_success']      = 'USPEŠNO: Ste uredili filtre!';
-$_['text_list']         = 'Seznam filtrov';
-$_['text_add']          = 'Dodaj filter';
-$_['text_edit']         = 'Uredi filter';
+$_['text_success']           = 'USPEŠNO: Ste uredili informacije!';
+$_['text_list']              = 'Seznam informativnih strani';
+$_['text_add']               = 'Dodaj informativno stran';
+$_['text_edit']              = 'Uredi informativno stran';
+$_['text_default']           = 'Privzeto';
 
 // Column
-$_['column_group']      = 'Skupina filtrov';
-$_['column_sort_order'] = 'Vrstni red';
-$_['column_action']     = 'Dejanje';
+$_['column_title']           = 'Naslov informativne strani';
+$_['column_sort_order']	     = 'Vrstni red';
+$_['column_action']          = 'Dejanje';
 
 // Entry
-$_['entry_group']       = 'Ime skupine filtrov';
-$_['entry_name']        = 'Ime filtra';
-$_['entry_sort_order']  = 'Vrstni red';
+$_['entry_title']            = 'Naslov informativne strani';
+$_['entry_description']      = 'Opis';
+$_['entry_store']            = 'Trgovina';
+$_['entry_meta_title'] 	     = 'Meta naslov';
+$_['entry_meta_keyword'] 	 = 'Meta ključne besede';
+$_['entry_meta_description'] = 'Meta opis';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Spodaj';
+$_['entry_status']           = 'Status';
+$_['entry_sort_order']       = 'Vrstni red';
+$_['entry_layout']           = 'Postavitev';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Prikaži v podnožju strani.';
 
 // Error
-$_['error_warning']     = 'POZOR: Prosimo, pazljivo preverite polja za morebitne napake!';
-$_['error_permission']  = 'POZOR: Nimate dovoljenja za urejanje filtrov!';
-$_['error_group']       = 'Ime skupine filtrov mora vsebovati med 1 in 64 znakov!';
-$_['error_name']        = 'Ime filtra mora vsebovati med 1 in 64 znakov!';
+$_['error_warning']          = 'POZOR: Prosimo, pazljivo preverite polja za morebitne napake!';
+$_['error_permission']       = 'POZOR: Nimate dovoljenja za urejanje informacij!';
+$_['error_title']            = 'Ime informacije mora imeti več kot 3 in manj kot 64 znakov!';
+$_['error_description']      = 'Opis mora vsebovati več kot 3 znake!';
+$_['error_meta_title']       = 'Meta Naslov mora imeti več kot 3 in manj kot 255 znakov!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'POZOR: Te strani me morete izbrisati, ker se zahteva za potrditev pri registraciji novega uporabnika!';
+$_['error_checkout']         = 'POZOR: Te strani me morete izbrisati, ker se zahteva za potrditev splošnih pogojev poslovanja pri oddaji naročila!';
+$_['error_affiliate']        = 'POZOR: Te strani me morete izbrisati, ker se zahteva za potrditev pri partnerskem programu!';
+$_['error_return']           = 'POZOR: Te strani me morete izbrisati, ker se zahteva za potrditev pri registraciji novega uporabnika!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
