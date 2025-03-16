dc9ac18c9c0dec655938add4aa47995611118fcb
OSWorX <info@osworx.net>
2025-03-16 11:08:32 +0100

    New translations filter.php (Hungarian)

--- upload/admin/language/en-gb/catalog/hu-HU.php
+++ upload/admin/language/en-gb/catalog/hu-HU.php
@@ -1,35 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = 'Letöltések';
+$_['heading_title']     = 'Szűrők';
 
 // Text
-$_['text_success']      = 'Siker: Letöltések módosítva!';
-$_['text_list']         = 'Letöltés lista';
-$_['text_add']          = 'Új letöltés';
-$_['text_edit']         = 'Letöltés szerkesztése';
-$_['text_upload']       = 'A fájl sikeresen feltöltve!';
+$_['text_success']      = 'Siker: Szűrők sikeresen módosítva!';
+$_['text_list']         = 'Lista szűrése';
+$_['text_add']          = 'Szűrő hozzáadása';
+$_['text_edit']         = 'Szűrő szerkesztése';
 
 // Column
-$_['column_name']       = 'Letöltés neve';
-$_['column_date_added'] = 'Hozzáadás dátuma';
+$_['column_group']      = 'Szűrőcsoport';
+$_['column_sort_order'] = 'Rendezés';
 $_['column_action']     = 'Művelet';
 
 // Entry
-$_['entry_name']        = 'Letöltés neve';
-$_['entry_filename']    = 'Fájlnév';
-$_['entry_mask']        = 'Maszk';
-
-// Help
-$_['help_filename']     = 'Feltölthet a feltöltés gomb segítségével, vagy FTP segítségével töltse fel a letöltési könyvtárat, és adja meg a részleteket az alábbi mezőben.';
-$_['help_mask']         = 'Ajánlatos, hogy a fájlnév és a maszk különbözőek legyenek, hogy megállítsuk a fájlok közvetlen letöltését.';
+$_['entry_group']       = 'Szűrő csoport neve';
+$_['entry_name']        = 'Szűrő neve';
+$_['entry_sort_order']  = 'Rendezés';
 
 // Error
 $_['error_warning']     = 'Figyelem: Kérjük, figyelmesen ellenőrizze a hibákat!';
-$_['error_permission']  = 'Figyelem: Nincs engedélye a letöltések változtatására!';
-$_['error_name']        = 'A letöltés név 3 és 64 karakter között kell lennie!';
-$_['error_upload']      = 'Feltöltés szükséges!';
-$_['error_filename']    = 'A fájlnév 3 és 128 karakter között kell lennie!';
-$_['error_exists']      = 'A fájl nem létezik!';
-$_['error_mask']        = 'A maszk 3 és 128 karakter között kell lennie!';
-$_['error_filetype']    = 'Érvénytelen fájltípus!';
-$_['error_product']     = 'Figyelem: Ez a letöltés nem törölhető, mert jelenleg hozzá van rendelve a(z) %s termékekhez!';
+$_['error_permission']  = 'Figyelmeztetés: Nincs elegendő jogosultság a szűrők módosításához!';
+$_['error_group']       = 'A szűrő csoport nevének 1 és 64 karakter között kell lennie!';
+$_['error_name']        = 'A szűrő nevének 1 és 64 karakter között kell lennie!';
