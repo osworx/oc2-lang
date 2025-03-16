dfd1b3d6b1d34c8a688bdaa2c95a8211ff8695ea
OSWorX <info@osworx.net>
2025-03-16 11:06:18 +0100

    New translations attribute_group.php (Latvian)

--- upload/admin/language/en-gb/catalog/lv-LV.php
+++ upload/admin/language/en-gb/catalog/lv-LV.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Atribūti';
+$_['heading_title']     = 'Atribūta Grupa';
 
 // Text
-$_['text_success']           = 'Veiksmīgi: Ir modificēti atribūti!';
-$_['text_list']              = 'Atribūtu saraksts';
-$_['text_add']               = 'Pievienot atribūtu';
-$_['text_edit']              = 'Labot atribūtu';
+$_['text_success']      = 'Veiksmīgi: Jūs esat modificējis atribūtu grupas!';
+$_['text_list']         = 'Atribūtu grupu saraksts';
+$_['text_add']          = 'Pievienot atribūtu grupu';
+$_['text_edit']         = 'Atribūtu grupas rediģēšana';
 
 // Column
-$_['column_name']            = 'Atribūta nosaukums';
-$_['column_attribute_group'] = 'Atribūta Grupa';
-$_['column_sort_order']      = 'Kārtošanas secība';
-$_['column_action']          = 'Darbība';
+$_['column_name']       = 'Atribūtu grupas nosaukums';
+$_['column_sort_order'] = 'Kārtošanas secība';
+$_['column_action']     = 'Darbība';
 
 // Entry
-$_['entry_name']             = 'Atribūta nosaukums';
-$_['entry_attribute_group']  = 'Atribūta Grupa';
-$_['entry_sort_order']       = 'Kārtošanas secība';
+$_['entry_name']        = 'Atribūtu grupas nosaukums';
+$_['entry_sort_order']  = 'Kārtošanas secība';
 
 // Error
-$_['error_warning']          = 'Brīdinājums: Lūdzu pārbaudiet formu rūpīgi ,ir dažas kļūdas!';
-$_['error_permission']       = 'Brīdinājums: Jums nav tiesību labot atribūtus!';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'Atribūta nosaukumam jābūt no 3 līdz 64 rakstzīmēm!';
-$_['error_product']          = 'Brīdinājums: Šo atribūtu nevar dzēst, jo tas pašlaik ir piešķirts %s precēm!';
+$_['error_warning']     = 'Brīdinājums: Lūdzu pārbaudiet formu rūpīgi ,ir dažas kļūdas!';
+$_['error_permission']  = 'Brīdinājums: Jums nav tiesību labot atribūtu grupas!';
+$_['error_name']        = 'Atribūts grupas nosaukumā jābūt no 3 līdz 64 rakstzīmēm!';
+$_['error_attribute']   = 'Brīdinājums: Šo atribūtu grupu nevar dzēst, jo tā pašlaik ir piešķirts %s atribūtiem!';
+$_['error_product']     = 'Brīdinājums: Šo atribūtu grupu nevar dzēst, jo tā pašlaik ir piešķirts %s precēm!';
