e91483029899c97f98b97e2f33f022c089824a86
OSWorX <info@osworx.net>
2025-03-16 11:11:31 +0100

    New translations option.php (Galician)

--- upload/admin/language/en-gb/catalog/gl-ES.php
+++ upload/admin/language/en-gb/catalog/gl-ES.php
@@ -1,35 +1,40 @@
 <?php
 // Heading
-$_['heading_title']     = 'Fabricantes';
+$_['heading_title']      = 'Opcións';
 
 // Text
-$_['text_success']      = 'Modifcouse o fabricante correctamente!';
-$_['text_list']         = 'Lista de fabricantes';
-$_['text_add']          = 'Engadir fabricante';
-$_['text_edit']         = 'Editar fabricante';
-$_['text_default']      = 'Por defecto';
-$_['text_percent']      = 'Porcentaxe';
-$_['text_amount']       = 'Cantidade fixa';
+$_['text_success']       = 'Modificaronse as opcións correctamente!';
+$_['text_list']          = 'Lista de opcións';
+$_['text_add']           = 'Engadir opción';
+$_['text_edit']          = 'Editar opción';
+$_['text_choose']        = 'Elixa';
+$_['text_select']        = 'Seleccionar';
+$_['text_radio']         = 'Radio';
+$_['text_checkbox']      = 'Caixas de verificación';
+$_['text_input']         = 'Entrada';
+$_['text_text']          = 'Texto';
+$_['text_textarea']      = 'Área de texto';
+$_['text_file']          = 'Ficheiro';
+$_['text_date']          = 'Data';
+$_['text_datetime']      = 'Data e hora';
+$_['text_time']          = 'Hora';
 
 // Column
-$_['column_name']       = 'Nome do fabricante';
-$_['column_sort_order'] = 'Orde da clasificación';
-$_['column_action']     = 'Acción';
+$_['column_name']        = 'Nome da opción';
+$_['column_sort_order']  = 'Orde da clasificación';
+$_['column_action']      = 'Acción';
 
 // Entry
-$_['entry_name']        = 'Nome do fabricante';
-$_['entry_store']       = 'Tendas';
-$_['entry_keyword']     = 'SEO URL';
-$_['entry_image']       = 'Imaxe';
-$_['entry_sort_order']  = 'Orde da clasificación';
-$_['entry_type']        = 'Tipo';
-
-// Help
-$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['entry_name']         = 'Nome da opción';
+$_['entry_type']         = 'Tipo';
+$_['entry_option_value'] = 'Valor do nome da opcion';
+$_['entry_image']        = 'Imaxe';
+$_['entry_sort_order']   = 'Orde da clasificación';
 
 // Error
-$_['error_warning']     = 'Aviso: Por favor, revise o formulario e busque os erros!';
-$_['error_permission']  = 'Aviso: non ten os permisos necesarios para modificar os fabricantes!';
-$_['error_name']        = 'O nome do fabricante debe ter entre 2 e 64 caracteres!';
-$_['error_keyword']     = 'SEO URL already in use!';
-$_['error_product']     = 'Aviso: este fabricante non pode ser eliminado xa que actualmente está asignado a %s produtos!';
+$_['error_warning']      = 'Aviso: Por favor, revise o formulario e busque os erros!';
+$_['error_permission']   = 'Aviso: non ten permisos para modificar as opcións!';
+$_['error_name']         = 'O nome da opcion debe ter entre 1 e 128 caracteres!';
+$_['error_type']         = 'Aviso: Valor da opción requirido!';
+$_['error_option_value'] = 'O nome do valor da opción debe ter entre 1 e 128 caracteres!';
+$_['error_product']      = 'Aviso: Esta opción non pode ser eliminada xa que actualmente esta asignada a %s productos!';
