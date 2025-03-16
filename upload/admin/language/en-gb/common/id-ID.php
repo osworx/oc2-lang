a683b5dfb656b22c5dc6b8c315fb4f9d49eb6083
OSWorX <info@osworx.net>
2025-03-16 14:28:04 +0100

    New translations footer.php (Indonesian)

--- upload/admin/language/en-gb/common/id-ID.php
+++ upload/admin/language/en-gb/common/id-ID.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Manajer Gambar';
-
 // Text
-$_['text_uploaded']    = 'Berhasil: File anda telah di unggah!';
-$_['text_directory']   = 'Berhasil: Direktori telah dibuat!';
-$_['text_delete']      = 'Berhasil: berkas atau direktori anda telah dihapus!';
-
-// Entry
-$_['entry_search']     = 'Cari..';
-$_['entry_folder']     = 'Nama Direktori';
-
-// Error
-$_['error_permission'] = 'Peringatan: Izin ditolak!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Peringatan: File atau direktori dengan nama yang sama sudah ada!';
-$_['error_directory']  = 'Peringatan: Direktori tidak ditemukan!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Peringatan: Jenis berkas salah!';
-$_['error_upload']     = 'Peringatan: Berkas tidak dapat di unggah untuk alasan yang tidak diketahui!';
-$_['error_delete']     = 'Peringatan: Anda tidak dapat menghapus direktori ini!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Versi %s';
