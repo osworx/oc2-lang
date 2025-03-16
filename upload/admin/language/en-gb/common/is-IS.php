7dfdd54bdae9b8b79d4e0f6c6c32400d452b9134
OSWorX <info@osworx.net>
2025-03-16 11:18:42 +0100

    New translations forgotten.php (Icelandic)

--- upload/admin/language/en-gb/common/is-IS.php
+++ upload/admin/language/en-gb/common/is-IS.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Gleymt lykilorð?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Version %s';
+$_['text_forgotten']  = 'Gleymt lykilorð';
+$_['text_your_email'] = 'Netfangið þitt';
+$_['text_email']      = 'Sláðu inn því netfangi sem tengt er við reikninginn. Smelltu á Senda og þú færð krækju senda á netfangið svo þú getir endursett lykilorðið.';
+$_['text_success']    = 'Póstur með staðfestingarkrækju hefur verið send á netfangið þitt.';
+
+// Entry
+$_['entry_email']     = 'Netfang';
+$_['entry_password']  = 'Nýtt lykilorð';
+$_['entry_confirm']   = 'Staðfesta';
+
+// Error
+$_['error_email']     = 'Ath. Netfangið fannst ekki í skrá.  Reyndu aftur!';
+$_['error_password']  = 'Password must be between 4 and 20 characters!';
+$_['error_confirm']   = 'Lykilorð og staðfest lykilorð passa ekki!';
