4969db53b521b3c9076bfc50cdb34c9fbdc6aa3a
OSWorX <info@osworx.net>
2025-03-16 11:18:22 +0100

    New translations forgotten.php (Hungarian)

--- upload/admin/language/en-gb/common/hu-HU.php
+++ upload/admin/language/en-gb/common/hu-HU.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Elfelejtette a jelszavát?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Verzió: %s';
+$_['text_forgotten']  = 'Elfelejtett jelszó';
+$_['text_your_email'] = 'Az Ön e-mail címe';
+$_['text_email']      = 'Adja meg az e-mailét, majd küldés gombra. Ezután jelszó-visszaállító linket e-mailben küldjük el.';
+$_['text_success']    = 'Egy megerõsítõ link el lett küldve az e-mail címére.';
+
+// Entry
+$_['entry_email']     = 'E-mail cím';
+$_['entry_password']  = 'Új jelszó';
+$_['entry_confirm']   = 'Új jelszó mégegyszer';
+
+// Error
+$_['error_email']     = 'A megadott e-mail cím nem szerepel adatbázisunkban! Kérjük, hogy próbálja meg újra!';
+$_['error_password']  = 'Password must be between 4 and 20 characters!';
+$_['error_confirm']   = 'A két jelszó nem egyezik!';
