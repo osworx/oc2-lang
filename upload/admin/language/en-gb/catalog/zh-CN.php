a150410844ad16bff1e84e1fa96293a6fce1b899
OSWorX <info@osworx.net>
2025-03-16 11:13:58 +0100

    New translations review.php (Chinese Simplified)

--- upload/admin/language/en-gb/catalog/zh-CN.php
+++ upload/admin/language/en-gb/catalog/zh-CN.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= '重复配置文件';
+$_['heading_title']     = '商品点评';
 
 // Text
-$_['text_success']          = ' 您已成功修改了重复配置文件!';
-$_['text_list']             = '重复配置文件列表';
-$_['text_add']              = '添加重复配置文件';
-$_['text_edit']             = '编辑重复配置文件';
-$_['text_day']				= '天';
-$_['text_week']				= '周';
-$_['text_semi_month']		= '半个月';
-$_['text_month']			= '每月';
-$_['text_year']				= '每年';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> 定期金额必须重复和周期计算。</p><p>例如如果您使用"周"频率和周期为"2"，然后用户将每 2 个星期结算一次。</p><p>持续时间是用户付款的次数,如果你想要他们取消付款,则将此值设置为 0。</p>';
-$_['text_profile']			= '重复配置文件';
-$_['text_trial']			= '试用';
-
-// Entry
-$_['entry_name']		    = '名称';
-$_['entry_price']			= '单价';
-$_['entry_duration']		= '持续时间';
-$_['entry_cycle']			= '周期';
-$_['entry_frequency']		= '频率';
-$_['entry_trial_price']		= '价格';
-$_['entry_trial_duration']	= '试用持续时间';
-$_['entry_trial_status']	= '试用状态';
-$_['entry_trial_cycle']	    = '试用周期';
-$_['entry_trial_frequency'] = '试用频率';
-$_['entry_status']			= '状态';
-$_['entry_sort_order']		= '排列顺序';
+$_['text_success']      = '修改成功: 您已经修改了评论.';
+$_['text_list']         = '评论列表';
+$_['text_add']          = '我要点评';
+$_['text_edit']         = '编辑评论';
 
 // Column
-$_['column_name']			= '名称';
-$_['column_sort_order']	    = '排列顺序';
-$_['column_action']         = '操作';
+$_['column_product']    = '产品列表';
+$_['column_author']     = '点评人列表';
+$_['column_rating']     = '等级分';
+$_['column_status']     = '状态';
+$_['column_date_added'] = '添加日期';
+$_['column_action']     = '操作';
+
+// Entry
+$_['entry_product']     = '产品列表';
+$_['entry_author']      = '点评人列表';
+$_['entry_rating']      = '等级分';
+$_['entry_status']      = '状态';
+$_['entry_text']        = '文本框';
+$_['entry_date_added']  = '添加日期';
+
+// Help
+$_['help_product']      = '自动完成';
 
 // Error
-$_['error_warning']         = '警告:请仔细检查错误格式!';
-$_['error_permission']		= '警告︰ 您没有权限修改重复配置文件 ！';
-$_['error_name']			= '产品名称必须介于3-255 个字符之间!';
-$_['error_product']			= '警告︰ 不能删除此重复的配置文件，因为它目前分配给 %s 产品 ！';
+$_['error_warning']     = '警告:请仔细检查错误格式!';
+$_['error_permission']  = '提示: 您没有权限修改评论!';
+$_['error_product']     = '产品错误! 请选择要评论的产品.';
+$_['error_author']      = '点评人名称长度为3到64个字符长度';
+$_['error_text']        = '请至少输入一个字符!';
+$_['error_rating']      = '请选择一个评分等级!';
