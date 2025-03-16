1ccd7e3e0b53eaccf50b2e792cd35c0399b9a603
OSWorX <info@osworx.net>
2025-03-16 14:26:54 +0100

    New translations filemanager.php (Italian)

--- upload/admin/language/en-gb/common/it-IT.php
+++ upload/admin/language/en-gb/common/it-IT.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Pannello di amministrazione';
+$_['heading_title']    = 'Gestore delle Immagini';
+
+// Text
+$_['text_uploaded']    = 'Il file è stato caricato con successo!';
+$_['text_directory']   = 'Directory creata con successo!';
+$_['text_delete']      = 'La directory o il file è stato eliminato con successo!';
+
+// Entry
+$_['entry_search']     = 'Cerca..';
+$_['entry_folder']     = 'Nome cartella';
 
 // Error
-$_['error_install'] = 'Avviso: La cartella "Install" deve essere eliminata per ragioni di sicurezza ! ';
+$_['error_permission'] = 'Avviso: Autorizzazione negata!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Attenzione: Un file o una directory con lo stesso nome esiste gi&agrave!';
+$_['error_directory']  = 'Attenzione: La Directory non esiste!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Avviso: Tipo file errato!';
+$_['error_upload']     = 'Attenzione: Il File non potrebbe essere caricato per un motivo sconosciuto!';
+$_['error_delete']     = 'Attenzione: Non &egrave possibile eliminare questa directory!';
