84d4588292c731dea500043d60a8071778c4e5a7
OSWorX <info@osworx.net>
2025-03-16 11:21:20 +0100

    New translations reset.php (German, Switzerland)

--- upload/admin/language/en-gb/common/de-CH.php
+++ upload/admin/language/en-gb/common/de-CH.php
@@ -1,19 +1,15 @@
 <?php
 // header
-$_['heading_title']  = 'Verwaltung';
+$_['heading_title']  = '';
 
 // Text
-$_['text_heading']   = 'Verwaltung';
-$_['text_login']     = 'Anmeldung Verwaltung';
-$_['text_forgotten'] = 'Passwort vergessen';
+$_['text_password']  = 'Neues Passwort angeben (zw. 5 &amp; 20 Zeichen)';
+$_['text_success']   = 'Passwort wurde erfolgreich aktualisiert';
 
 // Entry
-$_['entry_username'] = 'Benutzername';
 $_['entry_password'] = 'Passwort';
-
-// Button
-$_['button_login']   = 'Anmelden';
+$_['entry_confirm']  = '';
 
 // Error
-$_['error_login']    = 'Benutzername und/oder Passwort falsch';
-$_['error_token']    = 'Sitzung ist abgelaufen - bitte nochmals anmelden';
+$_['error_password'] = 'Passwort muss zwischen 4 und 20 Zeichen lang sein';
+$_['error_confirm']  = '';
