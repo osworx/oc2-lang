fe5814a08621404a493546cd9ad40f3ab8e44d35
OSWorX <info@osworx.net>
2025-03-16 11:06:56 +0100

    New translations category.php (Turkish)

--- upload/admin/language/en-gb/catalog/tr-TR.php
+++ upload/admin/language/en-gb/catalog/tr-TR.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Öznitelik Grupları';
+$_['heading_title']          = 'Kategoriler';
 
 // Text
-$_['text_success']      = 'Başarı: Öznitelik gruplarını değiştirdiniz!';
-$_['text_list']         = 'Öznitelik Grup listesi';
-$_['text_add']          = 'Öznitelik Grubu Ekle';
-$_['text_edit']         = 'Öznitelik Grubu Düzenle';
+$_['text_success']           = 'Başarılı: Kategorileri düzenlediniz!';
+$_['text_list']              = 'Kategori Listesi';
+$_['text_add']               = 'Kategori Ekle';
+$_['text_edit']              = 'Kategoriyi Düzenle';
+$_['text_default']           = 'Varsayılan';
 
 // Column
-$_['column_name']       = 'Öznitelik Grup Adı';
-$_['column_sort_order'] = 'Sıralama Düzeni';
-$_['column_action']     = 'Eylem';
+$_['column_name']            = 'Kategori Adı';
+$_['column_sort_order']      = 'Sıralama Düzeni';
+$_['column_action']          = 'Eylem';
 
 // Entry
-$_['entry_name']        = 'Öznitelik Grup Adı';
-$_['entry_sort_order']  = 'Sıralama Düzeni';
+$_['entry_name']             = 'Kategori Adı';
+$_['entry_description']      = 'Açıklama';
+$_['entry_meta_title'] 	     = 'Meta etiketi başlığı';
+$_['entry_meta_keyword']     = 'Meta etiketi anahtar kelimeler';
+$_['entry_meta_description'] = 'Meta etiketi açıklamasına';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Üst';
+$_['entry_filter']           = 'Filtreler';
+$_['entry_store']            = 'Dükkanlar';
+$_['entry_image']            = 'Resim';
+$_['entry_top']              = 'Üst';
+$_['entry_column']           = 'Sütunlar';
+$_['entry_sort_order']       = 'Sıralama Düzeni';
+$_['entry_status']           = 'Durum';
+$_['entry_layout']           = 'Düzeni Geçersiz Kılma';
+
+// Help
+$_['help_filter']            = '(Otomatik Tamamla)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Üst menü çubuğunda görüntülenir. Sadece en üst kategoriler için çalışır.';
+$_['help_column']            = '3 alt kategori için kullanılacak sütun sayısı. Sadece en üst kategorileri için çalışır.';
 
 // Error
-$_['error_warning']     = 'Uyarı: Lütfen formu gösterilen hatalar nedeniyle dikkatlice kontrol edin!';
-$_['error_permission']  = 'Uyarı: Öznitelik gruplarını değiştirme iznine sahip değilsiniz!';
-$_['error_name']        = 'Öznitelik grup adı 3 ile 64 karakter arasında olmalıdır!';
-$_['error_attribute']   = 'Uyarı: Bu öznitelik grubu halen  %s özniteliklerine bağlı olduğundan silinemez!';
-$_['error_product']     = 'Uyarı: Bu öznitelik grubu halen %s ürününe atandığından silinemez!';
+$_['error_warning']          = 'Uyarı: Lütfen formu gösterilen hatalar nedeniyle dikkatlice kontrol edin!';
+$_['error_permission']       = 'Uyarı: Kategorileri değiştirme iznine sahip değilsiniz!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Meta Başlık en az 3 ve en fazla 255 karakter olmalıdır!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
