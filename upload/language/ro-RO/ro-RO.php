944075fb2ac9b000c6e9cde1a1a45274e55b30ec
OSWorX <info@osworx.net>
2025-03-16 09:06:01 +0100

    New translations header.php (Romanian)

--- upload/language/ro-RO/ro-RO.php
+++ upload/language/ro-RO/ro-RO.php
@@ -1,19 +1,23 @@
 <?php
-// header
-$_['heading_title']   = 'V-aţi uitat parola?';
+// Heading
+$_['heading_title']          = 'OpenCart';
 
 // Text
-$_['text_forgotten']  = 'Parolă uitată';
-$_['text_your_email'] = 'Adresa de e-mail';
-$_['text_email']      = 'Introduceţi adresa de e-mail asociată contului. Faceţi clic pe Trimite pentru a vi se trimite prin e-mail un link de resetare a parolei.';
-$_['text_success']    = 'Un mesaj e-mail cu un link de confirmare v-a fost trimis la adresa de e-mail de admin.';
-
-// Entry
-$_['entry_email']     = 'Adresa de e-mail';
-$_['entry_password']  = 'Noua parola';
-$_['entry_confirm']   = 'Confirmare';
-
-// Error
-$_['error_email']     = 'Avertisment: Adresa de e-mail nu a fost găsită în evidenţele noastre, vă rugăm să încercaţi din nou!';
-$_['error_password']  = 'Parola trebuie să fie între 4 și 20 de caractere!';
-$_['error_confirm']   = 'Parola și confirmarea parolei nu coincid!';
+$_['text_order']             = 'Comenzi';
+$_['text_processing_status'] = 'Procesare';
+$_['text_complete_status']   = 'Finalizat';
+$_['text_customer']          = 'Clienți';
+$_['text_online']            = 'Clienți online';
+$_['text_approval']          = 'În așteptarea aprobării';
+$_['text_product']           = 'Produse';
+$_['text_stock']             = 'Stoc epuizat';
+$_['text_review']            = 'Opinii';
+$_['text_return']            = 'Retururi';
+$_['text_affiliate']         = 'Afiliați';
+$_['text_store']             = 'Magazine';
+$_['text_front']             = 'Fața Magazinului';
+$_['text_help']              = 'Ajutor';
+$_['text_homepage']          = 'OpenCart Homepage';
+$_['text_support']           = 'Forum de asistență';
+$_['text_documentation']     = 'Documentație';
+$_['text_logout']            = 'Delogare';
