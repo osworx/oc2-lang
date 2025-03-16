28988755b4f1818feada810ee566ad11e8a2434a
OSWorX <info@osworx.net>
2025-03-16 14:24:03 +0100

    New translations review.php (Dutch)

--- upload/admin/language/en-gb/catalog/nl-NL.php
+++ upload/admin/language/en-gb/catalog/nl-NL.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'Wederkerende Profielen';
+$_['heading_title']     = 'Recensies';
 
 // Text
-$_['text_success']          = 'Gefeliciteerd: U heeft de wederkerende profielen gewijzigd!';
-$_['text_list']             = 'Wederkerende profielen lijst';
-$_['text_add']              = 'Voeg een wederkerend profiel toe';
-$_['text_edit']             = 'Wederkerend profiel aanpassen';
-$_['text_day']				= 'Dag';
-$_['text_week']				= 'Week';
-$_['text_semi_month']		= 'Halve maand';
-$_['text_month']			= 'Maand';
-$_['text_year']				= 'Jaar';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Wederkerende bedragen berekend op basis van Veelvuldigheid en Cyclus.</p><p>Bijvoorbeeld indien u op basis van een "week" en een cyclus van "2" ,Dan zal de gebruiker om de 2 weken gefactureerd worden.</p><p>De duur is het aantal keer dat de gebruiker een betaling doet,zet de cyclus op 0 indien u betalingen wenst tot wanneer deze geannuleerd word.</p>';
-$_['text_profile']			= 'Periodiek profiel';
-$_['text_trial']			= 'Proef Profiel';
-
-// Entry
-$_['entry_name']		    = 'Naam';
-$_['entry_price']			= 'Prijs per stuk';
-$_['entry_duration']		= 'Duur';
-$_['entry_cycle']			= 'Cyclus';
-$_['entry_frequency']		= 'Frequentie';
-$_['entry_trial_price']		= 'Proef prijs';
-$_['entry_trial_duration']	= 'Duur Proef Periode';
-$_['entry_trial_status']	= 'Proef status';
-$_['entry_trial_cycle']	    = 'Proef cyclus';
-$_['entry_trial_frequency'] = 'Proef frequentie';
-$_['entry_status']			= 'status';
-$_['entry_sort_order']		= 'Sorteervolgorde';
+$_['text_success']      = 'Gefeliciteerd: U heeft de Recensies gewijzigd!';
+$_['text_list']         = 'Recensie lijst';
+$_['text_add']          = 'Voeg een Recensie toe';
+$_['text_edit']         = 'Recensie bewerken';
 
 // Column
-$_['column_name']			= 'Naam';
-$_['column_sort_order']	    = 'Sorteervolgorde';
-$_['column_action']         = 'Aktie';
+$_['column_product']    = 'Product';
+$_['column_author']     = 'Auteur';
+$_['column_rating']     = 'Waardering';
+$_['column_status']     = 'status';
+$_['column_date_added'] = 'Datum';
+$_['column_action']     = 'Aktie';
+
+// Entry
+$_['entry_product']     = 'Product';
+$_['entry_author']      = 'Auteur';
+$_['entry_rating']      = 'Waardering';
+$_['entry_status']      = 'status';
+$_['entry_text']        = 'Tekst';
+$_['entry_date_added']  = 'Datum';
+
+// Help
+$_['help_product']      = '(AutoAanvullen)';
 
 // Error
-$_['error_warning']         = 'Waarschuwing: Controlleer of alle verplichte velden volledig zijn ingevuld!';
-$_['error_permission']		= 'Opgelet: U heeft geen machtiging om wederkerende profielen te wijzigen!';
-$_['error_name']			= 'Een profielnaam dient minimaal 3 en minder dan 255 karakters te bevatten.';
-$_['error_product']			= 'Opgelet: Dit wederkerend profiel kan niet verwijderd worden omdat deze nog aan %s is toegewezen.';
+$_['error_warning']     = 'Waarschuwing: Controlleer of alle verplichte velden volledig zijn ingevuld!';
+$_['error_permission']  = 'Opgelet: U heeft geen rechten om recensies te wijzigen!';
+$_['error_product']     = 'Product vereist!';
+$_['error_author']      = 'De auteur moet tussen de 3 en 64 karakters bevatten!';
+$_['error_text']        = 'Recensie tekst moet minimaal 1 karakter bevatten!';
+$_['error_rating']      = 'Recensie beoordeling vereist!';
