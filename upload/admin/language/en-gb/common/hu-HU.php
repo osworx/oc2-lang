53ea5c7db5edc63b5a6500a2021f08b1e482830f
OSWorX <info@osworx.net>
2025-03-16 11:16:35 +0100

    New translations filemanager.php (Hungarian)

--- upload/admin/language/en-gb/common/hu-HU.php
+++ upload/admin/language/en-gb/common/hu-HU.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Vezérlőpult';
+$_['heading_title']    = 'Kép kezelő';
+
+// Text
+$_['text_uploaded']    = 'Siker: A file-t feltöltöttük!';
+$_['text_directory']   = 'Siker: A könyvtár elkészült!';
+$_['text_delete']      = 'Siker: A file vagy könyvtár törölve!';
+
+// Entry
+$_['entry_search']     = 'Keresés..';
+$_['entry_folder']     = 'Mappa neve';
 
 // Error
-$_['error_install'] = 'Figyelmeztetés: Telepítési mappa továbbra is létezik, és le kell, biztonsági okokból!';
+$_['error_permission'] = 'Figyelmeztetés: Az engedély megtagadva!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Figyelmeztetés: Ez a fájlnév vagy könyvtárnév már létezik!';
+$_['error_directory']  = 'Figyelmeztetés: Könyvtár nem létezik!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Figyelmeztetés: Helytelen a fájl típus!';
+$_['error_upload']     = 'Figyelmeztetés: Fájl nem tölthető fel ismeretlen okból!';
+$_['error_delete']     = 'Figyelmeztetés: Nem lehet törölni ezt a könyvtárat!';
