1f47b6f7cf6e947a3f1902d5cf3f73fccac54593
OSWorX <info@osworx.net>
2025-03-16 11:13:59 +0100

    New translations review.php (Chinese Traditional)

--- upload/admin/language/en-gb/catalog/zh-TW.php
+++ upload/admin/language/en-gb/catalog/zh-TW.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= '定期設定檔';
+$_['heading_title']     = '評論';
 
 // Text
-$_['text_success']          = '成功：您已經修改分期設定檔！';
-$_['text_list']             = '分期設定檔清單';
-$_['text_add']              = '新增分期設定檔';
-$_['text_edit']             = '編輯分期設定檔';
-$_['text_day']				= '日';
-$_['text_week']				= '週';
-$_['text_semi_month']		= '半個月';
-$_['text_month']			= '月';
-$_['text_year']				= '年';
-$_['text_recurring']	    = '<p> <i class="fa fa-info-circle"> </i> 定期金額由頻率和週期計算. </p><p> 例如, 如果您使用 "周" 的頻率和 "2" 的週期, 則使用者將每2周計費一次. </p><p> 持續時間是使用者進行付款的次數, 如果您希望在取消之前付款, 則將其設置為 0. </p>';
-$_['text_profile']			= '分期設定';
-$_['text_trial']			= '試用簡介';
-
-// Entry
-$_['entry_name']		    = '名稱';
-$_['entry_price']			= '價格';
-$_['entry_duration']		= '長度';
-$_['entry_cycle']			= '週期';
-$_['entry_frequency']		= '頻率';
-$_['entry_trial_price']		= '試用價格';
-$_['entry_trial_duration']	= '試用時間';
-$_['entry_trial_status']	= '試用狀態';
-$_['entry_trial_cycle']	    = '試驗週期';
-$_['entry_trial_frequency'] = '試用頻率';
-$_['entry_status']			= '狀態';
-$_['entry_sort_order']		= '排序次序';
+$_['text_success']      = '成功：您已經修改評論！';
+$_['text_list']         = '評論清單';
+$_['text_add']          = '新增評論';
+$_['text_edit']         = '編輯評論';
 
 // Column
-$_['column_name']			= '名稱';
-$_['column_sort_order']	    = '排序次序';
-$_['column_action']         = '動作';
+$_['column_product']    = '商品';
+$_['column_author']     = '作者';
+$_['column_rating']     = '評分';
+$_['column_status']     = '狀態';
+$_['column_date_added'] = '新增日期';
+$_['column_action']     = '動作';
+
+// Entry
+$_['entry_product']     = '商品';
+$_['entry_author']      = '作者';
+$_['entry_rating']      = '評分';
+$_['entry_status']      = '狀態';
+$_['entry_text']        = '文字';
+$_['entry_date_added']  = '新增日期';
+
+// Help
+$_['help_product']      = '(自動完成)';
 
 // Error
-$_['error_warning']         = '警告：請仔細檢查表格的錯誤！';
-$_['error_permission']		= '警告：您沒有修改分期設定檔的權限！';
-$_['error_name']			= '設定檔名稱必須大於 3 且小於 255 個字元之間！';
-$_['error_product']			= '警告：無法刪除此分期設定檔，因為目前已指派給 %s 個商品！';
+$_['error_warning']     = '警告：請仔細檢查表格的錯誤！';
+$_['error_permission']  = '警告：您沒有修改評論的權限！';
+$_['error_product']     = '需要商品！';
+$_['error_author']      = '作者必須介於 3 到 64 個字元之間！';
+$_['error_text']        = '評論文字必須至少 1 個字元！';
+$_['error_rating']      = '需要評論評分！';
