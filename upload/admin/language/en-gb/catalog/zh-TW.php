4f445d2a4ab0c561090226974d68073b12840bba
OSWorX <info@osworx.net>
2025-03-16 14:21:52 +0100

    New translations option.php (Chinese Traditional)

--- upload/admin/language/en-gb/catalog/zh-TW.php
+++ upload/admin/language/en-gb/catalog/zh-TW.php
@@ -1,35 +1,40 @@
 <?php
 // Heading
-$_['heading_title']     = '製造商';
+$_['heading_title']      = '選項';
 
 // Text
-$_['text_success']      = '成功：您已經修改製造商！';
-$_['text_list']         = '製造商清單';
-$_['text_add']          = '新增製造商';
-$_['text_edit']         = '編輯製造商';
-$_['text_default']      = '預設';
-$_['text_percent']      = '百分比';
-$_['text_amount']       = '固定金額';
+$_['text_success']       = '成功：您已經修改選項！';
+$_['text_list']          = '選項清單';
+$_['text_add']           = '新增選項';
+$_['text_edit']          = '編輯選項';
+$_['text_choose']        = '選擇';
+$_['text_select']        = '選取';
+$_['text_radio']         = '選項按鈕';
+$_['text_checkbox']      = '核取方塊';
+$_['text_input']         = '插入';
+$_['text_text']          = '文字';
+$_['text_textarea']      = '文字區域';
+$_['text_file']          = '檔案';
+$_['text_date']          = '日期';
+$_['text_datetime']      = '日期和時間';
+$_['text_time']          = '時間';
 
 // Column
-$_['column_name']       = '製造商名稱';
-$_['column_sort_order'] = '排序次序';
-$_['column_action']     = '動作';
+$_['column_name']        = '選項名稱';
+$_['column_sort_order']  = '排序次序';
+$_['column_action']      = '動作';
 
 // Entry
-$_['entry_name']        = '製造商名稱';
-$_['entry_store']       = '商店';
-$_['entry_keyword']     = 'SEO URL';
-$_['entry_image']       = '圖像';
-$_['entry_sort_order']  = '排序次序';
-$_['entry_type']        = '類型';
-
-// Help
-$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['entry_name']         = '選項名稱';
+$_['entry_type']         = '類型';
+$_['entry_option_value'] = '選項值名稱';
+$_['entry_image']        = '圖像';
+$_['entry_sort_order']   = '排序次序';
 
 // Error
-$_['error_warning']     = '警告：請仔細檢查表格的錯誤！';
-$_['error_permission']  = '警告：您沒有修改製造商的權限！';
-$_['error_name']        = '製造商名稱必須介於 2 到 64 個字元之間！';
-$_['error_keyword']     = 'SEO URL already in use!';
-$_['error_product']     = '警告：無法刪除這個製造商，因為目前已指派給 %s 個商品！';
+$_['error_warning']      = '警告：請仔細檢查表格的錯誤！';
+$_['error_permission']   = '警告：您沒有修改選項的權限！';
+$_['error_name']         = '選項名稱必須介於 1 到 128 個字元之間！';
+$_['error_type']         = '警告：需要選項值！';
+$_['error_option_value'] = '選項值名稱必須介於 1 到 128 個字元之間！';
+$_['error_product']      = '警告：無法刪除這個選項，因為目前已指派給 %s 個商品！';
