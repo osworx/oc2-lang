4d0338950d909eac33909c5f0bfcb0e2c7f3c293
OSWorX <info@osworx.net>
2025-03-16 11:18:56 +0100

    New translations forgotten.php (Serbian (Latin))

--- upload/admin/language/en-gb/common/sr-CS.php
+++ upload/admin/language/en-gb/common/sr-CS.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Zaboravili ste šifru?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Verzija %s';
+$_['text_forgotten']  = 'Zaboravljena Šifra';
+$_['text_your_email'] = 'Vaša Adresa E-Pošte';
+$_['text_email']      = 'Unesite adresu e-pošte povezanu sa vašim nalogom. Kliknite pošalji da bi dobili poništenje lozinke na vašu e-adresu.';
+$_['text_success']    = 'E-poruka je poslata sa vezom za potvrdu na vaš admin adresu e-pošte.';
+
+// Entry
+$_['entry_email']     = 'Adresa E-Pošte';
+$_['entry_password']  = 'Nova Šifra';
+$_['entry_confirm']   = 'Potvrdi';
+
+// Error
+$_['error_email']     = 'Upozorenje: Navedena E-Mail adresa nije registrovana kod nas, pokušajte ponovo!';
+$_['error_password']  = 'Šifra mora biti između 4 i 20 karaktera!';
+$_['error_confirm']   = 'Šifra i Podvrdna Šifra se ne poklapaju!';
