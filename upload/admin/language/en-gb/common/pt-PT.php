714efe283d1867d8c4b9288e9084c302246e502f
OSWorX <info@osworx.net>
2025-03-16 11:19:20 +0100

    New translations header.php (Portuguese)

--- upload/admin/language/en-gb/common/pt-PT.php
+++ upload/admin/language/en-gb/common/pt-PT.php
@@ -1,19 +1,23 @@
 <?php
-// header
-$_['heading_title']   = 'Esqueceu sua Palavra-Chave?';
+// Heading
+$_['heading_title']          = 'OpenCart';
 
 // Text
-$_['text_forgotten']  = 'Palavra-chave Esquecida';
-$_['text_your_email'] = 'O seu E-mail';
-$_['text_email']      = 'Coloque o endereço de E-mail com que registou à sua conta. Clique em enviar para receber um email com um link para redefinir a sua Palavra-Chave.';
-$_['text_success']    = 'Um email com o link de confirmação foi enviado para o seu Email.';
-
-// Entry
-$_['entry_email']     = 'E-mail';
-$_['entry_password']  = 'Nova Palavra-Chave';
-$_['entry_confirm']   = 'Confirmar';
-
-// Error
-$_['error_email']     = 'Aviso: O endereço de E-mail não foi encontrado na nossa base de dados. Por favor, tente novamente!';
-$_['error_password']  = 'A palavra-passe deve conter entre 4 a 20 carateres!';
-$_['error_confirm']   = 'A Palavra-Chave e a confirmação da Palavra-Chave não são iguais!';
+$_['text_order']             = 'Encomendas';
+$_['text_processing_status'] = 'Processing';
+$_['text_complete_status']   = 'Concluída';
+$_['text_customer']          = 'Clientes';
+$_['text_online']            = 'Clientes Online';
+$_['text_approval']          = 'Pendente de Aprovação';
+$_['text_product']           = 'Produtos';
+$_['text_stock']             = 'Sem Stock';
+$_['text_review']            = 'Comentários';
+$_['text_return']            = 'Devoluções';
+$_['text_affiliate']         = 'Afiliados';
+$_['text_store']             = 'Lojas';
+$_['text_front']             = 'Frente de Loja';
+$_['text_help']              = 'Ajuda';
+$_['text_homepage']          = 'OpenCart Homepage';
+$_['text_support']           = 'Fórum de Suporte';
+$_['text_documentation']     = 'Documentação';
+$_['text_logout']            = 'Saír';
