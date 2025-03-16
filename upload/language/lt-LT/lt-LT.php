38ae68610adf1f032dc3c48ea427f057249049d9
OSWorX <info@osworx.net>
2025-03-16 09:11:14 +0100

    New translations information.php (Lithuanian)

--- upload/language/lt-LT/lt-LT.php
+++ upload/language/lt-LT/lt-LT.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'Filtrai';
+$_['heading_title']          = 'Informacija';
 
 // Text
-$_['text_success']      = 'Atlikta: Sėkmingai pakeitėte filtrus!';
-$_['text_list']         = 'Filtrų sąrašas';
-$_['text_add']          = 'Pridėti filtrą';
-$_['text_edit']         = 'Redaguoti filtrą';
+$_['text_success']           = 'Atlikta: Jūs atnaujinote informaciją!';
+$_['text_list']              = 'Informacijos sąrašas';
+$_['text_add']               = 'Pridėti informaciją';
+$_['text_edit']              = 'Redaguoti informaciją';
+$_['text_default']           = 'Numatytasis';
 
 // Column
-$_['column_group']      = 'Filtrų grupės';
-$_['column_sort_order'] = 'Rikiavimo tvarka';
-$_['column_action']     = 'Veiksmas';
+$_['column_title']           = 'Informacijos pavadinimas';
+$_['column_sort_order']	     = 'Rikiavimo tvarka';
+$_['column_action']          = 'Veiksmas';
 
 // Entry
-$_['entry_group']       = 'Filtrų grupės pavadinimas';
-$_['entry_name']        = 'Filtro pavadinimas';
-$_['entry_sort_order']  = 'Rikiavimo tvarka';
+$_['entry_title']            = 'Informacijos pavadinimas';
+$_['entry_description']      = 'Aprašymas';
+$_['entry_store']            = 'Parduotuvės';
+$_['entry_meta_title'] 	     = 'Meta Tago pavadinimas';
+$_['entry_meta_keyword'] 	 = 'Meta Tag Raktažodžiai';
+$_['entry_meta_description'] = 'Meta Tag Aprašymas';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Į apačia';
+$_['entry_status']           = 'Statusas';
+$_['entry_sort_order']       = 'Rikiavimo tvarka';
+$_['entry_layout']           = 'Šablono valdymas';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Rodyti apatinėje juostoje.';
 
 // Error
-$_['error_warning']     = 'Įspėjimas: Patikrinkite klaidas formoje!';
-$_['error_permission']  = 'Įspėjimas: Jūs neturite teisės redaguoti filtrus!';
-$_['error_group']       = 'Filtrų grupės pavadinimas turi būti tarp 1 ir 64 simbolių!';
-$_['error_name']        = 'Filtro pavadinimas turi būti tarp 1 ir 64 simbolių!';
+$_['error_warning']          = 'Įspėjimas: Patikrinkite klaidas formoje!';
+$_['error_permission']       = 'Įspėjimas: Jūs neturite teisės keisti informacijos!';
+$_['error_title']            = 'Informacijos pavadinimas turi būti nuo 3 iki 64 simbolių ilgio!';
+$_['error_description']      = 'Aprašymas turi būti daugiau nei 3 simboliai!';
+$_['error_meta_title']       = 'Meta pavadinimas turi būti ilgesnis kaip 3 ir trumpesnis kaip 255 simboliai!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'Įspėjimas: Šis informacinis puslapis negali būti pašalintas, nes jis susijęs su puslapio terminais!';
+$_['error_checkout']         = 'Įspėjimas: Šis informacinis puslapis negali būti pašalintas, nes jis susijęs su puslapio terminais!';
+$_['error_affiliate']        = 'Įspėjimas: Šis informacinis puslapis negali būti pašalintas, nes jis susijęs su puslapio terminais!';
+$_['error_return']           = 'Įspėjimas: Šis informacinis puslapis negali būti pašalintas, nes jis susijęs su puslapio terminais!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
