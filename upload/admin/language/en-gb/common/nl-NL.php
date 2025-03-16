ab8aafaeebb0d64323c21fd5f85e5bd9af8e8a98
OSWorX <info@osworx.net>
2025-03-16 11:19:18 +0100

    New translations header.php (Dutch)

--- upload/admin/language/en-gb/common/nl-NL.php
+++ upload/admin/language/en-gb/common/nl-NL.php
@@ -1,19 +1,23 @@
 <?php
-// header
-$_['heading_title']   = 'Je wachtwoord vergeten?';
+// Heading
+$_['heading_title']          = 'Openkaart';
 
 // Text
-$_['text_forgotten']  = 'Vergeten wachtwoord';
-$_['text_your_email'] = 'Uw e-mailadres';
-$_['text_email']      = 'Voer het e-mailadres gekoppeld aan uw account in.Klik op verzenden om de wachtwoord reset link te verzenden naar jou e-mailadres.';
-$_['text_success']    = 'Een email met een bevestigingslink is naar jou beheerder e-mailadres gestuurd.';
-
-// Entry
-$_['entry_email']     = 'E-mailadres';
-$_['entry_password']  = 'Nieuw wachtwoord';
-$_['entry_confirm']   = 'Bevestigen';
-
-// Error
-$_['error_email']     = 'Opgelet: Het e-mailadres is niet gevonden in onze bestanden, probeert u opnieuw.';
-$_['error_password']  = 'Password must be between 4 and 20 characters!';
-$_['error_confirm']   = 'Wachtwoorden komen niet overeen.';
+$_['text_order']             = 'Bestellingen';
+$_['text_processing_status'] = 'Processing';
+$_['text_complete_status']   = 'Voltooid';
+$_['text_customer']          = 'Klanten';
+$_['text_online']            = 'Online klanten';
+$_['text_approval']          = 'Wachtende op goedkeuring';
+$_['text_product']           = 'Producten';
+$_['text_stock']             = 'Geen voorraad';
+$_['text_review']            = 'Recensies';
+$_['text_return']            = 'Opbrengst';
+$_['text_affiliate']         = 'Leden';
+$_['text_store']             = 'Winkel(s)';
+$_['text_front']             = 'Winkel voorkant';
+$_['text_help']              = 'Help';
+$_['text_homepage']          = 'OpenCart Homepage';
+$_['text_support']           = 'Help Forum';
+$_['text_documentation']     = 'Documentatie';
+$_['text_logout']            = 'Uitloggen';
