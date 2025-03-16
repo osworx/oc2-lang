d3e6cbf8af77ed27acdc737ecb4802dc4da80dc0
OSWorX <info@osworx.net>
2025-03-16 14:19:16 +0100

    New translations filter.php (Lithuanian)

--- upload/admin/language/en-gb/catalog/lt-LT.php
+++ upload/admin/language/en-gb/catalog/lt-LT.php
@@ -1,35 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = 'Atsisiuntimai';
+$_['heading_title']     = 'Filtrai';
 
 // Text
-$_['text_success']      = 'Atlikta: Jūs atnaujinote atsisiuntimus!';
-$_['text_list']         = 'Atsisiuntimų sąrašas';
-$_['text_add']          = 'Pridėti atsisiuntimą';
-$_['text_edit']         = 'Redaguoti atsisiuntimą';
-$_['text_upload']       = 'Jūsų failas sėkmingai įkeltas!';
+$_['text_success']      = 'Atlikta: Sėkmingai pakeitėte filtrus!';
+$_['text_list']         = 'Filtrų sąrašas';
+$_['text_add']          = 'Pridėti filtrą';
+$_['text_edit']         = 'Redaguoti filtrą';
 
 // Column
-$_['column_name']       = 'Atsisiuntimo pavadinimas';
-$_['column_date_added'] = 'Įkėlimo data';
+$_['column_group']      = 'Filtrų grupės';
+$_['column_sort_order'] = 'Rikiavimo tvarka';
 $_['column_action']     = 'Veiksmas';
 
 // Entry
-$_['entry_name']        = 'Atsisiuntimo pavadinimas';
-$_['entry_filename']    = 'Failo pavadinimas';
-$_['entry_mask']        = 'Kaukė';
-
-// Help
-$_['help_filename']     = 'Galite įkelti paspaudę įkėlimo mygtuką arba naudoti FTP prieigą.';
-$_['help_mask']         = 'Rekomenduojama, kad failo vardas ir kaukė būtų skirtingi. Taip bus galima išvengti žmonių, talpins nuorodas tiesiogiai į šį failą.';
+$_['entry_group']       = 'Filtrų grupės pavadinimas';
+$_['entry_name']        = 'Filtro pavadinimas';
+$_['entry_sort_order']  = 'Rikiavimo tvarka';
 
 // Error
 $_['error_warning']     = 'Įspėjimas: Patikrinkite klaidas formoje!';
-$_['error_permission']  = 'Įspėjimas: Jūs neturite teisės redaguoti atsisiuntimų!';
-$_['error_name']        = 'Atsisiuntimo pavadinimas turi būti tarp 3 ir 64 simbolių ilgio!';
-$_['error_upload']      = 'Įkėlimas privalomas!';
-$_['error_filename']    = 'Failo pavadinimas turi būti tarp 3 ir 128 simbolių!';
-$_['error_exists']      = 'Failas neegzistuoja!';
-$_['error_mask']        = 'Slaptumas turi būti nuo 3 iki 128 simbolių!';
-$_['error_filetype']    = 'Klaidingas failo tipas!';
-$_['error_product']     = 'Įspėjimas: Šis atsisiuntimas negali būti pašalintas, nes yra priskirtas %s produktams!';
+$_['error_permission']  = 'Įspėjimas: Jūs neturite teisės redaguoti filtrus!';
+$_['error_group']       = 'Filtrų grupės pavadinimas turi būti tarp 1 ir 64 simbolių!';
+$_['error_name']        = 'Filtro pavadinimas turi būti tarp 1 ir 64 simbolių!';
