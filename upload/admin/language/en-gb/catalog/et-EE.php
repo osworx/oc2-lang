9be09db3c49e4320437f7c228e5ffac4bd167b60
OSWorX <info@osworx.net>
2025-03-16 11:06:17 +0100

    New translations attribute_group.php (Estonian)

--- upload/admin/language/en-gb/catalog/et-EE.php
+++ upload/admin/language/en-gb/catalog/et-EE.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Atribuudid';
+$_['heading_title']     = 'Tunnuste rühm';
 
 // Text
-$_['text_success']           = 'Oled edukalt muutnud atribuute!';
-$_['text_list']              = 'Atribuutide nimekiri';
-$_['text_add']               = 'Lisa Atribuut';
-$_['text_edit']              = 'Muuda Atribuuti';
+$_['text_success']      = 'Tunnuste rühm edukalt muudetud!';
+$_['text_list']         = 'Tunnuste rühmaloend';
+$_['text_add']          = 'Lisa tunnuste rühm';
+$_['text_edit']         = 'Redigeeri tunnuste rühma';
 
 // Column
-$_['column_name']            = 'Atribuudi nimi';
-$_['column_attribute_group'] = 'Atribuudi rühm';
-$_['column_sort_order']      = 'Sorteerimis järjekord';
-$_['column_action']          = 'Tegevus';
+$_['column_name']       = 'Tunnuste rühma nimi';
+$_['column_sort_order'] = 'Sorteerimis järjekord';
+$_['column_action']     = 'Tegevus';
 
 // Entry
-$_['entry_name']             = 'Atribuudi nimi';
-$_['entry_attribute_group']  = 'Atribuudi rühm';
-$_['entry_sort_order']       = 'Sorteerimis järjekord';
+$_['entry_name']        = 'Tunnuste rühma nimi';
+$_['entry_sort_order']  = 'Sorteerimis järjekord';
 
 // Error
-$_['error_warning']          = 'Hoiatus: Palun kontrollige vormi vead hoolikalt üle!';
-$_['error_permission']       = 'Hoiatus: Teil ei ole õigust tunnuseid muuta!';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'Tunnuse pikkus peab olema vahemikus 3 kuni 64 märki!';
-$_['error_product']          = 'Hoiatus: Seda tunnust ei saa kustutada, kuna see sisaldub hetkel  %s tootes!';
+$_['error_warning']     = 'Hoiatus: Palun kontrollige vormi vead hoolikalt üle!';
+$_['error_permission']  = 'Hoiatus: Teil ei ole õigust tunnuseid muuta!';
+$_['error_name']        = 'Atribuudi nimi peab olema vahemikus 3 kuni 64 märki!';
+$_['error_attribute']   = 'Hoiatus: Seda tunnuste rühma ei saa kustutada, sest see on hetkel määratud  %s tootes!';
+$_['error_product']     = 'Hoiatus: Seda tunnust ei saa kustutada, kuna see sisaldub hetkel  %s tootes!';
