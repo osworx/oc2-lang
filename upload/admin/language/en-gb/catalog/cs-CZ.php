ce441d46c42c9d792ede046b6f80db6e86151061
OSWorX <info@osworx.net>
2025-03-16 11:13:36 +0100

    New translations review.php (Czech)

--- upload/admin/language/en-gb/catalog/cs-CZ.php
+++ upload/admin/language/en-gb/catalog/cs-CZ.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'Opakované profily';
+$_['heading_title']     = 'Recenze';
 
 // Text
-$_['text_success']          = 'Úspěch: Jste upravili opakující profily!';
-$_['text_list']             = 'Seznam opakujících se profilů';
-$_['text_add']              = 'Přidat opakující profil';
-$_['text_edit']             = 'Upravit opakující profil';
-$_['text_day']				= 'Den';
-$_['text_week']				= 'Týden';
-$_['text_semi_month']		= 'Půlměsíčně';
-$_['text_month']			= 'Měsíc';
-$_['text_year']				= 'Rok';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Opakující se částky se počítají podle frekvence a cyklů.</p><p>Například, pokud používáte frekvenci "týden" a cyklus "2", pak uživateli bude účtováno každé 2 týdny.</p><p>Doba trvání je počet, kolikrát uživatel bude platit; nastavte na 0 pokud chcete platby, dokud nebudou zrušeny.</p>';
-$_['text_profile']			= 'Profil opakování';
-$_['text_trial']			= 'Zkušební profil';
-
-// Entry
-$_['entry_name']		    = 'Název';
-$_['entry_price']			= 'Cena';
-$_['entry_duration']		= 'Doba trvání';
-$_['entry_cycle']			= 'Cyklus';
-$_['entry_frequency']		= 'Frekvence';
-$_['entry_trial_price']		= 'Zkušební cena';
-$_['entry_trial_duration']	= 'Zkušební doba';
-$_['entry_trial_status']	= 'Zkušební stav';
-$_['entry_trial_cycle']	    = 'Zkušební cyklus';
-$_['entry_trial_frequency'] = 'Zkušební frekvence';
-$_['entry_status']			= 'Stav';
-$_['entry_sort_order']		= 'Řadit podle';
+$_['text_success']      = 'Úspěch: Změnili jste Recenze!';
+$_['text_list']         = 'Seznam Recenzí';
+$_['text_add']          = 'Přidat recenzi';
+$_['text_edit']         = 'Upravit Recenze';
 
 // Column
-$_['column_name']			= 'Název';
-$_['column_sort_order']	    = 'Řadit podle';
-$_['column_action']         = 'Akce';
+$_['column_product']    = 'Produkt';
+$_['column_author']     = 'Author';
+$_['column_rating']     = 'Hodnocení';
+$_['column_status']     = 'Stav';
+$_['column_date_added'] = 'Datum přidání';
+$_['column_action']     = 'Akce';
+
+// Entry
+$_['entry_product']     = 'Produkt';
+$_['entry_author']      = 'Author';
+$_['entry_rating']      = 'Hodnocení';
+$_['entry_status']      = 'Stav';
+$_['entry_text']        = 'Text';
+$_['entry_date_added']  = 'Datum přidání';
+
+// Help
+$_['help_product']      = '(Funkce Automatické dokončování)';
 
 // Error
-$_['error_warning']         = 'Upozornění: Zkontrolujte formulář pečlivě pro chyby!';
-$_['error_permission']		= 'Upozornění: Nemáte oprávnění k úpravě opakujících profilů!';
-$_['error_name']			= 'Název profilu musí být větší než 3 a méně než 255 znaků!';
-$_['error_product']			= 'Varování: Tento opakující profil nelze odstranit, protože je v současnosti přiřazen k %s produktů!';
+$_['error_warning']     = 'Upozornění: Zkontrolujte formulář pečlivě pro chyby!';
+$_['error_permission']  = 'Upozornění: Nemáte oprávnění ke změně Recenze!';
+$_['error_product']     = 'Produkt je požadován!';
+$_['error_author']      = 'Autor musí být v rozmezí 3 až 64 znaků!';
+$_['error_text']        = 'Text Recenze musí být alespoň 1 znak!';
+$_['error_rating']      = 'Hodnocení recenze je vyžadováno!';
