a9521a02cba465b44a4044990fa117ce680f47f7
OSWorX <info@osworx.net>
2025-03-16 14:23:38 +0100

    New translations recurring.php (Hindi)

--- upload/admin/language/en-gb/catalog/hi-IN.php
+++ upload/admin/language/en-gb/catalog/hi-IN.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'उत्पाद';
+$_['heading_title']			= 'आवर्ती प्रोफ़ाइल';
 
 // Text
-$_['text_success']           = 'सफलता: आपने उत्पादों को संशोधित किया है!';
-$_['text_list']              = 'उत्पाद सूची';
-$_['text_add']               = 'उत्पाद जोड़ें';
-$_['text_edit']              = 'उत्पाद संपादित करें';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'सामान्य';
-$_['text_option']            = 'विकल्प';
-$_['text_option_value']      = 'विकल्प मूल्य';
-$_['text_percent']           = 'प्रतिशत';
-$_['text_amount']            = 'निश्चित रकम';
-
-// Column
-$_['column_name']            = 'उत्पाद का नाम';
-$_['column_model']           = 'नमूना';
-$_['column_image']           = 'छवि';
-$_['column_price']           = 'मूल्य';
-$_['column_quantity']        = 'मात्रा';
-$_['column_status']          = 'स्थिति';
-$_['column_action']          = 'कार्यवाही';
+$_['text_success']          = 'सफलता: आपने पुनरावर्ती प्रोफाइल को संशोधित किया है!';
+$_['text_list']             = 'आवर्ती प्रोफ़ाइल सूची';
+$_['text_add']              = 'आवर्ती प्रोफ़ाइल जोड़ें';
+$_['text_edit']             = 'आवर्ती प्रोफ़ाइल संपादित करें';
+$_['text_day']				= 'दिन';
+$_['text_week']				= 'सप्ताह';
+$_['text_semi_month']		= 'अर्ध माह';
+$_['text_month']			= 'माह';
+$_['text_year']				= 'वर्ष';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Recurring amounts are calculated by the frequency and cycles.</p><p>For example if you use a frequency of "week" and a cycle of "2", then the user will be billed every 2 weeks.</p><p>The duration is the number of times the user will make a payment, set this to 0 if you want payments until they are cancelled.</p>';
+$_['text_profile']			= 'आवर्ती प्रोफ़ाइल';
+$_['text_trial']			= 'परीक्षण प्रोफ़ाइल';
 
 // Entry
-$_['entry_name']             = 'उत्पाद का नाम';
-$_['entry_description']      = 'विवरण';
-$_['entry_meta_title'] 	     = 'मेटा टैग शीर्षक';
-$_['entry_meta_keyword'] 	 = 'मेटा टैग खोजशब्द';
-$_['entry_meta_description'] = 'मेटा टैग विवरण';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'नमूना';
-$_['entry_sku']              = 'SKU';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'स्थान';
-$_['entry_shipping']         = 'शिपिंग की आवश्यकता है';
-$_['entry_manufacturer']     = 'निर्माता';
-$_['entry_store']            = 'भंडार';
-$_['entry_date_available']   = 'उपलब्ध दिनांक';
-$_['entry_quantity']         = 'मात्रा';
-$_['entry_minimum']          = 'न्यूनतम मात्रा';
-$_['entry_stock_status']     = 'स्टॉक से बाहर की स्थिति';
-$_['entry_price']            = 'मूल्य';
-$_['entry_tax_class']        = 'कर वर्ग';
-$_['entry_points']           = 'अंक';
-$_['entry_option_points']    = 'अंक';
-$_['entry_subtract']         = 'भंडार घटाना';
-$_['entry_weight_class']     = 'वजन वर्ग';
-$_['entry_weight']           = 'वजन';
-$_['entry_dimension']        = 'Dimensions (L x W x H)';
-$_['entry_length_class']     = 'लंबाई वर्ग';
-$_['entry_length']           = 'लंबाई';
-$_['entry_width']            = 'चौड़ाई';
-$_['entry_height']           = 'ऊँचाई';
-$_['entry_image']            = 'छवि';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'ग्राहक समूह';
-$_['entry_date_start']       = 'प्रारंभ दिनांक';
-$_['entry_date_end']         = 'समाप्ति दिनांक';
-$_['entry_priority']         = 'प्राथमिकता';
-$_['entry_attribute']        = 'गुण';
-$_['entry_attribute_group']  = 'विशेषता समूह';
-$_['entry_text']             = 'शब्द';
-$_['entry_option']           = 'विकल्प';
-$_['entry_option_value']     = 'विकल्प मूल्य';
-$_['entry_required']         = 'आवश्यक';
-$_['entry_status']           = 'स्थिति';
-$_['entry_sort_order']       = 'क्रम';
-$_['entry_category']         = 'श्रेणियाँ';
-$_['entry_filter']           = 'छानना';
-$_['entry_download']         = 'डाउनलोड';
-$_['entry_related']          = 'संबंधित उत्पाद';
-$_['entry_tag']          	 = 'उत्पाद टैग';
-$_['entry_reward']           = 'इनाम के अंक';
-$_['entry_layout']           = 'लेआउट ओवरराइड';
-$_['entry_recurring']        = 'आवर्ती प्रोफ़ाइल';
+$_['entry_name']		    = 'नाम';
+$_['entry_price']			= 'मूल्य';
+$_['entry_duration']		= 'अवधि';
+$_['entry_cycle']			= 'चक्र';
+$_['entry_frequency']		= 'आवृत्ति';
+$_['entry_trial_price']		= 'परीक्षण की कीमत';
+$_['entry_trial_duration']	= 'परीक्षण अवधि';
+$_['entry_trial_status']	= 'परीक्षण स्थिति';
+$_['entry_trial_cycle']	    = 'परीक्षण चक्र';
+$_['entry_trial_frequency'] = 'परीक्षण आवृत्ति';
+$_['entry_status']			= 'स्थिति';
+$_['entry_sort_order']		= 'क्रम';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'स्टॉक रखने की इकाई';
-$_['help_upc']               = 'यूनिवर्सल उत्पाद कोड';
-$_['help_ean']               = 'यूरोपीय अनुच्छेद संख्या';
-$_['help_jan']               = 'जापानी आलेख संख्या';
-$_['help_isbn']              = 'अंतर्राष्ट्रीय मानक पुस्तक संख्या';
-$_['help_mpn']               = 'निर्माता भाग संख्या';
-$_['help_manufacturer']      = '(स्वत: पूर्ण)';
-$_['help_minimum']           = 'Force a minimum ordered amount';
-$_['help_stock_status']      = 'स्थिति जब एक उत्पाद स्टॉक से बाहर है';
-$_['help_points']            = 'Number of points needed to buy this item. If you don\'t want this product to be purchased with points leave as 0.';
-$_['help_category']          = '(स्वत: पूर्ण)';
-$_['help_filter']            = '(स्वत: पूर्ण)';
-$_['help_download']          = '(स्वत: पूर्ण)';
-$_['help_related']           = '(स्वत: पूर्ण)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'नाम';
+$_['column_sort_order']	    = 'क्रम';
+$_['column_action']         = 'कार्यवाही';
 
 // Error
-$_['error_warning']          = 'चेतावनी: कृपया ध्यान से त्रुटियों के लिए प्रपत्र की जाँच करें!';
-$_['error_permission']       = 'चेतावनी: आपको उत्पादों को संशोधित करने की अनुमति नहीं है!';
-$_['error_name']             = 'उत्पाद का नाम 3 वर्ण से अधिक और 255 वर्ण से कम होना आवश्यक है!';
-$_['error_meta_title']       = 'मेटा शीर्षक कम से कम 3 और अधिक से अधिक 255  होना आवश्यक है!';
-$_['error_model']            = 'उत्पाद मॉडल 1 वर्ण से अधिक और 64 वर्ण से कम होना आवश्यक है!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'चेतावनी: कृपया ध्यान से त्रुटियों के लिए प्रपत्र की जाँच करें!';
+$_['error_permission']		= 'चेतावनी: आपको पुनरावर्ती प्रोफाइल को संशोधित करने की अनुमति नहीं है!';
+$_['error_name']			= 'उत्पाद का नाम 3 कम से कम और अधिक से अधिक 255 वर्ण होना आवश्यक है!';
+$_['error_product']			= 'चेतावनी: यह वर्तमान में %s उत्पादों के लिए असाइन किया गया है अत: इस आवर्ती प्रोफ़ाइल हटाया नहीं जा सकता!';
