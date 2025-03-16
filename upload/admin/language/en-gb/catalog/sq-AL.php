42719b887bb754229e44f555cfd2400d8056148a
OSWorX <info@osworx.net>
2025-03-16 11:09:40 +0100

    New translations information.php (Albanian)

--- upload/admin/language/en-gb/catalog/sq-AL.php
+++ upload/admin/language/en-gb/catalog/sq-AL.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'Filtrat';
+$_['heading_title']          = 'Information';
 
 // Text
-$_['text_success']      = 'Success: You have modified filters!';
-$_['text_list']         = 'Filter List';
-$_['text_add']          = 'Add Filter';
-$_['text_edit']         = 'Edit Filter';
+$_['text_success']           = 'Success: You have modified information!';
+$_['text_list']              = 'Information List';
+$_['text_add']               = 'Add Information';
+$_['text_edit']              = 'Edit Information';
+$_['text_default']           = 'Parapërzgjedhur';
 
 // Column
-$_['column_group']      = 'Filter Group';
-$_['column_sort_order'] = 'Radhitja';
-$_['column_action']     = 'Veprimi';
+$_['column_title']           = 'Information Title';
+$_['column_sort_order']	     = 'Radhitja';
+$_['column_action']          = 'Veprimi';
 
 // Entry
-$_['entry_group']       = 'Filter Group Name';
-$_['entry_name']        = 'Filter Name';
-$_['entry_sort_order']  = 'Radhitja';
+$_['entry_title']            = 'Information Title';
+$_['entry_description']      = 'Përshkrimi';
+$_['entry_store']            = 'Dyqanet';
+$_['entry_meta_title'] 	     = 'Meta titulli';
+$_['entry_meta_keyword'] 	 = 'Fjalë kyçe meta.';
+$_['entry_meta_description'] = 'Përshkrimi Meta Tag';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Bottom';
+$_['entry_status']           = 'Statusi';
+$_['entry_sort_order']       = 'Radhitja';
+$_['entry_layout']           = 'Faqosja e shtypit';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Display in the bottom footer.';
 
 // Error
-$_['error_warning']     = 'Vërejtje: Ju lutem kontrolloni formën me kujdes për ndonjë gabim!';
-$_['error_permission']  = 'Warning: You do not have permission to modify filters!';
-$_['error_group']       = 'Filter Group Name must be between 1 and 64 characters!';
-$_['error_name']        = 'Filter Name must be between 1 and 64 characters!';
+$_['error_warning']          = 'Vërejtje: Ju lutem kontrolloni formën me kujdes për ndonjë gabim!';
+$_['error_permission']       = 'Warning: You do not have permission to modify information!';
+$_['error_title']            = 'Information Title must be between 3 and 64 characters!';
+$_['error_description']      = 'Description must be more than 3 characters!';
+$_['error_meta_title']       = 'Produkti Model duhet të jetë më i madh se 3 dhe më pak se 255 karaktere!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'Warning: This information page cannot be deleted as it is currently assigned as the store account terms!';
+$_['error_checkout']         = 'Warning: This information page cannot be deleted as it is currently assigned as the store checkout terms!';
+$_['error_affiliate']        = 'Warning: This information page cannot be deleted as it is currently assigned as the store affiliate terms!';
+$_['error_return']           = 'Warning: This information page cannot be deleted as it is currently assigned as the store return terms!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
