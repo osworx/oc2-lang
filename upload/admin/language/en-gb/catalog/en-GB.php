6594dcd9a1daba909f5194cbc7313ec7d1971248
OSWorX <info@osworx.net>
2025-03-16 14:16:29 +0100

    New translations attribute.php (English, United Kingdom)

--- upload/admin/language/en-gb/catalog/en-GB.php
+++ upload/admin/language/en-gb/catalog/en-GB.php
@@ -1,36 +1,27 @@
 <?php
 // Heading
-$_['heading_title']     = 'Reviews';
+$_['heading_title']          = 'Attributes';
 
 // Text
-$_['text_success']      = 'Success: You have modified reviews!';
-$_['text_list']         = 'Review List';
-$_['text_add']          = 'Add Review';
-$_['text_edit']         = 'Edit Review';
+$_['text_success']           = 'Success: You have modified attributes!';
+$_['text_list']              = 'Attribute List';
+$_['text_add']               = 'Add Attribute';
+$_['text_edit']              = 'Edit Attribute';
 
 // Column
-$_['column_product']    = 'Product';
-$_['column_author']     = 'Author';
-$_['column_rating']     = 'Rating';
-$_['column_status']     = 'Status';
-$_['column_date_added'] = 'Date Added';
-$_['column_action']     = 'Action';
+$_['column_name']            = 'Attribute Name';
+$_['column_attribute_group'] = 'Attribute Group';
+$_['column_sort_order']      = 'Sort Order';
+$_['column_action']          = 'Action';
 
 // Entry
-$_['entry_product']     = 'Product';
-$_['entry_author']      = 'Author';
-$_['entry_rating']      = 'Rating';
-$_['entry_status']      = 'Status';
-$_['entry_text']        = 'Text';
-$_['entry_date_added']  = 'Date Added';
-
-// Help
-$_['help_product']      = '(Autocomplete)';
+$_['entry_name']             = 'Attribute Name';
+$_['entry_attribute_group']  = 'Attribute Group';
+$_['entry_sort_order']       = 'Sort Order';
 
 // Error
-$_['error_warning']     = 'Warning: Please check the form carefully for errors!';
-$_['error_permission']  = 'Warning: You do not have permission to modify reviews!';
-$_['error_product']     = 'Product required!';
-$_['error_author']      = 'Author must be between 3 and 64 characters!';
-$_['error_text']        = 'Review Text must be at least 1 character!';
-$_['error_rating']      = 'Review rating required!';
+$_['error_warning']          = 'Warning: Please check the form carefully for errors!';
+$_['error_permission']       = 'Warning: You do not have permission to modify attributes!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Attribute Name must be between 3 and 64 characters!';
+$_['error_product']          = 'Warning: This attribute cannot be deleted as it is currently assigned to %s products!';
