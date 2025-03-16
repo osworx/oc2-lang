e4d3ad7f3b929e7ac09c888567011a5e167fd3c9
OSWorX <info@osworx.net>
2025-03-16 11:20:02 +0100

    New translations login.php (Georgian)

--- upload/admin/language/en-gb/common/ka-GE.php
+++ upload/admin/language/en-gb/common/ka-GE.php
@@ -1,23 +1,19 @@
 <?php
-// Heading
-$_['heading_title']          = 'ოფენ ქარდი';
+// header
+$_['heading_title']  = 'ადმინისტრირება';
 
 // Text
-$_['text_order']             = 'შეკვეთები';
-$_['text_processing_status'] = 'Processing';
-$_['text_complete_status']   = 'დასრულებულია';
-$_['text_customer']          = 'დამკვეთები';
-$_['text_online']            = 'დამკვეთები ონლაინში';
-$_['text_approval']          = 'დამტკიცების მოლოდინში';
-$_['text_product']           = 'პროდუქცია';
-$_['text_stock']             = 'საწყობში არ არის';
-$_['text_review']            = 'შეფასებები';
-$_['text_return']            = 'დაბრუნებები';
-$_['text_affiliate']         = 'პარტნიორული პროგრამა';
-$_['text_store']             = 'მაღაზიები';
-$_['text_front']             = 'მაღაზიის ვიტრინა';
-$_['text_help']              = 'დახმარება';
-$_['text_homepage']          = 'OpenCart Homepage';
-$_['text_support']           = 'ფორუმის დახმარება';
-$_['text_documentation']     = 'დოკუმენტაცია';
-$_['text_logout']            = 'გამოსვლა';
+$_['text_heading']   = 'ადმინისტრირება';
+$_['text_login']     = 'გთხოვთ შეიყვანოთ თქვენი დეტალები.';
+$_['text_forgotten'] = 'პაროლის აღდგენა';
+
+// Entry
+$_['entry_username'] = 'მომხმარებელი';
+$_['entry_password'] = 'პაროლი';
+
+// Button
+$_['button_login']   = 'შესვლა';
+
+// Error
+$_['error_login']    = 'არ არის მატჩი ნიკისთვის ან / და პაროლი.';
+$_['error_token']    = 'არასწორი სესია. გთხოვთ შეხვიდეთ კიდევ ერთხელ.';
