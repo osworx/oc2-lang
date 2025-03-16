73316cfb3945801a033850b394cc57a35e5b5b98
OSWorX <info@osworx.net>
2025-03-16 11:07:19 +0100

    New translations category.php (French, Canada)

--- upload/admin/language/en-gb/catalog/fr-CA.php
+++ upload/admin/language/en-gb/catalog/fr-CA.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Groupes d\'attributs';
+$_['heading_title']          = 'Catégories';
 
 // Text
-$_['text_success']      = 'Succès: Vous avec modifié les attributs de groupe!';
-$_['text_list']         = 'List des attributs de groupe';
-$_['text_add']          = 'Ajouter un attribut de groupe';
-$_['text_edit']         = 'Modifier un attribut de groupe';
+$_['text_success']           = 'Succès: Vous avez modifié les catégories !';
+$_['text_list']              = 'Liste des catégories';
+$_['text_add']               = 'Ajouter une catégorie';
+$_['text_edit']              = 'Modifier la catégorie';
+$_['text_default']           = 'Par défaut';
 
 // Column
-$_['column_name']       = 'Nom du groupe d\'attributs';
-$_['column_sort_order'] = 'Ordre de présentation';
-$_['column_action']     = 'Action';
+$_['column_name']            = 'Nom de la catégorie';
+$_['column_sort_order']      = 'Ordre de présentation';
+$_['column_action']          = 'Action';
 
 // Entry
-$_['entry_name']        = 'Nom du groupe d\'attributs';
-$_['entry_sort_order']  = 'Ordre de présentation';
+$_['entry_name']             = 'Nom de la catégorie';
+$_['entry_description']      = 'Description';
+$_['entry_meta_title'] 	     = 'Titre de métaélément';
+$_['entry_meta_keyword']     = 'Mots-clef de métaélément';
+$_['entry_meta_description'] = 'Description de métaélément';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Parent';
+$_['entry_filter']           = 'Filtres';
+$_['entry_store']            = 'Magasins';
+$_['entry_image']            = 'Image';
+$_['entry_top']              = 'Haut';
+$_['entry_column']           = 'Colonnes';
+$_['entry_sort_order']       = 'Ordre de présentation';
+$_['entry_status']           = 'État';
+$_['entry_layout']           = 'Outrepasser la mise en page';
+
+// Help
+$_['help_filter']            = '(Autocomplétion)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Afficher dans la barre de menu du haut. Applicable seulement pour les catégories de premier niveau.';
+$_['help_column']            = 'Nombre de colonnes à utiliser pour les 3 catégories du bas. Applicable seulement aux catégories de premier niveau.';
 
 // Error
-$_['error_warning']     = 'Attention: Veuillez vérifier que le formulaire ne contient pas d\'erreur !';
-$_['error_permission']  = 'Avertissement : Vous n\'êtes pas autorisé à modifier les groupes d\'attributs!';
-$_['error_name']        = 'Le nom de groupe d\'attributs doit être entre 3 et 64 caractères!';
-$_['error_attribute']   = 'AVERTISSEMENT : Ce groupe d\'attributs ne peut être supprimé car il est actuellement affecté aux attributs de %s !';
-$_['error_product']     = 'AVERTISSEMENT : Ce groupe d\'attributs ne peut être supprimé car il est actuellement affecté aux produits %s !';
+$_['error_warning']          = 'Attention: Veuillez vérifier que le formulaire ne contient pas d\'erreur !';
+$_['error_permission']       = 'Attention: vous n’êtes pas autorisé à modifier les catégories !';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Le titre méta doit contenir plus de 3 et moins de 255 caractères !';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
