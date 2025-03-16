7777afd4d6b979479ff7519ef9409a91fee66a7b
OSWorX <info@osworx.net>
2025-03-16 14:21:51 +0100

    New translations option.php (Chinese Simplified)

--- upload/admin/language/en-gb/catalog/zh-CN.php
+++ upload/admin/language/en-gb/catalog/zh-CN.php
@@ -1,35 +1,40 @@
 <?php
 // Heading
-$_['heading_title']     = '厂商管理';
+$_['heading_title']      = '选项';
 
 // Text
-$_['text_success']      = '您已经成功修改厂商管理!';
-$_['text_list']         = '厂商列表';
-$_['text_add']          = '新增厂商';
-$_['text_edit']         = '编辑厂商';
-$_['text_default']      = '默认';
-$_['text_percent']      = '百分比';
-$_['text_amount']       = '固定金额';
+$_['text_success']       = '成功： 你已修改选项 ！';
+$_['text_list']          = '选项列表';
+$_['text_add']           = '添加选项';
+$_['text_edit']          = '编辑选项';
+$_['text_choose']        = '选择';
+$_['text_select']        = '下拉框';
+$_['text_radio']         = '单选';
+$_['text_checkbox']      = '复选框';
+$_['text_input']         = '输入框';
+$_['text_text']          = '文本框';
+$_['text_textarea']      = '多行文本框';
+$_['text_file']          = '文件';
+$_['text_date']          = '日期';
+$_['text_datetime']      = '日期与时间';
+$_['text_time']          = '时间';
 
 // Column
-$_['column_name']       = '厂商名称';
-$_['column_sort_order'] = '排列顺序';
-$_['column_action']     = '操作';
+$_['column_name']        = '选项名称';
+$_['column_sort_order']  = '排列顺序';
+$_['column_action']      = '操作';
 
 // Entry
-$_['entry_name']        = '厂商名称';
-$_['entry_store']       = '商店';
-$_['entry_keyword']     = 'SEO URL';
-$_['entry_image']       = '图像';
-$_['entry_sort_order']  = '排列顺序';
-$_['entry_type']        = '类型';
-
-// Help
-$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['entry_name']         = '选项名称';
+$_['entry_type']         = '类型';
+$_['entry_option_value'] = '选项值的名称';
+$_['entry_image']        = '图像';
+$_['entry_sort_order']   = '排列顺序';
 
 // Error
-$_['error_warning']     = '警告:请仔细检查错误格式!';
-$_['error_permission']  = '警告: 您没有权限修改厂商管理!';
-$_['error_name']        = '厂商名称必须介于2-64个字符之间!';
-$_['error_keyword']     = 'SEO URL already in use!';
-$_['error_product']     = '警告:您不能删除该厂商,因为它目前正关联%s产品!';
+$_['error_warning']      = '警告:请仔细检查错误格式!';
+$_['error_permission']   = '警告: 您没有权限修改选项 ！';
+$_['error_name']         = '选项名称必须介于 1 到 128 个字符之间 ！';
+$_['error_type']         = '警告: 选项值必填！';
+$_['error_option_value'] = '选项值名称必须介于 1 到 128 个字符之间 ！';
+$_['error_product']      = '警告： 不能删除此选项，目前已分配给 %s 产品 ！';
