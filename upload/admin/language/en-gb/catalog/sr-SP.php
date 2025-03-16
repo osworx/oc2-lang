5b9c5b9564d1cb700fa7e00bf845ad3f61f77a37
OSWorX <info@osworx.net>
2025-03-16 14:23:23 +0100

    New translations recurring.php (Serbian (Cyrillic))

--- upload/admin/language/en-gb/catalog/sr-SP.php
+++ upload/admin/language/en-gb/catalog/sr-SP.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'Производи';
+$_['heading_title']			= 'Профили понављања';
 
 // Text
-$_['text_success']           = 'Успешно: изменили сте производе!';
-$_['text_list']              = 'Листа производа';
-$_['text_add']               = 'Додај производ';
-$_['text_edit']              = 'Измени производ';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'Подразумевано';
-$_['text_option']            = 'Опција';
-$_['text_option_value']      = 'Вредност опције';
-$_['text_percent']           = 'Проценат';
-$_['text_amount']            = 'Фиксни износ';
-
-// Column
-$_['column_name']            = 'Назив производа';
-$_['column_model']           = 'Модел';
-$_['column_image']           = 'Слика';
-$_['column_price']           = 'Цена';
-$_['column_quantity']        = 'Количина';
-$_['column_status']          = 'Статус';
-$_['column_action']          = 'Акција';
+$_['text_success']          = 'Успешно: изменили сте профиле понављања!';
+$_['text_list']             = 'Листа профила понављања';
+$_['text_add']              = 'Додај профил понављања';
+$_['text_edit']             = 'Измени профил понављања';
+$_['text_day']				= 'Дан';
+$_['text_week']				= 'Недеља';
+$_['text_semi_month']		= 'Пола месеца';
+$_['text_month']			= 'Месец';
+$_['text_year']				= 'Година';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> периодични износи се рачунају по учесталости и циклусу.</p><p>На пример, ако користите учесталост „недеља” и циклус „2”, онда ће купац добијати рачун сваке 2 недеље.</p><p>Трајање је број пута који купац мора да понови плаћање, поставите на 0 ако желите плаћања док се не откажу.</p>';
+$_['text_profile']			= 'Профил понављања';
+$_['text_trial']			= 'Профил пробе';
 
 // Entry
-$_['entry_name']             = 'Назив производа';
-$_['entry_description']      = 'Опис';
-$_['entry_meta_title'] 	     = 'Мета ознака Наслов';
-$_['entry_meta_keyword'] 	 = 'Мета ознака Кључне речи';
-$_['entry_meta_description'] = 'Мета ознака Опис';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Модел';
-$_['entry_sku']              = '„SKU”';
-$_['entry_upc']              = '„UPC”';
-$_['entry_ean']              = '„EAN”';
-$_['entry_jan']              = '„JAN”';
-$_['entry_isbn']             = '„ISBN”';
-$_['entry_mpn']              = '„MPN”';
-$_['entry_location']         = 'Локација';
-$_['entry_shipping']         = 'Захтева доставу';
-$_['entry_manufacturer']     = 'Произвођач';
-$_['entry_store']            = 'Продавнице';
-$_['entry_date_available']   = 'Датум доступности';
-$_['entry_quantity']         = 'Количина';
-$_['entry_minimum']          = 'Минимална количина';
-$_['entry_stock_status']     = 'Статус нема на стању';
-$_['entry_price']            = 'Цена';
-$_['entry_tax_class']        = 'Класа пореза';
-$_['entry_points']           = 'Поена';
-$_['entry_option_points']    = 'Поена';
-$_['entry_subtract']         = 'Умањи стање';
-$_['entry_weight_class']     = 'Класа тежине';
-$_['entry_weight']           = 'Тежина';
-$_['entry_dimension']        = 'Димензије (Д x Ш x В)';
-$_['entry_length_class']     = 'Класа дужине';
-$_['entry_length']           = 'Дужина';
-$_['entry_width']            = 'Ширина';
-$_['entry_height']           = 'Висина';
-$_['entry_image']            = 'Слика';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Група корисника';
-$_['entry_date_start']       = 'Датум почетка';
-$_['entry_date_end']         = 'Датум завршетка';
-$_['entry_priority']         = 'Приоритет';
-$_['entry_attribute']        = 'Атрибут';
-$_['entry_attribute_group']  = 'Група атрибута';
-$_['entry_text']             = 'Текст';
-$_['entry_option']           = 'Опција';
-$_['entry_option_value']     = 'Вредност опције';
-$_['entry_required']         = 'Обавезно';
-$_['entry_status']           = 'Статус';
-$_['entry_sort_order']       = 'Редослед';
-$_['entry_category']         = 'Категорије';
-$_['entry_filter']           = 'Филтери';
-$_['entry_download']         = 'Преузимања';
-$_['entry_related']          = 'Сродни производи';
-$_['entry_tag']          	 = 'Ознаке производа';
-$_['entry_reward']           = 'Наградни поени';
-$_['entry_layout']           = 'Заобилажење распореда';
-$_['entry_recurring']        = 'Профил понављања';
+$_['entry_name']		    = 'Назив';
+$_['entry_price']			= 'Цена';
+$_['entry_duration']		= 'Tрајање';
+$_['entry_cycle']			= 'Циклус';
+$_['entry_frequency']		= 'Фреквенција';
+$_['entry_trial_price']		= 'Пробна цена';
+$_['entry_trial_duration']	= 'Трајање пробе';
+$_['entry_trial_status']	= 'Статус пробе';
+$_['entry_trial_cycle']	    = 'Циклус пробе';
+$_['entry_trial_frequency'] = 'Учесталост пробе';
+$_['entry_status']			= 'Статус';
+$_['entry_sort_order']		= 'Редослед';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'Ознака за складишно праћење';
-$_['help_upc']               = 'Универзални код производа';
-$_['help_ean']               = 'Европски број артикла';
-$_['help_jan']               = 'Јапански број артикла';
-$_['help_isbn']              = 'Интернационални стандардни број књиге';
-$_['help_mpn']               = 'Произвођачки број дела';
-$_['help_manufacturer']      = '(Аутоматско довршавање)';
-$_['help_minimum']           = 'Обавезна минимална количина за поручивање';
-$_['help_stock_status']      = 'Статус који се приказује кад производа нема на стању';
-$_['help_points']            = 'Број поена потребних за куповину артикла. Ако не желите да се овај производ продаје за поене оставите 0.';
-$_['help_category']          = '(Аутоматско довршавање)';
-$_['help_filter']            = '(Аутоматско довршавање)';
-$_['help_download']          = '(Аутоматско довршавање)';
-$_['help_related']           = '(Аутоматско довршавање)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'Назив';
+$_['column_sort_order']	    = 'Редослед';
+$_['column_action']         = 'Акција';
 
 // Error
-$_['error_warning']          = 'Упозорење: молимо вас да пажљиво проверите могуће грешке у формулару!';
-$_['error_permission']       = 'Упозорење: Немате дозволу за измену производа!';
-$_['error_name']             = 'Назив производа мора бити дужи од 3 и краћи од 255 знакова!';
-$_['error_meta_title']       = 'Мета ознака Наслов мора бити дужа од 3 и краћа од 255 знакова!';
-$_['error_model']            = 'Модел производа мора бити дужи од 1 и краћи од 64 знакова!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Упозорење: молимо вас да пажљиво проверите могуће грешке у формулару!';
+$_['error_permission']		= 'Упозорење: Немате дозволу за измену профила понављања!';
+$_['error_name']			= 'Назив профила мора бити дужи од 3 и краћи од 255 карактера!';
+$_['error_product']			= 'Упозорење: Овај профил понављања није могуће обрисати јер тренутно има придружених %s производа!';
