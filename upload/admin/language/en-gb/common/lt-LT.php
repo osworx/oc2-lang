f97aac8f8f7ca2b72b640f4b2921c6a66834b85a
OSWorX <info@osworx.net>
2025-03-16 11:18:27 +0100

    New translations forgotten.php (Lithuanian)

--- upload/admin/language/en-gb/common/lt-LT.php
+++ upload/admin/language/en-gb/common/lt-LT.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Pamiršote savo slaptažodį?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Versija %s';
+$_['text_forgotten']  = 'Pamiršau slaptažodį';
+$_['text_your_email'] = 'Jūsų el. pašto adresas';
+$_['text_email']      = 'Įveskite el. pašto adresą susietą su jūsų paskyra. Paspauskite pateikti ir gausite slaptažodžio nustatymo el. laišką.';
+$_['text_success']    = 'Laiškas su patvirtinimo nuoroda buvo išsiųstas į jūsų administratoriaus el. pašto adresas.';
+
+// Entry
+$_['entry_email']     = 'El. pašto adresas';
+$_['entry_password']  = 'Naujas slaptažodis';
+$_['entry_confirm']   = 'Patvirtinti';
+
+// Error
+$_['error_email']     = 'Įspėjimas: El. pašto adresas nerastas mūsų įrašuose, prašome pabandyti dar kartą!';
+$_['error_password']  = 'Slaptažodis turi būti nuo 4 iki 20 simbolių!';
+$_['error_confirm']   = 'Slaptažodis ir slaptažodžio pakartojimas nesutampa!';
