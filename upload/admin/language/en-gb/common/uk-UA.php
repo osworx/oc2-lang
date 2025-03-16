21d435a0f2efaf4cebb9c702e3c1531bacca8d5f
OSWorX <info@osworx.net>
2025-03-16 14:28:45 +0100

    New translations forgotten.php (Ukrainian)

--- upload/admin/language/en-gb/common/uk-UA.php
+++ upload/admin/language/en-gb/common/uk-UA.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Забули пароль?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Версія %s';
+$_['text_forgotten']  = 'Нагадати пароль';
+$_['text_your_email'] = 'Ваш E-Mail';
+$_['text_email']      = 'Введіть адресу електронної пошти, пов\'язану з вашим обліковим записом. Посилання на зміну пароля буде надіслано на Ваш e-mail.';
+$_['text_success']    = 'Посилання на зміну пароля надіслано на Ваш e-mail.';
+
+// Entry
+$_['entry_email']     = 'E-Mail';
+$_['entry_password']  = 'Новий пароль';
+$_['entry_confirm']   = 'Підтвердити';
+
+// Error
+$_['error_email']     = 'Увага: Адресу електронної пошти не знайдено, будь ласка спробуйте ще раз!';
+$_['error_password']  = 'Password must be between 4 and 20 characters!';
+$_['error_confirm']   = 'Пароль і підтвердження пароля не співпадають!';
