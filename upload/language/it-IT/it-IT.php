627e0c22579435d757d987efa31dd5804024ee92
OSWorX <info@osworx.net>
2025-03-16 09:10:15 +0100

    New translations footer.php (Italian)

--- upload/language/it-IT/it-IT.php
+++ upload/language/it-IT/it-IT.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Gestore delle Immagini';
-
 // Text
-$_['text_uploaded']    = 'Il file è stato caricato con successo!';
-$_['text_directory']   = 'Directory creata con successo!';
-$_['text_delete']      = 'La directory o il file è stato eliminato con successo!';
-
-// Entry
-$_['entry_search']     = 'Cerca..';
-$_['entry_folder']     = 'Nome cartella';
-
-// Error
-$_['error_permission'] = 'Avviso: Autorizzazione negata!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Attenzione: Un file o una directory con lo stesso nome esiste gi&agrave!';
-$_['error_directory']  = 'Attenzione: La Directory non esiste!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Avviso: Tipo file errato!';
-$_['error_upload']     = 'Attenzione: Il File non potrebbe essere caricato per un motivo sconosciuto!';
-$_['error_delete']     = 'Attenzione: Non &egrave possibile eliminare questa directory!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Versione %s';
