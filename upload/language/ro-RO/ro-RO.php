30b252ddf8fba45f2e3b45d98f8d5e557b27bdec
OSWorX <info@osworx.net>
2025-03-16 09:05:55 +0100

    New translations recurring.php (Romanian)

--- upload/language/ro-RO/ro-RO.php
+++ upload/language/ro-RO/ro-RO.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'Produse';
+$_['heading_title']			= 'Profil recurent';
 
 // Text
-$_['text_success']           = 'Felicitări: Ați modificat produsele!';
-$_['text_list']              = 'Listă de produse';
-$_['text_add']               = 'Adaugă produs';
-$_['text_edit']              = 'Modifică produs';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'Implicit';
-$_['text_option']            = 'Opțiune';
-$_['text_option_value']      = 'Valoarea opțiunii';
-$_['text_percent']           = 'Procent';
-$_['text_amount']            = 'Sumă fixă';
-
-// Column
-$_['column_name']            = 'Nume produs';
-$_['column_model']           = 'Model';
-$_['column_image']           = 'imagine';
-$_['column_price']           = 'Preț';
-$_['column_quantity']        = 'Cantitate';
-$_['column_status']          = 'Stare';
-$_['column_action']          = 'Acțiune';
+$_['text_success']          = 'Felicitări: Ați modificat profilele recurente!';
+$_['text_list']             = 'Listă de profile recurente';
+$_['text_add']              = 'Adaugă un profil recurent';
+$_['text_edit']             = 'Modifică un profil recurent';
+$_['text_day']				= 'Zi';
+$_['text_week']				= 'Săptămână';
+$_['text_semi_month']		= 'Jumătate de lună';
+$_['text_month']			= 'Lună';
+$_['text_year']				= 'An';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i>Sumele recurente sunt calculate cu ajutorul frecvenței și al ciclurilor.</p><p>Spre exemplu dacă utiliați o frecvență de „o săptămână” și un ciclu de „2”, atunci utilizatorul va plăti la fiecare 2 săptămâni.</p><p>Durata este de câte ori utilizatorul va face plata, stabiliți la 0 dacă doriți plați până când vor fi anulate.</p>';
+$_['text_profile']			= 'Profil recurent';
+$_['text_trial']			= 'Profilul încercare';
 
 // Entry
-$_['entry_name']             = 'Nume produs';
-$_['entry_description']      = 'Descriere';
-$_['entry_meta_title'] 	     = 'Meta tag: Titlu';
-$_['entry_meta_keyword'] 	 = 'Meta tag: cuvinte cheie';
-$_['entry_meta_description'] = 'Meta tag: descriere';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Model';
-$_['entry_sku']              = 'SKU';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'Locație';
-$_['entry_shipping']         = 'Necesită livrare';
-$_['entry_manufacturer']     = 'Producător';
-$_['entry_store']            = 'Magazine';
-$_['entry_date_available']   = 'Dată disponibilă';
-$_['entry_quantity']         = 'Cantitate';
-$_['entry_minimum']          = 'Cantitate minimă';
-$_['entry_stock_status']     = 'Statut stoc epuizat';
-$_['entry_price']            = 'Preț';
-$_['entry_tax_class']        = 'Clasa de taxe';
-$_['entry_points']           = 'Puncte';
-$_['entry_option_points']    = 'Puncte';
-$_['entry_subtract']         = 'Scade stocul';
-$_['entry_weight_class']     = 'Clasa de greutate';
-$_['entry_weight']           = 'Greutate';
-$_['entry_dimension']        = 'Dimensiuni (Lugime x Lățime x Înălțtime)';
-$_['entry_length_class']     = 'Clasa de lungime';
-$_['entry_length']           = 'Lungime';
-$_['entry_width']            = 'Lățime';
-$_['entry_height']           = 'Înălțime';
-$_['entry_image']            = 'imagine';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Grup de clienți';
-$_['entry_date_start']       = 'Dată de început';
-$_['entry_date_end']         = 'Dată de sfârșit';
-$_['entry_priority']         = 'Prioritate';
-$_['entry_attribute']        = 'Atribut';
-$_['entry_attribute_group']  = 'Grup atribut';
-$_['entry_text']             = 'Text';
-$_['entry_option']           = 'Opțiune';
-$_['entry_option_value']     = 'Valoarea opțiunii';
-$_['entry_required']         = 'Obligatoriu';
-$_['entry_status']           = 'Stare';
-$_['entry_sort_order']       = 'Ordinea de sortare';
-$_['entry_category']         = 'Categorii';
-$_['entry_filter']           = 'Filtre';
-$_['entry_download']         = 'Descărcări';
-$_['entry_related']          = 'Produse similare';
-$_['entry_tag']          	 = 'Etichete produs';
-$_['entry_reward']           = 'Puncte recompensă';
-$_['entry_layout']           = 'Suprascrie Layout-ul';
-$_['entry_recurring']        = 'Profil recurent';
+$_['entry_name']		    = 'Nume';
+$_['entry_price']			= 'Preț';
+$_['entry_duration']		= 'Durată';
+$_['entry_cycle']			= 'Ciclu';
+$_['entry_frequency']		= 'Frecvență';
+$_['entry_trial_price']		= 'Preț încercare';
+$_['entry_trial_duration']	= 'Durată încercare';
+$_['entry_trial_status']	= 'Statut încercare';
+$_['entry_trial_cycle']	    = 'Ciclu încercare';
+$_['entry_trial_frequency'] = 'Frecvență încercare';
+$_['entry_status']			= 'Stare';
+$_['entry_sort_order']		= 'Ordinea de sortare';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'Unitate de păstrare stoc';
-$_['help_upc']               = 'Cod de produs universal';
-$_['help_ean']               = 'Număr european de articol';
-$_['help_jan']               = 'Număr japonez de articol';
-$_['help_isbn']              = 'Număr internațional standard de carte';
-$_['help_mpn']               = 'Seria producătorului';
-$_['help_manufacturer']      = '(Completare automată)';
-$_['help_minimum']           = 'Forțează o cantitate minimă de comandă';
-$_['help_stock_status']      = 'Statut indicat când un produs nu mai este pe stoc';
-$_['help_points']            = 'Număr de puncte necesare pentru a cumpăra acest obiect. Dacă nu doriți ca acest produs să fie cumpărat cu puncte lăsați 0.';
-$_['help_category']          = '(Completare automată)';
-$_['help_filter']            = '(Completare automată)';
-$_['help_download']          = '(Completare automată)';
-$_['help_related']           = '(Completare automată)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'Nume';
+$_['column_sort_order']	    = 'Ordinea de sortare';
+$_['column_action']         = 'Acțiune';
 
 // Error
-$_['error_warning']          = 'Avertisment: Vă rugăm să verificaţi formularul pentru erori!';
-$_['error_permission']       = 'Avertisment: Nu aveți permisiunea de a modifica produse!';
-$_['error_name']             = 'Numele produsului trebuie să aibă mai mult de 3 caractere și mai puțin de 255 caractere!';
-$_['error_meta_title']       = 'Meta tag : Titlu, trebuie să aibă mai mult de 3 şi mai puţin de 255 caractere!';
-$_['error_model']            = 'Modelul produsului trebuie să fie mai mare decât un caracter și mai mic decât 64 caractere!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Avertisment: Vă rugăm să verificaţi formularul pentru erori!';
+$_['error_permission']		= 'Avertisment: Nu aveţi permisiunea de a modifica profilele recurente!';
+$_['error_name']			= 'Numele profilului trebuie să fie mai mare de 3 caractere și mai puțin de 255 caractere!';
+$_['error_product']			= 'Avertisment: Acest profil recurent nu poate fi șters deoarece este asociat cu %s produse!';
