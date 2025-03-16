08c41332e781593d5e22def8acdedb7d13a8f151
OSWorX <info@osworx.net>
2025-03-16 09:11:55 +0100

    New translations forgotten.php (Norwegian)

--- upload/language/no-NO/no-NO.php
+++ upload/language/no-NO/no-NO.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Glemt ditt passord?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Versjon %s';
+$_['text_forgotten']  = 'Glemt passord';
+$_['text_your_email'] = 'Din e-postadresse';
+$_['text_email']      = 'Fyll inn e-postadressen som er tilknyttet din konto. Klikk \'Send\' for å få tilsendt en lenke for tilbakestilling av passord.';
+$_['text_success']    = 'En e-post med en bekreftelselenke har blitt sendt til din e-postadresse.';
+
+// Entry
+$_['entry_email']     = 'E-postadresse';
+$_['entry_password']  = 'Nytt passord';
+$_['entry_confirm']   = 'Bekreft';
+
+// Error
+$_['error_email']     = 'E-postadressen ble ikke funnet, kontroller at du har skrevet riktig og forsøk igjen.';
+$_['error_password']  = 'Passordet må inneholde mellom 4 og 20 tegn.';
+$_['error_confirm']   = 'Passord og bekreftelse er ikke lik.';
