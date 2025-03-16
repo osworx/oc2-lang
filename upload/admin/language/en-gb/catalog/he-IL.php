e33ce2bf75b63a8cd0d6cdcc481069da61f9ec95
OSWorX <info@osworx.net>
2025-03-16 14:17:34 +0100

    New translations category.php (Hebrew)

--- upload/admin/language/en-gb/catalog/he-IL.php
+++ upload/admin/language/en-gb/catalog/he-IL.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'קבוצות מאפיינים';
+$_['heading_title']          = 'קטגוריות';
 
 // Text
-$_['text_success']      = 'הצלחה: קבוצות תכונה שונתה!';
-$_['text_list']         = 'קבוצת תכונות הרשימה';
-$_['text_add']          = 'הוסף קבוצת תכונות';
-$_['text_edit']         = 'קבוצת תכונות עריכה';
+$_['text_success']           = 'הצלחה: שינית קטגוריות!';
+$_['text_list']              = 'רשימת הקטגוריות';
+$_['text_add']               = 'הוסף קטגוריה';
+$_['text_edit']              = 'ערוך קטגוריה';
+$_['text_default']           = 'ברירת המחדל';
 
 // Column
-$_['column_name']       = 'שם התכונה קבוצה';
-$_['column_sort_order'] = 'סדר מיון';
-$_['column_action']     = 'פעולה';
+$_['column_name']            = 'שם קטגוריה';
+$_['column_sort_order']      = 'סדר מיון';
+$_['column_action']          = 'פעולה';
 
 // Entry
-$_['entry_name']        = 'שם התכונה קבוצה';
-$_['entry_sort_order']  = 'סדר מיון';
+$_['entry_name']             = 'שם קטגוריה';
+$_['entry_description']      = 'תיאור';
+$_['entry_meta_title'] 	     = 'מטא -תג כותרת';
+$_['entry_meta_keyword']     = 'מטא -תג מילת מפתח';
+$_['entry_meta_description'] = 'מטא -תג תיאור';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'אב';
+$_['entry_filter']           = 'פילטרים';
+$_['entry_store']            = 'חנויות';
+$_['entry_image']            = 'תמונה';
+$_['entry_top']              = 'עליון';
+$_['entry_column']           = 'עמודות';
+$_['entry_sort_order']       = 'סדר מיון';
+$_['entry_status']           = 'סטטוס';
+$_['entry_layout']           = 'פריסת ביטול';
+
+// Help
+$_['help_filter']            = '(השלמה אוטומטית)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'הצג בבר התפריט העליון. פועל רק עבור קטגוריות האב.';
+$_['help_column']            = 'מספר העמודות שישמשו עבור 3 כפתורי הקטגוריות בתחתית. פועל רק עבור קטגוריות האב.';
 
 // Error
-$_['error_warning']     = 'אזהרה: אנא בדוק בקפידה אם יש שגיאות בטופס!';
-$_['error_permission']  = 'אזהרה: אין לך הרשאה לשינוי קבוצות תכונה!';
-$_['error_name']        = 'שם קבוצת תכונות חייב להיות בין 3 ל 64 תווים!';
-$_['error_attribute']   = 'אזהרה: לא ניתן למחוק את קבוצה תכונה זו מאחר והיא מוקצת כרגע ל %s תכונות!';
-$_['error_product']     = 'אזהרה: לא ניתן למחוק את קבוצה תכונה זו מאחר והיא מוקצת כרגע ל %s מוצרים!';
+$_['error_warning']          = 'אזהרה: אנא בדוק בקפידה אם יש שגיאות בטופס!';
+$_['error_permission']       = 'אזהרה: אין לך הרשאה לשינוי קטגוריות!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'מטה-תג הכותרת חייב להיות גדול מ- 3 לפחות מ- 255 תווים!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
