9b978c163145a3a41395b317c8583b3cca96b055
OSWorX <info@osworx.net>
2025-03-16 14:16:36 +0100

    New translations attribute_group.php (French)

--- upload/admin/language/en-gb/catalog/fr-FR.php
+++ upload/admin/language/en-gb/catalog/fr-FR.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Attributs';
+$_['heading_title']     = 'Groupe d’attributs';
 
 // Text
-$_['text_success']           = 'Les attributs ont correctement été sauvegardés.';
-$_['text_list']              = 'Liste des attributs';
-$_['text_add']               = 'Ajouter un attribut';
-$_['text_edit']              = 'Modifier l’attribut';
+$_['text_success']      = 'Les groupes d’attributs ont correctement été modifiés !';
+$_['text_list']         = 'Liste des groupes d’attributs';
+$_['text_add']          = 'Ajouter un groupe d’attributs';
+$_['text_edit']         = 'Modifier un groupe d’attributs';
 
 // Column
-$_['column_name']            = 'Nom de l\'attribut';
-$_['column_attribute_group'] = 'Groupe d\'attributs';
-$_['column_sort_order']      = 'Classement';
-$_['column_action']          = 'Action';
+$_['column_name']       = 'Nom du groupe d’attributs';
+$_['column_sort_order'] = 'Classement';
+$_['column_action']     = 'Action';
 
 // Entry
-$_['entry_name']             = 'Nom de l\'attribut';
-$_['entry_attribute_group']  = 'Groupe d\'attributs';
-$_['entry_sort_order']       = 'Classement';
+$_['entry_name']        = 'Nom du groupe d’attributs';
+$_['entry_sort_order']  = 'Classement';
 
 // Error
-$_['error_warning']          = 'Attention : veuillez vérifier le formulaire';
-$_['error_permission']       = 'Attention : Vous n\'êtes pas autorisé à modifier les attributs !';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'Le nom de l’attribut doit contenir entre 3 et 64 caractères !';
-$_['error_product']          = 'Attention : Cet attribut ne peut être supprimé car il est actuellement affecté à %s produits !';
+$_['error_warning']     = 'Attention : veuillez vérifier le formulaire';
+$_['error_permission']  = 'Attention : vous n’êtes pas autorisé à modifier les groupes d’attributs !';
+$_['error_name']        = 'Attention : le nom du groupe d’attributs doit contenir entre 3 et 64 caractères !';
+$_['error_attribute']   = 'Attention : ce groupe d’attributs ne peut être supprimé car il est actuellement attribué à %s attributs !';
+$_['error_product']     = 'Attention : ce groupe d’attributs ne peut être supprimé car il est actuellement attribué à %s produits !';
