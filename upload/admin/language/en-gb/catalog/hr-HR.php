7d02a12a6a025ebd466c53e09fd9ee5f79c2f94f
OSWorX <info@osworx.net>
2025-03-16 14:18:00 +0100

    New translations category.php (Croatian)

--- upload/admin/language/en-gb/catalog/hr-HR.php
+++ upload/admin/language/en-gb/catalog/hr-HR.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Grupe značajki';
+$_['heading_title']          = 'Kategorije';
 
 // Text
-$_['text_success']      = 'Uspjeh! Promijenili ste grupe značajki!';
-$_['text_list']         = 'Popis grupa značajki';
-$_['text_add']          = 'Dodaj grupu značajki';
-$_['text_edit']         = 'Uredi grupu značajki';
+$_['text_success']           = 'Uspjeh: Promjenili ste kategorije!';
+$_['text_list']              = 'Popis kategorija';
+$_['text_add']               = 'Dodaj kategoriju';
+$_['text_edit']              = 'Uredi kategoriju';
+$_['text_default']           = 'Zadano';
 
 // Column
-$_['column_name']       = 'Naziv grupe značajki';
-$_['column_sort_order'] = 'Redoslijed';
-$_['column_action']     = 'Radnja';
+$_['column_name']            = 'Naziv kategorije';
+$_['column_sort_order']      = 'Redoslijed';
+$_['column_action']          = 'Radnja';
 
 // Entry
-$_['entry_name']        = 'Naziv grupe značajki';
-$_['entry_sort_order']  = 'Redoslijed';
+$_['entry_name']             = 'Naziv kategorije';
+$_['entry_description']      = 'Opis';
+$_['entry_meta_title'] 	     = 'Meta tag naslov';
+$_['entry_meta_keyword']     = 'Meta tag ključne riječi';
+$_['entry_meta_description'] = 'Meta tag opis';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Nadređena kategorija';
+$_['entry_filter']           = 'Filteri';
+$_['entry_store']            = 'Trgovine';
+$_['entry_image']            = 'Slika';
+$_['entry_top']              = 'Vrh';
+$_['entry_column']           = 'Stupci';
+$_['entry_sort_order']       = 'Redoslijed';
+$_['entry_status']           = 'Status';
+$_['entry_layout']           = 'Premošćivanje izgleda';
+
+// Help
+$_['help_filter']            = '(Samodovršetak)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Prikazati kao vršnu stavku u glavnom izborniku. Radi samo za kategorije koje nemaju nadređenu kategoriju.';
+$_['help_column']            = 'Broj stupaca u glavnom izborniku za prve tri podređene kategorije. Radi samo za kategorije koje nemaju nadređenu kategoriju.';
 
 // Error
-$_['error_warning']     = 'Upozorenje: Pažljivo provjerite ima li pogrešaka u obrascu!';
-$_['error_permission']  = 'Upozorenje! Niste ovlašteni za promjenu grupe značajki!';
-$_['error_name']        = 'Naziv grupe značajki mora biti između 3 i 64 znaka!';
-$_['error_attribute']   = 'Upozorenje: Ova grupa značajki ne može se izbrisati jer ju koristi %s značajki!';
-$_['error_product']     = 'Upozorenje: Ova grupa značajki ne može se izbrisati jer ju koristi %s artikala!';
+$_['error_warning']          = 'Upozorenje: Pažljivo provjerite ima li pogrešaka u obrascu!';
+$_['error_permission']       = 'Upozorenje: Niste ovlašteni za izmjene u kategorijama!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Meta tag naslov mora biti između 3 i 255 znakova!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
