ea4babab4ed4d07f01b6a7894827e30255eda155
OSWorX <info@osworx.net>
2025-03-16 14:17:05 +0100

    New translations attribute_group.php (Galician)

--- upload/admin/language/en-gb/catalog/gl-ES.php
+++ upload/admin/language/en-gb/catalog/gl-ES.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Atributos';
+$_['heading_title']     = 'Grupo de atributos';
 
 // Text
-$_['text_success']           = 'Modificaronse os atributos correctamente!';
-$_['text_list']              = 'Lista de atributos';
-$_['text_add']               = 'Engadir atributo';
-$_['text_edit']              = 'Modifcar atributo';
+$_['text_success']      = 'Modificou o grupo de atributos correctamente!';
+$_['text_list']         = 'Lista de grupo de atributos';
+$_['text_add']          = 'Engadir grupo de atributos';
+$_['text_edit']         = 'Editar grupo de atributos';
 
 // Column
-$_['column_name']            = 'Nome do atributo';
-$_['column_attribute_group'] = 'Grupo de atributos';
-$_['column_sort_order']      = 'Orde da clasificación';
-$_['column_action']          = 'Acción';
+$_['column_name']       = 'Nome do grupo de atributos';
+$_['column_sort_order'] = 'Orde da clasificación';
+$_['column_action']     = 'Acción';
 
 // Entry
-$_['entry_name']             = 'Nome do atributo';
-$_['entry_attribute_group']  = 'Grupo de atributos';
-$_['entry_sort_order']       = 'Orde da clasificación';
+$_['entry_name']        = 'Nome do grupo de atributos';
+$_['entry_sort_order']  = 'Orde da clasificación';
 
 // Error
-$_['error_warning']          = 'Aviso: Por favor, revise o formulario e busque os erros!';
-$_['error_permission']       = 'Aviso: Non ten permisos para modificar os atributos!';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'O nome do atributo debe ter entre 3 e 64 caracteres!';
-$_['error_product']          = 'Aviso: Este atributo non pode ser eliminado, actualmente está atribuído a %s produtos!';
+$_['error_warning']     = 'Aviso: Por favor, revise o formulario e busque os erros!';
+$_['error_permission']  = 'Aviso: Non ten permisos para modificar os grupos de atributos!';
+$_['error_name']        = 'O nome do grupo de atributos debe ter entre 3 e 64 caracteres!';
+$_['error_attribute']   = 'Aviso: Este grupo de atributos non pode ser eliminado, actualmente esta asignado a %s atributos!';
+$_['error_product']     = 'Aviso: Este atributo non pode ser eliminado, actualmente está atribuído a %s produtos!';
