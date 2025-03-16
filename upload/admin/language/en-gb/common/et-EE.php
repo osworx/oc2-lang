6dd56cbca6bde9f3a5d111dc649aa52b13ea2555
OSWorX <info@osworx.net>
2025-03-16 14:27:22 +0100

    New translations filemanager.php (Estonian)

--- upload/admin/language/en-gb/common/et-EE.php
+++ upload/admin/language/en-gb/common/et-EE.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Töölaud';
+$_['heading_title']    = 'Pildihaldur';
+
+// Text
+$_['text_uploaded']    = 'Õnnestumine: Sinu fail on üleslaetud!';
+$_['text_directory']   = 'Õnnestumine: Kaust loodud!';
+$_['text_delete']      = 'Õnnestumine: Sinu fail või kaust on kustutatud!';
+
+// Entry
+$_['entry_search']     = 'Otsi..';
+$_['entry_folder']     = 'Kausta nimi';
 
 // Error
-$_['error_install'] = 'Hoiatus: Installimiskaust veel eksisteerib ja tuleks kustutada turvalisuse huvides!';
+$_['error_permission'] = 'Hoiatus: Luba keelatud!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Warning: A file or directory with the same name already exists!';
+$_['error_directory']  = 'Warning: Directory does not exist!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Hoiatus: Vale faili tüüp!';
+$_['error_upload']     = 'Hoiatus: Faili ei saanud üleslaadida teadmata põhjusel!';
+$_['error_delete']     = 'Hoiatus: Sa ei saa kustutada seda kausta!';
