3d4bd4d1c34d754cb00d548a4bd6b04c1e8b0c34
OSWorX <info@osworx.net>
2025-03-16 11:13:37 +0100

    New translations review.php (Danish)

--- upload/admin/language/en-gb/catalog/da-DK.php
+++ upload/admin/language/en-gb/catalog/da-DK.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'Tilbagevendende profil';
+$_['heading_title']     = 'Anmeldelser';
 
 // Text
-$_['text_success']          = 'Succes: Du har ændret tilbagevendende profiler!';
-$_['text_list']             = 'Tilbagevendende profiler';
-$_['text_add']              = 'Tilføj tilbagevendende profil';
-$_['text_edit']             = 'Rediger tilbagevendende profil';
-$_['text_day']				= 'Dag';
-$_['text_week']				= 'Uge';
-$_['text_semi_month']		= 'Halvmåned';
-$_['text_month']			= 'Måned';
-$_['text_year']				= 'År';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Gentagne mængder beregnes på baggrund af hyppighed og gentagelser.</p><p>Hvis du fx anvender hyppigheden "Ugentlig" og gentagelse "2", bliver kunden opkrævet hver anden uge.</p><p>Varigheden er antallet af gange, kunden ønsker at betale. Sæt værdien til 0, hvis der skal betales, indtil kunden opsiger aftalen.</p>';
-$_['text_profile']			= 'Tilbagevendende profil';
-$_['text_trial']			= 'Prøveprofil';
-
-// Entry
-$_['entry_name']		    = 'Navn';
-$_['entry_price']			= 'Pris';
-$_['entry_duration']		= 'Varighed';
-$_['entry_cycle']			= 'Gentagelse (hver x gange hyppighed)';
-$_['entry_frequency']		= 'Hyppighed';
-$_['entry_trial_price']		= 'Prøvepris';
-$_['entry_trial_duration']	= 'Prøvevarighed';
-$_['entry_trial_status']	= 'Prøve status';
-$_['entry_trial_cycle']	    = 'Prøvecyklus';
-$_['entry_trial_frequency'] = 'Prøvehyppighed';
-$_['entry_status']			= 'Status';
-$_['entry_sort_order']		= 'Sorteringsrækkefølge';
+$_['text_success']      = 'Succes: Du har ændret Anmeldelser!';
+$_['text_list']         = 'Anmeldelser';
+$_['text_add']          = 'Tilføj anmeldelse';
+$_['text_edit']         = 'Rediger anmeldelse';
 
 // Column
-$_['column_name']			= 'Navn';
-$_['column_sort_order']	    = 'Sorteringsrækkefølge';
-$_['column_action']         = 'Handling';
+$_['column_product']    = 'Produkt';
+$_['column_author']     = 'Forfatter';
+$_['column_rating']     = 'Bedømmelse';
+$_['column_status']     = 'Status';
+$_['column_date_added'] = 'Tilføjet dato';
+$_['column_action']     = 'Handling';
+
+// Entry
+$_['entry_product']     = 'Produkt';
+$_['entry_author']      = 'Forfatter';
+$_['entry_rating']      = 'Bedømmelse';
+$_['entry_status']      = 'Status';
+$_['entry_text']        = 'Tekst';
+$_['entry_date_added']  = 'Tilføjet dato';
+
+// Help
+$_['help_product']      = '(Autofuldførelse)';
 
 // Error
-$_['error_warning']         = 'Advarsel: Gennemgå venligst formularen for fejl!';
-$_['error_permission']		= 'Fejl: Du har ikke tilladelse til at redigere tilbagevendende profiler!';
-$_['error_name']			= 'Produktnavn skal være mellem 3 og 255 karakterer!';
-$_['error_product']			= 'Fejl: Denne tilbagevendende profil kan ikke slettes, da det er i øjeblikket tilknyttet %s produkter!';
+$_['error_warning']     = 'Advarsel: Gennemgå venligst formularen for fejl!';
+$_['error_permission']  = 'Fejl: Du har ikke tilladelse til at ændre valgmuligheder!';
+$_['error_product']     = 'Produkt påkrævet!';
+$_['error_author']      = 'Forfatternavn skal være mellem 3 og 64 tegn!';
+$_['error_text']        = 'Anmeldelsestekst skal være mindst ét tegn!';
+$_['error_rating']      = 'Bedømmelse påkrævet!';
