9bf5c9cc04275d1af7d4aec4a8ad4dda59502d18
OSWorX <info@osworx.net>
2025-03-16 09:07:03 +0100

    New translations manufacturer.php (Belarusian)

--- upload/language/be-BY/be-BY.php
+++ upload/language/be-BY/be-BY.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Інфармацыя';
+$_['heading_title']     = 'Вытворцы';
 
 // Text
-$_['text_success']           = 'Вы паспяхова змянілі інфармацыю!';
-$_['text_list']              = 'Спіс інфармацыі';
-$_['text_add']               = 'Дадаць інфармацыю';
-$_['text_edit']              = 'Рэдагаваць інфармацыю';
-$_['text_default']           = 'Па-змаўчанню';
+$_['text_success']      = 'Поспех: Вытворцы змененыя!';
+$_['text_list']         = 'Спіс вытворцаў';
+$_['text_add']          = 'Дадаць вытворцу';
+$_['text_edit']         = 'Рэдагаваць вытворцу';
+$_['text_default']      = 'Па-змаўчанню';
+$_['text_percent']      = 'Адсотак';
+$_['text_amount']       = 'Фіксіраваная колькасць';
 
 // Column
-$_['column_title']           = 'Тытул інфармацыі';
-$_['column_sort_order']	     = 'Парадак сартавання';
-$_['column_action']          = 'Дзеянне';
+$_['column_name']       = 'Назва вытворцы';
+$_['column_sort_order'] = 'Парадак сартавання';
+$_['column_action']     = 'Дзеянне';
 
 // Entry
-$_['entry_title']            = 'Тытул інфармацыі';
-$_['entry_description']      = 'Апісанне';
-$_['entry_store']            = 'Крамы';
-$_['entry_meta_title'] 	     = 'Мета-тэг Title';
-$_['entry_meta_keyword'] 	 = 'Мета-тэг Keywords';
-$_['entry_meta_description'] = 'Мета-тэг Description';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'Уніз';
-$_['entry_status']           = 'Статус';
-$_['entry_sort_order']       = 'Парадак сартавання';
-$_['entry_layout']           = 'Перавызначэнне разметкі';
+$_['entry_name']        = 'Назва вытворцы';
+$_['entry_store']       = 'Крамы';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'Выява';
+$_['entry_sort_order']  = 'Парадак сартавання';
+$_['entry_type']        = 'Тып';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'Паказаць у ніжняй частцы.';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'Увага: Калі ласка, ўважліва праверце ўведзеные дадзеныя!';
-$_['error_permission']       = 'Увага: у Вас няма права змяняць інфармацыю!';
-$_['error_title']            = 'Тытул інфармацыі павінен мець ад 3 да 64 сімвалаў!';
-$_['error_description']      = 'Апісанне павінна складацца больш чым з 64 сімвалаў!';
-$_['error_meta_title']       = 'Даўжыня meta title павінна быць ад 3 да 255 сімвалаў!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'Увага: Гэтая інфармацыйная старонка не можа быць выдалена, бо звязана з правіламі для профіляў карыстальнікаў!';
-$_['error_checkout']         = 'Увага: Гэтая інфармацыйная старонка не можа быць выдалена, бо звязана з правіламі разлікаў у краме!';
-$_['error_affiliate']        = 'Увага: Гэтая інфармацыйная старонка не можа быць выдалена, бо звязана з правіламі для партнёраў!';
-$_['error_return']           = 'Увага: Гэтая інфармацыйная старонка не можа быць выдалена бо выкарыстоўваецца для правілаў вяртання пакупак!';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'Увага: Калі ласка, ўважліва праверце ўведзеные дадзеныя!';
+$_['error_permission']  = 'Увага: Вы не можаце змяняць вытворцаў!';
+$_['error_name']        = 'Называ вытворцы павінна быць ад 2 да 64 сімвалаў!';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'Увага: Гэты вытворца не можа быць выдалены, бо звазаны з %s таварамі!';
