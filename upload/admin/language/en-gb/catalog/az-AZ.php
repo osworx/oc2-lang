103d92644ea19a060fb751722496d445e1d720aa
OSWorX <info@osworx.net>
2025-03-16 11:07:14 +0100

    New translations category.php (Azerbaijani)

--- upload/admin/language/en-gb/catalog/az-AZ.php
+++ upload/admin/language/en-gb/catalog/az-AZ.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Xüsusiyyət qrupu';
+$_['heading_title']          = 'Kateqoriyalar';
 
 // Text
-$_['text_success']      = 'Uğurlu: Xüsusiyyət qrupunun adı redaktə edildi!';
-$_['text_list']         = 'Atribut qurup siyahısı';
-$_['text_add']          = 'Atribut Qurupu əlavə edin';
-$_['text_edit']         = 'Atribut Qurupunu dəyişdirin';
+$_['text_success']           = 'Uğurlu: kateqoriya redaktə edildi!';
+$_['text_list']              = 'Kateqoriya siyahısı';
+$_['text_add']               = 'Kateqoriya Əlavə et';
+$_['text_edit']              = 'Kateqoriyaya düzəliş';
+$_['text_default']           = 'Varsayılan';
 
 // Column
-$_['column_name']       = 'Atribut Qurup adı';
-$_['column_sort_order'] = 'Növlərə görə sırala';
-$_['column_action']     = 'Hərəkət';
+$_['column_name']            = 'Kateqoriya adı';
+$_['column_sort_order']      = 'Növlərə görə sırala';
+$_['column_action']          = 'Hərəkət';
 
 // Entry
-$_['entry_name']        = 'Atribut Qurup adı';
-$_['entry_sort_order']  = 'Növlərə görə sırala';
+$_['entry_name']             = 'Kateqoriya adı';
+$_['entry_description']      = 'Açıqlama';
+$_['entry_meta_title'] 	     = 'Meta başlığı';
+$_['entry_meta_keyword']     = 'Meta açar sözləri';
+$_['entry_meta_description'] = 'Meta İzahat';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Valideyn';
+$_['entry_filter']           = 'Filterlər';
+$_['entry_store']            = 'Dükanlar';
+$_['entry_image']            = 'Şəkillər';
+$_['entry_top']              = 'Top';
+$_['entry_column']           = 'Sütunlar';
+$_['entry_sort_order']       = 'Növlərə görə sırala';
+$_['entry_status']           = 'Status';
+$_['entry_layout']           = 'Düzeni Geçersiz Kılma';
+
+// Help
+$_['help_filter']            = 'Avtomatik tamamlama';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Üst menyu bar göstərilir. Yalnız ən üst kateqoriya üçün çalışır.';
+$_['help_column']            = '3 alt kateqoriyalar üçün istifadə ediləcək sütun sayı. Yalnız ən üst kateqoriyaları üçün çalışır.';
 
 // Error
-$_['error_warning']     = 'Xəbərdarlıq: Xahiş edirik, formu göstərilən səhvlər səbəbiylə diqqətlə nəzarət edin!';
-$_['error_permission']  = 'Xəbərdarlıq: Sizin Atribut qrupların dəyişdirmək üçün icazəniz yoxdur!';
-$_['error_name']        = 'Atribut Qrupunun adları 3 və 64 simvol arasında olmalıdır!';
-$_['error_attribute']   = 'Xəbərdarlıq: Bu Atribut qrupu silinə bilməz hal-hazırda %s atributları verilir!';
-$_['error_product']     = 'Xəbərdarlıq: Bu əlamət silinə bilməz, necə ki bu hal-hazırda %s məhsullarına təyin edilir!';
+$_['error_warning']          = 'Xəbərdarlıq: Xahiş edirik, formu göstərilən səhvlər səbəbiylə diqqətlə nəzarət edin!';
+$_['error_permission']       = 'Xəbərdarlıq: Kateqoriyaları dəyişdirmə icazəsinə sahib deyilsiniz!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Meta başlıq ən az 3 və ən çox 255 simvol olmalıdır!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
