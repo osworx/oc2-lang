ddcbf2b2b75449f0486fadb8738e8f425ff194d2
OSWorX <info@osworx.net>
2025-03-16 14:23:05 +0100

    New translations recurring.php (Czech)

--- upload/admin/language/en-gb/catalog/cs-CZ.php
+++ upload/admin/language/en-gb/catalog/cs-CZ.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'Produkty';
+$_['heading_title']			= 'Opakované profily';
 
 // Text
-$_['text_success']           = 'Úspěch: Produkt byl upraven!';
-$_['text_list']              = 'Seznam produktů';
-$_['text_add']               = 'Přidat produkt';
-$_['text_edit']              = 'Upravit produkt';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'Výchozí';
-$_['text_option']            = 'Možnost';
-$_['text_option_value']      = 'Hodnota volby';
-$_['text_percent']           = 'Procenta';
-$_['text_amount']            = 'Pevná částka';
-
-// Column
-$_['column_name']            = 'Název produktu';
-$_['column_model']           = 'Model';
-$_['column_image']           = 'Obrázek';
-$_['column_price']           = 'Cena';
-$_['column_quantity']        = 'Množství';
-$_['column_status']          = 'Stav';
-$_['column_action']          = 'Akce';
+$_['text_success']          = 'Úspěch: Jste upravili opakující profily!';
+$_['text_list']             = 'Seznam opakujících se profilů';
+$_['text_add']              = 'Přidat opakující profil';
+$_['text_edit']             = 'Upravit opakující profil';
+$_['text_day']				= 'Den';
+$_['text_week']				= 'Týden';
+$_['text_semi_month']		= 'Půlměsíčně';
+$_['text_month']			= 'Měsíc';
+$_['text_year']				= 'Rok';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Opakující se částky se počítají podle frekvence a cyklů.</p><p>Například, pokud používáte frekvenci "týden" a cyklus "2", pak uživateli bude účtováno každé 2 týdny.</p><p>Doba trvání je počet, kolikrát uživatel bude platit; nastavte na 0 pokud chcete platby, dokud nebudou zrušeny.</p>';
+$_['text_profile']			= 'Profil opakování';
+$_['text_trial']			= 'Zkušební profil';
 
 // Entry
-$_['entry_name']             = 'Název produktu';
-$_['entry_description']      = 'Popis';
-$_['entry_meta_title'] 	     = 'Meta Tag Title';
-$_['entry_meta_keyword'] 	 = 'Klíčová slova Meta Tag';
-$_['entry_meta_description'] = 'Meta Tag Description';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Model';
-$_['entry_sku']              = 'SKU';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'Místo';
-$_['entry_shipping']         = 'Vyžaduje dopravu';
-$_['entry_manufacturer']     = 'Výrobce';
-$_['entry_store']            = 'Obchody';
-$_['entry_date_available']   = 'Datum k dispozici';
-$_['entry_quantity']         = 'Množství';
-$_['entry_minimum']          = 'Minimální množství';
-$_['entry_stock_status']     = 'Mimo sklad stav';
-$_['entry_price']            = 'Cena';
-$_['entry_tax_class']        = 'Daňová třída';
-$_['entry_points']           = 'Body';
-$_['entry_option_points']    = 'Body';
-$_['entry_subtract']         = 'Odečíst sklad';
-$_['entry_weight_class']     = 'Hmotnostní třída';
-$_['entry_weight']           = 'Váha';
-$_['entry_dimension']        = 'Rozměry (D x Š x V)';
-$_['entry_length_class']     = 'Délka třídy';
-$_['entry_length']           = 'Délka';
-$_['entry_width']            = 'Šířka';
-$_['entry_height']           = 'Výška';
-$_['entry_image']            = 'Obrázek';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Zákaznická skupina';
-$_['entry_date_start']       = 'Počáteční datum';
-$_['entry_date_end']         = 'Datum ukončení';
-$_['entry_priority']         = 'Priorita';
-$_['entry_attribute']        = 'Atribut';
-$_['entry_attribute_group']  = 'Skupina atributů';
-$_['entry_text']             = 'Text';
-$_['entry_option']           = 'Možnost';
-$_['entry_option_value']     = 'Hodnota volby';
-$_['entry_required']         = 'Požadováno';
-$_['entry_status']           = 'Stav';
-$_['entry_sort_order']       = 'Řadit podle';
-$_['entry_category']         = 'Kategorie';
-$_['entry_filter']           = 'Filtry';
-$_['entry_download']         = 'Soubory ke stažení';
-$_['entry_related']          = 'Související produkty';
-$_['entry_tag']          	 = 'Produkt značky';
-$_['entry_reward']           = 'Věrnostní body';
-$_['entry_layout']           = 'Přepsání layoutu';
-$_['entry_recurring']        = 'Profil opakování';
+$_['entry_name']		    = 'Název';
+$_['entry_price']			= 'Cena';
+$_['entry_duration']		= 'Doba trvání';
+$_['entry_cycle']			= 'Cyklus';
+$_['entry_frequency']		= 'Frekvence';
+$_['entry_trial_price']		= 'Zkušební cena';
+$_['entry_trial_duration']	= 'Zkušební doba';
+$_['entry_trial_status']	= 'Zkušební stav';
+$_['entry_trial_cycle']	    = 'Zkušební cyklus';
+$_['entry_trial_frequency'] = 'Zkušební frekvence';
+$_['entry_status']			= 'Stav';
+$_['entry_sort_order']		= 'Řadit podle';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'Skladová jednotka';
-$_['help_upc']               = 'Univerzální kód produktu';
-$_['help_ean']               = 'Evropské číslo produktu';
-$_['help_jan']               = 'Japonské číslo produktu';
-$_['help_isbn']              = 'Mezinárodní standardní číslo knihy';
-$_['help_mpn']               = 'Číslo dílu výrobce';
-$_['help_manufacturer']      = '(Funkce Automatické dokončování)';
-$_['help_minimum']           = 'Vynutit minimální objednané množství';
-$_['help_stock_status']      = 'Zobrazit status, že produkt není skladem';
-$_['help_points']            = 'Potřebný počet bodů k nákupu této položky. Pokud nechcete, aby tento produkt byl zakoupen s body, ponechte jako 0.';
-$_['help_category']          = '(Funkce Automatické dokončování)';
-$_['help_filter']            = '(Funkce Automatické dokončování)';
-$_['help_download']          = '(Funkce Automatické dokončování)';
-$_['help_related']           = '(Funkce Automatické dokončování)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'Název';
+$_['column_sort_order']	    = 'Řadit podle';
+$_['column_action']         = 'Akce';
 
 // Error
-$_['error_warning']          = 'Upozornění: Zkontrolujte formulář pečlivě pro chyby!';
-$_['error_permission']       = 'Upozornění: Nemáte oprávnění k úpravě produktů!';
-$_['error_name']             = 'Název produktu musí být větší než 3 a méně než 255 znaků!';
-$_['error_meta_title']       = 'Meta Title musí být větší než 3 a méně než 255 znaků!';
-$_['error_model']            = 'Model prduktu musí být větší než 1 a menší než 64 znaků!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Upozornění: Zkontrolujte formulář pečlivě pro chyby!';
+$_['error_permission']		= 'Upozornění: Nemáte oprávnění k úpravě opakujících profilů!';
+$_['error_name']			= 'Název profilu musí být větší než 3 a méně než 255 znaků!';
+$_['error_product']			= 'Varování: Tento opakující profil nelze odstranit, protože je v současnosti přiřazen k %s produktů!';
