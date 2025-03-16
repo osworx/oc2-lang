39d24eb5f93ced16454ef889f6ccfa9d9309f37a
OSWorX <info@osworx.net>
2025-03-16 11:06:40 +0100

    New translations category.php (Hungarian)

--- upload/admin/language/en-gb/catalog/hu-HU.php
+++ upload/admin/language/en-gb/catalog/hu-HU.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Jellemzőcsoportok';
+$_['heading_title']          = 'Kategóriák';
 
 // Text
-$_['text_success']      = 'Jellemzőcsoportok módosítása sikeres!';
-$_['text_list']         = 'Jellemzőcsoport lista';
-$_['text_add']          = 'Jellemzőcsoport hozzáadás';
-$_['text_edit']         = 'Jellemzőcsoport szerkesztése';
+$_['text_success']           = 'Siker: Kategóriák módosítva!';
+$_['text_list']              = 'Kategória lista';
+$_['text_add']               = 'Új kategória';
+$_['text_edit']              = 'Kategória szerkesztése';
+$_['text_default']           = 'Alapértelmezés';
 
 // Column
-$_['column_name']       = 'Jellemzőcsoport neve';
-$_['column_sort_order'] = 'Rendezés';
-$_['column_action']     = 'Művelet';
+$_['column_name']            = 'Kategória neve';
+$_['column_sort_order']      = 'Rendezés';
+$_['column_action']          = 'Művelet';
 
 // Entry
-$_['entry_name']        = 'Jellemzőcsoport neve';
-$_['entry_sort_order']  = 'Rendezés';
+$_['entry_name']             = 'Kategória neve';
+$_['entry_description']      = 'Leírás';
+$_['entry_meta_title'] 	     = 'Meta Tag-cím';
+$_['entry_meta_keyword']     = 'Meta Tag kulcsszó';
+$_['entry_meta_description'] = 'Meta-Tag leírás ';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Szülő';
+$_['entry_filter']           = 'Szűrők';
+$_['entry_store']            = 'Áruházak';
+$_['entry_image']            = 'Kép';
+$_['entry_top']              = 'Oldal teteje';
+$_['entry_column']           = 'Oszlopok';
+$_['entry_sort_order']       = 'Rendezés';
+$_['entry_status']           = 'Állapot';
+$_['entry_layout']           = 'Elrendezés felülbírálása';
+
+// Help
+$_['help_filter']            = '(Autó Kiegészítés)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'A felső meüsorban jelenítse meg. Csak a felső menüsor szülő kategóriákra érvényes.';
+$_['help_column']            = 'A használt oszlopok száma az alsó 3 kategóriára . Csak a felső szülő kategóriára érvényes.';
 
 // Error
-$_['error_warning']     = 'Figyelem: Kérjük, figyelmesen ellenőrizze a hibákat!';
-$_['error_permission']  = 'Figyelem: Nincs jogosultsága a jellemzőcsoportok módosítására!';
-$_['error_name']        = 'Jellemzőcsoport nevének 3 és 64 karakter között kell lennie!';
-$_['error_attribute']   = 'Figyelem: Ez az jellemzőcsoport nem törölhető, mivel jelenleg %s jellemző van hozzárendelve!';
-$_['error_product']     = 'Figyelem: Ez az jellemzőcsoport nem törölhető, mivel jelenleg %s termékhez van hozzárendelve!';
+$_['error_warning']          = 'Figyelem: Kérjük, figyelmesen ellenőrizze a hibákat!';
+$_['error_permission']       = 'Figyelem: Nincs jogosultsága kategóriák módosítására!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'A meta cím több mint 3, és kevesebb, mint 255 karakternek kell lennie!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
