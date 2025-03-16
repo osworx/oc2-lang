dd430b81d5b05905cbc664e565ab6b4b70408589
OSWorX <info@osworx.net>
2025-03-16 14:19:59 +0100

    New translations information.php (Hungarian)

--- upload/admin/language/en-gb/catalog/hu-HU.php
+++ upload/admin/language/en-gb/catalog/hu-HU.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'Szűrők';
+$_['heading_title']          = 'Információ';
 
 // Text
-$_['text_success']      = 'Siker: Szűrők sikeresen módosítva!';
-$_['text_list']         = 'Lista szűrése';
-$_['text_add']          = 'Szűrő hozzáadása';
-$_['text_edit']         = 'Szűrő szerkesztése';
+$_['text_success']           = 'Siker: Módosított információ!';
+$_['text_list']              = 'Információ lista';
+$_['text_add']               = 'Információ hozzáadása';
+$_['text_edit']              = 'Információ szerkesztése';
+$_['text_default']           = 'Alapértelmezés';
 
 // Column
-$_['column_group']      = 'Szűrőcsoport';
-$_['column_sort_order'] = 'Rendezés';
-$_['column_action']     = 'Művelet';
+$_['column_title']           = 'Információ címe';
+$_['column_sort_order']	     = 'Rendezés';
+$_['column_action']          = 'Művelet';
 
 // Entry
-$_['entry_group']       = 'Szűrő csoport neve';
-$_['entry_name']        = 'Szűrő neve';
-$_['entry_sort_order']  = 'Rendezés';
+$_['entry_title']            = 'Információ címe';
+$_['entry_description']      = 'Leírás';
+$_['entry_store']            = 'Áruházak';
+$_['entry_meta_title'] 	     = 'Meta Tag-cím';
+$_['entry_meta_keyword'] 	 = 'Meta Tag kulcsszó';
+$_['entry_meta_description'] = 'Meta-Tag leírás ';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Oldal alja';
+$_['entry_status']           = 'Állapot';
+$_['entry_sort_order']       = 'Rendezés';
+$_['entry_layout']           = 'Elrendezés felülbírálása';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Az oldal ajlán jelenítse meg.';
 
 // Error
-$_['error_warning']     = 'Figyelem: Kérjük, figyelmesen ellenőrizze a hibákat!';
-$_['error_permission']  = 'Figyelmeztetés: Nincs elegendő jogosultság a szűrők módosításához!';
-$_['error_group']       = 'A szűrő csoport nevének 1 és 64 karakter között kell lennie!';
-$_['error_name']        = 'A szűrő nevének 1 és 64 karakter között kell lennie!';
+$_['error_warning']          = 'Figyelem: Kérjük, figyelmesen ellenőrizze a hibákat!';
+$_['error_permission']       = 'Figyelem: Nincs jogosultsága módosítani az információt!';
+$_['error_title']            = 'Az információ címe 3 és 64 karakter között kell lennie!';
+$_['error_description']      = 'A leírás legfeljebb 3 karakterből kell állnia!';
+$_['error_meta_title']       = 'A meta cím több mint 3, és kevesebb, mint 255 karakternek kell lennie!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'Figyelem: Ez az információs oldal nem törölhető, mert ez a jelenleg hozzá van rendelve a használati feltételekhez!';
+$_['error_checkout']         = 'Figyelem: Ez az információs oldal nem törölhető, mert ez a jelenleg hozzá van rendelve az üzleti feltételekhez!';
+$_['error_affiliate']        = 'Figyelem: Ez az információs oldal nem törölhető, mert ez a jelenleg hozzá van rendelve a partner feltételekhez!';
+$_['error_return']           = 'Figyelem: Ez az információs oldal nem törölhető, mert ez a jelenleg hozzá van rendelve a visszatérítési feltételekhez!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
