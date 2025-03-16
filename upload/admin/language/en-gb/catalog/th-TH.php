efa3bec68808190c77794aff932bdb2f626ead2a
OSWorX <info@osworx.net>
2025-03-16 11:14:06 +0100

    New translations review.php (Thai)

--- upload/admin/language/en-gb/catalog/th-TH.php
+++ upload/admin/language/en-gb/catalog/th-TH.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'Recurring Profile';
+$_['heading_title']     = 'บทวิจารณ์';
 
 // Text
-$_['text_success']          = 'ความสำเร็จ: คุณได้ปรับเปลี่ยน Recurring Profiles!';
-$_['text_list']             = 'รายการ Recurring Profile';
-$_['text_add']              = 'เพิ่ม Recurring Profile';
-$_['text_edit']             = 'แก้ไข Recurring Profile';
-$_['text_day']				= 'วันที่';
-$_['text_week']				= 'สัปดาห์';
-$_['text_semi_month']		= 'กึ่งเดือน';
-$_['text_month']			= 'เดือน';
-$_['text_year']				= 'ปี';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i>การแบ่งจ่ายสามารถคำนวนได้จาก ความถี่และรอบ</p><p>ตัวอย่าง: ถ้าคุณใช้ความถี่เป็น "สัปดาห์" และรอบเป็น "2" ผู้ใช้จะถูกเรียกเก็บเงินทุกๆ 2 อาทิตย์</p><p>ระยะเวลามีไว้กำหนดจำนวนครั้งที่จะเรียกเก็บเงิน ถ้าตั้งระยะเวลาเป็น 0 ลูกค้าจะต้องเป็นคนยกเลิกเอง</p>';
-$_['text_profile']			= 'Recurring Profile';
-$_['text_trial']			= 'โพรไฟล์การทดลอง';
-
-// Entry
-$_['entry_name']		    = 'ชื่อ';
-$_['entry_price']			= 'ราคา';
-$_['entry_duration']		= 'ระยะเวลา';
-$_['entry_cycle']			= 'รอบ';
-$_['entry_frequency']		= 'ความถี่';
-$_['entry_trial_price']		= 'ราคาทดลอง';
-$_['entry_trial_duration']	= 'ระยะเวลาทดลองใช้';
-$_['entry_trial_status']	= 'สถานะการทดลอง';
-$_['entry_trial_cycle']	    = 'รอบการทดลอง';
-$_['entry_trial_frequency'] = 'ความถี่ในการทดลอง';
-$_['entry_status']			= 'สถานะ';
-$_['entry_sort_order']		= 'ลำดับการจัดเรียง';
+$_['text_success']      = 'ความสำเร็จ: คุณได้แก้ไขรีวิว';
+$_['text_list']         = 'รายการรีวิว';
+$_['text_add']          = 'เพิ่มรีวิว';
+$_['text_edit']         = 'แก้ไขรีวิว';
 
 // Column
-$_['column_name']			= 'ชื่อ';
-$_['column_sort_order']	    = 'ลำดับการจัดเรียง';
-$_['column_action']         = 'การดำเนินการ';
+$_['column_product']    = 'สินค้า';
+$_['column_author']     = 'ผู้เขียน';
+$_['column_rating']     = 'คะแนน';
+$_['column_status']     = 'สถานะ';
+$_['column_date_added'] = 'วันที่เพิ่ม';
+$_['column_action']     = 'การดำเนินการ';
+
+// Entry
+$_['entry_product']     = 'สินค้า';
+$_['entry_author']      = 'ผู้เขียน';
+$_['entry_rating']      = 'คะแนน';
+$_['entry_status']      = 'สถานะ';
+$_['entry_text']        = 'ข้อความ';
+$_['entry_date_added']  = 'วันที่เพิ่ม';
+
+// Help
+$_['help_product']      = '(อัตโนมัติ)';
 
 // Error
-$_['error_warning']         = 'คำเตือน: กรุณาตรวจสอบแบบฟอร์มอย่างรอบคอบสำหรับข้อผิดพลาด!';
-$_['error_permission']		= 'คำเตือน: คุณไม่มีสิทธิ์ในการปรับเปลี่ยน recurring profiles!';
-$_['error_name']			= 'ชื่อโปรไฟล์ ต้องมีมากกว่า 3 และน้อยกว่า 255 ตัวอักษร!';
-$_['error_product']			= 'คำเตือน: โปรไฟล์นี้เกิดไม่สามารถลบเนื่องปัจจุบันมีการกำหนดให้กับผลิตภัณฑ์ %s';
+$_['error_warning']     = 'คำเตือน: กรุณาตรวจสอบแบบฟอร์มอย่างรอบคอบสำหรับข้อผิดพลาด!';
+$_['error_permission']  = 'คำเตือน: คุณไม่มีสิทธิ์ในการแก้ไข!';
+$_['error_product']     = 'จำเป็นต้องมีผลิตภัณฑ์!';
+$_['error_author']      = 'ชื่อผู้เขียนต้องอยู่ระหว่าง 3 ถึง 64 อักขระ';
+$_['error_text']        = 'ข้อความรีวิวต้องยาวอย่างน้อย 1 ตัวอักษร';
+$_['error_rating']      = 'กรุณาใส่คะแนนรีวิว';
