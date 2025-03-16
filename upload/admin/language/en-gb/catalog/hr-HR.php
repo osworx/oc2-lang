a1e2a4bd0306673e31b3375621a7816b1e874b02
OSWorX <info@osworx.net>
2025-03-16 14:18:49 +0100

    New translations download.php (Croatian)

--- upload/admin/language/en-gb/catalog/hr-HR.php
+++ upload/admin/language/en-gb/catalog/hr-HR.php
@@ -1,46 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Kategorije';
+$_['heading_title']     = 'Preuzimanja';
 
 // Text
-$_['text_success']           = 'Uspjeh: Promjenili ste kategorije!';
-$_['text_list']              = 'Popis kategorija';
-$_['text_add']               = 'Dodaj kategoriju';
-$_['text_edit']              = 'Uredi kategoriju';
-$_['text_default']           = 'Zadano';
+$_['text_success']      = 'Uspjeh: Promijenili ste preuzimanja!';
+$_['text_list']         = 'Popis preuzimanja';
+$_['text_add']          = 'Dodaj preuzimanje';
+$_['text_edit']         = 'Uredi preuzimanje';
+$_['text_upload']       = 'Vaša datoteka uspješno je učitana!';
 
 // Column
-$_['column_name']            = 'Naziv kategorije';
-$_['column_sort_order']      = 'Redoslijed';
-$_['column_action']          = 'Radnja';
+$_['column_name']       = 'Naziv preuzimanja';
+$_['column_date_added'] = 'Datum dodavanja';
+$_['column_action']     = 'Radnja';
 
 // Entry
-$_['entry_name']             = 'Naziv kategorije';
-$_['entry_description']      = 'Opis';
-$_['entry_meta_title'] 	     = 'Meta tag naslov';
-$_['entry_meta_keyword']     = 'Meta tag ključne riječi';
-$_['entry_meta_description'] = 'Meta tag opis';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = 'Nadređena kategorija';
-$_['entry_filter']           = 'Filteri';
-$_['entry_store']            = 'Trgovine';
-$_['entry_image']            = 'Slika';
-$_['entry_top']              = 'Vrh';
-$_['entry_column']           = 'Stupci';
-$_['entry_sort_order']       = 'Redoslijed';
-$_['entry_status']           = 'Status';
-$_['entry_layout']           = 'Premošćivanje izgleda';
+$_['entry_name']        = 'Naziv preuzimanja';
+$_['entry_filename']    = 'Naziv datoteke';
+$_['entry_mask']        = 'Maska';
 
 // Help
-$_['help_filter']            = '(Samodovršetak)';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = 'Prikazati kao vršnu stavku u glavnom izborniku. Radi samo za kategorije koje nemaju nadređenu kategoriju.';
-$_['help_column']            = 'Broj stupaca u glavnom izborniku za prve tri podređene kategorije. Radi samo za kategorije koje nemaju nadređenu kategoriju.';
+$_['help_filename']     = 'Datoteku na server možete učitati klikom na gumb za učitavanje ili možete FTP-om datoteku prenijeti na server u mapu za preuzimanja, a onda niže unijeti detalje.';
+$_['help_mask']         = 'Preporučljivo je da naziv datoteke i maske budu različiti kako bi se otežali pokušaji izravnog preuzimanja datoteka.';
 
 // Error
-$_['error_warning']          = 'Upozorenje: Pažljivo provjerite ima li pogrešaka u obrascu!';
-$_['error_permission']       = 'Upozorenje: Niste ovlašteni za izmjene u kategorijama!';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = 'Meta tag naslov mora biti između 3 i 255 znakova!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = 'Upozorenje: Pažljivo provjerite ima li pogrešaka u obrascu!';
+$_['error_permission']  = 'Upozorenje: Niste ovlašteni za izmjene preuzimanja!';
+$_['error_name']        = 'Naziv preuzimanja mora biti od 3 do 64 znaka!';
+$_['error_upload']      = 'Preuzimanje je obavezno!';
+$_['error_filename']    = 'Naziv datoteke mora biti između 3 i 128 znakova!';
+$_['error_exists']      = 'Datoteka ne postoji!';
+$_['error_mask']        = 'Maska mora biti između 3 i 128 znakova!';
+$_['error_filetype']    = 'Neispravan tip datoteke!';
+$_['error_product']     = 'Upozorenje: Ovo preuzimanje nije moguće izbrisati jer je trenutno povezano s %s proizvoda!';
