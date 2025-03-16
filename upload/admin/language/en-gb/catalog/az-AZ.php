4a1a3ebf2e4fcdcc756010a25d23a8cd790151a5
OSWorX <info@osworx.net>
2025-03-16 14:23:38 +0100

    New translations recurring.php (Azerbaijani)

--- upload/admin/language/en-gb/catalog/az-AZ.php
+++ upload/admin/language/en-gb/catalog/az-AZ.php
@@ -1,108 +1,43 @@
 <?php
 // Heading
-$_['heading_title']          = 'Məhsullar';
+$_['heading_title']			= 'Yenilənən Profiller';
 
 // Text
-$_['text_success']           = 'Müvəffəqiyyət: Siz istifadəçiləri dəyişdirmişsiniz!';
-$_['text_list']              = 'Məhsul siyahısı';
-$_['text_add']               = 'Məhsul əlavə et';
-$_['text_edit']              = 'Məhsul redaktə et';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'Varsayılan';
-$_['text_option']            = 'Seçimlər';
-$_['text_option_value']      = 'Dəyər adı';
-$_['text_percent']           = 'Faiz';
-$_['text_amount']            = 'Müəyyən miqdar';
-
-// Column
-$_['column_name']            = 'Məhsulun adı';
-$_['column_model']           = 'Model';
-$_['column_image']           = 'Şəkillər';
-$_['column_price']           = 'Qiymət';
-$_['column_quantity']        = 'Miqdar';
-$_['column_status']          = 'Status';
-$_['column_action']          = 'Hərəkət';
+$_['text_success']          = 'Uğurlu: cateqoriya redaktə edildi!';
+$_['text_list']             = 'Yenilənən Profiller';
+$_['text_add']              = 'Profil əlavə et';
+$_['text_edit']             = 'Profil redaktə et';
+$_['text_day']				= 'Gün';
+$_['text_week']				= 'Həftə';
+$_['text_semi_month']		= 'Ayda iki dəfə olan';
+$_['text_month']			= 'Ay';
+$_['text_year']				= 'İl';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Yenilənən miqdarlar qarşılaşma sıxlığı ve dönüşüm miqdarına görə hesablanır. </p><p>Məsələn,  Qarşılaşma Sıxlığı olaraq "Həftə" və Dönüşüm Miqdarı olaraq "2" dəyərlərini istifadə etsəniz  istifadəçi fakturası 2 həftədə bir dəfə düzənlənir. </p><p>Zaan istifadəçinin ödəmə edəcəyi zaman vahidi miqdarı qədərdir, ödəmələri ləğv ediləcəyi zamana qədər gözlətmək istəsəniz bu dəyəri "0" olaraq təyin edin </p>';
+$_['text_profile']			= 'Yenilənən Profiller';
+$_['text_trial']			= 'Demo profil';
 
 // Entry
-$_['entry_name']             = 'Məhsulun adı';
-$_['entry_description']      = 'Açıqlama';
-$_['entry_meta_title'] 	     = 'Meta başlığı';
-$_['entry_meta_keyword'] 	 = 'Meta açar sözləri';
-$_['entry_meta_description'] = 'Meta İzahat';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Model';
-$_['entry_sku']              = 'SKU nömrəsi';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'Bölgə';
-$_['entry_shipping']         = 'Kargo';
-$_['entry_manufacturer']     = 'İstehsalçı';
-$_['entry_store']            = 'Dükanlar';
-$_['entry_date_available']   = 'İstehsal tarixi';
-$_['entry_quantity']         = 'Miqdar';
-$_['entry_minimum']          = 'Ən az miqdar';
-$_['entry_stock_status']     = 'Satışda yoxdur.';
-$_['entry_price']            = 'Qiymət';
-$_['entry_tax_class']        = 'Vergi sinfi';
-$_['entry_points']           = 'Xallar';
-$_['entry_option_points']    = 'Xallar';
-$_['entry_subtract']         = 'Satışdan çıxart';
-$_['entry_weight_class']     = 'Çəkilər';
-$_['entry_weight']           = 'Çəki';
-$_['entry_dimension']        = 'Ölçülər (uzunluq x en x hündürlük)';
-$_['entry_length_class']     = 'Uzunluq Sinifləri';
-$_['entry_length']           = 'Uzunluğ';
-$_['entry_width']            = 'Enlik';
-$_['entry_height']           = 'Hündürlük';
-$_['entry_image']            = 'Şəkillər';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Müştəri Qrupu';
-$_['entry_date_start']       = 'Başlanğıc tarixi';
-$_['entry_date_end']         = 'Bitiş tarixi';
-$_['entry_priority']         = 'Birincilik';
-$_['entry_attribute']        = 'Əlamət';
-$_['entry_attribute_group']  = 'Xüsusiyyət qrupu';
-$_['entry_text']             = 'Mətn';
-$_['entry_option']           = 'Seçimlər';
-$_['entry_option_value']     = 'Dəyər adı';
-$_['entry_required']         = 'Mütləq';
-$_['entry_status']           = 'Status';
-$_['entry_sort_order']       = 'Növlərə görə sırala';
-$_['entry_category']         = 'Kateqoriyalar';
-$_['entry_filter']           = 'Filterlər';
-$_['entry_download']         = 'Yükləmələr';
-$_['entry_related']          = 'Oxşar məhsullar';
-$_['entry_tag']          	 = 'Məhsulun adı';
-$_['entry_reward']           = 'Mükafat Xalları';
-$_['entry_layout']           = 'Düzeni Geçersiz Kılma';
-$_['entry_recurring']        = 'Yenilənən Profiller';
+$_['entry_name']		    = 'Ad';
+$_['entry_price']			= 'Qiymət';
+$_['entry_duration']		= 'Zaman';
+$_['entry_cycle']			= 'Döngü';
+$_['entry_frequency']		= 'Tezlik';
+$_['entry_trial_price']		= 'Sınaq qiymət';
+$_['entry_trial_duration']	= 'Sınaq üçün zaman';
+$_['entry_trial_status']	= 'Sınaq vəziyyəti';
+$_['entry_trial_cycle']	    = 'Sınaq döngüsü';
+$_['entry_trial_frequency'] = 'Sınaq sıxlıqı';
+$_['entry_status']			= 'Status';
+$_['entry_sort_order']		= 'Növlərə görə sırala';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'Stock Keeping Unit';
-$_['help_upc']               = 'Universal Product Code';
-$_['help_ean']               = 'European Article Number';
-$_['help_jan']               = 'Japanese Article Number';
-$_['help_isbn']              = 'International Standard Book Number';
-$_['help_mpn']               = 'Manufacturer Part Number';
-$_['help_manufacturer']      = 'Avtomatik tamamlama';
-$_['help_minimum']           = 'Force a minimum ordered amount';
-$_['help_stock_status']      = 'Status shown when a product is out of stock';
-$_['help_points']            = 'Number of points needed to buy this item. If you don\'t want this product to be purchased with points leave as 0.';
-$_['help_category']          = 'Avtomatik tamamlama';
-$_['help_filter']            = 'Avtomatik tamamlama';
-$_['help_download']          = 'Avtomatik tamamlama';
-$_['help_related']           = 'Avtomatik tamamlama';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'Ad';
+$_['column_sort_order']	    = 'Növlərə görə sırala';
+$_['column_action']         = 'Hərəkət';
 
 // Error
-$_['error_warning']          = 'Xəbərdarlıq: Xahiş edirik, formu göstərilən səhvlər səbəbiylə diqqətlə nəzarət edin!';
-$_['error_permission']       = 'Warning: You do not have permission to modify products!';
-$_['error_name']             = 'Məhsul adı 3-dən 255-dək simvol aralığında olmalıdır!';
-$_['error_meta_title']       = 'Meta başlıq ən az 3 və ən çox 255 simvol olmalıdır!';
-$_['error_model']            = 'Məhsul model adı 1-dən 64-dək simvol aralığında olmalıdır!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Xəbərdarlıq: Xahiş edirik, formu göstərilən səhvlər səbəbiylə diqqətlə nəzarət edin!';
+$_['error_permission']		= 'Xəbərdarlıq: Kateqoriyaları dəyişdirmə icazəsinə sahib deyilsiniz!';
+$_['error_name']			= 'Məhsul adı 3-dən 255-dək simvol aralığında olmalıdır!';
+$_['error_product']			= 'Xəbərdar etmək: Bu əlamət silinə bilməz, necə ki bu hal-hazırda %s məhsullarına təyin edilir!
+';
