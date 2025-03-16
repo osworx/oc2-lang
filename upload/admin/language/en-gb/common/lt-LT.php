9afd4a1aeca92a2121eefcf936c93a54436113b9
OSWorX <info@osworx.net>
2025-03-16 14:27:48 +0100

    New translations footer.php (Lithuanian)

--- upload/admin/language/en-gb/common/lt-LT.php
+++ upload/admin/language/en-gb/common/lt-LT.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Paveiksliukų tvarkytojas';
-
 // Text
-$_['text_uploaded']    = 'Atlikta: failas sėkmingai įkeltas!';
-$_['text_directory']   = 'Atlikta: aplankas sėkmingai sukurtas!';
-$_['text_delete']      = 'Atlikta: failas ar aplankas buvo ištrinta!';
-
-// Entry
-$_['entry_search']     = 'Paieška..';
-$_['entry_folder']     = 'Aplanko pavadinimas';
-
-// Error
-$_['error_permission'] = 'Įspėjimas: neturite leidimo!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Įspėjimas: failas ar aplankas tokiu pavadinimu jau yra!';
-$_['error_directory']  = 'Įspėjimas: aplankas neegzistuoja!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Įspėjimas: neteisingas failo tipas!';
-$_['error_upload']     = 'Įspėjimas: failas negali būti įkeltas dėl nežinomos priežasties!';
-$_['error_delete']     = 'Įspėjimas: Jūs negalite ištrinti šio aplanko!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Versija %s';
