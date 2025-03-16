34f07dc6e81a8b758cfece1b28c495e6030b286d
OSWorX <info@osworx.net>
2025-03-16 09:10:57 +0100

    New translations information.php (Korean)

--- upload/language/ko-KR/ko-KR.php
+++ upload/language/ko-KR/ko-KR.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = '필터';
+$_['heading_title']          = '정보 페이지';
 
 // Text
-$_['text_success']      = '완료: 필터를 수정했습니다.';
-$_['text_list']         = '필터 목록';
-$_['text_add']          = '필터 추가';
-$_['text_edit']         = '필터 수정';
+$_['text_success']           = '완료: 정보 페이지를 수정했습니다.';
+$_['text_list']              = '정보 페이지 목록';
+$_['text_add']               = '정보 페이지 추가';
+$_['text_edit']              = '정보 페이지 편집';
+$_['text_default']           = '기본값';
 
 // Column
-$_['column_group']      = '필터그룹';
-$_['column_sort_order'] = '정렬순서';
-$_['column_action']     = '작업';
+$_['column_title']           = '정보 페이지 제목';
+$_['column_sort_order']	     = '정렬순서';
+$_['column_action']          = '작업';
 
 // Entry
-$_['entry_group']       = '필터그룹 이름';
-$_['entry_name']        = '필터 이름';
-$_['entry_sort_order']  = '정렬순서';
+$_['entry_title']            = '정보 페이지 제목';
+$_['entry_description']      = '상세설명';
+$_['entry_store']            = '쇼핑몰';
+$_['entry_meta_title'] 	     = '메타 태그 제목';
+$_['entry_meta_keyword'] 	 = '메타 태그 키워드';
+$_['entry_meta_description'] = '메타 태그 설명';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = '하단에 표시';
+$_['entry_status']           = '상태';
+$_['entry_sort_order']       = '정렬순서';
+$_['entry_layout']           = '레이아웃 재정의';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = '하단 영역에 표시합니다.';
 
 // Error
-$_['error_warning']     = '주의: 오류가 발생했습니다. 입력 항목을 다시 확인해주세요.';
-$_['error_permission']  = '주의: 필터를 수정할 수 있는 권한이 없습니다.';
-$_['error_group']       = '필터그룹 이름은 1~64 글자 이내로 입력해주세요.';
-$_['error_name']        = '필터 이름은 1~64 글자 이내로 입력해주세요.';
+$_['error_warning']          = '주의: 오류가 발생했습니다. 입력 항목을 다시 확인해주세요.';
+$_['error_permission']       = '주의: 정보 페이지를 수정할 수 있는 권한이 없습니다.';
+$_['error_title']            = '정보 페이지 제목은 3~64 글자 이내로 입력해주세요.';
+$_['error_description']      = '세부내용은 3 글자 이상 입력해주세요.';
+$_['error_meta_title']       = '메타 태그 제목은 3~255 글자 이내로 입력해주세요.';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = '주의: 이 정보 페이지는 현재 쇼핑몰 회원약관으로 설정되어 있으므로 삭제할 수 없습니다.';
+$_['error_checkout']         = '주의: 이 정보 페이지는 현재 쇼핑몰 주문약관으로 설정되어 있으므로 삭제할 수 없습니다.';
+$_['error_affiliate']        = '주의: 이 정보 페이지는 현재 쇼핑몰 제휴약관으로 설정되어 있으므로 삭제할 수 없습니다.';
+$_['error_return']           = '주의: 이 정보 페이지는 현재 쇼핑몰 반품약관으로 설정되어 있으므로 삭제할 수 없습니다.';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
