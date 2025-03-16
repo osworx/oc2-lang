ca386c955f5192ca9d75733a2d1d37be2bed8a96
OSWorX <info@osworx.net>
2025-03-16 09:09:00 +0100

    New translations information.php (Finnish)

--- upload/language/fi-FI/fi-FI.php
+++ upload/language/fi-FI/fi-FI.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'Suodattimet';
+$_['heading_title']          = 'Tiedotteet';
 
 // Text
-$_['text_success']      = 'Suodattimien muokkaus onnistui!';
-$_['text_list']         = 'Suodattimet';
-$_['text_add']          = 'Lisää suodatin';
-$_['text_edit']         = 'Muokkaa suodatinta';
+$_['text_success']           = 'Olet muokannut tietoa onnistuneesti!';
+$_['text_list']              = 'Tiedotteet';
+$_['text_add']               = 'Lisää tiedote';
+$_['text_edit']              = 'Muokkaa tiedotetta';
+$_['text_default']           = 'Oletus';
 
 // Column
-$_['column_group']      = 'Suodatin ryhmä';
-$_['column_sort_order'] = 'Lajittelujärjestys';
-$_['column_action']     = 'Toiminto';
+$_['column_title']           = 'Tiedotteen otsikko';
+$_['column_sort_order']	     = 'Lajittelujärjestys';
+$_['column_action']          = 'Toiminto';
 
 // Entry
-$_['entry_group']       = 'Suodatinryhmän nimi';
-$_['entry_name']        = 'Suodattimen nimi';
-$_['entry_sort_order']  = 'Lajittelujärjestys';
+$_['entry_title']            = 'Tiedotteen otsikko';
+$_['entry_description']      = 'Kuvaus';
+$_['entry_store']            = 'Kaupat';
+$_['entry_meta_title'] 	     = 'Metatagien otsikko';
+$_['entry_meta_keyword'] 	 = 'Metatagien avainsanat';
+$_['entry_meta_description'] = 'Metatagin kuvaus';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Alaosa';
+$_['entry_status']           = 'Tila';
+$_['entry_sort_order']       = 'Lajittelujärjestys';
+$_['entry_layout']           = 'Ohita asettelu';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Näytä alatunnisteessa.';
 
 // Error
-$_['error_warning']     = 'Varoitus: tarkista lomake virheiden varalta!';
-$_['error_permission']  = 'Varoitus: Sinulla ei ole oikeuksia muokata suodattimia!';
-$_['error_group']       = 'Suodatinryhmän nimen täytyy olla 1-64 merkkiä pitkä!';
-$_['error_name']        = 'Suodattimen nimen täytyy olla 1-64 merkkiä pitkä!';
+$_['error_warning']          = 'Varoitus: tarkista lomake virheiden varalta!';
+$_['error_permission']       = 'Varoitus: sinulla ei ole oikeuksia muokata informaatio-sivuja!';
+$_['error_title']            = 'Tiedotteen otsikon tulee olla 3 - 64 merkkiä pitkä!';
+$_['error_description']      = 'Kuvauksen tulee olla vähintään 3 merkkiä pitkä!';
+$_['error_meta_title']       = 'Sivun otsikon  tulee olla 3 - 255 merkkiä pitkä!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'Varoitus: tätä tietosivua ei voi poistaa, koska se on määritetty verkkokaupan rekisteriselosteeksi!';
+$_['error_checkout']         = 'Varoitus: tätä tietosivua ei voi poistaa, koska se on määritetty verkkokaupan toimitusehdoiksi!';
+$_['error_affiliate']        = 'Varoitus: tätä tietosivua ei voi poistaa, koska se on määritetty verkkokaupan kumppanuusehdoiksi!';
+$_['error_return']           = 'Tiedotesivua ei voi poistaa, koska se on määritelty palautusehdoiksi.';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
