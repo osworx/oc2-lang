9b1168fc49c3ba988ed77e44bae6d8154e74df6c
OSWorX <info@osworx.net>
2025-03-16 14:27:11 +0100

    New translations filemanager.php (Chinese Simplified)

--- upload/admin/language/en-gb/common/zh-CN.php
+++ upload/admin/language/en-gb/common/zh-CN.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = '管理面板';
+$_['heading_title']    = '图片管理';
+
+// Text
+$_['text_uploaded']    = '上传成功: 您的文件已经上传!';
+$_['text_directory']   = '创建成功: 新目录已经创建!';
+$_['text_delete']      = '删除成功: 您的文件或目录已经删除!';
+
+// Entry
+$_['entry_search']     = '搜索';
+$_['entry_folder']     = '文件夹名称';
 
 // Error
-$_['error_install'] = '警告：出于安全考虑请立即删除安装(install)文件夹！';
+$_['error_permission'] = '警告: 权限不足!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = '警告: 已存在同名的文件或目录!';
+$_['error_directory']  = '警告: 目录不存在!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = '警告: 文件类型不正确!';
+$_['error_upload']     = '警告: 文件不能上传, 原因未知!';
+$_['error_delete']     = '警告: 不能删除目录!';
