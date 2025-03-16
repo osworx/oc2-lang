d47648ab5b4cb29a301915cc627b04d5ec50516f
OSWorX <info@osworx.net>
2025-03-16 11:05:20 +0100

    New translations attribute.php (Vietnamese)

---
+++ upload/admin/language/en-gb/catalog/vi-VN.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Các thuộc tính';
+
+// Text
+$_['text_success']           = 'Thành công: Bạn đã sửa các thuộc tính!';
+$_['text_list']              = 'Danh sách thuộc tính';
+$_['text_add']               = 'Thêm thuộc tính';
+$_['text_edit']              = 'Sửa thuộc tính';
+
+// Column
+$_['column_name']            = 'Tên thuộc tính';
+$_['column_attribute_group'] = 'Nhóm thuộc tính';
+$_['column_sort_order']      = 'Thứ tự sắp xếp';
+$_['column_action']          = 'Tác vụ';
+
+// Entry
+$_['entry_name']             = 'Tên thuộc tính';
+$_['entry_attribute_group']  = 'Nhóm thuộc tính';
+$_['entry_sort_order']       = 'Thứ tự sắp xếp';
+
+// Error
+$_['error_warning']          = 'Chú ý: Vui lòng kiểm tra lỗi cẩn thận trong biểu mẫu!';
+$_['error_permission']       = 'Chú ý: Bạn không có quyền sửa đổi các thuộc tính!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Tên thuộc tính phải từ 3 đến 64 ký tự!';
+$_['error_product']          = 'Cảnh báo: Thuộc tính này không thể bị xoá vì nó hiện đang được gán cho %s sản phẩm!';
