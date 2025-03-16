aa49f7af82d0b6f3e7d51dec164cd053567cde66
OSWorX <info@osworx.net>
2025-03-16 14:23:14 +0100

    New translations recurring.php (Korean)

--- upload/admin/language/en-gb/catalog/ko-KR.php
+++ upload/admin/language/en-gb/catalog/ko-KR.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = '상품';
+$_['heading_title']			= '할부방식';
 
 // Text
-$_['text_success']           = '완료: 상품을 수정했습니다.';
-$_['text_list']              = '상품 목록';
-$_['text_add']               = '상품 추가';
-$_['text_edit']              = '상품 수정';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = '기본값';
-$_['text_option']            = '옵션';
-$_['text_option_value']      = '옵션값';
-$_['text_percent']           = '비율';
-$_['text_amount']            = '고정금액';
-
-// Column
-$_['column_name']            = '상품명';
-$_['column_model']           = '모델명';
-$_['column_image']           = '이미지';
-$_['column_price']           = '판매가';
-$_['column_quantity']        = '수량';
-$_['column_status']          = '상태';
-$_['column_action']          = '작업';
+$_['text_success']          = '완료: 할부방식을 수정했습니다.';
+$_['text_list']             = '할부방식 목록';
+$_['text_add']              = '할부방식 추가';
+$_['text_edit']             = '할부방식 수정';
+$_['text_day']				= '일';
+$_['text_week']				= '주';
+$_['text_semi_month']		= '보름';
+$_['text_month']			= '월';
+$_['text_year']				= '년';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> 할부 금액은 주기와 주기단위를 이용해서 계산합니다.</p><p>예를 들어 주기가 "2"이고 주기단위가 "주"인 경우, 고객에게 2주마다 한 번씩 청구합니다.</p><p>할부횟수는 고객이 결제해야 하는 총 횟수입니다. 만약 취소할 때까지 계속 반복 결제되도록 하려면 할부횟수를 0으로 설정합니다.</p>';
+$_['text_profile']			= '할부방식';
+$_['text_trial']			= '상품체험';
 
 // Entry
-$_['entry_name']             = '상품명';
-$_['entry_description']      = '상세설명';
-$_['entry_meta_title'] 	     = '메타 태그 제목';
-$_['entry_meta_keyword'] 	 = '메타 태그 키워드';
-$_['entry_meta_description'] = '메타 태그 설명';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = '모델명';
-$_['entry_sku']              = 'SKU';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = '위치';
-$_['entry_shipping']         = '배송 필수';
-$_['entry_manufacturer']     = '제조사';
-$_['entry_store']            = '쇼핑몰';
-$_['entry_date_available']   = '진열 시작일';
-$_['entry_quantity']         = '수량';
-$_['entry_minimum']          = '최소 주문수량';
-$_['entry_stock_status']     = '품절상태 메시지';
-$_['entry_price']            = '판매가';
-$_['entry_tax_class']        = '세금유형';
-$_['entry_points']           = '포인트';
-$_['entry_option_points']    = '포인트';
-$_['entry_subtract']         = '재고 차감';
-$_['entry_weight_class']     = '무게단위';
-$_['entry_weight']           = '무게';
-$_['entry_dimension']        = '크기(가로 x 세로 x 높이)';
-$_['entry_length_class']     = '길이단위';
-$_['entry_length']           = '가로';
-$_['entry_width']            = '세로';
-$_['entry_height']           = '높이';
-$_['entry_image']            = '이미지';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = '고객그룹';
-$_['entry_date_start']       = '시작일';
-$_['entry_date_end']         = '종료일';
-$_['entry_priority']         = '우선순위';
-$_['entry_attribute']        = '속성';
-$_['entry_attribute_group']  = '속성그룹';
-$_['entry_text']             = 'Text';
-$_['entry_option']           = '옵션';
-$_['entry_option_value']     = '옵션값';
-$_['entry_required']         = '필수';
-$_['entry_status']           = '상태';
-$_['entry_sort_order']       = '정렬순서';
-$_['entry_category']         = '상품분류';
-$_['entry_filter']           = '필터';
-$_['entry_download']         = '다운로드';
-$_['entry_related']          = '관련 상품';
-$_['entry_tag']          	 = '상품 태그';
-$_['entry_reward']           = '적립포인트';
-$_['entry_layout']           = '레이아웃 재정의';
-$_['entry_recurring']        = '할부방식';
+$_['entry_name']		    = '이름';
+$_['entry_price']			= '판매가';
+$_['entry_duration']		= '할부횟수';
+$_['entry_cycle']			= '주기';
+$_['entry_frequency']		= '주기단위';
+$_['entry_trial_price']		= '상품체험 회당 결제금액';
+$_['entry_trial_duration']	= '상품체험 횟수';
+$_['entry_trial_status']	= '상품체험 상태';
+$_['entry_trial_cycle']	    = '상품체험 주기';
+$_['entry_trial_frequency'] = '상품체험 주기단위';
+$_['entry_status']			= '상태';
+$_['entry_sort_order']		= '정렬순서';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = '재고유지단위(SKU)';
-$_['help_upc']               = '범용상품코드(UPC)';
-$_['help_ean']               = '유럽상품번호(EAN)';
-$_['help_jan']               = '일본상품번호(JAN)';
-$_['help_isbn']              = '국제표준도서번호(ISBN)';
-$_['help_mpn']               = '제조사부품번호(MPN)';
-$_['help_manufacturer']      = '(자동완성)';
-$_['help_minimum']           = '설정한 수량 이상으로만 주문할 수 있음';
-$_['help_stock_status']      = '상품이 품절일 경우 표시할 상태';
-$_['help_points']            = '이 항목을 구입 하는 데 필요한 적립포인트입니다. 적립포인트로 구매할 수 없도록 하시려면 0으로 설정하세요.';
-$_['help_category']          = '(자동완성)';
-$_['help_filter']            = '(자동완성)';
-$_['help_download']          = '(자동완성)';
-$_['help_related']           = '(자동완성)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= '이름';
+$_['column_sort_order']	    = '정렬순서';
+$_['column_action']         = '작업';
 
 // Error
-$_['error_warning']          = '주의: 오류가 발생했습니다. 입력 항목을 다시 확인해주세요.';
-$_['error_permission']       = '주의: 상품을 수정할 수 있는 권한이 없습니다.';
-$_['error_name']             = '상품명은 3~255 글자 이내로 입력해주세요.';
-$_['error_meta_title']       = '메타 태그 제목은 3~255 글자 이내로 입력해주세요.';
-$_['error_model']            = '상품 모델명은 1~64 글자 이내로 입력해주세요.';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = '주의: 오류가 발생했습니다. 입력 항목을 다시 확인해주세요.';
+$_['error_permission']		= '주의: 할부방식을 수정할 수 있는 권한이 없습니다.';
+$_['error_name']			= '할부방식 이름은 3~255 글자 이내로 입력해주세요.';
+$_['error_product']			= '주의: 현재 %s개의 상품에 이 할부방식이 설정되어 있으므로 삭제할 수 없습니다.';
