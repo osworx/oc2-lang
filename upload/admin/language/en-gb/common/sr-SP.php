7fc1e5359cbc960a91ad32c890989a9c97ec002a
OSWorX <info@osworx.net>
2025-03-16 11:17:47 +0100

    New translations footer.php (Serbian (Cyrillic))

--- upload/admin/language/en-gb/common/sr-SP.php
+++ upload/admin/language/en-gb/common/sr-SP.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Управљање фотографијама';
-
 // Text
-$_['text_uploaded']    = 'Успешно: ваша датотека је отпремљена!';
-$_['text_directory']   = 'Успешно: директоријум је креиран!';
-$_['text_delete']      = 'Успешно: ваша датотека или директоријум је обрисан!';
-
-// Entry
-$_['entry_search']     = 'Претрага..';
-$_['entry_folder']     = 'Назив директоријума';
-
-// Error
-$_['error_permission'] = 'Упозорење: није дозвољено!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Упозорење: датотека или директоријум са истим именом већ постоји!';
-$_['error_directory']  = 'Упозорење: не постоји директоријум!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Упозорење: нетачан тип датотеке!';
-$_['error_upload']     = 'Упозорење: датотека се не може отпремити из непознатог разлога!';
-$_['error_delete']     = 'Упозорење: не можете обрисати овај директоријум!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Верзија %s';
