5ee907655e65175f69635e290a6450c1cd645752
OSWorX <info@osworx.net>
2025-03-16 11:06:09 +0100

    New translations attribute_group.php (Vietnamese)

--- upload/admin/language/en-gb/catalog/vi-VN.php
+++ upload/admin/language/en-gb/catalog/vi-VN.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Các thuộc tính';
+$_['heading_title']     = 'Các nhóm thuộc tính';
 
 // Text
-$_['text_success']           = 'Thành công: Bạn đã sửa các thuộc tính!';
-$_['text_list']              = 'Danh sách thuộc tính';
-$_['text_add']               = 'Thêm thuộc tính';
-$_['text_edit']              = 'Sửa thuộc tính';
+$_['text_success']      = 'Kết quả: Bạn đã sửa xong các nhóm thuộc tính!';
+$_['text_list']         = 'Danh sách nhóm thuộc tính';
+$_['text_add']          = 'Thêm 1 nhóm thuộc tính';
+$_['text_edit']         = 'Chỉnh sửa 1 một thuộc tính';
 
 // Column
-$_['column_name']            = 'Tên thuộc tính';
-$_['column_attribute_group'] = 'Nhóm thuộc tính';
-$_['column_sort_order']      = 'Thứ tự sắp xếp';
-$_['column_action']          = 'Tác vụ';
+$_['column_name']       = 'Tên 1 nhóm thuộc tính';
+$_['column_sort_order'] = 'Thứ tự sắp xếp';
+$_['column_action']     = 'Tác vụ';
 
 // Entry
-$_['entry_name']             = 'Tên thuộc tính';
-$_['entry_attribute_group']  = 'Nhóm thuộc tính';
-$_['entry_sort_order']       = 'Thứ tự sắp xếp';
+$_['entry_name']        = 'Tên 1 nhóm thuộc tính';
+$_['entry_sort_order']  = 'Thứ tự sắp xếp';
 
 // Error
-$_['error_warning']          = 'Chú ý: Vui lòng kiểm tra lỗi cẩn thận trong biểu mẫu!';
-$_['error_permission']       = 'Chú ý: Bạn không có quyền sửa đổi các thuộc tính!';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'Tên thuộc tính phải từ 3 đến 64 ký tự!';
-$_['error_product']          = 'Cảnh báo: Thuộc tính này không thể bị xoá vì nó hiện đang được gán cho %s sản phẩm!';
+$_['error_warning']     = 'Chú ý: Vui lòng kiểm tra lỗi cẩn thận trong biểu mẫu!';
+$_['error_permission']  = 'Chú ý: Bạn không có quyền sửa các nhóm thuộc tính!';
+$_['error_name']        = 'Tên của nhóm thuộc tính phải từ 3 tới 64 ký tự!';
+$_['error_attribute']   = 'Chú ý: Nhóm thuộc tính này không thể xóa khi nó đang được gán cho các thuộc tính %s !';
+$_['error_product']     = 'Chú ý: Nhóm thuộc tính này không thể xóa vì nó đang được gán cho các sản phẩm %s !';
