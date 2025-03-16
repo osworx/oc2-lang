a34b16c5459c30bd0a4d0ac97925d279c37bb976
OSWorX <info@osworx.net>
2025-03-16 11:10:14 +0100

    New translations manufacturer.php (Czech)

--- upload/admin/language/en-gb/catalog/cs-CZ.php
+++ upload/admin/language/en-gb/catalog/cs-CZ.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Informace';
+$_['heading_title']     = 'Výrobci';
 
 // Text
-$_['text_success']           = 'Úspěch: Upravili jste informace!';
-$_['text_list']              = 'Seznam informací';
-$_['text_add']               = 'Přidání Informaci';
-$_['text_edit']              = 'Upravit Informaci';
-$_['text_default']           = 'Výchozí';
+$_['text_success']      = 'Úspěch: Upravili jste Výrobce!';
+$_['text_list']         = 'Seznam Výrobců';
+$_['text_add']          = 'Přidat výrobce';
+$_['text_edit']         = 'Upravit výrobce';
+$_['text_default']      = 'Výchozí';
+$_['text_percent']      = 'Procenta';
+$_['text_amount']       = 'Pevná částka';
 
 // Column
-$_['column_title']           = 'Název Informace';
-$_['column_sort_order']	     = 'Řadit podle';
-$_['column_action']          = 'Akce';
+$_['column_name']       = 'Název Výrobce';
+$_['column_sort_order'] = 'Řadit podle';
+$_['column_action']     = 'Akce';
 
 // Entry
-$_['entry_title']            = 'Název Informace';
-$_['entry_description']      = 'Popis';
-$_['entry_store']            = 'Obchody';
-$_['entry_meta_title'] 	     = 'Meta Tag Title';
-$_['entry_meta_keyword'] 	 = 'Klíčová slova Meta Tag';
-$_['entry_meta_description'] = 'Meta Tag Description';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'Dole';
-$_['entry_status']           = 'Stav';
-$_['entry_sort_order']       = 'Řadit podle';
-$_['entry_layout']           = 'Přepsání layoutu';
+$_['entry_name']        = 'Název Výrobce';
+$_['entry_store']       = 'Obchody';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'Obrázek';
+$_['entry_sort_order']  = 'Řadit podle';
+$_['entry_type']        = 'Typ';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'Zobrazit v zápatí zdola.';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'Upozornění: Zkontrolujte formulář pečlivě pro chyby!';
-$_['error_permission']       = 'Upozornění: Nemáte oprávnění k úpravě informací!';
-$_['error_title']            = 'Název Informace musí být v rozmezí 3 až 64 znaků!';
-$_['error_description']      = 'Popis musí být delší než 3 znaky!';
-$_['error_meta_title']       = 'Meta Title musí být větší než 3 a méně než 255 znaků!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'Varování: Tato informační stránka nemůže být odstraněna, protože je v současnosti přiřazena k obchodním podmínkám účtu!';
-$_['error_checkout']         = 'Varování: Tato informační stránka nemůže být odstraněna, protože je v současnosti přiřazena jako objednací podmínky obchodu!';
-$_['error_affiliate']        = 'Varování: Tato informační stránka nemůže být odstraněn, protože je v současnosti přiřazena jako Affiliate podmínky obchodu!';
-$_['error_return']           = 'Varování: Tato informační stránka nemůže být odstraněna, protože je v současnosti přiřazena k vratkovým podmínkám obchodu!';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'Upozornění: Zkontrolujte formulář pečlivě pro chyby!';
+$_['error_permission']  = 'Upozornění: Nemáte oprávnění ke změně Výrobců!';
+$_['error_name']        = 'Název Výrobce musí být v rozmezí 2 až 64 znaků!';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'Varování: Tento výrobce nemůže být odstraněn, protože je v současnosti přiřazen k %s produktů!';
