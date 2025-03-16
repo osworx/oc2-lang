28c6027e250217ad62b0f28735007ddbe3640dca
OSWorX <info@osworx.net>
2025-03-16 11:07:49 +0100

    New translations download.php (Slovenian)

--- upload/admin/language/en-gb/catalog/sl-SI.php
+++ upload/admin/language/en-gb/catalog/sl-SI.php
@@ -1,46 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Kategorije';
+$_['heading_title']     = 'Prenosi';
 
 // Text
-$_['text_success']           = 'USPEŠNO: Ste uredili kategorije!';
-$_['text_list']              = 'Seznam Kategorij';
-$_['text_add']               = 'Dodaj Kategorijo';
-$_['text_edit']              = 'Uredi Kategorijo';
-$_['text_default']           = 'Privzeto';
+$_['text_success']      = 'USPEŠNO: Ste uredili prenose!';
+$_['text_list']         = 'Seznam prenosov';
+$_['text_add']          = 'Dodaj prenos';
+$_['text_edit']         = 'Uredi Prenos';
+$_['text_upload']       = 'Vaša datoteka je bila uspešno prenesena!';
 
 // Column
-$_['column_name']            = 'Ime Kategorije';
-$_['column_sort_order']      = 'Vrstni red';
-$_['column_action']          = 'Dejanje';
+$_['column_name']       = 'Ime prenosa';
+$_['column_date_added'] = 'Dodano dne';
+$_['column_action']     = 'Dejanje';
 
 // Entry
-$_['entry_name']             = 'Ime Kategorije';
-$_['entry_description']      = 'Opis';
-$_['entry_meta_title'] 	     = 'Meta naslov';
-$_['entry_meta_keyword']     = 'Meta ključne besede';
-$_['entry_meta_description'] = 'Meta opis';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = 'Nadrejena kategorija';
-$_['entry_filter']           = 'Filtri';
-$_['entry_store']            = 'Trgovina';
-$_['entry_image']            = 'Slika';
-$_['entry_top']              = 'Zgoraj';
-$_['entry_column']           = 'Stolpci';
-$_['entry_sort_order']       = 'Vrstni red';
-$_['entry_status']           = 'Status';
-$_['entry_layout']           = 'Postavitev';
+$_['entry_name']        = 'Ime prenosa';
+$_['entry_filename']    = 'Ime datoteke';
+$_['entry_mask']        = 'Maska';
 
 // Help
-$_['help_filter']            = '(Samodokončanje)';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = 'Prikaz v zgornjem meniju. Deluje samo z nadrejenimi kategorijami.';
-$_['help_column']            = 'Število stolpcev za spodnje 3 kategorije. Deluje samo z nadrejenimi kategorijami.';
+$_['help_filename']     = 'Lahko prenesete preko gumb upload ali uporaba FTP upload v imenik za prenos in vnesite podatke spodaj.';
+$_['help_mask']         = 'Priporočljivo je, da sta ime datoteke in maska različni kar bo ustavilo ljudi pri poskusu neposredne povezave na vaše prenose.';
 
 // Error
-$_['error_warning']          = 'POZOR: Prosimo, pazljivo preverite polja za morebitne napake!';
-$_['error_permission']       = 'POZOR: Nimate dovoljenja za urejanje kategorij!';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = 'Meta Naslov mora imeti več kot 3 in manj kot 255 znakov!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = 'POZOR: Prosimo, pazljivo preverite polja za morebitne napake!';
+$_['error_permission']  = 'POZOR: Nimate dovoljenja za urejanje prenosov!';
+$_['error_name']        = 'Ime mora imeti več kot 3 in manj kot 64 znakov!';
+$_['error_upload']      = 'Nalaganje je zahtevano!';
+$_['error_filename']    = 'Ime datoteke mora imeti več kot 3 in manj kot 128 znakov!';
+$_['error_exists']      = 'Datoteka ne obstaja!';
+$_['error_mask']        = 'Maska mora vsebovati med 3 in 128 znakov!';
+$_['error_filetype']    = 'Neveljaven tip datoteke!';
+$_['error_product']     = 'POZOR: Ta prenos ne more biti izbrisan, ker je vezan na %s izdelke!';
