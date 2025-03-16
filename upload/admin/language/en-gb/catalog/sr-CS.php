cd61259328cbf42a0d9d663a21dcd1ae9165e54d
OSWorX <info@osworx.net>
2025-03-16 11:05:36 +0100

    New translations attribute.php (Serbian (Latin))

---
+++ upload/admin/language/en-gb/catalog/sr-CS.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Atributi';
+
+// Text
+$_['text_success']           = 'Uspešno ste promenili atribute!';
+$_['text_list']              = 'Lista atributa';
+$_['text_add']               = 'Dodaj atribut';
+$_['text_edit']              = 'Izmeni atribut';
+
+// Column
+$_['column_name']            = 'Ime atributa';
+$_['column_attribute_group'] = 'Grupa atributa';
+$_['column_sort_order']      = 'Sortiraj po';
+$_['column_action']          = 'Akcija';
+
+// Entry
+$_['entry_name']             = 'Ime atributa';
+$_['entry_attribute_group']  = 'Grupa atributa';
+$_['entry_sort_order']       = 'Sortiraj po';
+
+// Error
+$_['error_warning']          = 'Upozorenje: Molimo proverite formu zbog grešaka!';
+$_['error_permission']       = 'Upozorenje: nemate ovlašćenje za izmenu atributa!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Ime atributa mora da ima između 3 i 64 karaktera!';
+$_['error_product']          = 'Upozorenje: Ovaj atribut ne može biti obrisan jer je trenutno povezan sa %s proizvoda!';
