9f5ba61228741fe111c71882e3a697a5b67ef31d
OSWorX <info@osworx.net>
2025-03-16 11:07:23 +0100

    New translations category.php (Rսssian, Սkraine)

--- upload/admin/language/en-gb/catalog/ru-UA.php
+++ upload/admin/language/en-gb/catalog/ru-UA.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Группы атрибутов';
+$_['heading_title']          = 'Категории';
 
 // Text
-$_['text_success']      = 'Успех! Группа атрибутов изменена!';
-$_['text_list']         = 'Список групп атрибутов';
-$_['text_add']          = 'Добавить группу атрибутов';
-$_['text_edit']         = 'Редактировать группу атрибутов';
+$_['text_success']           = 'Успех: Список категорий обновлен!';
+$_['text_list']              = 'Список категорий';
+$_['text_add']               = 'Добавить категорию';
+$_['text_edit']              = 'Редактировать категорию';
+$_['text_default']           = 'По умолчанию';
 
 // Column
-$_['column_name']       = 'Наименование группы атрибутов';
-$_['column_sort_order'] = 'Порядок сортировки';
-$_['column_action']     = 'Действие';
+$_['column_name']            = 'Название категории';
+$_['column_sort_order']      = 'Порядок сортировки';
+$_['column_action']          = 'Действие';
 
 // Entry
-$_['entry_name']        = 'Наименование группы атрибутов';
-$_['entry_sort_order']  = 'Порядок сортировки';
+$_['entry_name']             = 'Название категории';
+$_['entry_description']      = 'Описание';
+$_['entry_meta_title'] 	     = 'Мета тег Title';
+$_['entry_meta_keyword']     = 'Мета тег Ключевые слова';
+$_['entry_meta_description'] = 'Мета-тег описание';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Родительская';
+$_['entry_filter']           = 'Фильтры';
+$_['entry_store']            = 'Магазины';
+$_['entry_image']            = 'Изображение';
+$_['entry_top']              = 'Вверх';
+$_['entry_column']           = 'Столбцы';
+$_['entry_sort_order']       = 'Порядок сортировки';
+$_['entry_status']           = 'Статус';
+$_['entry_layout']           = 'Переопределение макета';
+
+// Help
+$_['help_filter']            = '(Автозаполнение)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Отображать в верхней строке меню. Работает только для верхних родительских категорий.';
+$_['help_column']            = 'Количество столбцов, используемых для нижних 3-х категорий. Работает только для верхних родительских категорий.';
 
 // Error
-$_['error_warning']     = 'Внимание: Проверьте форму на наличие ошибок!';
-$_['error_permission']  = 'Внимание: У вас нет прав на изменение групп атрибутов!';
-$_['error_name']        = 'Наименование группы атрибутов должно содержать от 3 до 64 символов!';
-$_['error_attribute']   = 'Внимание: эту группу атрибутов нельзя удалить, она связана с %s атрибутами!';
-$_['error_product']     = 'Внимание: эту группу атрибутов нельзя удалить, она связана с %s товарами!';
+$_['error_warning']          = 'Внимание: Проверьте форму на наличие ошибок!';
+$_['error_permission']       = 'Внимание: У вас нет прав на изменение категории!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Мета-тег описание должен быть больше чем 3 и менее 255 символов!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
