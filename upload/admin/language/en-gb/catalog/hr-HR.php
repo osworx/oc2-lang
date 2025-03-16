35db4d53aeae33ce89231823c4ccc4dc835bac97
OSWorX <info@osworx.net>
2025-03-16 14:17:12 +0100

    New translations attribute_group.php (Croatian)

--- upload/admin/language/en-gb/catalog/hr-HR.php
+++ upload/admin/language/en-gb/catalog/hr-HR.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Značajke';
+$_['heading_title']     = 'Grupe značajki';
 
 // Text
-$_['text_success']           = 'Uspjeh: Promijenili ste značajke!';
-$_['text_list']              = 'Popis značajki';
-$_['text_add']               = 'Dodaj značajku';
-$_['text_edit']              = 'Uredite značajku';
+$_['text_success']      = 'Uspjeh! Promijenili ste grupe značajki!';
+$_['text_list']         = 'Popis grupa značajki';
+$_['text_add']          = 'Dodaj grupu značajki';
+$_['text_edit']         = 'Uredi grupu značajki';
 
 // Column
-$_['column_name']            = 'Naziv značajke';
-$_['column_attribute_group'] = 'Grupa značajki';
-$_['column_sort_order']      = 'Redoslijed';
-$_['column_action']          = 'Radnja';
+$_['column_name']       = 'Naziv grupe značajki';
+$_['column_sort_order'] = 'Redoslijed';
+$_['column_action']     = 'Radnja';
 
 // Entry
-$_['entry_name']             = 'Naziv značajke';
-$_['entry_attribute_group']  = 'Grupa značajki';
-$_['entry_sort_order']       = 'Redoslijed';
+$_['entry_name']        = 'Naziv grupe značajki';
+$_['entry_sort_order']  = 'Redoslijed';
 
 // Error
-$_['error_warning']          = 'Upozorenje: Pažljivo provjerite ima li pogrešaka u obrascu!';
-$_['error_permission']       = 'Upozorenje: Niste ovlašteni za promjenu značajki!';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'Naziv značajke mora biti između 3 i 64 znaka!';
-$_['error_product']          = 'Upozorenje: Ovaj značajka ne može se izbrisati jer ju koristi %s artikala!';
+$_['error_warning']     = 'Upozorenje: Pažljivo provjerite ima li pogrešaka u obrascu!';
+$_['error_permission']  = 'Upozorenje! Niste ovlašteni za promjenu grupe značajki!';
+$_['error_name']        = 'Naziv grupe značajki mora biti između 3 i 64 znaka!';
+$_['error_attribute']   = 'Upozorenje: Ova grupa značajki ne može se izbrisati jer ju koristi %s značajki!';
+$_['error_product']     = 'Upozorenje: Ova grupa značajki ne može se izbrisati jer ju koristi %s artikala!';
