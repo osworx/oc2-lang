d0fab56eecc9c3b0a9b0b87417423b414a992307
OSWorX <info@osworx.net>
2025-03-16 11:13:29 +0100

    New translations recurring.php (Rսssian, Սkraine)

--- upload/admin/language/en-gb/catalog/ru-UA.php
+++ upload/admin/language/en-gb/catalog/ru-UA.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'Товары';
+$_['heading_title']			= 'Периодичные платежи';
 
 // Text
-$_['text_success']           = 'Успех: Вы изменили продукты!';
-$_['text_list']              = 'Список продуктов';
-$_['text_add']               = 'Добавить продукт';
-$_['text_edit']              = 'Изменить продукт';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'По умолчанию';
-$_['text_option']            = 'Параметр';
-$_['text_option_value']      = 'Значение параметра';
-$_['text_percent']           = 'Відсотки';
-$_['text_amount']            = 'Фіксована сума';
-
-// Column
-$_['column_name']            = 'Название продукта';
-$_['column_model']           = 'Модель';
-$_['column_image']           = 'Изображение';
-$_['column_price']           = 'Цена';
-$_['column_quantity']        = 'Количество';
-$_['column_status']          = 'Статус';
-$_['column_action']          = 'Действие';
+$_['text_success']          = 'Успех: Вы изменили Периодичные платежи!';
+$_['text_list']             = 'Список Периодичных платежей';
+$_['text_add']              = 'Добавить Периодичный платеж';
+$_['text_edit']             = 'Изменить Периодичный платеж';
+$_['text_day']				= 'День';
+$_['text_week']				= 'Неделя';
+$_['text_semi_month']		= 'Пол-месяца';
+$_['text_month']			= 'Месяц';
+$_['text_year']				= 'Год';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Периодические суммы вычисляются из частоты и циклов.</p><p>Например, если вы используете частоты «неделя» и цикл «2», то пользователь будет выставлен счет каждые 2 недели.</p><p>Продолжительность — количество раз, пользователь будет сделать платеж, установите это значение 0, если вы хотите, чтобы платежи до тех пор, пока они будут отменены.</p>';
+$_['text_profile']			= 'Периодичный платеж';
+$_['text_trial']			= 'Пробный профиль';
 
 // Entry
-$_['entry_name']             = 'Название продукта';
-$_['entry_description']      = 'Описание';
-$_['entry_meta_title'] 	     = 'Мета тег Title';
-$_['entry_meta_keyword'] 	 = 'Мета тег Ключевые слова';
-$_['entry_meta_description'] = 'Мета-тег описание';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Модель';
-$_['entry_sku']              = 'Единица складского учета';
-$_['entry_upc']              = 'Универсальный Код Товара';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'Наиболее вероятное количество';
-$_['entry_location']         = 'Местоположение';
-$_['entry_shipping']         = 'Требуется доставка';
-$_['entry_manufacturer']     = 'Производитель';
-$_['entry_store']            = 'Магазины';
-$_['entry_date_available']   = 'Дата доступности';
-$_['entry_quantity']         = 'Количество';
-$_['entry_minimum']          = 'Минимальное количество';
-$_['entry_stock_status']     = 'Закончилось';
-$_['entry_price']            = 'Цена';
-$_['entry_tax_class']        = 'Налоговый класс';
-$_['entry_points']           = 'Баллы';
-$_['entry_option_points']    = 'Баллы';
-$_['entry_subtract']         = 'Вычитать со склада';
-$_['entry_weight_class']     = 'Класс веса';
-$_['entry_weight']           = 'Вес';
-$_['entry_dimension']        = 'Габаритные размеры (Д x Ш x В)';
-$_['entry_length_class']     = 'Класс длины';
-$_['entry_length']           = 'Длина';
-$_['entry_width']            = 'Ширина';
-$_['entry_height']           = 'Высота';
-$_['entry_image']            = 'Изображение';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Группа покупателя';
-$_['entry_date_start']       = 'Дата начала';
-$_['entry_date_end']         = 'Дата окончания';
-$_['entry_priority']         = 'Приоритет';
-$_['entry_attribute']        = 'Характеристики';
-$_['entry_attribute_group']  = 'Группа аттрибутов';
-$_['entry_text']             = 'Текст';
-$_['entry_option']           = 'Параметр';
-$_['entry_option_value']     = 'Значение параметра';
-$_['entry_required']         = 'Необходимо';
-$_['entry_status']           = 'Статус';
-$_['entry_sort_order']       = 'Порядок сортировки';
-$_['entry_category']         = 'Категории';
-$_['entry_filter']           = 'Фильтры';
-$_['entry_download']         = 'Загрузки';
-$_['entry_related']          = 'Связанные товары';
-$_['entry_tag']          	 = 'Теги товаров';
-$_['entry_reward']           = 'Поощрительные очки';
-$_['entry_layout']           = 'Переопределение макета';
-$_['entry_recurring']        = 'Периодичный платеж';
+$_['entry_name']		    = 'Имя';
+$_['entry_price']			= 'Цена';
+$_['entry_duration']		= 'Продолжительность';
+$_['entry_cycle']			= 'Цикл';
+$_['entry_frequency']		= 'Частота';
+$_['entry_trial_price']		= 'Пробная цена';
+$_['entry_trial_duration']	= 'Пробный срок';
+$_['entry_trial_status']	= 'Пробный метод';
+$_['entry_trial_cycle']	    = 'Пробный цикл';
+$_['entry_trial_frequency'] = 'Пробная частота';
+$_['entry_status']			= 'Статус';
+$_['entry_sort_order']		= 'Порядок сортировки';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'Не вычитать со склада';
-$_['help_upc']               = 'Универсальный товарный код';
-$_['help_ean']               = 'Номер Европейского статьи';
-$_['help_jan']               = 'Номер японской статьи';
-$_['help_isbn']              = 'Международный Стандартный номер книги';
-$_['help_mpn']               = 'Номер детали у производителя';
-$_['help_manufacturer']      = '(Автозаполнение)';
-$_['help_minimum']           = 'Минимальное количество в заказе';
-$_['help_stock_status']      = 'Статус показывает, когда продукт будет в наличии';
-$_['help_points']            = 'Количество бонусов, необходимых для покупки этого товара. Если вы не хотите что бы этот продукт приобретался с бонусами оставьте значение 0.';
-$_['help_category']          = '(Автозаполнение)';
-$_['help_filter']            = '(Автозаполнение)';
-$_['help_download']          = '(Автозаполнение)';
-$_['help_related']           = '(Автозаполнение)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'Имя';
+$_['column_sort_order']	    = 'Порядок сортировки';
+$_['column_action']         = 'Действие';
 
 // Error
-$_['error_warning']          = 'Внимание: Проверьте форму на наличие ошибок!';
-$_['error_permission']       = 'Внимание: Вы не иметь разрешения на изменение продукции!';
-$_['error_name']             = 'Имя платежа должно быть больше чем 3 и менее 255 символов!';
-$_['error_meta_title']       = 'Мета-тег описание должен быть больше чем 3 и менее 255 символов!';
-$_['error_model']            = 'Модель продукта должна быть больше 1 и меньше, чем 64 символов!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Внимание: Проверьте форму на наличие ошибок!';
+$_['error_permission']		= 'Внимание: Вы не иметь разрешения на изменение Периодичных платежей!';
+$_['error_name']			= 'Имя платежа должно быть больше чем 3 и менее 255 символов!';
+$_['error_product']			= 'Предупреждение: Этот Периодичный платеж нельзя удалить, поскольку он в настоящее время назначен к %s товарам!';
