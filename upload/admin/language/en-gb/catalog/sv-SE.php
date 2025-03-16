999083372c90b488995de1496fccefb441b248a0
OSWorX <info@osworx.net>
2025-03-16 14:19:25 +0100

    New translations filter.php (Swedish)

--- upload/admin/language/en-gb/catalog/sv-SE.php
+++ upload/admin/language/en-gb/catalog/sv-SE.php
@@ -1,35 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = 'Nedladdningar';
+$_['heading_title']     = 'Filter';
 
 // Text
-$_['text_success']      = 'Klart: Du har ändrat nedladdningar!';
-$_['text_list']         = 'Ladda ner lista';
-$_['text_add']          = 'Lägg till hämtning';
-$_['text_edit']         = 'Redigera nedladdning';
-$_['text_upload']       = 'Din fil laddades upp framgångsrikt!';
+$_['text_success']      = 'Klart: Du har ändrat filter!';
+$_['text_list']         = 'Filtrera listan';
+$_['text_add']          = 'Lägg till filter';
+$_['text_edit']         = 'Redigera filter';
 
 // Column
-$_['column_name']       = 'Hämta namn';
-$_['column_date_added'] = 'Skapad';
+$_['column_group']      = 'Filtergrupp';
+$_['column_sort_order'] = 'Sorteringsordning';
 $_['column_action']     = 'Åtgärd';
 
 // Entry
-$_['entry_name']        = 'Hämta namn';
-$_['entry_filename']    = 'Filnamn';
-$_['entry_mask']        = 'Mask';
-
-// Help
-$_['help_filename']     = 'Du kan överföra via knappen Ladda upp eller använda FTP för att ladda upp till nedladdningskatalogen och ange uppgifterna nedan.';
-$_['help_mask']         = 'Det rekommenderas att filnamnet och masken är olika för förhindra att man försöker direktlänka till dina nedladdningar.';
+$_['entry_group']       = 'Namn på gruppfiltret';
+$_['entry_name']        = 'Filternamn';
+$_['entry_sort_order']  = 'Sorteringsordning';
 
 // Error
 $_['error_warning']     = 'Varning: Kolla gärna formuläret noga vad det gäller fel!';
-$_['error_permission']  = 'Varning: Du har inte behörighet att ändra nedladdningar!';
-$_['error_name']        = 'Hämta namn måste vara mellan 3 och 64 tecken långt!';
-$_['error_upload']      = 'Uppladdning krävs!';
-$_['error_filename']    = 'Filnamn måste vara mellan 3 och 128 tecken långt!';
-$_['error_exists']      = 'Filen existerar inte!';
-$_['error_mask']        = 'Mask måste vara mellan 3 och 128 tecken långt!';
-$_['error_filetype']    = 'Ogiltig filtyp!';
-$_['error_product']     = 'Varning: Den här nedladdningen kan inte raderas eftersom den för närvarande är tilldelad %s produkter!';
+$_['error_permission']  = 'Varning: Du har inte behörighet att ändra filter!';
+$_['error_group']       = 'Filtergruppens namn måste vara mellan 1 och 64 tecken långt!';
+$_['error_name']        = 'Filternamn måste vara mellan 1 och 64 tecken långt!';
