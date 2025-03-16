1e6347769ff3eded17eeb3cba3b5c9da1c87f18b
OSWorX <info@osworx.net>
2025-03-16 09:11:05 +0100

    New translations footer.php (Korean)

--- upload/language/ko-KR/ko-KR.php
+++ upload/language/ko-KR/ko-KR.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = '이미지 관리자';
-
 // Text
-$_['text_uploaded']    = '완료: 파일을 업로드했습니다.';
-$_['text_directory']   = '완료: 폴더를 만들었습니다.';
-$_['text_delete']      = '완료: 파일 또는 폴더를 삭제했습니다.';
-
-// Entry
-$_['entry_search']     = '검색..';
-$_['entry_folder']     = '폴더이름';
-
-// Error
-$_['error_permission'] = '주의: 권한이 없습니다.';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = '주의: 이미 같은 이름의 파일 또는 폴더가 있습니다.';
-$_['error_directory']  = '주의: 폴더가 존재하지 않습니다.';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = '주의: 잘못된 파일형식입니다.';
-$_['error_upload']     = '주의: 알 수 없는 이유로 파일을 업로드 할 수 없습니다.';
-$_['error_delete']     = '주의: 이 폴더는 삭제할 수 없습니다.';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = '버전 %s';
