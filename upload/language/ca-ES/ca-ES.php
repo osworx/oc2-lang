e279c878ade31594bc7dd9c8974d74dab64c3139
OSWorX <info@osworx.net>
2025-03-16 09:07:33 +0100

    New translations category.php (Catalan)

--- upload/language/ca-ES/ca-ES.php
+++ upload/language/ca-ES/ca-ES.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Atributs';
+$_['heading_title']          = 'Categoria';
 
 // Text
-$_['text_success']      = 'Èxit: Heu modificat els atributs!';
-$_['text_list']         = 'Llista d\'atributs';
-$_['text_add']          = 'Afegir atribut';
-$_['text_edit']         = 'Editar atribut';
+$_['text_success']           = 'Èxit: Heu modificat les categories!';
+$_['text_list']              = 'Llista de categories';
+$_['text_add']               = 'Afegir categoria';
+$_['text_edit']              = 'Editar categoria';
+$_['text_default']           = 'Predeterminat';
 
 // Column
-$_['column_name']       = 'Nom de l\'Atribut';
-$_['column_sort_order'] = 'Ordre de classificació';
-$_['column_action']     = 'Acció';
+$_['column_name']            = 'Nom de la Categoria';
+$_['column_sort_order']      = 'Ordre de classificació';
+$_['column_action']          = 'Acció';
 
 // Entry
-$_['entry_name']        = 'Nom de l\'Atribut';
-$_['entry_sort_order']  = 'Ordre de classificació';
+$_['entry_name']             = 'Nom de la Categoria';
+$_['entry_description']      = 'Descripció';
+$_['entry_meta_title'] 	     = 'Títol de l\'etiqueta meta';
+$_['entry_meta_keyword']     = 'Meta Paraules Clau Tag';
+$_['entry_meta_description'] = 'Meta Tag Descripció';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Categoria Pare';
+$_['entry_filter']           = 'Filtres';
+$_['entry_store']            = 'Botigues:';
+$_['entry_image']            = 'Imatge';
+$_['entry_top']              = 'Superior';
+$_['entry_column']           = 'Columnes';
+$_['entry_sort_order']       = 'Ordre de classificació';
+$_['entry_status']           = 'Estat';
+$_['entry_layout']           = 'Disposició En Passar Damunt';
+
+// Help
+$_['help_filter']            = '(Emplenar automàticament)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Mostrar a la barra de menú superior. Només funciona per a les categories principals.';
+$_['help_column']            = 'Nombre de columnes a utilitzar per a les 3 últimes categories. Només funciona per a les categories principals.';
 
 // Error
-$_['error_warning']     = 'Advertència: Si us plau reviseu acuradament el formulari pels errors!';
-$_['error_permission']  = 'Advertència: No teniu permís per modificar els atributs!';
-$_['error_name']        = 'Advertència: El Nom de l\'Atribut deu de tenir entre 3 i 64 caràcters!';
-$_['error_attribute']   = 'Advertència: Aquest Grup d\'Atributs no pot ser eliminat ja que està assignat actualment a %s atributs!';
-$_['error_product']     = 'Advertència: Aquest atribut no pot ser eliminat ja està assignat actualment a %s productes!';
+$_['error_warning']          = 'Advertència: Si us plau reviseu acuradament el formulari pels errors!';
+$_['error_permission']       = 'Advertència: No teniu permís per modificar categories!!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'El Títol Meta ha de ser major que 3 i menor de 255 caràcters!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
