6af5f2d82e9e7a08ef070b70deeec9e2a2f1e386
OSWorX <info@osworx.net>
2025-03-16 11:07:26 +0100

    New translations download.php (Spanish)

--- upload/admin/language/en-gb/catalog/es-ES.php
+++ upload/admin/language/en-gb/catalog/es-ES.php
@@ -1,46 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Categorías';
+$_['heading_title']     = 'Descargas';
 
 // Text
-$_['text_success']           = '¡Has modificado exitosamente las categorías!';
-$_['text_list']              = 'Lista de categorías';
-$_['text_add']               = 'Añadir Categoría';
-$_['text_edit']              = 'Editar categoría';
-$_['text_default']           = 'Predefinido';
+$_['text_success']      = '¡Tus descargas se han modificado exitosamente!';
+$_['text_list']         = 'Lista de Descargas';
+$_['text_add']          = 'Agregar Descarga';
+$_['text_edit']         = 'Editar Descarga';
+$_['text_upload']       = '¡Su archivo se ha cargado exitosamente!';
 
 // Column
-$_['column_name']            = 'Nombre de Categoría';
-$_['column_sort_order']      = 'Ordenar por';
-$_['column_action']          = 'Acción';
+$_['column_name']       = 'Nombre de la Descarga';
+$_['column_date_added'] = 'Agregar Fecha';
+$_['column_action']     = 'Acción';
 
 // Entry
-$_['entry_name']             = 'Nombre de Categoría';
-$_['entry_description']      = 'Descripción';
-$_['entry_meta_title'] 	     = 'Etiqueta de Meta Título';
-$_['entry_meta_keyword']     = 'Etiqueta Meta Palabras Claves';
-$_['entry_meta_description'] = 'Etiqueta Meta Descripción';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = 'Padres';
-$_['entry_filter']           = 'Filtros';
-$_['entry_store']            = 'Tiendas';
-$_['entry_image']            = 'Imagen';
-$_['entry_top']              = 'Top';
-$_['entry_column']           = 'Columnas';
-$_['entry_sort_order']       = 'Ordenar por';
-$_['entry_status']           = 'Estado';
-$_['entry_layout']           = 'Anulación de la disposición';
+$_['entry_name']        = 'Nombre de la Descarga';
+$_['entry_filename']    = 'Nombre del Archivo';
+$_['entry_mask']        = 'Máscara';
 
 // Help
-$_['help_filter']            = '(Autocompletar)';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = 'Mostrar en la barra de menú superior. Solo funciona para las categorías padres.';
-$_['help_column']            = 'Número de columnas a usar para las categorías inferiores 3. Sólo funciona para las categorías padres.';
+$_['help_filename']     = 'Puede subir utiliza el botón de subir o usando FTP para subir los archivos al directorio de descarga y luego incluir los detalles.';
+$_['help_mask']         = 'Es recomendable que el nombre del archivo y la máscara sean diferentes para evitar que las personas intenten descargar directamente el archivo del link.';
 
 // Error
-$_['error_warning']          = '¡Advertencia: Por favor revise cuidadosamente los errores en el formulario!';
-$_['error_permission']       = '¡Advertencia: No posee permisos para modificar las categorías!';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = '¡Meta Título debe ser mayor a 3 y menor a 255 caracteres!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = '¡Advertencia: Por favor revise cuidadosamente los errores en el formulario!';
+$_['error_permission']  = '¡Advertencia: No tiene permiso para modificar las descargas!';
+$_['error_name']        = '¡El nombre de la Descarga debe tener entre 3 y 64 carácteres!';
+$_['error_upload']      = '¡Carga Requerida!';
+$_['error_filename']    = '¡El nombre del archivo debe tener entre 3 y 128 carácteres!';
+$_['error_exists']      = '¡El archivo no existe!';
+$_['error_mask']        = '¡La máscara debe tener entre 3 y 128 carácteres!';
+$_['error_filetype']    = '¡Tipo de archivo inválido!';
+$_['error_product']     = '¡Advertencia: Esta descarga no puede ser borrada porque está asociada a %s productos!';
