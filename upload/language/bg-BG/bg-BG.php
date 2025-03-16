8c9beef8ff31cc77531a7ea82ccc9091a21ccfde
OSWorX <info@osworx.net>
2025-03-16 09:07:22 +0100

    New translations recurring.php (Bulgarian)

--- upload/language/bg-BG/bg-BG.php
+++ upload/language/bg-BG/bg-BG.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'Продукти';
+$_['heading_title']			= 'Периодични профили';
 
 // Text
-$_['text_success']           = 'Успех: Вие модифицирахте продукти!';
-$_['text_list']              = 'Списък с продукти';
-$_['text_add']               = 'Добавяне на продукт';
-$_['text_edit']              = 'Редактиране на продукт';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'По подразбиране';
-$_['text_option']            = 'Опции';
-$_['text_option_value']      = 'Стойност на опция';
-$_['text_percent']           = 'Процент';
-$_['text_amount']            = 'Фиксирана сума';
-
-// Column
-$_['column_name']            = 'Име на Продукта';
-$_['column_model']           = 'Модел';
-$_['column_image']           = 'Изображение';
-$_['column_price']           = 'Цена';
-$_['column_quantity']        = 'Количество';
-$_['column_status']          = 'Статус';
-$_['column_action']          = 'Действие';
+$_['text_success']          = 'Успех: Вие модифицирахте повтарящите профили!';
+$_['text_list']             = 'Списък на повтарящите профили';
+$_['text_add']              = 'Добавяне на повтарящ профил';
+$_['text_edit']             = 'Редактиране на повтарящи профили';
+$_['text_day']				= 'Ден';
+$_['text_week']				= 'Седмица';
+$_['text_semi_month']		= 'Полу месец';
+$_['text_month']			= 'Месец';
+$_['text_year']				= 'Година';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> периодични суми се изчисляват от честотата и цикли.</p><p>Например, ако използвате честота на "седмица" и един цикъл на "2", след това потребителят ще бъде фактурирана всеки 2 седмици.</p><p>Продължителността е колко пъти, потребителят ще направи плащане, поставям този към 0, ако искате плащания, докато те са отменени.</p>';
+$_['text_profile']			= 'Периодичен профил';
+$_['text_trial']			= 'Пробен профил';
 
 // Entry
-$_['entry_name']             = 'Име на Продукта';
-$_['entry_description']      = 'Описание';
-$_['entry_meta_title'] 	     = 'Мета име';
-$_['entry_meta_keyword'] 	 = 'Мета тагове ключови думи';
-$_['entry_meta_description'] = 'Мета тагове описание';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Модел';
-$_['entry_sku']              = 'Референтен номер';
-$_['entry_upc']              = 'Универсален продуктов код (баркод)';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'Местоположение';
-$_['entry_shipping']         = 'Изисква Доставка';
-$_['entry_manufacturer']     = 'Производител';
-$_['entry_store']            = 'Магазини';
-$_['entry_date_available']   = 'Наличен на дата';
-$_['entry_quantity']         = 'Количество';
-$_['entry_minimum']          = 'Минимално количество';
-$_['entry_stock_status']     = ' Статус: Не е в наличност';
-$_['entry_price']            = 'Цена';
-$_['entry_tax_class']        = 'Данъчна такса';
-$_['entry_points']           = 'Точки';
-$_['entry_option_points']    = 'Точки';
-$_['entry_subtract']         = 'Изваждане от наличност';
-$_['entry_weight_class']     = 'Клас тегло ';
-$_['entry_weight']           = 'Тегло';
-$_['entry_dimension']        = 'Размери (Д x Ш x В)';
-$_['entry_length_class']     = 'Дължина клас';
-$_['entry_length']           = 'Дължина';
-$_['entry_width']            = 'Ширина';
-$_['entry_height']           = 'Височина';
-$_['entry_image']            = 'Изображение';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Клиентска група';
-$_['entry_date_start']       = 'Дата на стартиране';
-$_['entry_date_end']         = 'Дата край';
-$_['entry_priority']         = 'Приоритет';
-$_['entry_attribute']        = 'Атрибут';
-$_['entry_attribute_group']  = 'Група на Атрибута';
-$_['entry_text']             = 'Текст';
-$_['entry_option']           = 'Опции';
-$_['entry_option_value']     = 'Стойност на опция';
-$_['entry_required']         = 'Изисква';
-$_['entry_status']           = 'Статус';
-$_['entry_sort_order']       = 'Ред на Сортиране';
-$_['entry_category']         = 'Категории';
-$_['entry_filter']           = 'Филтри';
-$_['entry_download']         = 'Сваляния';
-$_['entry_related']          = 'Свързани продукти';
-$_['entry_tag']          	 = 'Ключови думи';
-$_['entry_reward']           = 'Наградни точки ';
-$_['entry_layout']           = 'Замени оформление';
-$_['entry_recurring']        = 'Периодичен профил';
+$_['entry_name']		    = 'Име';
+$_['entry_price']			= 'Цена';
+$_['entry_duration']		= 'Продължителност';
+$_['entry_cycle']			= 'Цикъл';
+$_['entry_frequency']		= 'Честота';
+$_['entry_trial_price']		= 'Пробна цена';
+$_['entry_trial_duration']	= 'Пробна Продължителност';
+$_['entry_trial_status']	= 'Пробен статус';
+$_['entry_trial_cycle']	    = 'Пробен цикъл';
+$_['entry_trial_frequency'] = 'Пробна честота';
+$_['entry_status']			= 'Статус';
+$_['entry_sort_order']		= 'Ред на Сортиране';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'Наличност';
-$_['help_upc']               = 'Универсален код на продукта';
-$_['help_ean']               = 'Европейски номер';
-$_['help_jan']               = 'Японски номер';
-$_['help_isbn']              = 'Международен стандартен номер на книга';
-$_['help_mpn']               = 'Партиден номер на производителя';
-$_['help_manufacturer']      = '(Автодовършване)';
-$_['help_minimum']           = 'Задължителна минимална сума на поръчка';
-$_['help_stock_status']      = 'Показване на състояние при изчерпана наличност';
-$_['help_points']            = 'Броят на точките, необходими за закупуване на този артикул. Ако не искате този продукт да бъде закупен с точки, оставете 0.';
-$_['help_category']          = '(Автодовършване)';
-$_['help_filter']            = '(Автодовършване)';
-$_['help_download']          = '(Автодовършване)';
-$_['help_related']           = '(Автодовършване)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'Име';
+$_['column_sort_order']	    = 'Ред на Сортиране';
+$_['column_action']         = 'Действие';
 
 // Error
-$_['error_warning']          = 'Внимание: Моля, проверете формуляра внимателно за грешки!';
-$_['error_permission']       = 'Внимание: Нямате права да променяте параметрите на продуктите!';
-$_['error_name']             = 'Име на продукта трябва да бъде по-дълго от 3 и по-малко от 255 символа!';
-$_['error_meta_title']       = 'Мета заглавие трябва да бъде по-голямо от 3 и по-малко от 255 знака!';
-$_['error_model']            = 'Модел на продукта трябва да бъде по-дълъг от 1 и по-малък от 64 символа!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Внимание: Моля, проверете формуляра внимателно за грешки!';
+$_['error_permission']		= 'Предупреждение: Вие нямате разрешение да променяте периодични профили!';
+$_['error_name']			= 'Името на профила трябва да бъде по-голямо от 3 и по-малко от 255 знака!';
+$_['error_product']			= 'Предупреждение: Този периодичен профил не може да се заличи, тъй като понастоящем е зададен на %s продукти!';
