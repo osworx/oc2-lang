ac281630ef7095a0d46cab2048199efc81c299ff
OSWorX <info@osworx.net>
2025-03-16 14:21:18 +0100

    New translations manufacturer.php (French, Canada)

--- upload/admin/language/en-gb/catalog/fr-CA.php
+++ upload/admin/language/en-gb/catalog/fr-CA.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Information';
+$_['heading_title']     = 'Fabricants';
 
 // Text
-$_['text_success']           = 'Vous avez modifié les informations avec succès!';
-$_['text_list']              = 'Liste des information';
-$_['text_add']               = 'Ajouter une information';
-$_['text_edit']              = 'Modifier une information';
-$_['text_default']           = 'Par défaut';
+$_['text_success']      = 'Modification des marques réussie !';
+$_['text_list']         = 'Liste des marques';
+$_['text_add']          = 'Ajouter une marque';
+$_['text_edit']         = 'Modifier une marque';
+$_['text_default']      = 'Par défaut';
+$_['text_percent']      = 'Pourcentage';
+$_['text_amount']       = 'Montant fixe';
 
 // Column
-$_['column_title']           = 'Titre de l’information';
-$_['column_sort_order']	     = 'Ordre de présentation';
-$_['column_action']          = 'Action';
+$_['column_name']       = 'Nom de la marque';
+$_['column_sort_order'] = 'Ordre de présentation';
+$_['column_action']     = 'Action';
 
 // Entry
-$_['entry_title']            = 'Titre de l’information';
-$_['entry_description']      = 'Description';
-$_['entry_store']            = 'Magasins';
-$_['entry_meta_title'] 	     = 'Titre de métaélément';
-$_['entry_meta_keyword'] 	 = 'Mots-clef de métaélément';
-$_['entry_meta_description'] = 'Description de métaélément';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'Pied de page';
-$_['entry_status']           = 'État';
-$_['entry_sort_order']       = 'Ordre de présentation';
-$_['entry_layout']           = 'Outrepasser la mise en page';
+$_['entry_name']        = 'Nom de la marque';
+$_['entry_store']       = 'Magasins';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'Image';
+$_['entry_sort_order']  = 'Ordre de présentation';
+$_['entry_type']        = 'Type';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'Afficher dans le pied de page.';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'Attention: Veuillez vérifier que le formulaire ne contient pas d\'erreur !';
-$_['error_permission']       = 'Attention : vous n’êtes pas autorisé à modifier les Informations !';
-$_['error_title']            = 'Le titre de l’information doit contenir entre 3 et 64 caractères !';
-$_['error_description']      = 'La description de l’information doit contenir plus de 3 caractères !';
-$_['error_meta_title']       = 'Le titre méta doit contenir plus de 3 et moins de 255 caractères !';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'Attention : cette page d’information ne peut être supprimée car elle contient actuellement les modalités de compte du magasin !';
-$_['error_checkout']         = 'Attention : cette page d’information ne peut être supprimée car elle contient actuellement les modalités de passage en caisse du magasin !';
-$_['error_affiliate']        = 'Attention : cette page d’information ne peut être supprimée car il est actuellement assigné comme les conditions d’affiliation du magasin !';
-$_['error_return']           = 'Attention : cette page d’information ne peut être supprimée car il est actuellement assigné comme les conditions de retour de magasin !';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'Attention: Veuillez vérifier que le formulaire ne contient pas d\'erreur !';
+$_['error_permission']  = 'Avertissement : Vous n\'êtes pas autorisé à modifier les Marques !';
+$_['error_name']        = 'La composition du nom du fabricant doit être comprise entre de 2 à 64 caractères !';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'Attention : ce fabricant ne peut être supprimé car il est actuellement affecté aux produits %s !';
