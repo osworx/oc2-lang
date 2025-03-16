3abae510fed177703f13cc54e9d4304c4f939e21
OSWorX <info@osworx.net>
2025-03-16 11:05:36 +0100

    New translations attribute.php (Bosnian)

---
+++ upload/admin/language/en-gb/catalog/bs-BA.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Atributi';
+
+// Text
+$_['text_success']           = 'Uspjeh: Uspješno ste promjenili atribute!';
+$_['text_list']              = 'Popisa atributa';
+$_['text_add']               = 'Dodavanje atributa';
+$_['text_edit']              = 'Uredite atribut';
+
+// Column
+$_['column_name']            = 'Naziv atributa';
+$_['column_attribute_group'] = 'Grupe Atributa';
+$_['column_sort_order']      = 'Redoslijed sortiranja';
+$_['column_action']          = 'Akcija';
+
+// Entry
+$_['entry_name']             = 'Naziv atributa';
+$_['entry_attribute_group']  = 'Grupe Atributa';
+$_['entry_sort_order']       = 'Redoslijed sortiranja';
+
+// Error
+$_['error_warning']          = 'Upozorenje: Provjerite obrazac pažljivo radi grešaka!';
+$_['error_permission']       = 'Upozorenje: Nemate dozvolu za mijenjanje atributa!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Naziv atributa mora biti između 3 i 64 znakova!';
+$_['error_product']          = 'Upozorenje: Ovaj atribut se ne može izbrisati jer je trenutno dodijeljen %s proizvoda!';
