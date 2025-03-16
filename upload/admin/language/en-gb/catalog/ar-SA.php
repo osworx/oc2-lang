21187da829f6ae5b84dbd3057c22da43b9e6aa49
OSWorX <info@osworx.net>
2025-03-16 14:15:48 +0100

    New translations attribute.php (Arabic)

--- upload/admin/language/en-gb/catalog/ar-SA.php
+++ upload/admin/language/en-gb/catalog/ar-SA.php
@@ -1,36 +1,27 @@
 <?php
 // Heading
-$_['heading_title']     = 'المراجعات';
+$_['heading_title']          = 'الخصائص';
 
 // Text
-$_['text_success']      = 'تم بنجاح: لقد قمت بتعديل المراجعات!';
-$_['text_list']         = 'قائمة التعليقات';
-$_['text_add']          = 'أضف تعليق';
-$_['text_edit']         = 'تحرير تعليق';
+$_['text_success']           = 'تم بنجاح: لقد قمت بتعديل الخصائص!';
+$_['text_list']              = 'قائمة الخصائص';
+$_['text_add']               = 'اضافة الخاصيه';
+$_['text_edit']              = 'تعديل الخاصية';
 
 // Column
-$_['column_product']    = 'المنتج';
-$_['column_author']     = 'الكاتب';
-$_['column_rating']     = 'التقييم';
-$_['column_status']     = 'الحالة';
-$_['column_date_added'] = 'تاريخ اضافة الملف';
-$_['column_action']     = 'عمل';
+$_['column_name']            = 'اسم الخاصية';
+$_['column_attribute_group'] = 'مجموعة الخصائص';
+$_['column_sort_order']      = 'الترتيب';
+$_['column_action']          = 'عمل';
 
 // Entry
-$_['entry_product']     = 'المنتج';
-$_['entry_author']      = 'الكاتب';
-$_['entry_rating']      = 'التقييم';
-$_['entry_status']      = 'الحالة';
-$_['entry_text']        = 'نص';
-$_['entry_date_added']  = 'تاريخ اضافة الملف';
-
-// Help
-$_['help_product']      = '(إكمال تلقائي)';
+$_['entry_name']             = 'اسم الخاصية';
+$_['entry_attribute_group']  = 'مجموعة الخصائص';
+$_['entry_sort_order']       = 'الترتيب';
 
 // Error
-$_['error_warning']     = 'تحذير: الرجاء التحقق من النموذج واصلاح الأخطاء!';
-$_['error_permission']  = 'تحذير: ليس لديك صلاحية للتعديل على التعليقات!';
-$_['error_product']     = 'المنتج مطلوب!';
-$_['error_author']      = 'يجب أن يكون اسم الكاتب بين 3 و 64 حرفاً!';
-$_['error_text']        = 'نص التعليق يجب أن يكون حرف واحد على الأقل!';
-$_['error_rating']      = 'مطلوب التقييم عند التعليق!';
+$_['error_warning']          = 'تحذير: الرجاء التحقق من النموذج واصلاح الأخطاء!';
+$_['error_permission']       = 'تحذير: ليس لديك الإذن بتعديل الخصائص!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'يجب ان يكون اسم الخاصية بين 3 و 64 حرفاً!';
+$_['error_product']          = 'تحذير: هذه الخاصية لا يمكن حذفها لأنها مرتبطه بـ %s منتجات!';
