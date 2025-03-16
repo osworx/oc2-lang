849dc00ba4fef76f3bb1656901f9971c6dfc218e
OSWorX <info@osworx.net>
2025-03-16 11:17:50 +0100

    New translations footer.php (Ukrainian)

--- upload/admin/language/en-gb/common/uk-UA.php
+++ upload/admin/language/en-gb/common/uk-UA.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Менеджер зображень';
-
 // Text
-$_['text_uploaded']    = 'Вітаємо: Ви успішно завантажили файл!';
-$_['text_directory']   = 'Вітаємо: Ви успішно створили теку!';
-$_['text_delete']      = 'Вітаємо: Ви успішно видалили файл або теку!';
-
-// Entry
-$_['entry_search']     = 'Пошук..';
-$_['entry_folder']     = 'Назва теки';
-
-// Error
-$_['error_permission'] = 'Увага: Доступ заборонено!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Увага: Файл або каталог із таким іменем вже існує!';
-$_['error_directory']  = 'Увага: Теки не існує!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Увага: Неправильний тип файлу!';
-$_['error_upload']     = 'Увага: Файл не вдалося завантажити з невідомих причин!';
-$_['error_delete']     = 'Увага: Ви не можете видалити цю теку!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Версія %s';
