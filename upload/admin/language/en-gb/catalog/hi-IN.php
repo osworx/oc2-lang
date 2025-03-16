35e4264da10b359246261f40d1b968c0682e3d8f
OSWorX <info@osworx.net>
2025-03-16 11:09:58 +0100

    New translations information.php (Hindi)

--- upload/admin/language/en-gb/catalog/hi-IN.php
+++ upload/admin/language/en-gb/catalog/hi-IN.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'छानना';
+$_['heading_title']          = 'जानकारी';
 
 // Text
-$_['text_success']      = 'सफलता: आपने फ़िल्टर को संशोधित किया है!';
-$_['text_list']         = 'फ़िल्टर सूची';
-$_['text_add']          = 'फ़िल्टर जोड़ें';
-$_['text_edit']         = 'फ़िल्टर संशोधित करें';
+$_['text_success']           = 'सफलता: आपने जानकारी को संशोधित किया है!';
+$_['text_list']              = 'जानकारी सूची';
+$_['text_add']               = 'जानकारी जोड़ें';
+$_['text_edit']              = 'जानकारी संशोधित करें';
+$_['text_default']           = 'सामान्य';
 
 // Column
-$_['column_group']      = 'फ़िल्टर समूह';
-$_['column_sort_order'] = 'क्रम';
-$_['column_action']     = 'कार्यवाही';
+$_['column_title']           = 'जानकारी शीर्षक';
+$_['column_sort_order']	     = 'क्रम';
+$_['column_action']          = 'कार्यवाही';
 
 // Entry
-$_['entry_group']       = 'फ़िल्टर समूह का नाम';
-$_['entry_name']        = 'फ़िल्टर का नाम';
-$_['entry_sort_order']  = 'क्रम';
+$_['entry_title']            = 'जानकारी शीर्षक';
+$_['entry_description']      = 'विवरण';
+$_['entry_store']            = 'भंडार';
+$_['entry_meta_title'] 	     = 'मेटा टैग शीर्षक';
+$_['entry_meta_keyword'] 	 = 'मेटा टैग खोजशब्द';
+$_['entry_meta_description'] = 'मेटा टैग विवरण';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'नीचे';
+$_['entry_status']           = 'स्थिति';
+$_['entry_sort_order']       = 'क्रम';
+$_['entry_layout']           = 'लेआउट ओवरराइड';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'नीचे पाद-लेख में प्रदर्शित करें।';
 
 // Error
-$_['error_warning']     = 'चेतावनी: कृपया ध्यान से त्रुटियों के लिए प्रपत्र की जाँच करें!';
-$_['error_permission']  = 'चेतावनी: आपको फ़िल्टर संशोधित करने की अनुमति नहीं है!';
-$_['error_group']       = 'फ़िल्टर समूह का नाम 1 और 64 वर्णों के बीच होना चाहिए!';
-$_['error_name']        = 'फ़िल्टर का नाम 1 और 64 वर्णों के बीच होना चाहिए!';
+$_['error_warning']          = 'चेतावनी: कृपया ध्यान से त्रुटियों के लिए प्रपत्र की जाँच करें!';
+$_['error_permission']       = 'चेतावनी: आपको जानकारी संशोधित करने की अनुमति नहीं है!';
+$_['error_title']            = 'जानकारी शीर्षक 3 और 64 वर्णों के बीच होना चाहिए!';
+$_['error_description']      = 'विवरण 3 से अधिक अक्षर होना चाहिए!';
+$_['error_meta_title']       = 'मेटा शीर्षक कम से कम 3 और अधिक से अधिक 255  होना आवश्यक है!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'चेतावनी: इस जानकारी पृष्ठ को हटाया नहीं जा सकता, यह वर्तमान में स्टोर खाते शर्तों के रूप में असाइन किया गया है!';
+$_['error_checkout']         = 'चेतावनी: इस जानकारी पृष्ठ को हटाया नहीं जा सकता, यह वर्तमान में स्टोर चेकआउट शर्तों के रूप में असाइन किया गया है!';
+$_['error_affiliate']        = 'चेतावनी: इस जानकारी पृष्ठ को हटाया नहीं जा सकता, यह वर्तमान में स्टोर संबद्ध शर्तों के रूप में असाइन किया गया है!';
+$_['error_return']           = 'चेतावनी: इस जानकारी पृष्ठ को हटाया नहीं जा सकता, यह वर्तमान में स्टोर वापसी शर्तों के रूप में असाइन किया गया है!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
