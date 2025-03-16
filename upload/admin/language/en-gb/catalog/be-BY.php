7c77997c2988a31c5806fcefe0fe1a66da8e24c5
OSWorX <info@osworx.net>
2025-03-16 14:18:16 +0100

    New translations download.php (Belarusian)

--- upload/admin/language/en-gb/catalog/be-BY.php
+++ upload/admin/language/en-gb/catalog/be-BY.php
@@ -1,46 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Катэгорыі';
+$_['heading_title']     = 'Загрузкі';
 
 // Text
-$_['text_success']           = 'Поспех: катэгорыі паспяхова змененыя!';
-$_['text_list']              = 'Спіс катэгорый';
-$_['text_add']               = 'Дадаць катэгорыю';
-$_['text_edit']              = 'Рэдагаваць катэгорыю';
-$_['text_default']           = 'Па-змаўчанню';
+$_['text_success']      = 'Поспех: Загрузкі змененыя!';
+$_['text_list']         = 'Спіс загрузак';
+$_['text_add']          = 'Дадаць загрузку';
+$_['text_edit']         = 'Рэдагаваць загрузку';
+$_['text_upload']       = 'Ваш файл паспяхова загружаны!';
 
 // Column
-$_['column_name']            = 'Назва катэгорыі';
-$_['column_sort_order']      = 'Парадак сартавання';
-$_['column_action']          = 'Дзеянне';
+$_['column_name']       = 'Назва загрузкі';
+$_['column_date_added'] = 'Дададзена';
+$_['column_action']     = 'Дзеянне';
 
 // Entry
-$_['entry_name']             = 'Назва катэгорыі';
-$_['entry_description']      = 'Апісанне';
-$_['entry_meta_title'] 	     = 'Мета-тэг Title';
-$_['entry_meta_keyword']     = 'Мета-тэг Keywords';
-$_['entry_meta_description'] = 'Мета-тэг Description';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = 'Бацькоўскі аб\'ект';
-$_['entry_filter']           = 'Фільтры';
-$_['entry_store']            = 'Крамы';
-$_['entry_image']            = 'Выява';
-$_['entry_top']              = 'Уверсе';
-$_['entry_column']           = 'Слупкі';
-$_['entry_sort_order']       = 'Парадак сартавання';
-$_['entry_status']           = 'Статус';
-$_['entry_layout']           = 'Перавызначэнне разметкі';
+$_['entry_name']        = 'Назва загрузкі';
+$_['entry_filename']    = 'Назва файла';
+$_['entry_mask']        = 'Маска';
 
 // Help
-$_['help_filter']            = '(Аўтазавяршэнне)';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = 'Паказваць у верхнім меню. Працуе толькі для катэгорый вышэйшага ўзроўня.';
-$_['help_column']            = 'Колькасць слупкоў для катэгорый унізе. Працуе толькі для бацькоўскіх катэгорый.';
+$_['help_filename']     = 'Вы можаце ўжыць кнопку загрузкі альбо FTP каб загрузіць у дырэкторыю загрузак выкарыстаўшы дадзеныя ніжэй.';
+$_['help_mask']         = 'Рэкамендавана каб назва файла і маска адрозніваліся каб нельга было ўжыць прамую спасылку на вашыя загрузкі.';
 
 // Error
-$_['error_warning']          = 'Увага: Калі ласка, ўважліва праверце ўведзеные дадзеныя!';
-$_['error_permission']       = 'Увага: Вы не можаце змяняць гэтую інфармацыю!';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = 'Даўжыня meta title павінна быць ад 3 да 255 сімвалаў!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = 'Увага: Калі ласка, ўважліва праверце ўведзеные дадзеныя!';
+$_['error_permission']  = 'Увага: Вы не можаце змяняць загрузкі!';
+$_['error_name']        = 'Даўжыня назвы загрузкі павінна быць ад 3 да 64 сімвалаў!';
+$_['error_upload']      = 'Загрузка абавязковая!';
+$_['error_filename']    = 'Даўжыня назвы файла павінна быць ад 3 да 128 сімвалаў!';
+$_['error_exists']      = 'Файл не існуе!';
+$_['error_mask']        = 'Даўжыня маскі павінна быць ад 3 да 128 сімвалаў!';
+$_['error_filetype']    = 'Неправільнае імя файла!';
+$_['error_product']     = 'Увага: Гэтая загрузка не можа быць выдалена, бо звязана з %s прадуктамі!';
