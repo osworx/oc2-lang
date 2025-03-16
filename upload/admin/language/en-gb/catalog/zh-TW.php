79043cc45abe9dfa72abbf0750e01963e04b43f3
OSWorX <info@osworx.net>
2025-03-16 11:10:38 +0100

    New translations manufacturer.php (Chinese Traditional)

--- upload/admin/language/en-gb/catalog/zh-TW.php
+++ upload/admin/language/en-gb/catalog/zh-TW.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = '資訊';
+$_['heading_title']     = '製造商';
 
 // Text
-$_['text_success']           = '成功：您已經修改資訊！';
-$_['text_list']              = '資訊清單';
-$_['text_add']               = '新增資訊';
-$_['text_edit']              = '編輯資訊';
-$_['text_default']           = '預設';
+$_['text_success']      = '成功：您已經修改製造商！';
+$_['text_list']         = '製造商清單';
+$_['text_add']          = '新增製造商';
+$_['text_edit']         = '編輯製造商';
+$_['text_default']      = '預設';
+$_['text_percent']      = '百分比';
+$_['text_amount']       = '固定金額';
 
 // Column
-$_['column_title']           = '資訊標題';
-$_['column_sort_order']	     = '排序次序';
-$_['column_action']          = '動作';
+$_['column_name']       = '製造商名稱';
+$_['column_sort_order'] = '排序次序';
+$_['column_action']     = '動作';
 
 // Entry
-$_['entry_title']            = '資訊標題';
-$_['entry_description']      = '說明';
-$_['entry_store']            = '商店';
-$_['entry_meta_title'] 	     = 'Meta 標籤標題';
-$_['entry_meta_keyword'] 	 = 'Meta 標籤關鍵字';
-$_['entry_meta_description'] = 'Meta 標籤描述';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = '底端';
-$_['entry_status']           = '狀態';
-$_['entry_sort_order']       = '排序次序';
-$_['entry_layout']           = '覆蓋版面配置';
+$_['entry_name']        = '製造商名稱';
+$_['entry_store']       = '商店';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = '圖像';
+$_['entry_sort_order']  = '排序次序';
+$_['entry_type']        = '類型';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = '在底端的頁尾中顯示。';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = '警告：請仔細檢查表格的錯誤！';
-$_['error_permission']       = '警告：您沒有修改資訊的權限！';
-$_['error_title']            = '資訊標題必須在 3 到 64 個字元之間！';
-$_['error_description']      = '說明長度必須大於 3 個字元！';
-$_['error_meta_title']       = 'Meta 標題必須大於 3 且小於 255 個字元之間！';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = '警告：無法刪除此資訊頁面，因為目前已指定為商店的帳號條款！';
-$_['error_checkout']         = '警告：無法刪除此資訊頁面，因為目前已指定為商店的結帳條款！';
-$_['error_affiliate']        = '警告：無法刪除此資訊頁面，因為目前已指定為商店的聯盟會員條款！';
-$_['error_return']           = '警告：無法刪除此資訊頁面，因為目前已指定為商店的退換貨條款！';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = '警告：請仔細檢查表格的錯誤！';
+$_['error_permission']  = '警告：您沒有修改製造商的權限！';
+$_['error_name']        = '製造商名稱必須介於 2 到 64 個字元之間！';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = '警告：無法刪除這個製造商，因為目前已指派給 %s 個商品！';
