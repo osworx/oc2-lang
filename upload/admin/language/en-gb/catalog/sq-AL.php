ef1d65520bda51eba4ee57031e689b3ac33c5743
OSWorX <info@osworx.net>
2025-03-16 14:17:47 +0100

    New translations category.php (Albanian)

--- upload/admin/language/en-gb/catalog/sq-AL.php
+++ upload/admin/language/en-gb/catalog/sq-AL.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Grupet e Atributit';
+$_['heading_title']          = 'Kategoritë';
 
 // Text
-$_['text_success']      = 'Ju keni modifikuar grupet e atributeve me sukses!';
-$_['text_list']         = 'Lista e grupeve të atributeve';
-$_['text_add']          = 'Shto grupin e atributeve';
-$_['text_edit']         = 'Modifiko grupin e atributeve';
+$_['text_success']           = 'Ju keni modifikuar kategoritë me sukses!';
+$_['text_list']              = 'Lista e kategorive';
+$_['text_add']               = 'Shto Kategori';
+$_['text_edit']              = 'Redakto Kategorinë';
+$_['text_default']           = 'Parapërzgjedhur';
 
 // Column
-$_['column_name']       = 'Emri i grupit të atributeve';
-$_['column_sort_order'] = 'Radhitja';
-$_['column_action']     = 'Veprimi';
+$_['column_name']            = 'Emri Kategorisë';
+$_['column_sort_order']      = 'Radhitja';
+$_['column_action']          = 'Veprimi';
 
 // Entry
-$_['entry_name']        = 'Emri i grupit të atributeve';
-$_['entry_sort_order']  = 'Radhitja';
+$_['entry_name']             = 'Emri Kategorisë';
+$_['entry_description']      = 'Përshkrimi';
+$_['entry_meta_title'] 	     = 'Meta titulli';
+$_['entry_meta_keyword']     = 'Fjalë kyçe meta.';
+$_['entry_meta_description'] = 'Përshkrimi Meta Tag';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Prindi';
+$_['entry_filter']           = 'Filtrat';
+$_['entry_store']            = 'Dyqanet';
+$_['entry_image']            = 'Fotografia';
+$_['entry_top']              = 'Sipër';
+$_['entry_column']           = 'Kolonat';
+$_['entry_sort_order']       = 'Radhitja';
+$_['entry_status']           = 'Statusi';
+$_['entry_layout']           = 'Faqosja e shtypit';
+
+// Help
+$_['help_filter']            = '(Autokompletimi)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Shfaqe në pjesën e sipërme të shiritit të menusë. Funksionon vetëm për kategoritë e larta prindërore.';
+$_['help_column']            = 'Numri kolonave për të përdorur për 3 kategoritë e fundit. Funksionon vetëm për top kategoritë prind.';
 
 // Error
-$_['error_warning']     = 'Vërejtje: Ju lutem kontrolloni formën me kujdes për ndonjë gabim!';
-$_['error_permission']  = 'Vërejtje: Ju nuk keni autorizim për të modifikuar grupin e atributeve!';
-$_['error_name']        = 'Emri i grupit të atributeve duhet të jetë nga 3 deri në 64 karaktere!';
-$_['error_attribute']   = 'Vërejtje: Ky grup i atributeve nuk mund të fshihet sepse është i lirdhur aktualisht me atributet e %s!';
-$_['error_product']     = 'Vërejtje: Ky grup i atributeve nuk mund të fshihet sepse është i lirdhur aktualisht me produktet e %s!';
+$_['error_warning']          = 'Vërejtje: Ju lutem kontrolloni formën me kujdes për ndonjë gabim!';
+$_['error_permission']       = 'Vërejtje: Nuk keni autorizim të modifikoni kategoritë!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Produkti Model duhet të jetë më i madh se 3 dhe më pak se 255 karaktere!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
