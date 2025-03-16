15160e0d04bba6cd07c6bb5982d502611869ae72
OSWorX <info@osworx.net>
2025-03-16 11:04:58 +0100

    New translations attribute.php (Hebrew)

---
+++ upload/admin/language/en-gb/catalog/he-IL.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'מאפיינים';
+
+// Text
+$_['text_success']           = 'הצלחה: מודול תכונות שונה!';
+$_['text_list']              = 'רשימת תכונות';
+$_['text_add']               = 'הוסף תכונה';
+$_['text_edit']              = 'ערוך תכונה';
+
+// Column
+$_['column_name']            = 'שם תכונה';
+$_['column_attribute_group'] = 'קבוצת תכונות';
+$_['column_sort_order']      = 'סדר מיון';
+$_['column_action']          = 'פעולה';
+
+// Entry
+$_['entry_name']             = 'שם תכונה';
+$_['entry_attribute_group']  = 'קבוצת תכונות';
+$_['entry_sort_order']       = 'סדר מיון';
+
+// Error
+$_['error_warning']          = 'אזהרה: אנא בדוק בקפידה אם יש שגיאות בטופס!';
+$_['error_permission']       = 'אזהרה: אין לך הרשאה לשנות תכונות!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'תכונה חייבת להכיל בין 3 לבין 64 תווים!';
+$_['error_product']          = 'אזהרה: לא ניתן למחוק תכונה זו היות והיא יועדה ל %s מוצרים!';
