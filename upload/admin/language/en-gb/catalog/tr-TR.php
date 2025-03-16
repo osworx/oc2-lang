65964be58d75a272764f858a2173655bb715d3bf
OSWorX <info@osworx.net>
2025-03-16 14:21:49 +0100

    New translations option.php (Turkish)

--- upload/admin/language/en-gb/catalog/tr-TR.php
+++ upload/admin/language/en-gb/catalog/tr-TR.php
@@ -1,35 +1,40 @@
 <?php
 // Heading
-$_['heading_title']     = 'Üreticiler';
+$_['heading_title']      = 'Seçenekler';
 
 // Text
-$_['text_success']      = 'Başarılı: Üreticileri değiştirdiniz!';
-$_['text_list']         = 'Üretici listesi';
-$_['text_add']          = 'Üretici Ekle';
-$_['text_edit']         = 'Üretici düzenle';
-$_['text_default']      = 'Varsayılan';
-$_['text_percent']      = 'Yüzde';
-$_['text_amount']       = 'Sabit tutar';
+$_['text_success']       = 'Başarılı: Seçenekleri değiştirdiniz!';
+$_['text_list']          = 'Seçenek Listesi';
+$_['text_add']           = 'Seçenek Ekle';
+$_['text_edit']          = 'Seçenek Bilgilerini Değiştir';
+$_['text_choose']        = 'Seçin';
+$_['text_select']        = 'Seçin';
+$_['text_radio']         = 'Radyo';
+$_['text_checkbox']      = 'Onay Kutusu';
+$_['text_input']         = 'Girdi';
+$_['text_text']          = 'Yazı';
+$_['text_textarea']      = 'Yazı Alanı';
+$_['text_file']          = 'Dosya';
+$_['text_date']          = 'Tarih';
+$_['text_datetime']      = 'Tarih ve Saat';
+$_['text_time']          = 'Zaman';
 
 // Column
-$_['column_name']       = 'Üretici adı';
-$_['column_sort_order'] = 'Sıralama Düzeni';
-$_['column_action']     = 'Eylem';
+$_['column_name']        = 'Seçenek Adı';
+$_['column_sort_order']  = 'Sıralama Düzeni';
+$_['column_action']      = 'Eylem';
 
 // Entry
-$_['entry_name']        = 'Üretici adı';
-$_['entry_store']       = 'Dükkanlar';
-$_['entry_keyword']     = 'SEO URL';
-$_['entry_image']       = 'Resim';
-$_['entry_sort_order']  = 'Sıralama Düzeni';
-$_['entry_type']        = 'Tür';
-
-// Help
-$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['entry_name']         = 'Seçenek Adı';
+$_['entry_type']         = 'Tür';
+$_['entry_option_value'] = 'Seçenek Değer Adı';
+$_['entry_image']        = 'Resim';
+$_['entry_sort_order']   = 'Sıralama Düzeni';
 
 // Error
-$_['error_warning']     = 'Uyarı: Lütfen formu gösterilen hatalar nedeniyle dikkatlice kontrol edin!';
-$_['error_permission']  = 'Uyarı: Üreticileri değiştirme iznine sahip değilsiniz!';
-$_['error_name']        = 'Üretici adı 2 ile 64 karakter arasında olmalıdır!';
-$_['error_keyword']     = 'SEO URL already in use!';
-$_['error_product']     = 'Uyarı: Bu üretici %s ürüne atandığı için silinemez!';
+$_['error_warning']      = 'Uyarı: Lütfen formu gösterilen hatalar nedeniyle dikkatlice kontrol edin!';
+$_['error_permission']   = 'Uyarı: Seçenekleri değiştirme yetkiniz yok!';
+$_['error_name']         = 'Seçenek Adı 1 ile 128 karakter arasında olmalıdır!';
+$_['error_type']         = 'Uyarı: Seçenek Değerleri gereklidir!';
+$_['error_option_value'] = 'Seçenek Değer Adı 1 ile 128 karakter arasında olmalıdır!';
+$_['error_product']      = 'Uyarı: Bu seçeneğe bağlı %s ürün bulunduğu sürece silinemez!';
