85ef8e4c680cbdb7eecfaae05605de504c795fcb
OSWorX <info@osworx.net>
2025-03-16 11:09:01 +0100

    New translations filter.php (Estonian)

--- upload/admin/language/en-gb/catalog/et-EE.php
+++ upload/admin/language/en-gb/catalog/et-EE.php
@@ -1,35 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = 'Allalaadimised';
+$_['heading_title']     = 'Filtrid';
 
 // Text
-$_['text_success']      = 'Oled edukalt muutnud allalaadimisi!';
-$_['text_list']         = 'Allalaadimiste nimekiri';
-$_['text_add']          = 'Lisa allalaadimine';
-$_['text_edit']         = 'Muuda allalaadimist';
-$_['text_upload']       = 'Sinu fail on edukalt üles laaditud!';
+$_['text_success']      = 'Oled edukalt muutnud filtreid!';
+$_['text_list']         = 'Flitrite nimekirjad';
+$_['text_add']          = 'Lisa filter';
+$_['text_edit']         = 'Muuda filtrit';
 
 // Column
-$_['column_name']       = 'Allalaadimise nimi';
-$_['column_date_added'] = 'Lisamise kuupäev';
+$_['column_group']      = 'Filtri grupp';
+$_['column_sort_order'] = 'Sorteerimis järjekord';
 $_['column_action']     = 'Tegevus';
 
 // Entry
-$_['entry_name']        = 'Allalaadimise nimi';
-$_['entry_filename']    = 'Failinimi';
-$_['entry_mask']        = 'Maskeering';
-
-// Help
-$_['help_filename']     = 'Sa võid üles laadida läbi nuppu või kasutada FTP programmi, ning laadida üles vastavasse kausta ja sisestada andmed alla.';
-$_['help_mask']         = 'On soovitatav, et failinimi ja maskeering oleksid erinevad, et takistaks inimestel otsest ligipääsu teie allalaadimistele.';
+$_['entry_group']       = 'Filtri gruppi nimi';
+$_['entry_name']        = 'Filtri nimi';
+$_['entry_sort_order']  = 'Sorteerimis järjekord';
 
 // Error
 $_['error_warning']     = 'Hoiatus: Palun kontrollige vormi vead hoolikalt üle!';
-$_['error_permission']  = 'Hoiatus: Sul ei ole õigusi, et muuta allalaadimisi!';
-$_['error_name']        = 'Allalaadimise nime pikkus peab olema vahemikus 3 kuni 64 märki!';
-$_['error_upload']      = 'Üleslaadimine vajalik!';
-$_['error_filename']    = 'Failinimi peab olema 3 kuni 128 tähemärki pikk!';
-$_['error_exists']      = 'Faili ei eksiteeri!';
-$_['error_mask']        = 'Maskeering peab jääma 3 ja 128 tähemärgi vahemikku!';
-$_['error_filetype']    = 'Faili tüüp pole toetatud!';
-$_['error_product']     = 'Hoiatus: Seda allalaadimist ei saa kustutada, sest see on hetkel määratud tootele %s!';
+$_['error_permission']  = 'Hoiatus: Sul ei ole õigusi muuta filtreid!';
+$_['error_group']       = 'Filtri grupi nimi peab olema vahemikus 1 kuni 64 tähemärki pikk!';
+$_['error_name']        = 'Filtri nimi peab olema 1 kuni 64 tähemärki!';
