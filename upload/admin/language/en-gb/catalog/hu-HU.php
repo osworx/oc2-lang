d284beb4ca3682b5a62f56e790aeeef6a206ca49
OSWorX <info@osworx.net>
2025-03-16 14:15:57 +0100

    New translations attribute.php (Hungarian)

--- upload/admin/language/en-gb/catalog/hu-HU.php
+++ upload/admin/language/en-gb/catalog/hu-HU.php
@@ -1,36 +1,27 @@
 <?php
 // Heading
-$_['heading_title']     = 'Vélemények';
+$_['heading_title']          = 'Jellemzők';
 
 // Text
-$_['text_success']      = 'Sikeresen módosította a véleményt!';
-$_['text_list']         = 'Vélemények';
-$_['text_add']          = 'Vélemény írása';
-$_['text_edit']         = 'Vélemény javítása';
+$_['text_success']           = 'jellemzők módosítása sikeres!';
+$_['text_list']              = 'Jellemzőlista';
+$_['text_add']               = 'Jellemző hozzáadása';
+$_['text_edit']              = 'Jellemző szerkesztése';
 
 // Column
-$_['column_product']    = 'Termék';
-$_['column_author']     = 'Szerző';
-$_['column_rating']     = 'Értékelés';
-$_['column_status']     = 'Állapot';
-$_['column_date_added'] = 'Hozzáadás dátuma';
-$_['column_action']     = 'Művelet';
+$_['column_name']            = 'Jellemző neve';
+$_['column_attribute_group'] = 'Jellemzőcsoport';
+$_['column_sort_order']      = 'Rendezés';
+$_['column_action']          = 'Művelet';
 
 // Entry
-$_['entry_product']     = 'Termék';
-$_['entry_author']      = 'Szerző';
-$_['entry_rating']      = 'Értékelés';
-$_['entry_status']      = 'Állapot';
-$_['entry_text']        = 'Szöveg';
-$_['entry_date_added']  = 'Hozzáadás dátuma';
-
-// Help
-$_['help_product']      = '(Autó Kiegészítés)';
+$_['entry_name']             = 'Jellemző neve';
+$_['entry_attribute_group']  = 'Jellemzőcsoport';
+$_['entry_sort_order']       = 'Rendezés';
 
 // Error
-$_['error_warning']     = 'Figyelem: Kérjük, figyelmesen ellenőrizze a hibákat!';
-$_['error_permission']  = 'Figyelem: Nincs jogosultsága a módosításra!';
-$_['error_product']     = 'Szükséges terméket megadni!';
-$_['error_author']      = 'Szerző nevének 3 és 64 karakter között kell lennie!';
-$_['error_text']        = 'A szövegnek legalább 1 karakter hosszúnak kell lennie!';
-$_['error_rating']      = 'Értékelés is szükséges!';
+$_['error_warning']          = 'Figyelem: Kérjük, figyelmesen ellenőrizze a hibákat!';
+$_['error_permission']       = 'Figyelem: Nincs jogosultságod a jellemzők módosítására!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'A jellemző nevének 3 és 64 karakter között kell lennie!';
+$_['error_product']          = 'Figyelem: Ez a jellemző nem törölhető, mivel jelenleg %s termékhez van hozzárendelve!';
