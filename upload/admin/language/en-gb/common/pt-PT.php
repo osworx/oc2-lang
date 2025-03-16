fe101ab4063155c47f2a6d6ccfca31da52311d05
OSWorX <info@osworx.net>
2025-03-16 14:28:38 +0100

    New translations forgotten.php (Portuguese)

--- upload/admin/language/en-gb/common/pt-PT.php
+++ upload/admin/language/en-gb/common/pt-PT.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Esqueceu sua Palavra-Chave?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Versão %s';
+$_['text_forgotten']  = 'Palavra-chave Esquecida';
+$_['text_your_email'] = 'O seu E-mail';
+$_['text_email']      = 'Coloque o endereço de E-mail com que registou à sua conta. Clique em enviar para receber um email com um link para redefinir a sua Palavra-Chave.';
+$_['text_success']    = 'Um email com o link de confirmação foi enviado para o seu Email.';
+
+// Entry
+$_['entry_email']     = 'E-mail';
+$_['entry_password']  = 'Nova Palavra-Chave';
+$_['entry_confirm']   = 'Confirmar';
+
+// Error
+$_['error_email']     = 'Aviso: O endereço de E-mail não foi encontrado na nossa base de dados. Por favor, tente novamente!';
+$_['error_password']  = 'A palavra-passe deve conter entre 4 a 20 carateres!';
+$_['error_confirm']   = 'A Palavra-Chave e a confirmação da Palavra-Chave não são iguais!';
