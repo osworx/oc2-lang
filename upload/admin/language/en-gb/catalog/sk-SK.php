376f81c12bbc58e25f633cd7a7e2d9c2efb7d65e
OSWorX <info@osworx.net>
2025-03-16 14:19:21 +0100

    New translations filter.php (Slovak)

--- upload/admin/language/en-gb/catalog/sk-SK.php
+++ upload/admin/language/en-gb/catalog/sk-SK.php
@@ -1,35 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = 'Stiahnutia';
+$_['heading_title']     = 'Filtre';
 
 // Text
-$_['text_success']      = 'Úspech: Zmenili ste stiahnutia!';
-$_['text_list']         = 'Zoznam stiahnutí';
-$_['text_add']          = 'Pridaj stiahnutie';
-$_['text_edit']         = 'Upraviť stiahnutie';
-$_['text_upload']       = 'Váš súbor bol úspešne nahraný!';
+$_['text_success']      = 'Úspech: Zmenili ste filtre!';
+$_['text_list']         = 'Zoznam filtrov';
+$_['text_add']          = 'Pridaj filter';
+$_['text_edit']         = 'Upraviť filter';
 
 // Column
-$_['column_name']       = 'Meno stiahnutia';
-$_['column_date_added'] = 'Dátum pridaný';
+$_['column_group']      = 'Skupina filtrov';
+$_['column_sort_order'] = 'Poradie';
 $_['column_action']     = 'Akcia';
 
 // Entry
-$_['entry_name']        = 'Meno stiahnutia';
-$_['entry_filename']    = 'Meno súboru';
-$_['entry_mask']        = 'Maska';
-
-// Help
-$_['help_filename']     = 'Možete nahrať pomocou tlačítka nahrať alebo použiť FTP k nahraniu stiahnutého rozcestníka a vložte následovné údaje.';
-$_['help_mask']         = 'Odporúča sa, aby názov súboru a maska boli odlišné, aby zastavilo ľudí pokúšajúcich sa priamo spojiť s vašimi stianutiami.';
+$_['entry_group']       = 'Názov skupiny filtrov';
+$_['entry_name']        = 'Názov filtru';
+$_['entry_sort_order']  = 'Poradie';
 
 // Error
 $_['error_warning']     = 'Varovanie: Skontrolujte formulár starostlivo kvôli chybám!';
-$_['error_permission']  = 'Upozornenie: Nemáte oprávnenie k zmene sťahovania!';
-$_['error_name']        = 'Názov sťahovania musí mať medzi 3 a 64 znakov!';
-$_['error_upload']      = 'Nahranie nutné!';
-$_['error_filename']    = 'Meno súboru musí mať medzi 3 a 128 znakov!';
-$_['error_exists']      = 'Súbor neexistuje!';
-$_['error_mask']        = 'Maska musí mať medzi 3 a 128 znakov!';
-$_['error_filetype']    = 'Neplatný typ súboru!';
-$_['error_product']     = 'Upozornenie: Toto sťahovanie nemôže byť vymazané, pretože je teraz priradené k %s produktom!';
+$_['error_permission']  = 'Upozornenie: Nemáte povolenie k zmene filtrov!';
+$_['error_group']       = 'Názov skupiny filtrov musí mať medzi 1 a 64 znakov!';
+$_['error_name']        = 'Názov filtru musí mať medzi 1 a 64 znakov!';
