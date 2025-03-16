1101ca76e92b166f695513873f461459a9008751
OSWorX <info@osworx.net>
2025-03-16 14:16:02 +0100

    New translations attribute.php (Korean)

--- upload/admin/language/en-gb/catalog/ko-KR.php
+++ upload/admin/language/en-gb/catalog/ko-KR.php
@@ -1,36 +1,27 @@
 <?php
 // Heading
-$_['heading_title']     = '상품평';
+$_['heading_title']          = '속성';
 
 // Text
-$_['text_success']      = '완료: 상품평을 수정했습니다.';
-$_['text_list']         = '상품평 목록';
-$_['text_add']          = '상품평 추가';
-$_['text_edit']         = '상품평 수정';
+$_['text_success']           = '완료: 속성을 수정했습니다.';
+$_['text_list']              = '속성 목록';
+$_['text_add']               = '속성 추가';
+$_['text_edit']              = '속성 수정';
 
 // Column
-$_['column_product']    = '상품';
-$_['column_author']     = '작성자';
-$_['column_rating']     = '평점';
-$_['column_status']     = '상태';
-$_['column_date_added'] = '작성일';
-$_['column_action']     = '작업';
+$_['column_name']            = '속성이름';
+$_['column_attribute_group'] = '속성그룹';
+$_['column_sort_order']      = '정렬순서';
+$_['column_action']          = '작업';
 
 // Entry
-$_['entry_product']     = '상품';
-$_['entry_author']      = '작성자';
-$_['entry_rating']      = '평점';
-$_['entry_status']      = '상태';
-$_['entry_text']        = 'Text';
-$_['entry_date_added']  = '작성일';
-
-// Help
-$_['help_product']      = '(자동완성)';
+$_['entry_name']             = '속성이름';
+$_['entry_attribute_group']  = '속성그룹';
+$_['entry_sort_order']       = '정렬순서';
 
 // Error
-$_['error_warning']     = '주의: 오류가 발생했습니다. 입력 항목을 다시 확인해주세요.';
-$_['error_permission']  = '주의: 상품평을 수정할 수 있는 권한이 없습니다.';
-$_['error_product']     = '상품은 필수입니다.';
-$_['error_author']      = '작성자는 3~64 글자 이내로 입력해주세요.';
-$_['error_text']        = '상품평 내용은 적어도 한 글자 이상 입력해주세요.';
-$_['error_rating']      = '상품평 평점은 필수입니다.';
+$_['error_warning']          = '주의: 오류가 발생했습니다. 입력 항목을 다시 확인해주세요.';
+$_['error_permission']       = '주의: 속성을 수정할 수 있는 권한이 없습니다.';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = '속성이름은 3~64 글자 이내로 입력해주세요.';
+$_['error_product']          = '주의: 현재 %s개의 상품에 이 속성이 설정되어 있으므로 삭제할 수 없습니다.';
