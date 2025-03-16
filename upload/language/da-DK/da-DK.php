093296a0ee5e79c2d6ec5bc86ef3b411a98aad01
OSWorX <info@osworx.net>
2025-03-16 09:08:07 +0100

    New translations category.php (Danish)

--- upload/language/da-DK/da-DK.php
+++ upload/language/da-DK/da-DK.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Egenskabsgrupper';
+$_['heading_title']          = 'Kategorier';
 
 // Text
-$_['text_success']      = 'Succes: Du har ændre egenskabsgrupper!';
-$_['text_list']         = 'Egenskabsgrupper';
-$_['text_add']          = 'Tilføj egenskabsgruppe';
-$_['text_edit']         = 'Rediger egenskabsgruppe';
+$_['text_success']           = 'Succes! Du har ændret kategorier!';
+$_['text_list']              = 'Kategorier';
+$_['text_add']               = 'Tilføj kategori';
+$_['text_edit']              = 'Rediger kategori';
+$_['text_default']           = 'Standard';
 
 // Column
-$_['column_name']       = 'Egenskabsgruppe navn';
-$_['column_sort_order'] = 'Sorteringsrækkefølge';
-$_['column_action']     = 'Handling';
+$_['column_name']            = 'Kategorinavn';
+$_['column_sort_order']      = 'Sorteringsrækkefølge';
+$_['column_action']          = 'Handling';
 
 // Entry
-$_['entry_name']        = 'Egenskabsgruppe navn';
-$_['entry_sort_order']  = 'Sorteringsrækkefølge';
+$_['entry_name']             = 'Kategorinavn';
+$_['entry_description']      = 'Beskrivelse';
+$_['entry_meta_title'] 	     = 'Meta tag titel';
+$_['entry_meta_keyword']     = 'Meta tag nøgleord';
+$_['entry_meta_description'] = 'Meta tag Beskrivelse';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Overordnet';
+$_['entry_filter']           = 'Filtre';
+$_['entry_store']            = 'Butikker';
+$_['entry_image']            = 'Billede';
+$_['entry_top']              = 'Top';
+$_['entry_column']           = 'Kolonner';
+$_['entry_sort_order']       = 'Sorteringsrækkefølge';
+$_['entry_status']           = 'Status';
+$_['entry_layout']           = 'Andet layout';
+
+// Help
+$_['help_filter']            = '(Autofuldførelse)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Vis i topmenu-bjælken. Virker kun for første niveau overordnede kategorier.';
+$_['help_column']            = 'Antal kolonner, der skal bruges for 3 underordnede kategorier. Virker kun for øverste niveau overordnede kategorier.';
 
 // Error
-$_['error_warning']     = 'Advarsel: Gennemgå venligst formularen for fejl!';
-$_['error_permission']  = 'Advarsel: Du har ikke tilladelse til at ændre egenskabsgrupper!';
-$_['error_name']        = 'Egenskabsgruppe navn skal være mellem 3 og 64 karakterer!';
-$_['error_attribute']   = 'Advarsel: Denne egenskabsgruppe kan ikke slettes, da den i øjeblikket er tilknyttet %s egenskaber!';
-$_['error_product']     = 'Advarsel: Denne egenskabsgruppe kan ikke slettes, da den i øjeblikket er tilknyttet %s produkter!';
+$_['error_warning']          = 'Advarsel: Gennemgå venligst formularen for fejl!';
+$_['error_permission']       = 'Fejl: Du har ikke tilladelse til at ændre kategorier!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Metatitel skal være mellem 3 og 255 karakterer!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
