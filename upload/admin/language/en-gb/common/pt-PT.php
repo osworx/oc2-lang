9beb941adee4a79e5b394a3c9dc5652cb00675d3
OSWorX <info@osworx.net>
2025-03-16 14:30:14 +0100

    New translations login.php (Portuguese)

--- upload/admin/language/en-gb/common/pt-PT.php
+++ upload/admin/language/en-gb/common/pt-PT.php
@@ -1,23 +1,19 @@
 <?php
-// Heading
-$_['heading_title']          = 'OpenCart';
+// header
+$_['heading_title']  = 'Administração';
 
 // Text
-$_['text_order']             = 'Encomendas';
-$_['text_processing_status'] = 'Processing';
-$_['text_complete_status']   = 'Concluída';
-$_['text_customer']          = 'Clientes';
-$_['text_online']            = 'Clientes Online';
-$_['text_approval']          = 'Pendente de Aprovação';
-$_['text_product']           = 'Produtos';
-$_['text_stock']             = 'Sem Stock';
-$_['text_review']            = 'Comentários';
-$_['text_return']            = 'Devoluções';
-$_['text_affiliate']         = 'Afiliados';
-$_['text_store']             = 'Lojas';
-$_['text_front']             = 'Frente de Loja';
-$_['text_help']              = 'Ajuda';
-$_['text_homepage']          = 'OpenCart Homepage';
-$_['text_support']           = 'Fórum de Suporte';
-$_['text_documentation']     = 'Documentação';
-$_['text_logout']            = 'Saír';
+$_['text_heading']   = 'Administração';
+$_['text_login']     = 'Por favor, preencha os seus dados de login.';
+$_['text_forgotten'] = 'Palavra-chave Esquecida';
+
+// Entry
+$_['entry_username'] = 'Nome de Utilizador';
+$_['entry_password'] = 'Palavra-Chave';
+
+// Button
+$_['button_login']   = 'Iníciar Sessão';
+
+// Error
+$_['error_login']    = 'O Utilizador e/ou Palavra-Chave não estão corretos.';
+$_['error_token']    = 'Token de sessão inválido. Inicie Sessão novamente.';
