d7d62d0b78f2c9a907b3fb2c0a6a7b4ce1fa9869
OSWorX <info@osworx.net>
2025-03-16 09:08:35 +0100

    New translations forgotten.php (German)

--- upload/language/de-DE/de-DE.php
+++ upload/language/de-DE/de-DE.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Passwort vergessen?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Version %s';
+$_['text_forgotten']  = 'Passwort vergessen';
+$_['text_your_email'] = 'Emailadresse';
+$_['text_email']      = '';
+$_['text_success']    = 'Email wurde an die Adresse gesendet';
+
+// Entry
+$_['entry_email']     = 'Emailadresse';
+$_['entry_password']  = 'Neues Passwort';
+$_['entry_confirm']   = '';
+
+// Error
+$_['error_email']     = 'Hinweis: die angegebene Emailadresse ist unbekannt, bitte nochmals eingeben';
+$_['error_password']  = 'Passwort muss zwischen 4 und 20 Zeichen lang sein';
+$_['error_confirm']   = '';
