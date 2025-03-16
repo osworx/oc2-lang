4cd6eaa1065b85db09ccc806fd2068fef54a0176
OSWorX <info@osworx.net>
2025-03-16 14:17:59 +0100

    New translations category.php (Thai)

--- upload/admin/language/en-gb/catalog/th-TH.php
+++ upload/admin/language/en-gb/catalog/th-TH.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'กลุ่มคุณลักษณะ';
+$_['heading_title']          = 'หมวดหมู่สินค้า';
 
 // Text
-$_['text_success']      = 'ความสำเร็จ: คุณได้ปรับเปลี่ยนคุณลักษณะของกลุ่ม';
-$_['text_list']         = 'รายการกลุ่มคุณลักษณะ';
-$_['text_add']          = 'เพิ่มกลุ่มคุณลักษณะ';
-$_['text_edit']         = 'แก้ไขกลุ่มคุณลักษณะ';
+$_['text_success']           = 'คุณได้ปรับเปลี่ยนหมวดหมู่สินค้าเรียบร้อยแล้ว';
+$_['text_list']              = 'รายชื่อหมวดหมู่สินค้า';
+$_['text_add']               = 'เพิ่มหมวดหมู่';
+$_['text_edit']              = 'แก้ไขหมวดหมู่สินค้า';
+$_['text_default']           = 'ค่าเริ่มต้น';
 
 // Column
-$_['column_name']       = 'ชื่อกลุ่มคุณลักษณะ';
-$_['column_sort_order'] = 'ลำดับการจัดเรียง';
-$_['column_action']     = 'การดำเนินการ';
+$_['column_name']            = 'ชื่อหมวดหมู่';
+$_['column_sort_order']      = 'ลำดับการจัดเรียง';
+$_['column_action']          = 'การดำเนินการ';
 
 // Entry
-$_['entry_name']        = 'ชื่อกลุ่มคุณลักษณะ';
-$_['entry_sort_order']  = 'ลำดับการจัดเรียง';
+$_['entry_name']             = 'ชื่อหมวดหมู่';
+$_['entry_description']      = 'คำอธิบาย';
+$_['entry_meta_title'] 	     = 'ชื่อแท็ก Meta';
+$_['entry_meta_keyword']     = 'คำเรียก Tag Meta';
+$_['entry_meta_description'] = 'คำอธิบาย Tag Meta';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'หมวดแม่';
+$_['entry_filter']           = 'ตัวกรอง';
+$_['entry_store']            = 'ร้านค้า';
+$_['entry_image']            = 'รูปภาพ';
+$_['entry_top']              = 'ด้านบน';
+$_['entry_column']           = 'คอลัมน์';
+$_['entry_sort_order']       = 'ลำดับการจัดเรียง';
+$_['entry_status']           = 'สถานะ';
+$_['entry_layout']           = 'Layout Override';
+
+// Help
+$_['help_filter']            = '(อัตโนมัติ)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'แสดงในแถบเมนูด้านบน เฉพาะประเภทหลักด้านบน';
+$_['help_column']            = 'จำนวนคอลัมน์ที่จะใช้สำหรับประเภทด้านล่าง 3 เฉพาะประเภทหลักด้านบน';
 
 // Error
-$_['error_warning']     = 'คำเตือน: กรุณาตรวจสอบแบบฟอร์มอย่างรอบคอบสำหรับข้อผิดพลาด!';
-$_['error_permission']  = 'คำเตือน: คุณไม่มีสิทธิ์ในการปรับเปลี่ยนคุณลักษณะ!';
-$_['error_name']        = 'ชื่อกลุ่มคุณลักษณะต้องอยู่ระหว่าง 3 ถึง 64 อักษร';
-$_['error_attribute']   = 'คำเตือน: ไม่สามารถลบกลุ่มคุณลักษณะนี้เนื่องจากมีคุณลักษณะ %s ใช้อยู่!';
-$_['error_product']     = 'คำเตือน: ไม่สามารถลบกลุ่มคุณลักษณะนี้เนื่องจากมีผลิตภัณฑ์ %s ใช้อยู่!';
+$_['error_warning']          = 'คำเตือน: กรุณาตรวจสอบแบบฟอร์มอย่างรอบคอบสำหรับข้อผิดพลาด!';
+$_['error_permission']       = 'คำเตือน: คุณไม่มีสิทธิ์ในการแก้ไขหมวดหมู่';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'ชื่อ Meta ต้องมากกว่า 3 และน้อยกว่า 255 อักษร!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
