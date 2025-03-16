080a269de58db8a213a35e7f1d9ba0dc1150d522
OSWorX <info@osworx.net>
2025-03-16 09:11:01 +0100

    New translations review.php (Korean)

--- upload/language/ko-KR/ko-KR.php
+++ upload/language/ko-KR/ko-KR.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= '할부방식';
+$_['heading_title']     = '상품평';
 
 // Text
-$_['text_success']          = '완료: 할부방식을 수정했습니다.';
-$_['text_list']             = '할부방식 목록';
-$_['text_add']              = '할부방식 추가';
-$_['text_edit']             = '할부방식 수정';
-$_['text_day']				= '일';
-$_['text_week']				= '주';
-$_['text_semi_month']		= '보름';
-$_['text_month']			= '월';
-$_['text_year']				= '년';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> 할부 금액은 주기와 주기단위를 이용해서 계산합니다.</p><p>예를 들어 주기가 "2"이고 주기단위가 "주"인 경우, 고객에게 2주마다 한 번씩 청구합니다.</p><p>할부횟수는 고객이 결제해야 하는 총 횟수입니다. 만약 취소할 때까지 계속 반복 결제되도록 하려면 할부횟수를 0으로 설정합니다.</p>';
-$_['text_profile']			= '할부방식';
-$_['text_trial']			= '상품체험';
-
-// Entry
-$_['entry_name']		    = '이름';
-$_['entry_price']			= '판매가';
-$_['entry_duration']		= '할부횟수';
-$_['entry_cycle']			= '주기';
-$_['entry_frequency']		= '주기단위';
-$_['entry_trial_price']		= '상품체험 회당 결제금액';
-$_['entry_trial_duration']	= '상품체험 횟수';
-$_['entry_trial_status']	= '상품체험 상태';
-$_['entry_trial_cycle']	    = '상품체험 주기';
-$_['entry_trial_frequency'] = '상품체험 주기단위';
-$_['entry_status']			= '상태';
-$_['entry_sort_order']		= '정렬순서';
+$_['text_success']      = '완료: 상품평을 수정했습니다.';
+$_['text_list']         = '상품평 목록';
+$_['text_add']          = '상품평 추가';
+$_['text_edit']         = '상품평 수정';
 
 // Column
-$_['column_name']			= '이름';
-$_['column_sort_order']	    = '정렬순서';
-$_['column_action']         = '작업';
+$_['column_product']    = '상품';
+$_['column_author']     = '작성자';
+$_['column_rating']     = '평점';
+$_['column_status']     = '상태';
+$_['column_date_added'] = '작성일';
+$_['column_action']     = '작업';
+
+// Entry
+$_['entry_product']     = '상품';
+$_['entry_author']      = '작성자';
+$_['entry_rating']      = '평점';
+$_['entry_status']      = '상태';
+$_['entry_text']        = 'Text';
+$_['entry_date_added']  = '작성일';
+
+// Help
+$_['help_product']      = '(자동완성)';
 
 // Error
-$_['error_warning']         = '주의: 오류가 발생했습니다. 입력 항목을 다시 확인해주세요.';
-$_['error_permission']		= '주의: 할부방식을 수정할 수 있는 권한이 없습니다.';
-$_['error_name']			= '할부방식 이름은 3~255 글자 이내로 입력해주세요.';
-$_['error_product']			= '주의: 현재 %s개의 상품에 이 할부방식이 설정되어 있으므로 삭제할 수 없습니다.';
+$_['error_warning']     = '주의: 오류가 발생했습니다. 입력 항목을 다시 확인해주세요.';
+$_['error_permission']  = '주의: 상품평을 수정할 수 있는 권한이 없습니다.';
+$_['error_product']     = '상품은 필수입니다.';
+$_['error_author']      = '작성자는 3~64 글자 이내로 입력해주세요.';
+$_['error_text']        = '상품평 내용은 적어도 한 글자 이상 입력해주세요.';
+$_['error_rating']      = '상품평 평점은 필수입니다.';
