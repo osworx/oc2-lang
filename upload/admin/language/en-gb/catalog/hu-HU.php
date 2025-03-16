f4338cdaa9d609e23994247abc47efb7c7200f70
OSWorX <info@osworx.net>
2025-03-16 11:05:00 +0100

    New translations attribute.php (Hungarian)

---
+++ upload/admin/language/en-gb/catalog/hu-HU.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Jellemzők';
+
+// Text
+$_['text_success']           = 'jellemzők módosítása sikeres!';
+$_['text_list']              = 'Jellemzőlista';
+$_['text_add']               = 'Jellemző hozzáadása';
+$_['text_edit']              = 'Jellemző szerkesztése';
+
+// Column
+$_['column_name']            = 'Jellemző neve';
+$_['column_attribute_group'] = 'Jellemzőcsoport';
+$_['column_sort_order']      = 'Rendezés';
+$_['column_action']          = 'Művelet';
+
+// Entry
+$_['entry_name']             = 'Jellemző neve';
+$_['entry_attribute_group']  = 'Jellemzőcsoport';
+$_['entry_sort_order']       = 'Rendezés';
+
+// Error
+$_['error_warning']          = 'Figyelem: Kérjük, figyelmesen ellenőrizze a hibákat!';
+$_['error_permission']       = 'Figyelem: Nincs jogosultságod a jellemzők módosítására!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'A jellemző nevének 3 és 64 karakter között kell lennie!';
+$_['error_product']          = 'Figyelem: Ez a jellemző nem törölhető, mivel jelenleg %s termékhez van hozzárendelve!';
