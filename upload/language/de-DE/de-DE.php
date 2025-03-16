a4ecd49ec184387bdd1142f67a1a8dc0d50769f8
OSWorX <info@osworx.net>
2025-03-16 09:08:33 +0100

    New translations filemanager.php (German)

--- upload/language/de-DE/de-DE.php
+++ upload/language/de-DE/de-DE.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Übersicht';
+$_['heading_title']    = 'Bildverwaltung';
+
+// Text
+$_['text_uploaded']    = 'Datei erfolgreich hochgeladen';
+$_['text_directory']   = 'Ordner erfolgreich erstellt';
+$_['text_delete']      = '';
+
+// Entry
+$_['entry_search']     = 'Suchen ..';
+$_['entry_folder']     = 'Ordnername';
 
 // Error
-$_['error_install'] = 'Hinweis: der Installationsordner ist noch vorhanden!<br />Aus Sicherheitsgründen entweder löschen oder umbenennen und Rechte (auf 0644) anpassen.';
+$_['error_permission'] = '';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Achtung: es ist bereits eine Datei oder ein Ordner mit diesem Namen vorhanden';
+$_['error_directory']  = 'Achtung: Ordner ist nicht vorhanden';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Achtung: falsche Dateiart';
+$_['error_upload']     = 'Achtung: Datei konnt aus unbekannten Grund nicht hochgeladen werden';
+$_['error_delete']     = '';
