56d41811f4dffcc8eff0061c127464750f426059
OSWorX <info@osworx.net>
2025-03-16 14:17:40 +0100

    New translations category.php (Dutch)

--- upload/admin/language/en-gb/catalog/nl-NL.php
+++ upload/admin/language/en-gb/catalog/nl-NL.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Attribuut groepen';
+$_['heading_title']          = 'Categorie&euml;n';
 
 // Text
-$_['text_success']      = 'Succes: Instellingen gewijzigd!';
-$_['text_list']         = 'Lijst';
-$_['text_add']          = 'Toevoegen';
-$_['text_edit']         = 'Wijzigen';
+$_['text_success']           = 'Succes: Instellingen gewijzigd!';
+$_['text_list']              = 'Lijst';
+$_['text_add']               = 'Toevoegen';
+$_['text_edit']              = 'Wijzigen';
+$_['text_default']           = 'Hoofdwinkel';
 
 // Column
-$_['column_name']       = 'Attribuut groepsnaam';
-$_['column_sort_order'] = 'Sorteervolgorde';
-$_['column_action']     = 'Aktie';
+$_['column_name']            = 'Categorienaam';
+$_['column_sort_order']      = 'Sorteervolgorde';
+$_['column_action']          = 'Aktie';
 
 // Entry
-$_['entry_name']        = 'Attribuut groepsnaam';
-$_['entry_sort_order']  = 'Sorteervolgorde';
+$_['entry_name']             = 'Categorienaam';
+$_['entry_description']      = 'Beschrijving';
+$_['entry_meta_title'] 	     = 'Meta Tag Titel';
+$_['entry_meta_keyword']     = 'Meta Tag Zoekwoorden';
+$_['entry_meta_description'] = 'Meta Tag Omschrijving';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Hoofdcategorie';
+$_['entry_filter']           = 'Filters';
+$_['entry_store']            = 'Winkel(s)';
+$_['entry_image']            = 'Afbeelding';
+$_['entry_top']              = 'Naar boven';
+$_['entry_column']           = 'Kolommen';
+$_['entry_sort_order']       = 'Sorteervolgorde';
+$_['entry_status']           = 'status';
+$_['entry_layout']           = 'Layout';
+
+// Help
+$_['help_filter']            = '(AutoAanvullen)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Weergeven in de top menu bar. Alleen voor Hoofdcategorie&euml;n.';
+$_['help_column']            = 'Aantal kolommen voor de onderste 3 categorie&euml;n. Werkt alleen met Hoofdcategorie&euml;n.';
 
 // Error
-$_['error_warning']     = 'Waarschuwing: Controlleer of alle verplichte velden volledig zijn ingevuld!';
-$_['error_permission']  = 'Waarschuwing: U heeft geen rechten deze instellingen te wijzigen!';
-$_['error_name']        = 'Waarschuwing: Attribuut groepsnaam dient tussen de 3 en 64 tekens lang te zijn!';
-$_['error_attribute']   = 'Waarschuwing: Deze groep kan niet worden verwijderd omdat hij gekoppeld is aan %s attributen!';
-$_['error_product']     = 'Waarschuwing: Deze groep kan niet worden verwijderd omdat hij gekoppeld is aan %s product(en)!';
+$_['error_warning']          = 'Waarschuwing: Controlleer of alle verplichte velden volledig zijn ingevuld!';
+$_['error_permission']       = 'Waarschuwing: U heeft geen rechten deze instellingen te wijzigen!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Waarschuwing: Meta Titel dient tussen de 3 en 255 tekens lang te zijn!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
