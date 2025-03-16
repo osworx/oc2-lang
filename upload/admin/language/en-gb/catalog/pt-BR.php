03ac4df03e9a8b1b3c13dc54851e1b95bcbc804b
OSWorX <info@osworx.net>
2025-03-16 14:21:07 +0100

    New translations manufacturer.php (Portuguese, Brazilian)

--- upload/admin/language/en-gb/catalog/pt-BR.php
+++ upload/admin/language/en-gb/catalog/pt-BR.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Informação';
+$_['heading_title']     = 'Fabricantes';
 
 // Text
-$_['text_success']           = 'Sucesso: Você modificou informações!';
-$_['text_list']              = 'Lista de informações';
-$_['text_add']               = 'Adicionar informações';
-$_['text_edit']              = 'Editar informações';
-$_['text_default']           = 'Padrão';
+$_['text_success']      = 'Sucesso: Você modificou fabricantes!';
+$_['text_list']         = 'Lista de fabricante';
+$_['text_add']          = 'Adicionar fabricante';
+$_['text_edit']         = 'Editar fabricante';
+$_['text_default']      = 'Padrão';
+$_['text_percent']      = 'Porcentagem %';
+$_['text_amount']       = 'Montante fixo';
 
 // Column
-$_['column_title']           = 'Informações sobre o título';
-$_['column_sort_order']	     = 'Ordem de Classificação';
-$_['column_action']          = 'Ação';
+$_['column_name']       = 'Nome do fabricante';
+$_['column_sort_order'] = 'Ordem de Classificação';
+$_['column_action']     = 'Ação';
 
 // Entry
-$_['entry_title']            = 'Informações sobre o título';
-$_['entry_description']      = 'Descrição';
-$_['entry_store']            = 'Lojas';
-$_['entry_meta_title'] 	     = 'Titulo Meta Tag';
-$_['entry_meta_keyword'] 	 = 'Palavras-Chave Meta Tag';
-$_['entry_meta_description'] = 'Descrição Meta Tag';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'Rodapé';
-$_['entry_status']           = 'Estado';
-$_['entry_sort_order']       = 'Ordem de Classificação';
-$_['entry_layout']           = 'Substituição de layout';
+$_['entry_name']        = 'Nome do fabricante';
+$_['entry_store']       = 'Lojas';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'Imagem';
+$_['entry_sort_order']  = 'Ordem de Classificação';
+$_['entry_type']        = 'Tipo';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'Exibir no rodapé inferior.';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'Aviso: Por favor, verifique a forma cuidadosamente para evitar erros!';
-$_['error_permission']       = 'Aviso: Você não tem permissão para modificar as informações!';
-$_['error_title']            = 'Informações sobre o título deve ser entre 3 e 64 caracteres!';
-$_['error_description']      = 'Descrição deve ser mais de 3 caracteres!';
-$_['error_meta_title']       = 'O Meta Título deve ser maior que 3 e menor que 255 caracteres!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'Aviso: Esta página de informações não pode ser excluída, pois esta atualmente atribuída como termos de conta da loja!';
-$_['error_checkout']         = 'Aviso: Esta página de informações não pode ser excluída, pois esta atualmente atribuída como termos de conta da loja!';
-$_['error_affiliate']        = 'Aviso: Esta página de informações não pode ser excluída, pois esta atualmente atribuída como termos de conta da loja!';
-$_['error_return']           = 'Aviso: Esta página de informações não pode ser excluída, pois esta atualmente atribuída como termos de conta da loja!';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'Aviso: Por favor, verifique a forma cuidadosamente para evitar erros!';
+$_['error_permission']  = 'Aviso: Você não tem permissão para modificar os fabricantes!';
+$_['error_name']        = 'Nome do fabricante deve ser entre 2 e 64 caracteres!';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'Aviso: O fabricante não pode ser excluído pois esta   atualmente atribuído a produtos de %s!';
