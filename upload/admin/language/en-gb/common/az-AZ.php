e0ed30cf4963400d3bd3370ba2e4fa33a6172444
OSWorX <info@osworx.net>
2025-03-16 11:21:14 +0100

    New translations reset.php (Azerbaijani)

--- upload/admin/language/en-gb/common/az-AZ.php
+++ upload/admin/language/en-gb/common/az-AZ.php
@@ -1,19 +1,15 @@
 <?php
 // header
-$_['heading_title']  = 'Müdiriyyət';
+$_['heading_title']  = 'Şifrənin dəyişdirilməsi';
 
 // Text
-$_['text_heading']   = 'Müdiriyyət';
-$_['text_login']     = 'Giriş məlumatlarınızı daxil edin.';
-$_['text_forgotten'] = 'Unudulmuş Keçidsözü';
+$_['text_password']  = 'Enter the new password you wish to use.';
+$_['text_success']   = 'Success: Your password has been successfully updated.';
 
 // Entry
-$_['entry_username'] = 'İstifadəçi adı';
 $_['entry_password'] = 'Şifrə';
-
-// Button
-$_['button_login']   = 'Giriş';
+$_['entry_confirm']  = 'Doğrula';
 
 // Error
-$_['error_login']    = 'No match for Username and/or Password.';
-$_['error_token']    = 'Səhv token sessiyası. Yenidən Giriş edin.';
+$_['error_password'] = 'Password must be between 4 and 20 characters!';
+$_['error_confirm']  = 'Password and password confirmation do not match!';
