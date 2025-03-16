9b990666f574545fc5b616e8120818ddee9a3ba1
OSWorX <info@osworx.net>
2025-03-16 11:04:54 +0100

    New translations attribute.php (Danish)

---
+++ upload/admin/language/en-gb/catalog/da-DK.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Egenskaber';
+
+// Text
+$_['text_success']           = 'Succes! Du har ændret egenskaber!';
+$_['text_list']              = 'Egenskaber';
+$_['text_add']               = 'Tilføj egenskab';
+$_['text_edit']              = 'Rediger egenskab';
+
+// Column
+$_['column_name']            = 'Egenskab navn';
+$_['column_attribute_group'] = 'Egenskab gruppe';
+$_['column_sort_order']      = 'Sorteringsrækkefølge';
+$_['column_action']          = 'Handling';
+
+// Entry
+$_['entry_name']             = 'Egenskab navn';
+$_['entry_attribute_group']  = 'Egenskab gruppe';
+$_['entry_sort_order']       = 'Sorteringsrækkefølge';
+
+// Error
+$_['error_warning']          = 'Advarsel: Gennemgå venligst formularen for fejl!';
+$_['error_permission']       = 'Fejl! Du har ikke tilladelse til at ændre egenskaber!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Egenskabsnavn skal være mellem 3 og 64 karakterer!';
+$_['error_product']          = 'Fejl! Denne egenskab kan ikke slettes, da den i øjeblikket er tilknyttet %s produkter!';
