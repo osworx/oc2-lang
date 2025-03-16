7c0e3ddf7eb491f64563ee558f79d118b04cb518
OSWorX <info@osworx.net>
2025-03-16 14:23:02 +0100

    New translations recurring.php (Belarusian)

--- upload/admin/language/en-gb/catalog/be-BY.php
+++ upload/admin/language/en-gb/catalog/be-BY.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'Тавары';
+$_['heading_title']			= 'Перыядычныя плацяжы';
 
 // Text
-$_['text_success']           = 'Поспех: Тавары змененыя!';
-$_['text_list']              = 'Спіс тавараў';
-$_['text_add']               = 'Дадаць тавар';
-$_['text_edit']              = 'Рэдагаваць тавар';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'Па-змаўчанню';
-$_['text_option']            = 'Опцыя';
-$_['text_option_value']      = 'Значэнне';
-$_['text_percent']           = 'Адсотак';
-$_['text_amount']            = 'Фіксіраваная колькасць';
-
-// Column
-$_['column_name']            = 'Назва тавара';
-$_['column_model']           = 'Мадэль';
-$_['column_image']           = 'Выява';
-$_['column_price']           = 'Цана';
-$_['column_quantity']        = 'Колькасць';
-$_['column_status']          = 'Статус';
-$_['column_action']          = 'Дзеянне';
+$_['text_success']          = 'Success: You have modified recurring profiles!';
+$_['text_list']             = 'Recurring Profile List';
+$_['text_add']              = 'Add Recurring Profile';
+$_['text_edit']             = 'Edit Recurring Profile';
+$_['text_day']				= 'Дзень';
+$_['text_week']				= 'Тыдзень';
+$_['text_semi_month']		= '15 дзен';
+$_['text_month']			= 'Месяц';
+$_['text_year']				= 'Год';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Recurring amounts are calculated by the frequency and cycles.</p><p>For example if you use a frequency of "week" and a cycle of "2", then the user will be billed every 2 weeks.</p><p>The duration is the number of times the user will make a payment, set this to 0 if you want payments until they are cancelled.</p>';
+$_['text_profile']			= 'Перыядычныя плацяжы';
+$_['text_trial']			= 'Trial Profile';
 
 // Entry
-$_['entry_name']             = 'Назва тавара';
-$_['entry_description']      = 'Апісанне';
-$_['entry_meta_title'] 	     = 'Мета-тэг Title';
-$_['entry_meta_keyword'] 	 = 'Мета-тэг Keywords';
-$_['entry_meta_description'] = 'Мета-тэг Description';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Мадэль';
-$_['entry_sku']              = 'SKU';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'Месца знаходжання';
-$_['entry_shipping']         = 'Патрэбна дастаўка';
-$_['entry_manufacturer']     = 'Вытворца';
-$_['entry_store']            = 'Крамы';
-$_['entry_date_available']   = 'Дата даступнасці';
-$_['entry_quantity']         = 'Колькасць';
-$_['entry_minimum']          = 'Мінімальная колькасць';
-$_['entry_stock_status']     = 'Няма ў наяўнасці';
-$_['entry_price']            = 'Цана';
-$_['entry_tax_class']        = 'Клас падаткаабкладання';
-$_['entry_points']           = 'Балы';
-$_['entry_option_points']    = 'Балы';
-$_['entry_subtract']         = 'Выдаліць са склада';
-$_['entry_weight_class']     = 'Вагавы клас';
-$_['entry_weight']           = 'Вага';
-$_['entry_dimension']        = 'Памеры (Д x Ш x В)';
-$_['entry_length_class']     = 'Клас даўжыні';
-$_['entry_length']           = 'Даўжыня';
-$_['entry_width']            = 'Шырыня';
-$_['entry_height']           = 'Вышыня';
-$_['entry_image']            = 'Выява';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Група пакупнікоў';
-$_['entry_date_start']       = 'Дата пачатку';
-$_['entry_date_end']         = 'Дата заканчэння';
-$_['entry_priority']         = 'Прыярытэт';
-$_['entry_attribute']        = 'Атрыбут';
-$_['entry_attribute_group']  = 'Група атрыбутаў';
-$_['entry_text']             = 'Text';
-$_['entry_option']           = 'Опцыя';
-$_['entry_option_value']     = 'Значэнне';
-$_['entry_required']         = 'Патрабуецца';
-$_['entry_status']           = 'Статус';
-$_['entry_sort_order']       = 'Парадак сартавання';
-$_['entry_category']         = 'Катэгорыі';
-$_['entry_filter']           = 'Фільтры';
-$_['entry_download']         = 'Загрузкі';
-$_['entry_related']          = 'Звязаныя прадукты';
-$_['entry_tag']          	 = 'Тэгі тавараў';
-$_['entry_reward']           = 'Бонусныя балы';
-$_['entry_layout']           = 'Перавызначэнне разметкі';
-$_['entry_recurring']        = 'Перыядычныя плацяжы';
+$_['entry_name']		    = 'Назва';
+$_['entry_price']			= 'Цана';
+$_['entry_duration']		= 'Працягласць';
+$_['entry_cycle']			= 'Цыкл';
+$_['entry_frequency']		= 'Частата';
+$_['entry_trial_price']		= 'Trial price';
+$_['entry_trial_duration']	= 'Trial duration';
+$_['entry_trial_status']	= 'Trial status';
+$_['entry_trial_cycle']	    = 'Trial cycle';
+$_['entry_trial_frequency'] = 'Trial frequency';
+$_['entry_status']			= 'Статус';
+$_['entry_sort_order']		= 'Парадак сартавання';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'Пакоўка';
-$_['help_upc']               = 'Універсальны Код Тавара';
-$_['help_ean']               = 'Еўрапейскі Артыкул';
-$_['help_jan']               = 'Японскі Артыкул';
-$_['help_isbn']              = 'Міжнародны Стандартызаваны Нумар Выдання';
-$_['help_mpn']               = 'Нумар партыі';
-$_['help_manufacturer']      = '(Аўтазавяршэнне)';
-$_['help_minimum']           = 'Прапанаваць мінімальны памер замовы';
-$_['help_stock_status']      = 'Паказваць статус тавара пры яго адсутнасці на складзе';
-$_['help_points']            = 'Колькасць балаў, неабходная для пакупкі. Каб даць магчымасць купіць тавар без балаў, стаўце 0.';
-$_['help_category']          = '(Аўтазавяршэнне)';
-$_['help_filter']            = '(Аўтазавяршэнне)';
-$_['help_download']          = '(Аўтазавяршэнне)';
-$_['help_related']           = '(Аўтазавяршэнне)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'Назва';
+$_['column_sort_order']	    = 'Парадак сартавання';
+$_['column_action']         = 'Дзеянне';
 
 // Error
-$_['error_warning']          = 'Увага: Калі ласка, ўважліва праверце ўведзеные дадзеныя!';
-$_['error_permission']       = 'Увага: Вы не можаце змяняць тавары!';
-$_['error_name']             = 'Даўжыня назвы тавару павінна быць ад 3 да 255 сімвалаў!';
-$_['error_meta_title']       = 'Даўжыня meta title павінна быць ад 3 да 255 сімвалаў!';
-$_['error_model']            = 'Даўжыня назвы мадэлі тавара павінна быць ад 1 да 64 сімвалаў!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Увага: Калі ласка, ўважліва праверце ўведзеные дадзеныя!';
+$_['error_permission']		= 'Warning: You do not have permission to modify recurring profiles!';
+$_['error_name']			= 'Назва профілю павінна быць ад 3 да 255 сімвалаў!';
+$_['error_product']			= 'Warning: This recurring profile cannot be deleted as it is currently assigned to %s products!';
