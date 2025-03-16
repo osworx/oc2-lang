1fc66a73f79e75a499e56b163aa5782c352a336a
OSWorX <info@osworx.net>
2025-03-16 11:17:33 +0100

    New translations footer.php (Hungarian)

--- upload/admin/language/en-gb/common/hu-HU.php
+++ upload/admin/language/en-gb/common/hu-HU.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Kép kezelő';
-
 // Text
-$_['text_uploaded']    = 'Siker: A file-t feltöltöttük!';
-$_['text_directory']   = 'Siker: A könyvtár elkészült!';
-$_['text_delete']      = 'Siker: A file vagy könyvtár törölve!';
-
-// Entry
-$_['entry_search']     = 'Keresés..';
-$_['entry_folder']     = 'Mappa neve';
-
-// Error
-$_['error_permission'] = 'Figyelmeztetés: Az engedély megtagadva!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Figyelmeztetés: Ez a fájlnév vagy könyvtárnév már létezik!';
-$_['error_directory']  = 'Figyelmeztetés: Könyvtár nem létezik!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Figyelmeztetés: Helytelen a fájl típus!';
-$_['error_upload']     = 'Figyelmeztetés: Fájl nem tölthető fel ismeretlen okból!';
-$_['error_delete']     = 'Figyelmeztetés: Nem lehet törölni ezt a könyvtárat!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Verzió: %s';
