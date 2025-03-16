aa4c6675f9969821e4af29d091cff27fd1f90451
OSWorX <info@osworx.net>
2025-03-16 14:16:10 +0100

    New translations attribute.php (Albanian)

--- upload/admin/language/en-gb/catalog/sq-AL.php
+++ upload/admin/language/en-gb/catalog/sq-AL.php
@@ -1,36 +1,27 @@
 <?php
 // Heading
-$_['heading_title']     = 'Reviews';
+$_['heading_title']          = 'Atributet';
 
 // Text
-$_['text_success']      = 'Success: You have modified reviews!';
-$_['text_list']         = 'Review List';
-$_['text_add']          = 'Add Review';
-$_['text_edit']         = 'Edit Review';
+$_['text_success']           = 'Suksess: Ju keni mdifikuar atributet!';
+$_['text_list']              = 'Lista e atributeve';
+$_['text_add']               = 'Shto Atribut';
+$_['text_edit']              = 'Modifiko Atribut';
 
 // Column
-$_['column_product']    = 'Product';
-$_['column_author']     = 'Author';
-$_['column_rating']     = 'Rating';
-$_['column_status']     = 'Statusi';
-$_['column_date_added'] = 'Data e shtuar';
-$_['column_action']     = 'Veprimi';
+$_['column_name']            = 'Emri i Atributit';
+$_['column_attribute_group'] = 'Grupi i Atributit';
+$_['column_sort_order']      = 'Radhitja';
+$_['column_action']          = 'Veprimi';
 
 // Entry
-$_['entry_product']     = 'Product';
-$_['entry_author']      = 'Author';
-$_['entry_rating']      = 'Rating';
-$_['entry_status']      = 'Statusi';
-$_['entry_text']        = 'Text';
-$_['entry_date_added']  = 'Data e shtuar';
-
-// Help
-$_['help_product']      = '(Autokompletimi)';
+$_['entry_name']             = 'Emri i Atributit';
+$_['entry_attribute_group']  = 'Grupi i Atributit';
+$_['entry_sort_order']       = 'Radhitja';
 
 // Error
-$_['error_warning']     = 'Vërejtje: Ju lutem kontrolloni formën me kujdes për ndonjë gabim!';
-$_['error_permission']  = 'Warning: You do not have permission to modify reviews!';
-$_['error_product']     = 'Product required!';
-$_['error_author']      = 'Author must be between 3 and 64 characters!';
-$_['error_text']        = 'Review Text must be at least 1 character!';
-$_['error_rating']      = 'Review rating required!';
+$_['error_warning']          = 'Vërejtje: Ju lutem kontrolloni formën me kujdes për ndonjë gabim!';
+$_['error_permission']       = 'Kujdes: Ju nuk keni leje të modifikoni atributet!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Emri i Atributit duhet të jete nga 3 deri në 64 shkronja!';
+$_['error_product']          = 'Nuk mund te fshihet pasi eshte percaktuar produktit %s!';
