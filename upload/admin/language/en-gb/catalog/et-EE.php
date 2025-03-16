ade712c7071ce9f79d45bc06e81ea9c7ee23fe5b
OSWorX <info@osworx.net>
2025-03-16 11:05:28 +0100

    New translations attribute.php (Estonian)

---
+++ upload/admin/language/en-gb/catalog/et-EE.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Atribuudid';
+
+// Text
+$_['text_success']           = 'Oled edukalt muutnud atribuute!';
+$_['text_list']              = 'Atribuutide nimekiri';
+$_['text_add']               = 'Lisa Atribuut';
+$_['text_edit']              = 'Muuda Atribuuti';
+
+// Column
+$_['column_name']            = 'Atribuudi nimi';
+$_['column_attribute_group'] = 'Atribuudi rühm';
+$_['column_sort_order']      = 'Sorteerimis järjekord';
+$_['column_action']          = 'Tegevus';
+
+// Entry
+$_['entry_name']             = 'Atribuudi nimi';
+$_['entry_attribute_group']  = 'Atribuudi rühm';
+$_['entry_sort_order']       = 'Sorteerimis järjekord';
+
+// Error
+$_['error_warning']          = 'Hoiatus: Palun kontrollige vormi vead hoolikalt üle!';
+$_['error_permission']       = 'Hoiatus: Teil ei ole õigust tunnuseid muuta!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Tunnuse pikkus peab olema vahemikus 3 kuni 64 märki!';
+$_['error_product']          = 'Hoiatus: Seda tunnust ei saa kustutada, kuna see sisaldub hetkel  %s tootes!';
