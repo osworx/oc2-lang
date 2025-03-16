f2920001d509b47f44135ffee40a46f477f792cc
OSWorX <info@osworx.net>
2025-03-16 11:05:34 +0100

    New translations attribute.php (French, Canada)

---
+++ upload/admin/language/en-gb/catalog/fr-CA.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Attributs';
+
+// Text
+$_['text_success']           = 'Félicitation: Vous avez modifié un attribut!';
+$_['text_list']              = 'Liste des attributs';
+$_['text_add']               = 'Ajoutez un attribut';
+$_['text_edit']              = 'Modifiez un attribut';
+
+// Column
+$_['column_name']            = 'Nom de l\'attribut';
+$_['column_attribute_group'] = 'Groupe d\'attributs';
+$_['column_sort_order']      = 'Ordre de présentation';
+$_['column_action']          = 'Action';
+
+// Entry
+$_['entry_name']             = 'Nom de l\'attribut';
+$_['entry_attribute_group']  = 'Groupe d\'attributs';
+$_['entry_sort_order']       = 'Ordre de présentation';
+
+// Error
+$_['error_warning']          = 'Attention: Veuillez vérifier que le formulaire ne contient pas d\'erreur !';
+$_['error_permission']       = 'AVERTISSEMENT : Vous n\'êtes pas autorisé à modifier les attributs !';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Nom de l\'attribut doit être entre 3 et 64 caractères !';
+$_['error_product']          = 'AVERTISSEMENT : Cet attribut ne peut être supprimé car il est actuellement affecté a certains produits %s!';
