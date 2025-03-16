e10b23ab47799b25eab42dabcb829c9beec23240
OSWorX <info@osworx.net>
2025-03-16 11:14:10 +0100

    New translations review.php (Hindi)

--- upload/admin/language/en-gb/catalog/hi-IN.php
+++ upload/admin/language/en-gb/catalog/hi-IN.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'आवर्ती प्रोफ़ाइल';
+$_['heading_title']     = 'समीक्षाएं';
 
 // Text
-$_['text_success']          = 'सफलता: आपने पुनरावर्ती प्रोफाइल को संशोधित किया है!';
-$_['text_list']             = 'आवर्ती प्रोफ़ाइल सूची';
-$_['text_add']              = 'आवर्ती प्रोफ़ाइल जोड़ें';
-$_['text_edit']             = 'आवर्ती प्रोफ़ाइल संपादित करें';
-$_['text_day']				= 'दिन';
-$_['text_week']				= 'सप्ताह';
-$_['text_semi_month']		= 'अर्ध माह';
-$_['text_month']			= 'माह';
-$_['text_year']				= 'वर्ष';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Recurring amounts are calculated by the frequency and cycles.</p><p>For example if you use a frequency of "week" and a cycle of "2", then the user will be billed every 2 weeks.</p><p>The duration is the number of times the user will make a payment, set this to 0 if you want payments until they are cancelled.</p>';
-$_['text_profile']			= 'आवर्ती प्रोफ़ाइल';
-$_['text_trial']			= 'परीक्षण प्रोफ़ाइल';
-
-// Entry
-$_['entry_name']		    = 'नाम';
-$_['entry_price']			= 'मूल्य';
-$_['entry_duration']		= 'अवधि';
-$_['entry_cycle']			= 'चक्र';
-$_['entry_frequency']		= 'आवृत्ति';
-$_['entry_trial_price']		= 'परीक्षण की कीमत';
-$_['entry_trial_duration']	= 'परीक्षण अवधि';
-$_['entry_trial_status']	= 'परीक्षण स्थिति';
-$_['entry_trial_cycle']	    = 'परीक्षण चक्र';
-$_['entry_trial_frequency'] = 'परीक्षण आवृत्ति';
-$_['entry_status']			= 'स्थिति';
-$_['entry_sort_order']		= 'क्रम';
+$_['text_success']      = 'Success: You have modified reviews!';
+$_['text_list']         = 'Review List';
+$_['text_add']          = 'Add Review';
+$_['text_edit']         = 'Edit Review';
 
 // Column
-$_['column_name']			= 'नाम';
-$_['column_sort_order']	    = 'क्रम';
-$_['column_action']         = 'कार्यवाही';
+$_['column_product']    = 'उत्पाद';
+$_['column_author']     = 'लेखक';
+$_['column_rating']     = 'रेटिंग';
+$_['column_status']     = 'स्थिति';
+$_['column_date_added'] = 'जोड़ने की तिथि';
+$_['column_action']     = 'कार्यवाही';
+
+// Entry
+$_['entry_product']     = 'उत्पाद';
+$_['entry_author']      = 'लेखक';
+$_['entry_rating']      = 'रेटिंग';
+$_['entry_status']      = 'स्थिति';
+$_['entry_text']        = 'शब्द';
+$_['entry_date_added']  = 'जोड़ने की तिथि';
+
+// Help
+$_['help_product']      = '(स्वत: पूर्ण)';
 
 // Error
-$_['error_warning']         = 'चेतावनी: कृपया ध्यान से त्रुटियों के लिए प्रपत्र की जाँच करें!';
-$_['error_permission']		= 'चेतावनी: आपको पुनरावर्ती प्रोफाइल को संशोधित करने की अनुमति नहीं है!';
-$_['error_name']			= 'उत्पाद का नाम 3 कम से कम और अधिक से अधिक 255 वर्ण होना आवश्यक है!';
-$_['error_product']			= 'चेतावनी: यह वर्तमान में %s उत्पादों के लिए असाइन किया गया है अत: इस आवर्ती प्रोफ़ाइल हटाया नहीं जा सकता!';
+$_['error_warning']     = 'चेतावनी: कृपया ध्यान से त्रुटियों के लिए प्रपत्र की जाँच करें!';
+$_['error_permission']  = 'Warning: You do not have permission to modify reviews!';
+$_['error_product']     = 'Product required!';
+$_['error_author']      = 'Author must be between 3 and 64 characters!';
+$_['error_text']        = 'Review Text must be at least 1 character!';
+$_['error_rating']      = 'Review rating required!';
