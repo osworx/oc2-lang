1ab29630607d5787c8f730e1afd3deaaff0301c4
OSWorX <info@osworx.net>
2025-03-16 11:19:27 +0100

    New translations header.php (Ukrainian)

--- upload/admin/language/en-gb/common/uk-UA.php
+++ upload/admin/language/en-gb/common/uk-UA.php
@@ -1,19 +1,23 @@
 <?php
-// header
-$_['heading_title']   = 'Забули пароль?';
+// Heading
+$_['heading_title']          = 'OpenCart';
 
 // Text
-$_['text_forgotten']  = 'Нагадати пароль';
-$_['text_your_email'] = 'Ваш E-Mail';
-$_['text_email']      = 'Введіть адресу електронної пошти, пов\'язану з вашим обліковим записом. Посилання на зміну пароля буде надіслано на Ваш e-mail.';
-$_['text_success']    = 'Посилання на зміну пароля надіслано на Ваш e-mail.';
-
-// Entry
-$_['entry_email']     = 'E-Mail';
-$_['entry_password']  = 'Новий пароль';
-$_['entry_confirm']   = 'Підтвердити';
-
-// Error
-$_['error_email']     = 'Увага: Адресу електронної пошти не знайдено, будь ласка спробуйте ще раз!';
-$_['error_password']  = 'Password must be between 4 and 20 characters!';
-$_['error_confirm']   = 'Пароль і підтвердження пароля не співпадають!';
+$_['text_order']             = 'Замовлення';
+$_['text_processing_status'] = 'Processing';
+$_['text_complete_status']   = 'Завершено';
+$_['text_customer']          = 'Клієнти';
+$_['text_online']            = 'Клієнти онлайн';
+$_['text_approval']          = 'Очікує підтвердження';
+$_['text_product']           = 'Товари';
+$_['text_stock']             = 'Немає в наявності';
+$_['text_review']            = 'Відгуки';
+$_['text_return']            = 'Повернення';
+$_['text_affiliate']         = 'Партнери';
+$_['text_store']             = 'Магазини';
+$_['text_front']             = 'Вигляд магазину';
+$_['text_help']              = 'Довідка';
+$_['text_homepage']          = 'OpenCart Homepage';
+$_['text_support']           = 'Форум підтримки';
+$_['text_documentation']     = 'Документація';
+$_['text_logout']            = 'Вихід';
