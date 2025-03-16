37976657eab343d00f98c1cba91fceb6f6f9a2e5
OSWorX <info@osworx.net>
2025-03-16 09:09:32 +0100

    New translations download.php (Hungarian)

--- upload/language/hu-HU/hu-HU.php
+++ upload/language/hu-HU/hu-HU.php
@@ -1,46 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Kategóriák';
+$_['heading_title']     = 'Letöltések';
 
 // Text
-$_['text_success']           = 'Siker: Kategóriák módosítva!';
-$_['text_list']              = 'Kategória lista';
-$_['text_add']               = 'Új kategória';
-$_['text_edit']              = 'Kategória szerkesztése';
-$_['text_default']           = 'Alapértelmezés';
+$_['text_success']      = 'Siker: Letöltések módosítva!';
+$_['text_list']         = 'Letöltés lista';
+$_['text_add']          = 'Új letöltés';
+$_['text_edit']         = 'Letöltés szerkesztése';
+$_['text_upload']       = 'A fájl sikeresen feltöltve!';
 
 // Column
-$_['column_name']            = 'Kategória neve';
-$_['column_sort_order']      = 'Rendezés';
-$_['column_action']          = 'Művelet';
+$_['column_name']       = 'Letöltés neve';
+$_['column_date_added'] = 'Hozzáadás dátuma';
+$_['column_action']     = 'Művelet';
 
 // Entry
-$_['entry_name']             = 'Kategória neve';
-$_['entry_description']      = 'Leírás';
-$_['entry_meta_title'] 	     = 'Meta Tag-cím';
-$_['entry_meta_keyword']     = 'Meta Tag kulcsszó';
-$_['entry_meta_description'] = 'Meta-Tag leírás ';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = 'Szülő';
-$_['entry_filter']           = 'Szűrők';
-$_['entry_store']            = 'Áruházak';
-$_['entry_image']            = 'Kép';
-$_['entry_top']              = 'Oldal teteje';
-$_['entry_column']           = 'Oszlopok';
-$_['entry_sort_order']       = 'Rendezés';
-$_['entry_status']           = 'Állapot';
-$_['entry_layout']           = 'Elrendezés felülbírálása';
+$_['entry_name']        = 'Letöltés neve';
+$_['entry_filename']    = 'Fájlnév';
+$_['entry_mask']        = 'Maszk';
 
 // Help
-$_['help_filter']            = '(Autó Kiegészítés)';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = 'A felső meüsorban jelenítse meg. Csak a felső menüsor szülő kategóriákra érvényes.';
-$_['help_column']            = 'A használt oszlopok száma az alsó 3 kategóriára . Csak a felső szülő kategóriára érvényes.';
+$_['help_filename']     = 'Feltölthet a feltöltés gomb segítségével, vagy FTP segítségével töltse fel a letöltési könyvtárat, és adja meg a részleteket az alábbi mezőben.';
+$_['help_mask']         = 'Ajánlatos, hogy a fájlnév és a maszk különbözőek legyenek, hogy megállítsuk a fájlok közvetlen letöltését.';
 
 // Error
-$_['error_warning']          = 'Figyelem: Kérjük, figyelmesen ellenőrizze a hibákat!';
-$_['error_permission']       = 'Figyelem: Nincs jogosultsága kategóriák módosítására!';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = 'A meta cím több mint 3, és kevesebb, mint 255 karakternek kell lennie!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = 'Figyelem: Kérjük, figyelmesen ellenőrizze a hibákat!';
+$_['error_permission']  = 'Figyelem: Nincs engedélye a letöltések változtatására!';
+$_['error_name']        = 'A letöltés név 3 és 64 karakter között kell lennie!';
+$_['error_upload']      = 'Feltöltés szükséges!';
+$_['error_filename']    = 'A fájlnév 3 és 128 karakter között kell lennie!';
+$_['error_exists']      = 'A fájl nem létezik!';
+$_['error_mask']        = 'A maszk 3 és 128 karakter között kell lennie!';
+$_['error_filetype']    = 'Érvénytelen fájltípus!';
+$_['error_product']     = 'Figyelem: Ez a letöltés nem törölhető, mert jelenleg hozzá van rendelve a(z) %s termékekhez!';
