3bbc4fa563209819275909146c2dad8eee3c9a02
OSWorX <info@osworx.net>
2025-03-16 14:18:40 +0100

    New translations download.php (Chinese Simplified)

--- upload/admin/language/en-gb/catalog/zh-CN.php
+++ upload/admin/language/en-gb/catalog/zh-CN.php
@@ -1,46 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = '目录';
+$_['heading_title']     = '下载';
 
 // Text
-$_['text_success']           = '成功:你已修改了属性!';
-$_['text_list']              = '类别列表';
-$_['text_add']               = '添加产品分类';
-$_['text_edit']              = '编辑产品分类';
-$_['text_default']           = '默认';
+$_['text_success']      = '成功： 您修改了下载资源 ！';
+$_['text_list']         = '下载列表';
+$_['text_add']          = '添加下载资源';
+$_['text_edit']         = '编辑下载资源';
+$_['text_upload']       = '已成功上传文件 ！';
 
 // Column
-$_['column_name']            = '产品分类名称';
-$_['column_sort_order']      = '排列顺序';
-$_['column_action']          = '操作';
+$_['column_name']       = '下载资源名称';
+$_['column_date_added'] = '添加日期';
+$_['column_action']     = '操作';
 
 // Entry
-$_['entry_name']             = '产品分类名称';
-$_['entry_description']      = '描述';
-$_['entry_meta_title'] 	     = '标签名';
-$_['entry_meta_keyword']     = '关键词';
-$_['entry_meta_description'] = '标签描述';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = '父级';
-$_['entry_filter']           = '筛选器';
-$_['entry_store']            = '商店';
-$_['entry_image']            = '图像';
-$_['entry_top']              = '顶部';
-$_['entry_column']           = '分栏';
-$_['entry_sort_order']       = '排列顺序';
-$_['entry_status']           = '状态';
-$_['entry_layout']           = '布局';
+$_['entry_name']        = '下载资源名称';
+$_['entry_filename']    = '文件名';
+$_['entry_mask']        = '虚拟资源名称';
 
 // Help
-$_['help_filter']            = '自动完成';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = '在顶部菜单栏中显示.仅适用于一级产品分类';
-$_['help_column']            = '设置页底三个分类的分栏数.(仅适用于一级分类)';
+$_['help_filename']     = '你可以通过上传按钮上传或使用 FTP 上传到下载目录并在下面输入详细描述信息。';
+$_['help_mask']         = '建议的真实文件名和虚拟文件名不同，阻止人们试图直接链接到您的下载资源。';
 
 // Error
-$_['error_warning']          = '警告:请仔细检查错误格式!';
-$_['error_permission']       = '警告:您没有权限修改产品分类!';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = '元标题必须3-255个字符之间!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = '警告:请仔细检查错误格式!';
+$_['error_permission']  = '警告: 您没有权限修改下载资源 ！';
+$_['error_name']        = '下载名称必须是 3 和 64 个字符之间 ！';
+$_['error_upload']      = '上传资源必填 ！';
+$_['error_filename']    = '文件名必须介于 3 到 128 个字符之间 ！';
+$_['error_exists']      = '文件不存在！';
+$_['error_mask']        = '虚拟名称必须是 3 和 128 个字符之间 ！';
+$_['error_filetype']    = '不允许的文件类型。';
+$_['error_product']     = '警告： 不能删除此下载资源，因为它目前已分配给 %s 的产品 ！';
