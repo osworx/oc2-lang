dac3da519d1e74157146379751e74676c1a7e39d
OSWorX <info@osworx.net>
2025-03-16 11:12:58 +0100

    New translations recurring.php (Dutch)

--- upload/admin/language/en-gb/catalog/nl-NL.php
+++ upload/admin/language/en-gb/catalog/nl-NL.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'Producten';
+$_['heading_title']			= 'Wederkerende Profielen';
 
 // Text
-$_['text_success']           = 'gefeliciteerd:U heeft de producten gewijzigd!';
-$_['text_list']              = 'Product lijst';
-$_['text_add']               = 'Product toevoegen';
-$_['text_edit']              = 'Product bewerken';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'Hoofdwinkel';
-$_['text_option']            = 'Optie';
-$_['text_option_value']      = 'Optiewaarde';
-$_['text_percent']           = 'Percentage';
-$_['text_amount']            = 'Vast bedrag';
-
-// Column
-$_['column_name']            = 'Productnaam';
-$_['column_model']           = 'Model';
-$_['column_image']           = 'Afbeelding';
-$_['column_price']           = 'Prijs per stuk';
-$_['column_quantity']        = 'Aantal';
-$_['column_status']          = 'status';
-$_['column_action']          = 'Aktie';
+$_['text_success']          = 'Gefeliciteerd: U heeft de wederkerende profielen gewijzigd!';
+$_['text_list']             = 'Wederkerende profielen lijst';
+$_['text_add']              = 'Voeg een wederkerend profiel toe';
+$_['text_edit']             = 'Wederkerend profiel aanpassen';
+$_['text_day']				= 'Dag';
+$_['text_week']				= 'Week';
+$_['text_semi_month']		= 'Halve maand';
+$_['text_month']			= 'Maand';
+$_['text_year']				= 'Jaar';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Wederkerende bedragen berekend op basis van Veelvuldigheid en Cyclus.</p><p>Bijvoorbeeld indien u op basis van een "week" en een cyclus van "2" ,Dan zal de gebruiker om de 2 weken gefactureerd worden.</p><p>De duur is het aantal keer dat de gebruiker een betaling doet,zet de cyclus op 0 indien u betalingen wenst tot wanneer deze geannuleerd word.</p>';
+$_['text_profile']			= 'Periodiek profiel';
+$_['text_trial']			= 'Proef Profiel';
 
 // Entry
-$_['entry_name']             = 'Productnaam';
-$_['entry_description']      = 'Beschrijving';
-$_['entry_meta_title'] 	     = 'Meta Tag Titel';
-$_['entry_meta_keyword'] 	 = 'Meta Tag Zoekwoorden';
-$_['entry_meta_description'] = 'Meta Tag Omschrijving';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Model';
-$_['entry_sku']              = 'SKU';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'Locatie';
-$_['entry_shipping']         = 'Verzending vereist';
-$_['entry_manufacturer']     = 'Fabrikant';
-$_['entry_store']            = 'Winkel(s)';
-$_['entry_date_available']   = 'Beschikbaar vanaf';
-$_['entry_quantity']         = 'Aantal';
-$_['entry_minimum']          = 'Minimum aantal';
-$_['entry_stock_status']     = 'Uitverkocht';
-$_['entry_price']            = 'Prijs per stuk';
-$_['entry_tax_class']        = 'Belasting groep';
-$_['entry_points']           = 'Punten';
-$_['entry_option_points']    = 'Punten';
-$_['entry_subtract']         = 'Verminder voorraad';
-$_['entry_weight_class']     = 'Gewichtsklasse';
-$_['entry_weight']           = 'Gewicht';
-$_['entry_dimension']        = 'Afmetingen (L x b x H)';
-$_['entry_length_class']     = 'Afmetingen';
-$_['entry_length']           = 'Lengte';
-$_['entry_width']            = 'Breedte';
-$_['entry_height']           = 'Hoogte';
-$_['entry_image']            = 'Afbeelding';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Klantengroep';
-$_['entry_date_start']       = 'Startdatum';
-$_['entry_date_end']         = 'Einddatum';
-$_['entry_priority']         = 'Prioriteit';
-$_['entry_attribute']        = 'Attribuut';
-$_['entry_attribute_group']  = 'Attribuut groep';
-$_['entry_text']             = 'Tekst';
-$_['entry_option']           = 'Optie';
-$_['entry_option_value']     = 'Optiewaarde';
-$_['entry_required']         = 'Vereist';
-$_['entry_status']           = 'status';
-$_['entry_sort_order']       = 'Sorteervolgorde';
-$_['entry_category']         = 'Categorie&euml;n';
-$_['entry_filter']           = 'Filters';
-$_['entry_download']         = 'Downloads';
-$_['entry_related']          = 'Gerelateerde producten';
-$_['entry_tag']          	 = 'Product Tags';
-$_['entry_reward']           = 'Trofee-punten';
-$_['entry_layout']           = 'Layout';
-$_['entry_recurring']        = 'Periodiek profiel';
+$_['entry_name']		    = 'Naam';
+$_['entry_price']			= 'Prijs per stuk';
+$_['entry_duration']		= 'Duur';
+$_['entry_cycle']			= 'Cyclus';
+$_['entry_frequency']		= 'Frequentie';
+$_['entry_trial_price']		= 'Proef prijs';
+$_['entry_trial_duration']	= 'Duur Proef Periode';
+$_['entry_trial_status']	= 'Proef status';
+$_['entry_trial_cycle']	    = 'Proef cyclus';
+$_['entry_trial_frequency'] = 'Proef frequentie';
+$_['entry_status']			= 'status';
+$_['entry_sort_order']		= 'Sorteervolgorde';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'Voorraad Afdeling Beheer';
-$_['help_upc']               = 'Universele Product Code';
-$_['help_ean']               = 'Europees artikelnummer';
-$_['help_jan']               = 'Japans Artikelnummer';
-$_['help_isbn']              = 'Internationaal Standaard Boekingsnummer';
-$_['help_mpn']               = 'Onderdeelnummer fabrikant';
-$_['help_manufacturer']      = '(AutoAanvullen)';
-$_['help_minimum']           = 'Dwing een minimum bestelbedrag af';
-$_['help_stock_status']      = 'Getoonde status wanneer een product niet meer beschikbaar is';
-$_['help_points']            = 'Aantal punten die nodig zijn om dit item aan te schaffen.Indien u geen punten wenst te gebruiken laat u het veld leeg met een 0.';
-$_['help_category']          = '(AutoAanvullen)';
-$_['help_filter']            = '(AutoAanvullen)';
-$_['help_download']          = '(AutoAanvullen)';
-$_['help_related']           = '(AutoAanvullen)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'Naam';
+$_['column_sort_order']	    = 'Sorteervolgorde';
+$_['column_action']         = 'Aktie';
 
 // Error
-$_['error_warning']          = 'Waarschuwing: Controlleer of alle verplichte velden volledig zijn ingevuld!';
-$_['error_permission']       = 'Opgelet:U bent niet gemachtigd producten te wijzigen!';
-$_['error_name']             = 'De productnaam moet tenminste 3 en kleiner dan 255 karakters bevatten!';
-$_['error_meta_title']       = 'Waarschuwing: Meta Titel dient tussen de 3 en 255 tekens lang te zijn!';
-$_['error_model']            = 'Product model moet ten minste 1 en minder dan 64 karakters bevatten!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Waarschuwing: Controlleer of alle verplichte velden volledig zijn ingevuld!';
+$_['error_permission']		= 'Opgelet: U heeft geen machtiging om wederkerende profielen te wijzigen!';
+$_['error_name']			= 'Een profielnaam dient minimaal 3 en minder dan 255 karakters te bevatten.';
+$_['error_product']			= 'Opgelet: Dit wederkerend profiel kan niet verwijderd worden omdat deze nog aan %s is toegewezen.';
