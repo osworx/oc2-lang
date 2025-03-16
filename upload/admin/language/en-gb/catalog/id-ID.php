d235c6817903f5059cc9c0112bc124d0abe213ab
OSWorX <info@osworx.net>
2025-03-16 14:19:33 +0100

    New translations filter.php (Indonesian)

--- upload/admin/language/en-gb/catalog/id-ID.php
+++ upload/admin/language/en-gb/catalog/id-ID.php
@@ -1,35 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = 'Unduhan';
+$_['heading_title']     = 'Filter';
 
 // Text
-$_['text_success']      = 'Sukses: Anda telah memodifikasi unduhan!';
-$_['text_list']         = 'Daftar Unduhan';
-$_['text_add']          = 'Tambahkan Unduhan';
-$_['text_edit']         = 'Mengubah Unduhan';
-$_['text_upload']       = 'File anda berhasil diupload!';
+$_['text_success']      = 'Sukses: Anda telah memodifikasi filter!';
+$_['text_list']         = 'Daftar Filter';
+$_['text_add']          = 'Tambah Filter';
+$_['text_edit']         = 'Ubah Filter';
 
 // Column
-$_['column_name']       = 'Nama Download';
-$_['column_date_added'] = 'Tanggal Ditambahkan';
+$_['column_group']      = 'Grup Filter';
+$_['column_sort_order'] = 'Urutan';
 $_['column_action']     = 'Tindakan';
 
 // Entry
-$_['entry_name']        = 'Nama Download';
-$_['entry_filename']    = 'Nama File';
-$_['entry_mask']        = 'Kasker';
-
-// Help
-$_['help_filename']     = 'Anda dapat meng-upload melalui tombol upload atau menggunakan FTP untuk meng-upload ke direktori download dan masukkan rincian di bawah ini.';
-$_['help_mask']         = 'Disarankan bahwa nama file dan topeng yang berbeda untuk menghentikan orang-orang yang mencoba untuk langsung link ke download Anda.';
+$_['entry_group']       = 'Filter Nama Grup';
+$_['entry_name']        = 'Nama Filter';
+$_['entry_sort_order']  = 'Urutan';
 
 // Error
 $_['error_warning']     = 'Peringatan: Mohon periksa formulir secara teliti untuk kesalahan!';
-$_['error_permission']  = 'Peringatan: Anda tidak memiliki izin untuk memodifikasi download!';
-$_['error_name']        = 'Download nama harus antara 3 dan 64 karakter!';
-$_['error_upload']      = 'Upload Diperlukan!';
-$_['error_filename']    = 'Namafile harus antara 3 dan 128 karakter!';
-$_['error_exists']      = 'File tidak ada!';
-$_['error_mask']        = 'Masker harus antara 3 dan 128 karakter!';
-$_['error_filetype']    = 'Jenis berkas tidak valid!';
-$_['error_product']     = 'Peringatan: Download ini tidak bisa dihapus karena saat ini ditetapkan untuk produk %s!';
+$_['error_permission']  = 'Peringatan: Anda tidak memiliki izin untuk mengubah filter!';
+$_['error_group']       = 'Nama Filter Grup harus terdiri dari 1 sampai 64 karakter!';
+$_['error_name']        = 'Nama Filter harus antara 1sampai 64 karakter!';
