9e8c5eab7438a6a91924ae31cd0b50477914923b
OSWorX <info@osworx.net>
2025-03-16 09:08:10 +0100

    New translations information.php (Danish)

--- upload/language/da-DK/da-DK.php
+++ upload/language/da-DK/da-DK.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'Filtre';
+$_['heading_title']          = 'Information';
 
 // Text
-$_['text_success']      = 'Succes: Du har ændret filtre!';
-$_['text_list']         = 'Filtre';
-$_['text_add']          = 'Tilføj filter';
-$_['text_edit']         = 'Rediger filter';
+$_['text_success']           = 'Succes: Du har ændret information!';
+$_['text_list']              = 'Information';
+$_['text_add']               = 'Tilføj information';
+$_['text_edit']              = 'Rediger information';
+$_['text_default']           = 'Standard';
 
 // Column
-$_['column_group']      = 'Filtergruppe';
-$_['column_sort_order'] = 'Sorteringsrækkefølge';
-$_['column_action']     = 'Handling';
+$_['column_title']           = 'Informationstitel';
+$_['column_sort_order']	     = 'Sorteringsrækkefølge';
+$_['column_action']          = 'Handling';
 
 // Entry
-$_['entry_group']       = 'Filtergruppenavn';
-$_['entry_name']        = 'Filternavn';
-$_['entry_sort_order']  = 'Sorteringsrækkefølge';
+$_['entry_title']            = 'Informationstitel';
+$_['entry_description']      = 'Beskrivelse';
+$_['entry_store']            = 'Butikker';
+$_['entry_meta_title'] 	     = 'Meta tag titel';
+$_['entry_meta_keyword'] 	 = 'Meta tag nøgleord';
+$_['entry_meta_description'] = 'Meta tag Beskrivelse';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Ned';
+$_['entry_status']           = 'Status';
+$_['entry_sort_order']       = 'Sorteringsrækkefølge';
+$_['entry_layout']           = 'Andet layout';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Vis i sidefoden.';
 
 // Error
-$_['error_warning']     = 'Advarsel: Gennemgå venligst formularen for fejl!';
-$_['error_permission']  = 'Fejl: Du har ikke tilladelse til at ændre filtre!';
-$_['error_group']       = 'Filtergruppenavn skal være mellem 1 og 64 karakterer!';
-$_['error_name']        = 'Filternavn skal være mellem 1 og 64 karakterer!';
+$_['error_warning']          = 'Advarsel: Gennemgå venligst formularen for fejl!';
+$_['error_permission']       = 'Fejl: Du har ikke tilladelse til at ændre informationssider!';
+$_['error_title']            = 'Informationens titel skal være mellem 3 og 64 karakterer!';
+$_['error_description']      = 'Beskrivelse skal være på mere end 3 karakterer!';
+$_['error_meta_title']       = 'Metatitel skal være mellem 3 og 255 karakterer!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'Fejl: Denne informationsside kan ikke slettes, da den i øjeblikket er tilknyttet butikkens konto-betingelser!';
+$_['error_checkout']         = 'Fejl: Denne informationsside kan ikke slettes, da den i øjeblikket er tilknyttet butikkens købsbetingelser!';
+$_['error_affiliate']        = 'Fejl: Denne informationsside kan ikke slettes, da den i øjeblikket er tilknyttet butikkens partner-betingelser!';
+$_['error_return']           = 'Fejl: Denne informationsside kan ikke slettes, da den i øjeblikket er tilknyttet butikkens returneringsbetingelser!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
