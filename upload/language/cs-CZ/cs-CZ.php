cb4fc385926e63cbf67d2386f66a5b399654e5ef
OSWorX <info@osworx.net>
2025-03-16 09:08:02 +0100

    New translations forgotten.php (Czech)

--- upload/language/cs-CZ/cs-CZ.php
+++ upload/language/cs-CZ/cs-CZ.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Zapomněli jste své heslo?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Verze %s';
+$_['text_forgotten']  = 'Zapomenuté heslo';
+$_['text_your_email'] = 'Vaše e-mailová adresa';
+$_['text_email']      = 'Zadejte e-mailovou adresu spojenou s Vašm účtem. Klepněte na tlačítko Odeslat a e-mailem obdržíte odkaz pro resetování hesla.';
+$_['text_success']    = 'E-mail s potvrzujícím odkazem byl odeslán na vaši e-mailovou adresu správce.';
+
+// Entry
+$_['entry_email']     = 'E-mailová adresa';
+$_['entry_password']  = 'Nové heslo';
+$_['entry_confirm']   = 'Potvrdit';
+
+// Error
+$_['error_email']     = 'Upozornění: E-mailová adresa nebyla nalezena v našich záznamech, prosím zkuste znovu!';
+$_['error_password']  = 'Heslo musí být delší než 4 znaky a kratší než 20 znaků!';
+$_['error_confirm']   = 'Heslo a potvrzení hesla se neshodují!';
