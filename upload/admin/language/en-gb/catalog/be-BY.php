432a6d8f8250ba9d22f67e6b40094612affc80b5
OSWorX <info@osworx.net>
2025-03-16 11:06:32 +0100

    New translations category.php (Belarusian)

--- upload/admin/language/en-gb/catalog/be-BY.php
+++ upload/admin/language/en-gb/catalog/be-BY.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Група атрыбутаў';
+$_['heading_title']          = 'Катэгорыі';
 
 // Text
-$_['text_success']      = 'Вы паспяхова змянілі групу атрыбутаў!';
-$_['text_list']         = 'Спіс груп атрыбутаў';
-$_['text_add']          = 'Дадаць групу атрыбутаў';
-$_['text_edit']         = 'Рэдагаваць групу атрыбутаў';
+$_['text_success']           = 'Поспех: катэгорыі паспяхова змененыя!';
+$_['text_list']              = 'Спіс катэгорый';
+$_['text_add']               = 'Дадаць катэгорыю';
+$_['text_edit']              = 'Рэдагаваць катэгорыю';
+$_['text_default']           = 'Па-змаўчанню';
 
 // Column
-$_['column_name']       = 'Назва групы атрыбутаў';
-$_['column_sort_order'] = 'Парадак сартавання';
-$_['column_action']     = 'Дзеянне';
+$_['column_name']            = 'Назва катэгорыі';
+$_['column_sort_order']      = 'Парадак сартавання';
+$_['column_action']          = 'Дзеянне';
 
 // Entry
-$_['entry_name']        = 'Назва групы атрыбутаў';
-$_['entry_sort_order']  = 'Парадак сартавання';
+$_['entry_name']             = 'Назва катэгорыі';
+$_['entry_description']      = 'Апісанне';
+$_['entry_meta_title'] 	     = 'Мета-тэг Title';
+$_['entry_meta_keyword']     = 'Мета-тэг Keywords';
+$_['entry_meta_description'] = 'Мета-тэг Description';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Бацькоўскі аб\'ект';
+$_['entry_filter']           = 'Фільтры';
+$_['entry_store']            = 'Крамы';
+$_['entry_image']            = 'Выява';
+$_['entry_top']              = 'Уверсе';
+$_['entry_column']           = 'Слупкі';
+$_['entry_sort_order']       = 'Парадак сартавання';
+$_['entry_status']           = 'Статус';
+$_['entry_layout']           = 'Перавызначэнне разметкі';
+
+// Help
+$_['help_filter']            = '(Аўтазавяршэнне)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Паказваць у верхнім меню. Працуе толькі для катэгорый вышэйшага ўзроўня.';
+$_['help_column']            = 'Колькасць слупкоў для катэгорый унізе. Працуе толькі для бацькоўскіх катэгорый.';
 
 // Error
-$_['error_warning']     = 'Увага: Калі ласка, ўважліва праверце ўведзеные дадзеныя!';
-$_['error_permission']  = 'Увага: Вы не можаце змяняць групы атрыбутаў!';
-$_['error_name']        = 'Даўжыня назвы групы атрыбутаў павінна быць ад 3 да 64 сімвалаў!';
-$_['error_attribute']   = 'Увага: Гэтая група атрыбутаў не можа быць выдалена, бо ўжо прызначана да %s атрыбутаў!';
-$_['error_product']     = 'Увага: Гэтая група атрыбутаў не можа быць выдалена, бо ўжо прызначана да %s атрыбутаў!';
+$_['error_warning']          = 'Увага: Калі ласка, ўважліва праверце ўведзеные дадзеныя!';
+$_['error_permission']       = 'Увага: Вы не можаце змяняць гэтую інфармацыю!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Даўжыня meta title павінна быць ад 3 да 255 сімвалаў!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
