a9caee5d15c5dad9741e96126e44417c26d17c11
OSWorX <info@osworx.net>
2025-03-16 14:27:52 +0100

    New translations footer.php (Russian)

--- upload/admin/language/en-gb/common/ru-RU.php
+++ upload/admin/language/en-gb/common/ru-RU.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Менеджер изображений';
-
 // Text
-$_['text_uploaded']    = 'Поздравляю! : ваш файл был загружен на сервер!';
-$_['text_directory']   = 'Поздравляю! : Папка создана!';
-$_['text_delete']      = 'Поздравляю! : Файл или каталог удален!';
-
-// Entry
-$_['entry_search']     = 'Поиск..';
-$_['entry_folder']     = 'Имя папки';
-
-// Error
-$_['error_permission'] = 'Внимание: Доступ запрещен!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Внимание: Файл или каталог с таким же именем уже существует!';
-$_['error_directory']  = 'Внимание: Каталог не существует!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Внимание: Неправильный тип (формат) файла!';
-$_['error_upload']     = 'Внимание: Файл невозможно загрузить на сервер по неизвестной причине!';
-$_['error_delete']     = 'Внимание: Вы не можете удалить этот каталог!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Версия %s';
