c392ebef69d0b30b6979dd293826ea2d5a42a0b9
OSWorX <info@osworx.net>
2025-03-16 11:05:24 +0100

    New translations attribute.php (Persian)

---
+++ upload/admin/language/en-gb/catalog/fa-IR.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'ویژگی‌ها';
+
+// Text
+$_['text_success']           = 'موفقیت: شما ویژگی‌ها را تغییر داده‌اید!';
+$_['text_list']              = 'فهرست ویژگی';
+$_['text_add']               = 'افزودن ویژگی';
+$_['text_edit']              = 'ویرایش ویژگی';
+
+// Column
+$_['column_name']            = 'نام ویژگی';
+$_['column_attribute_group'] = 'گروه ویژگی';
+$_['column_sort_order']      = 'ترتیب';
+$_['column_action']          = 'کنش';
+
+// Entry
+$_['entry_name']             = 'نام ویژگی';
+$_['entry_attribute_group']  = 'گروه ویژگی';
+$_['entry_sort_order']       = 'ترتیب';
+
+// Error
+$_['error_warning']          = 'هشدار: لطفا فرم را به دقت برای خطاها بررسی نمایید!';
+$_['error_permission']       = 'هشدار: شما مجوز تغییر ویژگی‌ها را ندارید!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'نام ویژگی باید بین ۳ و ۶۴ نویسه باشد!';
+$_['error_product']          = 'هشدار: چون این ویژگی در حال حاضر به %s کالا اختصاص داده شده نمی‌تواند حذف گردد!';
