c1aea0e9ff4247e9b41e5228a9845d48684828c1
OSWorX <info@osworx.net>
2025-03-16 11:17:08 +0100

    New translations filemanager.php (Thai)

--- upload/admin/language/en-gb/common/th-TH.php
+++ upload/admin/language/en-gb/common/th-TH.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'หน้ากระดานหลัก';
+$_['heading_title']    = 'ตัวจัดการรูปภาพ';
+
+// Text
+$_['text_uploaded']    = 'สำเร็จ: แฟ้มของคุณได้ถูกอัปโหลด';
+$_['text_directory']   = 'สำเร็จ: สร้างไดเรกทอรีเรียบร้อย';
+$_['text_delete']      = 'ความสำเร็จ: ลบแฟ้มหรือไดเรกทอรีแล้ว';
+
+// Entry
+$_['entry_search']     = 'ค้นหา..';
+$_['entry_folder']     = 'ชื่อโฟลเดอร์';
 
 // Error
-$_['error_install'] = 'คำเตือน: โฟลเดอร์การติดตั้งยังคงมีอยู่ และควรลบออกเพื่อความปลอดภัย';
+$_['error_permission'] = 'คำเตือน: ไม่มีสิทธิ์เข้าถึง';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'คำเตือน: มีชื่อแฟ้มหรือไดเรกทอรีนี้อยู่แล้ว';
+$_['error_directory']  = 'คำเตือน: ไม่มีไดเรกทอรีนี้';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'คำเตือน: ชนิดแฟ้มไม่ถูกต้อง';
+$_['error_upload']     = 'คำเตือน: ไม่สามารถอัปโหลดแฟ้มโดยไม่ทราบสาเหตุ';
+$_['error_delete']     = 'คำเตือน: คุณไม่สามารถลบไดเรกทอรีนี้';
