86b68bd0971d93e3fbf8bfc26720f41cc3b48764
OSWorX <info@osworx.net>
2025-03-16 14:16:09 +0100

    New translations attribute.php (Slovak)

--- upload/admin/language/en-gb/catalog/sk-SK.php
+++ upload/admin/language/en-gb/catalog/sk-SK.php
@@ -1,36 +1,27 @@
 <?php
 // Heading
-$_['heading_title']     = 'Recenzie';
+$_['heading_title']          = 'Vlastnosti';
 
 // Text
-$_['text_success']      = 'Úspech: Zmenili ste Recenzie!';
-$_['text_list']         = 'Recenzie';
-$_['text_add']          = 'Pridať recenziu';
-$_['text_edit']         = 'Upraviť recenziu';
+$_['text_success']           = 'Hotovo: Atribút bol zmenený!';
+$_['text_list']              = 'Zoznam vlastností';
+$_['text_add']               = 'Pridať vlastnosť';
+$_['text_edit']              = 'Upraviť vlastnosť';
 
 // Column
-$_['column_product']    = 'Produkt';
-$_['column_author']     = 'Autor';
-$_['column_rating']     = 'Hodnotenie';
-$_['column_status']     = 'Stav';
-$_['column_date_added'] = 'Dátum pridaný';
-$_['column_action']     = 'Akcia';
+$_['column_name']            = 'Názov vlastnosti';
+$_['column_attribute_group'] = 'Skupina vlastností';
+$_['column_sort_order']      = 'Poradie';
+$_['column_action']          = 'Akcia';
 
 // Entry
-$_['entry_product']     = 'Produkt';
-$_['entry_author']      = 'Autor';
-$_['entry_rating']      = 'Hodnotenie';
-$_['entry_status']      = 'Stav';
-$_['entry_text']        = 'Text';
-$_['entry_date_added']  = 'Dátum pridaný';
-
-// Help
-$_['help_product']      = '(Automatické dokončovanie)';
+$_['entry_name']             = 'Názov vlastnosti';
+$_['entry_attribute_group']  = 'Skupina vlastností';
+$_['entry_sort_order']       = 'Poradie';
 
 // Error
-$_['error_warning']     = 'Varovanie: Skontrolujte formulár starostlivo kvôli chybám!';
-$_['error_permission']  = 'Upozornenie: Nemáte oprávnenie k zmene recenzí!';
-$_['error_product']     = 'Produkt je nutný!';
-$_['error_author']      = 'Autor musí mať medzi 3 a 64 znakov!';
-$_['error_text']        = 'Text recenzie musí mať aspoň 1 znak!';
-$_['error_rating']      = 'Hodnotenie recenzie nutné!';
+$_['error_warning']          = 'Varovanie: Skontrolujte formulár starostlivo kvôli chybám!';
+$_['error_permission']       = 'Varovanie: Nemáte oprávnenie k úprave vlastností!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Názov vlastnosti musí byť medzi v rozmedzí 3 až 64 znakov!';
+$_['error_product']          = 'Varovanie: Táto vlastnosť nemôže byť odstránená, pretože je v súčasnosti priradená k %s produktom!';
