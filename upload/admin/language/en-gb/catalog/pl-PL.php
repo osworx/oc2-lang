61ce32747ba5f822fb1b60309b049e74c334aef3
OSWorX <info@osworx.net>
2025-03-16 14:23:18 +0100

    New translations recurring.php (Polish)

--- upload/admin/language/en-gb/catalog/pl-PL.php
+++ upload/admin/language/en-gb/catalog/pl-PL.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'Produkty';
+$_['heading_title']			= 'Profile Ratalne';
 
 // Text
-$_['text_success']           = 'Sukces: Zmodyfikowałeś produkty!';
-$_['text_list']              = 'Lista produktów';
-$_['text_add']               = 'Dodaj produkt';
-$_['text_edit']              = 'Edytuj produkt';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'Domyślnie';
-$_['text_option']            = 'Opcja';
-$_['text_option_value']      = 'Wartość opcji';
-$_['text_percent']           = 'Procentowo';
-$_['text_amount']            = 'Stała kwota';
-
-// Column
-$_['column_name']            = 'Nazwa producenta';
-$_['column_model']           = 'Model';
-$_['column_image']           = 'Zdjęcie';
-$_['column_price']           = 'Cena';
-$_['column_quantity']        = 'Ilość';
-$_['column_status']          = 'Status';
-$_['column_action']          = 'Akcja';
+$_['text_success']          = 'Sukces: zmodyfikowałeś profile ratalne!';
+$_['text_list']             = 'Lista profili ratalnych';
+$_['text_add']              = 'Dodaj profil ratalny';
+$_['text_edit']             = 'Modyfikuj profil rataly';
+$_['text_day']				= 'Dzień';
+$_['text_week']				= 'Tydzień';
+$_['text_semi_month']		= 'Pół miesiąca';
+$_['text_month']			= 'Miesiąć';
+$_['text_year']				= 'Rok';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Rata obliczana jest na podstawie częstotliwości wpłat.</p><p>Na przykład, jeśli używasz częstotliwości "tydzień" i cyklu "2", to użytkownik będzie rozliczany co 2 tygodnie.</p><p>Czas trwania jest uzależniony od tego, ile razy użytkownik będzie dokonać płatności, ustaw 0 , jeśli chcesz płatności przed ich anulowaniem.</p>';
+$_['text_profile']			= 'Profil ratalny/abonamentowy';
+$_['text_trial']			= 'Profil próbny';
 
 // Entry
-$_['entry_name']             = 'Nazwa producenta';
-$_['entry_description']      = 'Opis';
-$_['entry_meta_title'] 	     = 'Meta Tag Tytuł';
-$_['entry_meta_keyword'] 	 = 'Słowa kluczowe Tag meta';
-$_['entry_meta_description'] = 'Opis Tag meta';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Model';
-$_['entry_sku']              = 'SKU';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'Miasto nadania';
-$_['entry_shipping']         = 'Wymaga wysyłki';
-$_['entry_manufacturer']     = 'Producent';
-$_['entry_store']            = 'Sklepy';
-$_['entry_date_available']   = 'Pokazać od';
-$_['entry_quantity']         = 'Ilość';
-$_['entry_minimum']          = 'Min. ilość';
-$_['entry_stock_status']     = 'Status braku';
-$_['entry_price']            = 'Cena';
-$_['entry_tax_class']        = 'Stawka podatku';
-$_['entry_points']           = 'Cena w punktach';
-$_['entry_option_points']    = 'Cena w punktach';
-$_['entry_subtract']         = 'Odejmowanie?';
-$_['entry_weight_class']     = 'Jm: waga';
-$_['entry_weight']           = 'Waga';
-$_['entry_dimension']        = 'Suma dł.+szer.+wys.';
-$_['entry_length_class']     = 'Jm: rozmiar';
-$_['entry_length']           = 'Długość';
-$_['entry_width']            = 'Szerokość';
-$_['entry_height']           = 'Wysokość';
-$_['entry_image']            = 'Zdjęcie';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Grupa klientów';
-$_['entry_date_start']       = 'Od';
-$_['entry_date_end']         = 'Do';
-$_['entry_priority']         = 'Priorytet';
-$_['entry_attribute']        = 'Cecha';
-$_['entry_attribute_group']  = 'Grupa atrybutów';
-$_['entry_text']             = 'Tekst';
-$_['entry_option']           = 'Opcja';
-$_['entry_option_value']     = 'Wartość opcji';
-$_['entry_required']         = 'Wymagane';
-$_['entry_status']           = 'Status';
-$_['entry_sort_order']       = 'Kolejność sortowania';
-$_['entry_category']         = 'Kategorie';
-$_['entry_filter']           = 'Filtry';
-$_['entry_download']         = 'Pliki do pobrania';
-$_['entry_related']          = 'Powiązane';
-$_['entry_tag']          	 = 'Tagi';
-$_['entry_reward']           = 'Punkty lojalnościowe';
-$_['entry_layout']           = 'Zastępowanie układu';
-$_['entry_recurring']        = 'Profil ratalny/abonamentowy';
+$_['entry_name']		    = 'Nazwa';
+$_['entry_price']			= 'Cena';
+$_['entry_duration']		= 'Czas trwania';
+$_['entry_cycle']			= 'Cykl';
+$_['entry_frequency']		= 'Częstotliwość';
+$_['entry_trial_price']		= 'Cena wersji ograniczonej czasowo';
+$_['entry_trial_duration']	= 'Okres ważności';
+$_['entry_trial_status']	= 'Status wersji ograniczonej czasowo';
+$_['entry_trial_cycle']	    = 'Cykl Próbny';
+$_['entry_trial_frequency'] = 'Próbna częstotliwość';
+$_['entry_status']			= 'Status';
+$_['entry_sort_order']		= 'Kolejność sortowania';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'Sklepowy kod towaru';
-$_['help_upc']               = 'Uniwersalny kod produktu - Amerykańsko-Kanadyjski';
-$_['help_ean']               = 'Europejski numer artykułu';
-$_['help_jan']               = 'Japoński numer artykułu';
-$_['help_isbn']              = 'Międzynarodowy standard  numeracji książek';
-$_['help_mpn']               = 'Fabryczny numer partii';
-$_['help_manufacturer']      = '(Autouzupełnianie)';
-$_['help_minimum']           = 'Minimalna ilość w zamówieniu';
-$_['help_stock_status']      = 'Status braku Produktu na stanie magazynowym';
-$_['help_points']            = 'Ilość punktów koniecznych do zakupu. Jeśli nie ma być dostępny za punkty, zostaw 0.';
-$_['help_category']          = '(Autouzupełnianie)';
-$_['help_filter']            = '(Autouzupełnianie)';
-$_['help_download']          = '(Autouzupełnianie)';
-$_['help_related']           = '(Autouzupełnianie)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'Nazwa';
+$_['column_sort_order']	    = 'Kolejność sortowania';
+$_['column_action']         = 'Akcja';
 
 // Error
-$_['error_warning']          = 'Uwaga: Sprawdź dokładnie formularz pod kątem błędów!';
-$_['error_permission']       = 'Uwaga: Nie masz uprawnień do modyfikowania produktów!';
-$_['error_name']             = 'Uwaga: Nazwa produktu musi zawierać od 3 do 255 znaków!';
-$_['error_meta_title']       = 'Nazwa kategorii musi zawierać od 3 do 255 znaków!';
-$_['error_model']            = 'Uwaga: Model produktu musi zawierać od 3 do 64 znaków!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Uwaga: Sprawdź dokładnie formularz pod kątem błędów!';
+$_['error_permission']		= 'Uwaga: nie posiadasz wystarczających uprawnień aby zmieniać profile ratalne!';
+$_['error_name']			= 'Nazwa profilu musi być dłuższa niż 3 znaki i krótsza niż 255 znaków!';
+$_['error_product']			= 'Uwaga: profil ratalny nie może zostać usunięty, ponieważ jest przypisany do %s produktów!';
