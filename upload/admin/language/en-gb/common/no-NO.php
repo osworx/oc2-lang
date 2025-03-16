fb72f148bfe25b01faa12439ad2b60831d7e96f4
OSWorX <info@osworx.net>
2025-03-16 14:27:02 +0100

    New translations filemanager.php (Norwegian)

--- upload/admin/language/en-gb/common/no-NO.php
+++ upload/admin/language/en-gb/common/no-NO.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Kontrollpanel';
+$_['heading_title']    = 'Bildebehandler';
+
+// Text
+$_['text_uploaded']    = 'Filen ble lastet opp.';
+$_['text_directory']   = 'Mappe ble opprettet.';
+$_['text_delete']      = 'Filen eller mappen ble slettet.';
+
+// Entry
+$_['entry_search']     = 'Søk ...';
+$_['entry_folder']     = 'Mappenavn';
 
 // Error
-$_['error_install'] = 'Installasjonsmappen bør slettes av sikkerhetshensyn.';
+$_['error_permission'] = 'Tilgang nektet.';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'En fil eller mappe med samme navn finnes allerede.';
+$_['error_directory']  = 'Mappe finnes ikke.';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Ugyldig filtype.';
+$_['error_upload']     = 'Filen kunne ikke lastes opp av ukjent grunn.';
+$_['error_delete']     = 'Du kan ikke slette denne mappen.';
