b76da4b1ddd63a55f845d1db94a970b38105f282
OSWorX <info@osworx.net>
2025-03-16 14:28:43 +0100

    New translations forgotten.php (Swedish)

--- upload/admin/language/en-gb/common/sv-SE.php
+++ upload/admin/language/en-gb/common/sv-SE.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Glömt ditt lösenord?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Version %s';
+$_['text_forgotten']  = 'Glömt lösenordet';
+$_['text_your_email'] = 'Din e-postadress';
+$_['text_email']      = 'Ange e-postadressen för ditt konto. Klicka därefter på "Fortsätt" så skickas en återställningslänk för ditt lösenord till din adress.';
+$_['text_success']    = 'Ett e-postmeddelande med en återställningslänk har skickats till din e-postadress.';
+
+// Entry
+$_['entry_email']     = 'E-postadress';
+$_['entry_password']  = 'Nytt lösenord';
+$_['entry_confirm']   = 'Bekräfta lösenordet';
+
+// Error
+$_['error_email']     = 'Fel: E-postadressen kunde inte hittas i registret, försök igen!';
+$_['error_password']  = 'Password must be between 4 and 20 characters!';
+$_['error_confirm']   = 'Lösenordet och bekräftelsen överensstämmer inte!';
