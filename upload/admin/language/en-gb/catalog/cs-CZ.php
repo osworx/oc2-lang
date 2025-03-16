2902814b1a969dac19c59281d54ace4e41b1a470
OSWorX <info@osworx.net>
2025-03-16 14:19:06 +0100

    New translations filter.php (Czech)

--- upload/admin/language/en-gb/catalog/cs-CZ.php
+++ upload/admin/language/en-gb/catalog/cs-CZ.php
@@ -1,35 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = 'Soubory ke stažení';
+$_['heading_title']     = 'Filtry';
 
 // Text
-$_['text_success']      = 'Úspěch: Změnili jste soubory ke stažení!';
-$_['text_list']         = 'Seznam souborů ke stažení';
-$_['text_add']          = 'Přidat ke stažení';
-$_['text_edit']         = 'Upravit ke stažení';
-$_['text_upload']       = 'Váš soubor byl úspěšně nahrán!';
+$_['text_success']      = 'Úspěch: Upravili jste filtry!';
+$_['text_list']         = 'Seznam filtrů';
+$_['text_add']          = 'Přidat filtr';
+$_['text_edit']         = 'Upravit filtr';
 
 // Column
-$_['column_name']       = 'Název souboru ke stažení';
-$_['column_date_added'] = 'Datum přidání';
+$_['column_group']      = 'Skupina filtrů';
+$_['column_sort_order'] = 'Řadit podle';
 $_['column_action']     = 'Akce';
 
 // Entry
-$_['entry_name']        = 'Název souboru ke stažení';
-$_['entry_filename']    = 'Název souboru';
-$_['entry_mask']        = 'Maska';
-
-// Help
-$_['help_filename']     = 'Můžete nahrát přes tlačítko nahrát nebo pomocí FTP upload na adresář pro stahování a zadejte níže uvedené údaje.';
-$_['help_mask']         = 'Doporučuje se, aby se název souboru a maska lišily, aby se zabránilo lidem snažícím se přímo napojit na Vaše stahování.';
+$_['entry_group']       = 'Název filtru skupiny';
+$_['entry_name']        = 'Název filtru';
+$_['entry_sort_order']  = 'Řadit podle';
 
 // Error
 $_['error_warning']     = 'Upozornění: Zkontrolujte formulář pečlivě pro chyby!';
-$_['error_permission']  = 'Upozornění: Nemáte oprávnění ke změně stahování!';
-$_['error_name']        = 'Název stahování musí být v rozmezí 3 až 64 znaků!';
-$_['error_upload']      = 'Nahrání je požadované!';
-$_['error_filename']    = 'Název souboru musí být v rozmezí 3 až 128 znaků!';
-$_['error_exists']      = 'Soubor neexistuje!';
-$_['error_mask']        = 'Maska musí být v rozmezí 3 až 128 znaků!';
-$_['error_filetype']    = 'Neplatný typ souboru!';
-$_['error_product']     = 'Upozornění: Tento Download nelze odstranit, protože je aktuálně přiřazen k %s produktů!';
+$_['error_permission']  = 'Upozornění: Nemáte oprávnění k úpravě filtrů!';
+$_['error_group']       = 'Název skupiny filtru musí být mezi 1 až 64 znaků!';
+$_['error_name']        = 'Název filtru musí být v rozmezí 1 až 64 znaků!';
