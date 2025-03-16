12eb691d769b763df47e1f1757b0b73839a0c0a9
OSWorX <info@osworx.net>
2025-03-16 11:07:12 +0100

    New translations category.php (Estonian)

--- upload/admin/language/en-gb/catalog/et-EE.php
+++ upload/admin/language/en-gb/catalog/et-EE.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Tunnuste rühm';
+$_['heading_title']          = 'Kategooriad';
 
 // Text
-$_['text_success']      = 'Tunnuste rühm edukalt muudetud!';
-$_['text_list']         = 'Tunnuste rühmaloend';
-$_['text_add']          = 'Lisa tunnuste rühm';
-$_['text_edit']         = 'Redigeeri tunnuste rühma';
+$_['text_success']           = 'Õnnestus: Te olete muutnud kategooriaid!';
+$_['text_list']              = 'Kategooria nimekiri';
+$_['text_add']               = 'Lisa Kategooria';
+$_['text_edit']              = 'Muuda kategooriat';
+$_['text_default']           = 'Vaikimisi';
 
 // Column
-$_['column_name']       = 'Tunnuste rühma nimi';
-$_['column_sort_order'] = 'Sorteerimis järjekord';
-$_['column_action']     = 'Tegevus';
+$_['column_name']            = 'Kategooria nimi';
+$_['column_sort_order']      = 'Sorteerimis järjekord';
+$_['column_action']          = 'Tegevus';
 
 // Entry
-$_['entry_name']        = 'Tunnuste rühma nimi';
-$_['entry_sort_order']  = 'Sorteerimis järjekord';
+$_['entry_name']             = 'Kategooria nimi';
+$_['entry_description']      = 'Kirjeldus';
+$_['entry_meta_title'] 	     = 'Meta Tag pealkiri';
+$_['entry_meta_keyword']     = 'Sisukirjelduse märksõnad';
+$_['entry_meta_description'] = 'Sisukirjelduse kirjeldus';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Vanem';
+$_['entry_filter']           = 'Filtrid';
+$_['entry_store']            = 'Poed';
+$_['entry_image']            = 'Pilt';
+$_['entry_top']              = 'Üles';
+$_['entry_column']           = 'Väljad';
+$_['entry_sort_order']       = 'Sorteerimis järjekord';
+$_['entry_status']           = 'Staatus';
+$_['entry_layout']           = 'Asetuse ülekirjutamine';
+
+// Help
+$_['help_filter']            = '(Automaatselt lõpetatud)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Ülemise Menüüriba kuvamine. Toimib ainult ülemkategooriate puhul.';
+$_['help_column']            = 'Number of columns to use for the bottom 3 categories. Only works for the top parent categories.';
 
 // Error
-$_['error_warning']     = 'Hoiatus: Palun kontrollige vormi vead hoolikalt üle!';
-$_['error_permission']  = 'Hoiatus: Teil ei ole õigust tunnuseid muuta!';
-$_['error_name']        = 'Atribuudi nimi peab olema vahemikus 3 kuni 64 märki!';
-$_['error_attribute']   = 'Hoiatus: Seda tunnuste rühma ei saa kustutada, sest see on hetkel määratud  %s tootes!';
-$_['error_product']     = 'Hoiatus: Seda tunnust ei saa kustutada, kuna see sisaldub hetkel  %s tootes!';
+$_['error_warning']          = 'Hoiatus: Palun kontrollige vormi vead hoolikalt üle!';
+$_['error_permission']       = 'Hoiatus! Teil pole luba muuta kategooriaid!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Meta pealkiri peab olema üle 3 ja alla 255 tähemärgi!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
