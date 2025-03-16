94b50af8ac2e12784f7380718971b1be4fd2f46f
OSWorX <info@osworx.net>
2025-03-16 11:19:22 +0100

    New translations header.php (Slovak)

--- upload/admin/language/en-gb/common/sk-SK.php
+++ upload/admin/language/en-gb/common/sk-SK.php
@@ -1,19 +1,23 @@
 <?php
-// header
-$_['heading_title']   = 'Zabudli ste Vaše heslo?';
+// Heading
+$_['heading_title']          = 'OpenCart';
 
 // Text
-$_['text_forgotten']  = 'Zabudnuté heslo';
-$_['text_your_email'] = 'Vaša E-mailová adresa';
-$_['text_email']      = 'Vložte emailovú adresu používanú s vašim účtom. Kliknite potvrdiť, aby ste dostali na email odkaz na obnovu hesla.';
-$_['text_success']    = 'Email s potvrdzujúcim odkazom bol odoslaný na vašú admin emailovú adresu.';
-
-// Entry
-$_['entry_email']     = 'E-mailová adresa';
-$_['entry_password']  = 'Nové Heslo';
-$_['entry_confirm']   = 'Potvrdiť';
-
-// Error
-$_['error_email']     = 'Upozornenie: Emailová adresa nebola nájdená v našich záznamoch, prosím opakujte znovu!';
-$_['error_password']  = 'Heslo musí byť dlhšie ako 4 znaky a kratšie než 20 znakov!';
-$_['error_confirm']   = 'Heslo a potvrdenie hesla sa nezhodujú!';
+$_['text_order']             = 'Objednávky';
+$_['text_processing_status'] = 'Processing';
+$_['text_complete_status']   = 'Dokončené';
+$_['text_customer']          = 'Zákazníci';
+$_['text_online']            = 'Online zákazníci';
+$_['text_approval']          = 'Čakajúce na schválenie';
+$_['text_product']           = 'Produkty';
+$_['text_stock']             = 'Vypredané';
+$_['text_review']            = 'Recenzie';
+$_['text_return']            = 'Reklamácie';
+$_['text_affiliate']         = 'Partnerský program';
+$_['text_store']             = 'Obchody';
+$_['text_front']             = 'Výklad';
+$_['text_help']              = 'Pomocník';
+$_['text_homepage']          = 'OpenCart Homepage';
+$_['text_support']           = 'Fórum Podpory';
+$_['text_documentation']     = 'Dokumentácia';
+$_['text_logout']            = 'Odhlásiť sa';
