2dbab5997efdc4f2d6a675bbf0ddbbf2bb2dac0a
OSWorX <info@osworx.net>
2025-03-16 11:09:33 +0100

    New translations information.php (Dutch)

--- upload/admin/language/en-gb/catalog/nl-NL.php
+++ upload/admin/language/en-gb/catalog/nl-NL.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'Filters';
+$_['heading_title']          = 'Informatiepagina\'s';
 
 // Text
-$_['text_success']      = 'Succes: Instellingen gewijzigd!';
-$_['text_list']         = 'Lijst';
-$_['text_add']          = 'Toevoegen';
-$_['text_edit']         = 'Wijzigen';
+$_['text_success']           = 'Succes: Instellingen gewijzigd!';
+$_['text_list']              = 'Lijst';
+$_['text_add']               = 'Toevoegen';
+$_['text_edit']              = 'Wijzigen';
+$_['text_default']           = 'Hoofdwinkel';
 
 // Column
-$_['column_group']      = 'Filtergroep(en)';
-$_['column_sort_order'] = 'Sorteervolgorde';
-$_['column_action']     = 'Aktie';
+$_['column_title']           = 'Paginatitel';
+$_['column_sort_order']	     = 'Sorteervolgorde';
+$_['column_action']          = 'Aktie';
 
 // Entry
-$_['entry_group']       = 'Filtergroep naam';
-$_['entry_name']        = 'Filter naam';
-$_['entry_sort_order']  = 'Sorteervolgorde';
+$_['entry_title']            = 'Paginatitel';
+$_['entry_description']      = 'Beschrijving';
+$_['entry_store']            = 'Winkel(s)';
+$_['entry_meta_title'] 	     = 'Meta Tag Titel';
+$_['entry_meta_keyword'] 	 = 'Meta Tag Zoekwoorden';
+$_['entry_meta_description'] = 'Meta Tag Omschrijving';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Footer';
+$_['entry_status']           = 'status';
+$_['entry_sort_order']       = 'Sorteervolgorde';
+$_['entry_layout']           = 'Layout';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Weergeven in footer.';
 
 // Error
-$_['error_warning']     = 'Waarschuwing: Controlleer of alle verplichte velden volledig zijn ingevuld!';
-$_['error_permission']  = 'Waarschuwing: U heeft geen rechten deze instellingen te wijzigen!';
-$_['error_group']       = 'Waarschuwing: Naam filtergroep dient tussen de 1 en 64 tekens lag te zijn!';
-$_['error_name']        = 'Waarschuwing: Naam filter dient tussen de 1 en 64 tekens lag te zijn!';
+$_['error_warning']          = 'Waarschuwing: Controlleer of alle verplichte velden volledig zijn ingevuld!';
+$_['error_permission']       = 'Waarschuwing: U heeft geen rechten deze instellingen te wijzigen!';
+$_['error_title']            = 'Waarschuwing: Informatie-naam dient tussen de 3 en 64 tekens lang te zijn!';
+$_['error_description']      = 'Waarschuwing: Omschrijving dient dient minimaal 3 tekens lang te zijn!';
+$_['error_meta_title']       = 'Waarschuwing: Meta Titel dient tussen de 3 en 255 tekens lang te zijn!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'Waarschuwing: Deze informatie-pagina kan niet verwijderd worden omdat deze is ingesteld als de standaard Account voorwaarden!';
+$_['error_checkout']         = 'Waarschuwing: Deze informatie-pagina kan niet verwijderd worden omdat deze is ingesteld als de stadaard Algemene voorwaarden!';
+$_['error_affiliate']        = 'Waarschuwing: Deze informatie-pagina kan niet verwijderd worden omdat deze wordt gebruikt als Affiliate voorwaarden!';
+$_['error_return']           = 'Waarschuwing: Deze informatie-pagina kan niet verwijderd worden omdat deze wordt gebruikt als Retourvoorwaarden!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
