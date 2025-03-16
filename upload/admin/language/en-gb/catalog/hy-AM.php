fc0b1080cd18926185fb4d801ab4829e9d700b2f
OSWorX <info@osworx.net>
2025-03-16 14:23:11 +0100

    New translations recurring.php (Armenian)

--- upload/admin/language/en-gb/catalog/hy-AM.php
+++ upload/admin/language/en-gb/catalog/hy-AM.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'Ապրանքներ';
+$_['heading_title']			= 'Recurring Profiles';
 
 // Text
-$_['text_success']           = 'Ապրանքների ցուցակը թարմացված են!';
-$_['text_list']              = 'Ապրանքների ցուցակ';
-$_['text_add']               = 'Ավելացնել ապրանք';
-$_['text_edit']              = 'Խմբագրել ապրանք';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'Դեֆոլտ';
-$_['text_option']            = 'Օպցիա';
-$_['text_option_value']      = 'Օպցիայի բովանդակությունը';
-$_['text_percent']           = 'Տոկոսը';
-$_['text_amount']            = 'Ֆիքսված գումարը';
-
-// Column
-$_['column_name']            = 'Ապրանքի անվանումը';
-$_['column_model']           = 'Մոդելը';
-$_['column_image']           = 'Նկար';
-$_['column_price']           = 'Գինը';
-$_['column_quantity']        = 'Քանակը';
-$_['column_status']          = 'Կարգավիճակ';
-$_['column_action']          = 'Գործողություն';
+$_['text_success']          = 'Հաջողությամբ recurring profiles-ը թարմացված են!';
+$_['text_list']             = 'Recurring Profile-ների ցուցակ';
+$_['text_add']              = 'Ավելացնել Recurring Profile';
+$_['text_edit']             = 'Խմբագրել Recurring Profile';
+$_['text_day']				= 'Օրեր';
+$_['text_week']				= 'Այս Շաբաթ';
+$_['text_semi_month']		= 'Ամսվա կես';
+$_['text_month']			= 'Ամիս';
+$_['text_year']				= 'Տարի';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Recurring amounts are calculated by the frequency and cycles.</p><p>For example if you use a frequency of "week" and a cycle of "2", then the user will be billed every 2 weeks.</p><p>The duration is the number of times the user will make a payment, set this to 0 if you want payments until they are cancelled.</p>';
+$_['text_profile']			= 'Recurring Profile';
+$_['text_trial']			= 'Trial Profile';
 
 // Entry
-$_['entry_name']             = 'Ապրանքի անվանումը';
-$_['entry_description']      = 'Նկարագրությունը';
-$_['entry_meta_title'] 	     = 'Մետա տեգի վերնագիրը';
-$_['entry_meta_keyword'] 	 = 'Մետա Տեգի հիմնաբառերը';
-$_['entry_meta_description'] = 'Մեթա նկարագրությունը';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Մոդելը';
-$_['entry_sku']              = 'Արտադրողի կոդը';
-$_['entry_upc']              = 'Շտրիխ կոդը';
-$_['entry_ean']              = 'EAN կոդը';
-$_['entry_jan']              = 'JAN կոդը';
-$_['entry_isbn']             = 'ISBN կոդը';
-$_['entry_mpn']              = 'MPN կոդը';
-$_['entry_location']         = 'Գտնվելու վայրը';
-$_['entry_shipping']         = 'Անհրաժեշտ է առաքում';
-$_['entry_manufacturer']     = 'Արտադրող';
-$_['entry_store']            = 'Խանութներ';
-$_['entry_date_available']   = 'Ներմուծման ամսաթիվը';
-$_['entry_quantity']         = 'Քանակը';
-$_['entry_minimum']          = 'Նվազագույն պատվերի քանակը';
-$_['entry_stock_status']     = 'Ապրանքի առկայության կարգավիճակը';
-$_['entry_price']            = 'Գինը';
-$_['entry_tax_class']        = 'Հարկման կարգ';
-$_['entry_points']           = 'Միավորները';
-$_['entry_option_points']    = 'Միավորները';
-$_['entry_subtract']         = 'Պակասեցնել Պահեստից';
-$_['entry_weight_class']     = 'Քաշի չափման միավորը';
-$_['entry_weight']           = 'Քաշը';
-$_['entry_dimension']        = 'Չափսերը (Երկար. x Լայն. x Բարձր.)';
-$_['entry_length_class']     = 'Երկարության չափման միավորը';
-$_['entry_length']           = 'Երկարությունը';
-$_['entry_width']            = 'Լայնություն';
-$_['entry_height']           = 'Բարձրություն';
-$_['entry_image']            = 'Նկար';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Հաճախորդների խումբը';
-$_['entry_date_start']       = 'Մեկնարկի ամսաթիվը';
-$_['entry_date_end']         = 'Ավարտի ամսաթիվը';
-$_['entry_priority']         = 'Առաջնահերթություն';
-$_['entry_attribute']        = 'Ատրիբուտը';
-$_['entry_attribute_group']  = 'Հատկանիշների խումբ';
-$_['entry_text']             = 'Տեքստը';
-$_['entry_option']           = 'Օպցիա';
-$_['entry_option_value']     = 'Օպցիայի բովանդակությունը';
-$_['entry_required']         = 'Պահանջվւմ է';
-$_['entry_status']           = 'Կարգավիճակ';
-$_['entry_sort_order']       = 'Դասավորման կարգ';
-$_['entry_category']         = 'Կատեգորիա';
-$_['entry_filter']           = 'Ֆիլտր';
-$_['entry_download']         = 'Ներբեռնումներ';
-$_['entry_related']          = 'Նմանատիպ ապրանքներ';
-$_['entry_tag']          	 = 'Ապրանքի Տեգերը';
-$_['entry_reward']           = 'Պարգեւատրման միավորներ';
-$_['entry_layout']           = 'Փոփոխել դասավորությունը';
-$_['entry_recurring']        = 'Recurring Profile';
+$_['entry_name']		    = 'Անուն';
+$_['entry_price']			= 'Գինը';
+$_['entry_duration']		= 'Տևողություն';
+$_['entry_cycle']			= 'Շրջան';
+$_['entry_frequency']		= 'Հաճախություն';
+$_['entry_trial_price']		= 'Trial price';
+$_['entry_trial_duration']	= 'Trial duration';
+$_['entry_trial_status']	= 'Trial status';
+$_['entry_trial_cycle']	    = 'Փորձաշրջան';
+$_['entry_trial_frequency'] = 'Trial frequency';
+$_['entry_status']			= 'Կարգավիճակ';
+$_['entry_sort_order']		= 'Դասավորման կարգ';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'Արտադրողի կոդը';
-$_['help_upc']               = 'Շտրիխ կոդը';
-$_['help_ean']               = 'European Article Number';
-$_['help_jan']               = 'Japanese Article Number';
-$_['help_isbn']              = 'International Standard Book Number';
-$_['help_mpn']               = 'Manufacturer Part Number';
-$_['help_manufacturer']      = '(Ինքնալրացում)';
-$_['help_minimum']           = 'Պատվերի պարտադիր մինիմալ քանակը';
-$_['help_stock_status']      = 'Կարգավճակը ցույց է տրվում երբ ապրանքը պահեստում վերջացել է';
-$_['help_points']            = 'Ապրանքի գնման համար անհրաժեշտ միավորները. Եթե չեք ցանկանում որ միավորներով գնեն, մուտքագրեք 0.';
-$_['help_category']          = '(Ինքնալրացում)';
-$_['help_filter']            = '(Ինքնալրացում)';
-$_['help_download']          = '(Ինքնալրացում)';
-$_['help_related']           = '(Ինքնալրացում)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'Անուն';
+$_['column_sort_order']	    = 'Դասավորման կարգ';
+$_['column_action']         = 'Գործողություն';
 
 // Error
-$_['error_warning']          = 'Զգուշացում. Խնդրում ենք ստուգել ձեւաթուղթը ուշադրությամբ, այն պետք է լինի առանց սխալների!';
-$_['error_permission']       = 'Զգուշացում. Դուք թույլտվություն չունեք փոփոխելու ապրանքները!';
-$_['error_name']             = 'Ապրանքի անվանումը պետք է լինի ավելի մեծ, քան 3 եւ ավելի քիչ, քան 255 նիշ!';
-$_['error_meta_title']       = 'Մետա վերնագիրը պետք է լինի ավելի մեծ, քան 3 եւ ավելի քիչ, քան 255 նիշ!';
-$_['error_model']            = 'Ապրանքի մոդելը պետք է լինի ավելի մեծ, քան 1 եւ ավելի քիչ, քան 64 նիշ!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Զգուշացում. Խնդրում ենք ստուգել ձեւաթուղթը ուշադրությամբ, այն պետք է լինի առանց սխալների!';
+$_['error_permission']		= 'Զգուշացում. Ձեզ թուլատրված չէ փոփոխելու recurring profiles-ը!';
+$_['error_name']			= 'Պրոֆիլի անունը պետք է լինի 3-ից 255 նիշ!';
+$_['error_product']			= 'Զգուշացում։Այս recurring profile չի կարող ջնջվել, քանի որ արդեն վերագրված է %s ապրանքներին!';
