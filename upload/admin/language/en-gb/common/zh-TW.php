bc98023ee427143b1df1981d97aad289423e8991
OSWorX <info@osworx.net>
2025-03-16 11:17:00 +0100

    New translations filemanager.php (Chinese Traditional)

--- upload/admin/language/en-gb/common/zh-TW.php
+++ upload/admin/language/en-gb/common/zh-TW.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = '後台管理';
+$_['heading_title']    = '圖像管理';
+
+// Text
+$_['text_uploaded']    = '您的文件已成功上載 !';
+$_['text_directory']   = '目錄創建成功！';
+$_['text_delete']      = '成功：您的檔案或目錄已刪除！';
+
+// Entry
+$_['entry_search']     = '搜尋...';
+$_['entry_folder']     = '資料夾名稱';
 
 // Error
-$_['error_install'] = '警告： 安裝資料夾仍然存在，出於安全原因請刪除該資料夾 ！';
+$_['error_permission'] = '警告：沒有使用權限！';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = '警告：已經有相同名稱的檔案或目錄存在！';
+$_['error_directory']  = '警告：目錄不存在！';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = '警告： 不正確的檔案類型 ！';
+$_['error_upload']     = '警告: 無法上載檔, 原因不明!';
+$_['error_delete']     = '警告: 您不能刪除此目錄!';
