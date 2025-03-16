901fb2e19964b372a55c248566069f7245c23460
OSWorX <info@osworx.net>
2025-03-16 14:22:24 +0100

    New translations product.php (Italian)

--- upload/admin/language/en-gb/catalog/it-IT.php
+++ upload/admin/language/en-gb/catalog/it-IT.php
@@ -1,40 +1,109 @@
 <?php
 // Heading
-$_['heading_title']      = 'Opzioni';
+$_['heading_title']          = 'Prodotti';
 
 // Text
-$_['text_success']       = 'Hai modificato le opzioni con successo!';
-$_['text_list']          = 'Elenco opzioni';
-$_['text_add']           = 'Aggiungi opzione';
-$_['text_edit']          = 'Modifica opzione';
-$_['text_choose']        = 'Scegli';
-$_['text_select']        = 'Select';
-$_['text_radio']         = 'Radio';
-$_['text_checkbox']      = 'Checkbox';
-$_['text_input']         = 'Input';
-$_['text_text']          = 'Testo';
-$_['text_textarea']      = 'Area di testo';
-$_['text_file']          = 'File';
-$_['text_date']          = 'Data';
-$_['text_datetime']      = 'Data e Ora';
-$_['text_time']          = 'Orario';
+$_['text_success']           = 'Hai modificato i prodotti con successo!';
+$_['text_list']              = 'Elenco prodotti';
+$_['text_add']               = 'Aggiungi prodotto';
+$_['text_edit']              = 'Modifica il Prodotto';
+$_['text_plus']              = '+';
+$_['text_minus']             = '-';
+$_['text_default']           = 'Predefinita';
+$_['text_option']            = 'Opzione';
+$_['text_option_value']      = 'Valore opzione';
+$_['text_percent']           = 'Percentuale';
+$_['text_amount']            = 'Importo fisso';
 
 // Column
-$_['column_name']        = 'Nome opzione';
-$_['column_sort_order']  = 'Ordinamento';
-$_['column_action']      = 'Azione';
+$_['column_name']            = 'Nome prodotto';
+$_['column_model']           = 'Modello';
+$_['column_image']           = 'Immagine';
+$_['column_price']           = 'Prezzo';
+$_['column_quantity']        = 'Quantità';
+$_['column_status']          = 'Stato';
+$_['column_action']          = 'Azione';
 
 // Entry
-$_['entry_name']         = 'Nome opzione';
-$_['entry_type']         = 'Tipologia';
-$_['entry_option_value'] = 'Valore opzione';
-$_['entry_image']        = 'Immagine';
-$_['entry_sort_order']   = 'Ordinamento';
+$_['entry_name']             = 'Nome prodotto';
+$_['entry_description']      = 'Descrizione';
+$_['entry_meta_title'] 	     = 'Meta Tag Title';
+$_['entry_meta_keyword'] 	 = 'Meta Tag Keywords';
+$_['entry_meta_description'] = 'Meta Tag Description';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_model']            = 'Modello';
+$_['entry_sku']              = 'SKU';
+$_['entry_upc']              = 'UPC';
+$_['entry_ean']              = 'EAN';
+$_['entry_jan']              = 'JAN';
+$_['entry_isbn']             = 'ISBN';
+$_['entry_mpn']              = 'MPN';
+$_['entry_location']         = 'Posizione';
+$_['entry_shipping']         = 'Richiede la spedizione';
+$_['entry_manufacturer']     = 'Produttore';
+$_['entry_store']            = 'Negozi';
+$_['entry_date_available']   = 'Data disponibilità';
+$_['entry_quantity']         = 'Quantità';
+$_['entry_minimum']          = 'Quantità minima';
+$_['entry_stock_status']     = 'Non disponibile';
+$_['entry_price']            = 'Prezzo';
+$_['entry_tax_class']        = 'Classe di imposta';
+$_['entry_points']           = 'Punti';
+$_['entry_option_points']    = 'Punti';
+$_['entry_subtract']         = 'Sottrarre al magazzino';
+$_['entry_weight_class']     = 'Classe di peso';
+$_['entry_weight']           = 'Peso';
+$_['entry_dimension']        = 'Dimensioni (L x P x A)';
+$_['entry_length_class']     = 'Classe di lunghezza';
+$_['entry_length']           = 'Lunghezza';
+$_['entry_width']            = 'Profondità';
+$_['entry_height']           = 'Altezza';
+$_['entry_image']            = 'Immagine';
+$_['entry_additional_image'] = 'Additional Images';
+$_['entry_customer_group']   = 'Gruppo clienti';
+$_['entry_date_start']       = 'Data inizio';
+$_['entry_date_end']         = 'Data fine';
+$_['entry_priority']         = 'Priorità';
+$_['entry_attribute']        = 'Attributo';
+$_['entry_attribute_group']  = 'Gruppo attributi';
+$_['entry_text']             = 'Testo';
+$_['entry_option']           = 'Opzione';
+$_['entry_option_value']     = 'Valore opzione';
+$_['entry_required']         = 'Obbligatorio';
+$_['entry_status']           = 'Stato';
+$_['entry_sort_order']       = 'Ordinamento';
+$_['entry_category']         = 'Categorie';
+$_['entry_filter']           = 'Filtri
+';
+$_['entry_download']         = 'Download';
+$_['entry_related']          = 'Prodotti correlati';
+$_['entry_tag']          	 = 'Tags dei prodotti';
+$_['entry_reward']           = 'Punti premio';
+$_['entry_layout']           = 'Sovrascrivere layout';
+$_['entry_recurring']        = 'Profilo ricorrente';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_sku']               = 'Stock Keeping Unit';
+$_['help_upc']               = 'Universal Product Code';
+$_['help_ean']               = 'European Article Number';
+$_['help_jan']               = 'Japanese Article Number';
+$_['help_isbn']              = 'International Standard Book Number';
+$_['help_mpn']               = 'Manufacturer Part Number';
+$_['help_manufacturer']      = '(Completamento automatico)';
+$_['help_minimum']           = 'Forzare quantità minima d\'ordine';
+$_['help_stock_status']      = 'Mostra lo stato prodotto esaurito';
+$_['help_points']            = 'Numero di punti necessari per acquistare questo articolo. Se non volete che questo prodotto sia acquistato con i punti lasciate 0.';
+$_['help_category']          = '(Completamento automatico)';
+$_['help_filter']            = '(Completamento automatico)';
+$_['help_download']          = '(Completamento automatico)';
+$_['help_related']           = '(Completamento automatico)';
+$_['help_tag']               = 'Comma separated';
 
 // Error
-$_['error_warning']      = 'Attenzione: Si prega di controllare attentamente gli errori del modulo!';
-$_['error_permission']   = 'Attenzione: Non hai il permesso per modificare le opzioni!';
-$_['error_name']         = 'Il nome dell\'opzione deve essere compreso tra 1 e 128 caratteri!';
-$_['error_type']         = 'Attenzione: I valori delle opzioni sono necessari!';
-$_['error_option_value'] = 'Il valore delle opzioni deve essere compreso tra 1 e 128 caratteri!';
-$_['error_product']      = 'Attenzione: questa opzione non può essere cancellata poiché è attualmente assegnata a %s prodotti!';
+$_['error_warning']          = 'Attenzione: Si prega di controllare attentamente gli errori del modulo!';
+$_['error_permission']       = 'Attenzione: Non hai il permesso per modificare i prodotti!';
+$_['error_name']             = 'll nome del prodotto deve essere maggiore di 3 e minore di 255 caratteri!';
+$_['error_meta_title']       = 'Il Meta Title deve essere maggiore di 3 e inferiore a 255 caratteri!';
+$_['error_model']            = 'Il Modello del prodotto deve essere maggiore di 1 e minore di 64 caratteri!';
+$_['error_keyword']          = 'SEO URL already in use!';
