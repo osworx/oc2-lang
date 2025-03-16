c49667c0dc31f0dde8796b0efa9ddef428f9989e
OSWorX <info@osworx.net>
2025-03-16 14:16:54 +0100

    New translations attribute_group.php (Polish)

--- upload/admin/language/en-gb/catalog/pl-PL.php
+++ upload/admin/language/en-gb/catalog/pl-PL.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Atrybuty';
+$_['heading_title']     = 'Grupa atrybutów';
 
 // Text
-$_['text_success']           = 'Sukces: Zmodyfikowano atrybuty!';
-$_['text_list']              = 'Lista atrybutów';
-$_['text_add']               = 'Dodaj atrybut';
-$_['text_edit']              = 'Edytuj atrybut';
+$_['text_success']      = 'Sukces: Grupy Atrybutów zostały zmienione!';
+$_['text_list']         = 'Lista grup atrybutów';
+$_['text_add']          = 'Dodaj atrybut grupy';
+$_['text_edit']         = 'Edytuj atrybut grupy';
 
 // Column
-$_['column_name']            = 'Nazwa atrybutu';
-$_['column_attribute_group'] = 'Grupa atrybutów';
-$_['column_sort_order']      = 'Kolejność sortowania';
-$_['column_action']          = 'Akcja';
+$_['column_name']       = 'Nazwa atrybut grupy';
+$_['column_sort_order'] = 'Kolejność sortowania';
+$_['column_action']     = 'Akcja';
 
 // Entry
-$_['entry_name']             = 'Nazwa atrybutu';
-$_['entry_attribute_group']  = 'Grupa atrybutów';
-$_['entry_sort_order']       = 'Kolejność sortowania';
+$_['entry_name']        = 'Nazwa atrybut grupy';
+$_['entry_sort_order']  = 'Kolejność sortowania';
 
 // Error
-$_['error_warning']          = 'Uwaga: Sprawdź dokładnie formularz pod kątem błędów!';
-$_['error_permission']       = 'Ostrzeżenie: Nie masz uprawnień do modyfikowania atrybutów!';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'Nazwa atrybutu musi zawierać od 3 do 64 znaków!';
-$_['error_product']          = 'Ostrzeżenie: Ten atrybut nie może być usunięty, ponieważ jest aktualnie przypisany do %s produktów!';
+$_['error_warning']     = 'Uwaga: Sprawdź dokładnie formularz pod kątem błędów!';
+$_['error_permission']  = 'Ostrzeżenie: Nie masz uprawnień do modyfikowania atrybutów!';
+$_['error_name']        = 'Nazwa grupy atrybut musi być między 3 i 64 znaków!';
+$_['error_attribute']   = 'Ostrzeżenie: Ta grupa atrybutów nie może zostać usunięta, ponieważ jest aktualnie przypisana do %s atrybutów!';
+$_['error_product']     = 'Ostrzeżenie: Ta grupa atrybutów nie może zostać usunięta, ponieważ jest aktualnie przypisana do %s produktów!';
