fdbca5dc97e3cfa7e311b46bdf740245d07f5584
OSWorX <info@osworx.net>
2025-03-16 11:16:23 +0100

    New translations filemanager.php (Romanian)

--- upload/admin/language/en-gb/common/ro-RO.php
+++ upload/admin/language/en-gb/common/ro-RO.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Panou general';
+$_['heading_title']    = 'Manager de imagini';
+
+// Text
+$_['text_uploaded']    = 'Succes: Fișierul dvs. a fost încarcat!';
+$_['text_directory']   = 'Succes: Director creat!';
+$_['text_delete']      = 'Succes: Fișierul sau directorul dvs. a fost șters!';
+
+// Entry
+$_['entry_search']     = 'Caută..';
+$_['entry_folder']     = 'Nume folder';
 
 // Error
-$_['error_install'] = 'Avertisment: Folderul de instalare încă există și trebuie șters din motive de securitate!';
+$_['error_permission'] = 'Avertisment: Acces refuzat!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Avertisment: Un fișier sau un director cu același nume există deja!';
+$_['error_directory']  = 'Avertisment: Directorul nu există!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Avertisment: Tip fișier incorect!';
+$_['error_upload']     = 'Avertisment: Fișierul nu a putut fi încărcat din motive necunoscute!';
+$_['error_delete']     = 'Avertisment: Nu puteți șterge acest director!';
