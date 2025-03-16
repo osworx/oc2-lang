7ec9464a6b30425a3a7c1b6c7098f98e9b922099
OSWorX <info@osworx.net>
2025-03-16 14:21:41 +0100

    New translations option.php (Norwegian)

--- upload/admin/language/en-gb/catalog/no-NO.php
+++ upload/admin/language/en-gb/catalog/no-NO.php
@@ -1,35 +1,40 @@
 <?php
 // Heading
-$_['heading_title']     = 'Produsenter';
+$_['heading_title']      = 'Produktvalg';
 
 // Text
-$_['text_success']      = 'Endringer ble vellykket lagret.';
-$_['text_list']         = 'Liste';
-$_['text_add']          = 'Legg til';
-$_['text_edit']         = 'Endre';
-$_['text_default']      = 'Standard';
-$_['text_percent']      = 'Prosent';
-$_['text_amount']       = 'Fastpris';
+$_['text_success']       = 'Endringer ble vellykket lagret.';
+$_['text_list']          = 'Liste';
+$_['text_add']           = 'Legg til';
+$_['text_edit']          = 'Endre';
+$_['text_choose']        = 'Valg';
+$_['text_select']        = 'Nedtrekksmeny';
+$_['text_radio']         = 'Radioknapp';
+$_['text_checkbox']      = 'Avkryssingsboks';
+$_['text_input']         = 'Inndata';
+$_['text_text']          = 'Tekstfelt';
+$_['text_textarea']      = 'Tekstboks';
+$_['text_file']          = 'Filopplasting';
+$_['text_date']          = 'Datovalg';
+$_['text_datetime']      = 'Dato- og tidsvalg';
+$_['text_time']          = 'Tidsvalg';
 
 // Column
-$_['column_name']       = 'Navn';
-$_['column_sort_order'] = 'Sortering';
-$_['column_action']     = 'Valg';
+$_['column_name']        = 'Navn';
+$_['column_sort_order']  = 'Sortering';
+$_['column_action']      = 'Valg';
 
 // Entry
-$_['entry_name']        = 'Navn';
-$_['entry_store']       = 'Butikker';
-$_['entry_keyword']     = 'SEO URL';
-$_['entry_image']       = 'Bilde';
-$_['entry_sort_order']  = 'Sortering';
-$_['entry_type']        = 'Type';
-
-// Help
-$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['entry_name']         = 'Navn';
+$_['entry_type']         = 'Type';
+$_['entry_option_value'] = 'Verdi';
+$_['entry_image']        = 'Bilde';
+$_['entry_sort_order']   = 'Sortering';
 
 // Error
-$_['error_warning']     = 'Påse at alle påkrevde felter er fylt inn.';
-$_['error_permission']  = 'Du har ikke rettigheter til å utføre valgte handling.';
-$_['error_name']        = 'Navn må inneholde mellom 2 og 64 tegn.';
-$_['error_keyword']     = 'SEO URL already in use!';
-$_['error_product']     = 'Denne produsenten kan ikke slettes ettersom den er tilknyttet %s produkter.';
+$_['error_warning']      = 'Påse at alle påkrevde felter er fylt inn.';
+$_['error_permission']   = 'Du har ikke rettigheter til å utføre valgte handling.';
+$_['error_name']         = 'Navn må inneholde mellom 1 og 128 tegn.';
+$_['error_type']         = 'Verdier må legges til.';
+$_['error_option_value'] = 'Verdi må inneholde mellom 1 og 128 tegn.';
+$_['error_product']      = 'Dette valget kan ikke slettes ettersom det er tilknyttet %s produkter.';
