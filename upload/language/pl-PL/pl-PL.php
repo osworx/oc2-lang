8a3fd553b939ca54d36b3c691c66135e91fe0380
OSWorX <info@osworx.net>
2025-03-16 09:12:11 +0100

    New translations footer.php (Polish)

--- upload/language/pl-PL/pl-PL.php
+++ upload/language/pl-PL/pl-PL.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Zarządzanie zdjęciami';
-
 // Text
-$_['text_uploaded']    = 'Sukces: Plik został przesłany!';
-$_['text_directory']   = 'Sukces: Katalog utworzony!';
-$_['text_delete']      = 'Sukces: Plik lub katalog został usunięty!';
-
-// Entry
-$_['entry_search']     = 'Wyszukaj..';
-$_['entry_folder']     = 'Nazwa folderu';
-
-// Error
-$_['error_permission'] = 'Ostrzeżenie: Odmowa uprawnień!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Uwaga: Plik lub katalog o takiej nazwie już istnieje!';
-$_['error_directory']  = 'Ostrzeżenie: Katalog nie istnieje!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Ostrzeżenie: Nieprawidłowy typ pliku!';
-$_['error_upload']     = 'Ostrzeżenie: Nie można przesłać pliku z nieznanego powodu!';
-$_['error_delete']     = 'Ostrzeżenie: Nie można usunąć tego katalogu!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Wersja %s';
