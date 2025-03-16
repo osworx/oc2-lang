0b9e26bfb2b207bc6e31aae9b7fedb937b7a0c2f
OSWorX <info@osworx.net>
2025-03-16 11:21:07 +0100

    New translations reset.php (Indonesian)

--- upload/admin/language/en-gb/common/id-ID.php
+++ upload/admin/language/en-gb/common/id-ID.php
@@ -1,19 +1,15 @@
 <?php
 // header
-$_['heading_title']  = 'Administrasi';
+$_['heading_title']  = 'Reset Kata Sandi Anda';
 
 // Text
-$_['text_heading']   = 'Administrasi';
-$_['text_login']     = 'Silahkan masukkan rincian login anda.';
-$_['text_forgotten'] = 'Lupa Kata Sandi';
+$_['text_password']  = 'Masukkan kata sandi yang ingin digunakan.';
+$_['text_success']   = 'Berhasil: Kata sandi anda telah diperbarui.';
 
 // Entry
-$_['entry_username'] = 'Nama Pengguna';
 $_['entry_password'] = 'Kata Sandi';
-
-// Button
-$_['button_login']   = 'Log Masuk';
+$_['entry_confirm']  = 'Konfirmasi';
 
 // Error
-$_['error_login']    = 'Username dan/atau Kata Sandi tidak cocok.';
-$_['error_token']    = 'Sesi token salah, silahkan log masuk kembali.';
+$_['error_password'] = 'Password harus antara 4 hingga 20 karakter!';
+$_['error_confirm']  = 'Kata sandi dan konfirmasi kata sandi tidak cocok!';
