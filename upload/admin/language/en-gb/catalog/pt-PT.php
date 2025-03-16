3eb6b4ddcb4373d7b54950e18aa8122cbc79857f
OSWorX <info@osworx.net>
2025-03-16 11:08:41 +0100

    New translations filter.php (Portuguese)

--- upload/admin/language/en-gb/catalog/pt-PT.php
+++ upload/admin/language/en-gb/catalog/pt-PT.php
@@ -1,35 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = 'Downloads';
+$_['heading_title']     = 'Filtros';
 
 // Text
-$_['text_success']      = 'Concluído: Modificou os downloads com sucesso!';
-$_['text_list']         = 'Lista de Downloads';
-$_['text_add']          = 'Adicionar Download';
-$_['text_edit']         = 'Editar Download';
-$_['text_upload']       = 'Ficheiro enviado com sucesso!';
+$_['text_success']      = 'Concluído: Modificou os Filtros com sucesso!';
+$_['text_list']         = 'Lista de Filtros';
+$_['text_add']          = 'Adicionar Filtro';
+$_['text_edit']         = 'Editar Filtro';
 
 // Column
-$_['column_name']       = 'Node do Download';
-$_['column_date_added'] = 'Data de Adição';
+$_['column_group']      = 'Grupo de Filtros';
+$_['column_sort_order'] = 'Posição';
 $_['column_action']     = 'Ação';
 
 // Entry
-$_['entry_name']        = 'Node do Download';
-$_['entry_filename']    = 'Nome de Arquivo';
-$_['entry_mask']        = 'Máscara';
-
-// Help
-$_['help_filename']     = 'Pode fazer o upload através do botão de upload ou usar FTP para fazer upload para o diretório de download e inserir os detalhes do Ficheiro abaixo.';
-$_['help_mask']         = 'É recomendável que o nome do ficheiro e a máscara sejam diferentes para impedir que as pessoas tentem aceder diretamente a seus downloads.';
+$_['entry_group']       = 'Nome do Grupo de Filtros';
+$_['entry_name']        = 'Nome do Filtro';
+$_['entry_sort_order']  = 'Posição';
 
 // Error
 $_['error_warning']     = 'Aviso: Por favor, verifique o formulário cuidadosamente para evitar erros!';
-$_['error_permission']  = 'Aviso: Não tem permissões para modificar os Downloads!';
-$_['error_name']        = 'Node do Download deve conter entre 3 e 64 caracteres!';
-$_['error_upload']      = 'Necessário fazer o Upload!';
-$_['error_filename']    = 'Nome do Ficheiro deve conter entre 3 e 128 caracteres!';
-$_['error_exists']      = 'O Ficheiro não existe!';
-$_['error_mask']        = 'Máscara deve conter entre 3 e 128 caracteres!';
-$_['error_filetype']    = 'Tipo de arquivo inválido!';
-$_['error_product']     = 'Aviso: Este download não pode ser excluído, pois esta atualmente atribuído a %s produtos!';
+$_['error_permission']  = 'Aviso: Não tem permissões para modificar os Filtros!';
+$_['error_group']       = 'Nome do Grupo de Filtro deve conter entre 1 e 64 caracteres!';
+$_['error_name']        = 'Nome do Filtro deve conter entre 1 e 64 caracteres!';
