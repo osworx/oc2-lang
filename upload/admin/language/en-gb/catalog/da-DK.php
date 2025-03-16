17fd4fc669338346b66375593f549778f3814116
OSWorX <info@osworx.net>
2025-03-16 14:19:07 +0100

    New translations filter.php (Danish)

--- upload/admin/language/en-gb/catalog/da-DK.php
+++ upload/admin/language/en-gb/catalog/da-DK.php
@@ -1,35 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = 'Downloads';
+$_['heading_title']     = 'Filtre';
 
 // Text
-$_['text_success']      = 'Succes: Du har ændret downloads!';
-$_['text_list']         = 'Downloads';
-$_['text_add']          = 'Tilføj fil til download';
-$_['text_edit']         = 'Rediger download';
-$_['text_upload']       = 'Din fil er blevet uploadet!';
+$_['text_success']      = 'Succes: Du har ændret filtre!';
+$_['text_list']         = 'Filtre';
+$_['text_add']          = 'Tilføj filter';
+$_['text_edit']         = 'Rediger filter';
 
 // Column
-$_['column_name']       = 'Downloadnavn';
-$_['column_date_added'] = 'Tilføjet dato';
+$_['column_group']      = 'Filtergruppe';
+$_['column_sort_order'] = 'Sorteringsrækkefølge';
 $_['column_action']     = 'Handling';
 
 // Entry
-$_['entry_name']        = 'Downloadnavn';
-$_['entry_filename']    = 'Filnavn';
-$_['entry_mask']        = 'Mønster';
-
-// Help
-$_['help_filename']     = 'Du kan uploade ved hjælp af upload-knappen eller bruge FTP til at uploade til download-mappen og tilføje detaljerne herunder.';
-$_['help_mask']         = 'Det anbefales at filnavnet og mønsteret er forskellige for at forhindre andre i at linke direkte til dine filer.';
+$_['entry_group']       = 'Filtergruppenavn';
+$_['entry_name']        = 'Filternavn';
+$_['entry_sort_order']  = 'Sorteringsrækkefølge';
 
 // Error
 $_['error_warning']     = 'Advarsel: Gennemgå venligst formularen for fejl!';
-$_['error_permission']  = 'Advarsel: Du har ikke tilladelse til at ændre downloads!';
-$_['error_name']        = 'Downloadnavn skal være mellem 3 og 64 karakterer!';
-$_['error_upload']      = 'Upload påkrævet!';
-$_['error_filename']    = 'Filnavn skal være mellem 3 og 128 karakterer!';
-$_['error_exists']      = 'Filen findes ikke!';
-$_['error_mask']        = 'Mønsteret skal være mellem 3 og 128 karakterer!';
-$_['error_filetype']    = 'Ugyldig filtype!';
-$_['error_product']     = 'Fejl: Denne download kan ikke slettes, da den i øjeblikket er tilknyttet %s produkter!';
+$_['error_permission']  = 'Fejl: Du har ikke tilladelse til at ændre filtre!';
+$_['error_group']       = 'Filtergruppenavn skal være mellem 1 og 64 karakterer!';
+$_['error_name']        = 'Filternavn skal være mellem 1 og 64 karakterer!';
