6b602b28b20126c010448e392d0208891ee36896
OSWorX <info@osworx.net>
2025-03-16 11:07:55 +0100

    New translations download.php (Chinese Traditional)

--- upload/admin/language/en-gb/catalog/zh-TW.php
+++ upload/admin/language/en-gb/catalog/zh-TW.php
@@ -1,46 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = '類別';
+$_['heading_title']     = '下載';
 
 // Text
-$_['text_success']           = '成功：您已經修改類別！';
-$_['text_list']              = '類別清單';
-$_['text_add']               = '新增類別';
-$_['text_edit']              = '編輯類別';
-$_['text_default']           = '預設';
+$_['text_success']      = '成功：您已經修改下載！';
+$_['text_list']         = '下載清單';
+$_['text_add']          = '新增下載';
+$_['text_edit']         = '編輯下載';
+$_['text_upload']       = '您的檔案已成功上傳！';
 
 // Column
-$_['column_name']            = '類別名稱';
-$_['column_sort_order']      = '排序次序';
-$_['column_action']          = '動作';
+$_['column_name']       = '下載名稱';
+$_['column_date_added'] = '新增日期';
+$_['column_action']     = '動作';
 
 // Entry
-$_['entry_name']             = '類別名稱';
-$_['entry_description']      = '說明';
-$_['entry_meta_title'] 	     = 'Meta 標籤標題';
-$_['entry_meta_keyword']     = 'Meta 標籤關鍵字';
-$_['entry_meta_description'] = 'Meta 標籤描述';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = '上層';
-$_['entry_filter']           = '篩選條件';
-$_['entry_store']            = '商店';
-$_['entry_image']            = '圖像';
-$_['entry_top']              = '頂端';
-$_['entry_column']           = '直欄';
-$_['entry_sort_order']       = '排序次序';
-$_['entry_status']           = '狀態';
-$_['entry_layout']           = '覆蓋版面配置';
+$_['entry_name']        = '下載名稱';
+$_['entry_filename']    = '檔案名稱';
+$_['entry_mask']        = '遮罩';
 
 // Help
-$_['help_filter']            = '(自動完成)';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = '在頂端的功能列中顯示。僅適用於頂端的上層類別。';
-$_['help_column']            = '使用於底端的 3 個類別欄數。僅適用於頂端的上層類別。';
+$_['help_filename']     = '您可以透過上傳按鈕直接上傳或使用 FTP工具上傳資料到下載目錄並輸入下面的詳細資訊。';
+$_['help_mask']         = '建議把檔案與遮罩設為不同的名稱以防止有人試圖直接連結到您的下載。';
 
 // Error
-$_['error_warning']          = '警告：請仔細檢查表格的錯誤！';
-$_['error_permission']       = '警告：您沒有修改類別的權限！';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = 'Meta 標題必須大於 3 且小於 255 個字元之間！';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = '警告：請仔細檢查表格的錯誤！';
+$_['error_permission']  = '警告：您沒有修改下載的權限！';
+$_['error_name']        = '下載名稱必須介於 3 到 64 個字元之間！';
+$_['error_upload']      = '需要上傳！';
+$_['error_filename']    = '檔案名稱必須介於 3 到 128 個字元之間！';
+$_['error_exists']      = '檔案不存在！';
+$_['error_mask']        = '遮罩必須介於 3 到 128 個字元之間！';
+$_['error_filetype']    = '無效的檔案類型！';
+$_['error_product']     = '警告：無法刪除這個下載，因為目前已指派給 %s 個商品！';
