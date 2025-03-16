c17abbecdf339b7201fd3f261e1e4e4bac590aed
OSWorX <info@osworx.net>
2025-03-16 11:20:12 +0100

    New translations login.php (Serbian (Cyrillic))

--- upload/admin/language/en-gb/common/sr-SP.php
+++ upload/admin/language/en-gb/common/sr-SP.php
@@ -1,23 +1,19 @@
 <?php
-// Heading
-$_['heading_title']          = '"OpenCart"';
+// header
+$_['heading_title']  = 'Администрација';
 
 // Text
-$_['text_order']             = 'Поруџбине';
-$_['text_processing_status'] = 'Обрађивање';
-$_['text_complete_status']   = 'Завршено';
-$_['text_customer']          = 'Корисници';
-$_['text_online']            = 'Корисници на мрежи';
-$_['text_approval']          = 'Чека одобрење';
-$_['text_product']           = 'Производи';
-$_['text_stock']             = 'Нема на стању';
-$_['text_review']            = 'Рецензије';
-$_['text_return']            = 'Повраћаји';
-$_['text_affiliate']         = 'Сарадници';
-$_['text_store']             = 'Продавнице';
-$_['text_front']             = 'Излог';
-$_['text_help']              = 'Помоћ';
-$_['text_homepage']          = 'OpenCart Homepage';
-$_['text_support']           = 'Форум за подршку';
-$_['text_documentation']     = 'Документација';
-$_['text_logout']            = 'Одјава';
+$_['text_heading']   = 'Администрација';
+$_['text_login']     = 'Молимо да унесете ваше податке за пријаву.';
+$_['text_forgotten'] = 'Заборављена лозинка';
+
+// Entry
+$_['entry_username'] = 'Корисничко име';
+$_['entry_password'] = 'Лозинка';
+
+// Button
+$_['button_login']   = 'Пријава';
+
+// Error
+$_['error_login']    = 'Нема поклапања за корисничко име и/или лозинку.';
+$_['error_token']    = 'Неисправан токен сесије. Молимо да се пријавите поново.';
