888286391ac5e04a291776ac73e81be2fde4a4a9
OSWorX <info@osworx.net>
2025-03-16 09:06:38 +0100

    New translations attribute.php (Arabic)

---
+++ upload/language/ar-SA/ar-SA.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'الخصائص';
+
+// Text
+$_['text_success']           = 'تم بنجاح: لقد قمت بتعديل الخصائص!';
+$_['text_list']              = 'قائمة الخصائص';
+$_['text_add']               = 'اضافة الخاصيه';
+$_['text_edit']              = 'تعديل الخاصية';
+
+// Column
+$_['column_name']            = 'اسم الخاصية';
+$_['column_attribute_group'] = 'مجموعة الخصائص';
+$_['column_sort_order']      = 'الترتيب';
+$_['column_action']          = 'عمل';
+
+// Entry
+$_['entry_name']             = 'اسم الخاصية';
+$_['entry_attribute_group']  = 'مجموعة الخصائص';
+$_['entry_sort_order']       = 'الترتيب';
+
+// Error
+$_['error_warning']          = 'تحذير: الرجاء التحقق من النموذج واصلاح الأخطاء!';
+$_['error_permission']       = 'تحذير: ليس لديك الإذن بتعديل الخصائص!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'يجب ان يكون اسم الخاصية بين 3 و 64 حرفاً!';
+$_['error_product']          = 'تحذير: هذه الخاصية لا يمكن حذفها لأنها مرتبطه بـ %s منتجات!';
