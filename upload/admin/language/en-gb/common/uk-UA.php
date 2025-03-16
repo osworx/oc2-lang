06c65c7e461b67b5e7d81a0d09c60231f4ea641a
OSWorX <info@osworx.net>
2025-03-16 11:16:59 +0100

    New translations filemanager.php (Ukrainian)

--- upload/admin/language/en-gb/common/uk-UA.php
+++ upload/admin/language/en-gb/common/uk-UA.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Панель керування';
+$_['heading_title']    = 'Менеджер зображень';
+
+// Text
+$_['text_uploaded']    = 'Вітаємо: Ви успішно завантажили файл!';
+$_['text_directory']   = 'Вітаємо: Ви успішно створили теку!';
+$_['text_delete']      = 'Вітаємо: Ви успішно видалили файл або теку!';
+
+// Entry
+$_['entry_search']     = 'Пошук..';
+$_['entry_folder']     = 'Назва теки';
 
 // Error
-$_['error_install'] = 'Увага: Тека "Install" все ще існує. З міркувань безпеки її необхідно видалити!';
+$_['error_permission'] = 'Увага: Доступ заборонено!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Увага: Файл або каталог із таким іменем вже існує!';
+$_['error_directory']  = 'Увага: Теки не існує!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Увага: Неправильний тип файлу!';
+$_['error_upload']     = 'Увага: Файл не вдалося завантажити з невідомих причин!';
+$_['error_delete']     = 'Увага: Ви не можете видалити цю теку!';
