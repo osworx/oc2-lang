e51e90a202a258a83683efb89fef152f7108037d
OSWorX <info@osworx.net>
2025-03-16 14:16:16 +0100

    New translations attribute.php (Chinese Traditional)

--- upload/admin/language/en-gb/catalog/zh-TW.php
+++ upload/admin/language/en-gb/catalog/zh-TW.php
@@ -1,36 +1,27 @@
 <?php
 // Heading
-$_['heading_title']     = '評論';
+$_['heading_title']          = '屬性';
 
 // Text
-$_['text_success']      = '成功：您已經修改評論！';
-$_['text_list']         = '評論清單';
-$_['text_add']          = '新增評論';
-$_['text_edit']         = '編輯評論';
+$_['text_success']           = '成功：您已經修改屬性！';
+$_['text_list']              = '屬性清單';
+$_['text_add']               = '新增屬性';
+$_['text_edit']              = '編輯屬性';
 
 // Column
-$_['column_product']    = '商品';
-$_['column_author']     = '作者';
-$_['column_rating']     = '評分';
-$_['column_status']     = '狀態';
-$_['column_date_added'] = '新增日期';
-$_['column_action']     = '動作';
+$_['column_name']            = '屬性名稱';
+$_['column_attribute_group'] = '屬性群組';
+$_['column_sort_order']      = '排序次序';
+$_['column_action']          = '動作';
 
 // Entry
-$_['entry_product']     = '商品';
-$_['entry_author']      = '作者';
-$_['entry_rating']      = '評分';
-$_['entry_status']      = '狀態';
-$_['entry_text']        = '文字';
-$_['entry_date_added']  = '新增日期';
-
-// Help
-$_['help_product']      = '(自動完成)';
+$_['entry_name']             = '屬性名稱';
+$_['entry_attribute_group']  = '屬性群組';
+$_['entry_sort_order']       = '排序次序';
 
 // Error
-$_['error_warning']     = '警告：請仔細檢查表格的錯誤！';
-$_['error_permission']  = '警告：您沒有修改評論的權限！';
-$_['error_product']     = '需要商品！';
-$_['error_author']      = '作者必須介於 3 到 64 個字元之間！';
-$_['error_text']        = '評論文字必須至少 1 個字元！';
-$_['error_rating']      = '需要評論評分！';
+$_['error_warning']          = '警告：請仔細檢查表格的錯誤！';
+$_['error_permission']       = '警告：您沒有修改屬性的權限！';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = '屬性名稱必須介於 3 到 64 個字元之間！';
+$_['error_product']          = '警告：無法刪除這個屬性，因為目前已指派給 %s 個商品！';
