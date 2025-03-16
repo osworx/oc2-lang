0e82a5ec799ec33c83bd20ae3ee6b38574c46b8b
OSWorX <info@osworx.net>
2025-03-16 11:05:31 +0100

    New translations attribute.php (Hindi)

---
+++ upload/admin/language/en-gb/catalog/hi-IN.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'विशेषता';
+
+// Text
+$_['text_success']           = 'सफलता: आपने विशेषताएँ संशोधित की है!';
+$_['text_list']              = 'विशेषता सूची';
+$_['text_add']               = 'विशेषता जोड़ें';
+$_['text_edit']              = 'विशेषता संशोधित करें';
+
+// Column
+$_['column_name']            = 'विशेषता नाम';
+$_['column_attribute_group'] = 'विशेषता समूह';
+$_['column_sort_order']      = 'क्रम';
+$_['column_action']          = 'कार्यवाही';
+
+// Entry
+$_['entry_name']             = 'विशेषता नाम';
+$_['entry_attribute_group']  = 'विशेषता समूह';
+$_['entry_sort_order']       = 'क्रम';
+
+// Error
+$_['error_warning']          = 'चेतावनी: कृपया ध्यान से त्रुटियों के लिए प्रपत्र की जाँच करें!';
+$_['error_permission']       = 'चेतावनी: आप को विशेषता को संशोधित करने की अनुमति नहीं है!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'विशेषता का नाम 3 और 64 वर्णों के बीच होना चाहिए!';
+$_['error_product']          = 'चेतावनी: इस डाउनलोड को नहीं हटाया जा सकता क्योंकि यह वर्तमान में %s उत्पादों के लिए निश्चित किया गया है!';
