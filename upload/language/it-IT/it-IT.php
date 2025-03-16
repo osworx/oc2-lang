84e39c1475c3def7acd9026c4bba1847dbe75aae
OSWorX <info@osworx.net>
2025-03-16 09:10:09 +0100

    New translations option.php (Italian)

--- upload/language/it-IT/it-IT.php
+++ upload/language/it-IT/it-IT.php
@@ -1,35 +1,40 @@
 <?php
 // Heading
-$_['heading_title']     = 'Produttori';
+$_['heading_title']      = 'Opzioni';
 
 // Text
-$_['text_success']      = 'Hai modificato i produttori con successo!';
-$_['text_list']         = 'Elenco produttori';
-$_['text_add']          = 'Aggiungi produttore';
-$_['text_edit']         = 'Modifica produttore';
-$_['text_default']      = 'Predefinita';
-$_['text_percent']      = 'Percentuale';
-$_['text_amount']       = 'Importo fisso';
+$_['text_success']       = 'Hai modificato le opzioni con successo!';
+$_['text_list']          = 'Elenco opzioni';
+$_['text_add']           = 'Aggiungi opzione';
+$_['text_edit']          = 'Modifica opzione';
+$_['text_choose']        = 'Scegli';
+$_['text_select']        = 'Select';
+$_['text_radio']         = 'Radio';
+$_['text_checkbox']      = 'Checkbox';
+$_['text_input']         = 'Input';
+$_['text_text']          = 'Testo';
+$_['text_textarea']      = 'Area di testo';
+$_['text_file']          = 'File';
+$_['text_date']          = 'Data';
+$_['text_datetime']      = 'Data e Ora';
+$_['text_time']          = 'Orario';
 
 // Column
-$_['column_name']       = 'Nome produttore';
-$_['column_sort_order'] = 'Ordinamento';
-$_['column_action']     = 'Azione';
+$_['column_name']        = 'Nome opzione';
+$_['column_sort_order']  = 'Ordinamento';
+$_['column_action']      = 'Azione';
 
 // Entry
-$_['entry_name']        = 'Nome produttore';
-$_['entry_store']       = 'Negozi';
-$_['entry_keyword']     = 'SEO URL';
-$_['entry_image']       = 'Immagine';
-$_['entry_sort_order']  = 'Ordinamento';
-$_['entry_type']        = 'Tipologia';
-
-// Help
-$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['entry_name']         = 'Nome opzione';
+$_['entry_type']         = 'Tipologia';
+$_['entry_option_value'] = 'Valore opzione';
+$_['entry_image']        = 'Immagine';
+$_['entry_sort_order']   = 'Ordinamento';
 
 // Error
-$_['error_warning']     = 'Attenzione: Si prega di controllare attentamente gli errori del modulo!';
-$_['error_permission']  = 'Attenzione: Non hai il permesso per modificare i produttori!';
-$_['error_name']        = 'Il nome del Produttore deve essere compreso tra 2 e 64 caratteri!';
-$_['error_keyword']     = 'SEO URL already in use!';
-$_['error_product']     = 'Attenzione: Questo produttore non può essere eliminato perché è attualmente assegnato a %s prodotti!';
+$_['error_warning']      = 'Attenzione: Si prega di controllare attentamente gli errori del modulo!';
+$_['error_permission']   = 'Attenzione: Non hai il permesso per modificare le opzioni!';
+$_['error_name']         = 'Il nome dell\'opzione deve essere compreso tra 1 e 128 caratteri!';
+$_['error_type']         = 'Attenzione: I valori delle opzioni sono necessari!';
+$_['error_option_value'] = 'Il valore delle opzioni deve essere compreso tra 1 e 128 caratteri!';
+$_['error_product']      = 'Attenzione: questa opzione non può essere cancellata poiché è attualmente assegnata a %s prodotti!';
