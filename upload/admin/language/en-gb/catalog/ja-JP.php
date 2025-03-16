01dd1fde7265b7a2766ddf4806e473f4fe50e175
OSWorX <info@osworx.net>
2025-03-16 14:20:02 +0100

    New translations information.php (Japanese)

--- upload/admin/language/en-gb/catalog/ja-JP.php
+++ upload/admin/language/en-gb/catalog/ja-JP.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'フィルタ';
+$_['heading_title']          = 'Information';
 
 // Text
-$_['text_success']      = 'Success: You have modified filters!';
-$_['text_list']         = 'Filter List';
-$_['text_add']          = 'Add Filter';
-$_['text_edit']         = 'Edit Filter';
+$_['text_success']           = 'Success: You have modified information!';
+$_['text_list']              = 'Information List';
+$_['text_add']               = 'Add Information';
+$_['text_edit']              = 'Edit Information';
+$_['text_default']           = 'デフォルト';
 
 // Column
-$_['column_group']      = 'Filter Group';
-$_['column_sort_order'] = '並べ替え順序';
-$_['column_action']     = 'アクション';
+$_['column_title']           = 'Information Title';
+$_['column_sort_order']	     = '並べ替え順序';
+$_['column_action']          = 'アクション';
 
 // Entry
-$_['entry_group']       = 'Filter Group Name';
-$_['entry_name']        = 'Filter Name';
-$_['entry_sort_order']  = '並べ替え順序';
+$_['entry_title']            = 'Information Title';
+$_['entry_description']      = '説明';
+$_['entry_store']            = 'ストア';
+$_['entry_meta_title'] 	     = 'Meta タグのタイトル';
+$_['entry_meta_keyword'] 	 = 'Meta タグのキーワード';
+$_['entry_meta_description'] = 'Meta タグの説明';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Bottom';
+$_['entry_status']           = 'Status';
+$_['entry_sort_order']       = '並べ替え順序';
+$_['entry_layout']           = 'Layout Override';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Display in the bottom footer.';
 
 // Error
-$_['error_warning']     = 'Warning: Please check the form carefully for errors!';
-$_['error_permission']  = 'Warning: You do not have permission to modify filters!';
-$_['error_group']       = 'Filter Group Name must be between 1 and 64 characters!';
-$_['error_name']        = 'Filter Name must be between 1 and 64 characters!';
+$_['error_warning']          = 'Warning: Please check the form carefully for errors!';
+$_['error_permission']       = 'Warning: You do not have permission to modify information!';
+$_['error_title']            = 'Information Title must be between 3 and 64 characters!';
+$_['error_description']      = 'Description must be more than 3 characters!';
+$_['error_meta_title']       = 'Meta Title must be greater than 3 and less than 255 characters!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'Warning: This information page cannot be deleted as it is currently assigned as the store account terms!';
+$_['error_checkout']         = 'Warning: This information page cannot be deleted as it is currently assigned as the store checkout terms!';
+$_['error_affiliate']        = 'Warning: This information page cannot be deleted as it is currently assigned as the store affiliate terms!';
+$_['error_return']           = 'Warning: This information page cannot be deleted as it is currently assigned as the store return terms!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
