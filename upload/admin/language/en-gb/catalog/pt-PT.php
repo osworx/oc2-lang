f2e1dd4275037c2b3d264c18cb1df60b9c566160
OSWorX <info@osworx.net>
2025-03-16 11:07:46 +0100

    New translations download.php (Portuguese)

--- upload/admin/language/en-gb/catalog/pt-PT.php
+++ upload/admin/language/en-gb/catalog/pt-PT.php
@@ -1,46 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Categorias';
+$_['heading_title']     = 'Downloads';
 
 // Text
-$_['text_success']           = 'Concluído: Modificou as categorias com sucesso!';
-$_['text_list']              = 'Lista de Categorias';
-$_['text_add']               = 'Adicionar Categoria';
-$_['text_edit']              = 'Editar Categoria';
-$_['text_default']           = 'Predefinição';
+$_['text_success']      = 'Concluído: Modificou os downloads com sucesso!';
+$_['text_list']         = 'Lista de Downloads';
+$_['text_add']          = 'Adicionar Download';
+$_['text_edit']         = 'Editar Download';
+$_['text_upload']       = 'Ficheiro enviado com sucesso!';
 
 // Column
-$_['column_name']            = 'Nome da Categoria';
-$_['column_sort_order']      = 'Posição';
-$_['column_action']          = 'Ação';
+$_['column_name']       = 'Node do Download';
+$_['column_date_added'] = 'Data de Adição';
+$_['column_action']     = 'Ação';
 
 // Entry
-$_['entry_name']             = 'Nome da Categoria';
-$_['entry_description']      = 'Descrição';
-$_['entry_meta_title'] 	     = 'Titulo Meta Tag';
-$_['entry_meta_keyword']     = 'Palavras-Chave Meta Tag';
-$_['entry_meta_description'] = 'Descrição Meta Tag';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = 'Superior';
-$_['entry_filter']           = 'Filtros';
-$_['entry_store']            = 'Lojas';
-$_['entry_image']            = 'Imagem';
-$_['entry_top']              = 'Topo';
-$_['entry_column']           = 'Colunas';
-$_['entry_sort_order']       = 'Posição';
-$_['entry_status']           = 'Estado';
-$_['entry_layout']           = 'Substituição de Layout';
+$_['entry_name']        = 'Node do Download';
+$_['entry_filename']    = 'Nome de Arquivo';
+$_['entry_mask']        = 'Máscara';
 
 // Help
-$_['help_filter']            = '(Autocompletar)';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = 'Mostrar na barra de menu superior. Só funciona para as categorias Superiores.';
-$_['help_column']            = 'Número de colunas a serem usadas para as 3 categorias inferiores. Só funciona para as categorias Superiores.';
+$_['help_filename']     = 'Pode fazer o upload através do botão de upload ou usar FTP para fazer upload para o diretório de download e inserir os detalhes do Ficheiro abaixo.';
+$_['help_mask']         = 'É recomendável que o nome do ficheiro e a máscara sejam diferentes para impedir que as pessoas tentem aceder diretamente a seus downloads.';
 
 // Error
-$_['error_warning']          = 'Aviso: Por favor, verifique o formulário cuidadosamente para evitar erros!';
-$_['error_permission']       = 'Aviso: Não tem permissões para modificar Categorias!';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = 'O Meta Título deve ser conter mais de 3 e menos de 255 caracteres!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = 'Aviso: Por favor, verifique o formulário cuidadosamente para evitar erros!';
+$_['error_permission']  = 'Aviso: Não tem permissões para modificar os Downloads!';
+$_['error_name']        = 'Node do Download deve conter entre 3 e 64 caracteres!';
+$_['error_upload']      = 'Necessário fazer o Upload!';
+$_['error_filename']    = 'Nome do Ficheiro deve conter entre 3 e 128 caracteres!';
+$_['error_exists']      = 'O Ficheiro não existe!';
+$_['error_mask']        = 'Máscara deve conter entre 3 e 128 caracteres!';
+$_['error_filetype']    = 'Tipo de arquivo inválido!';
+$_['error_product']     = 'Aviso: Este download não pode ser excluído, pois esta atualmente atribuído a %s produtos!';
