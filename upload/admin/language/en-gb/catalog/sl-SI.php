9f61ca161e915f215f6e6426f502ce9cddf8786f
OSWorX <info@osworx.net>
2025-03-16 11:08:44 +0100

    New translations filter.php (Slovenian)

--- upload/admin/language/en-gb/catalog/sl-SI.php
+++ upload/admin/language/en-gb/catalog/sl-SI.php
@@ -1,35 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = 'Prenosi';
+$_['heading_title']     = 'Filtri';
 
 // Text
-$_['text_success']      = 'USPEŠNO: Ste uredili prenose!';
-$_['text_list']         = 'Seznam prenosov';
-$_['text_add']          = 'Dodaj prenos';
-$_['text_edit']         = 'Uredi Prenos';
-$_['text_upload']       = 'Vaša datoteka je bila uspešno prenesena!';
+$_['text_success']      = 'USPEŠNO: Ste uredili filtre!';
+$_['text_list']         = 'Seznam filtrov';
+$_['text_add']          = 'Dodaj filter';
+$_['text_edit']         = 'Uredi filter';
 
 // Column
-$_['column_name']       = 'Ime prenosa';
-$_['column_date_added'] = 'Dodano dne';
+$_['column_group']      = 'Skupina filtrov';
+$_['column_sort_order'] = 'Vrstni red';
 $_['column_action']     = 'Dejanje';
 
 // Entry
-$_['entry_name']        = 'Ime prenosa';
-$_['entry_filename']    = 'Ime datoteke';
-$_['entry_mask']        = 'Maska';
-
-// Help
-$_['help_filename']     = 'Lahko prenesete preko gumb upload ali uporaba FTP upload v imenik za prenos in vnesite podatke spodaj.';
-$_['help_mask']         = 'Priporočljivo je, da sta ime datoteke in maska različni kar bo ustavilo ljudi pri poskusu neposredne povezave na vaše prenose.';
+$_['entry_group']       = 'Ime skupine filtrov';
+$_['entry_name']        = 'Ime filtra';
+$_['entry_sort_order']  = 'Vrstni red';
 
 // Error
 $_['error_warning']     = 'POZOR: Prosimo, pazljivo preverite polja za morebitne napake!';
-$_['error_permission']  = 'POZOR: Nimate dovoljenja za urejanje prenosov!';
-$_['error_name']        = 'Ime mora imeti več kot 3 in manj kot 64 znakov!';
-$_['error_upload']      = 'Nalaganje je zahtevano!';
-$_['error_filename']    = 'Ime datoteke mora imeti več kot 3 in manj kot 128 znakov!';
-$_['error_exists']      = 'Datoteka ne obstaja!';
-$_['error_mask']        = 'Maska mora vsebovati med 3 in 128 znakov!';
-$_['error_filetype']    = 'Neveljaven tip datoteke!';
-$_['error_product']     = 'POZOR: Ta prenos ne more biti izbrisan, ker je vezan na %s izdelke!';
+$_['error_permission']  = 'POZOR: Nimate dovoljenja za urejanje filtrov!';
+$_['error_group']       = 'Ime skupine filtrov mora vsebovati med 1 in 64 znakov!';
+$_['error_name']        = 'Ime filtra mora vsebovati med 1 in 64 znakov!';
