580988df89c4dcf15d8fe36badf92fef545de6fa
OSWorX <info@osworx.net>
2025-03-16 14:24:35 +0100

    New translations review.php (Serbian (Latin))

--- upload/admin/language/en-gb/catalog/sr-CS.php
+++ upload/admin/language/en-gb/catalog/sr-CS.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'Povratni Profil';
+$_['heading_title']     = 'Kritike';
 
 // Text
-$_['text_success']          = 'Uspeh: Vi ste izmenili povratne profile!';
-$_['text_list']             = 'Lista Povratnih Profila';
-$_['text_add']              = 'Dodaj Povratni Profil';
-$_['text_edit']             = 'Izmeni Povratne Profile';
-$_['text_day']				= 'Dan';
-$_['text_week']				= 'Nedelja';
-$_['text_semi_month']		= 'Pola Meseca';
-$_['text_month']			= 'Mesec';
-$_['text_year']				= 'Godina';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i>Povratni iznos je izračunat učestalošću i ciklusom.</p><p>Na primer ako koristite učestalost "nedelja" a ciklus "2", onda korisnici se obaveštavaju svake druge nedelje.</p><p>Trajanje je broj koliko puta korisnik će izvršiti plaćanje, postavite ovo na 0 ako želite plaćanje sve dok oni ne otkažu.</p>';
-$_['text_profile']			= 'Povratni Profil';
-$_['text_trial']			= 'Probni Profil';
-
-// Entry
-$_['entry_name']		    = 'Ime';
-$_['entry_price']			= 'Cena';
-$_['entry_duration']		= 'Trajanje';
-$_['entry_cycle']			= 'Ciklus';
-$_['entry_frequency']		= 'Učestalost';
-$_['entry_trial_price']		= 'Probna cena';
-$_['entry_trial_duration']	= 'Trajanje probe';
-$_['entry_trial_status']	= 'Stanje Probe';
-$_['entry_trial_cycle']	    = 'Ciklus Probe';
-$_['entry_trial_frequency'] = 'Učestalost Probe';
-$_['entry_status']			= 'Stanje';
-$_['entry_sort_order']		= 'Sortiraj po';
+$_['text_success']      = 'Uspeh: Vi ste izmenili kritike!';
+$_['text_list']         = 'Lista Kritika';
+$_['text_add']          = 'Dodaj Kritiku';
+$_['text_edit']         = 'Izmeni Kritiku';
 
 // Column
-$_['column_name']			= 'Ime';
-$_['column_sort_order']	    = 'Sortiraj po';
-$_['column_action']         = 'Akcija';
+$_['column_product']    = 'Proizvod';
+$_['column_author']     = 'Autor';
+$_['column_rating']     = 'Ocena';
+$_['column_status']     = 'Stanje';
+$_['column_date_added'] = 'Datum Dodavanja';
+$_['column_action']     = 'Akcija';
+
+// Entry
+$_['entry_product']     = 'Proizvod';
+$_['entry_author']      = 'Autor';
+$_['entry_rating']      = 'Ocena';
+$_['entry_status']      = 'Stanje';
+$_['entry_text']        = 'Тekst';
+$_['entry_date_added']  = 'Datum Dodavanja';
+
+// Help
+$_['help_product']      = '(Automatsko Dovršavanje)';
 
 // Error
-$_['error_warning']         = 'Upozorenje: Molimo proverite formu zbog grešaka!';
-$_['error_permission']		= 'Upozorenje: Nemate pravo da izmenite povratne profile!';
-$_['error_name']			= 'Naziv Profila mora biti veći od 3 i manji od 255 karaktera!';
-$_['error_product']			= 'Upozorenje: Ovaj Povratni profil nemože biti izbrisan jer je trenutno dodeljen %s proizvodima!';
+$_['error_warning']     = 'Upozorenje: Molimo proverite formu zbog grešaka!';
+$_['error_permission']  = 'Upozorenje: Vi nemate dozvolu da biste prilagodili kritike!';
+$_['error_product']     = 'Potreban proizvod!';
+$_['error_author']      = 'Autor morra biti između 3 i 64 karaktera!';
+$_['error_text']        = 'Tekst Kritike mora biti naj manje 1 karakter!';
+$_['error_rating']      = 'Potrebna ocena kritike!';
