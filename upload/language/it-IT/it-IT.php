9a9dd0fbbe0a60c6497876156b6ee93cea70700c
OSWorX <info@osworx.net>
2025-03-16 09:10:02 +0100

    New translations attribute_group.php (Italian)

--- upload/language/it-IT/it-IT.php
+++ upload/language/it-IT/it-IT.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Attributi';
+$_['heading_title']     = 'Gruppi Attributi';
 
 // Text
-$_['text_success']           = 'Hai modificato gli attributi con successo!';
-$_['text_list']              = 'Elenco attributi';
-$_['text_add']               = 'Aggiungi attributo';
-$_['text_edit']              = 'Modifica attributo';
+$_['text_success']      = 'Hai modificato i gruppi attributi con successo!';
+$_['text_list']         = 'Elenco Gruppo Attributi';
+$_['text_add']          = 'Aggiungi Gruppo Attributi';
+$_['text_edit']         = 'Modifica Gruppo Attributi';
 
 // Column
-$_['column_name']            = 'Nome attributo';
-$_['column_attribute_group'] = 'Gruppo attributi';
-$_['column_sort_order']      = 'Ordinamento';
-$_['column_action']          = 'Azione';
+$_['column_name']       = 'Nome Gruppo Attributi';
+$_['column_sort_order'] = 'Ordinamento';
+$_['column_action']     = 'Azione';
 
 // Entry
-$_['entry_name']             = 'Nome attributo';
-$_['entry_attribute_group']  = 'Gruppo attributi';
-$_['entry_sort_order']       = 'Ordinamento';
+$_['entry_name']        = 'Nome Gruppo Attributi';
+$_['entry_sort_order']  = 'Ordinamento';
 
 // Error
-$_['error_warning']          = 'Attenzione: Si prega di controllare attentamente gli errori del modulo!';
-$_['error_permission']       = 'Attenzione: Non hai il permesso per modificare gli attributi!';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'Il nome dell\'attributo deve avere tra 3 e 64 caratteri!';
-$_['error_product']          = 'Attenzione: questo attributo non può essere cancellato poiché è assegnato a %s prodotti!';
+$_['error_warning']     = 'Attenzione: Si prega di controllare attentamente gli errori del modulo!';
+$_['error_permission']  = 'Attenzione: Non hai il permesso di modificare i gruppi attributi!';
+$_['error_name']        = 'Il Nome del Gruppo Attributi deve essere compreso tra 3 e 64 caratteri!';
+$_['error_attribute']   = 'Attenzione: Questo gruppo attributi non può essere eliminato in quanto è attualmente assegnato agli attributi di %s!';
+$_['error_product']     = 'Attenzione: Questo gruppo di attributi non può essere eliminato in quanto è assegnato a %s prodotti!';
