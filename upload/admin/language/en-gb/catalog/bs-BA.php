d4f45988f70e1630bc92b69812ddd284bd757b10
OSWorX <info@osworx.net>
2025-03-16 14:21:19 +0100

    New translations manufacturer.php (Bosnian)

--- upload/admin/language/en-gb/catalog/bs-BA.php
+++ upload/admin/language/en-gb/catalog/bs-BA.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Informacije';
+$_['heading_title']     = 'Proizvođači';
 
 // Text
-$_['text_success']           = 'Uspjeh: Promjenili ste podatke!';
-$_['text_list']              = 'Lista informacija';
-$_['text_add']               = 'Dodavanje informacija';
-$_['text_edit']              = 'Uređivanje informacija';
-$_['text_default']           = 'Zadano';
+$_['text_success']      = 'Uspjeh: Izmjena proizvođača!';
+$_['text_list']         = 'Popis proizvođača';
+$_['text_add']          = 'Dodajte proizvođača';
+$_['text_edit']         = 'Uređivanje proizvođača';
+$_['text_default']      = 'Zadano';
+$_['text_percent']      = 'Postotak';
+$_['text_amount']       = 'Fiksni iznos';
 
 // Column
-$_['column_title']           = 'Naslov Informacija';
-$_['column_sort_order']	     = 'Redoslijed sortiranja';
-$_['column_action']          = 'Akcija';
+$_['column_name']       = 'Ime Proizvođača';
+$_['column_sort_order'] = 'Redoslijed sortiranja';
+$_['column_action']     = 'Akcija';
 
 // Entry
-$_['entry_title']            = 'Naslov Informacija';
-$_['entry_description']      = 'Opis';
-$_['entry_store']            = 'Prodavnice';
-$_['entry_meta_title'] 	     = 'Meta Tag naslov';
-$_['entry_meta_keyword'] 	 = 'Ključne riječi meta Tag';
-$_['entry_meta_description'] = 'Meta Tag opis';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'Dno';
-$_['entry_status']           = 'Status';
-$_['entry_sort_order']       = 'Redoslijed sortiranja';
-$_['entry_layout']           = 'Layout Override';
+$_['entry_name']        = 'Ime Proizvođača';
+$_['entry_store']       = 'Prodavnice';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'Slika';
+$_['entry_sort_order']  = 'Redoslijed sortiranja';
+$_['entry_type']        = 'Tip';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'Prikaz u dnu podnožja.';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'Upozorenje: Provjerite obrazac pažljivo radi grešaka!';
-$_['error_permission']       = 'Upozorenje: Nemate dozvolu za izmjenu informacija!';
-$_['error_title']            = 'Naslov Informacija mora biti između 3 i 64 znakova!';
-$_['error_description']      = 'Opis mora biti više od 3 znakova!';
-$_['error_meta_title']       = 'Meta naslov mora biti veći od 3 i manji od 255 znakova!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'Upozorenje: Ovu stranicu informacija se ne može izbrisati jer je trenutačno dodijeljena kao trgovina računovodstvenim terminima!';
-$_['error_checkout']         = 'Upozorenje: Ovu stranicu informacija se ne može izbrisati jer je trenutačno dodijeljena kao trgovina računovodstvenim terminima!';
-$_['error_affiliate']        = 'Upozorenje: Ovu stranicu informacija se ne može izbrisati jer je trenutačno dodijeljena kao trgovina računovodstvenim terminima!';
-$_['error_return']           = 'Upozorenje: Ovu stranicu informacija se ne može izbrisati jer je trenutačno dodijeljena kao trgovina povratnim terminima!';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'Upozorenje: Provjerite obrazac pažljivo radi grešaka!';
+$_['error_permission']  = 'Upozorenje: Nemate dozvolu za izmjenu proizvođača!';
+$_['error_name']        = 'Ime Proizvođača mora biti između 2 i 64 znakova!';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'Upozorenje: Ovaj proizvođač ne može biti izbrisan jer je trenutno dodijeljen %s proizvoda!';
