10915b701f022bc370903823f2763cf75a068112
OSWorX <info@osworx.net>
2025-03-16 11:13:20 +0100

    New translations recurring.php (Latvian)

--- upload/admin/language/en-gb/catalog/lv-LV.php
+++ upload/admin/language/en-gb/catalog/lv-LV.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'Produkti';
+$_['heading_title']			= 'Periodiski profili';
 
 // Text
-$_['text_success']           = 'Veiksmīgi: Jūs esat labojis produktus!';
-$_['text_list']              = 'Produktu saraksts';
-$_['text_add']               = 'Pievienot produktu';
-$_['text_edit']              = 'Labot produktu';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'Noklusējums';
-$_['text_option']            = 'Opcija';
-$_['text_option_value']      = 'Opcijas vērtība';
-$_['text_percent']           = 'Procentos';
-$_['text_amount']            = 'Fiksēta summa';
-
-// Column
-$_['column_name']            = 'Produkta nosaukums';
-$_['column_model']           = 'Modelis';
-$_['column_image']           = 'Attēls';
-$_['column_price']           = 'Cena';
-$_['column_quantity']        = 'Daudzums';
-$_['column_status']          = 'Statuss';
-$_['column_action']          = 'Darbība';
+$_['text_success']          = 'Panākumi: Esat izmainījis periodiskā profili!';
+$_['text_list']             = 'Periodiska profila saraksts';
+$_['text_add']              = 'Periodiska profila pievienošana';
+$_['text_edit']             = 'Periodiska profila rediģēšanai';
+$_['text_day']				= 'Diena';
+$_['text_week']				= 'Nedēļa';
+$_['text_semi_month']		= 'Daļēji mēnesī';
+$_['text_month']			= 'Mēnesis';
+$_['text_year']				= 'Gads';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> periodisks summas tiek aprēķinātas, biežumu un cikli.</p><p>Piemēram, lietojot frekvences "nedēļas" un ciklu "% 2", tad lietotājam būs jāmaksā ik pēc 2 nedēļām.</p><p>Ilgums ir, cik reizes lietotājs būs veikt maksājumu, kopa ar 0, ja vēlaties maksājumiem, līdz brīdim, kad tie tiek atcelti.</p>';
+$_['text_profile']			= 'Periodiski profili';
+$_['text_trial']			= 'Izmēģinājuma profils';
 
 // Entry
-$_['entry_name']             = 'Produkta nosaukums';
-$_['entry_description']      = 'Apraksts';
-$_['entry_meta_title'] 	     = 'Meta Tag nosaukums';
-$_['entry_meta_keyword'] 	 = 'Meta Tag atslēgvārdi';
-$_['entry_meta_description'] = 'Meta Tag apraksts';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Modelis';
-$_['entry_sku']              = 'Artikuls';
-$_['entry_upc']              = 'upc';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'Atrašanās vieta';
-$_['entry_shipping']         = 'Ir nepieciešama piegāde';
-$_['entry_manufacturer']     = 'Ražotājs';
-$_['entry_store']            = 'Veikali';
-$_['entry_date_available']   = 'Datums, kad ir pieejams';
-$_['entry_quantity']         = 'Daudzums';
-$_['entry_minimum']          = 'Minimālais daudzums';
-$_['entry_stock_status']     = 'Pašlaik nav';
-$_['entry_price']            = 'Cena';
-$_['entry_tax_class']        = 'Nodokļu kategorijas';
-$_['entry_points']           = 'Punkti';
-$_['entry_option_points']    = 'Punkti';
-$_['entry_subtract']         = 'Atņemt Stock';
-$_['entry_weight_class']     = 'Svara kategorijas';
-$_['entry_weight']           = 'Svars';
-$_['entry_dimension']        = 'Izmēri (G x P x A)';
-$_['entry_length_class']     = 'Garuma klases';
-$_['entry_length']           = 'Garums';
-$_['entry_width']            = 'Platums';
-$_['entry_height']           = 'Augstums';
-$_['entry_image']            = 'Attēls';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Klienta grupa';
-$_['entry_date_start']       = 'Sākuma datums';
-$_['entry_date_end']         = 'Beigu datums';
-$_['entry_priority']         = 'Prioritāte';
-$_['entry_attribute']        = 'Atribūts';
-$_['entry_attribute_group']  = 'Atribūta Grupa';
-$_['entry_text']             = 'Teksts';
-$_['entry_option']           = 'Opcija';
-$_['entry_option_value']     = 'Opcijas vērtība';
-$_['entry_required']         = 'Nepieciešams';
-$_['entry_status']           = 'Statuss';
-$_['entry_sort_order']       = 'Kārtošanas secība';
-$_['entry_category']         = 'Sadaļas';
-$_['entry_filter']           = 'Filtri';
-$_['entry_download']         = 'Lejupielādes';
-$_['entry_related']          = 'Saistītie produkti';
-$_['entry_tag']          	 = 'Produkta Tags';
-$_['entry_reward']           = 'Bonusa punkti';
-$_['entry_layout']           = 'Izkārtojuma ignorēšana';
-$_['entry_recurring']        = 'Periodiski profili';
+$_['entry_name']		    = 'Nosaukums';
+$_['entry_price']			= 'Cena';
+$_['entry_duration']		= 'Ilgums';
+$_['entry_cycle']			= 'Cikls';
+$_['entry_frequency']		= 'Frekvence';
+$_['entry_trial_price']		= 'Izmēģinājuma cena';
+$_['entry_trial_duration']	= 'Pārbaudes ilgums';
+$_['entry_trial_status']	= 'Pārbaudes statuss';
+$_['entry_trial_cycle']	    = 'Pārbaudes cikls';
+$_['entry_trial_frequency'] = 'Pārbaudes biežums';
+$_['entry_status']			= 'Statuss';
+$_['entry_sort_order']		= 'Kārtošanas secība';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'Cik ir pieejami';
-$_['help_upc']               = 'Universal produkta kods';
-$_['help_ean']               = 'Eiropas pants numurs';
-$_['help_jan']               = 'Japāņu rakstu skaits';
-$_['help_isbn']              = 'Starptautiskais grāmatas standartnumurs';
-$_['help_mpn']               = 'Ražotāja daļas numurs';
-$_['help_manufacturer']      = '(Automātiskās pabeigšanas)';
-$_['help_minimum']           = 'Spiesti pasūtīt minimālo daudzumu';
-$_['help_stock_status']      = 'Statusu, kas tiek parādīts, kad produkts nav noliktavā';
-$_['help_points']            = 'Punktu, nepieciešams iegādāties šo vienumu skaits. Ja jūs nevēlaties, lai šis produkts tiks iepirkta ar punktu atstāt kā 0.';
-$_['help_category']          = '(Automātiskās pabeigšanas)';
-$_['help_filter']            = '(Automātiskās pabeigšanas)';
-$_['help_download']          = '(Automātiskās pabeigšanas)';
-$_['help_related']           = '(Automātiskās pabeigšanas)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'Nosaukums';
+$_['column_sort_order']	    = 'Kārtošanas secība';
+$_['column_action']         = 'Darbība';
 
 // Error
-$_['error_warning']          = 'Brīdinājums: Lūdzu pārbaudiet formu rūpīgi ,ir dažas kļūdas!';
-$_['error_permission']       = 'Brīdinājums: Jums nav pietiekamu pilnvaru, lai modificētu produktus!';
-$_['error_name']             = 'Produkta nosaukumam jābūt lielākam par 3 līdz mazāk nekā 255 rakstzīmes!';
-$_['error_meta_title']       = 'Galvenajā nosaukumā jābūt lielākam par 3 līdz mazāk nekā 255 rakstzīmes!';
-$_['error_model']            = 'Preču modelim jāsatur vairāk par 1 un mazāks nekā 64 rakstzīmes!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Brīdinājums: Lūdzu pārbaudiet formu rūpīgi ,ir dažas kļūdas!';
+$_['error_permission']		= 'Brīdinājums: Jums nav tiesību modificēt periodisku profili!';
+$_['error_name']			= 'Profila nosaukumā ir jābūt lielākam par 3 līdz mazāk nekā 255 rakstzīmes!';
+$_['error_product']			= 'Brīdinājums: Šī periodiskā profila nevar dzēst, jo tā pašlaik ir piešķirts %s produktus!';
