462d8d2e6731e724a7e85e99bf90d112eebc4cb2
OSWorX <info@osworx.net>
2025-03-16 14:21:14 +0100

    New translations manufacturer.php (Azerbaijani)

--- upload/admin/language/en-gb/catalog/az-AZ.php
+++ upload/admin/language/en-gb/catalog/az-AZ.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Məlumat';
+$_['heading_title']     = 'İstehsalçılar';
 
 // Text
-$_['text_success']           = 'Müvəffəqiyyət: Siz məlumatı dəyişdirdiniz!';
-$_['text_list']              = 'Məlumat Listəsi';
-$_['text_add']               = 'Məlumat əlavə et';
-$_['text_edit']              = 'Məlumatlara düzəliş et';
-$_['text_default']           = 'Varsayılan';
+$_['text_success']      = 'Uğurlu: Siz filtrləri redaktə etdiniz!';
+$_['text_list']         = 'İstehsalçılar';
+$_['text_add']          = 'İstehsalçı əlavə et';
+$_['text_edit']         = 'İstehsalçı redaktə et';
+$_['text_default']      = 'Varsayılan';
+$_['text_percent']      = 'Faiz';
+$_['text_amount']       = 'Müəyyən miqdar';
 
 // Column
-$_['column_title']           = 'Məlumat başlığı';
-$_['column_sort_order']	     = 'Növlərə görə sırala';
-$_['column_action']          = 'Hərəkət';
+$_['column_name']       = 'İstehsalçıların adı';
+$_['column_sort_order'] = 'Növlərə görə sırala';
+$_['column_action']     = 'Hərəkət';
 
 // Entry
-$_['entry_title']            = 'Məlumat başlığı';
-$_['entry_description']      = 'Açıqlama';
-$_['entry_store']            = 'Dükanlar';
-$_['entry_meta_title'] 	     = 'Meta başlığı';
-$_['entry_meta_keyword'] 	 = 'Meta açar sözləri';
-$_['entry_meta_description'] = 'Meta İzahat';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'Alt';
-$_['entry_status']           = 'Status';
-$_['entry_sort_order']       = 'Növlərə görə sırala';
-$_['entry_layout']           = 'Düzeni Geçersiz Kılma';
+$_['entry_name']        = 'İstehsalçıların adı';
+$_['entry_store']       = 'Dükanlar';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'Şəkillər';
+$_['entry_sort_order']  = 'Növlərə görə sırala';
+$_['entry_type']        = 'Növ';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'Alt göstərmək.';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'Xəbərdarlıq: Xahiş edirik, formu göstərilən səhvlər səbəbiylə diqqətlə nəzarət edin!';
-$_['error_permission']       = 'Xəbərdarlıq: filtr adlarını dəyişmək üçün, sizin icazəniz yoxdur!';
-$_['error_title']            = 'Xüsusiyyət Adı 3 və 64 simvol arası olmalıdır!';
-$_['error_description']      = 'Simvol 3 dən çox olmalıdır!';
-$_['error_meta_title']       = 'Meta başlıq ən az 3 və ən çox 255 simvol olmalıdır!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'Diqqət: Bu yükləmə silinə bilməz, necə ki bu hal-hazırda %s məhsullarına təyin edilir!';
-$_['error_checkout']         = 'Xəbərdarlıq: Bu məlumat səhifəsi silinə bilməz, çünki hal-hazırda mağazanın yoxlama şərtləri kimi təyin olunur!';
-$_['error_affiliate']        = 'Diqqət: Bu məlumat silinə bilməz, çünki hal-hazırda mağazanın bağlılıq şərtləri kimi istifadə olunur!';
-$_['error_return']           = 'Diqqət: Bu yükləmə silinə bilməz, necə ki bu hal-hazırda %s məhsullarına təyin edilir!';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'Xəbərdarlıq: Xahiş edirik, formu göstərilən səhvlər səbəbiylə diqqətlə nəzarət edin!';
+$_['error_permission']  = 'Xəbərdarlıq: filtr adlarını dəyişmək üçün, sizin icazəniz yoxdur!';
+$_['error_name']        = 'Filtr adı 1 və 64 simvol arasında olmalıdır!';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'Xəbərdar etmək: Bu əlamət silinə bilməz, necə ki bu hal-hazırda %s məhsullarına təyin edilir!';
