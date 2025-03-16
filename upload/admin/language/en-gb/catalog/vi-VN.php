4630213f98ad33822919b71a77c8095bbc168064
OSWorX <info@osworx.net>
2025-03-16 11:11:30 +0100

    New translations option.php (Vietnamese)

--- upload/admin/language/en-gb/catalog/vi-VN.php
+++ upload/admin/language/en-gb/catalog/vi-VN.php
@@ -1,35 +1,40 @@
 <?php
 // Heading
-$_['heading_title']     = 'Các nhà sản xuất';
+$_['heading_title']      = 'Các tùy chọn';
 
 // Text
-$_['text_success']      = 'Kết quả: Bạn đã sửa xong các nhà sản xuất!';
-$_['text_list']         = 'Danh sách nhà sản xuất';
-$_['text_add']          = 'Thêm nhà sản xuất';
-$_['text_edit']         = 'Sửa nhà sản xuất';
-$_['text_default']      = 'Mặc định';
-$_['text_percent']      = 'Phần trăm';
-$_['text_amount']       = 'Số tiền cố định';
+$_['text_success']       = 'Kết quả: Bạn đã sửa xong các tùy chọn!';
+$_['text_list']          = 'Danh sách tùy chọn';
+$_['text_add']           = 'Thêm tùy chọn';
+$_['text_edit']          = 'Sửa tùy chọn';
+$_['text_choose']        = 'Chọn';
+$_['text_select']        = 'Chọn';
+$_['text_radio']         = 'Loại trừ';
+$_['text_checkbox']      = 'Hộp chọn';
+$_['text_input']         = 'Nhập vào';
+$_['text_text']          = 'Mô tả';
+$_['text_textarea']      = 'Khu vực mô tả';
+$_['text_file']          = 'File';
+$_['text_date']          = 'Ngày';
+$_['text_datetime']      = 'Ngày &amp; Giờ';
+$_['text_time']          = 'Giờ';
 
 // Column
-$_['column_name']       = 'Tên nhà sản xuất';
-$_['column_sort_order'] = 'Thứ tự sắp xếp';
-$_['column_action']     = 'Tác vụ';
+$_['column_name']        = 'Tên kiểu tùy chọn';
+$_['column_sort_order']  = 'Thứ tự sắp xếp';
+$_['column_action']      = 'Tác vụ';
 
 // Entry
-$_['entry_name']        = 'Tên nhà sản xuất';
-$_['entry_store']       = 'Gian hàng';
-$_['entry_keyword']     = 'SEO URL';
-$_['entry_image']       = 'Hình ảnh';
-$_['entry_sort_order']  = 'Thứ tự sắp xếp';
-$_['entry_type']        = 'Kiểu';
-
-// Help
-$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['entry_name']         = 'Tên kiểu tùy chọn';
+$_['entry_type']         = 'Kiểu';
+$_['entry_option_value'] = 'Tên giá trị kiểu tùy chọn';
+$_['entry_image']        = 'Hình ảnh';
+$_['entry_sort_order']   = 'Thứ tự sắp xếp';
 
 // Error
-$_['error_warning']     = 'Chú ý: Vui lòng kiểm tra lỗi cẩn thận trong biểu mẫu!';
-$_['error_permission']  = 'Chú ý: Bạn không có quyền sửa nhà sản xuất!';
-$_['error_name']        = 'Tên nhà sản xuất phải từ 2 đến 64 ký tự!';
-$_['error_keyword']     = 'SEO URL already in use!';
-$_['error_product']     = 'Chú ý: Nhà sản xuất này không thể bị xoá vì nó đang được gán cho các sản phẩm %s !';
+$_['error_warning']      = 'Chú ý: Vui lòng kiểm tra lỗi cẩn thận trong biểu mẫu!';
+$_['error_permission']   = 'Chú ý: Bạn không có quyền sửa các tùy chọn!';
+$_['error_name']         = 'Tên loại tùy chọn phải từ 1 đến 128 ký tự!';
+$_['error_type']         = 'Chú ý: Giá trị tùy chọn cần phải có!';
+$_['error_option_value'] = 'Tên giá trị loại tùy chọn phải từ 1 đến 128 ký tự!';
+$_['error_product']      = 'Chú ý: Tùy chọn này không thể bị xoá vì nó đang được gán cho %s sản phẩm!';
