36b0a064b7a19d9a1395e48262b8a2d06157e824
OSWorX <info@osworx.net>
2025-03-16 14:26:42 +0100

    New translations filemanager.php (Arabic)

--- upload/admin/language/en-gb/common/ar-SA.php
+++ upload/admin/language/en-gb/common/ar-SA.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'لوحة التحكم';
+$_['heading_title']    = 'مدير الصور';
+
+// Text
+$_['text_uploaded']    = 'تم رفع الملف الخاص بك بنجاح!';
+$_['text_directory']   = 'تم إنشاء المجلد بنجاح!';
+$_['text_delete']      = 'تم حذف الملف او المجلد بنجاح!';
+
+// Entry
+$_['entry_search']     = 'بحث..';
+$_['entry_folder']     = 'اسم المجلد';
 
 // Error
-$_['error_install'] = 'تحذير: مجلد التثبيت لا يزال موجود وينبغي حذفه لأسباب سلاماتية!';
+$_['error_permission'] = 'تحذير: لا يوجد صلاحيات لديك!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'تحذير: ملف او مجلد موجود حاليا يحمل نفس الاسم!';
+$_['error_directory']  = 'تحذير: المجلد غير موجود!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'تحذير: نمط الملف غير صحيح!';
+$_['error_upload']     = 'تحذير: لم يتم رفع الملف لسبب غير معروف!';
+$_['error_delete']     = 'تحذير: لا يمكنك حذف هذا المجلد!';
