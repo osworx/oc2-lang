97d39cb07b324798f7a746bb87b455cfaae1355c
OSWorX <info@osworx.net>
2025-03-16 14:28:46 +0100

    New translations forgotten.php (Chinese Simplified)

--- upload/admin/language/en-gb/common/zh-CN.php
+++ upload/admin/language/en-gb/common/zh-CN.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = '忘记您的密码？';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = '版本号 %s';
+$_['text_forgotten']  = '忘记密码';
+$_['text_your_email'] = '您的电子邮件地址';
+$_['text_email']      = '请输入您帐户关联的电子邮件地址。点击“提交”按钮，一封重置密码邮件会发送去您的电子邮件地址。';
+$_['text_success']    = '密码重置邮件已发去您的管理员电子邮件地址。';
+
+// Entry
+$_['entry_email']     = '电子邮件地址';
+$_['entry_password']  = '新密码';
+$_['entry_confirm']   = '确认';
+
+// Error
+$_['error_email']     = '警告：找不到当前电子邮件地址，请重试！';
+$_['error_password']  = '密码必须在4到20个字符之间！';
+$_['error_confirm']   = '密码和确认密码不匹配 ！';
