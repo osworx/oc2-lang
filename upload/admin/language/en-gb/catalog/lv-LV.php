e49bf0d836709d31e17841471fc5fbe607cae5ff
OSWorX <info@osworx.net>
2025-03-16 14:16:26 +0100

    New translations attribute.php (Latvian)

--- upload/admin/language/en-gb/catalog/lv-LV.php
+++ upload/admin/language/en-gb/catalog/lv-LV.php
@@ -1,36 +1,27 @@
 <?php
 // Heading
-$_['heading_title']     = 'Apraksti';
+$_['heading_title']          = 'Atribūti';
 
 // Text
-$_['text_success']      = 'Veiksmīga: Jūs esat modificējis atsauksmes!';
-$_['text_list']         = 'Pārskata sarakstā';
-$_['text_add']          = 'Pievienot atsauksmes';
-$_['text_edit']         = 'Rediģēt atsauksmes';
+$_['text_success']           = 'Veiksmīgi: Ir modificēti atribūti!';
+$_['text_list']              = 'Atribūtu saraksts';
+$_['text_add']               = 'Pievienot atribūtu';
+$_['text_edit']              = 'Labot atribūtu';
 
 // Column
-$_['column_product']    = 'Produkts';
-$_['column_author']     = 'Autors';
-$_['column_rating']     = 'Vērtējums';
-$_['column_status']     = 'Statuss';
-$_['column_date_added'] = 'Pievienošanas datums';
-$_['column_action']     = 'Darbība';
+$_['column_name']            = 'Atribūta nosaukums';
+$_['column_attribute_group'] = 'Atribūta Grupa';
+$_['column_sort_order']      = 'Kārtošanas secība';
+$_['column_action']          = 'Darbība';
 
 // Entry
-$_['entry_product']     = 'Produkts';
-$_['entry_author']      = 'Autors';
-$_['entry_rating']      = 'Vērtējums';
-$_['entry_status']      = 'Statuss';
-$_['entry_text']        = 'Teksts';
-$_['entry_date_added']  = 'Pievienošanas datums';
-
-// Help
-$_['help_product']      = '(Automātiskās pabeigšanas)';
+$_['entry_name']             = 'Atribūta nosaukums';
+$_['entry_attribute_group']  = 'Atribūta Grupa';
+$_['entry_sort_order']       = 'Kārtošanas secība';
 
 // Error
-$_['error_warning']     = 'Brīdinājums: Lūdzu pārbaudiet formu rūpīgi ,ir dažas kļūdas!';
-$_['error_permission']  = 'Brīdinājums: Jums nav tiesību modificēt atsauksmes!';
-$_['error_product']     = 'Produkts, kas nepieciešama!';
-$_['error_author']      = 'Autors ir jābūt no 3 līdz 64 rakstzīmēm!';
-$_['error_text']        = 'Pārskatīšanas tekstam ir jābūt vismaz 1 zīmi garam!';
-$_['error_rating']      = 'Pārskata vērtējumu, kas vajadzīgs!';
+$_['error_warning']          = 'Brīdinājums: Lūdzu pārbaudiet formu rūpīgi ,ir dažas kļūdas!';
+$_['error_permission']       = 'Brīdinājums: Jums nav tiesību labot atribūtus!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Atribūta nosaukumam jābūt no 3 līdz 64 rakstzīmēm!';
+$_['error_product']          = 'Brīdinājums: Šo atribūtu nevar dzēst, jo tas pašlaik ir piešķirts %s precēm!';
