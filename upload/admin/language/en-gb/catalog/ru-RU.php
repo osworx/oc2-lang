297acc800cd3021a6b842822103c6ed56af38733
OSWorX <info@osworx.net>
2025-03-16 11:10:30 +0100

    New translations manufacturer.php (Russian)

--- upload/admin/language/en-gb/catalog/ru-RU.php
+++ upload/admin/language/en-gb/catalog/ru-RU.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Информация';
+$_['heading_title']     = 'Производители';
 
 // Text
-$_['text_success']           = 'Успех: Вы изменили информацию!';
-$_['text_list']              = 'Информационный список';
-$_['text_add']               = 'Добавить информацию';
-$_['text_edit']              = 'Редактировать информацию';
-$_['text_default']           = 'По умолчанию';
+$_['text_success']      = 'Успех: Вы изменили производителей!';
+$_['text_list']         = 'Список производителей';
+$_['text_add']          = 'Добавить производителя';
+$_['text_edit']         = 'Редактировать производителей';
+$_['text_default']      = 'По умолчанию';
+$_['text_percent']      = 'Процентаж';
+$_['text_amount']       = 'Фиксированная сумма';
 
 // Column
-$_['column_title']           = 'Заголовок информации';
-$_['column_sort_order']	     = 'Порядок сортировки';
-$_['column_action']          = 'Действие';
+$_['column_name']       = 'Название производителя';
+$_['column_sort_order'] = 'Порядок сортировки';
+$_['column_action']     = 'Действие';
 
 // Entry
-$_['entry_title']            = 'Заголовок информации';
-$_['entry_description']      = 'Описание';
-$_['entry_store']            = 'Магазины';
-$_['entry_meta_title'] 	     = 'Мета тег Title';
-$_['entry_meta_keyword'] 	 = 'Мета тег Ключевые слова';
-$_['entry_meta_description'] = 'Мета-тег описание';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'Низ';
-$_['entry_status']           = 'Статус';
-$_['entry_sort_order']       = 'Порядок сортировки';
-$_['entry_layout']           = 'Переопределение макета';
+$_['entry_name']        = 'Название производителя';
+$_['entry_store']       = 'Магазины';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'Изображение';
+$_['entry_sort_order']  = 'Порядок сортировки';
+$_['entry_type']        = 'Тип';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'Отобразить в нижней части.';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'Внимание: Проверьте форму на наличие ошибок!';
-$_['error_permission']       = 'Внимание: У Вас нет прав на изменение информации!';
-$_['error_title']            = 'Заголовок информации должен быть от 3 до 64 символов!';
-$_['error_description']      = 'Описание должно быть более 3 символов!';
-$_['error_meta_title']       = 'Мета-тег описание должен быть больше чем 3 и менее 255 символов!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'Внимание: Эту информационная страница не может быть удалена так как сейчас назначена как элемент аккаунта магазина!';
-$_['error_checkout']         = 'Внимание: Эта страница не может быть удалена, так как она назначена страницей условий оплаты магазина!';
-$_['error_affiliate']        = 'Внимание: Эту информационную страницу нельзя удалить, так как она назначена как элемент филиала магазина!';
-$_['error_return']           = 'Внимание: Эта страница не может быть удалена, так как она назначена как элемент возвратов магазина!';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'Внимание: Проверьте форму на наличие ошибок!';
+$_['error_permission']  = 'Внимание: Недостаточно прав на изменение производителей!';
+$_['error_name']        = 'Имя производителя должно быть от 2 до 64 символов!';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'Предупреждение: Этот производитель не может быть удален, поскольку он в настоящее время назначается %s продукции!';
