9de417acca2871e1c94266ad6f50ac3646e34b8a
OSWorX <info@osworx.net>
2025-03-16 11:10:24 +0100

    New translations manufacturer.php (Korean)

--- upload/admin/language/en-gb/catalog/ko-KR.php
+++ upload/admin/language/en-gb/catalog/ko-KR.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = '정보 페이지';
+$_['heading_title']     = '제조사';
 
 // Text
-$_['text_success']           = '완료: 정보 페이지를 수정했습니다.';
-$_['text_list']              = '정보 페이지 목록';
-$_['text_add']               = '정보 페이지 추가';
-$_['text_edit']              = '정보 페이지 편집';
-$_['text_default']           = '기본값';
+$_['text_success']      = '완료: 제조사를 수정했습니다.';
+$_['text_list']         = '제조사 목록';
+$_['text_add']          = '제조사 추가';
+$_['text_edit']         = '제조사 수정';
+$_['text_default']      = '기본값';
+$_['text_percent']      = '비율';
+$_['text_amount']       = '고정금액';
 
 // Column
-$_['column_title']           = '정보 페이지 제목';
-$_['column_sort_order']	     = '정렬순서';
-$_['column_action']          = '작업';
+$_['column_name']       = '제조사명';
+$_['column_sort_order'] = '정렬순서';
+$_['column_action']     = '작업';
 
 // Entry
-$_['entry_title']            = '정보 페이지 제목';
-$_['entry_description']      = '상세설명';
-$_['entry_store']            = '쇼핑몰';
-$_['entry_meta_title'] 	     = '메타 태그 제목';
-$_['entry_meta_keyword'] 	 = '메타 태그 키워드';
-$_['entry_meta_description'] = '메타 태그 설명';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = '하단에 표시';
-$_['entry_status']           = '상태';
-$_['entry_sort_order']       = '정렬순서';
-$_['entry_layout']           = '레이아웃 재정의';
+$_['entry_name']        = '제조사명';
+$_['entry_store']       = '쇼핑몰';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = '이미지';
+$_['entry_sort_order']  = '정렬순서';
+$_['entry_type']        = '유형';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = '하단 영역에 표시합니다.';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = '주의: 오류가 발생했습니다. 입력 항목을 다시 확인해주세요.';
-$_['error_permission']       = '주의: 정보 페이지를 수정할 수 있는 권한이 없습니다.';
-$_['error_title']            = '정보 페이지 제목은 3~64 글자 이내로 입력해주세요.';
-$_['error_description']      = '세부내용은 3 글자 이상 입력해주세요.';
-$_['error_meta_title']       = '메타 태그 제목은 3~255 글자 이내로 입력해주세요.';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = '주의: 이 정보 페이지는 현재 쇼핑몰 회원약관으로 설정되어 있으므로 삭제할 수 없습니다.';
-$_['error_checkout']         = '주의: 이 정보 페이지는 현재 쇼핑몰 주문약관으로 설정되어 있으므로 삭제할 수 없습니다.';
-$_['error_affiliate']        = '주의: 이 정보 페이지는 현재 쇼핑몰 제휴약관으로 설정되어 있으므로 삭제할 수 없습니다.';
-$_['error_return']           = '주의: 이 정보 페이지는 현재 쇼핑몰 반품약관으로 설정되어 있으므로 삭제할 수 없습니다.';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = '주의: 오류가 발생했습니다. 입력 항목을 다시 확인해주세요.';
+$_['error_permission']  = '주의: 제조사를 수정할 수 있는 권한이 없습니다.';
+$_['error_name']        = '제조사명은 2~64 글자 이내로 입력해주세요.';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = '주의: 현재 %s개의 상품에 이 제조사가 설정되어 있으므로 삭제할 수 없습니다.';
