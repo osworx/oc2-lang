5f3cefd28db5978d50bd48ae96b98ec043ea1c66
OSWorX <info@osworx.net>
2025-03-16 09:09:30 +0100

    New translations attribute_group.php (Hungarian)

--- upload/language/hu-HU/hu-HU.php
+++ upload/language/hu-HU/hu-HU.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Jellemzők';
+$_['heading_title']     = 'Jellemzőcsoportok';
 
 // Text
-$_['text_success']           = 'jellemzők módosítása sikeres!';
-$_['text_list']              = 'Jellemzőlista';
-$_['text_add']               = 'Jellemző hozzáadása';
-$_['text_edit']              = 'Jellemző szerkesztése';
+$_['text_success']      = 'Jellemzőcsoportok módosítása sikeres!';
+$_['text_list']         = 'Jellemzőcsoport lista';
+$_['text_add']          = 'Jellemzőcsoport hozzáadás';
+$_['text_edit']         = 'Jellemzőcsoport szerkesztése';
 
 // Column
-$_['column_name']            = 'Jellemző neve';
-$_['column_attribute_group'] = 'Jellemzőcsoport';
-$_['column_sort_order']      = 'Rendezés';
-$_['column_action']          = 'Művelet';
+$_['column_name']       = 'Jellemzőcsoport neve';
+$_['column_sort_order'] = 'Rendezés';
+$_['column_action']     = 'Művelet';
 
 // Entry
-$_['entry_name']             = 'Jellemző neve';
-$_['entry_attribute_group']  = 'Jellemzőcsoport';
-$_['entry_sort_order']       = 'Rendezés';
+$_['entry_name']        = 'Jellemzőcsoport neve';
+$_['entry_sort_order']  = 'Rendezés';
 
 // Error
-$_['error_warning']          = 'Figyelem: Kérjük, figyelmesen ellenőrizze a hibákat!';
-$_['error_permission']       = 'Figyelem: Nincs jogosultságod a jellemzők módosítására!';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'A jellemző nevének 3 és 64 karakter között kell lennie!';
-$_['error_product']          = 'Figyelem: Ez a jellemző nem törölhető, mivel jelenleg %s termékhez van hozzárendelve!';
+$_['error_warning']     = 'Figyelem: Kérjük, figyelmesen ellenőrizze a hibákat!';
+$_['error_permission']  = 'Figyelem: Nincs jogosultsága a jellemzőcsoportok módosítására!';
+$_['error_name']        = 'Jellemzőcsoport nevének 3 és 64 karakter között kell lennie!';
+$_['error_attribute']   = 'Figyelem: Ez az jellemzőcsoport nem törölhető, mivel jelenleg %s jellemző van hozzárendelve!';
+$_['error_product']     = 'Figyelem: Ez az jellemzőcsoport nem törölhető, mivel jelenleg %s termékhez van hozzárendelve!';
