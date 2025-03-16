21ce9adf3ac6305cdf1cfa07fd6a2291e8166723
OSWorX <info@osworx.net>
2025-03-16 11:13:07 +0100

    New translations recurring.php (Turkish)

--- upload/admin/language/en-gb/catalog/tr-TR.php
+++ upload/admin/language/en-gb/catalog/tr-TR.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'Ürünler';
+$_['heading_title']			= 'Yinelenen Profiller';
 
 // Text
-$_['text_success']           = 'Başarılı: Ürün bilgilerini değiştirdiniz!';
-$_['text_list']              = 'Ürün Listesi';
-$_['text_add']               = 'Ürün Ekle';
-$_['text_edit']              = 'Ürün Bilgilerini Değiştir';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'Varsayılan';
-$_['text_option']            = 'Seçenek';
-$_['text_option_value']      = 'Seçenek Değeri';
-$_['text_percent']           = 'Yüzde';
-$_['text_amount']            = 'Sabit tutar';
-
-// Column
-$_['column_name']            = 'Ürün Adı';
-$_['column_model']           = 'Model';
-$_['column_image']           = 'Resim';
-$_['column_price']           = 'Fiyat';
-$_['column_quantity']        = 'Miktar';
-$_['column_status']          = 'Durum';
-$_['column_action']          = 'Eylem';
+$_['text_success']          = 'Başarı:Yinelenen Profilleri düzenlediniz!';
+$_['text_list']             = 'Yinelenen Profil Listesi';
+$_['text_add']              = 'Yinelenen Profil ekle';
+$_['text_edit']             = 'Yinelenen Profilleri düzenle';
+$_['text_day']				= 'Gün';
+$_['text_week']				= 'Hafta';
+$_['text_semi_month']		= 'ayda iki kez olan';
+$_['text_month']			= 'Ay';
+$_['text_year']				= 'Yıl';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Yinelenen Miktarlar karşılaşma sıklığı ve dönüşüm miktarına göre hesaplanmaktadır. </p><p>Örneğin Karşılaşma Sıklığı olarak "Hafta" ve Dönüşüm Miktarı olarak "2" değerlerini kullanırsanız kullanıcı faturası 2 haftada bir defa düzenlenecektir. </p><p>Süre kullanıcının ödeme yapacağı Zaman Birimi Miktarı kadardır, ödemeleri iptal edileceği zamana kadar bekletmek isterseniz bu değeri "0" olarak belirleyin. </p>';
+$_['text_profile']			= 'Yinelenen profil';
+$_['text_trial']			= 'Deneme Profili';
 
 // Entry
-$_['entry_name']             = 'Ürün Adı';
-$_['entry_description']      = 'Açıklama';
-$_['entry_meta_title'] 	     = 'Meta etiketi başlığı';
-$_['entry_meta_keyword'] 	 = 'Meta etiketi anahtar kelimeler';
-$_['entry_meta_description'] = 'Meta etiketi açıklamasına';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Model';
-$_['entry_sku']              = 'SKU Numarası';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'Yerleşim';
-$_['entry_shipping']         = 'Kargo Gerekli';
-$_['entry_manufacturer']     = 'Üretici';
-$_['entry_store']            = 'Dükkanlar';
-$_['entry_date_available']   = 'Çıkış Tarihi';
-$_['entry_quantity']         = 'Miktar';
-$_['entry_minimum']          = 'En Az Miktar';
-$_['entry_stock_status']     = 'Stokta Yok';
-$_['entry_price']            = 'Fiyat';
-$_['entry_tax_class']        = 'Vergi Sınıfı';
-$_['entry_points']           = 'Puanlar';
-$_['entry_option_points']    = 'Puanlar';
-$_['entry_subtract']         = 'Stoktan Eksilt';
-$_['entry_weight_class']     = 'Ağırlık Sınıfı';
-$_['entry_weight']           = 'Ağırlık';
-$_['entry_dimension']        = 'Ebatlar (E x B x Y)';
-$_['entry_length_class']     = 'Uzunluk Sınıfı';
-$_['entry_length']           = 'Uzunluk';
-$_['entry_width']            = 'Genişlik';
-$_['entry_height']           = 'Yükseklik';
-$_['entry_image']            = 'Resim';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Müşteri Grubu';
-$_['entry_date_start']       = 'Başlangıç tarihi';
-$_['entry_date_end']         = 'Bitiş tarihi';
-$_['entry_priority']         = 'Öncelik';
-$_['entry_attribute']        = 'Özellik';
-$_['entry_attribute_group']  = 'Öznitelik Grubu';
-$_['entry_text']             = 'Yazı';
-$_['entry_option']           = 'Seçenek';
-$_['entry_option_value']     = 'Seçenek Değeri';
-$_['entry_required']         = 'Zorunlu';
-$_['entry_status']           = 'Durum';
-$_['entry_sort_order']       = 'Sıralama Düzeni';
-$_['entry_category']         = 'Kategoriler';
-$_['entry_filter']           = 'Filtreler';
-$_['entry_download']         = 'İndirmeler';
-$_['entry_related']          = 'İlgili ürünler';
-$_['entry_tag']          	 = 'Ürün etiketleri';
-$_['entry_reward']           = 'Ödül puanı';
-$_['entry_layout']           = 'Düzeni Geçersiz Kılma';
-$_['entry_recurring']        = 'Yinelenen profil';
+$_['entry_name']		    = 'İsim';
+$_['entry_price']			= 'Fiyat';
+$_['entry_duration']		= 'Süre';
+$_['entry_cycle']			= 'Döngü';
+$_['entry_frequency']		= 'Sıklık';
+$_['entry_trial_price']		= 'Deneme Fiyatı';
+$_['entry_trial_duration']	= 'Deneme Süresi';
+$_['entry_trial_status']	= 'Deneme Durumu';
+$_['entry_trial_cycle']	    = 'Deneme Döngüsü';
+$_['entry_trial_frequency'] = 'Deneme Sıklığı';
+$_['entry_status']			= 'Durum';
+$_['entry_sort_order']		= 'Sıralama Düzeni';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'Minimum Stok';
-$_['help_upc']               = 'Evrensel ürün kodu';
-$_['help_ean']               = 'Avrupa Madde Numarası';
-$_['help_jan']               = 'Japon Madde Numarası';
-$_['help_isbn']              = 'Uluslararası Standart Kitap Numarası';
-$_['help_mpn']               = 'Üretici Parça Numarası';
-$_['help_manufacturer']      = '(Otomatik Tamamla)';
-$_['help_minimum']           = 'Zorunlu Minimum Sipariş Miktarı belirle';
-$_['help_stock_status']      = 'Stoğu Tükenen Ürünün gösterilmesi veya gösterilmemesi';
-$_['help_points']            = 'Bu ürünü satın almak için gereken puan.';
-$_['help_category']          = '(Otomatik Tamamla)';
-$_['help_filter']            = '(Otomatik Tamamla)';
-$_['help_download']          = '(Otomatik Tamamla)';
-$_['help_related']           = '(Otomatik Tamamla)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'İsim';
+$_['column_sort_order']	    = 'Sıralama Düzeni';
+$_['column_action']         = 'Eylem';
 
 // Error
-$_['error_warning']          = 'Uyarı: Lütfen formu gösterilen hatalar nedeniyle dikkatlice kontrol edin!';
-$_['error_permission']       = 'Uyarı: Ürün bilgilerini değiştirme yetkiniz yok!';
-$_['error_name']             = 'Ürün Adı 3 karakterden büyük ve 255 karakterden küçük olmalıdır !';
-$_['error_meta_title']       = 'Meta Başlık en az 3 ve en fazla 255 karakter olmalıdır!';
-$_['error_model']            = 'Ürün Modeli 1 karakterden büyük ve 64 karakterden küçük olmalıdır !';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Uyarı: Lütfen formu gösterilen hatalar nedeniyle dikkatlice kontrol edin!';
+$_['error_permission']		= 'Uyarı: Yinelenen profilleri düzenleme iznine sahip değilsiniz!';
+$_['error_name']			= 'Profil Adı 3 karakterden büyük ve 255 karakterden küçük olmalıdır!';
+$_['error_product']			= 'Uyarı:Yinelenen Profil ürünlerin %s kısmına atanmış olduğu için silinemez!';
