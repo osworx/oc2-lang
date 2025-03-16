7e1ac08868eaa2e774836a36992633d068def393
OSWorX <info@osworx.net>
2025-03-16 14:17:23 +0100

    New translations category.php (Romanian)

--- upload/admin/language/en-gb/catalog/ro-RO.php
+++ upload/admin/language/en-gb/catalog/ro-RO.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Grupuri de atribute';
+$_['heading_title']          = 'Categorii';
 
 // Text
-$_['text_success']      = 'Succes: Ai modificat grupuri de atribute!';
-$_['text_list']         = 'Lista grupurilor de atribute';
-$_['text_add']          = 'Adaugă grup de atribute';
-$_['text_edit']         = 'Modifică grup de atribute';
+$_['text_success']           = 'Succes: Ai modificat categoriile!';
+$_['text_list']              = 'Lista de categorii';
+$_['text_add']               = 'Adaugă categorie';
+$_['text_edit']              = 'Modifică Categoria';
+$_['text_default']           = 'Implicit';
 
 // Column
-$_['column_name']       = 'Denumirea grupului de atribute';
-$_['column_sort_order'] = 'Ordinea de sortare';
-$_['column_action']     = 'Acțiune';
+$_['column_name']            = 'Denumire categorie';
+$_['column_sort_order']      = 'Ordinea de sortare';
+$_['column_action']          = 'Acțiune';
 
 // Entry
-$_['entry_name']        = 'Denumirea grupului de atribute';
-$_['entry_sort_order']  = 'Ordinea de sortare';
+$_['entry_name']             = 'Denumire categorie';
+$_['entry_description']      = 'Descriere';
+$_['entry_meta_title'] 	     = 'Meta tag: Titlu';
+$_['entry_meta_keyword']     = 'Meta tag: cuvinte cheie';
+$_['entry_meta_description'] = 'Meta tag: descriere';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Părinte';
+$_['entry_filter']           = 'Filtre';
+$_['entry_store']            = 'Magazine';
+$_['entry_image']            = 'imagine';
+$_['entry_top']              = 'Top';
+$_['entry_column']           = 'Coloane';
+$_['entry_sort_order']       = 'Ordinea de sortare';
+$_['entry_status']           = 'Stare';
+$_['entry_layout']           = 'Suprascrie Layout-ul';
+
+// Help
+$_['help_filter']            = '(Completare automată)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Afişat în bara de sus din meniu. Functioneaza doar pentru categoriile sus părinte.';
+$_['help_column']            = 'Numărul de coloane pentru categoriile de jos 3. Functioneaza doar pentru categoriile sus părinte.';
 
 // Error
-$_['error_warning']     = 'Avertisment: Vă rugăm să verificaţi formularul pentru erori!';
-$_['error_permission']  = 'Avertisment: Nu aveţi permisiunile necesare pentru a modifica grupurile de atribute!';
-$_['error_name']        = 'Denumirea grupului de atribute trebuie să aibă între 3 şi 64 de caractere!';
-$_['error_attribute']   = 'Avertisment: Acest grup de atribute nu poate fi șters deoarece este asociat la %s atribute!';
-$_['error_product']     = 'Avertisment: Acest grup de atribute nu poate fi șters deoarece este asociat la %s produse!';
+$_['error_warning']          = 'Avertisment: Vă rugăm să verificaţi formularul pentru erori!';
+$_['error_permission']       = 'Avertisment: Nu aveţi permisiunile necesare pentru a modifica categorii!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Meta tag : Titlu, trebuie să aibă mai mult de 3 şi mai puţin de 255 caractere!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
