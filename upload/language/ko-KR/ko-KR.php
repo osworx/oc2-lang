b81f2603bd0b52556bf51e7cd5118a8d1ac86151
OSWorX <info@osworx.net>
2025-03-16 09:11:04 +0100

    New translations filemanager.php (Korean)

--- upload/language/ko-KR/ko-KR.php
+++ upload/language/ko-KR/ko-KR.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = '대시보드';
+$_['heading_title']    = '이미지 관리자';
+
+// Text
+$_['text_uploaded']    = '완료: 파일을 업로드했습니다.';
+$_['text_directory']   = '완료: 폴더를 만들었습니다.';
+$_['text_delete']      = '완료: 파일 또는 폴더를 삭제했습니다.';
+
+// Entry
+$_['entry_search']     = '검색..';
+$_['entry_folder']     = '폴더이름';
 
 // Error
-$_['error_install'] = '주의: 설치 디렉토리(install)가 아직 남아있습니다. 보안 강화를 위해 반드시 삭제하시기 바랍니다.';
+$_['error_permission'] = '주의: 권한이 없습니다.';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = '주의: 이미 같은 이름의 파일 또는 폴더가 있습니다.';
+$_['error_directory']  = '주의: 폴더가 존재하지 않습니다.';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = '주의: 잘못된 파일형식입니다.';
+$_['error_upload']     = '주의: 알 수 없는 이유로 파일을 업로드 할 수 없습니다.';
+$_['error_delete']     = '주의: 이 폴더는 삭제할 수 없습니다.';
