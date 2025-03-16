2776a3918cf08fa6bb08af841b91158ab4b9f009
OSWorX <info@osworx.net>
2025-03-16 11:05:15 +0100

    New translations attribute.php (Swedish)

---
+++ upload/admin/language/en-gb/catalog/sv-SE.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Attribut';
+
+// Text
+$_['text_success']           = 'Klart: Du har ändrat attributen!';
+$_['text_list']              = 'Attributlista';
+$_['text_add']               = 'Lägg till Attribut';
+$_['text_edit']              = 'Redigera Attribut';
+
+// Column
+$_['column_name']            = 'Attributnamn';
+$_['column_attribute_group'] = 'Attributgrupp';
+$_['column_sort_order']      = 'Sorteringsordning';
+$_['column_action']          = 'Åtgärd';
+
+// Entry
+$_['entry_name']             = 'Attributnamn';
+$_['entry_attribute_group']  = 'Attributgrupp';
+$_['entry_sort_order']       = 'Sorteringsordning';
+
+// Error
+$_['error_warning']          = 'Varning: Kolla gärna formuläret noga vad det gäller fel!';
+$_['error_permission']       = 'Varning: Du har inte behörighet att redigera attribut!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Attributets Namn, måste vara mellan 3 och 64 tecken!';
+$_['error_product']          = 'Varning: Detta attribut kan inte raderas, det är tilldelat %s produkter!';
