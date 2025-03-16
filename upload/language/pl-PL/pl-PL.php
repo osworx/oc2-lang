fc200c4a2a7e83977c14e69e231cd4fd4d9f2ba1
OSWorX <info@osworx.net>
2025-03-16 09:12:12 +0100

    New translations forgotten.php (Polish)

--- upload/language/pl-PL/pl-PL.php
+++ upload/language/pl-PL/pl-PL.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Zapomniałeś hasła?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Wersja %s';
+$_['text_forgotten']  = 'Zapomniane hasło';
+$_['text_your_email'] = 'Twój adres e-mail';
+$_['text_email']      = 'Wprowadź adres e-mail powiązany z Twoim kontem. Kliknij opcję Prześlij, aby otrzymać na skrzynkę e-mail link umożliwiający zresetowanie hasła.';
+$_['text_success']    = 'Mail z linkiem potwierdzającym został wysłany na adres email admina.';
+
+// Entry
+$_['entry_email']     = 'Adres e-mail';
+$_['entry_password']  = 'Nowe hasło';
+$_['entry_confirm']   = 'Potwierdź';
+
+// Error
+$_['error_email']     = 'Ostrzeżenie: Adres e-mail nie został odnaleziony, proszę spróbuj ponownie!';
+$_['error_password']  = 'Hasło musi zawierać od 4 do 20 znaków!';
+$_['error_confirm']   = 'Hasło i potwierdzenie hasła nie są identyczne!';
