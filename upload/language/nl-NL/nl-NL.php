7b96a9508e41b14a9a030e3d78254b7ec422195b
OSWorX <info@osworx.net>
2025-03-16 09:11:39 +0100

    New translations forgotten.php (Dutch)

--- upload/language/nl-NL/nl-NL.php
+++ upload/language/nl-NL/nl-NL.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Je wachtwoord vergeten?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Versie %s';
+$_['text_forgotten']  = 'Vergeten wachtwoord';
+$_['text_your_email'] = 'Uw e-mailadres';
+$_['text_email']      = 'Voer het e-mailadres gekoppeld aan uw account in.Klik op verzenden om de wachtwoord reset link te verzenden naar jou e-mailadres.';
+$_['text_success']    = 'Een email met een bevestigingslink is naar jou beheerder e-mailadres gestuurd.';
+
+// Entry
+$_['entry_email']     = 'E-mailadres';
+$_['entry_password']  = 'Nieuw wachtwoord';
+$_['entry_confirm']   = 'Bevestigen';
+
+// Error
+$_['error_email']     = 'Opgelet: Het e-mailadres is niet gevonden in onze bestanden, probeert u opnieuw.';
+$_['error_password']  = 'Password must be between 4 and 20 characters!';
+$_['error_confirm']   = 'Wachtwoorden komen niet overeen.';
