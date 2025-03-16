d60bfbed25d3514da43d2a067caf90ae6a55b21d
OSWorX <info@osworx.net>
2025-03-16 14:28:47 +0100

    New translations forgotten.php (Chinese Traditional)

--- upload/admin/language/en-gb/common/zh-TW.php
+++ upload/admin/language/en-gb/common/zh-TW.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = '忘記密碼？';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = '版本 %s';
+$_['text_forgotten']  = '忘記密碼';
+$_['text_your_email'] = '您的電子郵件地址';
+$_['text_email']      = '請輸入與您帳號連結的電子郵件地址。';
+$_['text_success']    = '系統已經將確認信發送到您的電子信箱';
+
+// Entry
+$_['entry_email']     = '電子郵件地址';
+$_['entry_password']  = '新密碼';
+$_['entry_confirm']   = '確認';
+
+// Error
+$_['error_email']     = '警告： 在我們的記錄中找不到您的電子郵件地址，請再試一次！';
+$_['error_password']  = '密碼必須介於 4 到 20 個字元之間！';
+$_['error_confirm']   = '密碼與確認密碼結果不符！';
