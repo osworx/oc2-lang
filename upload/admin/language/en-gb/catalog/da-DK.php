e17a4e906715efb14924628734a9c2f10eb27f81
OSWorX <info@osworx.net>
2025-03-16 14:16:42 +0100

    New translations attribute_group.php (Danish)

--- upload/admin/language/en-gb/catalog/da-DK.php
+++ upload/admin/language/en-gb/catalog/da-DK.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Egenskaber';
+$_['heading_title']     = 'Egenskabsgrupper';
 
 // Text
-$_['text_success']           = 'Succes! Du har ændret egenskaber!';
-$_['text_list']              = 'Egenskaber';
-$_['text_add']               = 'Tilføj egenskab';
-$_['text_edit']              = 'Rediger egenskab';
+$_['text_success']      = 'Succes: Du har ændre egenskabsgrupper!';
+$_['text_list']         = 'Egenskabsgrupper';
+$_['text_add']          = 'Tilføj egenskabsgruppe';
+$_['text_edit']         = 'Rediger egenskabsgruppe';
 
 // Column
-$_['column_name']            = 'Egenskab navn';
-$_['column_attribute_group'] = 'Egenskab gruppe';
-$_['column_sort_order']      = 'Sorteringsrækkefølge';
-$_['column_action']          = 'Handling';
+$_['column_name']       = 'Egenskabsgruppe navn';
+$_['column_sort_order'] = 'Sorteringsrækkefølge';
+$_['column_action']     = 'Handling';
 
 // Entry
-$_['entry_name']             = 'Egenskab navn';
-$_['entry_attribute_group']  = 'Egenskab gruppe';
-$_['entry_sort_order']       = 'Sorteringsrækkefølge';
+$_['entry_name']        = 'Egenskabsgruppe navn';
+$_['entry_sort_order']  = 'Sorteringsrækkefølge';
 
 // Error
-$_['error_warning']          = 'Advarsel: Gennemgå venligst formularen for fejl!';
-$_['error_permission']       = 'Fejl! Du har ikke tilladelse til at ændre egenskaber!';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'Egenskabsnavn skal være mellem 3 og 64 karakterer!';
-$_['error_product']          = 'Fejl! Denne egenskab kan ikke slettes, da den i øjeblikket er tilknyttet %s produkter!';
+$_['error_warning']     = 'Advarsel: Gennemgå venligst formularen for fejl!';
+$_['error_permission']  = 'Advarsel: Du har ikke tilladelse til at ændre egenskabsgrupper!';
+$_['error_name']        = 'Egenskabsgruppe navn skal være mellem 3 og 64 karakterer!';
+$_['error_attribute']   = 'Advarsel: Denne egenskabsgruppe kan ikke slettes, da den i øjeblikket er tilknyttet %s egenskaber!';
+$_['error_product']     = 'Advarsel: Denne egenskabsgruppe kan ikke slettes, da den i øjeblikket er tilknyttet %s produkter!';
