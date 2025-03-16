b88c05613f874a2144a9175652f29ac285a2320b
OSWorX <info@osworx.net>
2025-03-16 11:14:15 +0100

    New translations review.php (Bosnian)

--- upload/admin/language/en-gb/catalog/bs-BA.php
+++ upload/admin/language/en-gb/catalog/bs-BA.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'Ponavljajući profili';
+$_['heading_title']     = 'Recenzije';
 
 // Text
-$_['text_success']          = 'Uspjeh: Izmjena ponavljajućih profila!';
-$_['text_list']             = 'Lista Ponavljajućih profila';
-$_['text_add']              = 'Dodajte ponavljajući profil';
-$_['text_edit']             = 'Uređivanje ponavljajućeg profila';
-$_['text_day']				= 'Dan';
-$_['text_week']				= 'Sedmica';
-$_['text_semi_month']		= 'Polu mjesec';
-$_['text_month']			= 'Mjesec';
-$_['text_year']				= 'Godina';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Recurring amounts are calculated by the frequency and cycles.</p><p>For example if you use a frequency of "week" and a cycle of "2", then the user will be billed every 2 weeks.</p><p>The duration is the number of times the user will make a payment, set this to 0 if you want payments until they are cancelled.</p>';
-$_['text_profile']			= 'Ponavljajući profil';
-$_['text_trial']			= 'Probni profil';
-
-// Entry
-$_['entry_name']		    = 'Ime';
-$_['entry_price']			= 'Cijena';
-$_['entry_duration']		= 'Trajanje';
-$_['entry_cycle']			= 'Ciklus';
-$_['entry_frequency']		= 'Frekvencija';
-$_['entry_trial_price']		= 'Probna cijena';
-$_['entry_trial_duration']	= 'Probno trajanje';
-$_['entry_trial_status']	= 'Probni status';
-$_['entry_trial_cycle']	    = 'Probni ciklus';
-$_['entry_trial_frequency'] = 'Probna frekvencija';
-$_['entry_status']			= 'Status';
-$_['entry_sort_order']		= 'Redoslijed sortiranja';
+$_['text_success']      = 'Uspjeh: Izmjena recenzija!';
+$_['text_list']         = 'Pregled recenzija';
+$_['text_add']          = 'Dodaj recenziju';
+$_['text_edit']         = 'Uređivanje recenzije';
 
 // Column
-$_['column_name']			= 'Ime';
-$_['column_sort_order']	    = 'Redoslijed sortiranja';
-$_['column_action']         = 'Akcija';
+$_['column_product']    = 'Proizvod';
+$_['column_author']     = 'Autor';
+$_['column_rating']     = 'Rejting';
+$_['column_status']     = 'Status';
+$_['column_date_added'] = 'Datum dodavanja';
+$_['column_action']     = 'Akcija';
+
+// Entry
+$_['entry_product']     = 'Proizvod';
+$_['entry_author']      = 'Autor';
+$_['entry_rating']      = 'Rejting';
+$_['entry_status']      = 'Status';
+$_['entry_text']        = 'Tekst';
+$_['entry_date_added']  = 'Datum dodavanja';
+
+// Help
+$_['help_product']      = '(Samodovršetak)';
 
 // Error
-$_['error_warning']         = 'Upozorenje: Provjerite obrazac pažljivo radi grešaka!';
-$_['error_permission']		= 'Upozorenje: Nemate dozvolu za mijenjanje ponavljajućih profila!';
-$_['error_name']			= 'Naziv proizvoda mora biti veći od 3 i manji od 255 znakova!';
-$_['error_product']			= 'Upozorenje: Ovaj ponavljajući profil se ne može izbrisati jer je trenutno dodijeljen %s proizvoda!';
+$_['error_warning']     = 'Upozorenje: Provjerite obrazac pažljivo radi grešaka!';
+$_['error_permission']  = 'Upozorenje: Nemate dozvolu za mijenjanje recenzija!';
+$_['error_product']     = 'Proizvoda potrebno!';
+$_['error_author']      = 'Autor mora biti između 3 i 64 znakova!';
+$_['error_text']        = 'Tekst recenzije mora biti barem 1 znak!';
+$_['error_rating']      = 'Potreban rejting recenzije!';
