bebaaa9b4c908b78a03ddf9c5169b1b13f0d2d51
OSWorX <info@osworx.net>
2025-03-16 14:16:57 +0100

    New translations attribute_group.php (Slovak)

--- upload/admin/language/en-gb/catalog/sk-SK.php
+++ upload/admin/language/en-gb/catalog/sk-SK.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Vlastnosti';
+$_['heading_title']     = 'Skupiny vlastností';
 
 // Text
-$_['text_success']           = 'Hotovo: Atribút bol zmenený!';
-$_['text_list']              = 'Zoznam vlastností';
-$_['text_add']               = 'Pridať vlastnosť';
-$_['text_edit']              = 'Upraviť vlastnosť';
+$_['text_success']      = 'Hotovo: Upravili ste skupinu vlastností!';
+$_['text_list']         = 'Zoznam skupín vlastností';
+$_['text_add']          = 'Pridať skupinu vlastností';
+$_['text_edit']         = 'Upraviť skupinu vlastností';
 
 // Column
-$_['column_name']            = 'Názov vlastnosti';
-$_['column_attribute_group'] = 'Skupina vlastností';
-$_['column_sort_order']      = 'Poradie';
-$_['column_action']          = 'Akcia';
+$_['column_name']       = 'Názov skupiny vlastností';
+$_['column_sort_order'] = 'Poradie';
+$_['column_action']     = 'Akcia';
 
 // Entry
-$_['entry_name']             = 'Názov vlastnosti';
-$_['entry_attribute_group']  = 'Skupina vlastností';
-$_['entry_sort_order']       = 'Poradie';
+$_['entry_name']        = 'Názov skupiny vlastností';
+$_['entry_sort_order']  = 'Poradie';
 
 // Error
-$_['error_warning']          = 'Varovanie: Skontrolujte formulár starostlivo kvôli chybám!';
-$_['error_permission']       = 'Varovanie: Nemáte oprávnenie k úprave vlastností!';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'Názov vlastnosti musí byť medzi v rozmedzí 3 až 64 znakov!';
-$_['error_product']          = 'Varovanie: Táto vlastnosť nemôže byť odstránená, pretože je v súčasnosti priradená k %s produktom!';
+$_['error_warning']     = 'Varovanie: Skontrolujte formulár starostlivo kvôli chybám!';
+$_['error_permission']  = 'Varovanie: Nemáte potrebné oprávnenia na upravenie skupiny vlastností!';
+$_['error_name']        = 'Názov skupiny vlastností musí byť v rozmedzí 3 až 64 znakov!';
+$_['error_attribute']   = 'Varovanie: Táto skupina vlastností nemôže byť zmazaná! Je priradená k %s vlastnostiam!';
+$_['error_product']     = 'Varovanie: Táto skupina vlastností nemôže byť zmazaná! Je priradená k %s produktom!';
