58ab819fa1e8ec5d4f2d764b8df439e7f5f23b7b
OSWorX <info@osworx.net>
2025-03-16 09:11:44 +0100

    New translations category.php (Norwegian)

--- upload/language/no-NO/no-NO.php
+++ upload/language/no-NO/no-NO.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Produktegenskaper - Grupper';
+$_['heading_title']          = 'Kategorier';
 
 // Text
-$_['text_success']      = 'Endringer ble vellykket lagret.';
-$_['text_list']         = 'Liste';
-$_['text_add']          = 'Legg til';
-$_['text_edit']         = 'Endre';
+$_['text_success']           = 'Endringer ble vellykket lagret.';
+$_['text_list']              = 'Liste';
+$_['text_add']               = 'Legg til';
+$_['text_edit']              = 'Endre';
+$_['text_default']           = 'Standard';
 
 // Column
-$_['column_name']       = 'Navn';
-$_['column_sort_order'] = 'Sortering';
-$_['column_action']     = 'Valg';
+$_['column_name']            = 'Navn';
+$_['column_sort_order']      = 'Sortering';
+$_['column_action']          = 'Valg';
 
 // Entry
-$_['entry_name']        = 'Navn';
-$_['entry_sort_order']  = 'Sortering';
+$_['entry_name']             = 'Navn';
+$_['entry_description']      = 'Beskrivelse';
+$_['entry_meta_title'] 	     = 'Meta-tittel';
+$_['entry_meta_keyword']     = 'Meta-nøkkelord';
+$_['entry_meta_description'] = 'Meta-beskrivelse';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Overordnet kategori';
+$_['entry_filter']           = 'Filtre';
+$_['entry_store']            = 'Butikker';
+$_['entry_image']            = 'Bilde';
+$_['entry_top']              = 'Toppmeny';
+$_['entry_column']           = 'Kolonner';
+$_['entry_sort_order']       = 'Sortering';
+$_['entry_status']           = 'Status';
+$_['entry_layout']           = 'Overstyring av visning';
+
+// Help
+$_['help_filter']            = '(Autofullfør)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Vis i toppmenyen. Dette virker bare for toppkategorier.';
+$_['help_column']            = 'Antall kolonner som skal brukes til de nederste 3 kategorier. Dette virker bare for toppkategorier.';
 
 // Error
-$_['error_warning']     = 'Påse at alle påkrevde felter er fylt inn.';
-$_['error_permission']  = 'Du har ikke rettigheter til å utføre valgte handling.';
-$_['error_name']        = 'Navn må inneholde mellom 3 og 64 tegn.';
-$_['error_attribute']   = 'Denne gruppen kan ikke slettes ettersom den er tilknyttet %s produktegenskaper.';
-$_['error_product']     = 'Denne gruppen kan ikke slettes ettersom den er tilknyttet %s produkter.';
+$_['error_warning']          = 'Påse at alle påkrevde felter er fylt inn.';
+$_['error_permission']       = 'Du har ikke rettigheter til å utføre valgte handling.';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Meta-tittel må inneholde mellom 3 og 255 tegn.';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
