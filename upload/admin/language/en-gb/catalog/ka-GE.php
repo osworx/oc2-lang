a85f4e026c976ae8373b73f3f2a489bd32f930d6
OSWorX <info@osworx.net>
2025-03-16 11:12:55 +0100

    New translations recurring.php (Georgian)

--- upload/admin/language/en-gb/catalog/ka-GE.php
+++ upload/admin/language/en-gb/catalog/ka-GE.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'პროდუქცია';
+$_['heading_title']			= 'განმეორებადი პროფილები';
 
 // Text
-$_['text_success']           = 'წარმატება თქვენ უკვე  პროდუქტები მოდიფიცირებულია!';
-$_['text_list']              = 'პროდუქციის სია';
-$_['text_add']               = 'პროდუქციის დამატება';
-$_['text_edit']              = 'პროდუქციის რედაქტირება ';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'სტანდარტი';
-$_['text_option']            = 'ვარიანტი';
-$_['text_option_value']      = 'ვარიანტის ფასი';
-$_['text_percent']           = 'პროცენტი';
-$_['text_amount']            = 'ფიქსირებული თანხა';
-
-// Column
-$_['column_name']            = 'პროდუქციის დასახელება';
-$_['column_model']           = 'მოდელი';
-$_['column_image']           = 'სურათი';
-$_['column_price']           = 'ფასი';
-$_['column_quantity']        = 'რაოდენობა';
-$_['column_status']          = 'სტატუსი';
-$_['column_action']          = 'მოქმედება';
+$_['text_success']          = 'წარმატება თქვენ  განმეორებადი პროფილები შეცვლილია!';
+$_['text_list']             = ' განმეორებადი პროფილების სია';
+$_['text_add']              = ' განმეორებადი პროფილების დამატება';
+$_['text_edit']             = ' განმეორებადი პროფილების რედაქტირება';
+$_['text_day']				= 'დღე';
+$_['text_week']				= 'კვირა';
+$_['text_semi_month']		= 'ნახევარი თვე';
+$_['text_month']			= 'თვე';
+$_['text_year']				= 'წელი';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> განმეორებადი თანხები გათვლილი მიერ სიხშირე და ციკლები.</p><p>მაგალითად, თუ თქვენ იყენებთ სიხშირე "კვირა" და ციკლი "2", მაშინ მომხმარებელს იქნება დარიცხული ყოველ 2 კვირაში.</p><p>ხანგრძლივობა არის რამდენჯერმე შესახებ გახდის გადახდის, მითითებული 0 თუ გსურთ გადასახადები მათ გაუქმებამდე.</p>';
+$_['text_profile']			= 'განმეორებადი პროფილი';
+$_['text_trial']			= 'საცდელი პროფილი';
 
 // Entry
-$_['entry_name']             = 'პროდუქციის დასახელება';
-$_['entry_description']      = 'აღწერა';
-$_['entry_meta_title'] 	     = 'მეტა ტეგი სათაური';
-$_['entry_meta_keyword'] 	 = 'მეტა ტეგი საკვანძო სიტყვები';
-$_['entry_meta_description'] = 'მეტა ტეგი აღწერა';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'მოდელი';
-$_['entry_sku']              = 'არტიკული';
-$_['entry_upc']              = 'პროდუქციის უნივერსალური კოდი ';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'ადგილმდებარეობა';
-$_['entry_shipping']         = 'მიწოდების მოთხოვნა';
-$_['entry_manufacturer']     = 'მწარმოებელი';
-$_['entry_store']            = 'მაღაზიები';
-$_['entry_date_available']   = 'თარიღი ხელმისაწვდომია';
-$_['entry_quantity']         = 'რაოდენობა';
-$_['entry_minimum']          = 'მინ.რაოდენობა';
-$_['entry_stock_status']     = 'სტატუსი - საწყობში წვდომა არ არის';
-$_['entry_price']            = 'ფასი';
-$_['entry_tax_class']        = 'საგადასახადო კლასი';
-$_['entry_points']           = 'ქულები';
-$_['entry_option_points']    = 'ქულები';
-$_['entry_subtract']         = 'საწყობიდან გამოკლება';
-$_['entry_weight_class']     = 'წონის კლასი';
-$_['entry_weight']           = 'წონა';
-$_['entry_dimension']        = 'ზომები';
-$_['entry_length_class']     = 'სიგრძის კლასი';
-$_['entry_length']           = 'სიგრძე';
-$_['entry_width']            = 'სიგანე';
-$_['entry_height']           = 'სიმაღლე';
-$_['entry_image']            = 'სურათი';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'დამკვეთების ჯგუფი';
-$_['entry_date_start']       = 'საწყისი თარიღი';
-$_['entry_date_end']         = 'დასასრულის თარიღი';
-$_['entry_priority']         = 'პრიორიტეტული';
-$_['entry_attribute']        = 'მახასიათებლები';
-$_['entry_attribute_group']  = 'ატრიბუტის ჯგუფი';
-$_['entry_text']             = 'ტექსტი';
-$_['entry_option']           = 'ვარიანტი';
-$_['entry_option_value']     = 'ვარიანტის ფასი';
-$_['entry_required']         = 'სავალდებულოა';
-$_['entry_status']           = 'სტატუსი';
-$_['entry_sort_order']       = 'შეკვეთის სორტირება';
-$_['entry_category']         = 'კატეგორიები';
-$_['entry_filter']           = 'ფილტრები';
-$_['entry_download']         = 'ჩამოტვირთვები';
-$_['entry_related']          = 'მსგავსი პროდუქტები';
-$_['entry_tag']          	 = 'პროდუქტის საკვანძო სიტყვები';
-$_['entry_reward']           = 'საჩუქრების პუნქტი';
-$_['entry_layout']           = 'მაკეტის გარდაქმნა';
-$_['entry_recurring']        = 'განმეორებადი პროფილი';
+$_['entry_name']		    = 'სახელი';
+$_['entry_price']			= 'ფასი';
+$_['entry_duration']		= 'ხანგრძლივობა';
+$_['entry_cycle']			= 'ციკლი';
+$_['entry_frequency']		= 'სიხშირე';
+$_['entry_trial_price']		= 'საცდელი ფასი';
+$_['entry_trial_duration']	= 'საცდელი ხანგრძლივობა';
+$_['entry_trial_status']	= 'საცდელი სტატუსი';
+$_['entry_trial_cycle']	    = 'საცდელი ციკლი';
+$_['entry_trial_frequency'] = 'საცდელი სიხშირე';
+$_['entry_status']			= 'სტატუსი';
+$_['entry_sort_order']		= 'შეკვეთის სორტირება';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'საწყობში შენახვა ერთეული';
-$_['help_upc']               = 'პროდუქტის უნივერსალური კოდი';
-$_['help_ean']               = 'ევროპული მუხლი რაოდენობა';
-$_['help_jan']               = 'იაპონური მუხლი რაოდენობა';
-$_['help_isbn']              = 'საერთაშორისო სტანდარტული  ნომერი';
-$_['help_mpn']               = 'მწარმოებელი ნაწილი ნომერი';
-$_['help_manufacturer']      = 'ავტოშესრულება';
-$_['help_minimum']           = 'აიძულოს მინიმალური მოცულობის შეუკვეთა';
-$_['help_stock_status']      = 'სტატუსი ნაჩვენებია, როდესაც პროდუქტი საწყობში არ არის';
-$_['help_points']            = 'პუნქტების რაოდენობა შესაძენად ამ ერთეულზე. თუ თქვენ არ გსურთ ამ პროდუქტის უნდა იყოს შეძენილი რაოდენობა დატოვონ, როგორც 0.';
-$_['help_category']          = 'ავტოშესრულება';
-$_['help_filter']            = 'ავტოშესრულება';
-$_['help_download']          = 'ავტოშესრულება';
-$_['help_related']           = 'ავტოშესრულება';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'სახელი';
+$_['column_sort_order']	    = 'შეკვეთის სორტირება';
+$_['column_action']         = 'მოქმედება';
 
 // Error
-$_['error_warning']          = 'გაფრთხილება: გთხოვთ ყურადღებით შეამოწმოთ შეცდომების ფორმები!';
-$_['error_permission']       = 'გაფრთხილება: თქვენ არ გაქვთ უფლება შეცვალოთ პროდუქცია!';
-$_['error_name']             = 'პროდუქტის სახელი უნდა იყოს მეტი 3 და ნაკლები 255 სიმბოლო!';
-$_['error_meta_title']       = 'მეტა სათაური უნდა იყოს მეტი 3 და ნაკლები 255 სიმბოლო!';
-$_['error_model']            = 'პროდუქტის მოდელი უნდა იყოს მეტი 1 და ნაკლები 64 სიმბოლო!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'გაფრთხილება: გთხოვთ ყურადღებით შეამოწმოთ შეცდომების ფორმები!';
+$_['error_permission']		= 'გაფრთხილება: თქვენ არ გაქვთ უფლება შეცვალოთ განმეორებადი პროფილები!';
+$_['error_name']			= 'პროფილი სახელი უნდა იყოს მეტი 3 და ნაკლები 255 სიმბოლო!';
+$_['error_product']			= 'გაფრთხილება: ეს განმეორებადი ნახვა არ შეიძლება წაიშალოს რადგან ის ამჟამად ენიჭება%s პროდუქცია!';
