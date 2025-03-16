ae270ab59b07250f2cf1a5a05072a732c69847b6
OSWorX <info@osworx.net>
2025-03-16 09:06:07 +0100

    New translations download.php (French)

--- upload/language/fr-FR/fr-FR.php
+++ upload/language/fr-FR/fr-FR.php
@@ -1,46 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Catégories';
+$_['heading_title']     = 'Téléchargements';
 
 // Text
-$_['text_success']           = 'Modification des catégories réussie !';
-$_['text_list']              = 'Liste des catégories';
-$_['text_add']               = 'Ajouter une catégorie';
-$_['text_edit']              = 'Modifier une catégorie';
-$_['text_default']           = 'Par défaut';
+$_['text_success']      = 'Modification de téléchargements réussie!';
+$_['text_list']         = 'Liste des téléchargements';
+$_['text_add']          = 'Ajouter un téléchargement';
+$_['text_edit']         = 'Modifier un téléchargement';
+$_['text_upload']       = 'Transfert du fichier réussi!';
 
 // Column
-$_['column_name']            = 'Nom de catégorie';
-$_['column_sort_order']      = 'Classement';
-$_['column_action']          = 'Action';
+$_['column_name']       = 'Nom du téléchargement';
+$_['column_date_added'] = 'Ajouté le';
+$_['column_action']     = 'Action';
 
 // Entry
-$_['entry_name']             = 'Nom de catégorie';
-$_['entry_description']      = 'Description';
-$_['entry_meta_title'] 	     = 'Titre de métaélément';
-$_['entry_meta_keyword']     = 'Mots-clef de métaélément';
-$_['entry_meta_description'] = 'Description de métaélément';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = 'Parent';
-$_['entry_filter']           = 'Filtres';
-$_['entry_store']            = 'Boutiques';
-$_['entry_image']            = 'Image';
-$_['entry_top']              = 'Haut';
-$_['entry_column']           = 'Colonnes';
-$_['entry_sort_order']       = 'Classement';
-$_['entry_status']           = 'État';
-$_['entry_layout']           = 'Outrepasser mise en page';
+$_['entry_name']        = 'Nom du téléchargement';
+$_['entry_filename']    = 'Nom du fichier';
+$_['entry_mask']        = 'Masquer';
 
 // Help
-$_['help_filter']            = 'Autocomplétion';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = 'Afficher dans la barre en tête. Applicable uniquement aux catégories clefs.';
-$_['help_column']            = 'Nombre de colonnes à utiliser pour les trois catégories au bas. Applicable uniquement aux catégories clefs.';
+$_['help_filename']     = 'Vous pouvez transférer à l\'aide du bouton d\'envoi ou utiliser la convention FTP pour envoyer au répertoire de téléchargement et entrer les détails ci-dessous.';
+$_['help_mask']         = 'Il est recommandée de différencier le nom de fichier et son masque pour empêcher la création de liens directs vers vos téléchargements.';
 
 // Error
-$_['error_warning']          = 'Attention : veuillez vérifier le formulaire';
-$_['error_permission']       = 'Attention : vous n’êtes pas autorisé à modifier les catégories !';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = 'Le titre méta doit contenir plus de 3 et moins de 255 caractères !';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = 'Attention : veuillez vérifier le formulaire';
+$_['error_permission']  = 'Avertissement: vous n\'êtes pas autorisé à modifier les téléchargements!';
+$_['error_name']        = 'Le nom du téléchargement doit contenir entre 3 et 64 caractères!';
+$_['error_upload']      = 'Transfert nécessaire!';
+$_['error_filename']    = 'Le nom de fichier doit contenir entre 3 et 128 caractères!';
+$_['error_exists']      = 'Fichier non existant!';
+$_['error_mask']        = 'La masque doit contenir entre 3 et 128 caractères!';
+$_['error_filetype']    = 'Type de fichier non valide!';
+$_['error_product']     = 'Avertissement: il est impossible d\'effacer ce téléchargement car il est actuellement alloué à %s produits!';
