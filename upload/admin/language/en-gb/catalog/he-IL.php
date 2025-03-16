789a33a32698f6fdc1d255849148927b935ebc4b
OSWorX <info@osworx.net>
2025-03-16 11:09:25 +0100

    New translations information.php (Hebrew)

--- upload/admin/language/en-gb/catalog/he-IL.php
+++ upload/admin/language/en-gb/catalog/he-IL.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'פילטרים';
+$_['heading_title']          = 'מידע';
 
 // Text
-$_['text_success']      = 'הצלחה: שינית מסננים!';
-$_['text_list']         = 'רשימת המסננים';
-$_['text_add']          = 'הוסף מסנן';
-$_['text_edit']         = 'ערוך מסנן';
+$_['text_success']           = 'הצלחה: שינית מידע!';
+$_['text_list']              = 'רשימת מידע';
+$_['text_add']               = 'הוסף מידע';
+$_['text_edit']              = 'ערוך מידע';
+$_['text_default']           = 'ברירת המחדל';
 
 // Column
-$_['column_group']      = 'קבוצת סינון';
-$_['column_sort_order'] = 'סדר מיון';
-$_['column_action']     = 'פעולה';
+$_['column_title']           = 'כותרת מידע';
+$_['column_sort_order']	     = 'סדר מיון';
+$_['column_action']          = 'פעולה';
 
 // Entry
-$_['entry_group']       = 'שם קבוצת סינון';
-$_['entry_name']        = 'שם מסנן';
-$_['entry_sort_order']  = 'סדר מיון';
+$_['entry_title']            = 'כותרת מידע';
+$_['entry_description']      = 'תיאור';
+$_['entry_store']            = 'חנויות';
+$_['entry_meta_title'] 	     = 'מטא -תג כותרת';
+$_['entry_meta_keyword'] 	 = 'מטא -תג מילת מפתח';
+$_['entry_meta_description'] = 'מטא -תג תיאור';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'תחתית';
+$_['entry_status']           = 'סטטוס';
+$_['entry_sort_order']       = 'סדר מיון';
+$_['entry_layout']           = 'פריסת ביטול';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'להציג בפוטר התחתון.';
 
 // Error
-$_['error_warning']     = 'אזהרה: אנא בדוק בקפידה אם יש שגיאות בטופס!';
-$_['error_permission']  = 'אזהרה: אין לך הרשאה לשינוי מסננים!';
-$_['error_group']       = 'שם קבוצת סינון חייב להיות בין 1 ל- 64 תווים!';
-$_['error_name']        = 'שם מסנן חייב להיות בין 1 ל- 64 תווים!';
+$_['error_warning']          = 'אזהרה: אנא בדוק בקפידה אם יש שגיאות בטופס!';
+$_['error_permission']       = 'אזהרה: אין לך הרשאה לשנות מידע!';
+$_['error_title']            = 'כותרת מידע חייב להיות בין 3 ל 64 תווים!';
+$_['error_description']      = 'תיאור חייב להיות יותר מ 3 תווים!';
+$_['error_meta_title']       = 'מטה-תג הכותרת חייב להיות גדול מ- 3 לפחות מ- 255 תווים!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'אזהרה: אין אפשרות למחוק את דף המידע הזה מאחר והוא מוקצה כרגע כתנאים בחשבון החנות!';
+$_['error_checkout']         = 'אזהרה: אין אפשרות למחוק את דף המידע הזה מאחר והוא מוקצה כרגע כתנאים בקופת החנות!';
+$_['error_affiliate']        = 'אזהרה: אין אפשרות למחוק את דף המידע הזה מאחר והוא מוקצה כרגע כתנאים בסוכנים של החנות!';
+$_['error_return']           = 'אזהרה: אין אפשרות למחוק את דף המידע הזה מאחר והוא מוקצה כרגע כתנאים בהחזרות החנות!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
