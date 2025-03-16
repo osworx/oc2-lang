861e6d07cedfd034adabac46df891835bc182be1
OSWorX <info@osworx.net>
2025-03-16 11:09:43 +0100

    New translations information.php (Turkish)

--- upload/admin/language/en-gb/catalog/tr-TR.php
+++ upload/admin/language/en-gb/catalog/tr-TR.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'Filtreler';
+$_['heading_title']          = 'Bilgilendirme';
 
 // Text
-$_['text_success']      = 'BAŞARILI: Filtreleri düzenlediniz !';
-$_['text_list']         = 'Filtre Listesi';
-$_['text_add']          = 'Filtre Ekle';
-$_['text_edit']         = 'Filtreyi Değiştir';
+$_['text_success']           = 'BAŞARILI: Bilgilendirmeyi değiştirdiniz!';
+$_['text_list']              = 'Bilgilendirme Listesi';
+$_['text_add']               = 'Bilgilendirme Ekle';
+$_['text_edit']              = 'Bilgilendirmeyi Değiştir';
+$_['text_default']           = 'Varsayılan';
 
 // Column
-$_['column_group']      = 'Filtre Grubu';
-$_['column_sort_order'] = 'Sıralama Düzeni';
-$_['column_action']     = 'Eylem';
+$_['column_title']           = 'Bilgilendirme Başlığı';
+$_['column_sort_order']	     = 'Sıralama Düzeni';
+$_['column_action']          = 'Eylem';
 
 // Entry
-$_['entry_group']       = 'Filtre Grup Adı';
-$_['entry_name']        = 'Filtre Adı';
-$_['entry_sort_order']  = 'Sıralama Düzeni';
+$_['entry_title']            = 'Bilgilendirme Başlığı';
+$_['entry_description']      = 'Açıklama';
+$_['entry_store']            = 'Dükkanlar';
+$_['entry_meta_title'] 	     = 'Meta etiketi başlığı';
+$_['entry_meta_keyword'] 	 = 'Meta etiketi anahtar kelimeler';
+$_['entry_meta_description'] = 'Meta etiketi açıklamasına';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Alt';
+$_['entry_status']           = 'Durum';
+$_['entry_sort_order']       = 'Sıralama Düzeni';
+$_['entry_layout']           = 'Düzeni Geçersiz Kılma';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Alt kısımda göster.';
 
 // Error
-$_['error_warning']     = 'Uyarı: Lütfen formu gösterilen hatalar nedeniyle dikkatlice kontrol edin!';
-$_['error_permission']  = 'UYARI: Filtreleri değiştirme yetkiniz yok!';
-$_['error_group']       = 'Filtre Grup Adı 1 ile 64 karakter arasında olmalıdır!';
-$_['error_name']        = 'Filtre Adı 1 ile 64 karakter arasında olmalıdır!';
+$_['error_warning']          = 'Uyarı: Lütfen formu gösterilen hatalar nedeniyle dikkatlice kontrol edin!';
+$_['error_permission']       = 'Uyarı. Bilgilendirmeyi değiştirme yetkiniz yok!';
+$_['error_title']            = 'Bilgilendirme Başlığı 3 ile 64 karakter arasında olmalıdır!';
+$_['error_description']      = 'Tanım en az 4 karakter olmalıdır!';
+$_['error_meta_title']       = 'Meta Başlık en az 3 ve en fazla 255 karakter olmalıdır!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'Uyarı: Şu an için mağaza hesap terimlerine bağlı olduğu sürece bu bilgilendirme sayfası silinemez!';
+$_['error_checkout']         = 'Uyarı: Şu an için mağaza ödeme terimlerine bağlı olduğu sürece bu bilgilendirme sayfası silinemez!';
+$_['error_affiliate']        = 'Uyarı: Şu an için mağaza link değiştirme terimlerine bağlı olduğu sürece bu bilgilendirme sayfası silinemez!';
+$_['error_return']           = 'Uyarı: Şu an için mağaza iade terimlerine bağlı olduğu sürece bu bilgilendirme sayfası silinemez!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
