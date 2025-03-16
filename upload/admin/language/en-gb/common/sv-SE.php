bb561fe4873710d0ea85403d1a5ef361b974356a
OSWorX <info@osworx.net>
2025-03-16 11:17:48 +0100

    New translations footer.php (Swedish)

--- upload/admin/language/en-gb/common/sv-SE.php
+++ upload/admin/language/en-gb/common/sv-SE.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Bildhanterare';
-
 // Text
-$_['text_uploaded']    = 'Klart: Din fil är uppladdad!';
-$_['text_directory']   = 'Klart: Katalog skapad!';
-$_['text_delete']      = 'Klart: Din fil eller mapp är raderad!';
-
-// Entry
-$_['entry_search']     = 'Sök..';
-$_['entry_folder']     = 'Ny mapp';
-
-// Error
-$_['error_permission'] = 'Varning: Behörighet saknas!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Varning: En fil eller mapp med samma namn finns redan!';
-$_['error_directory']  = 'Varning: Välj en mapp!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Varning: Felaktig filtyp!';
-$_['error_upload']     = 'Varning: Filen kunde inte överföras av okänd anledning!';
-$_['error_delete']     = 'Varning: Du kan inte ta bort denna katalog!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Version %s';
