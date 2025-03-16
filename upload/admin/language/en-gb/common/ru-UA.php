e614e7b2783cbd2a944ae38cdf202db8ee6a55cf
OSWorX <info@osworx.net>
2025-03-16 14:30:41 +0100

    New translations login.php (Rսssian, Սkraine)

--- upload/admin/language/en-gb/common/ru-UA.php
+++ upload/admin/language/en-gb/common/ru-UA.php
@@ -1,23 +1,19 @@
 <?php
-// Heading
-$_['heading_title']          = 'OpenCart';
+// header
+$_['heading_title']  = 'Администрирование';
 
 // Text
-$_['text_order']             = 'Заказы';
-$_['text_processing_status'] = 'Processing';
-$_['text_complete_status']   = 'Выполненно';
-$_['text_customer']          = 'Клиенты';
-$_['text_online']            = 'Покупатели онлайн';
-$_['text_approval']          = 'Ожидает подтверждения';
-$_['text_product']           = 'Товары';
-$_['text_stock']             = 'Нет в наличии';
-$_['text_review']            = 'Отзывы';
-$_['text_return']            = 'Возвраты';
-$_['text_affiliate']         = 'Партнерская программа';
-$_['text_store']             = 'Магазины';
-$_['text_front']             = 'Витрина';
-$_['text_help']              = 'Помощь';
-$_['text_homepage']          = 'OpenCart Homepage';
-$_['text_support']           = 'Форум поддержки';
-$_['text_documentation']     = 'Документация';
-$_['text_logout']            = 'Выход';
+$_['text_heading']   = 'Администрирование';
+$_['text_login']     = 'Пожалуйста, введите ваши регистрационные данные.';
+$_['text_forgotten'] = 'Забыл пароль';
+
+// Entry
+$_['entry_username'] = 'Имя Пользователя';
+$_['entry_password'] = 'Пароль';
+
+// Button
+$_['button_login']   = 'Войти';
+
+// Error
+$_['error_login']    = 'Введён неверный пароль или имя пользователя!';
+$_['error_token']    = 'Время сеанса вышло. Пожалуйста, войдите снова.';
