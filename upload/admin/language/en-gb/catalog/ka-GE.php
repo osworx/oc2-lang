620de0acfa4492314aaad976f2f863e943199fe8
OSWorX <info@osworx.net>
2025-03-16 14:17:38 +0100

    New translations category.php (Georgian)

--- upload/admin/language/en-gb/catalog/ka-GE.php
+++ upload/admin/language/en-gb/catalog/ka-GE.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'ატრიბუტის ჯგუფი';
+$_['heading_title']          = 'კატეგორიები';
 
 // Text
-$_['text_success']      = 'წარმატება: თქვენ  ატრიბუტების ჯგუფები შეცვლილია!';
-$_['text_list']         = 'ატრიბუტების ჯგუფების ჩამონათვალი';
-$_['text_add']          = 'ატრიბუტების ჯგუფების დამატება';
-$_['text_edit']         = 'ატრიბუტების ჯგუფების რედაქტირება';
+$_['text_success']           = 'წარმატება: თქვენ შეცვალეთ კატეგორიები!';
+$_['text_list']              = 'კატეგორიების სია';
+$_['text_add']               = 'კატეგორიის დამატება';
+$_['text_edit']              = 'კატეგორიის შეცვლა';
+$_['text_default']           = 'სტანდარტი';
 
 // Column
-$_['column_name']       = 'ატრიბუტების ჯგუფების დასახელება';
-$_['column_sort_order'] = 'შეკვეთის სორტირება';
-$_['column_action']     = 'მოქმედება';
+$_['column_name']            = 'კატეგორიის დასახელება';
+$_['column_sort_order']      = 'შეკვეთის სორტირება';
+$_['column_action']          = 'მოქმედება';
 
 // Entry
-$_['entry_name']        = 'ატრიბუტების ჯგუფების დასახელება';
-$_['entry_sort_order']  = 'შეკვეთის სორტირება';
+$_['entry_name']             = 'კატეგორიის დასახელება';
+$_['entry_description']      = 'აღწერა';
+$_['entry_meta_title'] 	     = 'მეტა ტეგი სათაური';
+$_['entry_meta_keyword']     = 'მეტა ტეგი საკვანძო სიტყვები';
+$_['entry_meta_description'] = 'მეტა ტეგი აღწერა';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'მშობელი';
+$_['entry_filter']           = 'ფილტრები';
+$_['entry_store']            = 'მაღაზიები';
+$_['entry_image']            = 'სურათი';
+$_['entry_top']              = 'ტოპი';
+$_['entry_column']           = 'სვეტები';
+$_['entry_sort_order']       = 'შეკვეთის სორტირება';
+$_['entry_status']           = 'სტატუსი';
+$_['entry_layout']           = 'მაკეტის გარდაქმნა';
+
+// Help
+$_['help_filter']            = 'ავტოშესრულება';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'ჩვენების ზედა მენიუში ბარი. მუშაობს მხოლოდ ზედა მშობელი კატეგორიებისათვის.';
+$_['help_column']            = 'სვეტების რაოდენობის გამოიყენება მხოლოდ ქვედა 3 კატეგორიაში. მუშაობს მხოლოდ ზედა მშობელი კატეგორიებისათვის.';
 
 // Error
-$_['error_warning']     = 'გაფრთხილება: გთხოვთ ყურადღებით შეამოწმოთ შეცდომების ფორმები!';
-$_['error_permission']  = 'გაფრთხილება: თქვენ არ გაქვთ უფლება შეცვალოთ ატრიბუტები!';
-$_['error_name']        = 'ატრიბუტი ჯგუფის სახელი უნდა იყოს 3-დან 64 სიმბოლო!';
-$_['error_attribute']   = 'გაფრთხილება: ეს ატრიბუტი ჯგუფი არ შეიძლება წაიშალოს რადგან მას ამჟამად ენიჭება%s  ატრიბუტები!';
-$_['error_product']     = 'შენიშვნა: ამ ატრიბუტის წაშლა შეუძლებელია რადგან ეკუთვნის %s პროდუქტს!';
+$_['error_warning']          = 'გაფრთხილება: გთხოვთ ყურადღებით შეამოწმოთ შეცდომების ფორმები!';
+$_['error_permission']       = 'გაფრთხილება: თქვენ არ გაქვთ უფლება შეცვალოთ კატეგორიები!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'მეტა სათაური უნდა იყოს მეტი 3 და ნაკლები 255 სიმბოლო!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
