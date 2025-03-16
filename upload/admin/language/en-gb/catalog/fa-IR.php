9e4110bc426f2374a13144873ecd5a4fb92098b6
OSWorX <info@osworx.net>
2025-03-16 11:13:16 +0100

    New translations recurring.php (Persian)

--- upload/admin/language/en-gb/catalog/fa-IR.php
+++ upload/admin/language/en-gb/catalog/fa-IR.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'کالاها';
+$_['heading_title']			= 'نمایه‌های تکراری';
 
 // Text
-$_['text_success']           = 'موفقیت: شما کالاها را تغییر داده‌اید!';
-$_['text_list']              = 'فهرست کالاها';
-$_['text_add']               = 'افزودن کالا';
-$_['text_edit']              = 'ویرایش کالا';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'پیش‎فرض';
-$_['text_option']            = 'گزینه';
-$_['text_option_value']      = 'ارزش گزینه';
-$_['text_percent']           = 'درصد';
-$_['text_amount']            = 'مقدار ثابت';
-
-// Column
-$_['column_name']            = 'نام کالا';
-$_['column_model']           = 'مدل';
-$_['column_image']           = 'تصویر';
-$_['column_price']           = 'بها';
-$_['column_quantity']        = 'مقدار';
-$_['column_status']          = 'وضعيت';
-$_['column_action']          = 'کنش';
+$_['text_success']          = 'موفقیت: شما نمایه‌های تکراری را تغییر داده‌اید!';
+$_['text_list']             = 'فهرست نمایه تکراری';
+$_['text_add']              = 'افزودن نمایه تکراری';
+$_['text_edit']             = 'ویرایش نمایه تکراری';
+$_['text_day']				= 'روز';
+$_['text_week']				= 'هفته';
+$_['text_semi_month']		= 'نیمه ماه';
+$_['text_month']			= 'ماه';
+$_['text_year']				= 'سال';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> مقدارهای تکراری توسط تناوب و دوره محاسبه می‌شوند.</p><p> برای مثال اگر شما تناوب «هفته» و دوره «۲» استفاده نمایید، کاربر هر دو هفته یک بار صورت‌حساب دریافت خواهد کرد.</p><p>مدت زمان تعداد زمان‌هایی است که کاربر باید پرداخت نماید، صفر را تنظیم نمایید اگر شما می‌خواهید پرداخت‌ها تا زمانی آنها لغو می‌نمایند ادامه یابد.</p>';
+$_['text_profile']			= 'نمایه تکراری';
+$_['text_trial']			= 'نمایه آزمایشی';
 
 // Entry
-$_['entry_name']             = 'نام کالا';
-$_['entry_description']      = 'توضیحات';
-$_['entry_meta_title'] 	     = 'عنوان متا تگ';
-$_['entry_meta_keyword'] 	 = 'کلیدواژه‌های متا تگ';
-$_['entry_meta_description'] = 'توضیحات متا تگ';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'مدل';
-$_['entry_sku']              = 'SKU';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'شابک';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'مكان';
-$_['entry_shipping']         = 'نیازمند ارسال';
-$_['entry_manufacturer']     = 'تولیدکننده';
-$_['entry_store']            = 'فروشگاه‌ها';
-$_['entry_date_available']   = 'تاریخ موجود بودن';
-$_['entry_quantity']         = 'مقدار';
-$_['entry_minimum']          = 'کمینه مقدار';
-$_['entry_stock_status']     = 'وضعیت عدم موجودی';
-$_['entry_price']            = 'بها';
-$_['entry_tax_class']        = 'کلاس مالیاتی';
-$_['entry_points']           = 'امتیازها';
-$_['entry_option_points']    = 'امتیازها';
-$_['entry_subtract']         = 'کم کردن از موجودی';
-$_['entry_weight_class']     = 'کلاس وزن';
-$_['entry_weight']           = 'وزن';
-$_['entry_dimension']        = 'ابعاد (L x W x H)';
-$_['entry_length_class']     = 'کلاس طول';
-$_['entry_length']           = 'طول';
-$_['entry_width']            = 'عرض';
-$_['entry_height']           = 'ارتفاع';
-$_['entry_image']            = 'تصویر';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'گروه مشتری';
-$_['entry_date_start']       = 'تاریخ شروع';
-$_['entry_date_end']         = 'تاریخ پایان';
-$_['entry_priority']         = 'اولویت';
-$_['entry_attribute']        = 'ویژگی';
-$_['entry_attribute_group']  = 'گروه ویژگی';
-$_['entry_text']             = 'متن';
-$_['entry_option']           = 'گزینه';
-$_['entry_option_value']     = 'ارزش گزینه';
-$_['entry_required']         = 'الزامی';
-$_['entry_status']           = 'وضعيت';
-$_['entry_sort_order']       = 'ترتیب';
-$_['entry_category']         = 'دسته‌بندی‌ها';
-$_['entry_filter']           = 'صافی‌ها';
-$_['entry_download']         = 'بارگیری‌ها';
-$_['entry_related']          = 'کالاهای مرتبط';
-$_['entry_tag']          	 = 'برچسب‌های کالا';
-$_['entry_reward']           = 'امتیازهای پاداش';
-$_['entry_layout']           = 'جایگزینی چیدمان';
-$_['entry_recurring']        = 'نمایه تکراری';
+$_['entry_name']		    = 'نام';
+$_['entry_price']			= 'بها';
+$_['entry_duration']		= 'مدت زمان';
+$_['entry_cycle']			= 'چرخه';
+$_['entry_frequency']		= 'تناوب';
+$_['entry_trial_price']		= 'بهای آزمایشی';
+$_['entry_trial_duration']	= 'مدت آزمایشی';
+$_['entry_trial_status']	= 'وضعیت آزمایشی';
+$_['entry_trial_cycle']	    = 'چرخه آزمایشی';
+$_['entry_trial_frequency'] = 'تناوب آزمایشی';
+$_['entry_status']			= 'وضعيت';
+$_['entry_sort_order']		= 'ترتیب';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'واحد نگهداری انبار';
-$_['help_upc']               = 'کد کالای جهانی';
-$_['help_ean']               = 'شماره مقاله اروپایی';
-$_['help_jan']               = 'شماره مقاله ژاپنی';
-$_['help_isbn']              = 'شماره استاندارد بین‌المللی کتاب';
-$_['help_mpn']               = 'شماره قطعه تولیدکننده';
-$_['help_manufacturer']      = '(کامل کردن خودکار)';
-$_['help_minimum']           = 'وادار کردن مقدار کمینه سفارش';
-$_['help_stock_status']      = 'وضعیت نمایش داده شده برای عدم موجودی یک محصول';
-$_['help_points']            = 'تعداد امتیازهای لازم برای خرید این مورد. اگر شما نمی‌خواهید این کالا با امتیاز فروخته شود صفر قرار دهید.';
-$_['help_category']          = '(کامل کردن خودکار)';
-$_['help_filter']            = '(کامل کردن خودکار)';
-$_['help_download']          = '(کامل کردن خودکار)';
-$_['help_related']           = '(کامل کردن خودکار)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'نام';
+$_['column_sort_order']	    = 'ترتیب';
+$_['column_action']         = 'کنش';
 
 // Error
-$_['error_warning']          = 'هشدار: لطفا فرم را به دقت برای خطاها بررسی نمایید!';
-$_['error_permission']       = 'هشدار: شما مجوز تغییر کالاها را ندارید!';
-$_['error_name']             = 'نام کالا باید بیشتر از ۳ و کمتر از ۲۵۵ نویسه باشد!';
-$_['error_meta_title']       = 'عنوان متا باید بزرگ‌تر از ۳ و کمتر از ۲۵۵ نویسه باشد!';
-$_['error_model']            = 'مدل کالا باید بیشتر از ۱ و کمتر از ۶۴ نویسه باشد!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'هشدار: لطفا فرم را به دقت برای خطاها بررسی نمایید!';
+$_['error_permission']		= 'هشدار: شما مجوز تغییر نمایه‌های تکراری را ندارید!';
+$_['error_name']			= 'نام نمایه باید بزرگ‌تر از ۳ و کمتر از ۲۵۵ نویسه باشد!';
+$_['error_product']			= 'هشدار: این نمایه تکراری از آن جا که هم اکنون برای %s کالا اختصاص یافته است نمی‌تواند حذف شود!';
