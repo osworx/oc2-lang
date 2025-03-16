388e77c40abde881e45ab91da33167adf8761f40
OSWorX <info@osworx.net>
2025-03-16 11:14:00 +0100

    New translations review.php (Vietnamese)

--- upload/admin/language/en-gb/catalog/vi-VN.php
+++ upload/admin/language/en-gb/catalog/vi-VN.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'Các hồ sơ định kỳ';
+$_['heading_title']     = 'Đánh giá';
 
 // Text
-$_['text_success']          = 'Kết quả: Bạn không có quyền sửa các hồ sơ định kỳ!';
-$_['text_list']             = 'Danh sách hồ sơ định kỳ';
-$_['text_add']              = 'Thêm hồ sơ định kỳ';
-$_['text_edit']             = 'Sửa hồ sơ định kỳ';
-$_['text_day']				= 'Ngày';
-$_['text_week']				= 'Tuần';
-$_['text_semi_month']		= 'Nửa tháng';
-$_['text_month']			= 'Tháng';
-$_['text_year']				= 'Năm';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Số lượng định kỳ được tính bởi tần suất và chu kỳ.</p><p>Ví dụ nếu bạn dùng tần suất là "tuần" và chu kỳ là "2", thì người dùng sẽ nhận hóa đơn mỗi 2 tuần.</p><p>Trong khoảng thời gian đó thì số lần người dùng trả tiền là một lần, gán cái này bằng 0 nếu bạn muốn thanh toán cho tới khi chúng được hủy.</p>';
-$_['text_profile']			= 'Hồ sơ định kỳ';
-$_['text_trial']			= 'Hồ sơ thử nghiệm';
-
-// Entry
-$_['entry_name']		    = 'Tên';
-$_['entry_price']			= 'Giá';
-$_['entry_duration']		= 'Khoảng thời gian';
-$_['entry_cycle']			= 'Chu kỳ';
-$_['entry_frequency']		= 'Tần suất';
-$_['entry_trial_price']		= 'Giá thử nghiệm';
-$_['entry_trial_duration']	= 'Thời gian thử nghiệm';
-$_['entry_trial_status']	= 'Trạng thái thử nghiệm';
-$_['entry_trial_cycle']	    = 'Chu kỳ thử nghiệm';
-$_['entry_trial_frequency'] = 'Tần suất thử nghiệm';
-$_['entry_status']			= 'Trạng thái';
-$_['entry_sort_order']		= 'Thứ tự sắp xếp';
+$_['text_success']      = 'Thành công: Bạn đã sửa đổi đánh giá!';
+$_['text_list']         = 'Danh sach đánh giá';
+$_['text_add']          = 'Thêm đánh giá';
+$_['text_edit']         = 'Sửa đánh giá';
 
 // Column
-$_['column_name']			= 'Tên';
-$_['column_sort_order']	    = 'Thứ tự sắp xếp';
-$_['column_action']         = 'Tác vụ';
+$_['column_product']    = 'Sản phẩm';
+$_['column_author']     = 'Tác giả';
+$_['column_rating']     = 'Xếp hạng';
+$_['column_status']     = 'Trạng thái';
+$_['column_date_added'] = 'Ngày thêm vào';
+$_['column_action']     = 'Tác vụ';
+
+// Entry
+$_['entry_product']     = 'Sản phẩm';
+$_['entry_author']      = 'Tác giả';
+$_['entry_rating']      = 'Xếp hạng';
+$_['entry_status']      = 'Trạng thái';
+$_['entry_text']        = 'Mô tả';
+$_['entry_date_added']  = 'Ngày thêm vào';
+
+// Help
+$_['help_product']      = '(Tự động gợi ý)';
 
 // Error
-$_['error_warning']         = 'Chú ý: Vui lòng kiểm tra lỗi cẩn thận trong biểu mẫu!';
-$_['error_permission']		= 'Chú ý: Bạn không có quyền sửa các hồ sơ định kỳ!';
-$_['error_name']			= 'Tên hồ sơ phải lớn hơn 3 và ít hơn 255 ký tự!';
-$_['error_product']			= 'Chú ý: Hồ sơ định kỳ này không thể xóa vì nó đang được gán cho các sản phẩm  %s !';
+$_['error_warning']     = 'Chú ý: Vui lòng kiểm tra lỗi cẩn thận trong biểu mẫu!';
+$_['error_permission']  = 'Chú ý: Bạn không có quyền sửa đổi đánh giá!';
+$_['error_product']     = 'Buộc phải nhập sản phẩm!';
+$_['error_author']      = 'Tác giả phải từ 3 đến 64 ký tự!';
+$_['error_text']        = 'Nội dung đánh giá phải có ít nhất 1 ký tự!';
+$_['error_rating']      = 'Buộc phải bầu chọn!';
