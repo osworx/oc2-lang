66a03c5f3ffb20c3aa0e3a9d7d1337831d1bad01
OSWorX <info@osworx.net>
2025-03-16 14:19:45 +0100

    New translations filter.php (Serbian (Latin))

--- upload/admin/language/en-gb/catalog/sr-CS.php
+++ upload/admin/language/en-gb/catalog/sr-CS.php
@@ -1,35 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = 'Preuzimanje';
+$_['heading_title']     = 'Filteri';
 
 // Text
-$_['text_success']      = 'Uspeh: Vi ste uspešno izmenili preuzimanja!';
-$_['text_list']         = 'Lista Preuzimanja';
-$_['text_add']          = 'Dodaj Preuzimanje';
-$_['text_edit']         = 'Izmeni Preuzimanje';
-$_['text_upload']       = 'Vaša datoteka je uspešno dodana!';
+$_['text_success']      = 'Uspeh: Izmenili ste filtere!';
+$_['text_list']         = 'Lista Filtera';
+$_['text_add']          = 'Dodaj Filter';
+$_['text_edit']         = 'Izmeni Filter';
 
 // Column
-$_['column_name']       = 'Ime Skidanja';
-$_['column_date_added'] = 'Datum Dodavanja';
+$_['column_group']      = 'Grupa Filtera';
+$_['column_sort_order'] = 'Sortiraj po';
 $_['column_action']     = 'Akcija';
 
 // Entry
-$_['entry_name']        = 'Ime Skidanja';
-$_['entry_filename']    = 'Ime datoteke';
-$_['entry_mask']        = 'Maska';
-
-// Help
-$_['help_filename']     = 'Možete dodati preko dugmeta za dodavanje ili koristiti FTP da dodate direktorijum za preuzimanje i unesete detalje ispod.';
-$_['help_mask']         = 'Preporučljivo je da naziv datoteke i maska budu razlicitog imena da bi sprečili ljude da povezuju njihova preuzimanja.';
+$_['entry_group']       = 'Ime Grupe Filtera';
+$_['entry_name']        = 'Ime Filtera';
+$_['entry_sort_order']  = 'Sortiraj po';
 
 // Error
 $_['error_warning']     = 'Upozorenje: Molimo proverite formu zbog grešaka!';
-$_['error_permission']  = 'Upozorenje: Nemate dozvolu da izmenite preuzimanja!';
-$_['error_name']        = 'Ime Preuzimanja mora biti između 3 i 64 karaktera!';
-$_['error_upload']      = 'Dodavanje potrebno!';
-$_['error_filename']    = 'Ime datoteke mora biti između 3 i 64 karaktera!';
-$_['error_exists']      = 'Datoteka ne postoji!';
-$_['error_mask']        = 'Maska mora imaiti između 2 i 128 znakova!';
-$_['error_filetype']    = 'Nepodržana datoteka!';
-$_['error_product']     = 'Upozorenje: Ova preuzimanja ne mogu biti obrisana jer su trenutno dodeljena %s proizvodima!';
+$_['error_permission']  = 'Upozorenje: Vi nemate dozvolu da biste izmenili filtere!';
+$_['error_group']       = 'Ime Grupe Filtera mora biti izmedju 1 i 64 karaktera!';
+$_['error_name']        = 'Ime Filtera mora biti između 1 i 64 karaktera!';
