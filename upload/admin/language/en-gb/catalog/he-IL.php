133f65ccf6e23d34c47dd9106773a034148f4aff
OSWorX <info@osworx.net>
2025-03-16 11:07:36 +0100

    New translations download.php (Hebrew)

--- upload/admin/language/en-gb/catalog/he-IL.php
+++ upload/admin/language/en-gb/catalog/he-IL.php
@@ -1,46 +1,37 @@
 <?php
 // Heading
-$_['heading_title']          = 'קטגוריות';
+$_['heading_title']     = 'הורדות';
 
 // Text
-$_['text_success']           = 'הצלחה: שינית קטגוריות!';
-$_['text_list']              = 'רשימת הקטגוריות';
-$_['text_add']               = 'הוסף קטגוריה';
-$_['text_edit']              = 'ערוך קטגוריה';
-$_['text_default']           = 'ברירת המחדל';
+$_['text_success']      = 'הצלחה!
+שינית הורדות.';
+$_['text_list']         = 'רשימת הורדות';
+$_['text_add']          = 'הוסף הורדה';
+$_['text_edit']         = 'ערוך הורדה';
+$_['text_upload']       = 'הקובץ שלך הועלה בהצלחה!';
 
 // Column
-$_['column_name']            = 'שם קטגוריה';
-$_['column_sort_order']      = 'סדר מיון';
-$_['column_action']          = 'פעולה';
+$_['column_name']       = 'שם ההורדה';
+$_['column_date_added'] = 'תאריך שנוסף';
+$_['column_action']     = 'פעולה';
 
 // Entry
-$_['entry_name']             = 'שם קטגוריה';
-$_['entry_description']      = 'תיאור';
-$_['entry_meta_title'] 	     = 'מטא -תג כותרת';
-$_['entry_meta_keyword']     = 'מטא -תג מילת מפתח';
-$_['entry_meta_description'] = 'מטא -תג תיאור';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = 'אב';
-$_['entry_filter']           = 'פילטרים';
-$_['entry_store']            = 'חנויות';
-$_['entry_image']            = 'תמונה';
-$_['entry_top']              = 'עליון';
-$_['entry_column']           = 'עמודות';
-$_['entry_sort_order']       = 'סדר מיון';
-$_['entry_status']           = 'סטטוס';
-$_['entry_layout']           = 'פריסת ביטול';
+$_['entry_name']        = 'שם ההורדה';
+$_['entry_filename']    = 'שם הקובץ';
+$_['entry_mask']        = 'מסכה';
 
 // Help
-$_['help_filter']            = '(השלמה אוטומטית)';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = 'הצג בבר התפריט העליון. פועל רק עבור קטגוריות האב.';
-$_['help_column']            = 'מספר העמודות שישמשו עבור 3 כפתורי הקטגוריות בתחתית. פועל רק עבור קטגוריות האב.';
+$_['help_filename']     = 'אתה יכול להעלות ע"י לחיצה על כפתור ההעלאה, או להשתמש בFTP על מנת להעלות תיקייה שלמה ולהכניס את הפרטים למטה.';
+$_['help_mask']         = 'מומלץ ששם הקובץ והמסכה יהיו שונים כדי למנוע מאנשים לבצע הורדות באמצעות קישור ישיר להורדות שלך.';
 
 // Error
-$_['error_warning']          = 'אזהרה: אנא בדוק בקפידה אם יש שגיאות בטופס!';
-$_['error_permission']       = 'אזהרה: אין לך הרשאה לשינוי קטגוריות!';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = 'מטה-תג הכותרת חייב להיות גדול מ- 3 לפחות מ- 255 תווים!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = 'אזהרה: אנא בדוק בקפידה אם יש שגיאות בטופס!';
+$_['error_permission']  = 'אזהרה!
+אין לך הרשאות על מנת לשנות הורדות.';
+$_['error_name']        = 'שם ההורדה חייב להיות בין 3 ל-64 תווים!';
+$_['error_upload']      = 'דרושה העלאה!';
+$_['error_filename']    = 'שם הקובץ חייב להיות בין 3 ל- 128 תווים!';
+$_['error_exists']      = 'הקובץ אינו קיים!';
+$_['error_mask']        = 'מסכה חייב להיות בין 3 עד 128 תווים!';
+$_['error_filetype']    = 'סוג קובץ לא נתמך!';
+$_['error_product']     = 'אזהרה: לא ניתן למחוק את ההורדה הזו מאחר והיא מוקצת כרגע ל %s מוצרים!';
