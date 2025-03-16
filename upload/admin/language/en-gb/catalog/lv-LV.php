59868e0283552d43b4e2668211136299f00c542c
OSWorX <info@osworx.net>
2025-03-16 11:05:29 +0100

    New translations attribute.php (Latvian)

---
+++ upload/admin/language/en-gb/catalog/lv-LV.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Atribūti';
+
+// Text
+$_['text_success']           = 'Veiksmīgi: Ir modificēti atribūti!';
+$_['text_list']              = 'Atribūtu saraksts';
+$_['text_add']               = 'Pievienot atribūtu';
+$_['text_edit']              = 'Labot atribūtu';
+
+// Column
+$_['column_name']            = 'Atribūta nosaukums';
+$_['column_attribute_group'] = 'Atribūta Grupa';
+$_['column_sort_order']      = 'Kārtošanas secība';
+$_['column_action']          = 'Darbība';
+
+// Entry
+$_['entry_name']             = 'Atribūta nosaukums';
+$_['entry_attribute_group']  = 'Atribūta Grupa';
+$_['entry_sort_order']       = 'Kārtošanas secība';
+
+// Error
+$_['error_warning']          = 'Brīdinājums: Lūdzu pārbaudiet formu rūpīgi ,ir dažas kļūdas!';
+$_['error_permission']       = 'Brīdinājums: Jums nav tiesību labot atribūtus!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Atribūta nosaukumam jābūt no 3 līdz 64 rakstzīmēm!';
+$_['error_product']          = 'Brīdinājums: Šo atribūtu nevar dzēst, jo tas pašlaik ir piešķirts %s precēm!';
