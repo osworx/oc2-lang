92ec347dc101de4ffc3140f9fbe826f1a9f2d317
OSWorX <info@osworx.net>
2025-03-16 11:16:34 +0100

    New translations filemanager.php (Hebrew)

--- upload/admin/language/en-gb/common/he-IL.php
+++ upload/admin/language/en-gb/common/he-IL.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'לוח בקרה';
+$_['heading_title']    = 'מנהל תמונות';
+
+// Text
+$_['text_uploaded']    = 'הצלחה: הקובץ עלה!';
+$_['text_directory']   = 'הצלחה: נוצרה תיקייה!';
+$_['text_delete']      = 'הצלחה: הקובץ או התיקייה שלך נמחקו!';
+
+// Entry
+$_['entry_search']     = 'חיפוש..';
+$_['entry_folder']     = 'שם תיקייה';
 
 // Error
-$_['error_install'] = 'אזהרה: תיקיית ההתקנה קיימת, יש למחוק אותה מטעמי אבטחה!';
+$_['error_permission'] = 'אזהרה: הרשאה נדחתה!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'אזהרה: שם הקובץ או שם התיקייה כבר קיימים!';
+$_['error_directory']  = 'אזהרה: תיקייה לא קיימת!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'אזהרה: סוג קובץ שגוי!';
+$_['error_upload']     = 'אזהרה: קובץ לא היתה אפשרות לטעון מסיבה שאיננה ידועה!';
+$_['error_delete']     = 'אזהרה: לא יכול למחוק תיקייה זו!';
