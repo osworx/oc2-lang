358cc145e7672d86969ae94018e1d00af94b1d3a
OSWorX <info@osworx.net>
2025-03-16 14:30:08 +0100

    New translations login.php (Japanese)

--- upload/admin/language/en-gb/common/ja-JP.php
+++ upload/admin/language/en-gb/common/ja-JP.php
@@ -1,23 +1,19 @@
 <?php
-// Heading
-$_['heading_title']          = 'OpenCart';
+// header
+$_['heading_title']  = 'Administration';
 
 // Text
-$_['text_order']             = 'Orders';
-$_['text_processing_status'] = 'Processing';
-$_['text_complete_status']   = 'Completed';
-$_['text_customer']          = 'Customers';
-$_['text_online']            = 'Customers Online';
-$_['text_approval']          = 'Pending approval';
-$_['text_product']           = 'Products';
-$_['text_stock']             = 'Out of stock';
-$_['text_review']            = 'Reviews';
-$_['text_return']            = 'Returns';
-$_['text_affiliate']         = 'Affiliates';
-$_['text_store']             = 'ストア';
-$_['text_front']             = 'Store Front';
-$_['text_help']              = 'Help';
-$_['text_homepage']          = 'OpenCart Homepage';
-$_['text_support']           = 'Support Forum';
-$_['text_documentation']     = 'Documentation';
-$_['text_logout']            = 'Logout';
+$_['text_heading']   = 'Administration';
+$_['text_login']     = 'Please enter your login details.';
+$_['text_forgotten'] = 'Forgotten Password';
+
+// Entry
+$_['entry_username'] = 'Username';
+$_['entry_password'] = 'Password';
+
+// Button
+$_['button_login']   = 'Login';
+
+// Error
+$_['error_login']    = 'No match for Username and/or Password.';
+$_['error_token']    = 'Invalid token session. Please login again.';
