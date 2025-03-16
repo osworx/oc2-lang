7161c62eda45d91a53976c28979277a28261a5ea
OSWorX <info@osworx.net>
2025-03-16 14:28:15 +0100

    New translations footer.php (Bosnian)

--- upload/admin/language/en-gb/common/bs-BA.php
+++ upload/admin/language/en-gb/common/bs-BA.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Mwnadžer sigurnosne kopije slike';
-
 // Text
-$_['text_uploaded']    = 'Uspjeh: Vaš fajl je prenesen!';
-$_['text_directory']   = 'Uspjeh: Mapa je napravljena!';
-$_['text_delete']      = 'Uspjeh: Vaš fajl ili mapa je obrisan!';
-
-// Entry
-$_['entry_search']     = 'Pretraga..';
-$_['entry_folder']     = 'Naziv mape';
-
-// Error
-$_['error_permission'] = 'Upozorenje: Dopuštenje odbijeno!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Upozorenje: Fajl ili mapa s tim nazivom već postoji!';
-$_['error_directory']  = 'Upozorenje: Mapa ne postoji!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Upozorenje: Neisprava tip fajla!';
-$_['error_upload']     = 'Upozorenje: Fajl nije mogao biti prenešen iz nepoznatog razloga!';
-$_['error_delete']     = 'Upozorenje: Ne možete obrisati ovu mapu!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Verzija %s';
