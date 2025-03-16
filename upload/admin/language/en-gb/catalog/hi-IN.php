f59da0ef135e0938e1edce9bab44e3e914f36386
OSWorX <info@osworx.net>
2025-03-16 14:18:04 +0100

    New translations category.php (Hindi)

--- upload/admin/language/en-gb/catalog/hi-IN.php
+++ upload/admin/language/en-gb/catalog/hi-IN.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'विशेषता समूह';
+$_['heading_title']          = 'श्रेणियाँ';
 
 // Text
-$_['text_success']      = 'सफलता: आपने विशेषता समूहों को संशोधित किया है!';
-$_['text_list']         = 'विशेषता समूह सूची';
-$_['text_add']          = 'विशेषता समूह जोड़ें';
-$_['text_edit']         = 'विशेषता समूह संशोधित करें';
+$_['text_success']           = 'सफलता: आप श्रेणियों को संशोधित किया है!';
+$_['text_list']              = 'श्रेणी सूची';
+$_['text_add']               = 'श्रेणी जोड़ें';
+$_['text_edit']              = 'श्रेणी संपादित करें';
+$_['text_default']           = 'सामान्य';
 
 // Column
-$_['column_name']       = 'विशेषता समूह नाम';
-$_['column_sort_order'] = 'क्रम';
-$_['column_action']     = 'कार्यवाही';
+$_['column_name']            = 'श्रेणी नाम';
+$_['column_sort_order']      = 'क्रम';
+$_['column_action']          = 'कार्यवाही';
 
 // Entry
-$_['entry_name']        = 'विशेषता समूह नाम';
-$_['entry_sort_order']  = 'क्रम';
+$_['entry_name']             = 'श्रेणी नाम';
+$_['entry_description']      = 'विवरण';
+$_['entry_meta_title'] 	     = 'मेटा टैग शीर्षक';
+$_['entry_meta_keyword']     = 'मेटा टैग खोजशब्द';
+$_['entry_meta_description'] = 'मेटा टैग विवरण';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'पैरेंट';
+$_['entry_filter']           = 'छानना';
+$_['entry_store']            = 'भंडार';
+$_['entry_image']            = 'छवि';
+$_['entry_top']              = 'शीर्ष';
+$_['entry_column']           = 'स्तंभ';
+$_['entry_sort_order']       = 'क्रम';
+$_['entry_status']           = 'स्थिति';
+$_['entry_layout']           = 'लेआउट ओवरराइड';
+
+// Help
+$_['help_filter']            = '(स्वत: पूर्ण)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'शीर्ष मेनू पट्टी में प्रदर्शित करें। केवल शीर्ष पैरेंट श्रेणियों के लिए काम करता है।';
+$_['help_column']            = 'नीचे 3 श्रेणियों के लिए उपयोग करने के लिए स्तंभों की संख्या। केवल शीर्ष पैरेंट श्रेणियों के लिए काम करता है।';
 
 // Error
-$_['error_warning']     = 'चेतावनी: कृपया ध्यान से त्रुटियों के लिए प्रपत्र की जाँच करें!';
-$_['error_permission']  = 'चेतावनी: आप को विशेषता को संशोधित करने की अनुमति नहीं है!';
-$_['error_name']        = 'विशेषता समूह नाम 3 और 64 वर्णों के बीच होना चाहिए!';
-$_['error_attribute']   = 'चेतावनी: इस विशेषता समूह को हटाया नहीं जा सकता क्योंकि यह वर्तमान में %s विशेषताओं के लिए निश्चित किया गया है!';
-$_['error_product']     = 'चेतावनी: इस विशेषता समूह को हटाया नहीं जा सकता क्योंकि यह वर्तमान में %s उत्पादों के लिए निश्चित किया गया है!';
+$_['error_warning']          = 'चेतावनी: कृपया ध्यान से त्रुटियों के लिए प्रपत्र की जाँच करें!';
+$_['error_permission']       = 'चेतावनी: आप श्रेणियों को संशोधित करने की अनुमति नहीं है!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'मेटा शीर्षक कम से कम 3 और अधिक से अधिक 255  होना आवश्यक है!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
