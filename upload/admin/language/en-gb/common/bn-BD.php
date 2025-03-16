9b89e1c13e8052940cb0df57ba9b6e627635edb4
OSWorX <info@osworx.net>
2025-03-16 14:28:53 +0100

    New translations forgotten.php (Bengali)

--- upload/admin/language/en-gb/common/bn-BD.php
+++ upload/admin/language/en-gb/common/bn-BD.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'আপনার পাসওয়ার্ড ভুলে গেছেন?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'সংস্করণ %s ';
+$_['text_forgotten']  = 'পাসওয়ার্ড ভুলে গেছেন';
+$_['text_your_email'] = 'আপনার ই-মেইল ঠিকানা';
+$_['text_email']      = 'আপনার হিসাবের সাথে সংশ্লিষ্ট ই-মেইল ঠিকানা প্রবেশ করান। ক্লিক জমা করুন আপনার গুপ্ত-সংকেত আপনাকে ই-মেইল যোগসূত্র পুনঃবিন্যাস করুন।.';
+$_['text_success']    = 'Admin আপনার ই-মেইল ঠিকানা নিশ্চিতকরণ লিংক দিয়ে ই-মেইল প্রেরণ করা হয়েছে।.';
+
+// Entry
+$_['entry_email']     = 'ই-মেইল ঠিকানা';
+$_['entry_password']  = 'নতুন পাসওয়ার্ড';
+$_['entry_confirm']   = 'নিশ্চিত করুন';
+
+// Error
+$_['error_email']     = 'Warning: The E-Mail Address was not found in our records, please try again!';
+$_['error_password']  = 'Password must be between 4 and 20 characters!';
+$_['error_confirm']   = 'Password and password confirmation do not match!';
