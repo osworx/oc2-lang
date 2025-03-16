f82cc0884cb69f829db89a6cb9030610d62539d8
OSWorX <info@osworx.net>
2025-03-16 14:20:13 +0100

    New translations information.php (Swedish)

--- upload/admin/language/en-gb/catalog/sv-SE.php
+++ upload/admin/language/en-gb/catalog/sv-SE.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'Filter';
+$_['heading_title']          = 'Information';
 
 // Text
-$_['text_success']      = 'Klart: Du har ändrat filter!';
-$_['text_list']         = 'Filtrera listan';
-$_['text_add']          = 'Lägg till filter';
-$_['text_edit']         = 'Redigera filter';
+$_['text_success']           = 'Klart: Du har ändrat i "Information"!';
+$_['text_list']              = 'Informationslista';
+$_['text_add']               = 'Lägg till Information';
+$_['text_edit']              = 'Redigera information';
+$_['text_default']           = 'Standard';
 
 // Column
-$_['column_group']      = 'Filtergrupp';
-$_['column_sort_order'] = 'Sorteringsordning';
-$_['column_action']     = 'Åtgärd';
+$_['column_title']           = 'Rubrik';
+$_['column_sort_order']	     = 'Sorteringsordning';
+$_['column_action']          = 'Åtgärd';
 
 // Entry
-$_['entry_group']       = 'Namn på gruppfiltret';
-$_['entry_name']        = 'Filternamn';
-$_['entry_sort_order']  = 'Sorteringsordning';
+$_['entry_title']            = 'Rubrik';
+$_['entry_description']      = 'Beskrivning';
+$_['entry_store']            = 'Butiker';
+$_['entry_meta_title'] 	     = 'Metatagtitel';
+$_['entry_meta_keyword'] 	 = 'Metataggsökord';
+$_['entry_meta_description'] = 'Metataggbeskrivning';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Nedtill';
+$_['entry_status']           = 'Status';
+$_['entry_sort_order']       = 'Sorteringsordning';
+$_['entry_layout']           = 'Layoutomställning';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Visas i sidfoten längst ner.';
 
 // Error
-$_['error_warning']     = 'Varning: Kolla gärna formuläret noga vad det gäller fel!';
-$_['error_permission']  = 'Varning: Du har inte behörighet att ändra filter!';
-$_['error_group']       = 'Filtergruppens namn måste vara mellan 1 och 64 tecken långt!';
-$_['error_name']        = 'Filternamn måste vara mellan 1 och 64 tecken långt!';
+$_['error_warning']          = 'Varning: Kolla gärna formuläret noga vad det gäller fel!';
+$_['error_permission']       = 'Varning: Du har inte behörighet att ändra i "Information"!';
+$_['error_title']            = 'Rubriken måste innehålla mellan 3 och 64 tecken!';
+$_['error_description']      = 'Beskrivningen måste innehålla minst 3 tecken!';
+$_['error_meta_title']       = 'Metatitel måste vara större än 3 och mindre än 255 tecken!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'Varning: Den här informationssidan kan inte raderas eftersom den är kopplad till butikens kontovillkor!';
+$_['error_checkout']         = 'Varning: Den här informationssidan kan inte raderas eftersom den är kopplad till butikens köpvillkor!';
+$_['error_affiliate']        = 'Varning: Den här informationssidan kan inte raderas eftersom den är kopplad till butikens affiliatevillkor!';
+$_['error_return']           = 'Varning: Denna informationssida kan inte tas bort eftersom det är kopplad till butikens returvillkor!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
