146a5b07bf9850e6869d6c6b19b88d6e8ad5c84b
OSWorX <info@osworx.net>
2025-03-16 11:17:44 +0100

    New translations footer.php (Slovak)

--- upload/admin/language/en-gb/common/sk-SK.php
+++ upload/admin/language/en-gb/common/sk-SK.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Obrázkový manažér';
-
 // Text
-$_['text_uploaded']    = 'Úspech: Váš súbor bol nahraný!';
-$_['text_directory']   = 'Úspech: Rozcestník bol vytvorený!';
-$_['text_delete']      = 'Úspech: Váš súbor alebo rozcestník bol odstránený!';
-
-// Entry
-$_['entry_search']     = 'Hľadať..';
-$_['entry_folder']     = 'Názov priečinku';
-
-// Error
-$_['error_permission'] = 'Upozornenie: Prístup zamietnutý!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Upozornenie: Súbor alebo rozcestník s rovnakým menom už existuje!';
-$_['error_directory']  = 'Upozornenie: Rozcestník neexistuje!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Upozornenie: Nesprávny typ súboru!';
-$_['error_upload']     = 'Upozornenie: Súbor nemohol byť nahraný z neznámych príčin!';
-$_['error_delete']     = 'Upozornenie: Nemožte zmazať tento rozcestník!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Verzia %s';
