62bdfee59f625f677709f32eb631da7ee7226557
OSWorX <info@osworx.net>
2025-03-16 14:28:41 +0100

    New translations forgotten.php (Slovenian)

--- upload/admin/language/en-gb/common/sl-SI.php
+++ upload/admin/language/en-gb/common/sl-SI.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Ste pozabili geslo?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Različica %s';
+$_['text_forgotten']  = 'Pozabljeno geslo';
+$_['text_your_email'] = 'Vaš E-Mail naslov';
+$_['text_email']      = 'Vpišite e-mail naslov, ki se nanaša na Vaš račun, potrdite in pošljite.';
+$_['text_success']    = 'Na Vaš e-mail naslov je bila poslana potrditvena povezava.';
+
+// Entry
+$_['entry_email']     = 'E-mail naslov';
+$_['entry_password']  = 'Novo geslo';
+$_['entry_confirm']   = 'Potrdite novo geslo';
+
+// Error
+$_['error_email']     = 'POZOR: Vpisanega e-mail naslova ni v naši bazi, prosimo, poizkusite znova!';
+$_['error_password']  = 'Geslo mora vsebovati med 4 in 20 znakov!';
+$_['error_confirm']   = 'Vpisa v Novo geslo in Potrdite novo geslo nista enaka!';
