78826d1dbf53311230cfbc90f2740012a4bc36a1
OSWorX <info@osworx.net>
2025-03-16 09:06:17 +0100

    New translations footer.php (French)

--- upload/language/fr-FR/fr-FR.php
+++ upload/language/fr-FR/fr-FR.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Gestionnaire d’images';
-
 // Text
-$_['text_uploaded']    = 'Félicitations, votre fichier a correctement été transféré !';
-$_['text_directory']   = 'Le dossier vient d’être créé !';
-$_['text_delete']      = 'Votre fichier ou dossier a correctement été supprimé !';
-
-// Entry
-$_['entry_search']     = 'Recherche...';
-$_['entry_folder']     = 'Nom du dossier';
-
-// Error
-$_['error_permission'] = 'Attention : accès refusé !';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Attention : un fichier ou dossier portant le même nom existe déjà !';
-$_['error_directory']  = 'Attention : le dossier n’existe pas !';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Attention : type de fichier incorrect !';
-$_['error_upload']     = 'Attention : le fichier n’a pas pu être transféré pour une raison inconnue !';
-$_['error_delete']     = 'Attention : vous ne pouvez pas supprimer ce dossier !';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Version %s';
