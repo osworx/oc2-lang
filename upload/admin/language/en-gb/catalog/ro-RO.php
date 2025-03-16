0020ae0c0d904aaa15d0011d0808cd811fd2a0b0
OSWorX <info@osworx.net>
2025-03-16 14:19:00 +0100

    New translations filter.php (Romanian)

--- upload/admin/language/en-gb/catalog/ro-RO.php
+++ upload/admin/language/en-gb/catalog/ro-RO.php
@@ -1,36 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = 'Descărcări';
+$_['heading_title']     = 'Filtre';
 
 // Text
-$_['text_success']      = 'Succes: Ai modificat descărcările!';
-$_['text_list']         = 'Lista descărcări';
-$_['text_add']          = 'Adauga Download';
-$_['text_edit']         = 'Editare Download
-';
-$_['text_upload']       = 'Fişierul a fost încărcat cu succes!';
+$_['text_success']      = 'Succes: Ai modificat filtrele!';
+$_['text_list']         = 'Lista de filtre';
+$_['text_add']          = 'Adaugă filtru';
+$_['text_edit']         = 'Modifică filtru';
 
 // Column
-$_['column_name']       = 'Nume Download';
-$_['column_date_added'] = 'Data adăugării';
+$_['column_group']      = 'Grup de filtre';
+$_['column_sort_order'] = 'Ordinea de sortare';
 $_['column_action']     = 'Acțiune';
 
 // Entry
-$_['entry_name']        = 'Nume Download';
-$_['entry_filename']    = 'Nume fișier';
-$_['entry_mask']        = 'Mască';
-
-// Help
-$_['help_filename']     = 'Puteţi încărca prin butonul de upload sau utilizaţi FTP pentru a încărca în directorul de descărcare şi introduceţi detaliile de mai jos.';
-$_['help_mask']         = 'Se recomandă ca numele fişierului şi masca sunt diferite pentru a opri oamenii încearcă să se leagă direct download-uri.';
+$_['entry_group']       = 'Denumirea grupului de filtre';
+$_['entry_name']        = 'Denumire filtru';
+$_['entry_sort_order']  = 'Ordinea de sortare';
 
 // Error
 $_['error_warning']     = 'Avertisment: Vă rugăm să verificaţi formularul pentru erori!';
-$_['error_permission']  = 'Avertisment: Nu aveţi permisiunea de a modifica descărcări!';
-$_['error_name']        = 'Numele trebuie să fie între 3 şi 64 de caractere!';
-$_['error_upload']      = 'Încărcaţi necesare!';
-$_['error_filename']    = 'Numele fişierului trebuie să fie între 3 şi 128 de caractere!';
-$_['error_exists']      = 'Fișierul nu există!';
-$_['error_mask']        = 'Masca trebuie să fie între 3 şi 128 de caractere!';
-$_['error_filetype']    = 'Fisier invalid!';
-$_['error_product']     = 'Avertisment: Această descărcare nu poate fi ștearsă cât este în prezent asociată %s produse!';
+$_['error_permission']  = 'Avertisment: Nu aveţi permisiunile necesare pentru a putea modifica filtrele!';
+$_['error_group']       = 'Denumirea grupului de filtre trebuie să aibă între 1 și 64 de caractere!';
+$_['error_name']        = 'Denumirea filtrului trebuie să aibă între 1 și 64 de caractere!';
