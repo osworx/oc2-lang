9d97d54351f3a214e2f301f41a9ae33895ec5684
OSWorX <info@osworx.net>
2025-03-16 14:21:09 +0100

    New translations manufacturer.php (Persian)

--- upload/admin/language/en-gb/catalog/fa-IR.php
+++ upload/admin/language/en-gb/catalog/fa-IR.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'اطلاعات';
+$_['heading_title']     = 'تولیدکننده‌ها';
 
 // Text
-$_['text_success']           = 'موفقیت: شما اطلاعات را تغییر دادید!';
-$_['text_list']              = 'فهرست اطلاعات';
-$_['text_add']               = 'افزودن اطلاعات';
-$_['text_edit']              = 'ویرایش اطلاعات';
-$_['text_default']           = 'پیش‎فرض';
+$_['text_success']      = 'موفقیت: شما تولیدکنندگان را تغییر داده‌اید!';
+$_['text_list']         = 'فهرست تولیدکننده';
+$_['text_add']          = 'افزودن تولیدکننده';
+$_['text_edit']         = 'ویرایش تولیدکننده';
+$_['text_default']      = 'پیش‎فرض';
+$_['text_percent']      = 'درصد';
+$_['text_amount']       = 'مقدار ثابت';
 
 // Column
-$_['column_title']           = 'عنوان اطلاعات';
-$_['column_sort_order']	     = 'ترتیب';
-$_['column_action']          = 'کنش';
+$_['column_name']       = 'نام تولیدکننده';
+$_['column_sort_order'] = 'ترتیب';
+$_['column_action']     = 'کنش';
 
 // Entry
-$_['entry_title']            = 'عنوان اطلاعات';
-$_['entry_description']      = 'توضیحات';
-$_['entry_store']            = 'فروشگاه‌ها';
-$_['entry_meta_title'] 	     = 'عنوان متا تگ';
-$_['entry_meta_keyword'] 	 = 'کلیدواژه‌های متا تگ';
-$_['entry_meta_description'] = 'توضیحات متا تگ';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'پایین';
-$_['entry_status']           = 'وضعيت';
-$_['entry_sort_order']       = 'ترتیب';
-$_['entry_layout']           = 'جایگزینی چیدمان';
+$_['entry_name']        = 'نام تولیدکننده';
+$_['entry_store']       = 'فروشگاه‌ها';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'تصویر';
+$_['entry_sort_order']  = 'ترتیب';
+$_['entry_type']        = 'نوع';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'نمایش در پاورقی پایین.';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'هشدار: لطفا فرم را به دقت برای خطاها بررسی نمایید!';
-$_['error_permission']       = 'هشدار: شما مجوز تغییر اطلاعات را ندارید!';
-$_['error_title']            = 'نام اطلاعات باید بین ۳ و ۶۴ نویسه باشد!';
-$_['error_description']      = 'توضیحات باید بیشتر از ۳ نویسه باشد!';
-$_['error_meta_title']       = 'عنوان متا باید بزرگ‌تر از ۳ و کمتر از ۲۵۵ نویسه باشد!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'هشدار: این برگه اطلاعات از آن جا که هم اکنون به عنوان قوانین حساب کاربری فروشگاه در نظر گرفته شده است نمی‌تواند حذف گردد!';
-$_['error_checkout']         = 'هشدار: این برگه اطلاعات از آن جا که هم اکنون به عنوان قوانین پرداخت نهایی فروشگاه در نظر گرفته شده است نمی‌تواند حذف گردد!';
-$_['error_affiliate']        = 'هشدار: این برگه اطلاعات از آن جا که هم اکنون به عنوان قوانین بازاریاب فروشگاه در نظر گرفته شده است نمی‌تواند حذف گردد!';
-$_['error_return']           = 'هشدار: این برگه اطلاعات از آن جا که هم اکنون به عنوان قوانین برگشتی فروشگاه در نظر گرفته شده است نمی‌تواند حذف گردد!';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'هشدار: لطفا فرم را به دقت برای خطاها بررسی نمایید!';
+$_['error_permission']  = 'هشدار: شما مجوز تغییر تولیدکنندگان را ندارید!';
+$_['error_name']        = 'نام تولیدکننده باید بین ۲ و ۶۴ نویسه باشد!';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'هشدار: این تولیدکننده از آن جا که هم اکنون برای %s محصول در نظر گرفته شده است نمی‌تواند حذف گردد!';
