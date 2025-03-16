829071bbfbbb30db8fc66cfe82f30ae865cce72d
OSWorX <info@osworx.net>
2025-03-16 11:07:01 +0100

    New translations category.php (Vietnamese)

--- upload/admin/language/en-gb/catalog/vi-VN.php
+++ upload/admin/language/en-gb/catalog/vi-VN.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Các nhóm thuộc tính';
+$_['heading_title']          = 'Các hạng mục';
 
 // Text
-$_['text_success']      = 'Kết quả: Bạn đã sửa xong các nhóm thuộc tính!';
-$_['text_list']         = 'Danh sách nhóm thuộc tính';
-$_['text_add']          = 'Thêm 1 nhóm thuộc tính';
-$_['text_edit']         = 'Chỉnh sửa 1 một thuộc tính';
+$_['text_success']           = 'Kết quả: Bạn đã sửa đổi xong các hạng mục!';
+$_['text_list']              = 'Danh sách hạng mục';
+$_['text_add']               = 'Thêm hạng mục';
+$_['text_edit']              = 'Chỉnh sửa hạng mục';
+$_['text_default']           = 'Mặc định';
 
 // Column
-$_['column_name']       = 'Tên 1 nhóm thuộc tính';
-$_['column_sort_order'] = 'Thứ tự sắp xếp';
-$_['column_action']     = 'Tác vụ';
+$_['column_name']            = 'Tên hạng mục';
+$_['column_sort_order']      = 'Thứ tự sắp xếp';
+$_['column_action']          = 'Tác vụ';
 
 // Entry
-$_['entry_name']        = 'Tên 1 nhóm thuộc tính';
-$_['entry_sort_order']  = 'Thứ tự sắp xếp';
+$_['entry_name']             = 'Tên hạng mục';
+$_['entry_description']      = 'Mô tả';
+$_['entry_meta_title'] 	     = 'Tên thẻ Meta';
+$_['entry_meta_keyword']     = 'Từ khóa cho thẻ Meta';
+$_['entry_meta_description'] = 'Mô tả cho thẻ Meta';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Mục cha';
+$_['entry_filter']           = 'Bộ lọc';
+$_['entry_store']            = 'Gian hàng';
+$_['entry_image']            = 'Hình ảnh';
+$_['entry_top']              = 'Đầu trang';
+$_['entry_column']           = 'Cột';
+$_['entry_sort_order']       = 'Thứ tự sắp xếp';
+$_['entry_status']           = 'Trạng thái';
+$_['entry_layout']           = 'Ghi đè layout';
+
+// Help
+$_['help_filter']            = '(Tự động gợi ý)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Hiện trong thanh trình đơn trên cùng. Chỉ làm việc đối với các hạng mục gốc.';
+$_['help_column']            = 'Số cột dùng cho 3 hạng mục cuối. Chỉ làm việc đối với các hạng mục gốc.';
 
 // Error
-$_['error_warning']     = 'Chú ý: Vui lòng kiểm tra lỗi cẩn thận trong biểu mẫu!';
-$_['error_permission']  = 'Chú ý: Bạn không có quyền sửa các nhóm thuộc tính!';
-$_['error_name']        = 'Tên của nhóm thuộc tính phải từ 3 tới 64 ký tự!';
-$_['error_attribute']   = 'Chú ý: Nhóm thuộc tính này không thể xóa khi nó đang được gán cho các thuộc tính %s !';
-$_['error_product']     = 'Chú ý: Nhóm thuộc tính này không thể xóa vì nó đang được gán cho các sản phẩm %s !';
+$_['error_warning']          = 'Chú ý: Vui lòng kiểm tra lỗi cẩn thận trong biểu mẫu!';
+$_['error_permission']       = 'Chú ý: Bạn không có quyền sửa đổi các hạng mục!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Tên thẻ Meta phải nhiều hơn 3 và ít hơn 255 ký tự!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
