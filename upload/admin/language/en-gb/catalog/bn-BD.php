24c0e7c9604956451453a91fce4aa447ee87fe9b
OSWorX <info@osworx.net>
2025-03-16 14:24:22 +0100

    New translations review.php (Bengali)

--- upload/admin/language/en-gb/catalog/bn-BD.php
+++ upload/admin/language/en-gb/catalog/bn-BD.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'Recurring Profiles';
+$_['heading_title']     = 'Reviews';
 
 // Text
-$_['text_success']          = 'Success: You have modified recurring profiles!';
-$_['text_list']             = 'পৌনঃপুনিক প্রোফাইল';
-$_['text_add']              = 'পৌনঃপুনিক প্রোফাইল';
-$_['text_edit']             = 'পৌনঃপুনিক প্রোফাইল';
-$_['text_day']				= 'Day';
-$_['text_week']				= 'Week';
-$_['text_semi_month']		= 'অর্ধদক্ষ মাস';
-$_['text_month']			= 'Month';
-$_['text_year']				= 'Year';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Recurring amounts are calculated by the frequency and cycles.</p><p>For example if you use a frequency of "week" and a cycle of "2", then the user will be billed every 2 weeks.</p><p>The duration is the number of times the user will make a payment, set this to 0 if you want payments until they are cancelled.</p>';
-$_['text_profile']			= 'Recurring Profile';
-$_['text_trial']			= 'Trial Profile';
-
-// Entry
-$_['entry_name']		    = 'Name';
-$_['entry_price']			= 'Price';
-$_['entry_duration']		= 'Duration';
-$_['entry_cycle']			= 'Cycle';
-$_['entry_frequency']		= 'Frequency';
-$_['entry_trial_price']		= 'Trial price';
-$_['entry_trial_duration']	= 'Trial duration';
-$_['entry_trial_status']	= 'Trial status';
-$_['entry_trial_cycle']	    = 'Trial cycle';
-$_['entry_trial_frequency'] = 'Trial frequency';
-$_['entry_status']			= 'অবস্থা';
-$_['entry_sort_order']		= 'সর্ট অর্ডার';
+$_['text_success']      = 'Success: You have modified reviews!';
+$_['text_list']         = 'তালিকা পর্যালোচনা';
+$_['text_add']          = 'রিভিউ যুক্ত করুন';
+$_['text_edit']         = 'রিভিউ যুক্ত করুন';
 
 // Column
-$_['column_name']			= 'Name';
-$_['column_sort_order']	    = 'সর্ট অর্ডার';
-$_['column_action']         = 'অ্যাকশনঃ';
+$_['column_product']    = 'Product';
+$_['column_author']     = 'Author';
+$_['column_rating']     = 'Rating';
+$_['column_status']     = 'অবস্থা';
+$_['column_date_added'] = 'তারিখ যুক্ত হয়েছে';
+$_['column_action']     = 'অ্যাকশনঃ';
+
+// Entry
+$_['entry_product']     = 'Product';
+$_['entry_author']      = 'Author';
+$_['entry_rating']      = 'Rating';
+$_['entry_status']      = 'অবস্থা';
+$_['entry_text']        = 'Text';
+$_['entry_date_added']  = 'তারিখ যুক্ত হয়েছে';
+
+// Help
+$_['help_product']      = '(স্বয়ংক্রিয় সমাপ্তি)';
 
 // Error
-$_['error_warning']         = 'সতর্কীকরণ: অনুগ্রহ করে, ফর্মের ভুলভ্রান্তি, মনোযোগ সহকারে দেখে নিন I!';
-$_['error_permission']		= 'Warning: You do not have permission to modify recurring profiles!';
-$_['error_name']			= 'Profile Name must be greater than 3 and less than 255 characters!';
-$_['error_product']			= 'Warning: This recurring profile cannot be deleted as it is currently assigned to %s products!';
+$_['error_warning']     = 'সতর্কীকরণ: অনুগ্রহ করে, ফর্মের ভুলভ্রান্তি, মনোযোগ সহকারে দেখে নিন I!';
+$_['error_permission']  = 'Warning: You do not have permission to modify reviews!';
+$_['error_product']     = 'Product required!';
+$_['error_author']      = 'Author must be between 3 and 64 characters!';
+$_['error_text']        = 'Review Text must be at least 1 character!';
+$_['error_rating']      = 'Review rating required!';
