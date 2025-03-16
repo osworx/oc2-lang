bc7b78931f1828b0165ed33c5c610e4ebb6d3de2
OSWorX <info@osworx.net>
2025-03-16 14:17:03 +0100

    New translations attribute_group.php (Chinese Simplified)

--- upload/admin/language/en-gb/catalog/zh-CN.php
+++ upload/admin/language/en-gb/catalog/zh-CN.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = '属性';
+$_['heading_title']     = '属性组';
 
 // Text
-$_['text_success']           = '修改属性成功!';
-$_['text_list']              = '属性列表';
-$_['text_add']               = '添加属性';
-$_['text_edit']              = '编辑属性';
+$_['text_success']      = '成功：你成功修改了属性组！';
+$_['text_list']         = '属性组列表';
+$_['text_add']          = '增加属性组';
+$_['text_edit']         = '编辑属性组';
 
 // Column
-$_['column_name']            = '属性名称';
-$_['column_attribute_group'] = '属性分组';
-$_['column_sort_order']      = '排列顺序';
-$_['column_action']          = '操作';
+$_['column_name']       = '属性组名';
+$_['column_sort_order'] = '排列顺序';
+$_['column_action']     = '操作';
 
 // Entry
-$_['entry_name']             = '属性名称';
-$_['entry_attribute_group']  = '属性分组';
-$_['entry_sort_order']       = '排列顺序';
+$_['entry_name']        = '属性组名';
+$_['entry_sort_order']  = '排列顺序';
 
 // Error
-$_['error_warning']          = '警告:请仔细检查错误格式!';
-$_['error_permission']       = '警告: 您没有修改属性的权限！';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = '属性名称必须介于 3 至 64 个字符之间 ！';
-$_['error_product']          = '警告： 无法删除此属性，因其已经分配给 %s 产品 ！';
+$_['error_warning']     = '警告:请仔细检查错误格式!';
+$_['error_permission']  = '警告：你没有修改属性组的权限！';
+$_['error_name']        = '属性组名必须在3至64个字符间！';
+$_['error_attribute']   = '警告：这个属性组不能删除,因为他现在已分配给%s属性!';
+$_['error_product']     = '警告：这个属性组不能删除,因为他现在已分配给%s产品!';
