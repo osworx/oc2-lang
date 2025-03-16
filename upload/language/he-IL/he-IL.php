1230c9683092cdae493e900e710e5658c0d4fbab
OSWorX <info@osworx.net>
2025-03-16 09:09:18 +0100

    New translations option.php (Hebrew)

--- upload/language/he-IL/he-IL.php
+++ upload/language/he-IL/he-IL.php
@@ -1,35 +1,40 @@
 <?php
 // Heading
-$_['heading_title']     = 'יצרנים';
+$_['heading_title']      = 'אפשרויות';
 
 // Text
-$_['text_success']      = 'הצלחה: שינית יצרנים!';
-$_['text_list']         = 'רשימת יצרנים';
-$_['text_add']          = 'הוסף יצרן';
-$_['text_edit']         = 'ערוך יצרן';
-$_['text_default']      = 'ברירת המחדל';
-$_['text_percent']      = 'אחוז';
-$_['text_amount']       = 'כמות קבועה';
+$_['text_success']       = 'הצלחה: שינית אפשרויות!';
+$_['text_list']          = 'רשימת אפשרויות';
+$_['text_add']           = 'הוסף אפשרות';
+$_['text_edit']          = 'ערוך אפשרות';
+$_['text_choose']        = 'בחר';
+$_['text_select']        = 'בחירה';
+$_['text_radio']         = 'רדיו';
+$_['text_checkbox']      = 'תיבת סימון';
+$_['text_input']         = 'קלט';
+$_['text_text']          = 'טקסט';
+$_['text_textarea']      = 'אזור טקסט';
+$_['text_file']          = 'קובץ';
+$_['text_date']          = 'תאריך';
+$_['text_datetime']      = 'תאריך ושעה';
+$_['text_time']          = 'שעה';
 
 // Column
-$_['column_name']       = 'שם היצרן';
-$_['column_sort_order'] = 'סדר מיון';
-$_['column_action']     = 'פעולה';
+$_['column_name']        = 'שם אפשרות';
+$_['column_sort_order']  = 'סדר מיון';
+$_['column_action']      = 'פעולה';
 
 // Entry
-$_['entry_name']        = 'שם היצרן';
-$_['entry_store']       = 'חנויות';
-$_['entry_keyword']     = 'SEO URL';
-$_['entry_image']       = 'תמונה';
-$_['entry_sort_order']  = 'סדר מיון';
-$_['entry_type']        = 'סוג';
-
-// Help
-$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['entry_name']         = 'שם אפשרות';
+$_['entry_type']         = 'סוג';
+$_['entry_option_value'] = 'ערך שם אפשרות';
+$_['entry_image']        = 'תמונה';
+$_['entry_sort_order']   = 'סדר מיון';
 
 // Error
-$_['error_warning']     = 'אזהרה: אנא בדוק בקפידה אם יש שגיאות בטופס!';
-$_['error_permission']  = 'אזהרה: אין לך הרשאה לשינוי יצרנים!';
-$_['error_name']        = 'שם היצרן חייב להיות בין 2 ל- 64 תווים!';
-$_['error_keyword']     = 'SEO URL already in use!';
-$_['error_product']     = 'אזהרה: לא ניתן למחוק את היצרן הזה מאחר והוא מוקצה כרגע ל %s מוצרים!';
+$_['error_warning']      = 'אזהרה: אנא בדוק בקפידה אם יש שגיאות בטופס!';
+$_['error_permission']   = 'אזהרה: אין לך הרשאה לשינוי אפשרויות!';
+$_['error_name']         = 'שם האופציה חייב להיות בין 1 ל- 128 תווים!';
+$_['error_type']         = 'אזהרה: ערכי אפשרויות דרוש!';
+$_['error_option_value'] = 'שם ערך האופציה חייב להיות בין 1 ל- 128 תווים!';
+$_['error_product']      = 'אזהרה: לא ניתן למחוק את ההורדה הזו מאחר והיא מוקצת כרגע ל %s מוצרים!';
