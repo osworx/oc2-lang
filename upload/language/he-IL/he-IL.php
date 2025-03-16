1ed08a37af0f3419e5dc9ab78c63804b90df8e31
OSWorX <info@osworx.net>
2025-03-16 09:09:26 +0100

    New translations forgotten.php (Hebrew)

--- upload/language/he-IL/he-IL.php
+++ upload/language/he-IL/he-IL.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'שכחת סיסמא?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'גירסה %s';
+$_['text_forgotten']  = 'סיסמה נשכחה';
+$_['text_your_email'] = 'כתובת הדואר האלקטרוני שלך';
+$_['text_email']      = 'הזן את כתובת הדואר האלקטרוני המשויכת לחשבון שלך. לחץ על שלח כדי לקבל קישור בדואל שלך לאיפוס סיסמתך.';
+$_['text_success']    = 'הודעת דואל עם קישור נשלחה לכתובת הדוא"ל לאישור כניסת המנהל שלך.';
+
+// Entry
+$_['entry_email']     = 'כתובת דואר אלקטרוני';
+$_['entry_password']  = 'סיסמא חדשה';
+$_['entry_confirm']   = 'אשר';
+
+// Error
+$_['error_email']     = 'אזהרה: כתובת הדואר האלקטרוני לא נמצא ברשומות שלנו, אנא נסה שוב!';
+$_['error_password']  = 'הסיסמה חייבת להיות בין 3 ל 20 תווים!';
+$_['error_confirm']   = 'הסיסמה ואישור סיסמה אינם תואמים!';
