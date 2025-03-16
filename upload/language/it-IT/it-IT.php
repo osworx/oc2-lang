dcf8a7f8600fa06a39105511adfe1411feac7400
OSWorX <info@osworx.net>
2025-03-16 09:10:16 +0100

    New translations forgotten.php (Italian)

--- upload/language/it-IT/it-IT.php
+++ upload/language/it-IT/it-IT.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Hai dimenticato la Password?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Versione %s';
+$_['text_forgotten']  = 'Password dimenticata';
+$_['text_your_email'] = 'Inserisci Il Tuo Indirizzo di posta elettronica.';
+$_['text_email']      = 'Immettere l\'indirizzo e-mail associato al tuo account. Fare clic su Invia per ricevere una password e reimpostare il collegamento via e-mail.';
+$_['text_success']    = 'È stata inviata un\'e-mail con un link di conferma all\'indirizzo e-mail admin.';
+
+// Entry
+$_['entry_email']     = 'Indirizzo di posta elettronica';
+$_['entry_password']  = 'Nuova Password';
+$_['entry_confirm']   = 'Conferma';
+
+// Error
+$_['error_email']     = 'Attenzione: L\'indirizzo di posta elettronica non &egrave stato trovato nei nostri archivi, per favore prova ancora una volta!';
+$_['error_password']  = 'La password deve essere compresa tra 3 e 20 caratteri!';
+$_['error_confirm']   = 'La Password e La conferma Della password non corrispondono!';
