c920996ff78808f16f72e21c79cfbefcf3b4d607
OSWorX <info@osworx.net>
2025-03-16 11:05:27 +0100

    New translations attribute.php (Croatian)

---
+++ upload/admin/language/en-gb/catalog/hr-HR.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Značajke';
+
+// Text
+$_['text_success']           = 'Uspjeh: Promijenili ste značajke!';
+$_['text_list']              = 'Popis značajki';
+$_['text_add']               = 'Dodaj značajku';
+$_['text_edit']              = 'Uredite značajku';
+
+// Column
+$_['column_name']            = 'Naziv značajke';
+$_['column_attribute_group'] = 'Grupa značajki';
+$_['column_sort_order']      = 'Redoslijed';
+$_['column_action']          = 'Radnja';
+
+// Entry
+$_['entry_name']             = 'Naziv značajke';
+$_['entry_attribute_group']  = 'Grupa značajki';
+$_['entry_sort_order']       = 'Redoslijed';
+
+// Error
+$_['error_warning']          = 'Upozorenje: Pažljivo provjerite ima li pogrešaka u obrascu!';
+$_['error_permission']       = 'Upozorenje: Niste ovlašteni za promjenu značajki!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Naziv značajke mora biti između 3 i 64 znaka!';
+$_['error_product']          = 'Upozorenje: Ovaj značajka ne može se izbrisati jer ju koristi %s artikala!';
