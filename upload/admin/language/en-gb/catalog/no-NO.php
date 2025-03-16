66ebc0edcc45158553a29ea5419d3be237d1caa1
OSWorX <info@osworx.net>
2025-03-16 14:16:53 +0100

    New translations attribute_group.php (Norwegian)

--- upload/admin/language/en-gb/catalog/no-NO.php
+++ upload/admin/language/en-gb/catalog/no-NO.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Produktegenskaper';
+$_['heading_title']     = 'Produktegenskaper - Grupper';
 
 // Text
-$_['text_success']           = 'Endringene ble vellykket lagret.';
-$_['text_list']              = 'Liste';
-$_['text_add']               = 'Legg til';
-$_['text_edit']              = 'Endre';
+$_['text_success']      = 'Endringer ble vellykket lagret.';
+$_['text_list']         = 'Liste';
+$_['text_add']          = 'Legg til';
+$_['text_edit']         = 'Endre';
 
 // Column
-$_['column_name']            = 'Navn';
-$_['column_attribute_group'] = 'Gruppe';
-$_['column_sort_order']      = 'Sortering';
-$_['column_action']          = 'Valg';
+$_['column_name']       = 'Navn';
+$_['column_sort_order'] = 'Sortering';
+$_['column_action']     = 'Valg';
 
 // Entry
-$_['entry_name']             = 'Navn';
-$_['entry_attribute_group']  = 'Gruppe';
-$_['entry_sort_order']       = 'Sortering';
+$_['entry_name']        = 'Navn';
+$_['entry_sort_order']  = 'Sortering';
 
 // Error
-$_['error_warning']          = 'Påse at alle påkrevde felter er fylt inn.';
-$_['error_permission']       = 'Du har ikke rettigheter til å utføre valgte handling.';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'Navn må inneholde mellom 3 og 64 tegn.';
-$_['error_product']          = 'Denne produktegenskapen kan ikke slettes ettersom den er tilknyttet %s produkter.';
+$_['error_warning']     = 'Påse at alle påkrevde felter er fylt inn.';
+$_['error_permission']  = 'Du har ikke rettigheter til å utføre valgte handling.';
+$_['error_name']        = 'Navn må inneholde mellom 3 og 64 tegn.';
+$_['error_attribute']   = 'Denne gruppen kan ikke slettes ettersom den er tilknyttet %s produktegenskaper.';
+$_['error_product']     = 'Denne gruppen kan ikke slettes ettersom den er tilknyttet %s produkter.';
