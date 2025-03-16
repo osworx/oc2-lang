14562f10938e92772f34b4b0279676997adb3378
OSWorX <info@osworx.net>
2025-03-16 14:20:43 +0100

    New translations manufacturer.php (Danish)

--- upload/admin/language/en-gb/catalog/da-DK.php
+++ upload/admin/language/en-gb/catalog/da-DK.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Information';
+$_['heading_title']     = 'Producenter';
 
 // Text
-$_['text_success']           = 'Succes: Du har ændret information!';
-$_['text_list']              = 'Information';
-$_['text_add']               = 'Tilføj information';
-$_['text_edit']              = 'Rediger information';
-$_['text_default']           = 'Standard';
+$_['text_success']      = 'Succes: Du har ændret producenter!';
+$_['text_list']         = 'Producenter';
+$_['text_add']          = 'Tilføj producent';
+$_['text_edit']         = 'Rediger producent';
+$_['text_default']      = 'Standard';
+$_['text_percent']      = 'Procentdel';
+$_['text_amount']       = 'Fast mængde';
 
 // Column
-$_['column_title']           = 'Informationstitel';
-$_['column_sort_order']	     = 'Sorteringsrækkefølge';
-$_['column_action']          = 'Handling';
+$_['column_name']       = 'Producentnavn';
+$_['column_sort_order'] = 'Sorteringsrækkefølge';
+$_['column_action']     = 'Handling';
 
 // Entry
-$_['entry_title']            = 'Informationstitel';
-$_['entry_description']      = 'Beskrivelse';
-$_['entry_store']            = 'Butikker';
-$_['entry_meta_title'] 	     = 'Meta tag titel';
-$_['entry_meta_keyword'] 	 = 'Meta tag nøgleord';
-$_['entry_meta_description'] = 'Meta tag Beskrivelse';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'Ned';
-$_['entry_status']           = 'Status';
-$_['entry_sort_order']       = 'Sorteringsrækkefølge';
-$_['entry_layout']           = 'Andet layout';
+$_['entry_name']        = 'Producentnavn';
+$_['entry_store']       = 'Butikker';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'Billede';
+$_['entry_sort_order']  = 'Sorteringsrækkefølge';
+$_['entry_type']        = 'Type';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'Vis i sidefoden.';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'Advarsel: Gennemgå venligst formularen for fejl!';
-$_['error_permission']       = 'Fejl: Du har ikke tilladelse til at ændre informationssider!';
-$_['error_title']            = 'Informationens titel skal være mellem 3 og 64 karakterer!';
-$_['error_description']      = 'Beskrivelse skal være på mere end 3 karakterer!';
-$_['error_meta_title']       = 'Metatitel skal være mellem 3 og 255 karakterer!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'Fejl: Denne informationsside kan ikke slettes, da den i øjeblikket er tilknyttet butikkens konto-betingelser!';
-$_['error_checkout']         = 'Fejl: Denne informationsside kan ikke slettes, da den i øjeblikket er tilknyttet butikkens købsbetingelser!';
-$_['error_affiliate']        = 'Fejl: Denne informationsside kan ikke slettes, da den i øjeblikket er tilknyttet butikkens partner-betingelser!';
-$_['error_return']           = 'Fejl: Denne informationsside kan ikke slettes, da den i øjeblikket er tilknyttet butikkens returneringsbetingelser!';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'Advarsel: Gennemgå venligst formularen for fejl!';
+$_['error_permission']  = 'Fejl: Du har ikke tilladelse til at ændre producenter!';
+$_['error_name']        = 'Producentnavn skal være mellem 2 og 64 karakterer!';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'Fejl: Denne producent kan ikke slettes, da den i øjeblikket er tilknyttet %s producenter!';
