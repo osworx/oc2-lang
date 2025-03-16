049a944e19cdc883324a6a9a007ee37acfd8737f
OSWorX <info@osworx.net>
2025-03-16 11:05:30 +0100

    New translations attribute.php (Azerbaijani)

---
+++ upload/admin/language/en-gb/catalog/az-AZ.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']          = 'Xüsusiyyətlər';
+
+// Text
+$_['text_success']           = 'Uğurlu olaraq: Siz Atributları dəyişdirdiniz!';
+$_['text_list']              = 'Xüsusiyyət Adı';
+$_['text_add']               = 'Atribut əlavə et';
+$_['text_edit']              = 'Atributları dəyişditin';
+
+// Column
+$_['column_name']            = 'Xüsusiyyət Adı';
+$_['column_attribute_group'] = 'Xüsusiyyət qrupu';
+$_['column_sort_order']      = 'Növlərə görə sırala';
+$_['column_action']          = 'Hərəkət';
+
+// Entry
+$_['entry_name']             = 'Xüsusiyyət Adı';
+$_['entry_attribute_group']  = 'Xüsusiyyət qrupu';
+$_['entry_sort_order']       = 'Növlərə görə sırala';
+
+// Error
+$_['error_warning']          = 'Xəbərdarlıq: Xahiş edirik, formu göstərilən səhvlər səbəbiylə diqqətlə nəzarət edin!';
+$_['error_permission']       = 'Xəbərdarlıq: Sizin Atribut qrupların dəyişdirmək üçün icazəniz yoxdur!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Xüsusiyyət Adı 3 və 64 simvol arası olmalıdır!';
+$_['error_product']          = 'Xəbərdar etmək: Bu əlamət silinə bilməz, necə ki bu hal-hazırda %s məhsullarına təyin edilir!
+';
