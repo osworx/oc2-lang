38e2991cb611f3e26e64eafc59dc86e39a7db090
OSWorX <info@osworx.net>
2025-03-16 14:20:18 +0100

    New translations information.php (Galician)

--- upload/admin/language/en-gb/catalog/gl-ES.php
+++ upload/admin/language/en-gb/catalog/gl-ES.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'Filtros';
+$_['heading_title']          = 'Informacións';
 
 // Text
-$_['text_success']      = 'Modificarnse os filtros correctamente!';
-$_['text_list']         = 'Lista de filtros';
-$_['text_add']          = 'Engadir filtro';
-$_['text_edit']         = 'Editar filtro';
+$_['text_success']           = 'Success: You have modified information!';
+$_['text_list']              = 'Lista de informacións';
+$_['text_add']               = 'Engadir información';
+$_['text_edit']              = 'Editar información';
+$_['text_default']           = 'Por defecto';
 
 // Column
-$_['column_group']      = 'Grupo de filtros';
-$_['column_sort_order'] = 'Orde da clasificación';
-$_['column_action']     = 'Acción';
+$_['column_title']           = 'Título da información';
+$_['column_sort_order']	     = 'Orde da clasificación';
+$_['column_action']          = 'Acción';
 
 // Entry
-$_['entry_group']       = 'Nome do grupo de filtros';
-$_['entry_name']        = 'Nome do filtro';
-$_['entry_sort_order']  = 'Orde da clasificación';
+$_['entry_title']            = 'Título da información';
+$_['entry_description']      = 'Descrición';
+$_['entry_store']            = 'Tendas';
+$_['entry_meta_title'] 	     = 'Titulo meta etiqueta';
+$_['entry_meta_keyword'] 	 = 'Meta etiquetas de palabras chave';
+$_['entry_meta_description'] = 'Descrición meta etiquetas';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Abaixo';
+$_['entry_status']           = 'Estado';
+$_['entry_sort_order']       = 'Orde da clasificación';
+$_['entry_layout']           = 'Deseño Override';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Amosar o final do pé.';
 
 // Error
-$_['error_warning']     = 'Aviso: Por favor, revise o formulario e busque os erros!';
-$_['error_permission']  = 'Aviso: Non ten os permisos para modificar os filtros!';
-$_['error_group']       = 'O nome do grupo de filtros debe ter entre 1 e 64 caracteres!';
-$_['error_name']        = 'O nome do filtro debe ter entre 1 e 64 caracteres!';
+$_['error_warning']          = 'Aviso: Por favor, revise o formulario e busque os erros!';
+$_['error_permission']       = 'Aviso: non ten permisos para modificar as informacións!';
+$_['error_title']            = 'O título da información debe ter entre 3 e 64 caracteres!';
+$_['error_description']      = 'A descripción debe ter mais de 3 caracteres!';
+$_['error_meta_title']       = 'O Meta Title debe ter entre 3 e 255 caracteres!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'Aviso: Esta páxina de información non se pode eliminar xa que esta asignada a os termos de conta da tenda!';
+$_['error_checkout']         = 'Aviso: esta páxina de información non pode ser eliminada xa que actualmente esta asignada a os termos do checkout da tenda!';
+$_['error_affiliate']        = 'Aviso: esta páxina de información non se pode eliminar xa que actualmente esta asignada a os termos de afiliación da tenda!';
+$_['error_return']           = 'Aviso: esta páxina de información non pode ser eliminada xa que actualmente esta asignada a os termos de devolución da tenda!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
