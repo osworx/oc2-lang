7814118875427b6ea7ca47f1c3a4364f94388451
OSWorX <info@osworx.net>
2025-03-16 09:09:37 +0100

    New translations recurring.php (Hungarian)

--- upload/language/hu-HU/hu-HU.php
+++ upload/language/hu-HU/hu-HU.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'Termékek';
+$_['heading_title']			= 'Ismétlődő profilok';
 
 // Text
-$_['text_success']           = 'Siker: Módosította a terméket!';
-$_['text_list']              = 'Terméklista';
-$_['text_add']               = 'Termék hozzáadása';
-$_['text_edit']              = 'Termék szerkesztése';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'Alapértelmezés';
-$_['text_option']            = 'Beállítás';
-$_['text_option_value']      = 'Beállítás értéke';
-$_['text_percent']           = 'Százalék';
-$_['text_amount']            = 'Fix összeg';
-
-// Column
-$_['column_name']            = 'Terméknév';
-$_['column_model']           = 'Model';
-$_['column_image']           = 'Kép';
-$_['column_price']           = 'Ár';
-$_['column_quantity']        = 'Mennyiség';
-$_['column_status']          = 'Állapot';
-$_['column_action']          = 'Művelet';
+$_['text_success']          = 'Siker: Módosította az ismétlődő profilokat!';
+$_['text_list']             = 'Ismétlődő profilok listája';
+$_['text_add']              = 'Ismétlődő profil hozzáadása';
+$_['text_edit']             = 'Ismétlődő profil szerkesztése';
+$_['text_day']				= 'Nap';
+$_['text_week']				= 'Hét';
+$_['text_semi_month']		= 'Fél hónap';
+$_['text_month']			= 'Hónap';
+$_['text_year']				= 'Év';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i>A gyakoriság és a ciklusok alapján számítjuk ki az ismétlődő összegeket.</p> <p>Például, ha az Ön által használt gyakoriság a "hét" és a ciklus "2", akkor 2 hetente számlázunk.</p><p>Az időintervallum az a szám, amely alatt a felhasználó teljesíti a fizetést, beállíthatja nullára, ha szeretné a kifizetéseket, amég nem törlik azokat.</p>';
+$_['text_profile']			= 'Ismétlődő profil';
+$_['text_trial']			= 'Próba profil';
 
 // Entry
-$_['entry_name']             = 'Terméknév';
-$_['entry_description']      = 'Leírás';
-$_['entry_meta_title'] 	     = 'Meta Tag-cím';
-$_['entry_meta_keyword'] 	 = 'Meta Tag kulcsszó';
-$_['entry_meta_description'] = 'Meta-Tag leírás ';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Model';
-$_['entry_sku']              = 'SKU';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'Útvonal';
-$_['entry_shipping']         = 'Szállítandó termék';
-$_['entry_manufacturer']     = 'Gyártó';
-$_['entry_store']            = 'Áruházak';
-$_['entry_date_available']   = 'Mióta kapható';
-$_['entry_quantity']         = 'Mennyiség';
-$_['entry_minimum']          = 'Minimum mennyiség';
-$_['entry_stock_status']     = 'Hiánycikk állapot';
-$_['entry_price']            = 'Ár';
-$_['entry_tax_class']        = 'Adó osztály';
-$_['entry_points']           = 'Pontok';
-$_['entry_option_points']    = 'Pontok';
-$_['entry_subtract']         = 'Készlet kivonása';
-$_['entry_weight_class']     = 'Súlymérték';
-$_['entry_weight']           = 'Súly';
-$_['entry_dimension']        = 'Méret (H x SZ x M)';
-$_['entry_length_class']     = 'Hosszúság osztály';
-$_['entry_length']           = 'Hossz';
-$_['entry_width']            = 'Szélesség';
-$_['entry_height']           = 'Magasság';
-$_['entry_image']            = 'Kép';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Vevő csoport';
-$_['entry_date_start']       = 'Kezdő dátum';
-$_['entry_date_end']         = 'Záró dátum';
-$_['entry_priority']         = 'Prioritás';
-$_['entry_attribute']        = 'Tulajdonság';
-$_['entry_attribute_group']  = 'Jellemzőcsoport';
-$_['entry_text']             = 'Szöveg';
-$_['entry_option']           = 'Beállítás';
-$_['entry_option_value']     = 'Beállítás értéke';
-$_['entry_required']         = 'Kötelező';
-$_['entry_status']           = 'Állapot';
-$_['entry_sort_order']       = 'Rendezés';
-$_['entry_category']         = 'Kategóriák';
-$_['entry_filter']           = 'Szűrők';
-$_['entry_download']         = 'Letöltések';
-$_['entry_related']          = 'Kapcsolódó termékek';
-$_['entry_tag']          	 = 'Termék címkék';
-$_['entry_reward']           = 'Hűségpontok';
-$_['entry_layout']           = 'Elrendezés felülbírálása';
-$_['entry_recurring']        = 'Ismétlődő profil';
+$_['entry_name']		    = 'Név';
+$_['entry_price']			= 'Ár';
+$_['entry_duration']		= 'Időtartam';
+$_['entry_cycle']			= 'Ciklus';
+$_['entry_frequency']		= 'Gyakoriság';
+$_['entry_trial_price']		= 'Próba ára';
+$_['entry_trial_duration']	= 'Próba időtartama';
+$_['entry_trial_status']	= 'Próba állapota';
+$_['entry_trial_cycle']	    = 'Próba ciklusa';
+$_['entry_trial_frequency'] = 'Próba gyakorisága';
+$_['entry_status']			= 'Állapot';
+$_['entry_sort_order']		= 'Rendezés';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'Raktározási Cikkszám';
-$_['help_upc']               = 'Univerzáils Termékkód';
-$_['help_ean']               = 'Európai Cikkszám';
-$_['help_jan']               = 'Japán Cikkszám';
-$_['help_isbn']              = 'Nemzetközi Könyvszámozási Szabvány';
-$_['help_mpn']               = 'Gyártói Termékszám';
-$_['help_manufacturer']      = '(Autó Kiegészítés)';
-$_['help_minimum']           = 'Minimum rendelhető mennyiség';
-$_['help_stock_status']      = 'Látható állapot, amikor a termék nincs raktáron';
-$_['help_points']            = 'Pontok száma, ami szükséges a tétel vásárlásához. Ha nem akarjuk, hogy ez a termék megvásárolegyen pontokért, akkor hagyja 0-án.';
-$_['help_category']          = '(Autó Kiegészítés)';
-$_['help_filter']            = '(Autó Kiegészítés)';
-$_['help_download']          = '(Autó Kiegészítés)';
-$_['help_related']           = '(Autó Kiegészítés)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'Név';
+$_['column_sort_order']	    = 'Rendezés';
+$_['column_action']         = 'Művelet';
 
 // Error
-$_['error_warning']          = 'Figyelem: Kérjük, figyelmesen ellenőrizze a hibákat!';
-$_['error_permission']       = 'Figyelmeztetés: Nincs jogosultsága módosítani terméket!';
-$_['error_name']             = 'A termék nevének 3 és 255 karakter között kell lennie!';
-$_['error_meta_title']       = 'A meta cím több mint 3, és kevesebb, mint 255 karakternek kell lennie!';
-$_['error_model']            = 'A cikkszám legalább 3 és legfeljebb 64 karakterből állhat!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Figyelem: Kérjük, figyelmesen ellenőrizze a hibákat!';
+$_['error_permission']		= 'Figyelem: Nincs jogosultsága az ismétlődő profilok módosításához!';
+$_['error_name']			= 'A profilnév 3-nál több és 255-nél kevesebb karaktert tartalmazhat!';
+$_['error_product']			= 'Figyelem: Ez az ismétlődő profil nem törölhető, mivel jelenleg %s termékhez van hozzárendelve!';
