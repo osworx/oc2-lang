9767dea12c7d80b9449b8c909cd9022e4fa2fe35
OSWorX <info@osworx.net>
2025-03-16 11:17:28 +0100

    New translations footer.php (Czech)

--- upload/admin/language/en-gb/common/cs-CZ.php
+++ upload/admin/language/en-gb/common/cs-CZ.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Manažer obrázků';
-
 // Text
-$_['text_uploaded']    = 'Úspěch: Váš soubor byl nahrán!';
-$_['text_directory']   = 'Úspěch: Adresář vytvořen!';
-$_['text_delete']      = 'Úspěch: Soubor nebo adresář byl smazán!';
-
-// Entry
-$_['entry_search']     = 'Hledat..';
-$_['entry_folder']     = 'Název složky';
-
-// Error
-$_['error_permission'] = 'Varování: Přístup odepřen!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Varování: Soubor nebo adresář se stejným názvem již existuje!';
-$_['error_directory']  = 'Varování: Adresář neexistuje!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Varování: Nesprávný typ souboru!';
-$_['error_upload']     = 'Varování: Soubor nebylo možné odeslat z neznámého důvodu!';
-$_['error_delete']     = 'Varování: Tento adresář nemůžete smazat!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Verze %s';
