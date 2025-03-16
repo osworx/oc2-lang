f81fe988664b000c5457dc9ae162e60553fe0b38
OSWorX <info@osworx.net>
2025-03-16 09:07:11 +0100

    New translations header.php (Belarusian)

--- upload/language/be-BY/be-BY.php
+++ upload/language/be-BY/be-BY.php
@@ -1,19 +1,23 @@
 <?php
-// header
-$_['heading_title']   = 'Забылі пароль?';
+// Heading
+$_['heading_title']          = 'OpenCart';
 
 // Text
-$_['text_forgotten']  = 'Забылі пароль';
-$_['text_your_email'] = 'Your E-Mail Address';
-$_['text_email']      = 'Enter the e-mail address associated with your account. Click submit to have a password reset link e-mailed to you.';
-$_['text_success']    = 'An email with a confirmation link has been sent your admin email address.';
-
-// Entry
-$_['entry_email']     = 'E-Mail Address';
-$_['entry_password']  = 'New Password';
-$_['entry_confirm']   = 'Confirm';
-
-// Error
-$_['error_email']     = 'Warning: The E-Mail Address was not found in our records, please try again!';
-$_['error_password']  = 'Password must be between 4 and 20 characters!';
-$_['error_confirm']   = 'Password and password confirmation do not match!';
+$_['text_order']             = 'Замовы';
+$_['text_processing_status'] = 'Processing';
+$_['text_complete_status']   = 'Завершана';
+$_['text_customer']          = 'Пакупнікі';
+$_['text_online']            = 'Пакупнікоў анлайн';
+$_['text_approval']          = 'Чакае падцверджання';
+$_['text_product']           = 'Тавары';
+$_['text_stock']             = 'Няма ў наяўнасці';
+$_['text_review']            = 'Агляды';
+$_['text_return']            = 'Вяртанне';
+$_['text_affiliate']         = 'Партнёрская праграма';
+$_['text_store']             = 'Крамы';
+$_['text_front']             = 'Вітрына';
+$_['text_help']              = 'Дапамога';
+$_['text_homepage']          = 'OpenCart Homepage';
+$_['text_support']           = 'Форум падтрымкі';
+$_['text_documentation']     = 'Дакументацыя';
+$_['text_logout']            = 'Выйсці';
