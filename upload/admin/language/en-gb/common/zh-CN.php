3bc2251b478c41b9e899083ef9888e5a5e8e7aca
OSWorX <info@osworx.net>
2025-03-16 14:29:33 +0100

    New translations header.php (Chinese Simplified)

--- upload/admin/language/en-gb/common/zh-CN.php
+++ upload/admin/language/en-gb/common/zh-CN.php
@@ -1,19 +1,23 @@
 <?php
-// header
-$_['heading_title']   = '忘记您的密码？';
+// Heading
+$_['heading_title']          = 'OpenCart';
 
 // Text
-$_['text_forgotten']  = '忘记密码';
-$_['text_your_email'] = '您的电子邮件地址';
-$_['text_email']      = '请输入您帐户关联的电子邮件地址。点击“提交”按钮，一封重置密码邮件会发送去您的电子邮件地址。';
-$_['text_success']    = '密码重置邮件已发去您的管理员电子邮件地址。';
-
-// Entry
-$_['entry_email']     = '电子邮件地址';
-$_['entry_password']  = '新密码';
-$_['entry_confirm']   = '确认';
-
-// Error
-$_['error_email']     = '警告：找不到当前电子邮件地址，请重试！';
-$_['error_password']  = '密码必须在4到20个字符之间！';
-$_['error_confirm']   = '密码和确认密码不匹配 ！';
+$_['text_order']             = '订单';
+$_['text_processing_status'] = '正在处理中';
+$_['text_complete_status']   = '已完成';
+$_['text_customer']          = '客户';
+$_['text_online']            = '在线客户';
+$_['text_approval']          = '待审批';
+$_['text_product']           = '产品管理';
+$_['text_stock']             = '缺货';
+$_['text_review']            = '商品点评';
+$_['text_return']            = '退货';
+$_['text_affiliate']         = '推广联盟';
+$_['text_store']             = '商店';
+$_['text_front']             = '店铺前台';
+$_['text_help']              = '帮助';
+$_['text_homepage']          = 'OpenCart Homepage';
+$_['text_support']           = '支持论坛';
+$_['text_documentation']     = '文档';
+$_['text_logout']            = '退出登录';
