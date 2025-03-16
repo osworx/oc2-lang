2de39498f7af248a4c46cd7d16ae3542c9c48c3f
OSWorX <info@osworx.net>
2025-03-16 11:12:50 +0100

    New translations recurring.php (Finnish)

--- upload/admin/language/en-gb/catalog/fi-FI.php
+++ upload/admin/language/en-gb/catalog/fi-FI.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'Tuotteet';
+$_['heading_title']			= 'Toistuvat profiilit';
 
 // Text
-$_['text_success']           = 'Olet muokannut tuotetta onnistuneesti!';
-$_['text_list']              = 'Tuoteluettelo';
-$_['text_add']               = 'Lisää tuote';
-$_['text_edit']              = 'Muokkaa tuotetta';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'Oletus';
-$_['text_option']            = 'Valinta';
-$_['text_option_value']      = 'Valinnan arvo';
-$_['text_percent']           = 'Prosenttiosuus';
-$_['text_amount']            = 'Kiinteä summa';
-
-// Column
-$_['column_name']            = 'Tuotenimi';
-$_['column_model']           = 'Malli';
-$_['column_image']           = 'Kuva';
-$_['column_price']           = 'Hinta';
-$_['column_quantity']        = 'Määrä';
-$_['column_status']          = 'Tila';
-$_['column_action']          = 'Toiminto';
+$_['text_success']          = 'Onnistui: Olet muuttanut toistuvia profiiliileja!';
+$_['text_list']             = 'Toistuvat profiilit';
+$_['text_add']              = 'Lisää toistuva profiili';
+$_['text_edit']             = 'Toistuvan profiilin muokkaaminen';
+$_['text_day']				= 'Päivä';
+$_['text_week']				= 'Viikko';
+$_['text_semi_month']		= 'Kahdesti kuussa';
+$_['text_month']			= 'Kuukausi';
+$_['text_year']				= 'Vuosi';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Toistuvat summat lasketaan tiheydestä ja syklistä.</p><p>Esimerkiksi jos tiheys on "viikko" ja sykli on "2", käyttäjää laskutetaan 2 viikon välein.</p><p>Kesto on montako maksua käyttäjälle tulee. Aseta kestoksi 0, jos maksut jatkuvat toistaiseksi.</p>';
+$_['text_profile']			= 'Toistuva profiili';
+$_['text_trial']			= 'Koejakson profiili';
 
 // Entry
-$_['entry_name']             = 'Tuotenimi';
-$_['entry_description']      = 'Kuvaus';
-$_['entry_meta_title'] 	     = 'Metatagien otsikko';
-$_['entry_meta_keyword'] 	 = 'Metatagien avainsanat';
-$_['entry_meta_description'] = 'Metatagin kuvaus';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Malli';
-$_['entry_sku']              = 'Tuotenro';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'Sijainti';
-$_['entry_shipping']         = 'Vaatii toimituksen';
-$_['entry_manufacturer']     = 'Valmistaja';
-$_['entry_store']            = 'Kaupat';
-$_['entry_date_available']   = 'Saatavilla alkaen';
-$_['entry_quantity']         = 'Määrä';
-$_['entry_minimum']          = 'Vähimmäismäärä';
-$_['entry_stock_status']     = 'Varaston tila';
-$_['entry_price']            = 'Hinta';
-$_['entry_tax_class']        = 'Veroluokka';
-$_['entry_points']           = 'Pisteet';
-$_['entry_option_points']    = 'Pisteet';
-$_['entry_subtract']         = 'Vähennä varastosta';
-$_['entry_weight_class']     = 'Painoluokka';
-$_['entry_weight']           = 'Paino';
-$_['entry_dimension']        = 'Mitat (syvyys x leveys x korkeus)';
-$_['entry_length_class']     = 'Pituusluokka';
-$_['entry_length']           = 'Pituus';
-$_['entry_width']            = 'Leveys';
-$_['entry_height']           = 'Korkeus';
-$_['entry_image']            = 'Kuva';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Asiakasryhmä';
-$_['entry_date_start']       = 'Aloituspäivä';
-$_['entry_date_end']         = 'Lopetuspäivä';
-$_['entry_priority']         = 'Tärkeys';
-$_['entry_attribute']        = 'Ominaisuus';
-$_['entry_attribute_group']  = 'Ominaisuusryhmä';
-$_['entry_text']             = 'Teksti';
-$_['entry_option']           = 'Valinta';
-$_['entry_option_value']     = 'Valinnan arvo';
-$_['entry_required']         = 'Vaadittu';
-$_['entry_status']           = 'Tila';
-$_['entry_sort_order']       = 'Lajittelujärjestys';
-$_['entry_category']         = 'Kategoriat';
-$_['entry_filter']           = 'Suodattimet';
-$_['entry_download']         = 'Lataukset';
-$_['entry_related']          = 'Katso myös nämä tuotteet';
-$_['entry_tag']          	 = 'Tuotteen tagit';
-$_['entry_reward']           = 'Palkitsemispisteet';
-$_['entry_layout']           = 'Ohita asettelu';
-$_['entry_recurring']        = 'Toistuva profiili';
+$_['entry_name']		    = 'Nimi';
+$_['entry_price']			= 'Hinta';
+$_['entry_duration']		= 'Kesto';
+$_['entry_cycle']			= 'Sykli';
+$_['entry_frequency']		= 'Tiheys';
+$_['entry_trial_price']		= 'Koejakson hinta';
+$_['entry_trial_duration']	= 'Koejakson kesto';
+$_['entry_trial_status']	= 'Koejakson tila';
+$_['entry_trial_cycle']	    = 'Koejakson sykli';
+$_['entry_trial_frequency'] = 'Koejakson tiheys';
+$_['entry_status']			= 'Tila';
+$_['entry_sort_order']		= 'Lajittelujärjestys';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'varastoinnin yksikkö';
-$_['help_upc']               = 'Yleinen Tuotekoodi';
-$_['help_ean']               = 'Eurooppalainen viitenumero';
-$_['help_jan']               = 'Japanilainen viitenumero';
-$_['help_isbn']              = 'Kirjallisen tuotteen kansainvälinen standarditunnus';
-$_['help_mpn']               = 'Valmistajanumero';
-$_['help_manufacturer']      = '(Automaattinen täydennys)';
-$_['help_minimum']           = 'Pakottaa tilaamaan vähimmäismäärän';
-$_['help_stock_status']      = 'Näytettävä tila kun tuotetta ei ole saatavilla';
-$_['help_points']            = 'Palkitsemispisteet, jotka vaaditaan tuotteen ostamiseksi. Jos et halua tuotetta ostettavan pisteillä, jätä arvoksi 0.';
-$_['help_category']          = '(Automaattinen täydennys)';
-$_['help_filter']            = '(Automaattinen täydennys)';
-$_['help_download']          = '(Automaattinen täydennys)';
-$_['help_related']           = '(Automaattinen täydennys)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'Nimi';
+$_['column_sort_order']	    = 'Lajittelujärjestys';
+$_['column_action']         = 'Toiminto';
 
 // Error
-$_['error_warning']          = 'Varoitus: tarkista lomake virheiden varalta!';
-$_['error_permission']       = 'Varoitus: sinulla ei ole oikeuksia muokata tuotteita!';
-$_['error_name']             = 'Tuotteen nimen tulee olla 3 - 255 merkkiä pitkä!';
-$_['error_meta_title']       = 'Sivun otsikon  tulee olla 3 - 255 merkkiä pitkä!';
-$_['error_model']            = 'Tuotekoodin tulee olla 3 - 64 merkkiä pitkä!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Varoitus: tarkista lomake virheiden varalta!';
+$_['error_permission']		= 'Varoitus: Sinulla ei ole oikeuksia muokata toistuvia profiileja!';
+$_['error_name']			= 'Profiilin nimen tulee olla pidempi kuin 3 ja lyhyempi kuin 255 merkkiä!';
+$_['error_product']			= 'Varoitus: Tätä toistuvaa profiilia ei voida poistaa, koska se on määritetty %s tuotteelle!';
