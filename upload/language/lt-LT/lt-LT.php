f7e8be6b978110889efa8a68d5c32e0d194c05d1
OSWorX <info@osworx.net>
2025-03-16 09:11:19 +0100

    New translations review.php (Lithuanian)

--- upload/language/lt-LT/lt-LT.php
+++ upload/language/lt-LT/lt-LT.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'Pasikartojantys profiliai';
+$_['heading_title']     = 'Apžvalgos';
 
 // Text
-$_['text_success']          = 'Atlikta: pasikartojantys profiliai sėkmingai pakeisti!';
-$_['text_list']             = 'Pasikartojančių profilių sąrašas';
-$_['text_add']              = 'Pridėti pasikartojantį profilį';
-$_['text_edit']             = 'Redaguoti pasikartojantį profilį';
-$_['text_day']				= 'Diena';
-$_['text_week']				= 'Savaitė';
-$_['text_semi_month']		= 'Pusę mėnesio';
-$_['text_month']			= 'Mėnuo';
-$_['text_year']				= 'Metai';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Pasikartojančios sumos yra suskaičiuojamos pagal dažnį ir ciklus.</p><p>Pavyzdžiui, jei naudojamas dažnumas "savaitė" ir ciklas "2", tuomet vartotojas bus apmokęstintas kas dvi savaites.</p><p>Trukmė yra skaičius kartų, kiek vartotojas mokės. Nustatykite 0, jei norite tęsti mokėjimus iki kols jie bus nutraukti.</p>';
-$_['text_profile']			= 'Periodinis profilis';
-$_['text_trial']			= 'Bandomasis profilis';
-
-// Entry
-$_['entry_name']		    = 'Vardas';
-$_['entry_price']			= 'Kaina';
-$_['entry_duration']		= 'Trukmė';
-$_['entry_cycle']			= 'Ciklas';
-$_['entry_frequency']		= 'Dažnis';
-$_['entry_trial_price']		= 'Bandymo kaina';
-$_['entry_trial_duration']	= 'Bandymo trukmė';
-$_['entry_trial_status']	= 'Bandymo būsena';
-$_['entry_trial_cycle']	    = 'Bandymo ciklas';
-$_['entry_trial_frequency'] = 'Bandymo dažnis';
-$_['entry_status']			= 'Statusas';
-$_['entry_sort_order']		= 'Rikiavimo tvarka';
+$_['text_success']      = 'Pavyko: Modifikavote atsiliepimus!';
+$_['text_list']         = 'Apžvalgų sąrašas';
+$_['text_add']          = 'Pridėti apžvalgą';
+$_['text_edit']         = 'Redaguoti apžvalgą';
 
 // Column
-$_['column_name']			= 'Vardas';
-$_['column_sort_order']	    = 'Rikiavimo tvarka';
-$_['column_action']         = 'Veiksmas';
+$_['column_product']    = 'Produktas';
+$_['column_author']     = 'Autorius';
+$_['column_rating']     = 'Įvertinimas';
+$_['column_status']     = 'Statusas';
+$_['column_date_added'] = 'Įkėlimo data';
+$_['column_action']     = 'Veiksmas';
+
+// Entry
+$_['entry_product']     = 'Produktas';
+$_['entry_author']      = 'Autorius';
+$_['entry_rating']      = 'Įvertinimas';
+$_['entry_status']      = 'Statusas';
+$_['entry_text']        = 'Tekstas';
+$_['entry_date_added']  = 'Įkėlimo data';
+
+// Help
+$_['help_product']      = '(Užbaigti automatiškai)';
 
 // Error
-$_['error_warning']         = 'Įspėjimas: Patikrinkite klaidas formoje!';
-$_['error_permission']		= 'Įspėjimas: neturite teisių keisti pasikartojančius profilius!';
-$_['error_name']			= 'Profilio pavadinimas turi būti didesnis nei 3 ir mažesnis nei 255 simboliai!';
-$_['error_product']			= 'Įspėjimas: šis pasikartojantis profilis negali būti ištrintas, kadangi yra priskirtas prie %s produktų!';
+$_['error_warning']     = 'Įspėjimas: Patikrinkite klaidas formoje!';
+$_['error_permission']  = 'Įspėjimas: Jūs neturite teisės redaguoti apžvalgas!';
+$_['error_product']     = 'Būtinas produktas!';
+$_['error_author']      = 'Autoriaus vardas turi būti tarp 3 ir 64 simbolių!';
+$_['error_text']        = 'Apžvalga turi būti ne trumpesnė, nei 1 simbolis!';
+$_['error_rating']      = 'Būtinas apžvalgos įvertinimas!';
