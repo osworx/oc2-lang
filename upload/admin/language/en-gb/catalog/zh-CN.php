2d255ab5bd424b5c537b7dd3ea860de4245e1b5d
OSWorX <info@osworx.net>
2025-03-16 11:09:45 +0100

    New translations information.php (Chinese Simplified)

--- upload/admin/language/en-gb/catalog/zh-CN.php
+++ upload/admin/language/en-gb/catalog/zh-CN.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = '筛选器';
+$_['heading_title']          = '信息管理';
 
 // Text
-$_['text_success']      = '您已经成功修改筛选器!';
-$_['text_list']         = '筛选列表';
-$_['text_add']          = '新增筛选';
-$_['text_edit']         = '编辑筛选';
+$_['text_success']           = '您已成功修改信息!';
+$_['text_list']              = '信息列表';
+$_['text_add']               = '添加信息';
+$_['text_edit']              = '编辑信息';
+$_['text_default']           = '默认';
 
 // Column
-$_['column_group']      = '筛选组';
-$_['column_sort_order'] = '排列顺序';
-$_['column_action']     = '操作';
+$_['column_title']           = '信息标题';
+$_['column_sort_order']	     = '排列顺序';
+$_['column_action']          = '操作';
 
 // Entry
-$_['entry_group']       = '筛选组名称';
-$_['entry_name']        = '筛选名称';
-$_['entry_sort_order']  = '排列顺序';
+$_['entry_title']            = '信息标题';
+$_['entry_description']      = '描述';
+$_['entry_store']            = '商店';
+$_['entry_meta_title'] 	     = '标签名';
+$_['entry_meta_keyword'] 	 = '关键词';
+$_['entry_meta_description'] = '标签描述';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = '底部';
+$_['entry_status']           = '状态';
+$_['entry_sort_order']       = '排列顺序';
+$_['entry_layout']           = '布局';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = '在页面底部显示.';
 
 // Error
-$_['error_warning']     = '警告:请仔细检查错误格式!';
-$_['error_permission']  = '警告:您没有权限修改此筛选器!';
-$_['error_group']       = '筛选组名称必须介于1-64个字符之间!';
-$_['error_name']        = '筛选名称必须介于1-64个字符之间!';
+$_['error_warning']          = '警告:请仔细检查错误格式!';
+$_['error_permission']       = '警告:您没有权限修改信息!';
+$_['error_title']            = '信息标题必须介于3-64个字符之间!';
+$_['error_description']      = '内容必须超过3个字符!';
+$_['error_meta_title']       = '元标题必须3-255个字符之间!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = '警告:该信息页面不能被删除,因为它目前正适用于商店帐户条款!';
+$_['error_checkout']         = '警告:该信息页面不能被删除,因为它目前被适用于商店结算条款!';
+$_['error_affiliate']        = '警告:该信息页面不能被删除,因为它目前被适用于商店加盟条款!';
+$_['error_return']           = '警告:该信息页面不能被删除,因为它目前正被适用于商店退货条款!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
