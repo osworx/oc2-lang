4ad3ebc73f800f0c796c35b1ffe733e2fa56043f
OSWorX <info@osworx.net>
2025-03-16 14:28:53 +0100

    New translations forgotten.php (Thai)

--- upload/admin/language/en-gb/common/th-TH.php
+++ upload/admin/language/en-gb/common/th-TH.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'ลืมรหัสผ่าน?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'เวอร์ชั่น %s';
+$_['text_forgotten']  = 'ลืมรหัสผ่าน';
+$_['text_your_email'] = 'อีเมล์ของคุณ';
+$_['text_email']      = 'ป้อนอยู่อีเมล์ที่เกี่ยวข้องกับบัญชีของคุณ คลิกส่งเพื่อส่งรหัสผ่านใหม่ไปยังเมลคุณ';
+$_['text_success']    = 'ส่งอีเมล์ยืนยันไปยังอีเมล์ admin ของคุณแล้ว';
+
+// Entry
+$_['entry_email']     = 'อีเมล์';
+$_['entry_password']  = 'รหัสผ่านใหม่';
+$_['entry_confirm']   = 'ยืนยัน';
+
+// Error
+$_['error_email']     = 'คำเตือน: ไม่พบอีเมล์นี้ในระบบ กรุณาลองใหม่อีกครั้ง!';
+$_['error_password']  = 'Password must be between 4 and 20 characters!';
+$_['error_confirm']   = 'รหัสผ่านและรหัสยืนยันไม่ตรงกัน!';
