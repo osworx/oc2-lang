32a2b94bb3dee734160fc1103c0a0d6d7facc532
OSWorX <info@osworx.net>
2025-03-16 11:17:01 +0100

    New translations filemanager.php (Vietnamese)

--- upload/admin/language/en-gb/common/vi-VN.php
+++ upload/admin/language/en-gb/common/vi-VN.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Bảng theo dõi';
+$_['heading_title']    = 'Quản lý ảnh';
+
+// Text
+$_['text_uploaded']    = 'Kết quả: Tập tin của bạn đã được tải lên!';
+$_['text_directory']   = 'Kết quả: Thư mục đã được tạo!';
+$_['text_delete']      = 'Kết quả: Tập tin hoặc thư mục của bạn đã được xóa!';
+
+// Entry
+$_['entry_search']     = 'Tìm..';
+$_['entry_folder']     = 'Tên thư mục';
 
 // Error
-$_['error_install'] = 'Chú ý: Thư mục cài đặt vẫn còn tồn tại và cần phải xóa vì lý do bảo mật!';
+$_['error_permission'] = 'Chú ý: Bị cấm!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Chú ý: File hoặc thư mục này bị trùng tên!';
+$_['error_directory']  = 'Chú ý: Thư mục này không có!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Chú ý: Kiểu file không đúng!';
+$_['error_upload']     = 'Chú ý: File này không thể tải lên vì một lý do chưa rõ!';
+$_['error_delete']     = 'Chú ý: Bạn không thể xóa thư mục này!';
