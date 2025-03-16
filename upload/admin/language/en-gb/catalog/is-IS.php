64ade0a03eea15cf14282659ff54f0630991cd61
OSWorX <info@osworx.net>
2025-03-16 14:17:55 +0100

    New translations category.php (Icelandic)

--- upload/admin/language/en-gb/catalog/is-IS.php
+++ upload/admin/language/en-gb/catalog/is-IS.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Eigindaflokkar';
+$_['heading_title']          = 'Vöruflokkar';
 
 // Text
-$_['text_success']      = 'Fínt: Þú hefur breytt eigindaflokkum!';
-$_['text_list']         = 'Listi Eigindaflokka';
-$_['text_add']          = 'Bæta við eigindaflokki';
-$_['text_edit']         = 'Breyta eigindaflokki';
+$_['text_success']           = 'Flott: Þú hefur breytt vöruflokkum';
+$_['text_list']              = 'Listi vöruflokka';
+$_['text_add']               = 'Bæta við vöruflokki';
+$_['text_edit']              = 'Breyta vöruflokki';
+$_['text_default']           = 'Sjálfgefið';
 
 // Column
-$_['column_name']       = 'Heiti eigindaflokks';
-$_['column_sort_order'] = 'Breytiröð';
-$_['column_action']     = 'Aðgerð';
+$_['column_name']            = 'Heiti vöruflokks';
+$_['column_sort_order']      = 'Breytiröð';
+$_['column_action']          = 'Aðgerð';
 
 // Entry
-$_['entry_name']        = 'Heiti eigindaflokks';
-$_['entry_sort_order']  = 'Breytiröð';
+$_['entry_name']             = 'Heiti vöruflokks';
+$_['entry_description']      = 'Lýsing';
+$_['entry_meta_title'] 	     = 'Meta Tag Title';
+$_['entry_meta_keyword']     = 'Meta Tag Keywords';
+$_['entry_meta_description'] = 'Meta Tag Description';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Aðalvöruflokkur';
+$_['entry_filter']           = 'Síur';
+$_['entry_store']            = 'Verslanir';
+$_['entry_image']            = 'Mynd';
+$_['entry_top']              = 'Efst';
+$_['entry_column']           = 'Dálkur';
+$_['entry_sort_order']       = 'Breytiröð';
+$_['entry_status']           = 'Staða';
+$_['entry_layout']           = 'Layout Override';
+
+// Help
+$_['help_filter']            = '(Autocomplete)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Display in the top menu bar. Only works for the top parent categories.';
+$_['help_column']            = 'Number of columns to use for the bottom 3 categories. Only works for the top parent categories.';
 
 // Error
-$_['error_warning']     = 'Aðvörun: Farðu vel yfir formið til að forðast villur!';
-$_['error_permission']  = 'Aðvörun: Þú hefur ekki heimild til að breyta eigindaflokki!';
-$_['error_name']        = 'Heiti eigindaflokks verður að vera 3 til 64 stafir!';
-$_['error_attribute']   = 'Aðvörun: Þessum eigindaflokki verður ekki breytt því hann er tengdur %s eigindum!';
-$_['error_product']     = 'Aðvörun: Þessum eigindaflokki verður ekki eytt því hann er tengdur %s vörum!';
+$_['error_warning']          = 'Aðvörun: Farðu vel yfir formið til að forðast villur!';
+$_['error_permission']       = 'Aðvörun: Þú hefur ekki leyfi til að breyta vöruflokkum!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Meta Title verður að vera 3 til 255 stafir!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
