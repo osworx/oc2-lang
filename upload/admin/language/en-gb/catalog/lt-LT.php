28250592c23b19e021c20d59865992efc4e9ce64
OSWorX <info@osworx.net>
2025-03-16 11:05:05 +0100

    New translations attribute.php (Lithuanian)

---
+++ upload/admin/language/en-gb/catalog/lt-LT.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Savybės';
+
+// Text
+$_['text_success']           = 'Atlikta: Jūs pakeitėte savybes!';
+$_['text_list']              = 'Savybių sąrašas';
+$_['text_add']               = 'Pridėti savybes';
+$_['text_edit']              = 'Redaguoti savybes';
+
+// Column
+$_['column_name']            = 'Savybės pavadinimas';
+$_['column_attribute_group'] = 'Savybės grupė';
+$_['column_sort_order']      = 'Rikiavimo tvarka';
+$_['column_action']          = 'Veiksmas';
+
+// Entry
+$_['entry_name']             = 'Savybės pavadinimas';
+$_['entry_attribute_group']  = 'Savybės grupė';
+$_['entry_sort_order']       = 'Rikiavimo tvarka';
+
+// Error
+$_['error_warning']          = 'Įspėjimas: Patikrinkite klaidas formoje!';
+$_['error_permission']       = 'Įspėjimas: Jūs neturite leidimo keisti savybes!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Savybės pavadinimas turi būti nuo 3 iki 64 simbolių!';
+$_['error_product']          = 'Įspėjimas: Ši savybė negali būti panaikinta, nes yra priskirta %s produktams!';
