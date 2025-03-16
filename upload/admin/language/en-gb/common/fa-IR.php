3953adf336b90365808c01bb22368e9b7d4853cb
OSWorX <info@osworx.net>
2025-03-16 11:17:06 +0100

    New translations filemanager.php (Persian)

--- upload/admin/language/en-gb/common/fa-IR.php
+++ upload/admin/language/en-gb/common/fa-IR.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'پیشخوان';
+$_['heading_title']    = 'مدیریت تصویر';
+
+// Text
+$_['text_uploaded']    = 'موفقیت: پرونده شما بارگذاری شده است!';
+$_['text_directory']   = 'موفقیت: دایرکتوری ایجاد شد!';
+$_['text_delete']      = 'موفقیت: پرونده یا دایرکتوری شما حذف شده است!';
+
+// Entry
+$_['entry_search']     = 'جستجو..';
+$_['entry_folder']     = 'نام پوشه';
 
 // Error
-$_['error_install'] = 'هشدار: پوشه نصب هنوز هم وجود دارد و باید به دلایل امنیتی حذف شود!';
+$_['error_permission'] = 'هشدار: دسترسی ممنوع است!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'هشدار: یک پرونده یا دایرکتوری با نام مشابه در حال حاضر وجود دارد!';
+$_['error_directory']  = 'هشدار: دایرکتوری وجود ندارد!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'هشدار: نوع پرونده نادرست!';
+$_['error_upload']     = 'هشدار: پرونده به دلیل ناشناخته نمی‌تواند بارگذاری شود!';
+$_['error_delete']     = 'هشدار: شما نمی‌توانید این دایرکتوری را حذف کنید!';
