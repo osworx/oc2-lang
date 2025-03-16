ce9e649b1761ab0b641138a0ae88f7ae05587d15
OSWorX <info@osworx.net>
2025-03-16 14:24:29 +0100

    New translations review.php (Azerbaijani)

--- upload/admin/language/en-gb/catalog/az-AZ.php
+++ upload/admin/language/en-gb/catalog/az-AZ.php
@@ -1,43 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'Yenilənən Profiller';
+$_['heading_title']     = 'Rəylər';
 
 // Text
-$_['text_success']          = 'Uğurlu: cateqoriya redaktə edildi!';
-$_['text_list']             = 'Yenilənən Profiller';
-$_['text_add']              = 'Profil əlavə et';
-$_['text_edit']             = 'Profil redaktə et';
-$_['text_day']				= 'Gün';
-$_['text_week']				= 'Həftə';
-$_['text_semi_month']		= 'Ayda iki dəfə olan';
-$_['text_month']			= 'Ay';
-$_['text_year']				= 'İl';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Yenilənən miqdarlar qarşılaşma sıxlığı ve dönüşüm miqdarına görə hesablanır. </p><p>Məsələn,  Qarşılaşma Sıxlığı olaraq "Həftə" və Dönüşüm Miqdarı olaraq "2" dəyərlərini istifadə etsəniz  istifadəçi fakturası 2 həftədə bir dəfə düzənlənir. </p><p>Zaan istifadəçinin ödəmə edəcəyi zaman vahidi miqdarı qədərdir, ödəmələri ləğv ediləcəyi zamana qədər gözlətmək istəsəniz bu dəyəri "0" olaraq təyin edin </p>';
-$_['text_profile']			= 'Yenilənən Profiller';
-$_['text_trial']			= 'Demo profil';
-
-// Entry
-$_['entry_name']		    = 'Ad';
-$_['entry_price']			= 'Qiymət';
-$_['entry_duration']		= 'Zaman';
-$_['entry_cycle']			= 'Döngü';
-$_['entry_frequency']		= 'Tezlik';
-$_['entry_trial_price']		= 'Sınaq qiymət';
-$_['entry_trial_duration']	= 'Sınaq üçün zaman';
-$_['entry_trial_status']	= 'Sınaq vəziyyəti';
-$_['entry_trial_cycle']	    = 'Sınaq döngüsü';
-$_['entry_trial_frequency'] = 'Sınaq sıxlıqı';
-$_['entry_status']			= 'Status';
-$_['entry_sort_order']		= 'Növlərə görə sırala';
+$_['text_success']      = 'Success: You have modified reviews!';
+$_['text_list']         = 'Review List';
+$_['text_add']          = 'Add Review';
+$_['text_edit']         = 'Edit Review';
 
 // Column
-$_['column_name']			= 'Ad';
-$_['column_sort_order']	    = 'Növlərə görə sırala';
-$_['column_action']         = 'Hərəkət';
+$_['column_product']    = 'Məhsul';
+$_['column_author']     = 'Author';
+$_['column_rating']     = 'Rating';
+$_['column_status']     = 'Status';
+$_['column_date_added'] = 'Əlavə Olunma Tarixi';
+$_['column_action']     = 'Hərəkət';
+
+// Entry
+$_['entry_product']     = 'Məhsul';
+$_['entry_author']      = 'Author';
+$_['entry_rating']      = 'Rating';
+$_['entry_status']      = 'Status';
+$_['entry_text']        = 'Mətn';
+$_['entry_date_added']  = 'Əlavə Olunma Tarixi';
+
+// Help
+$_['help_product']      = 'Avtomatik tamamlama';
 
 // Error
-$_['error_warning']         = 'Xəbərdarlıq: Xahiş edirik, formu göstərilən səhvlər səbəbiylə diqqətlə nəzarət edin!';
-$_['error_permission']		= 'Xəbərdarlıq: Kateqoriyaları dəyişdirmə icazəsinə sahib deyilsiniz!';
-$_['error_name']			= 'Məhsul adı 3-dən 255-dək simvol aralığında olmalıdır!';
-$_['error_product']			= 'Xəbərdar etmək: Bu əlamət silinə bilməz, necə ki bu hal-hazırda %s məhsullarına təyin edilir!
-';
+$_['error_warning']     = 'Xəbərdarlıq: Xahiş edirik, formu göstərilən səhvlər səbəbiylə diqqətlə nəzarət edin!';
+$_['error_permission']  = 'Warning: You do not have permission to modify reviews!';
+$_['error_product']     = 'Product required!';
+$_['error_author']      = 'Author must be between 3 and 64 characters!';
+$_['error_text']        = 'Review Text must be at least 1 character!';
+$_['error_rating']      = 'Review rating required!';
