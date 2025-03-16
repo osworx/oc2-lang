25215643c921c16db9cf4bba7520ca91a8396ca1
OSWorX <info@osworx.net>
2025-03-16 11:13:56 +0100

    New translations review.php (Swedish)

--- upload/admin/language/en-gb/catalog/sv-SE.php
+++ upload/admin/language/en-gb/catalog/sv-SE.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'Avbetalningsprofil';
+$_['heading_title']     = 'Omdömen';
 
 // Text
-$_['text_success']          = 'Klart: Du har ändrat avbetalningsplan!';
-$_['text_list']             = 'Avbetalningsplanslista';
-$_['text_add']              = 'Lägg till avbetalningsplan';
-$_['text_edit']             = 'Redigera avbetalningsplan';
-$_['text_day']				= 'Dag';
-$_['text_week']				= 'Vecka';
-$_['text_semi_month']		= '14 dagars';
-$_['text_month']			= 'Månad';
-$_['text_year']				= 'År';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Återkommande belopp beräknas på period och antal.</p><p>Till exempel om du använder en period med "vecka" och antal "2", då debiteras användaren var 14: e dag.</p><p>Avbetalningslängden är antalet gånger som användaren kommer att göra en betalning, ange detta till 0 om du vill ha betalningar tills de annulleras.</p>';
-$_['text_profile']			= 'Återkommande profiler';
-$_['text_trial']			= 'Avbetalningsprofil';
-
-// Entry
-$_['entry_name']		    = 'Namn';
-$_['entry_price']			= 'Pris';
-$_['entry_duration']		= 'Varaktighet';
-$_['entry_cycle']			= 'Cykel';
-$_['entry_frequency']		= 'Frekvens';
-$_['entry_trial_price']		= 'Prov pris';
-$_['entry_trial_duration']	= 'Avbetalningens längd';
-$_['entry_trial_status']	= 'Avbetalningens status';
-$_['entry_trial_cycle']	    = 'Avbetalningens antal';
-$_['entry_trial_frequency'] = 'Avbetalningsperiod';
-$_['entry_status']			= 'Status';
-$_['entry_sort_order']		= 'Sorteringsordning';
+$_['text_success']      = 'Klart: Du har ändrat recensioner!';
+$_['text_list']         = 'Recensionslista';
+$_['text_add']          = 'Lägg till recension';
+$_['text_edit']         = 'Redigera recension';
 
 // Column
-$_['column_name']			= 'Namn';
-$_['column_sort_order']	    = 'Sorteringsordning';
-$_['column_action']         = 'Åtgärd';
+$_['column_product']    = 'Produkt';
+$_['column_author']     = 'Skapare';
+$_['column_rating']     = 'Betyg';
+$_['column_status']     = 'Status';
+$_['column_date_added'] = 'Skapad';
+$_['column_action']     = 'Åtgärd';
+
+// Entry
+$_['entry_product']     = 'Produkt';
+$_['entry_author']      = 'Skapare';
+$_['entry_rating']      = 'Betyg';
+$_['entry_status']      = 'Status';
+$_['entry_text']        = 'Text';
+$_['entry_date_added']  = 'Skapad';
+
+// Help
+$_['help_product']      = '(Komplettera automatiskt)';
 
 // Error
-$_['error_warning']         = 'Varning: Kolla gärna formuläret noga vad det gäller fel!';
-$_['error_permission']		= 'Varning: Du har inte behörighet att ändra avbetalningsplan!';
-$_['error_name']			= 'Profilnamn måste vara större än 3 och mindre än 255 tecken!';
-$_['error_product']			= 'Varning: Denna avbetalningsplan kan inte raderas eftersom det för närvarande är tilldelad %s produkter!';
+$_['error_warning']     = 'Varning: Kolla gärna formuläret noga vad det gäller fel!';
+$_['error_permission']  = 'Varning: Du har inte behörighet att ändra recensioner!';
+$_['error_product']     = 'Produkt krävs!';
+$_['error_author']      = 'Skapare måste vara mellan 3 och 64 tecken långt!';
+$_['error_text']        = 'Recensionstexten måste vara minst 1 tecken!';
+$_['error_rating']      = 'Recensionsbetyg krävs!';
