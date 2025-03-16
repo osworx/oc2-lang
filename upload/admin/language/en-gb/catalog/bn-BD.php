6b57efd29aacb5810868ef1044aabab92ba0d2da
OSWorX <info@osworx.net>
2025-03-16 11:10:44 +0100

    New translations manufacturer.php (Bengali)

--- upload/admin/language/en-gb/catalog/bn-BD.php
+++ upload/admin/language/en-gb/catalog/bn-BD.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'তথ্য';
+$_['heading_title']     = 'Manufacturers';
 
 // Text
-$_['text_success']           = 'সাফল্য: আপনি তথ্য পরিবর্তন করেছেন!';
-$_['text_list']              = 'তথ্য তালিকা';
-$_['text_add']               = 'তথ্য সম্পাদনা করুন';
-$_['text_edit']              = 'তথ্য সম্পাদনা';
-$_['text_default']           = 'ডিফল্ট (পূর্ব-নির্ধারিত)';
+$_['text_success']      = 'Success: You have modified manufacturers!';
+$_['text_list']         = 'উত্পাদক';
+$_['text_add']          = 'নির্মাতার';
+$_['text_edit']         = 'Edit Manufacturer';
+$_['text_default']      = 'ডিফল্ট (পূর্ব-নির্ধারিত)';
+$_['text_percent']      = 'Percentage';
+$_['text_amount']       = 'Fixed Amount';
 
 // Column
-$_['column_title']           = 'তথ্য শিরোনাম';
-$_['column_sort_order']	     = 'সর্ট অর্ডার';
-$_['column_action']          = 'অ্যাকশনঃ';
+$_['column_name']       = 'Manufacturer Name';
+$_['column_sort_order'] = 'সর্ট অর্ডার';
+$_['column_action']     = 'অ্যাকশনঃ';
 
 // Entry
-$_['entry_title']            = 'তথ্য শিরোনাম';
-$_['entry_description']      = 'বিবরণ';
-$_['entry_store']            = 'ভাণ্ডার';
-$_['entry_meta_title'] 	     = 'মেটা ট্যাগ টাইটেল';
-$_['entry_meta_keyword'] 	 = 'মেটা ট্যাগ কীওয়ার্ডসমূহ';
-$_['entry_meta_description'] = 'মেটা ট্যাগ- বিবরণ';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'Bottom';
-$_['entry_status']           = 'অবস্থা';
-$_['entry_sort_order']       = 'সর্ট অর্ডার';
-$_['entry_layout']           = 'লেআউট ওভাররাইড';
+$_['entry_name']        = 'Manufacturer Name';
+$_['entry_store']       = 'ভাণ্ডার';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'ছবি';
+$_['entry_sort_order']  = 'সর্ট অর্ডার';
+$_['entry_type']        = 'Type';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'Display in the bottom footer.';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'সতর্কীকরণ: অনুগ্রহ করে, ফর্মের ভুলভ্রান্তি, মনোযোগ সহকারে দেখে নিন I!';
-$_['error_permission']       = 'Warning: You do not have permission to modify information!';
-$_['error_title']            = 'Information Title must be between 3 and 64 characters!';
-$_['error_description']      = 'Description must be more than 3 characters!';
-$_['error_meta_title']       = 'মেটা টাইটেল -এ, অবশ্যই, ৩ -এর বেশি ও ২৫৫ -র কম চিহ্ন থাকবে I!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'Warning: This information page cannot be deleted as it is currently assigned as the store account terms!';
-$_['error_checkout']         = 'Warning: This information page cannot be deleted as it is currently assigned as the store checkout terms!';
-$_['error_affiliate']        = 'Warning: This information page cannot be deleted as it is currently assigned as the store affiliate terms!';
-$_['error_return']           = 'Warning: This information page cannot be deleted as it is currently assigned as the store return terms!';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'সতর্কীকরণ: অনুগ্রহ করে, ফর্মের ভুলভ্রান্তি, মনোযোগ সহকারে দেখে নিন I!';
+$_['error_permission']  = 'Warning: You do not have permission to modify manufacturers!';
+$_['error_name']        = 'Manufacturer Name must be between 2 and 64 characters!';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'Warning: This manufacturer cannot be deleted as it is currently assigned to %s products!';
