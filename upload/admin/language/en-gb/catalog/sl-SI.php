99e148f0f92143314407ec2fe300174d14be42f8
OSWorX <info@osworx.net>
2025-03-16 14:17:46 +0100

    New translations category.php (Slovenian)

--- upload/admin/language/en-gb/catalog/sl-SI.php
+++ upload/admin/language/en-gb/catalog/sl-SI.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Skupine lastnosti';
+$_['heading_title']          = 'Kategorije';
 
 // Text
-$_['text_success']      = 'USPEŠNO: Ste uredili skupine lastnosti!';
-$_['text_list']         = 'Seznam skupin lastnosti';
-$_['text_add']          = 'Dodaj skupino lastnosti';
-$_['text_edit']         = 'Uredi skupino lastnosti';
+$_['text_success']           = 'USPEŠNO: Ste uredili kategorije!';
+$_['text_list']              = 'Seznam Kategorij';
+$_['text_add']               = 'Dodaj Kategorijo';
+$_['text_edit']              = 'Uredi Kategorijo';
+$_['text_default']           = 'Privzeto';
 
 // Column
-$_['column_name']       = 'Ime skupine lastnosti';
-$_['column_sort_order'] = 'Vrstni red';
-$_['column_action']     = 'Dejanje';
+$_['column_name']            = 'Ime Kategorije';
+$_['column_sort_order']      = 'Vrstni red';
+$_['column_action']          = 'Dejanje';
 
 // Entry
-$_['entry_name']        = 'Ime skupine lastnosti';
-$_['entry_sort_order']  = 'Vrstni red';
+$_['entry_name']             = 'Ime Kategorije';
+$_['entry_description']      = 'Opis';
+$_['entry_meta_title'] 	     = 'Meta naslov';
+$_['entry_meta_keyword']     = 'Meta ključne besede';
+$_['entry_meta_description'] = 'Meta opis';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Nadrejena kategorija';
+$_['entry_filter']           = 'Filtri';
+$_['entry_store']            = 'Trgovina';
+$_['entry_image']            = 'Slika';
+$_['entry_top']              = 'Zgoraj';
+$_['entry_column']           = 'Stolpci';
+$_['entry_sort_order']       = 'Vrstni red';
+$_['entry_status']           = 'Status';
+$_['entry_layout']           = 'Postavitev';
+
+// Help
+$_['help_filter']            = '(Samodokončanje)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Prikaz v zgornjem meniju. Deluje samo z nadrejenimi kategorijami.';
+$_['help_column']            = 'Število stolpcev za spodnje 3 kategorije. Deluje samo z nadrejenimi kategorijami.';
 
 // Error
-$_['error_warning']     = 'POZOR: Prosimo, pazljivo preverite polja za morebitne napake!';
-$_['error_permission']  = 'POZOR: Nimate dovoljenja za urejanje skupine lastnosti!';
-$_['error_name']        = 'Ime skupine lastnosti mora vsebovati med 3 in 64 znakov!';
-$_['error_attribute']   = 'POZOR: Te skupine lastnosti ne morete izbrisati, ker je trenutno vezana na %s lastnosti!';
-$_['error_product']     = 'POZOR: Te skupine lastnosti ne morete izbrisati, ker je trenutno vezana na %s izdelke!';
+$_['error_warning']          = 'POZOR: Prosimo, pazljivo preverite polja za morebitne napake!';
+$_['error_permission']       = 'POZOR: Nimate dovoljenja za urejanje kategorij!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Meta Naslov mora imeti več kot 3 in manj kot 255 znakov!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
