e86940359dc9e0d775e21e06251478b898a65f39
OSWorX <info@osworx.net>
2025-03-16 14:26:58 +0100

    New translations filemanager.php (Georgian)

--- upload/admin/language/en-gb/common/ka-GE.php
+++ upload/admin/language/en-gb/common/ka-GE.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'მთავარი პანელი';
+$_['heading_title']    = 'გამოსახულების მენეჯერი';
+
+// Text
+$_['text_uploaded']    = 'წარმატება: თქვენი ფაილი აიტვირთა!';
+$_['text_directory']   = 'წარმატება: დირექტორია შექმნილია!';
+$_['text_delete']      = 'წარმატება: თქვენი ფაილი ან დირექტორია წაშლილია!';
+
+// Entry
+$_['entry_search']     = 'ძებნა..';
+$_['entry_folder']     = 'ფოლდერის სახელი';
 
 // Error
-$_['error_install'] = 'გაფრთხილება: საინსტალაციო ფოლდერი ჯერ კიდევ არსებობს და უნდა წაიშალოს უსაფრთხოების მიზნით!';
+$_['error_permission'] = 'გაფრთხილება: ნებართვა აკრძალულია!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'გაფრთხილება: ფაილი ან დირექტორია იგივე სახელით უკვე არსებობს!';
+$_['error_directory']  = 'გაფრთხილება: დირექტორია არ არსებობს!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'გაფრთხილება: არასწორი ფაილის ტიპი!';
+$_['error_upload']     = 'გაფრთხილება: ფაილი არ აიტვირთვა უცნობი მიზეზით!';
+$_['error_delete']     = 'გაფრთხილება: თქვენ არ შეგიძლიათ წაშალოთ ეს დირექტორია!';
