f264377dac8bb2c659c566bcdd4de7df46efe9d5
OSWorX <info@osworx.net>
2025-03-16 11:17:25 +0100

    New translations footer.php (Belarusian)

--- upload/admin/language/en-gb/common/be-BY.php
+++ upload/admin/language/en-gb/common/be-BY.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Менеджэр выяў';
-
 // Text
-$_['text_uploaded']    = 'Поспех: Ваш файл загружаны!';
-$_['text_directory']   = 'Поспех: Дырэкторыя створана!';
-$_['text_delete']      = 'Поспех: Ваш файл / тэчка выдалена!';
-
-// Entry
-$_['entry_search']     = 'Пошук..';
-$_['entry_folder']     = 'Назва тэчкі';
-
-// Error
-$_['error_permission'] = 'Увага: Вы не маеце дазволу на гэтую аперацыю!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Увага: Файл / тэчка за такой назвай ўжо існуе!';
-$_['error_directory']  = 'Увага: Дырэкторыя не існуе!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Увага: Хібны тып файла!';
-$_['error_upload']     = 'Увага: Файл не можа быць загружаны па нявызначанай прычыне!';
-$_['error_delete']     = 'Увага: Вы не можаце выдаліць гэтую дырэкторыю!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Версія %s';
