5b98a5cce1e3b36c93b8e59383992da6b79eaa6d
OSWorX <info@osworx.net>
2025-03-16 14:16:51 +0100

    New translations attribute_group.php (Lithuanian)

--- upload/admin/language/en-gb/catalog/lt-LT.php
+++ upload/admin/language/en-gb/catalog/lt-LT.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Savybės';
+$_['heading_title']     = 'Savybių grupės';
 
 // Text
-$_['text_success']           = 'Atlikta: Jūs pakeitėte savybes!';
-$_['text_list']              = 'Savybių sąrašas';
-$_['text_add']               = 'Pridėti savybes';
-$_['text_edit']              = 'Redaguoti savybes';
+$_['text_success']      = 'Atlikta: Jūs pakeitėte savybių grupes!';
+$_['text_list']         = 'Savybių grupių sąrašas';
+$_['text_add']          = 'Pridėti savybių grupę';
+$_['text_edit']         = 'Redaguoti savybių grupę';
 
 // Column
-$_['column_name']            = 'Savybės pavadinimas';
-$_['column_attribute_group'] = 'Savybės grupė';
-$_['column_sort_order']      = 'Rikiavimo tvarka';
-$_['column_action']          = 'Veiksmas';
+$_['column_name']       = 'Savybių grupės pavadinimas';
+$_['column_sort_order'] = 'Rikiavimo tvarka';
+$_['column_action']     = 'Veiksmas';
 
 // Entry
-$_['entry_name']             = 'Savybės pavadinimas';
-$_['entry_attribute_group']  = 'Savybės grupė';
-$_['entry_sort_order']       = 'Rikiavimo tvarka';
+$_['entry_name']        = 'Savybių grupės pavadinimas';
+$_['entry_sort_order']  = 'Rikiavimo tvarka';
 
 // Error
-$_['error_warning']          = 'Įspėjimas: Patikrinkite klaidas formoje!';
-$_['error_permission']       = 'Įspėjimas: Jūs neturite leidimo keisti savybes!';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'Savybės pavadinimas turi būti nuo 3 iki 64 simbolių!';
-$_['error_product']          = 'Įspėjimas: Ši savybė negali būti panaikinta, nes yra priskirta %s produktams!';
+$_['error_warning']     = 'Įspėjimas: Patikrinkite klaidas formoje!';
+$_['error_permission']  = 'Įspėjimas: Jūs neturite teisių keisti savybių grupių!';
+$_['error_name']        = 'Savybių grupės pavadinimas turi būti nuo 3 iki 64 simbolių!';
+$_['error_attribute']   = 'Įspėjimas: Ši savybių grupė negali būti ištrinta, nes yra priskirta %s savybėms!';
+$_['error_product']     = 'Įspėjimas: Ši savybių grupė negali būti ištrinta, nes yra priskirta %s produktams!';
