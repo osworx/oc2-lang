059fc9370dd8861bdc541c46e32562a1cafcb4cd
OSWorX <info@osworx.net>
2025-03-16 11:12:51 +0100

    New translations recurring.php (Hebrew)

--- upload/admin/language/en-gb/catalog/he-IL.php
+++ upload/admin/language/en-gb/catalog/he-IL.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'מוצרים';
+$_['heading_title']			= 'פרופילי החזרות';
 
 // Text
-$_['text_success']           = 'הצלחה: שינית מוצרים!';
-$_['text_list']              = 'רשימת מוצרים';
-$_['text_add']               = 'הוסף מוצר';
-$_['text_edit']              = 'ערוך מוצר';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'ברירת המחדל';
-$_['text_option']            = 'אפשרות';
-$_['text_option_value']      = 'ערך אפשרות';
-$_['text_percent']           = 'אחוז';
-$_['text_amount']            = 'כמות קבועה';
-
-// Column
-$_['column_name']            = 'שם מוצר';
-$_['column_model']           = 'דגם';
-$_['column_image']           = 'תמונה';
-$_['column_price']           = 'מחיר';
-$_['column_quantity']        = 'כמות';
-$_['column_status']          = 'סטטוס';
-$_['column_action']          = 'פעולה';
+$_['text_success']          = 'הצלחה: שינית פרופילי החזרות!';
+$_['text_list']             = 'רשימת פרופילי החזרות';
+$_['text_add']              = 'הוסף פרופיל החזרה';
+$_['text_edit']             = 'ערוך פרופיל החזרה';
+$_['text_day']				= 'יום';
+$_['text_week']				= 'שבוע';
+$_['text_semi_month']		= 'חצי חודש';
+$_['text_month']			= 'חודש';
+$_['text_year']				= 'שנה';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> הסכומים מחושבים עפי "תדירות" ו "מחזוריות".</p><p>לדוגמה אם אתה משתמש ב "תדירות" של "שבוע" וב "מחזוריות" של "2", המשתמש יחויב כל 2 שבועות.</p><p>משך הזמן הוא מספר הפעמים שהמשתמש יבצע תשלום. עליך להגדיר את זה כ- 0, אם אתה רוצה שתשלומים ימשיכו עד שהם יתבטלו.</p>';
+$_['text_profile']			= 'פרופילי החזרות';
+$_['text_trial']			= 'פרופיל ניסיוני';
 
 // Entry
-$_['entry_name']             = 'שם מוצר';
-$_['entry_description']      = 'תיאור';
-$_['entry_meta_title'] 	     = 'מטא -תג כותרת';
-$_['entry_meta_keyword'] 	 = 'מטא -תג מילת מפתח';
-$_['entry_meta_description'] = 'מטא -תג תיאור';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'דגם';
-$_['entry_sku']              = 'מק\'\'ט';
-$_['entry_upc']              = 'UPC (קוד מוצר אוניברסלי)';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'מיקום';
-$_['entry_shipping']         = 'משלוח דרוש!';
-$_['entry_manufacturer']     = 'יצרן';
-$_['entry_store']            = 'חנויות';
-$_['entry_date_available']   = 'תאריך זמין';
-$_['entry_quantity']         = 'כמות';
-$_['entry_minimum']          = 'כמות מינימום';
-$_['entry_stock_status']     = 'מצב חסר במלאי';
-$_['entry_price']            = 'מחיר';
-$_['entry_tax_class']        = 'שיעור המס';
-$_['entry_points']           = 'נקודות';
-$_['entry_option_points']    = 'נקודות';
-$_['entry_subtract']         = 'הפחת מהמלאי';
-$_['entry_weight_class']     = 'סוג משקל';
-$_['entry_weight']           = 'משקל';
-$_['entry_dimension']        = 'מידות (ע / ר / ג)';
-$_['entry_length_class']     = 'סוג אורך';
-$_['entry_length']           = 'אורך';
-$_['entry_width']            = 'רוחב';
-$_['entry_height']           = 'גובה';
-$_['entry_image']            = 'תמונה';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'קבוצת לקוחות';
-$_['entry_date_start']       = 'תאריך התחלה';
-$_['entry_date_end']         = 'תאריך סיום';
-$_['entry_priority']         = 'עדיפות';
-$_['entry_attribute']        = 'תכונה';
-$_['entry_attribute_group']  = 'קבוצת תכונות';
-$_['entry_text']             = 'טקסט';
-$_['entry_option']           = 'אפשרות';
-$_['entry_option_value']     = 'ערך אפשרות';
-$_['entry_required']         = 'נדרש';
-$_['entry_status']           = 'סטטוס';
-$_['entry_sort_order']       = 'סדר מיון';
-$_['entry_category']         = 'קטגוריות';
-$_['entry_filter']           = 'פילטרים';
-$_['entry_download']         = 'הורדות';
-$_['entry_related']          = 'מוצרים קשורים';
-$_['entry_tag']          	 = 'תגיות המוצר';
-$_['entry_reward']           = 'נקודות מצטברות';
-$_['entry_layout']           = 'פריסת ביטול';
-$_['entry_recurring']        = 'פרופילי החזרות';
+$_['entry_name']		    = 'שם';
+$_['entry_price']			= 'מחיר';
+$_['entry_duration']		= 'משך זמן';
+$_['entry_cycle']			= 'מחזוריות';
+$_['entry_frequency']		= 'תדירות';
+$_['entry_trial_price']		= 'מחיר ניסיוני';
+$_['entry_trial_duration']	= 'משך זמן ניסיוני';
+$_['entry_trial_status']	= 'מצב ניסיוני';
+$_['entry_trial_cycle']	    = 'מחזוריות ניסיוני';
+$_['entry_trial_frequency'] = 'תדירות ניסיוני';
+$_['entry_status']			= 'סטטוס';
+$_['entry_sort_order']		= 'סדר מיון';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'יחידת שמירת מלאי';
-$_['help_upc']               = 'קוד מוצר אוניברסלי';
-$_['help_ean']               = 'מספר מאמר אירופי';
-$_['help_jan']               = 'מספר מאמר יפני';
-$_['help_isbn']              = 'מספר ספר סטנדרטי בינלאומי';
-$_['help_mpn']               = 'מספר חלק של היצרן';
-$_['help_manufacturer']      = '(השלמה אוטומטית)';
-$_['help_minimum']           = 'כפה ערך הזמנה מינימלית';
-$_['help_stock_status']      = 'המצב שמוצג כאשר המוצר אזל מהמלאי';
-$_['help_points']            = 'מספר הנקודות שצריך כדי לקנות את הפריט זה. אם אתה לא רוצה שניתן יהיה לרכוש את המוצר הזה עם נקודות עליך להשאיר כ- 0.';
-$_['help_category']          = '(השלמה אוטומטית)';
-$_['help_filter']            = '(השלמה אוטומטית)';
-$_['help_download']          = '(השלמה אוטומטית)';
-$_['help_related']           = '(השלמה אוטומטית)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'שם';
+$_['column_sort_order']	    = 'סדר מיון';
+$_['column_action']         = 'פעולה';
 
 // Error
-$_['error_warning']          = 'אזהרה: אנא בדוק בקפידה אם יש שגיאות בטופס!';
-$_['error_permission']       = 'אזהרה: אין לך הרשאה לשינוי מוצרים!';
-$_['error_name']             = 'שם המוצר חייב להיות גדול מ- 3 לפחות מ- 255 תווים!';
-$_['error_meta_title']       = 'מטה-תג הכותרת חייב להיות גדול מ- 3 לפחות מ- 255 תווים!';
-$_['error_model']            = 'דגם המוצר חייב להיות גדול מ 1 ל פחות מ-64 תווים!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'אזהרה: אנא בדוק בקפידה אם יש שגיאות בטופס!';
+$_['error_permission']		= 'אזהרה: אין לך הרשאה לשנות פרופילי החזרות!';
+$_['error_name']			= 'שם פרופיל חייב להיות גדול מ- 3 לפחות מ- 255 תווים!';
+$_['error_product']			= 'אזהרה: לא ניתן למחוק פרופילי החזרות מאחר והם מוקצים כרגע ל %s מוצרים!';
