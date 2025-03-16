6f8ec91e49bd1741f5f75da771c7cbec9f8bb671
OSWorX <info@osworx.net>
2025-03-16 11:04:53 +0100

    New translations attribute.php (Czech)

---
+++ upload/admin/language/en-gb/catalog/cs-CZ.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Atributy';
+
+// Text
+$_['text_success']           = 'Úspěch: Jste změnili atributy!';
+$_['text_list']              = 'Seznam atributů';
+$_['text_add']               = 'Přidat atribut';
+$_['text_edit']              = 'Upravit atribut';
+
+// Column
+$_['column_name']            = 'Název atributu';
+$_['column_attribute_group'] = 'Skupina atributů';
+$_['column_sort_order']      = 'Řadit podle';
+$_['column_action']          = 'Akce';
+
+// Entry
+$_['entry_name']             = 'Název atributu';
+$_['entry_attribute_group']  = 'Skupina atributů';
+$_['entry_sort_order']       = 'Řadit podle';
+
+// Error
+$_['error_warning']          = 'Upozornění: Zkontrolujte formulář pečlivě pro chyby!';
+$_['error_permission']       = 'Upozornění: Nemáte oprávnění ke změně atributů!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Název atributu musí být v rozmezí 3 až 64 znaků!';
+$_['error_product']          = 'Varování: Tento atribut nemůže být odstraněn, protože je v současnosti přiřazen k %s produktů!';
