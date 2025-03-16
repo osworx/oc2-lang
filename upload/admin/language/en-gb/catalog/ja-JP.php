bf28c196f7431064f09ab54c32696babc355b2bc
OSWorX <info@osworx.net>
2025-03-16 11:07:40 +0100

    New translations download.php (Japanese)

--- upload/admin/language/en-gb/catalog/ja-JP.php
+++ upload/admin/language/en-gb/catalog/ja-JP.php
@@ -1,46 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'カテゴリ';
+$_['heading_title']     = 'Downloads';
 
 // Text
-$_['text_success']           = '成功: カテゴリを変更しました!';
-$_['text_list']              = 'カテゴリ一覧';
-$_['text_add']               = 'カテゴリを追加';
-$_['text_edit']              = 'カテゴリを編集';
-$_['text_default']           = 'デフォルト';
+$_['text_success']      = 'Success: You have modified downloads!';
+$_['text_list']         = 'Download List';
+$_['text_add']          = 'Add Download';
+$_['text_edit']         = 'Edit Download';
+$_['text_upload']       = 'Your file was successfully uploaded!';
 
 // Column
-$_['column_name']            = 'カテゴリ名';
-$_['column_sort_order']      = '並べ替え順序';
-$_['column_action']          = 'アクション';
+$_['column_name']       = 'Download Name';
+$_['column_date_added'] = 'Date Added';
+$_['column_action']     = 'アクション';
 
 // Entry
-$_['entry_name']             = 'カテゴリ名';
-$_['entry_description']      = '説明';
-$_['entry_meta_title'] 	     = 'Meta タグのタイトル';
-$_['entry_meta_keyword']     = 'Meta タグのキーワード';
-$_['entry_meta_description'] = 'Meta タグの説明';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = '上位';
-$_['entry_filter']           = 'フィルタ';
-$_['entry_store']            = 'ストア';
-$_['entry_image']            = '画像';
-$_['entry_top']              = 'Top';
-$_['entry_column']           = 'Columns';
-$_['entry_sort_order']       = '並べ替え順序';
-$_['entry_status']           = 'Status';
-$_['entry_layout']           = 'Layout Override';
+$_['entry_name']        = 'Download Name';
+$_['entry_filename']    = 'Filename';
+$_['entry_mask']        = 'Mask';
 
 // Help
-$_['help_filter']            = '(Autocomplete)';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = 'Display in the top menu bar. Only works for the top parent categories.';
-$_['help_column']            = 'Number of columns to use for the bottom 3 categories. Only works for the top parent categories.';
+$_['help_filename']     = 'You can upload via the upload button or use FTP to upload to the download directory and enter the details below.';
+$_['help_mask']         = 'It is recommended that the filename and the mask are different to stop people trying to directly link to your downloads.';
 
 // Error
-$_['error_warning']          = 'Warning: Please check the form carefully for errors!';
-$_['error_permission']       = 'Warning: You do not have permission to modify categories!';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = 'Meta Title must be greater than 3 and less than 255 characters!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = 'Warning: Please check the form carefully for errors!';
+$_['error_permission']  = 'Warning: You do not have permission to modify downloads!';
+$_['error_name']        = 'Download Name must be between 3 and 64 characters!';
+$_['error_upload']      = 'Upload required!';
+$_['error_filename']    = 'Filename must be between 3 and 128 characters!';
+$_['error_exists']      = 'File does not exist!';
+$_['error_mask']        = 'Mask must be between 3 and 128 characters!';
+$_['error_filetype']    = 'Invalid file type!';
+$_['error_product']     = 'Warning: This download cannot be deleted as it is currently assigned to %s products!';
