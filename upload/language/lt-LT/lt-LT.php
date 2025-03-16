76c400556f1fdd7619b44e95600cc2c36b1ced23
OSWorX <info@osworx.net>
2025-03-16 09:11:21 +0100

    New translations filemanager.php (Lithuanian)

--- upload/language/lt-LT/lt-LT.php
+++ upload/language/lt-LT/lt-LT.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Prietaisų skydelis';
+$_['heading_title']    = 'Paveiksliukų tvarkytojas';
+
+// Text
+$_['text_uploaded']    = 'Atlikta: failas sėkmingai įkeltas!';
+$_['text_directory']   = 'Atlikta: aplankas sėkmingai sukurtas!';
+$_['text_delete']      = 'Atlikta: failas ar aplankas buvo ištrinta!';
+
+// Entry
+$_['entry_search']     = 'Paieška..';
+$_['entry_folder']     = 'Aplanko pavadinimas';
 
 // Error
-$_['error_install'] = 'Įspėjimas: diegimo aplankas vis dar egzistuoja ir turėtų būti panaikintas dėl saugumo priežasčių!';
+$_['error_permission'] = 'Įspėjimas: neturite leidimo!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Įspėjimas: failas ar aplankas tokiu pavadinimu jau yra!';
+$_['error_directory']  = 'Įspėjimas: aplankas neegzistuoja!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Įspėjimas: neteisingas failo tipas!';
+$_['error_upload']     = 'Įspėjimas: failas negali būti įkeltas dėl nežinomos priežasties!';
+$_['error_delete']     = 'Įspėjimas: Jūs negalite ištrinti šio aplanko!';
