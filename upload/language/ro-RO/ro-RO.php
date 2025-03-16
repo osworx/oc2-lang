215a369040122b016afb2d0d5b9e40ca3bf9663b
OSWorX <info@osworx.net>
2025-03-16 09:05:52 +0100

    New translations manufacturer.php (Romanian)

--- upload/language/ro-RO/ro-RO.php
+++ upload/language/ro-RO/ro-RO.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Pagini informații';
+$_['heading_title']     = 'Producători';
 
 // Text
-$_['text_success']           = 'Succes: Ai modificat pagina de informaţii!';
-$_['text_list']              = 'Lista paginilor de informații';
-$_['text_add']               = 'Adaugă pagină de informații';
-$_['text_edit']              = 'Modifică pagină de informații';
-$_['text_default']           = 'Implicit';
+$_['text_success']      = 'Succes: Ai modificat producatorii!';
+$_['text_list']         = 'Lista de Producători';
+$_['text_add']          = 'Adaugă Producător';
+$_['text_edit']         = 'Modifică Producător';
+$_['text_default']      = 'Implicit';
+$_['text_percent']      = 'Procent';
+$_['text_amount']       = 'Sumă fixă';
 
 // Column
-$_['column_title']           = 'Titlul paginii de informații';
-$_['column_sort_order']	     = 'Ordinea de sortare';
-$_['column_action']          = 'Acțiune';
+$_['column_name']       = 'Denumire producător';
+$_['column_sort_order'] = 'Ordinea de sortare';
+$_['column_action']     = 'Acțiune';
 
 // Entry
-$_['entry_title']            = 'Titlul paginii de informații';
-$_['entry_description']      = 'Descriere';
-$_['entry_store']            = 'Magazine';
-$_['entry_meta_title'] 	     = 'Meta tag: Titlu';
-$_['entry_meta_keyword'] 	 = 'Meta tag: cuvinte cheie';
-$_['entry_meta_description'] = 'Meta tag: descriere';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'Partea de jos';
-$_['entry_status']           = 'Stare';
-$_['entry_sort_order']       = 'Ordinea de sortare';
-$_['entry_layout']           = 'Suprascrie Layout-ul';
+$_['entry_name']        = 'Denumire producător';
+$_['entry_store']       = 'Magazine';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'imagine';
+$_['entry_sort_order']  = 'Ordinea de sortare';
+$_['entry_type']        = 'Tip';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'Afişare în subsol (footer).';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'Avertisment: Vă rugăm să verificaţi formularul pentru erori!';
-$_['error_permission']       = 'Avertisment: Nu aveţi permisiunile necesare pentru a modifica paginile de informaţiile!';
-$_['error_title']            = 'Titlu paginii de informaţii trebuie să aibă între 3 şi 64 de caractere!';
-$_['error_description']      = 'Descrierea trebuie să aibă cel pțin 3 caractere!';
-$_['error_meta_title']       = 'Meta tag : Titlu, trebuie să aibă mai mult de 3 şi mai puţin de 255 caractere!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'Avertisment: Această pagină de informaţii nu poate fi ştearsă deoarece este în asociată ca termeni de cont ai magazinului!';
-$_['error_checkout']         = 'Avertisment: Această pagină de informaţii nu poate fi ştearsă deoarece este în asociată ca termeni de plasare comandă ai magazinului!';
-$_['error_affiliate']        = 'Avertisment: Această pagină de informaţii nu poate fi ştearsă deoarece este în asociată ca termeni afiliere ai magazinului!';
-$_['error_return']           = 'Avertisment: Această pagină de informaţii nu poate fi ştearsă deoarece este în asociată ca termeni de returnare ai magazinului!';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'Avertisment: Vă rugăm să verificaţi formularul pentru erori!';
+$_['error_permission']  = 'Avertisment: Nu aveţi permisiunile necesare pentru a modifica producatorii!';
+$_['error_name']        = 'Denumirea producătorului trebuie să aibă între 2 și 64 de caractere!';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'Avertisment: Acest producător nu poate fi şters deoarece este asociat la %s produse!';
