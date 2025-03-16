bf0660d7ec1a81b261c91bb2f12796752805c250
OSWorX <info@osworx.net>
2025-03-16 11:10:19 +0100

    New translations manufacturer.php (Hungarian)

--- upload/admin/language/en-gb/catalog/hu-HU.php
+++ upload/admin/language/en-gb/catalog/hu-HU.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Információ';
+$_['heading_title']     = 'Gyártók';
 
 // Text
-$_['text_success']           = 'Siker: Módosított információ!';
-$_['text_list']              = 'Információ lista';
-$_['text_add']               = 'Információ hozzáadása';
-$_['text_edit']              = 'Információ szerkesztése';
-$_['text_default']           = 'Alapértelmezés';
+$_['text_success']      = 'Siker: Módosította a gyártókat!';
+$_['text_list']         = 'Gyártó lista';
+$_['text_add']          = 'Gyártó hozzáadása';
+$_['text_edit']         = 'Gyártó szerkesztése';
+$_['text_default']      = 'Alapértelmezés';
+$_['text_percent']      = 'Százalék';
+$_['text_amount']       = 'Fix összeg';
 
 // Column
-$_['column_title']           = 'Információ címe';
-$_['column_sort_order']	     = 'Rendezés';
-$_['column_action']          = 'Művelet';
+$_['column_name']       = 'Gyártó neve';
+$_['column_sort_order'] = 'Rendezés';
+$_['column_action']     = 'Művelet';
 
 // Entry
-$_['entry_title']            = 'Információ címe';
-$_['entry_description']      = 'Leírás';
-$_['entry_store']            = 'Áruházak';
-$_['entry_meta_title'] 	     = 'Meta Tag-cím';
-$_['entry_meta_keyword'] 	 = 'Meta Tag kulcsszó';
-$_['entry_meta_description'] = 'Meta-Tag leírás ';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'Oldal alja';
-$_['entry_status']           = 'Állapot';
-$_['entry_sort_order']       = 'Rendezés';
-$_['entry_layout']           = 'Elrendezés felülbírálása';
+$_['entry_name']        = 'Gyártó neve';
+$_['entry_store']       = 'Áruházak';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'Kép';
+$_['entry_sort_order']  = 'Rendezés';
+$_['entry_type']        = 'Típus';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'Az oldal ajlán jelenítse meg.';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'Figyelem: Kérjük, figyelmesen ellenőrizze a hibákat!';
-$_['error_permission']       = 'Figyelem: Nincs jogosultsága módosítani az információt!';
-$_['error_title']            = 'Az információ címe 3 és 64 karakter között kell lennie!';
-$_['error_description']      = 'A leírás legfeljebb 3 karakterből kell állnia!';
-$_['error_meta_title']       = 'A meta cím több mint 3, és kevesebb, mint 255 karakternek kell lennie!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'Figyelem: Ez az információs oldal nem törölhető, mert ez a jelenleg hozzá van rendelve a használati feltételekhez!';
-$_['error_checkout']         = 'Figyelem: Ez az információs oldal nem törölhető, mert ez a jelenleg hozzá van rendelve az üzleti feltételekhez!';
-$_['error_affiliate']        = 'Figyelem: Ez az információs oldal nem törölhető, mert ez a jelenleg hozzá van rendelve a partner feltételekhez!';
-$_['error_return']           = 'Figyelem: Ez az információs oldal nem törölhető, mert ez a jelenleg hozzá van rendelve a visszatérítési feltételekhez!';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'Figyelem: Kérjük, figyelmesen ellenőrizze a hibákat!';
+$_['error_permission']  = 'Figyelmeztetés: Nincs engedélye a gyártók módosításához!';
+$_['error_name']        = 'Gyártó nevének 2 és 64 karakter között kell lennie!';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'Figyelmeztetés: Ez a gyártó nem törölhető, mert jelenleg hozzá van rendelve a(z) %s termék(ek)!';
