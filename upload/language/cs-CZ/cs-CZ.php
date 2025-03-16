1b2668455d49e3073ef2a0148c07e1c8cbe68ad3
OSWorX <info@osworx.net>
2025-03-16 09:07:55 +0100

    New translations option.php (Czech)

--- upload/language/cs-CZ/cs-CZ.php
+++ upload/language/cs-CZ/cs-CZ.php
@@ -1,35 +1,40 @@
 <?php
 // Heading
-$_['heading_title']     = 'Výrobci';
+$_['heading_title']      = 'Možnosti';
 
 // Text
-$_['text_success']      = 'Úspěch: Upravili jste Výrobce!';
-$_['text_list']         = 'Seznam Výrobců';
-$_['text_add']          = 'Přidat výrobce';
-$_['text_edit']         = 'Upravit výrobce';
-$_['text_default']      = 'Výchozí';
-$_['text_percent']      = 'Procenta';
-$_['text_amount']       = 'Pevná částka';
+$_['text_success']       = 'Úspěch: Změnili jste možnosti!';
+$_['text_list']          = 'Seznam možností';
+$_['text_add']           = 'Přidat volbu';
+$_['text_edit']          = 'Editovat volbu';
+$_['text_choose']        = 'Zvolit';
+$_['text_select']        = 'Vybrat';
+$_['text_radio']         = 'Radio';
+$_['text_checkbox']      = 'Zaškrtávací políčko';
+$_['text_input']         = 'Vstup';
+$_['text_text']          = 'Text';
+$_['text_textarea']      = 'Textové pole';
+$_['text_file']          = 'Soubor';
+$_['text_date']          = 'Datum';
+$_['text_datetime']      = 'Datum &amp; čas';
+$_['text_time']          = 'Čas';
 
 // Column
-$_['column_name']       = 'Název Výrobce';
-$_['column_sort_order'] = 'Řadit podle';
-$_['column_action']     = 'Akce';
+$_['column_name']        = 'Jméno volby';
+$_['column_sort_order']  = 'Řadit podle';
+$_['column_action']      = 'Akce';
 
 // Entry
-$_['entry_name']        = 'Název Výrobce';
-$_['entry_store']       = 'Obchody';
-$_['entry_keyword']     = 'SEO URL';
-$_['entry_image']       = 'Obrázek';
-$_['entry_sort_order']  = 'Řadit podle';
-$_['entry_type']        = 'Typ';
-
-// Help
-$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['entry_name']         = 'Jméno volby';
+$_['entry_type']         = 'Typ';
+$_['entry_option_value'] = 'Název možnosti';
+$_['entry_image']        = 'Obrázek';
+$_['entry_sort_order']   = 'Řadit podle';
 
 // Error
-$_['error_warning']     = 'Upozornění: Zkontrolujte formulář pečlivě pro chyby!';
-$_['error_permission']  = 'Upozornění: Nemáte oprávnění ke změně Výrobců!';
-$_['error_name']        = 'Název Výrobce musí být v rozmezí 2 až 64 znaků!';
-$_['error_keyword']     = 'SEO URL already in use!';
-$_['error_product']     = 'Varování: Tento výrobce nemůže být odstraněn, protože je v současnosti přiřazen k %s produktů!';
+$_['error_warning']      = 'Upozornění: Zkontrolujte formulář pečlivě pro chyby!';
+$_['error_permission']   = 'Upozornění: Nemáte oprávnění ke změně možností!';
+$_['error_name']         = 'Název možnosti musí být v rozmezí 1 až 128 znaků!';
+$_['error_type']         = 'Upozornění: Možnost hodnoty požadované!';
+$_['error_option_value'] = 'Název hodnoty možností musí být mezi 1 až 128 znaků!';
+$_['error_product']      = 'Varování: Tato možnost nemůže být odstraněna, protože je v současnosti přiřazena k %s výrobků!';
