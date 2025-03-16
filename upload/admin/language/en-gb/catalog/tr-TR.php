94c73dc3edcff66a376e0a1155bdac7a2ef3ca2c
OSWorX <info@osworx.net>
2025-03-16 11:05:16 +0100

    New translations attribute.php (Turkish)

---
+++ upload/admin/language/en-gb/catalog/tr-TR.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Özellikler';
+
+// Text
+$_['text_success']           = 'Başarılı: Öznitelikleri düzenlediniz!';
+$_['text_list']              = 'Öznitelik Listesi';
+$_['text_add']               = 'Öznitelik Ekle';
+$_['text_edit']              = 'Öznitelik Düzenle';
+
+// Column
+$_['column_name']            = 'Öznitelik Adı';
+$_['column_attribute_group'] = 'Öznitelik Grubu';
+$_['column_sort_order']      = 'Sıralama Düzeni';
+$_['column_action']          = 'Eylem';
+
+// Entry
+$_['entry_name']             = 'Öznitelik Adı';
+$_['entry_attribute_group']  = 'Öznitelik Grubu';
+$_['entry_sort_order']       = 'Sıralama Düzeni';
+
+// Error
+$_['error_warning']          = 'Uyarı: Lütfen formu gösterilen hatalar nedeniyle dikkatlice kontrol edin!';
+$_['error_permission']       = 'Uyarı: Öznitelikleri değiştirme izniniz yok!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Öznitelik adı 3 ile 64 karakter arasında olmalıdır!';
+$_['error_product']          = 'Uyarı: Bu öznitelik halen %s ürününe atandığından silinemez!';
