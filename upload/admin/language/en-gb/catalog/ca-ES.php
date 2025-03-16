ed41b7aa8ce760a48fb2cfe733a9334e41a23a0c
OSWorX <info@osworx.net>
2025-03-16 14:15:51 +0100

    New translations attribute.php (Catalan)

--- upload/admin/language/en-gb/catalog/ca-ES.php
+++ upload/admin/language/en-gb/catalog/ca-ES.php
@@ -1,36 +1,27 @@
 <?php
 // Heading
-$_['heading_title']     = 'Opinions';
+$_['heading_title']          = 'Atributs';
 
 // Text
-$_['text_success']      = 'Èxit: Heu modificat les opinions!';
-$_['text_list']         = 'Review List';
-$_['text_add']          = 'Add Review';
-$_['text_edit']         = 'Edit Review';
+$_['text_success']           = 'Èxit: Heu modificat els atributs!';
+$_['text_list']              = 'Llista d\'atributs';
+$_['text_add']               = 'Afegeix un atribut';
+$_['text_edit']              = 'Edita els atributs';
 
 // Column
-$_['column_product']    = 'Producte';
-$_['column_author']     = 'Autor';
-$_['column_rating']     = 'Valoració';
-$_['column_status']     = 'Estat';
-$_['column_date_added'] = 'Date Added';
-$_['column_action']     = 'Acció';
+$_['column_name']            = 'Nom de l\'atribut';
+$_['column_attribute_group'] = 'Grup d\'atributs';
+$_['column_sort_order']      = 'Ordre de classificació';
+$_['column_action']          = 'Acció';
 
 // Entry
-$_['entry_product']     = 'Producte';
-$_['entry_author']      = 'Autor';
-$_['entry_rating']      = 'Valoració';
-$_['entry_status']      = 'Estat';
-$_['entry_text']        = 'Text';
-$_['entry_date_added']  = 'Date Added';
-
-// Help
-$_['help_product']      = '(Emplenar automàticament)';
+$_['entry_name']             = 'Nom de l\'atribut';
+$_['entry_attribute_group']  = 'Grup d\'atributs';
+$_['entry_sort_order']       = 'Ordre de classificació';
 
 // Error
-$_['error_warning']     = 'Advertència: Si us plau reviseu acuradament el formulari pels errors!';
-$_['error_permission']  = 'Advertència: No teniu permís per a modificar opinions!';
-$_['error_product']     = 'És requereix especificar Producte!';
-$_['error_author']      = 'Author ha de tenir entre 3 i 64 caràcters!';
-$_['error_text']        = 'L\'Opinió ha de tenir almenys 1 caràcter!';
-$_['error_rating']      = 'És requereix valoració de l\'opinió!';
+$_['error_warning']          = 'Advertència: Si us plau reviseu acuradament el formulari pels errors!';
+$_['error_permission']       = 'Advertència: No teniu permís per modificar els atributs!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'El nom de l\'atribut ha de ser entre 3 i 64 caràcters!';
+$_['error_product']          = 'Advertència: Aquests atributs no es poden suprimir com actualment està assignat a %s productes!';
