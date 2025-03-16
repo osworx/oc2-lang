96c6506b12dd9fbd5a1d3b61aee4b91562b29770
OSWorX <info@osworx.net>
2025-03-16 14:29:21 +0100

    New translations header.php (Korean)

--- upload/admin/language/en-gb/common/ko-KR.php
+++ upload/admin/language/en-gb/common/ko-KR.php
@@ -1,19 +1,23 @@
 <?php
-// header
-$_['heading_title']   = '비밀번호를 잊어버리셨나요?';
+// Heading
+$_['heading_title']          = '오픈카트';
 
 // Text
-$_['text_forgotten']  = '비밀번호 분실';
-$_['text_your_email'] = '이메일';
-$_['text_email']      = '사용자계정에 등록된 이메일을 입력하신 후, \'재설정\' 버튼을 누르시면 비밀번호 재설정 링크를 이메일로 보내드립니다.';
-$_['text_success']    = '확인 링크를 관리자 이메일로 보냈습니다.';
-
-// Entry
-$_['entry_email']     = '이메일';
-$_['entry_password']  = '새 비밀번호';
-$_['entry_confirm']   = '비밀번호 확인';
-
-// Error
-$_['error_email']     = '주의: 입력하신 이메일을 찾을 수 없습니다. 다시 시도해주세요.';
-$_['error_password']  = '비밀번호는 4~20 글자 이내로 입력해주세요.';
-$_['error_confirm']   = '비밀번호와 비밀번호 확인이 일치하지 않습니다.';
+$_['text_order']             = '주문';
+$_['text_processing_status'] = 'Processing';
+$_['text_complete_status']   = '완료';
+$_['text_customer']          = '고객';
+$_['text_online']            = '고객 접속정보';
+$_['text_approval']          = '승인 대기 중';
+$_['text_product']           = '상품';
+$_['text_stock']             = '품절';
+$_['text_review']            = '상품평';
+$_['text_return']            = '반품';
+$_['text_affiliate']         = '제휴사';
+$_['text_store']             = '쇼핑몰';
+$_['text_front']             = '쇼핑몰 프론트';
+$_['text_help']              = '도움말';
+$_['text_homepage']          = 'OpenCart Homepage';
+$_['text_support']           = '지원 포럼';
+$_['text_documentation']     = '문서';
+$_['text_logout']            = '로그아웃';
