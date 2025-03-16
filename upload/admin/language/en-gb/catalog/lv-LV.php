2fb0ed9f50d4d7b2fdb0364254f6b26c985c34e1
OSWorX <info@osworx.net>
2025-03-16 14:18:02 +0100

    New translations category.php (Latvian)

--- upload/admin/language/en-gb/catalog/lv-LV.php
+++ upload/admin/language/en-gb/catalog/lv-LV.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Atribūta Grupa';
+$_['heading_title']          = 'Sadaļas';
 
 // Text
-$_['text_success']      = 'Veiksmīgi: Jūs esat modificējis atribūtu grupas!';
-$_['text_list']         = 'Atribūtu grupu saraksts';
-$_['text_add']          = 'Pievienot atribūtu grupu';
-$_['text_edit']         = 'Atribūtu grupas rediģēšana';
+$_['text_success']           = 'Veiksmīgi: Jūs esat modificējis sadaļas!';
+$_['text_list']              = 'Sadaļu saraksts';
+$_['text_add']               = 'Pievienot sadaļu';
+$_['text_edit']              = 'Labot sadaļu';
+$_['text_default']           = 'Noklusējums';
 
 // Column
-$_['column_name']       = 'Atribūtu grupas nosaukums';
-$_['column_sort_order'] = 'Kārtošanas secība';
-$_['column_action']     = 'Darbība';
+$_['column_name']            = 'Sadaļas nosaukums';
+$_['column_sort_order']      = 'Kārtošanas secība';
+$_['column_action']          = 'Darbība';
 
 // Entry
-$_['entry_name']        = 'Atribūtu grupas nosaukums';
-$_['entry_sort_order']  = 'Kārtošanas secība';
+$_['entry_name']             = 'Sadaļas nosaukums';
+$_['entry_description']      = 'Apraksts';
+$_['entry_meta_title'] 	     = 'Meta Tag nosaukums';
+$_['entry_meta_keyword']     = 'Meta Tag atslēgvārdi';
+$_['entry_meta_description'] = 'Meta Tag apraksts';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Pamat';
+$_['entry_filter']           = 'Filtri';
+$_['entry_store']            = 'Veikali';
+$_['entry_image']            = 'Attēls';
+$_['entry_top']              = 'Augšā';
+$_['entry_column']           = 'Kolonnas';
+$_['entry_sort_order']       = 'Kārtošanas secība';
+$_['entry_status']           = 'Statuss';
+$_['entry_layout']           = 'Izkārtojuma ignorēšana';
+
+// Help
+$_['help_filter']            = '(Automātiskās pabeigšanas)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Parādīt top menu bar. Darbojas tikai top vecāku kategoriju.';
+$_['help_column']            = 'Izmantot grunts 3 kategoriju kolonnu skaitu. Darbojas tikai top vecāku kategoriju.';
 
 // Error
-$_['error_warning']     = 'Brīdinājums: Lūdzu pārbaudiet formu rūpīgi ,ir dažas kļūdas!';
-$_['error_permission']  = 'Brīdinājums: Jums nav tiesību labot atribūtu grupas!';
-$_['error_name']        = 'Atribūts grupas nosaukumā jābūt no 3 līdz 64 rakstzīmēm!';
-$_['error_attribute']   = 'Brīdinājums: Šo atribūtu grupu nevar dzēst, jo tā pašlaik ir piešķirts %s atribūtiem!';
-$_['error_product']     = 'Brīdinājums: Šo atribūtu grupu nevar dzēst, jo tā pašlaik ir piešķirts %s precēm!';
+$_['error_warning']          = 'Brīdinājums: Lūdzu pārbaudiet formu rūpīgi ,ir dažas kļūdas!';
+$_['error_permission']       = 'Brīdinājums: Jums nav tiesību modificēt kategorijas!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Galvenajā nosaukumā jābūt lielākam par 3 līdz mazāk nekā 255 rakstzīmes!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
