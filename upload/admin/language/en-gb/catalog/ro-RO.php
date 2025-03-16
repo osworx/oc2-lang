4b6d2169cda1939dadaff0955ad0676e174fbe2a
OSWorX <info@osworx.net>
2025-03-16 14:21:23 +0100

    New translations option.php (Romanian)

--- upload/admin/language/en-gb/catalog/ro-RO.php
+++ upload/admin/language/en-gb/catalog/ro-RO.php
@@ -1,35 +1,40 @@
 <?php
 // Heading
-$_['heading_title']     = 'Producători';
+$_['heading_title']      = 'Opțiuni';
 
 // Text
-$_['text_success']      = 'Succes: Ai modificat producatorii!';
-$_['text_list']         = 'Lista de Producători';
-$_['text_add']          = 'Adaugă Producător';
-$_['text_edit']         = 'Modifică Producător';
-$_['text_default']      = 'Implicit';
-$_['text_percent']      = 'Procent';
-$_['text_amount']       = 'Sumă fixă';
+$_['text_success']       = 'Felicitări: Ați modificat opțiunile!';
+$_['text_list']          = 'Listă de opțiuni';
+$_['text_add']           = 'Adaugă o opțiune';
+$_['text_edit']          = 'Modifică o opțiune';
+$_['text_choose']        = 'Alege';
+$_['text_select']        = 'Selectează';
+$_['text_radio']         = 'Radio';
+$_['text_checkbox']      = 'Checkbox';
+$_['text_input']         = 'Intrare';
+$_['text_text']          = 'Text';
+$_['text_textarea']      = 'Zonă de text';
+$_['text_file']          = 'Fișier';
+$_['text_date']          = 'Dată';
+$_['text_datetime']      = 'Dată &amp; Oră';
+$_['text_time']          = 'Ora';
 
 // Column
-$_['column_name']       = 'Denumire producător';
-$_['column_sort_order'] = 'Ordinea de sortare';
-$_['column_action']     = 'Acțiune';
+$_['column_name']        = 'Nume opțiune';
+$_['column_sort_order']  = 'Ordinea de sortare';
+$_['column_action']      = 'Acțiune';
 
 // Entry
-$_['entry_name']        = 'Denumire producător';
-$_['entry_store']       = 'Magazine';
-$_['entry_keyword']     = 'SEO URL';
-$_['entry_image']       = 'imagine';
-$_['entry_sort_order']  = 'Ordinea de sortare';
-$_['entry_type']        = 'Tip';
-
-// Help
-$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['entry_name']         = 'Nume opțiune';
+$_['entry_type']         = 'Tip';
+$_['entry_option_value'] = 'Numele valorii opțiunii';
+$_['entry_image']        = 'imagine';
+$_['entry_sort_order']   = 'Ordinea de sortare';
 
 // Error
-$_['error_warning']     = 'Avertisment: Vă rugăm să verificaţi formularul pentru erori!';
-$_['error_permission']  = 'Avertisment: Nu aveţi permisiunile necesare pentru a modifica producatorii!';
-$_['error_name']        = 'Denumirea producătorului trebuie să aibă între 2 și 64 de caractere!';
-$_['error_keyword']     = 'SEO URL already in use!';
-$_['error_product']     = 'Avertisment: Acest producător nu poate fi şters deoarece este asociat la %s produse!';
+$_['error_warning']      = 'Avertisment: Vă rugăm să verificaţi formularul pentru erori!';
+$_['error_permission']   = 'Atenție: Nu aveți permisiunea de a modifica opțiuni!';
+$_['error_name']         = 'Numele opțiunii trebuie să fie între 1 și 128 de caractere!';
+$_['error_type']         = 'Atenție: Valorile opțiunii sunt necesare!';
+$_['error_option_value'] = 'Numele valorii opțiunii trebuie să fie între 1 și 128 de caractere!';
+$_['error_product']      = 'Atenție: Acest program nu poate fi șters pentru că este asociat cu %s produse!';
