8e2f193ac12f75c3f26b0a3471f924915ea3f3f5
OSWorX <info@osworx.net>
2025-03-16 14:28:31 +0100

    New translations forgotten.php (Armenian)

--- upload/admin/language/en-gb/common/hy-AM.php
+++ upload/admin/language/en-gb/common/hy-AM.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Մոռացել եք Գաղտնաբառը?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Տարբերակ՝ %s';
+$_['text_forgotten']  = 'Մոռացված Գաղտանբառը';
+$_['text_your_email'] = 'Ձեր E-Mail Հասցեն';
+$_['text_email']      = 'Մուտքագրեք այն e-mail հասցեն որը վերաբերվում է Ձեր Պահոցին. Սեղմեք հաստատել, որից հետո էլ. փոստով Ձեզ կուղարկվի գաղտնաբառը վերականգնելու հղում.';
+$_['text_success']    = 'Հաստատման համար հղումը ուղարկվել է Ձեր Էլ. փոստի հասցեին.';
+
+// Entry
+$_['entry_email']     = 'E-Mail հասցե';
+$_['entry_password']  = 'Նոր գաղտնաբառ';
+$_['entry_confirm']   = 'Կրկնել գաղտնաբառը';
+
+// Error
+$_['error_email']     = 'Այդպիսի E-Mail Հասցե չի գտնվել, խնդրում ենք փորձել նորից!';
+$_['error_password']  = 'Password must be between 4 and 20 characters!';
+$_['error_confirm']   = 'Գաղտնաբառերը չեն համընկնում!';
