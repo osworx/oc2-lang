d167c6485d7f21ffac71155f69aa53dda3b25607
OSWorX <info@osworx.net>
2025-03-16 14:28:19 +0100

    New translations forgotten.php (Romanian)

--- upload/admin/language/en-gb/common/ro-RO.php
+++ upload/admin/language/en-gb/common/ro-RO.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'V-aţi uitat parola?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Versiunea %s';
+$_['text_forgotten']  = 'Parolă uitată';
+$_['text_your_email'] = 'Adresa de e-mail';
+$_['text_email']      = 'Introduceţi adresa de e-mail asociată contului. Faceţi clic pe Trimite pentru a vi se trimite prin e-mail un link de resetare a parolei.';
+$_['text_success']    = 'Un mesaj e-mail cu un link de confirmare v-a fost trimis la adresa de e-mail de admin.';
+
+// Entry
+$_['entry_email']     = 'Adresa de e-mail';
+$_['entry_password']  = 'Noua parola';
+$_['entry_confirm']   = 'Confirmare';
+
+// Error
+$_['error_email']     = 'Avertisment: Adresa de e-mail nu a fost găsită în evidenţele noastre, vă rugăm să încercaţi din nou!';
+$_['error_password']  = 'Parola trebuie să fie între 4 și 20 de caractere!';
+$_['error_confirm']   = 'Parola și confirmarea parolei nu coincid!';
