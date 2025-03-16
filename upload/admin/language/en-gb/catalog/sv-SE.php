f9d09dbde27f211256b41b3e84eac02b594e1763
OSWorX <info@osworx.net>
2025-03-16 11:10:34 +0100

    New translations manufacturer.php (Swedish)

--- upload/admin/language/en-gb/catalog/sv-SE.php
+++ upload/admin/language/en-gb/catalog/sv-SE.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Information';
+$_['heading_title']     = 'Tillverkare';
 
 // Text
-$_['text_success']           = 'Klart: Du har ändrat i "Information"!';
-$_['text_list']              = 'Informationslista';
-$_['text_add']               = 'Lägg till Information';
-$_['text_edit']              = 'Redigera information';
-$_['text_default']           = 'Standard';
+$_['text_success']      = 'Klart: Du har ändrat tillverkare!';
+$_['text_list']         = 'Lista över tillverkare';
+$_['text_add']          = 'Lägg till tillverkare';
+$_['text_edit']         = 'Redigera tillverkare';
+$_['text_default']      = 'Standard';
+$_['text_percent']      = 'Procentandel';
+$_['text_amount']       = 'Fast belopp';
 
 // Column
-$_['column_title']           = 'Rubrik';
-$_['column_sort_order']	     = 'Sorteringsordning';
-$_['column_action']          = 'Åtgärd';
+$_['column_name']       = 'Tillverkarens namn';
+$_['column_sort_order'] = 'Sorteringsordning';
+$_['column_action']     = 'Åtgärd';
 
 // Entry
-$_['entry_title']            = 'Rubrik';
-$_['entry_description']      = 'Beskrivning';
-$_['entry_store']            = 'Butiker';
-$_['entry_meta_title'] 	     = 'Metatagtitel';
-$_['entry_meta_keyword'] 	 = 'Metataggsökord';
-$_['entry_meta_description'] = 'Metataggbeskrivning';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'Nedtill';
-$_['entry_status']           = 'Status';
-$_['entry_sort_order']       = 'Sorteringsordning';
-$_['entry_layout']           = 'Layoutomställning';
+$_['entry_name']        = 'Tillverkarens namn';
+$_['entry_store']       = 'Butiker';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'Bild';
+$_['entry_sort_order']  = 'Sorteringsordning';
+$_['entry_type']        = 'Typ';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'Visas i sidfoten längst ner.';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'Varning: Kolla gärna formuläret noga vad det gäller fel!';
-$_['error_permission']       = 'Varning: Du har inte behörighet att ändra i "Information"!';
-$_['error_title']            = 'Rubriken måste innehålla mellan 3 och 64 tecken!';
-$_['error_description']      = 'Beskrivningen måste innehålla minst 3 tecken!';
-$_['error_meta_title']       = 'Metatitel måste vara större än 3 och mindre än 255 tecken!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'Varning: Den här informationssidan kan inte raderas eftersom den är kopplad till butikens kontovillkor!';
-$_['error_checkout']         = 'Varning: Den här informationssidan kan inte raderas eftersom den är kopplad till butikens köpvillkor!';
-$_['error_affiliate']        = 'Varning: Den här informationssidan kan inte raderas eftersom den är kopplad till butikens affiliatevillkor!';
-$_['error_return']           = 'Varning: Denna informationssida kan inte tas bort eftersom det är kopplad till butikens returvillkor!';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'Varning: Kolla gärna formuläret noga vad det gäller fel!';
+$_['error_permission']  = 'Varning: Du har inte behörighet att ändra tillverkare!';
+$_['error_name']        = 'Tillverkarens namn måste vara mellan 2 och 64 tecken långt!';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'Varning: Denna tillverkare kan inte raderas eftersom det för närvarande är tilldelad %s produkter!';
