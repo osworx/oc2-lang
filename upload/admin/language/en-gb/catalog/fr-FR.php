edc11fd258fdd948cbaa9e1d948ae46f07eba34c
OSWorX <info@osworx.net>
2025-03-16 11:08:20 +0100

    New translations filter.php (French)

--- upload/admin/language/en-gb/catalog/fr-FR.php
+++ upload/admin/language/en-gb/catalog/fr-FR.php
@@ -1,35 +1,25 @@
 <?php
 // Heading
-$_['heading_title']     = 'Téléchargements';
+$_['heading_title']     = 'Filtres';
 
 // Text
-$_['text_success']      = 'Modification de téléchargements réussie!';
-$_['text_list']         = 'Liste des téléchargements';
-$_['text_add']          = 'Ajouter un téléchargement';
-$_['text_edit']         = 'Modifier un téléchargement';
-$_['text_upload']       = 'Transfert du fichier réussi!';
+$_['text_success']      = 'Modification des filtres réussie !';
+$_['text_list']         = 'Liste des filtres';
+$_['text_add']          = 'Ajouter un filtre';
+$_['text_edit']         = 'Modifier un filtre';
 
 // Column
-$_['column_name']       = 'Nom du téléchargement';
-$_['column_date_added'] = 'Ajouté le';
+$_['column_group']      = 'Groupe de filtres';
+$_['column_sort_order'] = 'Classement';
 $_['column_action']     = 'Action';
 
 // Entry
-$_['entry_name']        = 'Nom du téléchargement';
-$_['entry_filename']    = 'Nom du fichier';
-$_['entry_mask']        = 'Masquer';
-
-// Help
-$_['help_filename']     = 'Vous pouvez transférer à l\'aide du bouton d\'envoi ou utiliser la convention FTP pour envoyer au répertoire de téléchargement et entrer les détails ci-dessous.';
-$_['help_mask']         = 'Il est recommandée de différencier le nom de fichier et son masque pour empêcher la création de liens directs vers vos téléchargements.';
+$_['entry_group']       = 'Nom de groupe de filtres';
+$_['entry_name']        = 'Nom de filtre';
+$_['entry_sort_order']  = 'Classement';
 
 // Error
 $_['error_warning']     = 'Attention : veuillez vérifier le formulaire';
-$_['error_permission']  = 'Avertissement: vous n\'êtes pas autorisé à modifier les téléchargements!';
-$_['error_name']        = 'Le nom du téléchargement doit contenir entre 3 et 64 caractères!';
-$_['error_upload']      = 'Transfert nécessaire!';
-$_['error_filename']    = 'Le nom de fichier doit contenir entre 3 et 128 caractères!';
-$_['error_exists']      = 'Fichier non existant!';
-$_['error_mask']        = 'La masque doit contenir entre 3 et 128 caractères!';
-$_['error_filetype']    = 'Type de fichier non valide!';
-$_['error_product']     = 'Avertissement: il est impossible d\'effacer ce téléchargement car il est actuellement alloué à %s produits!';
+$_['error_permission']  = 'Attention : vous n’êtes pas autorisé à modifier les filtres !';
+$_['error_group']       = 'Le nom du groupe de filtres doit contenir entre 1 et 64 caractères !';
+$_['error_name']        = 'Le nom du filtre doit contenir entre 1 et 64 caractères !';
