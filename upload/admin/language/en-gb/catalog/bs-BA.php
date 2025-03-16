d35e6dc284ffa5a0358d27f86e92d171e11e8d6f
OSWorX <info@osworx.net>
2025-03-16 14:23:43 +0100

    New translations recurring.php (Bosnian)

--- upload/admin/language/en-gb/catalog/bs-BA.php
+++ upload/admin/language/en-gb/catalog/bs-BA.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'Proizvodi';
+$_['heading_title']			= 'Ponavljajući profili';
 
 // Text
-$_['text_success']           = 'Uspjeh: Izmjena proizvoda!';
-$_['text_list']              = 'Lista proizvoda';
-$_['text_add']               = 'Dodavanje proizvoda';
-$_['text_edit']              = 'Uređivanje proizvoda';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'Zadano';
-$_['text_option']            = 'Opcija';
-$_['text_option_value']      = 'Vrijednost opcije';
-$_['text_percent']           = 'Postotak';
-$_['text_amount']            = 'Fiksni iznos';
-
-// Column
-$_['column_name']            = 'Naziv proizvoda';
-$_['column_model']           = 'Model';
-$_['column_image']           = 'Slika';
-$_['column_price']           = 'Cijena';
-$_['column_quantity']        = 'Količina';
-$_['column_status']          = 'Status';
-$_['column_action']          = 'Akcija';
+$_['text_success']          = 'Uspjeh: Izmjena ponavljajućih profila!';
+$_['text_list']             = 'Lista Ponavljajućih profila';
+$_['text_add']              = 'Dodajte ponavljajući profil';
+$_['text_edit']             = 'Uređivanje ponavljajućeg profila';
+$_['text_day']				= 'Dan';
+$_['text_week']				= 'Sedmica';
+$_['text_semi_month']		= 'Polu mjesec';
+$_['text_month']			= 'Mjesec';
+$_['text_year']				= 'Godina';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Recurring amounts are calculated by the frequency and cycles.</p><p>For example if you use a frequency of "week" and a cycle of "2", then the user will be billed every 2 weeks.</p><p>The duration is the number of times the user will make a payment, set this to 0 if you want payments until they are cancelled.</p>';
+$_['text_profile']			= 'Ponavljajući profil';
+$_['text_trial']			= 'Probni profil';
 
 // Entry
-$_['entry_name']             = 'Naziv proizvoda';
-$_['entry_description']      = 'Opis';
-$_['entry_meta_title'] 	     = 'Meta Tag naslov';
-$_['entry_meta_keyword'] 	 = 'Ključne riječi meta Tag';
-$_['entry_meta_description'] = 'Meta Tag opis';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'Model';
-$_['entry_sku']              = 'SKU';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'Lokacija';
-$_['entry_shipping']         = 'Zahtjev dostave';
-$_['entry_manufacturer']     = 'Proizvođač';
-$_['entry_store']            = 'Prodavnice';
-$_['entry_date_available']   = 'Datum dostupnosti';
-$_['entry_quantity']         = 'Količina';
-$_['entry_minimum']          = 'Minimalna količina';
-$_['entry_stock_status']     = 'Nema na stanju status';
-$_['entry_price']            = 'Cijena';
-$_['entry_tax_class']        = 'Porezni razred';
-$_['entry_points']           = 'Poeni';
-$_['entry_option_points']    = 'Poeni';
-$_['entry_subtract']         = 'Subtract Stock';
-$_['entry_weight_class']     = 'Težinska kategorija';
-$_['entry_weight']           = 'Težina';
-$_['entry_dimension']        = 'Dimenzije (D x Š x V)';
-$_['entry_length_class']     = 'Length Class';
-$_['entry_length']           = 'Dužina';
-$_['entry_width']            = 'Širina';
-$_['entry_height']           = 'Visina';
-$_['entry_image']            = 'Slika';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'Grupa kupaca';
-$_['entry_date_start']       = 'Datum početka';
-$_['entry_date_end']         = 'Datum završetka';
-$_['entry_priority']         = 'Prioritet';
-$_['entry_attribute']        = 'Atribut';
-$_['entry_attribute_group']  = 'Grupe Atributa';
-$_['entry_text']             = 'Tekst';
-$_['entry_option']           = 'Opcija';
-$_['entry_option_value']     = 'Vrijednost opcije';
-$_['entry_required']         = 'Obavezno';
-$_['entry_status']           = 'Status';
-$_['entry_sort_order']       = 'Redoslijed sortiranja';
-$_['entry_category']         = 'Kategorije';
-$_['entry_filter']           = 'Filteri';
-$_['entry_download']         = 'Preuzimanja';
-$_['entry_related']          = 'Srodni proizvodi';
-$_['entry_tag']          	 = 'Oznake proizvoda';
-$_['entry_reward']           = 'Skupljeni poeni';
-$_['entry_layout']           = 'Layout Override';
-$_['entry_recurring']        = 'Ponavljajući profil';
+$_['entry_name']		    = 'Ime';
+$_['entry_price']			= 'Cijena';
+$_['entry_duration']		= 'Trajanje';
+$_['entry_cycle']			= 'Ciklus';
+$_['entry_frequency']		= 'Frekvencija';
+$_['entry_trial_price']		= 'Probna cijena';
+$_['entry_trial_duration']	= 'Probno trajanje';
+$_['entry_trial_status']	= 'Probni status';
+$_['entry_trial_cycle']	    = 'Probni ciklus';
+$_['entry_trial_frequency'] = 'Probna frekvencija';
+$_['entry_status']			= 'Status';
+$_['entry_sort_order']		= 'Redoslijed sortiranja';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'Stock Keeping Unit';
-$_['help_upc']               = 'Univerzalna šifra proizvoda';
-$_['help_ean']               = 'Europski broj';
-$_['help_jan']               = 'Japanski broj artikla';
-$_['help_isbn']              = 'Međunarodni standardni Knjižni broj';
-$_['help_mpn']               = 'Kataloški broj Proizvođača';
-$_['help_manufacturer']      = '(Samodovršetak)';
-$_['help_minimum']           = 'Zahtjevaj minimalni iznos narudžbe';
-$_['help_stock_status']      = 'Stanje prikazano kada je proizvod rasprodan';
-$_['help_points']            = 'Broj bodova potreban za kupnju ove stavke. Ako ne želite kupiti ovaj proizvod s bodovima ostavite 0.';
-$_['help_category']          = '(Samodovršetak)';
-$_['help_filter']            = '(Samodovršetak)';
-$_['help_download']          = '(Samodovršetak)';
-$_['help_related']           = '(Samodovršetak)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'Ime';
+$_['column_sort_order']	    = 'Redoslijed sortiranja';
+$_['column_action']         = 'Akcija';
 
 // Error
-$_['error_warning']          = 'Upozorenje: Provjerite obrazac pažljivo radi grešaka!';
-$_['error_permission']       = 'Upozorenje: Nemate dozvolu za mijenjanje proizvoda!';
-$_['error_name']             = 'Naziv proizvoda mora biti veći od 3 i manji od 255 znakova!';
-$_['error_meta_title']       = 'Meta naslov mora biti veći od 3 i manji od 255 znakova!';
-$_['error_model']            = 'Model proizvoda mora biti veći od 1 i manji od 64 znakova!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'Upozorenje: Provjerite obrazac pažljivo radi grešaka!';
+$_['error_permission']		= 'Upozorenje: Nemate dozvolu za mijenjanje ponavljajućih profila!';
+$_['error_name']			= 'Naziv proizvoda mora biti veći od 3 i manji od 255 znakova!';
+$_['error_product']			= 'Upozorenje: Ovaj ponavljajući profil se ne može izbrisati jer je trenutno dodijeljen %s proizvoda!';
