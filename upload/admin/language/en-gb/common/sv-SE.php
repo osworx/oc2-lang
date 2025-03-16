b0da87e7777ee11c6765ceb94e73f5b5be46427f
OSWorX <info@osworx.net>
2025-03-16 11:20:13 +0100

    New translations login.php (Swedish)

--- upload/admin/language/en-gb/common/sv-SE.php
+++ upload/admin/language/en-gb/common/sv-SE.php
@@ -1,34 +1,19 @@
 <?php
-// Heading
-$_['heading_title']          = '$_[\'text_checkout\'] = \'Till kassan\';?>
-
-<? php
-// Text
-$_[\'text_hem\'] = \'Hem\';
-$_[\'text_önskelistan\'] = \'önskelistan (%s)\';
-$_[\'text_Kundvagn\'] = \'Shopping Cart\';
-$_[\'text_Sök\'] = \'Search\';
-$_[\'text_Välkommen\'] = \'Välkommen besökare du kan <a href="%s">login</a> or <a href="%s">Registrera Konto</a>.\';
-$_[\'text_loggas\'] = \'Du är inloggad som <a href="%s">%s</a> <b>(</b> <a href="%s">Logout</a> <b>)</b>\';
-$_[\'text_konto\'] = \'Mitt konto\';
-$_[\'text_checka ut\'] = \'checka ut\';?>';
+// header
+$_['heading_title']  = 'Administration';
 
 // Text
-$_['text_order']             = 'Order';
-$_['text_processing_status'] = 'Bearbetar';
-$_['text_complete_status']   = 'Slutförd';
-$_['text_customer']          = 'Kunder';
-$_['text_online']            = 'Kunder on-line';
-$_['text_approval']          = 'Väntar på godkännande';
-$_['text_product']           = 'Produkter';
-$_['text_stock']             = 'Ej i lager';
-$_['text_review']            = 'Omdömen';
-$_['text_return']            = 'Returer';
-$_['text_affiliate']         = 'Dotterbolag';
-$_['text_store']             = 'Butiker';
-$_['text_front']             = 'Butikens framsida';
-$_['text_help']              = 'Hjälp';
-$_['text_homepage']          = 'OpenCart Homepage';
-$_['text_support']           = 'Supportforum';
-$_['text_documentation']     = 'Dokumentation';
-$_['text_logout']            = 'Logga ut';
+$_['text_heading']   = 'Administration';
+$_['text_login']     = 'Ange dina inloggningsuppgifter.';
+$_['text_forgotten'] = 'Glömt lösenordet';
+
+// Entry
+$_['entry_username'] = 'Användarnamn';
+$_['entry_password'] = 'Lösenord';
+
+// Button
+$_['button_login']   = 'Logga in';
+
+// Error
+$_['error_login']    = 'Användarnamnet och/eller lösenordet finns inte.';
+$_['error_token']    = 'Utgången/förfallen token-session. Logga in igen.';
