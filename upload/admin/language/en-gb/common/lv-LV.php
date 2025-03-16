3153b07e931414fe224de4268adab098289aecd1
OSWorX <info@osworx.net>
2025-03-16 14:28:56 +0100

    New translations forgotten.php (Latvian)

--- upload/admin/language/en-gb/common/lv-LV.php
+++ upload/admin/language/en-gb/common/lv-LV.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Aizmirsi paroli?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Versija %s';
+$_['text_forgotten']  = 'Aizmirstā parole';
+$_['text_your_email'] = 'E-pasta adrese';
+$_['text_email']      = 'Ievadiet e-pasta adresi, kas saistīta ar jūsu kontu. Noklikšķiniet uz iesniegt, paroles atjaunošanas saiti pa e-pastu.';
+$_['text_success']    = 'E-pasts tika nosūtīts uz  e-pasta adresi ar apstiprinājuma saiti.';
+
+// Entry
+$_['entry_email']     = 'E-pasta adrese';
+$_['entry_password']  = 'Jauna Parole';
+$_['entry_confirm']   = 'Apstiprināt';
+
+// Error
+$_['error_email']     = 'Brīdinājums: E-pasta adrese netika atrasta mūsu datubāzē. Lūdzu mēģiniet vēlreiz!';
+$_['error_password']  = 'Password must be between 4 and 20 characters!';
+$_['error_confirm']   = 'Parole un paroles apstiprinājums nesaskan!';
