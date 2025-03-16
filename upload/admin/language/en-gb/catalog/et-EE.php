a6a6e04b70aa38ff1be7bc6cfca656a6db14ea9e
OSWorX <info@osworx.net>
2025-03-16 14:24:27 +0100

    New translations review.php (Estonian)

--- upload/admin/language/en-gb/catalog/et-EE.php
+++ upload/admin/language/en-gb/catalog/et-EE.php
@@ -1,42 +1,36 @@
 <?php
 // Heading
-$_['heading_title']			= 'Korduvad profiilid';
+$_['heading_title']     = 'Ülevaated';
 
 // Text
-$_['text_success']          = 'Success: You have modified recurring profiles!';
-$_['text_list']             = 'Recurring Profile List';
-$_['text_add']              = 'Add Recurring Profile';
-$_['text_edit']             = 'Edit Recurring Profile';
-$_['text_day']				= 'Päev';
-$_['text_week']				= 'nädal';
-$_['text_semi_month']		= 'Pool kuud';
-$_['text_month']			= 'Month';
-$_['text_year']				= 'Year';
-$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i> Recurring amounts are calculated by the frequency and cycles.</p><p>For example if you use a frequency of "week" and a cycle of "2", then the user will be billed every 2 weeks.</p><p>The duration is the number of times the user will make a payment, set this to 0 if you want payments until they are cancelled.</p>';
-$_['text_profile']			= 'Korduvad profiilid';
-$_['text_trial']			= 'Trial Profile';
-
-// Entry
-$_['entry_name']		    = 'Name';
-$_['entry_price']			= 'Hind';
-$_['entry_duration']		= 'Duration';
-$_['entry_cycle']			= 'Cycle';
-$_['entry_frequency']		= 'Frequency';
-$_['entry_trial_price']		= 'Trial price';
-$_['entry_trial_duration']	= 'Trial duration';
-$_['entry_trial_status']	= 'Trial status';
-$_['entry_trial_cycle']	    = 'Trial cycle';
-$_['entry_trial_frequency'] = 'Trial frequency';
-$_['entry_status']			= 'Staatus';
-$_['entry_sort_order']		= 'Sorteerimis järjekord';
+$_['text_success']      = 'Oled edukalt muutnud ülevaadet!';
+$_['text_list']         = 'Ülevaate nimekiri';
+$_['text_add']          = 'Lisa ülevaade';
+$_['text_edit']         = 'Muuda ülevaadet';
 
 // Column
-$_['column_name']			= 'Name';
-$_['column_sort_order']	    = 'Sorteerimis järjekord';
-$_['column_action']         = 'Tegevus';
+$_['column_product']    = 'Toode';
+$_['column_author']     = 'Autor';
+$_['column_rating']     = 'Hinnang';
+$_['column_status']     = 'Staatus';
+$_['column_date_added'] = 'Lisamise kuupäev';
+$_['column_action']     = 'Tegevus';
+
+// Entry
+$_['entry_product']     = 'Toode';
+$_['entry_author']      = 'Autor';
+$_['entry_rating']      = 'Hinnang';
+$_['entry_status']      = 'Staatus';
+$_['entry_text']        = 'Tekst';
+$_['entry_date_added']  = 'Lisamise kuupäev';
+
+// Help
+$_['help_product']      = '(Automaatselt lõpetatud)';
 
 // Error
-$_['error_warning']         = 'Hoiatus: Palun kontrollige vormi vead hoolikalt üle!';
-$_['error_permission']		= 'Warning: You do not have permission to modify recurring profiles!';
-$_['error_name']			= 'Profile Name must be greater than 3 and less than 255 characters!';
-$_['error_product']			= 'Warning: This recurring profile cannot be deleted as it is currently assigned to %s products!';
+$_['error_warning']     = 'Hoiatus: Palun kontrollige vormi vead hoolikalt üle!';
+$_['error_permission']  = 'Hoiatus: Sul ei ole õigusi ülevaadete muutmiseks!';
+$_['error_product']     = 'Toode nõutud!';
+$_['error_author']      = 'Autori väli peab olema 3 kuni 64 tähemärki pikk!';
+$_['error_text']        = 'Ülevaate tekst peab olema vähemalt üks tähemärki pikk!';
+$_['error_rating']      = 'Ülevaate hinnang nõutud!';
