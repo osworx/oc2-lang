4a2fbf433f361e982949159dac7c85ae3bae4e06
OSWorX <info@osworx.net>
2025-03-16 14:19:53 +0100

    New translations information.php (Catalan)

--- upload/admin/language/en-gb/catalog/ca-ES.php
+++ upload/admin/language/en-gb/catalog/ca-ES.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'Filtres';
+$_['heading_title']          = 'Informació';
 
 // Text
-$_['text_success']      = 'Success: You have modified filters!';
-$_['text_list']         = 'Filter List';
-$_['text_add']          = 'Add Filter';
-$_['text_edit']         = 'Edit Filter';
+$_['text_success']           = 'Èxit: Heu modificat la informació!';
+$_['text_list']              = 'Information List';
+$_['text_add']               = 'Add Information';
+$_['text_edit']              = 'Edit Information';
+$_['text_default']           = 'Predeterminat';
 
 // Column
-$_['column_group']      = 'Filter Group';
-$_['column_sort_order'] = 'Ordre de classificació';
-$_['column_action']     = 'Acció';
+$_['column_title']           = 'Títol de la Informació';
+$_['column_sort_order']	     = 'Ordre de classificació';
+$_['column_action']          = 'Acció';
 
 // Entry
-$_['entry_group']       = 'Filter Group Name';
-$_['entry_name']        = 'Filter Name';
-$_['entry_sort_order']  = 'Ordre de classificació';
+$_['entry_title']            = 'Títol de la Informació';
+$_['entry_description']      = 'Descripció';
+$_['entry_store']            = 'Botigues:';
+$_['entry_meta_title'] 	     = 'Títol de l\'etiqueta meta';
+$_['entry_meta_keyword'] 	 = 'Meta Paraules Clau Tag';
+$_['entry_meta_description'] = 'Meta Tag Descripció';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Bottom';
+$_['entry_status']           = 'Estat';
+$_['entry_sort_order']       = 'Ordre de classificació';
+$_['entry_layout']           = 'Disposició En Passar Damunt';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Display in the bottom footer.';
 
 // Error
-$_['error_warning']     = 'Advertència: Si us plau reviseu acuradament el formulari pels errors!';
-$_['error_permission']  = 'Warning: You do not have permission to modify filters!';
-$_['error_group']       = 'Filter Group Name must be between 1 and 64 characters!';
-$_['error_name']        = 'Filter Name must be between 1 and 64 characters!';
+$_['error_warning']          = 'Advertència: Si us plau reviseu acuradament el formulari pels errors!';
+$_['error_permission']       = 'Warning: No teniu permís per a modificar la informació!';
+$_['error_title']            = 'El Títol de la Informació ha de tenir entre 3 i 64 caràcters!';
+$_['error_description']      = 'La Descripció ha de tenir entre 3 i 128 caràcters!';
+$_['error_meta_title']       = 'El Títol Meta ha de ser major que 3 i menor de 255 caràcters!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'Advertència: Aquesta Pàgina d\'Informació no pot ser eliminada ja que conté els Termes d\'Us de: Comptes!';
+$_['error_checkout']         = 'Advertència: Aquesta Pàgina d\'Informació no pot ser eliminada ja que conté els Termes d\'Us de: Caixa!!';
+$_['error_affiliate']        = 'Advertència: Aquesta Pàgina d\'Informació no pot ser eliminada ja que conté els Termes d\'Us de: Afiliats!!';
+$_['error_return']           = 'Warning: This information page cannot be deleted as it is currently assigned as the store return terms!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
