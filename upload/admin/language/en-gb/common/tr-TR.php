70a2d195e84ce58c612a4c24cd10101394294863
OSWorX <info@osworx.net>
2025-03-16 11:18:36 +0100

    New translations forgotten.php (Turkish)

--- upload/admin/language/en-gb/common/tr-TR.php
+++ upload/admin/language/en-gb/common/tr-TR.php
@@ -1,4 +1,19 @@
 <?php
+// header
+$_['heading_title']   = 'Parolanızı mı unuttunuz?';
+
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] = 'Sürüm %s';
+$_['text_forgotten']  = 'Parolamı Unuttum';
+$_['text_your_email'] = 'E-Posta Adresiniz:';
+$_['text_email']      = 'Kayıt olurken yazdığınız E-Posta adresinizi yazınız. Şifreniz E-Posta adresinize gönderilecektir.';
+$_['text_success']    = 'Yeni parolanız E-Posta adresinize gönderildi.';
+
+// Entry
+$_['entry_email']     = 'E-Posta Adresi:';
+$_['entry_password']  = 'Yeni Parola';
+$_['entry_confirm']   = 'Onayla';
+
+// Error
+$_['error_email']     = 'Uyarı: E-Posta adresi bulunamadı. Lütfen tekrar deneyiniz!';
+$_['error_password']  = 'Şifre 4 ile 20 karakter arasında olmalıdır!';
+$_['error_confirm']   = 'Parola ve parola onayı eşleşmiyor!';
