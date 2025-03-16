c34a0b2c60283cdb533c4da5250083df4f211d5b
OSWorX <info@osworx.net>
2025-03-16 14:20:46 +0100

    New translations manufacturer.php (Hebrew)

--- upload/admin/language/en-gb/catalog/he-IL.php
+++ upload/admin/language/en-gb/catalog/he-IL.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'מידע';
+$_['heading_title']     = 'יצרנים';
 
 // Text
-$_['text_success']           = 'הצלחה: שינית מידע!';
-$_['text_list']              = 'רשימת מידע';
-$_['text_add']               = 'הוסף מידע';
-$_['text_edit']              = 'ערוך מידע';
-$_['text_default']           = 'ברירת המחדל';
+$_['text_success']      = 'הצלחה: שינית יצרנים!';
+$_['text_list']         = 'רשימת יצרנים';
+$_['text_add']          = 'הוסף יצרן';
+$_['text_edit']         = 'ערוך יצרן';
+$_['text_default']      = 'ברירת המחדל';
+$_['text_percent']      = 'אחוז';
+$_['text_amount']       = 'כמות קבועה';
 
 // Column
-$_['column_title']           = 'כותרת מידע';
-$_['column_sort_order']	     = 'סדר מיון';
-$_['column_action']          = 'פעולה';
+$_['column_name']       = 'שם היצרן';
+$_['column_sort_order'] = 'סדר מיון';
+$_['column_action']     = 'פעולה';
 
 // Entry
-$_['entry_title']            = 'כותרת מידע';
-$_['entry_description']      = 'תיאור';
-$_['entry_store']            = 'חנויות';
-$_['entry_meta_title'] 	     = 'מטא -תג כותרת';
-$_['entry_meta_keyword'] 	 = 'מטא -תג מילת מפתח';
-$_['entry_meta_description'] = 'מטא -תג תיאור';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'תחתית';
-$_['entry_status']           = 'סטטוס';
-$_['entry_sort_order']       = 'סדר מיון';
-$_['entry_layout']           = 'פריסת ביטול';
+$_['entry_name']        = 'שם היצרן';
+$_['entry_store']       = 'חנויות';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'תמונה';
+$_['entry_sort_order']  = 'סדר מיון';
+$_['entry_type']        = 'סוג';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'להציג בפוטר התחתון.';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'אזהרה: אנא בדוק בקפידה אם יש שגיאות בטופס!';
-$_['error_permission']       = 'אזהרה: אין לך הרשאה לשנות מידע!';
-$_['error_title']            = 'כותרת מידע חייב להיות בין 3 ל 64 תווים!';
-$_['error_description']      = 'תיאור חייב להיות יותר מ 3 תווים!';
-$_['error_meta_title']       = 'מטה-תג הכותרת חייב להיות גדול מ- 3 לפחות מ- 255 תווים!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'אזהרה: אין אפשרות למחוק את דף המידע הזה מאחר והוא מוקצה כרגע כתנאים בחשבון החנות!';
-$_['error_checkout']         = 'אזהרה: אין אפשרות למחוק את דף המידע הזה מאחר והוא מוקצה כרגע כתנאים בקופת החנות!';
-$_['error_affiliate']        = 'אזהרה: אין אפשרות למחוק את דף המידע הזה מאחר והוא מוקצה כרגע כתנאים בסוכנים של החנות!';
-$_['error_return']           = 'אזהרה: אין אפשרות למחוק את דף המידע הזה מאחר והוא מוקצה כרגע כתנאים בהחזרות החנות!';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'אזהרה: אנא בדוק בקפידה אם יש שגיאות בטופס!';
+$_['error_permission']  = 'אזהרה: אין לך הרשאה לשינוי יצרנים!';
+$_['error_name']        = 'שם היצרן חייב להיות בין 2 ל- 64 תווים!';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'אזהרה: לא ניתן למחוק את היצרן הזה מאחר והוא מוקצה כרגע ל %s מוצרים!';
