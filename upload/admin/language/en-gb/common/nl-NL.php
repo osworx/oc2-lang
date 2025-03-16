bb48264a44c0133c0ef60366b31b7612fa480b6c
OSWorX <info@osworx.net>
2025-03-16 14:27:01 +0100

    New translations filemanager.php (Dutch)

--- upload/admin/language/en-gb/common/nl-NL.php
+++ upload/admin/language/en-gb/common/nl-NL.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Recente Activiteiten';
+$_['heading_title']    = 'Afbeelding Beheer';
+
+// Text
+$_['text_uploaded']    = 'Gefeliciteerd: U bestand is geüpload. ';
+$_['text_directory']   = 'Gefeliciteerd: Map aangemaakt. ';
+$_['text_delete']      = 'Gefeliciteerd: U bestand of map is verwijderd.';
+
+// Entry
+$_['entry_search']     = 'Zoeken..';
+$_['entry_folder']     = 'Naam van de map';
 
 // Error
-$_['error_install'] = 'Waarschuwing: De installatie map bestaat nog! Verwijder deze map i.v.m.. de veiligheid!';
+$_['error_permission'] = 'Opgelet: Toestemming geweigerd!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Opgelet: Een bestand of map met dezelfde naam bestaat reeds.';
+$_['error_directory']  = 'Opgelet: Map bestaat niet.';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Opgelet: Onjuiste bestandstype.';
+$_['error_upload']     = 'Opgelet: Bestand kon niet geüpload worden voor onbekende redenen.';
+$_['error_delete']     = 'Opgelet: U kan deze map niet verwijderen.';
