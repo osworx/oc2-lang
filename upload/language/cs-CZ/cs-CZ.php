60531d4b58bcc91a9ee89407b48b4934e5be89cf
OSWorX <info@osworx.net>
2025-03-16 09:07:50 +0100

    New translations category.php (Czech)

--- upload/language/cs-CZ/cs-CZ.php
+++ upload/language/cs-CZ/cs-CZ.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Skupina atributů';
+$_['heading_title']          = 'Kategorie';
 
 // Text
-$_['text_success']      = 'Úspěch: Jste změnili atribut skupiny!';
-$_['text_list']         = 'Seznam skupiny atributů';
-$_['text_add']          = 'Přidat skupinu atributů';
-$_['text_edit']         = 'Upravit skupiny atributů';
+$_['text_success']           = 'Úspěch: Upravili jste kategorie!';
+$_['text_list']              = 'Seznam kategorií';
+$_['text_add']               = 'Přidat kategorii';
+$_['text_edit']              = 'Upravit kategorii';
+$_['text_default']           = 'Výchozí';
 
 // Column
-$_['column_name']       = 'Název skupiny atributů';
-$_['column_sort_order'] = 'Řadit podle';
-$_['column_action']     = 'Akce';
+$_['column_name']            = 'Název kategorie';
+$_['column_sort_order']      = 'Řadit podle';
+$_['column_action']          = 'Akce';
 
 // Entry
-$_['entry_name']        = 'Název skupiny atributů';
-$_['entry_sort_order']  = 'Řadit podle';
+$_['entry_name']             = 'Název kategorie';
+$_['entry_description']      = 'Popis';
+$_['entry_meta_title'] 	     = 'Meta Tag Title';
+$_['entry_meta_keyword']     = 'Klíčová slova Meta Tag';
+$_['entry_meta_description'] = 'Meta Tag Description';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Rodič';
+$_['entry_filter']           = 'Filtry';
+$_['entry_store']            = 'Obchody';
+$_['entry_image']            = 'Obrázek';
+$_['entry_top']              = 'Top';
+$_['entry_column']           = 'Sloupce';
+$_['entry_sort_order']       = 'Řadit podle';
+$_['entry_status']           = 'Stav';
+$_['entry_layout']           = 'Přepsání layoutu';
+
+// Help
+$_['help_filter']            = '(Funkce Automatické dokončování)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Zobrazit v horním menu. Funguje pouze pro horní nadřazené kategorie.';
+$_['help_column']            = 'Počet sloupců pro spodní 3 kategorie. Funguje pouze pro horní nadřazené kategorie.';
 
 // Error
-$_['error_warning']     = 'Upozornění: Zkontrolujte formulář pečlivě pro chyby!';
-$_['error_permission']  = 'Upozornění: Nemáte oprávnění ke změně skupiny atributů!';
-$_['error_name']        = 'Název skupiny atributů musí být 3 až 64 znaků!';
-$_['error_attribute']   = 'Varování: Tato skupina atribut nemůže být odstraněny, protože je aktuálně přiřazeno atributů %s!';
-$_['error_product']     = 'Varování: Tato skupina atributů nemůže být odstraněna, protože je v současnosti přiřazena k %s výrobků!';
+$_['error_warning']          = 'Upozornění: Zkontrolujte formulář pečlivě pro chyby!';
+$_['error_permission']       = 'Upozornění: Nemáte oprávnění ke změně kategorií!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Meta Title musí být větší než 3 a méně než 255 znaků!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
