1ed37ddb7c37bf8e3348ae71374fd8d6fc7c9ea6
OSWorX <info@osworx.net>
2025-03-16 11:06:46 +0100

    New translations category.php (Lithuanian)

--- upload/admin/language/en-gb/catalog/lt-LT.php
+++ upload/admin/language/en-gb/catalog/lt-LT.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Savybių grupės';
+$_['heading_title']          = 'Kategorijos';
 
 // Text
-$_['text_success']      = 'Atlikta: Jūs pakeitėte savybių grupes!';
-$_['text_list']         = 'Savybių grupių sąrašas';
-$_['text_add']          = 'Pridėti savybių grupę';
-$_['text_edit']         = 'Redaguoti savybių grupę';
+$_['text_success']           = 'Atlikta: Jūs pakeitėte kategorijas!';
+$_['text_list']              = 'Kategorijų sąrašas';
+$_['text_add']               = 'Pridėti kategoriją';
+$_['text_edit']              = 'Redaguoti kategoriją';
+$_['text_default']           = 'Numatytasis';
 
 // Column
-$_['column_name']       = 'Savybių grupės pavadinimas';
-$_['column_sort_order'] = 'Rikiavimo tvarka';
-$_['column_action']     = 'Veiksmas';
+$_['column_name']            = 'Kategorijos pavadinimas';
+$_['column_sort_order']      = 'Rikiavimo tvarka';
+$_['column_action']          = 'Veiksmas';
 
 // Entry
-$_['entry_name']        = 'Savybių grupės pavadinimas';
-$_['entry_sort_order']  = 'Rikiavimo tvarka';
+$_['entry_name']             = 'Kategorijos pavadinimas';
+$_['entry_description']      = 'Aprašymas';
+$_['entry_meta_title'] 	     = 'Meta Tago pavadinimas';
+$_['entry_meta_keyword']     = 'Meta Tag Raktažodžiai';
+$_['entry_meta_description'] = 'Meta Tag Aprašymas';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Pirminis';
+$_['entry_filter']           = 'Filtrai';
+$_['entry_store']            = 'Parduotuvės';
+$_['entry_image']            = 'Paveikslėlis';
+$_['entry_top']              = 'Viršus';
+$_['entry_column']           = 'Stulpeliai';
+$_['entry_sort_order']       = 'Rikiavimo tvarka';
+$_['entry_status']           = 'Statusas';
+$_['entry_layout']           = 'Šablono valdymas';
+
+// Help
+$_['help_filter']            = '(Užbaigti automatiškai)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Rodyti viršutiniame meniu. Veikia tik su aukščiausio lygio kategorijomis.';
+$_['help_column']            = 'Stulpelių skaičius apatinėms 3 kategorijoms. Veikia tik su aukščiausio lygio kategorijoms.';
 
 // Error
-$_['error_warning']     = 'Įspėjimas: Patikrinkite klaidas formoje!';
-$_['error_permission']  = 'Įspėjimas: Jūs neturite teisių keisti savybių grupių!';
-$_['error_name']        = 'Savybių grupės pavadinimas turi būti nuo 3 iki 64 simbolių!';
-$_['error_attribute']   = 'Įspėjimas: Ši savybių grupė negali būti ištrinta, nes yra priskirta %s savybėms!';
-$_['error_product']     = 'Įspėjimas: Ši savybių grupė negali būti ištrinta, nes yra priskirta %s produktams!';
+$_['error_warning']          = 'Įspėjimas: Patikrinkite klaidas formoje!';
+$_['error_permission']       = 'Įspėjimas: Jūs neturite teisės redaguoti kategorijas!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Meta pavadinimas turi būti ilgesnis kaip 3 ir trumpesnis kaip 255 simboliai!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
