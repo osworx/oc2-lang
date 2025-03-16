6adb6b99b4d1c65dee799020f66f6686fac82141
OSWorX <info@osworx.net>
2025-03-16 11:10:23 +0100

    New translations manufacturer.php (Georgian)

--- upload/admin/language/en-gb/catalog/ka-GE.php
+++ upload/admin/language/en-gb/catalog/ka-GE.php
@@ -1,45 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'ინფორმაცია';
+$_['heading_title']     = 'მწარმოებლები';
 
 // Text
-$_['text_success']           = 'წარმატება: თქვენი ინფორმაცია შეცვლილია!';
-$_['text_list']              = 'ინფორმაციის სია';
-$_['text_add']               = 'ინფორმაციის დამატება';
-$_['text_edit']              = 'ინფორმაციის რედაქტირება';
-$_['text_default']           = 'სტანდარტი';
+$_['text_success']      = 'წარმატება: თქვენს მიერ რედაქტირებულია მწარმოებლები!';
+$_['text_list']         = 'მწარმოებლების სია';
+$_['text_add']          = 'მწარმოებელის დამატება';
+$_['text_edit']         = 'მწარმოებელის რედაქტირება';
+$_['text_default']      = 'სტანდარტი';
+$_['text_percent']      = 'პროცენტი';
+$_['text_amount']       = 'ფიქსირებული თანხა';
 
 // Column
-$_['column_title']           = 'ინფორმაციის აღწერა';
-$_['column_sort_order']	     = 'შეკვეთის სორტირება';
-$_['column_action']          = 'მოქმედება';
+$_['column_name']       = 'მწარმოებლის სახელი';
+$_['column_sort_order'] = 'შეკვეთის სორტირება';
+$_['column_action']     = 'მოქმედება';
 
 // Entry
-$_['entry_title']            = 'ინფორმაციის აღწერა';
-$_['entry_description']      = 'აღწერა';
-$_['entry_store']            = 'მაღაზიები';
-$_['entry_meta_title'] 	     = 'მეტა ტეგი სათაური';
-$_['entry_meta_keyword'] 	 = 'მეტა ტეგი საკვანძო სიტყვები';
-$_['entry_meta_description'] = 'მეტა ტეგი აღწერა';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_bottom']           = 'ქვევით';
-$_['entry_status']           = 'სტატუსი';
-$_['entry_sort_order']       = 'შეკვეთის სორტირება';
-$_['entry_layout']           = 'მაკეტის გარდაქმნა';
+$_['entry_name']        = 'მწარმოებლის სახელი';
+$_['entry_store']       = 'მაღაზიები';
+$_['entry_keyword']     = 'SEO URL';
+$_['entry_image']       = 'სურათი';
+$_['entry_sort_order']  = 'შეკვეთის სორტირება';
+$_['entry_type']        = 'სახეობა';
 
 // Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_bottom']            = 'ქვედა ძირის ჩვენება.';
+$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
 
 // Error
-$_['error_warning']          = 'გაფრთხილება: გთხოვთ ყურადღებით შეამოწმოთ შეცდომების ფორმები!';
-$_['error_permission']       = 'გაფრთხილება: თქვენ არ გაქვთ უფლება შეცვალოთ ინფორმაცია';
-$_['error_title']            = 'ინფორმაციის სათაური უნდა იყოს 3-დან 64 სიმბოლოს';
-$_['error_description']      = 'აღწერა უნდა იყოს 3 სიმბოლოზე მეტი!';
-$_['error_meta_title']       = 'მეტა სათაური უნდა იყოს მეტი 3 და ნაკლები 255 სიმბოლო!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_account']          = 'გაფრთხილება: ამ ინფორმაციის წაშლა ვერ მოხდება, რადგან მიმდინარედ განსაზღვრულია როგორც ანგარიშების მონაცემთა ბაზის ადგილად!';
-$_['error_checkout']         = 'გაფრთხილება: ამ ინფორმაციის წაშლა ვერ მოხდება, რადგან მიმდინარედ განსაზღვრულია როგორც სალაროს მონაცემთა ბაზის ადგილად!';
-$_['error_affiliate']        = 'გაფრთხილება: ამ ინფორმაციის წაშლა ვერ მოხდება, რადგან მიმდინარედ განსაზღვრულია როგორც მაღაზიის პარტნიორების მონაცემთა ბაზის ადგილად!';
-$_['error_return']           = 'გაფრთხილება: ამ ინფორმაციის წაშლა ვერ მოხდება, რადგან მიმდინარედ განსაზღვრულია როგორც მაღაზიის დაბრუნების პირობების მონაცემთა ბაზის ადგილად!';
-$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
+$_['error_warning']     = 'გაფრთხილება: გთხოვთ ყურადღებით შეამოწმოთ შეცდომების ფორმები!';
+$_['error_permission']  = 'გაფრთხილება: თქვენ არ გაქვთ უფლება შეცვალოთ მწარმოებლები!';
+$_['error_name']        = 'მწარმოებელი დასახელება უნდა იყოს 2 და 64 სიმბოლო!';
+$_['error_keyword']     = 'SEO URL already in use!';
+$_['error_product']     = 'გაფრთხილება:  მწარმოებლის წაშლა ვერ მოხდება, რადგან იგი ამჟამად ენიჭება %s პროდუქციას!';
