eec8df5924c232db83c0dc0ec0495913e8a07040
OSWorX <info@osworx.net>
2025-03-16 11:07:05 +0100

    New translations category.php (Portuguese, Brazilian)

--- upload/admin/language/en-gb/catalog/pt-BR.php
+++ upload/admin/language/en-gb/catalog/pt-BR.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Grupos de atributos';
+$_['heading_title']          = 'Categorias';
 
 // Text
-$_['text_success']      = 'Sucesso: Você modificou grupos de atributos!';
-$_['text_list']         = 'Lista de grupo de atributo';
-$_['text_add']          = 'Adicionar o grupo de atributo';
-$_['text_edit']         = 'Editar atributo de grupo';
+$_['text_success']           = 'Sucesso: Você modificou as categorias!';
+$_['text_list']              = 'Lista de Categorias';
+$_['text_add']               = 'Adicionar Categoria';
+$_['text_edit']              = 'Editar Categoria';
+$_['text_default']           = 'Padrão';
 
 // Column
-$_['column_name']       = 'Nome do grupo de atributo';
-$_['column_sort_order'] = 'Ordem de Classificação';
-$_['column_action']     = 'Ação';
+$_['column_name']            = 'Nome da Categoria';
+$_['column_sort_order']      = 'Ordem de Classificação';
+$_['column_action']          = 'Ação';
 
 // Entry
-$_['entry_name']        = 'Nome do grupo de atributo';
-$_['entry_sort_order']  = 'Ordem de Classificação';
+$_['entry_name']             = 'Nome da Categoria';
+$_['entry_description']      = 'Descrição';
+$_['entry_meta_title'] 	     = 'Titulo Meta Tag';
+$_['entry_meta_keyword']     = 'Palavras-Chave Meta Tag';
+$_['entry_meta_description'] = 'Descrição Meta Tag';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Principal';
+$_['entry_filter']           = 'Filtros';
+$_['entry_store']            = 'Lojas';
+$_['entry_image']            = 'Imagem';
+$_['entry_top']              = 'Topo';
+$_['entry_column']           = 'Colunas';
+$_['entry_sort_order']       = 'Ordem de Classificação';
+$_['entry_status']           = 'Estado';
+$_['entry_layout']           = 'Substituição de layout';
+
+// Help
+$_['help_filter']            = '(Auto-Completa)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Exibir na barra de menu superior. Só funciona para as categorias  principais.';
+$_['help_column']            = 'Número de colunas a serem usadas para as 3 categorias de fundo. Só funciona para as categorias principais.';
 
 // Error
-$_['error_warning']     = 'Aviso: Por favor, verifique a forma cuidadosamente para evitar erros!';
-$_['error_permission']  = 'Aviso: Você não tem permissão para modificar os atributos!';
-$_['error_name']        = 'Nome do grupo de atributo deve estar entre 3 e 64 caracteres!';
-$_['error_attribute']   = 'Aviso: Este grupo de atributos não pode ser excluído, pois esta  atualmente atribuído a atributos de %s!';
-$_['error_product']     = 'Aviso: Este atributo não pode ser excluído porque esta atualmente atribuído a %s produtos!';
+$_['error_warning']          = 'Aviso: Por favor, verifique a forma cuidadosamente para evitar erros!';
+$_['error_permission']       = 'Aviso: Você não tem permissão para modificar as categorias!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'O Meta Título deve ser maior que 3 e menor que 255 caracteres!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
