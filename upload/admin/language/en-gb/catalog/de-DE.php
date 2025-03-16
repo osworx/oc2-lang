eee9471f678fd5e521bd792bcb9a7eb00717ae83
OSWorX <info@osworx.net>
2025-03-16 14:17:31 +0100

    New translations category.php (German)

--- upload/admin/language/en-gb/catalog/de-DE.php
+++ upload/admin/language/en-gb/catalog/de-DE.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'Eigenschaftsgruppen';
+$_['heading_title']          = 'Kategorien';
 
 // Text
-$_['text_success']      = 'Gruppe erfolgreich bearbeitet';
-$_['text_list']         = 'Übersicht';
-$_['text_add']          = '+ Gruppe';
-$_['text_edit']         = 'Bearbeiten';
+$_['text_success']           = 'Kategorie erfolgreich bearbeitet';
+$_['text_list']              = 'Übersicht';
+$_['text_add']               = 'Neu';
+$_['text_edit']              = 'Bearbeiten';
+$_['text_default']           = 'Standardshop';
 
 // Column
-$_['column_name']       = 'Name';
-$_['column_sort_order'] = 'Reihenfolge';
-$_['column_action']     = 'Aktion';
+$_['column_name']            = 'Bezeichnung';
+$_['column_sort_order']      = 'Reihenfolge';
+$_['column_action']          = 'Aktion';
 
 // Entry
-$_['entry_name']        = 'Name';
-$_['entry_sort_order']  = 'Reihenfolge';
+$_['entry_name']             = 'Bezeichnung';
+$_['entry_description']      = 'Beschreibung';
+$_['entry_meta_title'] 	     = 'Metatag Titel';
+$_['entry_meta_keyword']     = 'Metatag Schlüsselwörter';
+$_['entry_meta_description'] = 'Metatag Beschreibung';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'Hauptkategorie';
+$_['entry_filter']           = 'Filter';
+$_['entry_store']            = 'Shops';
+$_['entry_image']            = 'Bild';
+$_['entry_top']              = 'Oben';
+$_['entry_column']           = 'Spalten';
+$_['entry_sort_order']       = 'Reihenfolge';
+$_['entry_status']           = 'Status';
+$_['entry_layout']           = 'Layout überschreiben';
+
+// Help
+$_['help_filter']            = 'Begriff eintippen, System zeigt passende an';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'Zeige im Topmenü (nur bei Hauptkategorien möglich)';
+$_['help_column']            = 'Anzahl Spalten für die ersten 3 Kategorien  (nur bei Hauptkategorien möglich)';
 
 // Error
-$_['error_warning']     = 'Erforderliche Daten fehlen, bitte Felder überprüfen.';
-$_['error_permission']  = 'Keine Rechte für diese Aktion';
-$_['error_name']        = 'Name muss zwischen 3 und 64 Zeichen lang sein!';
-$_['error_attribute']   = 'Diese Eigenschaftsgruppe kann nicht gelöscht werden, da ihr %s Attribut(e) zugeordnet ist!';
-$_['error_product']     = 'Diese Eigenschaftsgruppe kann nicht gelöscht werden, da sie %s Produkt(en) zugeordnet ist!';
+$_['error_warning']          = 'Erforderliche Daten fehlen, bitte Felder überprüfen.';
+$_['error_permission']       = 'Keine Rechte für diese Aktion';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'Metatag Titel muss zwischen 3 und 255 Zeichen lang sein';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
