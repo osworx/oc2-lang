e26b8df86d95b9d4376706f9eae60739a4dbb695
OSWorX <info@osworx.net>
2025-03-16 11:07:08 +0100

    New translations category.php (Persian)

--- upload/admin/language/en-gb/catalog/fa-IR.php
+++ upload/admin/language/en-gb/catalog/fa-IR.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = 'گروه‌های ویژگی';
+$_['heading_title']          = 'دسته‌بندی‌ها';
 
 // Text
-$_['text_success']      = 'موفقیت: شما گروه‌های ویژگی را تغییر داده‌اید!';
-$_['text_list']         = 'فهرست گروه ویژگی';
-$_['text_add']          = 'افزودن گروه ویژگی';
-$_['text_edit']         = 'ویرایش گروه ویژگی';
+$_['text_success']           = 'موفقیت:‌ شما دسته‌بندی‌ها را تغییر داده‌اید!';
+$_['text_list']              = 'فهرست دسته‌بندی';
+$_['text_add']               = 'افزودن دسته‌بندی';
+$_['text_edit']              = 'ویرایش دسته‌بندی';
+$_['text_default']           = 'پیش‎فرض';
 
 // Column
-$_['column_name']       = 'نام گروه ویژگی';
-$_['column_sort_order'] = 'ترتیب';
-$_['column_action']     = 'کنش';
+$_['column_name']            = 'نام دسته‌بندی';
+$_['column_sort_order']      = 'ترتیب';
+$_['column_action']          = 'کنش';
 
 // Entry
-$_['entry_name']        = 'نام گروه ویژگی';
-$_['entry_sort_order']  = 'ترتیب';
+$_['entry_name']             = 'نام دسته‌بندی';
+$_['entry_description']      = 'توضیحات';
+$_['entry_meta_title'] 	     = 'عنوان متا تگ';
+$_['entry_meta_keyword']     = 'کلیدواژه‌های متا تگ';
+$_['entry_meta_description'] = 'توضیحات متا تگ';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = 'والد';
+$_['entry_filter']           = 'صافی‌ها';
+$_['entry_store']            = 'فروشگاه‌ها';
+$_['entry_image']            = 'تصویر';
+$_['entry_top']              = 'بالا';
+$_['entry_column']           = 'ستون‌ها';
+$_['entry_sort_order']       = 'ترتیب';
+$_['entry_status']           = 'وضعيت';
+$_['entry_layout']           = 'جایگزینی چیدمان';
+
+// Help
+$_['help_filter']            = '(کامل کردن خودکار)';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = 'نمایش در نوار منو بالا. فقط برای دسته‌بندی‌های والد بالایی کار می‌کند.';
+$_['help_column']            = 'تعداد ستون‌ها برای استفاده در ۳ دسته‌بندی زیر. فقط برای دسته‌بندی‌های والد بالا کار می‌کند.';
 
 // Error
-$_['error_warning']     = 'هشدار: لطفا فرم را به دقت برای خطاها بررسی نمایید!';
-$_['error_permission']  = 'هشدار: شما مجوز تغییر گروه‌های ویژگی را ندارید!';
-$_['error_name']        = 'نام گروه ویژگی باید بین ۳ و ۶۴ نویسه باشد!';
-$_['error_attribute']   = 'هشدار: گروه ویژگی نمی‌تواند حذف شود زیرا در حال حاضر به %s ویژگی اختصاص یافته است!';
-$_['error_product']     = 'هشدار: چون این گروه ویژگی در حال حاضر برای %s محصول اختصاص داده است نمی‌تواند حذف گردد!';
+$_['error_warning']          = 'هشدار: لطفا فرم را به دقت برای خطاها بررسی نمایید!';
+$_['error_permission']       = 'هشدار: شما مجوز تغییر دسته‌بندی‌ها را ندارید!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = 'عنوان متا باید بزرگ‌تر از ۳ و کمتر از ۲۵۵ نویسه باشد!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
