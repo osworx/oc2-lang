ae8409390b5ec7cb901e469426c3528aed170cb7
OSWorX <info@osworx.net>
2025-03-16 09:11:45 +0100

    New translations download.php (Norwegian)

--- upload/language/no-NO/no-NO.php
+++ upload/language/no-NO/no-NO.php
@@ -1,46 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Kategorier';
+$_['heading_title']     = 'Nedlastninger';
 
 // Text
-$_['text_success']           = 'Endringer ble vellykket lagret.';
-$_['text_list']              = 'Liste';
-$_['text_add']               = 'Legg til';
-$_['text_edit']              = 'Endre';
-$_['text_default']           = 'Standard';
+$_['text_success']      = 'Endringer ble vellykket lagret.';
+$_['text_list']         = 'Liste';
+$_['text_add']          = 'Legg til';
+$_['text_edit']         = 'Endre';
+$_['text_upload']       = 'Filen ble lastet opp.';
 
 // Column
-$_['column_name']            = 'Navn';
-$_['column_sort_order']      = 'Sortering';
-$_['column_action']          = 'Valg';
+$_['column_name']       = 'Navn';
+$_['column_date_added'] = 'Lagt til';
+$_['column_action']     = 'Valg';
 
 // Entry
-$_['entry_name']             = 'Navn';
-$_['entry_description']      = 'Beskrivelse';
-$_['entry_meta_title'] 	     = 'Meta-tittel';
-$_['entry_meta_keyword']     = 'Meta-nøkkelord';
-$_['entry_meta_description'] = 'Meta-beskrivelse';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = 'Overordnet kategori';
-$_['entry_filter']           = 'Filtre';
-$_['entry_store']            = 'Butikker';
-$_['entry_image']            = 'Bilde';
-$_['entry_top']              = 'Toppmeny';
-$_['entry_column']           = 'Kolonner';
-$_['entry_sort_order']       = 'Sortering';
-$_['entry_status']           = 'Status';
-$_['entry_layout']           = 'Overstyring av visning';
+$_['entry_name']        = 'Navn';
+$_['entry_filename']    = 'Filnavn';
+$_['entry_mask']        = 'Maskering';
 
 // Help
-$_['help_filter']            = '(Autofullfør)';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = 'Vis i toppmenyen. Dette virker bare for toppkategorier.';
-$_['help_column']            = 'Antall kolonner som skal brukes til de nederste 3 kategorier. Dette virker bare for toppkategorier.';
+$_['help_filename']     = 'Du kan laste opp ved å klikke på \'Last opp\'-knappen, eller ved å bruke FTP for å laste opp til \'download\'-mappen, og deretter fylle inn detaljene nedenfor.';
+$_['help_mask']         = 'Det er anbefalt at filnavn og maskering er forskjellige, for å forhindre direkte nedlasting via lenke.';
 
 // Error
-$_['error_warning']          = 'Påse at alle påkrevde felter er fylt inn.';
-$_['error_permission']       = 'Du har ikke rettigheter til å utføre valgte handling.';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = 'Meta-tittel må inneholde mellom 3 og 255 tegn.';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = 'Påse at alle påkrevde felter er fylt inn.';
+$_['error_permission']  = 'Du har ikke rettigheter til å utføre valgte handling.';
+$_['error_name']        = 'Navn må inneholde mellom 3 og 64 tegn.';
+$_['error_upload']      = 'Du må velg fil som skal lastes opp.';
+$_['error_filename']    = 'Filnavn må inneholde mellom 3 og 128 tegn.';
+$_['error_exists']      = 'Filen finnes ikke.';
+$_['error_mask']        = 'Maskering må inneholde mellom 3 og 128 tegn.';
+$_['error_filetype']    = 'Ugyldig filtype.';
+$_['error_product']     = 'Denne nedlastingen kan ikke slettes ettersom den er tilknyttet %s produkter.';
