463c1c1dd876e8e32e0e861875ed6c4da605a56b
OSWorX <info@osworx.net>
2025-03-16 09:07:34 +0100

    New translations download.php (Catalan)

--- upload/language/ca-ES/ca-ES.php
+++ upload/language/ca-ES/ca-ES.php
@@ -1,46 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Categoria';
+$_['heading_title']     = 'Descàrregues';
 
 // Text
-$_['text_success']           = 'Èxit: Heu modificat les categories!';
-$_['text_list']              = 'Llista de categories';
-$_['text_add']               = 'Afegir categoria';
-$_['text_edit']              = 'Editar categoria';
-$_['text_default']           = 'Predeterminat';
+$_['text_success']      = 'Èxit: Heu modificat descàrrregues!';
+$_['text_list']         = 'Llista de descàrregues';
+$_['text_add']          = 'Afegir a descàrregues';
+$_['text_edit']         = 'Editar descàrregues';
+$_['text_upload']       = 'El teu arxiu s\'ha pujat amb èxit!';
 
 // Column
-$_['column_name']            = 'Nom de la Categoria';
-$_['column_sort_order']      = 'Ordre de classificació';
-$_['column_action']          = 'Acció';
+$_['column_name']       = 'Nom de la Descàrrega';
+$_['column_date_added'] = 'Date Added';
+$_['column_action']     = 'Acció';
 
 // Entry
-$_['entry_name']             = 'Nom de la Categoria';
-$_['entry_description']      = 'Descripció';
-$_['entry_meta_title'] 	     = 'Títol de l\'etiqueta meta';
-$_['entry_meta_keyword']     = 'Meta Paraules Clau Tag';
-$_['entry_meta_description'] = 'Meta Tag Descripció';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = 'Categoria Pare';
-$_['entry_filter']           = 'Filtres';
-$_['entry_store']            = 'Botigues:';
-$_['entry_image']            = 'Imatge';
-$_['entry_top']              = 'Superior';
-$_['entry_column']           = 'Columnes';
-$_['entry_sort_order']       = 'Ordre de classificació';
-$_['entry_status']           = 'Estat';
-$_['entry_layout']           = 'Disposició En Passar Damunt';
+$_['entry_name']        = 'Nom de la Descàrrega';
+$_['entry_filename']    = 'Nom del Fitxer';
+$_['entry_mask']        = 'Màscara';
 
 // Help
-$_['help_filter']            = '(Emplenar automàticament)';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = 'Mostrar a la barra de menú superior. Només funciona per a les categories principals.';
-$_['help_column']            = 'Nombre de columnes a utilitzar per a les 3 últimes categories. Només funciona per a les categories principals.';
+$_['help_filename']     = 'Pots pujar arxius usant el botó PUJAR o usar FTP per pujar-los al directori de descàrregues i ingressar els detalls a continuació.';
+$_['help_mask']         = 'Es recomana que el nom de l\'arxiu i la màscara siguin diferents per evitar que la gent posi enllaços directes a les teves descàrregues.';
 
 // Error
-$_['error_warning']          = 'Advertència: Si us plau reviseu acuradament el formulari pels errors!';
-$_['error_permission']       = 'Advertència: No teniu permís per modificar categories!!';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = 'El Títol Meta ha de ser major que 3 i menor de 255 caràcters!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = 'Advertència: Si us plau reviseu acuradament el formulari pels errors!';
+$_['error_permission']  = 'Advertència: No teniu permís per modificar descàrregues!';
+$_['error_name']        = 'El Nom de la Descàrrega ha de tenir entre 3 i 64 caràcters!';
+$_['error_upload']      = 'Upload required!';
+$_['error_filename']    = 'El Nom del Fitxer ha de tenir entre 3 i 128 caràcters!';
+$_['error_exists']      = 'El fitxer no existeix!';
+$_['error_mask']        = 'Mask must be between 3 and 128 characters!';
+$_['error_filetype']    = 'Tipus de fitxer no vàlid!';
+$_['error_product']     = 'Advertència: Aquesta descàrrega no pot ser esborrada ja que està assignada a %s productes!';
