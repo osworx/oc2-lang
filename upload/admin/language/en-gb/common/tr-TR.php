d1af7d35460e398c920c69ef976acd9b935969bd
OSWorX <info@osworx.net>
2025-03-16 11:16:58 +0100

    New translations filemanager.php (Turkish)

--- upload/admin/language/en-gb/common/tr-TR.php
+++ upload/admin/language/en-gb/common/tr-TR.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Gösterge Paneli';
+$_['heading_title']    = 'Görüntü Yöneticisi';
+
+// Text
+$_['text_uploaded']    = 'Başarılı: Dosyanız başarılı şekilde yüklendi!';
+$_['text_directory']   = 'Başarılı: Dizin başarılı şekilde oluşturuldu!';
+$_['text_delete']      = 'Başarılı: Dosya veya Dizin başarılı şekilde silindi!';
+
+// Entry
+$_['entry_search']     = 'Ne Aramıştınız..';
+$_['entry_folder']     = 'Klasör Adı';
 
 // Error
-$_['error_install'] = 'Uyarı: Kurulum klasörü hala mevcut ve güvenlik nedeniyle silinmelidir!';
+$_['error_permission'] = 'Uyarı: İzin Reddedildi!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'Uyarı: Aynı isme sahip bir dosya veya klasör zaten mevcut!';
+$_['error_directory']  = 'Uyarı: Dizin yok!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'Uyarı: Hatalı dosya tipi!';
+$_['error_upload']     = 'Uyarı: Dosya bilinmeyen bir nedenle yüklenemedi!';
+$_['error_delete']     = 'Uyarı: Bu dizini silemezsiniz!';
