75809ca1e23741c93315bc2e1e07e75e1d409818
OSWorX <info@osworx.net>
2025-03-16 09:07:51 +0100

    New translations download.php (Czech)

--- upload/language/cs-CZ/cs-CZ.php
+++ upload/language/cs-CZ/cs-CZ.php
@@ -1,46 +1,35 @@
 <?php
 // Heading
-$_['heading_title']          = 'Kategorie';
+$_['heading_title']     = 'Soubory ke stažení';
 
 // Text
-$_['text_success']           = 'Úspěch: Upravili jste kategorie!';
-$_['text_list']              = 'Seznam kategorií';
-$_['text_add']               = 'Přidat kategorii';
-$_['text_edit']              = 'Upravit kategorii';
-$_['text_default']           = 'Výchozí';
+$_['text_success']      = 'Úspěch: Změnili jste soubory ke stažení!';
+$_['text_list']         = 'Seznam souborů ke stažení';
+$_['text_add']          = 'Přidat ke stažení';
+$_['text_edit']         = 'Upravit ke stažení';
+$_['text_upload']       = 'Váš soubor byl úspěšně nahrán!';
 
 // Column
-$_['column_name']            = 'Název kategorie';
-$_['column_sort_order']      = 'Řadit podle';
-$_['column_action']          = 'Akce';
+$_['column_name']       = 'Název souboru ke stažení';
+$_['column_date_added'] = 'Datum přidání';
+$_['column_action']     = 'Akce';
 
 // Entry
-$_['entry_name']             = 'Název kategorie';
-$_['entry_description']      = 'Popis';
-$_['entry_meta_title'] 	     = 'Meta Tag Title';
-$_['entry_meta_keyword']     = 'Klíčová slova Meta Tag';
-$_['entry_meta_description'] = 'Meta Tag Description';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_parent']           = 'Rodič';
-$_['entry_filter']           = 'Filtry';
-$_['entry_store']            = 'Obchody';
-$_['entry_image']            = 'Obrázek';
-$_['entry_top']              = 'Top';
-$_['entry_column']           = 'Sloupce';
-$_['entry_sort_order']       = 'Řadit podle';
-$_['entry_status']           = 'Stav';
-$_['entry_layout']           = 'Přepsání layoutu';
+$_['entry_name']        = 'Název souboru ke stažení';
+$_['entry_filename']    = 'Název souboru';
+$_['entry_mask']        = 'Maska';
 
 // Help
-$_['help_filter']            = '(Funkce Automatické dokončování)';
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_top']               = 'Zobrazit v horním menu. Funguje pouze pro horní nadřazené kategorie.';
-$_['help_column']            = 'Počet sloupců pro spodní 3 kategorie. Funguje pouze pro horní nadřazené kategorie.';
+$_['help_filename']     = 'Můžete nahrát přes tlačítko nahrát nebo pomocí FTP upload na adresář pro stahování a zadejte níže uvedené údaje.';
+$_['help_mask']         = 'Doporučuje se, aby se název souboru a maska lišily, aby se zabránilo lidem snažícím se přímo napojit na Vaše stahování.';
 
 // Error
-$_['error_warning']          = 'Upozornění: Zkontrolujte formulář pečlivě pro chyby!';
-$_['error_permission']       = 'Upozornění: Nemáte oprávnění ke změně kategorií!';
-$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
-$_['error_meta_title']       = 'Meta Title musí být větší než 3 a méně než 255 znaků!';
-$_['error_keyword']          = 'SEO URL already in use!';
-$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
+$_['error_warning']     = 'Upozornění: Zkontrolujte formulář pečlivě pro chyby!';
+$_['error_permission']  = 'Upozornění: Nemáte oprávnění ke změně stahování!';
+$_['error_name']        = 'Název stahování musí být v rozmezí 3 až 64 znaků!';
+$_['error_upload']      = 'Nahrání je požadované!';
+$_['error_filename']    = 'Název souboru musí být v rozmezí 3 až 128 znaků!';
+$_['error_exists']      = 'Soubor neexistuje!';
+$_['error_mask']        = 'Maska musí být v rozmezí 3 až 128 znaků!';
+$_['error_filetype']    = 'Neplatný typ souboru!';
+$_['error_product']     = 'Upozornění: Tento Download nelze odstranit, protože je aktuálně přiřazen k %s produktů!';
