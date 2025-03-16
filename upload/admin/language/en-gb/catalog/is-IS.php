b5f74539f1c328daec825bf408fa591e7b4c29eb
OSWorX <info@osworx.net>
2025-03-16 14:20:19 +0100

    New translations information.php (Icelandic)

--- upload/admin/language/en-gb/catalog/is-IS.php
+++ upload/admin/language/en-gb/catalog/is-IS.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'Síur';
+$_['heading_title']          = 'Upplýsingar';
 
 // Text
-$_['text_success']      = 'Success: You have modified filters!';
-$_['text_list']         = 'Filter List';
-$_['text_add']          = 'Add Filter';
-$_['text_edit']         = 'Edit Filter';
+$_['text_success']           = 'Success: You have modified information!';
+$_['text_list']              = 'Information List';
+$_['text_add']               = 'Add Information';
+$_['text_edit']              = 'Edit Information';
+$_['text_default']           = 'Sjálfgefið';
 
 // Column
-$_['column_group']      = 'Filter Group';
-$_['column_sort_order'] = 'Breytiröð';
-$_['column_action']     = 'Aðgerð';
+$_['column_title']           = 'Information Title';
+$_['column_sort_order']	     = 'Breytiröð';
+$_['column_action']          = 'Aðgerð';
 
 // Entry
-$_['entry_group']       = 'Filter Group Name';
-$_['entry_name']        = 'Filter Name';
-$_['entry_sort_order']  = 'Breytiröð';
+$_['entry_title']            = 'Information Title';
+$_['entry_description']      = 'Lýsing';
+$_['entry_store']            = 'Verslanir';
+$_['entry_meta_title'] 	     = 'Meta Tag Title';
+$_['entry_meta_keyword'] 	 = 'Meta Tag Keywords';
+$_['entry_meta_description'] = 'Meta Tag Description';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Bottom';
+$_['entry_status']           = 'Staða';
+$_['entry_sort_order']       = 'Breytiröð';
+$_['entry_layout']           = 'Layout Override';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Display in the bottom footer.';
 
 // Error
-$_['error_warning']     = 'Aðvörun: Farðu vel yfir formið til að forðast villur!';
-$_['error_permission']  = 'Warning: You do not have permission to modify filters!';
-$_['error_group']       = 'Filter Group Name must be between 1 and 64 characters!';
-$_['error_name']        = 'Filter Name must be between 1 and 64 characters!';
+$_['error_warning']          = 'Aðvörun: Farðu vel yfir formið til að forðast villur!';
+$_['error_permission']       = 'Warning: You do not have permission to modify information!';
+$_['error_title']            = 'Information Title must be between 3 and 64 characters!';
+$_['error_description']      = 'Description must be more than 3 characters!';
+$_['error_meta_title']       = 'Meta Title verður að vera 3 til 255 stafir!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'Warning: This information page cannot be deleted as it is currently assigned as the store account terms!';
+$_['error_checkout']         = 'Warning: This information page cannot be deleted as it is currently assigned as the store checkout terms!';
+$_['error_affiliate']        = 'Warning: This information page cannot be deleted as it is currently assigned as the store affiliate terms!';
+$_['error_return']           = 'Warning: This information page cannot be deleted as it is currently assigned as the store return terms!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
