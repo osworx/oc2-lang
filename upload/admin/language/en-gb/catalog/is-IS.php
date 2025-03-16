76c637f073fb93699ad7475c5848ac060e264245
OSWorX <info@osworx.net>
2025-03-16 11:05:22 +0100

    New translations attribute.php (Icelandic)

---
+++ upload/admin/language/en-gb/catalog/is-IS.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Eigindir';
+
+// Text
+$_['text_success']           = 'Fínt: Þú hefur breytt eigindum.';
+$_['text_list']              = 'Eigindalisti';
+$_['text_add']               = 'Bæta við eigind';
+$_['text_edit']              = 'Breyta eigind';
+
+// Column
+$_['column_name']            = 'Heiti eigindar';
+$_['column_attribute_group'] = 'Eigindaflokkur';
+$_['column_sort_order']      = 'Breytiröð';
+$_['column_action']          = 'Aðgerð';
+
+// Entry
+$_['entry_name']             = 'Heiti eigindar';
+$_['entry_attribute_group']  = 'Eigindaflokkur';
+$_['entry_sort_order']       = 'Breytiröð';
+
+// Error
+$_['error_warning']          = 'Aðvörun: Farðu vel yfir formið til að forðast villur!';
+$_['error_permission']       = 'Aðvörun: Þú hefur ekki heimild til að breyta eigindum!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Eigindarheiti verður að vera 3 til 64 stafir!';
+$_['error_product']          = 'Aðvörun: Þessari eigind verður ekki eytt því hún er tengd %s vörum!';
