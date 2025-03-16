f2769068ba1443adcd4e51809a20c3942aaee086
OSWorX <info@osworx.net>
2025-03-16 11:06:24 +0100

    New translations attribute_group.php (Bosnian)

--- upload/admin/language/en-gb/catalog/bs-BA.php
+++ upload/admin/language/en-gb/catalog/bs-BA.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Atributi';
+$_['heading_title']     = 'Grupe Atributa';
 
 // Text
-$_['text_success']           = 'Uspjeh: Uspješno ste promjenili atribute!';
-$_['text_list']              = 'Popisa atributa';
-$_['text_add']               = 'Dodavanje atributa';
-$_['text_edit']              = 'Uredite atribut';
+$_['text_success']      = 'Uspjeh: Promjene atributa grupe!';
+$_['text_list']         = 'Popis atributa grupa';
+$_['text_add']          = 'Dodavanje atributa grupe';
+$_['text_edit']         = 'Uređivanje atributa grupe';
 
 // Column
-$_['column_name']            = 'Naziv atributa';
-$_['column_attribute_group'] = 'Grupe Atributa';
-$_['column_sort_order']      = 'Redoslijed sortiranja';
-$_['column_action']          = 'Akcija';
+$_['column_name']       = 'Naziv atributa grupe';
+$_['column_sort_order'] = 'Redoslijed sortiranja';
+$_['column_action']     = 'Akcija';
 
 // Entry
-$_['entry_name']             = 'Naziv atributa';
-$_['entry_attribute_group']  = 'Grupe Atributa';
-$_['entry_sort_order']       = 'Redoslijed sortiranja';
+$_['entry_name']        = 'Naziv atributa grupe';
+$_['entry_sort_order']  = 'Redoslijed sortiranja';
 
 // Error
-$_['error_warning']          = 'Upozorenje: Provjerite obrazac pažljivo radi grešaka!';
-$_['error_permission']       = 'Upozorenje: Nemate dozvolu za mijenjanje atributa!';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'Naziv atributa mora biti između 3 i 64 znakova!';
-$_['error_product']          = 'Upozorenje: Ovaj atribut se ne može izbrisati jer je trenutno dodijeljen %s proizvoda!';
+$_['error_warning']     = 'Upozorenje: Provjerite obrazac pažljivo radi grešaka!';
+$_['error_permission']  = 'Upozorenje: Nemate dozvolu za mijenjanje atributa grupe!';
+$_['error_name']        = 'Naziv atributa grupe mora biti između 3 do 64 znakova!';
+$_['error_attribute']   = 'Upozorenje: Ovaj atribut grupe nije moguće izbrisati kao trenutno dodijeljen %s atributa!';
+$_['error_product']     = 'Upozorenje: Ovaj atribut grupe nije moguće izbrisati jer je trenutno dodijeljen %s atributa!';
