70f2078a9f97155674494d7c34e4f4d0a4253261
OSWorX <info@osworx.net>
2025-03-16 11:09:36 +0100

    New translations information.php (Portuguese)

--- upload/admin/language/en-gb/catalog/pt-PT.php
+++ upload/admin/language/en-gb/catalog/pt-PT.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'Filtros';
+$_['heading_title']          = 'Informação';
 
 // Text
-$_['text_success']      = 'Concluído: Modificou os Filtros com sucesso!';
-$_['text_list']         = 'Lista de Filtros';
-$_['text_add']          = 'Adicionar Filtro';
-$_['text_edit']         = 'Editar Filtro';
+$_['text_success']           = 'Concluído: Modificou as Informações com sucesso!';
+$_['text_list']              = 'Lista de Informação';
+$_['text_add']               = 'Adicionar Informação';
+$_['text_edit']              = 'Editar Informação';
+$_['text_default']           = 'Predefinição';
 
 // Column
-$_['column_group']      = 'Grupo de Filtros';
-$_['column_sort_order'] = 'Posição';
-$_['column_action']     = 'Ação';
+$_['column_title']           = 'Título da Informação';
+$_['column_sort_order']	     = 'Posição';
+$_['column_action']          = 'Ação';
 
 // Entry
-$_['entry_group']       = 'Nome do Grupo de Filtros';
-$_['entry_name']        = 'Nome do Filtro';
-$_['entry_sort_order']  = 'Posição';
+$_['entry_title']            = 'Título da Informação';
+$_['entry_description']      = 'Descrição';
+$_['entry_store']            = 'Lojas';
+$_['entry_meta_title'] 	     = 'Titulo Meta Tag';
+$_['entry_meta_keyword'] 	 = 'Palavras-Chave Meta Tag';
+$_['entry_meta_description'] = 'Descrição Meta Tag';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Inferior';
+$_['entry_status']           = 'Estado';
+$_['entry_sort_order']       = 'Posição';
+$_['entry_layout']           = 'Substituição de Layout';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Mostrar no rodapé inferior.';
 
 // Error
-$_['error_warning']     = 'Aviso: Por favor, verifique o formulário cuidadosamente para evitar erros!';
-$_['error_permission']  = 'Aviso: Não tem permissões para modificar os Filtros!';
-$_['error_group']       = 'Nome do Grupo de Filtro deve conter entre 1 e 64 caracteres!';
-$_['error_name']        = 'Nome do Filtro deve conter entre 1 e 64 caracteres!';
+$_['error_warning']          = 'Aviso: Por favor, verifique o formulário cuidadosamente para evitar erros!';
+$_['error_permission']       = 'Atenção: Não tem permissões para alterar as informações!';
+$_['error_title']            = 'Título das Informações deve conter entre 3 e 64 caracteres !';
+$_['error_description']      = 'A Descrição deve conter mais de 3 caracteres!';
+$_['error_meta_title']       = 'O Meta Título deve ser conter mais de 3 e menos de 255 caracteres!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'Atenção: Esta página de informações não pode ser excluída, pois está a ser actualmente utilizada como os termos utilização das contas da loja!';
+$_['error_checkout']         = 'Atenção: Esta página de informações não pode ser excluída, pois está a ser actualmente utilizada como os termos checkout da loja!';
+$_['error_affiliate']        = 'Atenção: Esta página de informações não pode ser excluída , pois está a ser actualmente utilizada como os termos utilização das contas de affiliados da loja!';
+$_['error_return']           = 'Atenção: Esta página de informações não pode ser excluída, pois está a ser actualmente utilizada como os termos devoluções da loja!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
