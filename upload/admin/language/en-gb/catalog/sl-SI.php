db4672ff66593d2a61e24449954ce32ed69c7c03
OSWorX <info@osworx.net>
2025-03-16 11:05:12 +0100

    New translations attribute.php (Slovenian)

---
+++ upload/admin/language/en-gb/catalog/sl-SI.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']          = 'Lastnosti';
+
+// Text
+$_['text_success']           = 'USPEŠNO: Ste uredili lastnosti!';
+$_['text_list']              = 'Seznam lastnosti';
+$_['text_add']               = 'Dodaj lastnost';
+$_['text_edit']              = 'Uredi lastnost';
+
+// Column
+$_['column_name']            = 'Ime lastnosti';
+$_['column_attribute_group'] = 'Skupina lastnosti';
+$_['column_sort_order']      = 'Vrstni red';
+$_['column_action']          = 'Dejanje';
+
+// Entry
+$_['entry_name']             = 'Ime lastnosti';
+$_['entry_attribute_group']  = 'Skupina lastnosti';
+$_['entry_sort_order']       = 'Vrstni red';
+
+// Error
+$_['error_warning']          = 'POZOR: Prosimo, pazljivo preverite polja za morebitne napake!';
+$_['error_permission']       = 'POZOR: Nimate dovoljenja za urejanje lastnosti!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'Ime lastnosti mora vsebovati med 3 in 64 znakov!';
+$_['error_product']          = 'POZOR: Te lastnosti ne morete izbrisati, ker je trenutno vezana na %s izdelke!';
