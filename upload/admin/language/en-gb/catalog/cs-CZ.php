d2caa7f240445f223751b20257d91fc8f4bb450c
OSWorX <info@osworx.net>
2025-03-16 14:19:54 +0100

    New translations information.php (Czech)

--- upload/admin/language/en-gb/catalog/cs-CZ.php
+++ upload/admin/language/en-gb/catalog/cs-CZ.php
@@ -1,25 +1,45 @@
 <?php
 // Heading
-$_['heading_title']     = 'Filtry';
+$_['heading_title']          = 'Informace';
 
 // Text
-$_['text_success']      = 'Úspěch: Upravili jste filtry!';
-$_['text_list']         = 'Seznam filtrů';
-$_['text_add']          = 'Přidat filtr';
-$_['text_edit']         = 'Upravit filtr';
+$_['text_success']           = 'Úspěch: Upravili jste informace!';
+$_['text_list']              = 'Seznam informací';
+$_['text_add']               = 'Přidání Informaci';
+$_['text_edit']              = 'Upravit Informaci';
+$_['text_default']           = 'Výchozí';
 
 // Column
-$_['column_group']      = 'Skupina filtrů';
-$_['column_sort_order'] = 'Řadit podle';
-$_['column_action']     = 'Akce';
+$_['column_title']           = 'Název Informace';
+$_['column_sort_order']	     = 'Řadit podle';
+$_['column_action']          = 'Akce';
 
 // Entry
-$_['entry_group']       = 'Název filtru skupiny';
-$_['entry_name']        = 'Název filtru';
-$_['entry_sort_order']  = 'Řadit podle';
+$_['entry_title']            = 'Název Informace';
+$_['entry_description']      = 'Popis';
+$_['entry_store']            = 'Obchody';
+$_['entry_meta_title'] 	     = 'Meta Tag Title';
+$_['entry_meta_keyword'] 	 = 'Klíčová slova Meta Tag';
+$_['entry_meta_description'] = 'Meta Tag Description';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_bottom']           = 'Dole';
+$_['entry_status']           = 'Stav';
+$_['entry_sort_order']       = 'Řadit podle';
+$_['entry_layout']           = 'Přepsání layoutu';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_bottom']            = 'Zobrazit v zápatí zdola.';
 
 // Error
-$_['error_warning']     = 'Upozornění: Zkontrolujte formulář pečlivě pro chyby!';
-$_['error_permission']  = 'Upozornění: Nemáte oprávnění k úpravě filtrů!';
-$_['error_group']       = 'Název skupiny filtru musí být mezi 1 až 64 znaků!';
-$_['error_name']        = 'Název filtru musí být v rozmezí 1 až 64 znaků!';
+$_['error_warning']          = 'Upozornění: Zkontrolujte formulář pečlivě pro chyby!';
+$_['error_permission']       = 'Upozornění: Nemáte oprávnění k úpravě informací!';
+$_['error_title']            = 'Název Informace musí být v rozmezí 3 až 64 znaků!';
+$_['error_description']      = 'Popis musí být delší než 3 znaky!';
+$_['error_meta_title']       = 'Meta Title musí být větší než 3 a méně než 255 znaků!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_account']          = 'Varování: Tato informační stránka nemůže být odstraněna, protože je v současnosti přiřazena k obchodním podmínkám účtu!';
+$_['error_checkout']         = 'Varování: Tato informační stránka nemůže být odstraněna, protože je v současnosti přiřazena jako objednací podmínky obchodu!';
+$_['error_affiliate']        = 'Varování: Tato informační stránka nemůže být odstraněn, protože je v současnosti přiřazena jako Affiliate podmínky obchodu!';
+$_['error_return']           = 'Varování: Tato informační stránka nemůže být odstraněna, protože je v současnosti přiřazena k vratkovým podmínkám obchodu!';
+$_['error_store']            = 'Warning: This information page cannot be deleted as it is currently used by %s stores!';
