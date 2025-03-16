8f71b800938eaa7c2f86d50eae7be823b76ace87
OSWorX <info@osworx.net>
2025-03-16 14:16:41 +0100

    New translations attribute_group.php (Czech)

--- upload/admin/language/en-gb/catalog/cs-CZ.php
+++ upload/admin/language/en-gb/catalog/cs-CZ.php
@@ -1,27 +1,25 @@
 <?php
 // Heading
-$_['heading_title']          = 'Atributy';
+$_['heading_title']     = 'Skupina atributů';
 
 // Text
-$_['text_success']           = 'Úspěch: Jste změnili atributy!';
-$_['text_list']              = 'Seznam atributů';
-$_['text_add']               = 'Přidat atribut';
-$_['text_edit']              = 'Upravit atribut';
+$_['text_success']      = 'Úspěch: Jste změnili atribut skupiny!';
+$_['text_list']         = 'Seznam skupiny atributů';
+$_['text_add']          = 'Přidat skupinu atributů';
+$_['text_edit']         = 'Upravit skupiny atributů';
 
 // Column
-$_['column_name']            = 'Název atributu';
-$_['column_attribute_group'] = 'Skupina atributů';
-$_['column_sort_order']      = 'Řadit podle';
-$_['column_action']          = 'Akce';
+$_['column_name']       = 'Název skupiny atributů';
+$_['column_sort_order'] = 'Řadit podle';
+$_['column_action']     = 'Akce';
 
 // Entry
-$_['entry_name']             = 'Název atributu';
-$_['entry_attribute_group']  = 'Skupina atributů';
-$_['entry_sort_order']       = 'Řadit podle';
+$_['entry_name']        = 'Název skupiny atributů';
+$_['entry_sort_order']  = 'Řadit podle';
 
 // Error
-$_['error_warning']          = 'Upozornění: Zkontrolujte formulář pečlivě pro chyby!';
-$_['error_permission']       = 'Upozornění: Nemáte oprávnění ke změně atributů!';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'Název atributu musí být v rozmezí 3 až 64 znaků!';
-$_['error_product']          = 'Varování: Tento atribut nemůže být odstraněn, protože je v současnosti přiřazen k %s produktů!';
+$_['error_warning']     = 'Upozornění: Zkontrolujte formulář pečlivě pro chyby!';
+$_['error_permission']  = 'Upozornění: Nemáte oprávnění ke změně skupiny atributů!';
+$_['error_name']        = 'Název skupiny atributů musí být 3 až 64 znaků!';
+$_['error_attribute']   = 'Varování: Tato skupina atribut nemůže být odstraněny, protože je aktuálně přiřazeno atributů %s!';
+$_['error_product']     = 'Varování: Tato skupina atributů nemůže být odstraněna, protože je v současnosti přiřazena k %s výrobků!';
