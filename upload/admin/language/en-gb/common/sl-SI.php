79e764f01bba97b74c579abbcbcd499e3b02cecf
OSWorX <info@osworx.net>
2025-03-16 11:16:50 +0100

    New translations filemanager.php (Slovenian)

--- upload/admin/language/en-gb/common/sl-SI.php
+++ upload/admin/language/en-gb/common/sl-SI.php
@@ -1,6 +1,23 @@
 <?php
 // Heading
-$_['heading_title'] = 'Nadzorna plošča';
+$_['heading_title']    = 'Urejanje slik';
+
+// Text
+$_['text_uploaded']    = 'USPEŠNO: Ste naložili sliko!';
+$_['text_directory']   = 'USPEŠNO: Ste ustvarili mapo!';
+$_['text_delete']      = 'USPEŠNO: Slika ali mapa je bila izbrisana!';
+
+// Entry
+$_['entry_search']     = 'Išči..';
+$_['entry_folder']     = 'Ime mape';
 
 // Error
-$_['error_install'] = 'POZOR: Mapa install še vedno obstaja in je potrebno izbrisati iz varnostnih razlogov!';
+$_['error_permission'] = 'POZOR: Zavrnjen dostop!';
+$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
+$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
+$_['error_exists']     = 'POZOR: Slika ali mapa s tem imenom že obstaja!';
+$_['error_directory']  = 'POZOR: Oprostite, izberite mapo!';
+$_['error_filesize']   = 'Warning: Incorrect file size!';
+$_['error_filetype']   = 'POZOR: Nepravilna vrsta datoteke!';
+$_['error_upload']     = 'POZOR: Datoteke ni mogoče prenesti iz neznanega razloga!';
+$_['error_delete']     = 'POZOR: Te mape ne morete izbrisati!';
