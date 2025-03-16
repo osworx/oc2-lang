cf5e8bd7fae39009ac778f44f497a9d44358150c
OSWorX <info@osworx.net>
2025-03-16 14:23:34 +0100

    New translations recurring.php (Thai)

--- upload/admin/language/en-gb/catalog/th-TH.php
+++ upload/admin/language/en-gb/catalog/th-TH.php
@@ -1,108 +1,42 @@
 <?php
 // Heading
-$_['heading_title']          = 'สินค้า';
+$_['heading_title']			= 'Recurring Profile';
 
 // Text
-$_['text_success']           = 'สำเร็จ: คุณได้เปลี่ยนแปลงสินค้า';
-$_['text_list']              = 'รายการสินค้า';
-$_['text_add']               = 'เพิ่มสินค้า';
-$_['text_edit']              = 'แก้ไขสินค้า';
-$_['text_plus']              = '+';
-$_['text_minus']             = '-';
-$_['text_default']           = 'ค่าเริ่มต้น';
-$_['text_option']            = 'ตัวเลือก';
-$_['text_option_value']      = 'ค่าตัวเลือก';
-$_['text_percent']           = 'เปอร์เซ็นต์';
-$_['text_amount']            = 'คงที่';
-
-// Column
-$_['column_name']            = 'ชื่อสินค้า';
-$_['column_model']           = 'รหัสสินค้า';
-$_['column_image']           = 'รูปภาพ';
-$_['column_price']           = 'ราคา';
-$_['column_quantity']        = 'จำนวน';
-$_['column_status']          = 'สถานะ';
-$_['column_action']          = 'การดำเนินการ';
+$_['text_success']          = 'ความสำเร็จ: คุณได้ปรับเปลี่ยน Recurring Profiles!';
+$_['text_list']             = 'รายการ Recurring Profile';
+$_['text_add']              = 'เพิ่ม Recurring Profile';
+$_['text_edit']             = 'แก้ไข Recurring Profile';
+$_['text_day']				= 'วันที่';
+$_['text_week']				= 'สัปดาห์';
+$_['text_semi_month']		= 'กึ่งเดือน';
+$_['text_month']			= 'เดือน';
+$_['text_year']				= 'ปี';
+$_['text_recurring']	    = '<p><i class="fa fa-info-circle"></i>การแบ่งจ่ายสามารถคำนวนได้จาก ความถี่และรอบ</p><p>ตัวอย่าง: ถ้าคุณใช้ความถี่เป็น "สัปดาห์" และรอบเป็น "2" ผู้ใช้จะถูกเรียกเก็บเงินทุกๆ 2 อาทิตย์</p><p>ระยะเวลามีไว้กำหนดจำนวนครั้งที่จะเรียกเก็บเงิน ถ้าตั้งระยะเวลาเป็น 0 ลูกค้าจะต้องเป็นคนยกเลิกเอง</p>';
+$_['text_profile']			= 'Recurring Profile';
+$_['text_trial']			= 'โพรไฟล์การทดลอง';
 
 // Entry
-$_['entry_name']             = 'ชื่อสินค้า';
-$_['entry_description']      = 'คำอธิบาย';
-$_['entry_meta_title'] 	     = 'ชื่อแท็ก Meta';
-$_['entry_meta_keyword'] 	 = 'คำเรียก Tag Meta';
-$_['entry_meta_description'] = 'คำอธิบาย Tag Meta';
-$_['entry_keyword']          = 'SEO URL';
-$_['entry_model']            = 'รหัสสินค้า';
-$_['entry_sku']              = 'SKU';
-$_['entry_upc']              = 'UPC';
-$_['entry_ean']              = 'EAN';
-$_['entry_jan']              = 'JAN';
-$_['entry_isbn']             = 'ISBN';
-$_['entry_mpn']              = 'MPN';
-$_['entry_location']         = 'สถานที่';
-$_['entry_shipping']         = 'ต้องการจัดส่ง';
-$_['entry_manufacturer']     = 'ผู้ผลิต';
-$_['entry_store']            = 'ร้านค้า';
-$_['entry_date_available']   = 'วันที่จะมี';
-$_['entry_quantity']         = 'จำนวน';
-$_['entry_minimum']          = 'ขั้นต่ำ';
-$_['entry_stock_status']     = 'สถานะสินค้า';
-$_['entry_price']            = 'ราคา';
-$_['entry_tax_class']        = 'ประเภทภาษี';
-$_['entry_points']           = 'คะแนน';
-$_['entry_option_points']    = 'คะแนน';
-$_['entry_subtract']         = 'ลบสินค้าคงคลัง';
-$_['entry_weight_class']     = 'ประเภทสินค้า (น้ำหนัก)';
-$_['entry_weight']           = 'น้ำหนัก';
-$_['entry_dimension']        = 'ขนาด (ยาว x กว้าง x สูง)';
-$_['entry_length_class']     = 'ประเภทสินค้า (ยาว)';
-$_['entry_length']           = 'ความยาว';
-$_['entry_width']            = 'ความกว้าง';
-$_['entry_height']           = 'ความสูง';
-$_['entry_image']            = 'รูปภาพ';
-$_['entry_additional_image'] = 'Additional Images';
-$_['entry_customer_group']   = 'กลุ่มลูกค้า';
-$_['entry_date_start']       = 'วันที่เริ่มต้น';
-$_['entry_date_end']         = 'วันที่สิ้นสุด';
-$_['entry_priority']         = 'การจัดลำดับความสำคัญ';
-$_['entry_attribute']        = 'คุณลักษณะ';
-$_['entry_attribute_group']  = 'กลุ่มคุณสมบัติ';
-$_['entry_text']             = 'ข้อความ';
-$_['entry_option']           = 'ตัวเลือก';
-$_['entry_option_value']     = 'ค่าตัวเลือก';
-$_['entry_required']         = 'จำเป็นต้องระบุ';
-$_['entry_status']           = 'สถานะ';
-$_['entry_sort_order']       = 'ลำดับการจัดเรียง';
-$_['entry_category']         = 'หมวดหมู่สินค้า';
-$_['entry_filter']           = 'ตัวกรอง';
-$_['entry_download']         = 'ดาวน์โหลด';
-$_['entry_related']          = 'สินค้าที่เกี่ยวข้อง';
-$_['entry_tag']          	 = 'Tags ผลิตภัณฑ์';
-$_['entry_reward']           = 'คะแนน';
-$_['entry_layout']           = 'Layout Override';
-$_['entry_recurring']        = 'Recurring Profile';
+$_['entry_name']		    = 'ชื่อ';
+$_['entry_price']			= 'ราคา';
+$_['entry_duration']		= 'ระยะเวลา';
+$_['entry_cycle']			= 'รอบ';
+$_['entry_frequency']		= 'ความถี่';
+$_['entry_trial_price']		= 'ราคาทดลอง';
+$_['entry_trial_duration']	= 'ระยะเวลาทดลองใช้';
+$_['entry_trial_status']	= 'สถานะการทดลอง';
+$_['entry_trial_cycle']	    = 'รอบการทดลอง';
+$_['entry_trial_frequency'] = 'ความถี่ในการทดลอง';
+$_['entry_status']			= 'สถานะ';
+$_['entry_sort_order']		= 'ลำดับการจัดเรียง';
 
-// Help
-$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
-$_['help_sku']               = 'Stock Keeping Unit';
-$_['help_upc']               = 'รหัสสินค้าสากล';
-$_['help_ean']               = 'European Article Number';
-$_['help_jan']               = 'Japanese Article Number';
-$_['help_isbn']              = 'International Standard Book Number';
-$_['help_mpn']               = 'Manufacturer Part Number';
-$_['help_manufacturer']      = '(อัตโนมัติ)';
-$_['help_minimum']           = 'ยอดเงินสั่งซื้อขั้นต่ำ';
-$_['help_stock_status']      = 'สถานะที่แสดงเมื่อผลิตภัณฑ์หมดสต๊อก';
-$_['help_points']            = 'จำนวนคะแนนที่ต้องซื้อสินค้านี้ ถ้าคุณไม่ต้องการให้ใช้คะแนนซื้อผลิตภัณฑ์นี้ ปล่อยให้เป็น 0';
-$_['help_category']          = '(อัตโนมัติ)';
-$_['help_filter']            = '(อัตโนมัติ)';
-$_['help_download']          = '(อัตโนมัติ)';
-$_['help_related']           = '(อัตโนมัติ)';
-$_['help_tag']               = 'Comma separated';
+// Column
+$_['column_name']			= 'ชื่อ';
+$_['column_sort_order']	    = 'ลำดับการจัดเรียง';
+$_['column_action']         = 'การดำเนินการ';
 
 // Error
-$_['error_warning']          = 'คำเตือน: กรุณาตรวจสอบแบบฟอร์มอย่างรอบคอบสำหรับข้อผิดพลาด!';
-$_['error_permission']       = 'คำเตือน: คุณไม่ได้รับสิทธิ์ในการเปลี่ยนแปลงสินค้า!';
-$_['error_name']             = 'ชื่อสินค้า ต้องมีมากกว่า 3 และน้อยกว่า 255 ตัวอักษร!';
-$_['error_meta_title']       = 'ชื่อ Meta ต้องมากกว่า 3 และน้อยกว่า 255 อักษร!';
-$_['error_model']            = 'รหัสสินค้า ต้องมีมากกว่า 3 และน้อยกว่า 64 ตัวอักษร!';
-$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_warning']         = 'คำเตือน: กรุณาตรวจสอบแบบฟอร์มอย่างรอบคอบสำหรับข้อผิดพลาด!';
+$_['error_permission']		= 'คำเตือน: คุณไม่มีสิทธิ์ในการปรับเปลี่ยน recurring profiles!';
+$_['error_name']			= 'ชื่อโปรไฟล์ ต้องมีมากกว่า 3 และน้อยกว่า 255 ตัวอักษร!';
+$_['error_product']			= 'คำเตือน: โปรไฟล์นี้เกิดไม่สามารถลบเนื่องปัจจุบันมีการกำหนดให้กับผลิตภัณฑ์ %s';
