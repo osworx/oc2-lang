3d627a324746c06edfc46b8f5812a4126d2780af
OSWorX <info@osworx.net>
2025-03-16 14:16:17 +0100

    New translations attribute.php (Galician)

--- upload/admin/language/en-gb/catalog/gl-ES.php
+++ upload/admin/language/en-gb/catalog/gl-ES.php
@@ -1,36 +1,27 @@
 <?php
 // Heading
-$_['heading_title']     = 'Revisións';
+$_['heading_title']          = 'Atributos';
 
 // Text
-$_['text_success']      = 'Modificaronse as revisións correctamente!';
-$_['text_list']         = 'Lista de revisións';
-$_['text_add']          = 'Engadir revisión';
-$_['text_edit']         = 'Editar revisión';
+$_['text_success']           = 'Modificaronse os atributos correctamente!';
+$_['text_list']              = 'Lista de atributos';
+$_['text_add']               = 'Engadir atributo';
+$_['text_edit']              = 'Modifcar atributo';
 
 // Column
-$_['column_product']    = 'Produto';
-$_['column_author']     = 'Autor';
-$_['column_rating']     = 'Puntuación';
-$_['column_status']     = 'Estado';
-$_['column_date_added'] = 'Engadida o';
-$_['column_action']     = 'Acción';
+$_['column_name']            = 'Nome do atributo';
+$_['column_attribute_group'] = 'Grupo de atributos';
+$_['column_sort_order']      = 'Orde da clasificación';
+$_['column_action']          = 'Acción';
 
 // Entry
-$_['entry_product']     = 'Produto';
-$_['entry_author']      = 'Autor';
-$_['entry_rating']      = 'Puntuación';
-$_['entry_status']      = 'Estado';
-$_['entry_text']        = 'Texto';
-$_['entry_date_added']  = 'Engadida o';
-
-// Help
-$_['help_product']      = '(Autocompletar)';
+$_['entry_name']             = 'Nome do atributo';
+$_['entry_attribute_group']  = 'Grupo de atributos';
+$_['entry_sort_order']       = 'Orde da clasificación';
 
 // Error
-$_['error_warning']     = 'Aviso: Por favor, revise o formulario e busque os erros!';
-$_['error_permission']  = 'Aviso: non ten permisos para modificar as revisións!';
-$_['error_product']     = 'Produto requirido!';
-$_['error_author']      = 'O autor debe ter entre 3 e 64 caracteres!';
-$_['error_text']        = 'O texto da revisión debe ter polo menos 1 caracter!';
-$_['error_rating']      = 'Valoración da revisión requirida!';
+$_['error_warning']          = 'Aviso: Por favor, revise o formulario e busque os erros!';
+$_['error_permission']       = 'Aviso: Non ten permisos para modificar os atributos!';
+$_['error_attribute_group']  = 'Attribute Group Required!';
+$_['error_name']             = 'O nome do atributo debe ter entre 3 e 64 caracteres!';
+$_['error_product']          = 'Aviso: Este atributo non pode ser eliminado, actualmente está atribuído a %s produtos!';
