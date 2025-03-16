0a1da4a654b808cb9a88536f149b79d9f0b31d4e
OSWorX <info@osworx.net>
2025-03-16 11:11:02 +0100

    New translations option.php (Arabic)

--- upload/admin/language/en-gb/catalog/ar-SA.php
+++ upload/admin/language/en-gb/catalog/ar-SA.php
@@ -1,35 +1,40 @@
 <?php
 // Heading
-$_['heading_title']     = 'الشركات المصنعة';
+$_['heading_title']      = 'خيارات';
 
 // Text
-$_['text_success']      = 'تم بنجاح: لقد قمت بتعديل الشركات المصنعة!';
-$_['text_list']         = 'قائمة الشركات';
-$_['text_add']          = 'إضافة شركة';
-$_['text_edit']         = 'تعديل شركة';
-$_['text_default']      = 'الافتراضي';
-$_['text_percent']      = 'النسبة المئوية';
-$_['text_amount']       = 'المبلغ الثابت';
+$_['text_success']       = 'مبروك: لقد قمت بتعديل الخيارات بنجاح!';
+$_['text_list']          = 'قائمة الخيارات';
+$_['text_add']           = 'إضافة خيار';
+$_['text_edit']          = 'تعديل خيار';
+$_['text_choose']        = 'إختر';
+$_['text_select']        = 'قائمة منسدلة';
+$_['text_radio']         = 'راديو';
+$_['text_checkbox']      = 'خانة التحديد';
+$_['text_input']         = 'إدخال';
+$_['text_text']          = 'نص';
+$_['text_textarea']      = 'حقل كتابة النص';
+$_['text_file']          = 'ملف';
+$_['text_date']          = 'التاريخ';
+$_['text_datetime']      = 'التاريخ و الوقت';
+$_['text_time']          = 'الوقت';
 
 // Column
-$_['column_name']       = 'اسم الشركة';
-$_['column_sort_order'] = 'الترتيب';
-$_['column_action']     = 'عمل';
+$_['column_name']        = 'اسم الخيار';
+$_['column_sort_order']  = 'الترتيب';
+$_['column_action']      = 'عمل';
 
 // Entry
-$_['entry_name']        = 'اسم الشركة';
-$_['entry_store']       = 'المتاجر';
-$_['entry_keyword']     = 'SEO URL';
-$_['entry_image']       = 'صورة';
-$_['entry_sort_order']  = 'الترتيب';
-$_['entry_type']        = 'النوع';
-
-// Help
-$_['help_keyword']      = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['entry_name']         = 'اسم الخيار';
+$_['entry_type']         = 'النوع';
+$_['entry_option_value'] = 'اسم قيمة الخيار';
+$_['entry_image']        = 'صورة';
+$_['entry_sort_order']   = 'الترتيب';
 
 // Error
-$_['error_warning']     = 'تحذير: الرجاء التحقق من النموذج واصلاح الأخطاء!';
-$_['error_permission']  = 'تحذير: ليس لديك الصلحية للتعديل على الشركات!';
-$_['error_name']        = 'يجب أن يكون اسم الشركة ما بين 2 و 64 حرفاً!';
-$_['error_keyword']     = 'SEO URL already in use!';
-$_['error_product']     = 'تحذير: لا يمكن حذف هذه الشركة لإرتباطها بـ %s منتجات!';
+$_['error_warning']      = 'تحذير: الرجاء التحقق من النموذج واصلاح الأخطاء!';
+$_['error_permission']   = 'تحذير: ليست لديك الصلاحيات  لتعديل الخيارات!';
+$_['error_name']         = 'اسم الخيار يجب ان يكون محصوراً بين حرف و 128 حرف!';
+$_['error_type']         = 'تحذير: قيم الخيارات مطلوبة!';
+$_['error_option_value'] = 'اسم قيمة الخيار يجب ان يكون محصوراً بين حرف و 128 حرف!';
+$_['error_product']      = 'تحذير: لا يمكن حذف هذا الخيار كما تم تعيينه حاليا على %s منتجات!';
