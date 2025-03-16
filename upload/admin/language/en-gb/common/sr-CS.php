7f0ffb7bf7179979677a65aa2246a491614cd173
OSWorX <info@osworx.net>
2025-03-16 14:28:16 +0100

    New translations footer.php (Serbian (Latin))

--- upload/admin/language/en-gb/common/sr-CS.php
+++ upload/admin/language/en-gb/common/sr-CS.php
@@ -1,23 +1,4 @@
 <?php
-// Heading
-$_['heading_title']    = 'Menadžer Slika';
-
 // Text
-$_['text_uploaded']    = 'Uspeh: Vaš fajl je dodat!';
-$_['text_directory']   = 'Useh: Fascikla kreirana!';
-$_['text_delete']      = 'Uspeh: Vaš Fajl ili Direktorijum jeuklonjen!';
-
-// Entry
-$_['entry_search']     = 'Traži..';
-$_['entry_folder']     = 'Ime Foldera';
-
-// Error
-$_['error_permission'] = 'Upozornje: Dozvola Odbijena!';
-$_['error_filename']   = 'Warning: Filename must be between 3 and 255!';
-$_['error_folder']     = 'Warning: Folder name must be between 3 and 255!';
-$_['error_exists']     = 'Upozorenje: Fajl ili fascikla sa istim imenom već postoji!';
-$_['error_directory']  = 'Upozorenje: Fascikla ne postoji!';
-$_['error_filesize']   = 'Warning: Incorrect file size!';
-$_['error_filetype']   = 'Upozorenje: Ne važeći tip fajla!';
-$_['error_upload']     = 'Upozorenje: Fajl ne može biti dodan iz ne poznatih razloga!';
-$_['error_delete']     = 'Upozorenje: Ne možete ukloniti ovu fasciklu!';
+$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Verzija %s';
