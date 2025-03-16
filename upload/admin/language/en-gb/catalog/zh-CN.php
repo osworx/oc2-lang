1b583b639422dcdcb43430776fedebee1057dbf9
OSWorX <info@osworx.net>
2025-03-16 11:06:58 +0100

    New translations category.php (Chinese Simplified)

--- upload/admin/language/en-gb/catalog/zh-CN.php
+++ upload/admin/language/en-gb/catalog/zh-CN.php
@@ -1,25 +1,46 @@
 <?php
 // Heading
-$_['heading_title']     = '属性组';
+$_['heading_title']          = '目录';
 
 // Text
-$_['text_success']      = '成功：你成功修改了属性组！';
-$_['text_list']         = '属性组列表';
-$_['text_add']          = '增加属性组';
-$_['text_edit']         = '编辑属性组';
+$_['text_success']           = '成功:你已修改了属性!';
+$_['text_list']              = '类别列表';
+$_['text_add']               = '添加产品分类';
+$_['text_edit']              = '编辑产品分类';
+$_['text_default']           = '默认';
 
 // Column
-$_['column_name']       = '属性组名';
-$_['column_sort_order'] = '排列顺序';
-$_['column_action']     = '操作';
+$_['column_name']            = '产品分类名称';
+$_['column_sort_order']      = '排列顺序';
+$_['column_action']          = '操作';
 
 // Entry
-$_['entry_name']        = '属性组名';
-$_['entry_sort_order']  = '排列顺序';
+$_['entry_name']             = '产品分类名称';
+$_['entry_description']      = '描述';
+$_['entry_meta_title'] 	     = '标签名';
+$_['entry_meta_keyword']     = '关键词';
+$_['entry_meta_description'] = '标签描述';
+$_['entry_keyword']          = 'SEO URL';
+$_['entry_parent']           = '父级';
+$_['entry_filter']           = '筛选器';
+$_['entry_store']            = '商店';
+$_['entry_image']            = '图像';
+$_['entry_top']              = '顶部';
+$_['entry_column']           = '分栏';
+$_['entry_sort_order']       = '排列顺序';
+$_['entry_status']           = '状态';
+$_['entry_layout']           = '布局';
+
+// Help
+$_['help_filter']            = '自动完成';
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+$_['help_top']               = '在顶部菜单栏中显示.仅适用于一级产品分类';
+$_['help_column']            = '设置页底三个分类的分栏数.(仅适用于一级分类)';
 
 // Error
-$_['error_warning']     = '警告:请仔细检查错误格式!';
-$_['error_permission']  = '警告：你没有修改属性组的权限！';
-$_['error_name']        = '属性组名必须在3至64个字符间！';
-$_['error_attribute']   = '警告：这个属性组不能删除,因为他现在已分配给%s属性!';
-$_['error_product']     = '警告：这个属性组不能删除,因为他现在已分配给%s产品!';
+$_['error_warning']          = '警告:请仔细检查错误格式!';
+$_['error_permission']       = '警告:您没有权限修改产品分类!';
+$_['error_name']             = 'Category Name must be between 2 and 255 characters!';
+$_['error_meta_title']       = '元标题必须3-255个字符之间!';
+$_['error_keyword']          = 'SEO URL already in use!';
+$_['error_parent']           = 'The parent category you have chosen is a child of the current one!';
